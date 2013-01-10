e3448ca5be249ab5e71de3640a06ffffc880f7ef
iocikun.jujon <PAF01143@nifty.ne.jp>
2013-01-10 17:42:12 +0900

    add wfdbg for Warshall-Floyd debug

--- algorithms/WarshallFloyd/forth/Main.hs
+++ algorithms/WarshallFloyd/forth/Main.hs
@@ -1,3 +1,5 @@
+import Control.Monad
+
 test :: [[Int]]
 test = [
 	[  0, 18,999, 14,999],
@@ -17,3 +19,27 @@ for is s body = foldr body s is
 
 collect :: [a] -> (a -> b) -> [b]
 collect = flip map
+
+--------------------------------------------------------------------------------
+
+wfdbg :: Int -> [[Int]] -> IO [[Int]]
+wfdbg s ps0 = do
+	ret <- forM' [0 .. s] ps0 $ \v ps -> do
+		if v < 1 then putStrLn "初期値" else putStrLn $ "Via = " ++ show v
+		putStrLn $ unlines $ map (unwords . map (keta 3 . show)) ps
+		return $ collect [0 .. s] $ \f ->
+			collect [0 .. s] $ \t ->
+				min (ps !! f !! t) (ps !! f !! v + ps !! v !! t)
+	putStrLn $ "Via = " ++ show (s + 1)
+	putStrLn $ unlines $ map (unwords . map (keta 3 . show)) ret
+	return ret
+
+forM' :: Monad m => [a] -> b -> (a -> b -> m b) -> m b
+forM' is s body = foldM (flip body) s is
+
+next v s ps =
+	collect [0 .. s] $ \f ->
+		collect [0 .. s] $ \t ->
+			min (ps !! f !! t) (ps !! f !! v + ps !! v !! t)
+
+keta n s = replicate (n - length s) ' ' ++ s
