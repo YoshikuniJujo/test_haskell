5bf0d2acdb14c6eea774b0d14067c17f45b4e8fc
iocikun.juj <PAF01143@nifty.ne.jp>
2013-08-17 14:35:43 +0900

    use wrapper for Env

--- books/stone/Eval.hs
+++ books/stone/Eval.hs
@@ -29,10 +29,18 @@ showObject o = error $ "showObject: " ++ show o
 
 type Env = [(String, Object)]
 
-eval :: Program -> State Env [Object]
+putValue :: String -> Object -> Env -> Env
+putValue var val = ((var, val) :)
+
+getValue :: String -> Env -> Object
+getValue var = fromJust . lookup var
+
+type Eval = State Env
+
+eval :: Program -> Eval [Object]
 eval = mapM evalStatement
 
-evalStatement :: Statement -> State Env Object
+evalStatement :: Statement -> Eval Object
 evalStatement (If e tb eb) = do
 	b <- evalPrimary e
 	case b of
@@ -47,14 +55,14 @@ evalStatement (While e blk) = do
 			evalStatement (While e blk)
 evalStatement (Expr e) = evalPrimary e
 
-evalPrimary :: Primary -> State Env Object
+evalPrimary :: Primary -> Eval Object
 evalPrimary (PNumber n) = return $ ONumber n
-evalPrimary (PIdentifier i) = gets $ fromJust . lookup i
+evalPrimary (PIdentifier i) = gets $ getValue i
 evalPrimary (PString s) = return $ OString s
 evalPrimary (POp _) = error "evalPrimary: can't eval"
 evalPrimary (PInfix (PIdentifier i) "=" p) = do
 	r <- evalPrimary p
-	modify ((i, r) :)
+	modify $ putValue i r
 	return r
 evalPrimary (PInfix pl o pr) = do
 	l <- evalPrimary pl
