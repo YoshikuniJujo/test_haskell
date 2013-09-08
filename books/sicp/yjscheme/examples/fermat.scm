20974d61578ad864ba768954929ed01e87481251
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2013-09-08 10:52:32 +0900

    add scheme examples

---
+++ books/sicp/yjscheme/examples/fermat.scm
@@ -0,0 +1,13 @@
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+	((even? exp)
+	 (remainder (square (expmod base (/ exp 2) m))
+		    m))
+	(else
+	  (remainder (* base (expmod base (- exp 1) m))
+		     m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
