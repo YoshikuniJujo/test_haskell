5ef99911cd67571f3d771dab3116ae1b90810555
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2013-09-15 09:44:58 +0900

    add scheme example

---
+++ books/sicp/yjscheme/examples/list_exercise.scm
@@ -0,0 +1,28 @@
+(define (last-pair lst)
+  (if (null? (cdr lst))
+    lst
+    (last-pair (cdr lst))))
+
+(define (reverse lst)
+  (define (iter o r)
+    (if (null? o)
+      r
+      (iter (cdr o) (cons (car o) r))))
+  (iter lst ()))
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+	((or (< amount 0) (no-more? coin-values)) 0)
+	(else
+	  (+ (cc amount
+		 (except-first-denomination coin-values))
+	     (cc (- amount
+		    (first-denomination coin-values))
+		 coin-values)))))
+
+(define (no-more? cv) (null? cv))
+(define (first-denomination cv) (car cv))
+(define (except-first-denomination cv) (cdr cv))
+
+(define us-coins (list 50 25 10 5 1))
+(define uk-coins (list 100 50 20 10 5 2 1 0.5))
