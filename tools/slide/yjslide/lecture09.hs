933cf438904d9def753fcb8b1629301259ce3076
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2013-10-11 17:54:30 +0900

    edit lecture: add lecture 09

---
+++ tools/slide/yjslide/lecture09.hs
@@ -0,0 +1,192 @@
+module Main where
+
+import Lecture
+
+subtitle :: String
+subtitle = "第9回 落ち穂拾い"
+
+main :: IO ()
+main = runLecture pages
+
+pages :: [Page]
+pages = [
+	titlePage, prelude, index,
+	index' 1, comment,
+	index' 2, literate,
+	index' 3, offside1, offside2,
+	index' 4, whereClause, letin, doLet,
+	index' 5, topPatMatch, lambdaPatMatch, doPatMatch,
+	index' 6
+ ]
+
+titlePage :: Page
+titlePage = [flip writeTitle subtitle]
+
+prelude :: Page
+prelude = [\t -> do
+	writeTopTitle t "はじめに"
+	text t "", \t -> do
+	text t "* 初級編最終回", \t -> do
+	text t "* 今までの講義で触れられなかった機能について", \t -> do
+	text t "* 主な機能については今回で網羅することを目指す", \t -> do
+	itext t 1 "- ffiについては中級編で", \t -> do
+	itext t 1 "- template haskellについては上級編で"
+ ]
+
+contentsList :: [String]
+contentsList = [
+	"コメント",
+	"文芸的プログラミング",
+	"オフサイドルール",
+	"letやwhereによる変数のローカルな定義",
+	"いろいろな場所でのパターンマッチ",
+	"アズパターン",
+	"不可反駁パターン",
+	"フィールドラベル",
+	"パターンガード",
+	"ビューパターン"
+ ]
+
+index :: Page
+index = (\t -> writeTopTitle t "今回の内容") : makeRedList contentsList 0
+
+index' :: Int -> Page
+index' i = (: []) $ \t -> writeTopTitle t "今回の内容" >>
+	oneshot t (mapM_ ($ t) $ makeRedList contentsList i)
+
+comment :: Page
+comment = [\t -> do
+	writeTopTitle t "コメント"
+	text t "", \t -> do
+	text t "* 行コメントとブロックコメントとがある", \t -> do
+	text t "* 行コメントは'--'から改行まで", \t -> do
+	text t "* ブロックコメントは'{-'から'-}'まで", \t -> do
+	text t "* ブロックコメントはネスト可"
+ ]
+
+redPage :: (Turtle -> IO ()) -> Turtle -> IO ()
+redPage act t = withRed t $ act t
+
+makeRedList :: [String] -> Int -> Page
+makeRedList [] _ = []
+makeRedList (i : is) 1 = redPage (flip text $ "* " ++ i) : makeRedList is 0
+makeRedList (i : is) n = flip text ("* " ++ i) : makeRedList is (n - 1)
+
+literate :: Page
+literate = [\t -> do
+	writeTopTitle t "文芸的プログラミング"
+	text t "", \t -> do
+	text t "* 拡張子を.lhsとする", \t -> do
+	text t "* 普通に書いたものはコメント", \t -> do
+	text t "* '> 'で始まる行がプログラム", \t -> do
+	itext t 1 "- '>'の後のスペースは必要", \t -> do
+	itext t 1 "- コメントとコードの間には空行が必要", \t -> do
+	text t "* '\\begin{code}'から'\\end{code}'までの間もコードとなる"
+ ]
+
+offside1 :: Page
+offside1 = [\t -> do
+	writeTopTitle t "オフサイドルール"
+	text t "", \t -> do
+	text t "* Haskellでは'{', '}', ';'が省略できる", \t -> do
+	text t "* '{'の省略があった場合にオフサイドルールが始まる", \t -> do
+	text t "* '{'の省略の次の単語の先頭がインデントの基準となる", \t -> do
+	text t "* 基準よりも深いインデントであれば何もしない", \t -> do
+	text t "* 基準と同じインデントであれば直前に';'を挿入する", \t -> do
+	text t "* 基準より浅いインデントであれば直前に'}'を挿入する", \t -> do
+	text t "* パースに失敗した際にはそこに'}'を入れて再度パース", \t -> do
+	dvArrowShort t
+	semititle t "直観的に書けるようになっている"
+ ]
+
+offside2 :: Page
+offside2 = [\t -> do
+	writeTopTitle t "オフサイドルール"
+	text t "", \t -> do
+	text t "do someIO"
+	itext t 0.5 "otherIO"
+	itext t 0.5 "anotherIO", \t -> do
+	dvArrowShort t
+	text t "do { someIO"
+	itext t 0.8 ";otherIO"
+	itext t 0.8 ";otherIO"
+	itext t 0.8 ";anotherIO"
+	text t "}"
+ ]
+
+whereClause :: Page
+whereClause = [\t -> do
+	writeTopTitle t "where節"
+	text t "", \t -> do
+	text t "* 関数定義にはwhere節がつけられる", \t -> do
+	text t "* ガードにまたがるローカルな変数", \t -> do
+	text t "* スコープ以外はトップでの定義と同じ"
+	text t "", \t -> do
+	text t "例:"
+	text t "fun x"
+	itext t 0.5 "| x < 10 = x2"
+	itext t 0.5 "| otherwise = x2 + x2"
+	itext t 0.5 "where x2 = x * x"
+ ]
+
+letin :: Page
+letin = [\t -> do
+	writeTopTitle t "let ... in ..."
+	text t "", \t -> do
+	text t "* let [定義] in [表現]で定義を[表現]中で使える"
+	text t "", \t -> do
+	text t "例:"
+	text t "let x = 3 + 9 in x + x => 24"
+ ]
+
+doLet :: Page
+doLet = [\t -> do
+	writeTopTitle t "do内でのlet"
+	text t "", \t -> do
+	text t "* do構文のなかでもletは使える", \t -> do
+	text t "* この場合スコープはlet以降のdo内部"
+	text t "", \t -> do
+	text t "例:"
+	text t "do someIO"
+	itext t 0.5 "let x = 8"
+	itext t 0.5 "otherIO x"
+ ]
+
+topPatMatch :: Page
+topPatMatch = [\t -> do
+	writeTopTitle t "トップでのパターンマッチ", \t -> do
+	text t "* モジュールトップでもパターンマッチは使える", \t -> do
+	itext t 1 "- let構文、where節でも同様"
+	text t "", \t -> do
+	text t "例:"
+	text t "x, y :: Int"
+	text t "(x, y) = (3, 8)"
+	text t "", \t -> do
+	text t "z, w :: Double"
+	text t "[z, w] = [9, 3.8]"
+	text t "", \t -> do
+	text t "h :: Char; ello :: String"
+	text t "h : ello = \"hello\""
+ ]
+
+lambdaPatMatch :: Page
+lambdaPatMatch = [\t -> do
+	writeTopTitle t "関数リテラルでのパターンマッチ"
+	text t "", \t -> do
+	text t "* 関数リテラルの引数部分でもパターンマッチは可能"
+	text t "", \t -> do
+	text t "例:"
+	text t "\\(_ : xs) -> xs"
+ ]
+
+doPatMatch :: Page
+doPatMatch = [\t -> do
+	writeTopTitle t "do構文内でのパターンマッチ"
+	text t "", \t -> do
+	text t "* [パターン] <- [表現]という形"
+	text t "", \t -> do
+	text t "例:"
+	text t "do someIO"
+	itext t 0.5 "x : xs <- retListIO"
+	itext t 0.5 "otherIO x"
+ ]
