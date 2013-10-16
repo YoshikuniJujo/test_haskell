49717ea552173dc2fdb2409e6f3142a55a38aa67
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2013-10-16 17:20:29 +0900

    edit lecture: add whnf to test.hs

--- tools/slide/yjslide/examples/test.hs
+++ tools/slide/yjslide/examples/test.hs
@@ -6,3 +6,15 @@ some xs@(x : _) = x : xs
 some [] = []
 
 fib@(1:tfib) = 1 : 1 : [ a + b | (a, b) <- zip fib tfib ]
+
+-- before
+bfr = (\x y -> \z -> y ((\u -> u) z) x) 25
+
+-- weak head normal form
+whnf = \y -> (\z -> y ((\u -> u) z)) 25
+
+-- head normal form
+hnf = \y -> y ((\u -> u) 25)
+
+-- normal form
+nf = \y -> y 25
