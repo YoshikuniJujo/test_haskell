a6d8e766c67c16c0c84d3bb6b4ade6eee8371af5
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2013-10-16 11:16:03 +0900

    edit lecture: add compiler to lecture 10

--- tools/slide/yjslide/lecture10.hs
+++ tools/slide/yjslide/lecture10.hs
@@ -12,7 +12,8 @@ pages :: [Page]
 pages = [
 	titlePage, prelude,
 	interpreter1, interpreter2,
-	interactive
+	interactive1, interactive2,
+	compiler1, compiler2, compiler3, compiler4
  ]
 
 titlePage :: Page
@@ -63,16 +64,24 @@ interpreter2 = [\t -> do
 	itext t 1 "- runghc -Wall [ファイル名]とするのは良い習慣"
  ]
 
-interactive :: Page
-interactive = [\t -> do
-	writeTopTitle t "対話的環境", \t -> do
+interactive1 :: Page
+interactive1 = [\t -> do
+	writeTopTitle t "対話的環境"
+	text t "", \t -> do
 	text t "* ghci [ファイル名]", \t -> do
 	itext t 1 "- モジュールを読み込み対話環境に入る", \t -> do
-	itext t 1 "- ファイル名の指定なければPreludeだけを読み込む", \t -> do
+	itext t 1 "- ファイル名の指定なければPreludeだけを読み込む"
+ ]
+
+interactive2 :: Page
+interactive2 = [\t -> do
+	writeTopTitle t "対話的環境"
+	text t "", \t -> do
 	text t "* 対話環境において", \t -> do
 	itext t 1 "- 式を入力すると値を表示", \t -> do
 	itext t 1 "- IO aは実行し返値を表示できれば表示", \t -> do
 	itext t 1 "- let [パターン] = [表現]で変数束縛", \t -> do
+	itext t 1 "- :m [モジュール名]でモジュールの読み込み", \t -> do
 	itext t 1 "- :m + [モジュール名]でモジュールの追加読み込み", \t -> do
 	itext t 1 "- :reloadでモジュールの再読み込み", \t -> do
 	itext t 1 "- :t [表現]で型を表示", \t -> do
@@ -80,11 +89,64 @@ interactive = [\t -> do
 	itext t 1 "- :run act arg1 arg2 ...でコマンドライン引数指定"
  ]
 
-version :: Page
-version = [\t -> do
-	writeTopTitle t "インタプリタ"
+compiler1 :: Page
+compiler1 = [\t -> do
+	writeTopTitle t "コンパイラ"
 	text t "", \t -> do
-	text t "% runghc --version", \t -> do
-	text t "The Glorious Glasgow Haskell Compilation System,"
-	text t "version 7.6.3"
+	text t "* とりあえず ghc --version", \t -> do
+	itext t 1 "The Glorious Glasgow Haskell Compilation System,"
+	itext t 1 "version 7.6.3", \t -> do
+	text t "* 最も単純な例", \t -> do
+	itext t 1 "ghc hello.hs"
+	itext t 1 "./hello => Hello, world!", \t -> do
+	text t "* ghcはimportされているモジュールを追いかけてくれる"
+	text t "* 更新されているモジュールだけを再コンパイルしてくれる"
+	arrowIText t 1 "makeの機能が組み込まれているということ"
+ ]
+
+compiler2 :: Page
+compiler2 = [\t -> do
+	writeTopTitle t "コンパイラ"
+	text t "", \t -> do
+	text t "* -Wallをつけよう", \t -> do
+	itext t 1 "ghc -Wall hello.hs", \t -> do
+	itext t 1 "- 良くない(かもしれない)コードを指摘してくれる", \t -> do
+	text t "* C言語で書かれたコードを使うとき", \t -> do
+	itext t 1 "- 詳細はFFIの回で説明する", \t -> do
+	itext t 1 "ghc h_code.hs c_code.c", \t -> do
+	itext t 1 "ghc h_code.hs c_code.o (オブジェクトファイル)", \t -> do
+	text t "* モジュールの探索パス", \t -> do
+	itext t 1 "- モジュールA.B.Cは./A/B/C.hsに置く", \t -> do
+	itext t 1 "- d1/A/B/C.hsに置きたい場合、-id1をつける", \t -> do
+	itext t 1 "-id1:d2:d3... と複数指定することもできる"
+ ]
+
+compiler3 :: Page
+compiler3 = [\t -> do
+	writeTopTitle t "コンパイラ"
+	text t "", \t -> do
+	text t "* 出力ファイルの名前を指定する", \t -> do
+	itext t 1 "ghc -o goodbye hello.hs", \t -> do
+	text t "* 分割コンパイル", \t -> do
+	itext t 1 "ghc -c A.hs"
+	itext t 1 "ghc -c B.hs"
+	itext t 1 "ghc -c Main.hs"
+	itext t 1 "ghc -o foo A.o B.o Main.o"
+ ]
+
+compiler4 :: Page
+compiler4 = [\t -> do
+	writeTopTitle t "コンパイラ"
+	text t "", \t -> do
+	text t "* スタックサイズ", \t -> do
+	itext t 1 "./some"
+	itext t 1 "Stack space overflow: ...", \t -> do
+	itext t 1 "こういうとき、まずはソースコードを確認", \t -> do
+	itext t 2 "無限ループになってしまっていないか?", \t -> do
+	arrowIText t 1 "問題無い、たくさんスタックを使いそうなプログラム", \t -> do
+	dvArrowShort t
+	text t "ghc some.hs -rtsopts"
+	text t "./some +RTS -K400m -RTS", \t -> do
+	itext t 1 "- スタックサイズをデフォルトの8Mから400Mに"
+	arrowIText t 1 "ghc some.hs -with-rtsopts=\"-K400m\"としても"
  ]
