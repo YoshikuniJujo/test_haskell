598fc664ee2bb4d2c7e67f7f2dc39453f1cd7878
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2013-11-05 10:25:49 +0900

    edit lecture: lecture 16: begin data families

--- tools/slide/yjslide/examples/testDataFamilies.hs
+++ tools/slide/yjslide/examples/testDataFamilies.hs
@@ -1,10 +1,12 @@
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeFamilies, FlexibleContexts #-}
 
 import Data.Word
 import Data.Bits
 import Data.Either
 import Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as C (pack)
+
+import Data.Char
 
 data family Pack a
 
@@ -43,3 +45,60 @@ instance Packable Bool where
 
 instance Packable Char where
 	pack = packChar
+
+class Some a where
+	data Hoge a
+
+-- data instance Hoge Char = Int
+
+data family Identity a
+
+data instance Identity Int = PrimeFactors [Int]
+	deriving Show
+
+primeFactors :: Int -> Int -> [Int]
+primeFactors f n
+	| f <= 0 || n <= 0 = []
+	| f > n = []
+	| n `mod` f == 0 = f : primeFactors f (n `div` f)
+	| otherwise = primeFactors (f + 1) n
+
+toIdentityInt :: Int -> Maybe (Identity Int)
+toIdentityInt n
+	| n > 0 = Just $ PrimeFactors $ primeFactors 2 n
+	| otherwise = Nothing
+
+fromIdentityInt :: Identity Int -> Int
+fromIdentityInt (PrimeFactors pfs) = product pfs
+
+data CharClass = Upper | Lower | Digit deriving Show
+
+data instance Identity Char = CharID CharClass Int
+	deriving Show
+
+toIdentityChar :: Char -> Maybe (Identity Char)
+toIdentityChar c
+	| isUpper c = Just $ CharID Upper $ ord c - ord 'A'
+	| isLower c = Just $ CharID Lower $ ord c - ord 'a'
+	| isDigit c = Just $ CharID Digit $ ord c - ord '0'
+	| otherwise = Nothing
+
+fromIdentityChar :: Identity Char -> Char
+fromIdentityChar (CharID Upper n) = chr $ ord 'A' + n
+fromIdentityChar (CharID Lower n) = chr $ ord 'a' + n
+fromIdentityChar (CharID Digit n) = chr $ ord '0' + n
+
+class HaveIdentity a where
+	toIdentity :: a -> Maybe (Identity a)
+	fromIdentity :: Identity a -> a
+
+instance HaveIdentity Int where
+	toIdentity = toIdentityInt
+	fromIdentity = fromIdentityInt
+
+instance HaveIdentity Char where
+	toIdentity = toIdentityChar
+	fromIdentity = fromIdentityChar
+
+printIdentity :: (HaveIdentity a, Show (Identity a)) => a -> IO ()
+printIdentity = print . toIdentity
--- tools/slide/yjslide/lecture16.hs
+++ tools/slide/yjslide/lecture16.hs
@@ -10,7 +10,8 @@ main = runLecture pages
 
 pages :: [Page]
 pages = [
-	titlePage, prelude, classify
+	titlePage, prelude, whats, classify,
+	dataFamilies, identity, identityFamily
  ]
 
 titlePage :: Page
@@ -26,11 +27,74 @@ prelude = [\t -> do
 	text t "* ソースコードに{-# LANGUAGE TypeFamilies #-}をつける"
  ]
 
+whats :: Page
+whats = [\t -> do
+	writeTopTitle t "型族とは?"
+	text t "", \t -> do
+	text t "* Maybeについて見てみよう", \t -> do
+	itext t 1 "data Maybe a = Just a | Nothing", \t -> do
+	itext t 1 "- Maybeはすべての型aに対して同じ構造を取る", \t -> do
+	text t "* Hoge IntとHoge Charとで違う構造の型を作りたい", \t -> do
+	itext t 1 "- 今までの枠組では不可能", \t -> do
+	itext t 1 "- Hoge aはaごとに違う定義を取らなければならない", \t -> do
+	text t "* 中身の構造の違うデータ型や型シノニムをひとまとまりに", \t -> do
+	arrowIText t 1 "それが型族", \t -> do
+	arrowIText t 1 "具体例を挙げないとよくわからないと思う"
+ ]
+
 classify :: Page
 classify = [\t -> do
 	writeTopTitle t "分類"
 	text t "", \t -> do
 	text t "* 型族(type families)には以下の2つがある", \t -> do
-	itext t 1 "- data families", \t -> do
-	itext t 1 "- type synonym families"
+	itext t 1 "- データ族(data families)", \t -> do
+	itext t 1 "- 型シノニム族(type synonym families)"
+	text t "", \t -> do
+	text t "* 上のそれぞれについて関連型(associated)が対応する", \t -> do
+	itext t 1 "- 関連データ型(associated data type)", \t -> do
+	itext t 1 "- 関連型シノニム(associated type synonym)", \t -> do
+	text t "* 関連型は型族の構文糖なので、型族が理解できれば良い"
+ ]
+
+dataFamilies :: Page
+dataFamilies = [\t -> do
+	writeTopTitle t "データ族"
+	text t "", \t -> do
+	text t "* まずは普通の変数を含むデータ型について見てみる", \t -> do
+	text t "* 例としてリストを考えてみよう", \t -> do
+	itext t 1 "data [] a = a : ([] a) | []", \t -> do
+	itext t 1 "わかりやすく名前を変えてみる", \t -> do
+	itext t 1 "data List a = Cons a (List a) | Nil", \t -> do
+	text t "* 型aが何であっても同じ構造を共有している", \t -> do
+	text t "* 同じ構造でない同じ種類のデータ型も考えられる", \t -> do
+	text t "* 違う構造のデータ型を同じ種類の型としてまとめたい"
+ ]
+
+identity :: Page
+identity = [\t -> do
+	writeTopTitle t "データ族"
+	text t "", \t -> do
+	text t "* たとえば、値のアイデンティティを考えようと思う", \t -> do
+	itext t 1 "- 整数のアイデンティティは素因数分解の結果", \t -> do
+	itext t 1 "- 文字のアイデンティティは文字の種類と何番目か", \t -> do
+	itext t 1 "- と、勝手に決めたとする", \t -> do
+	text t "* Identity Intは素因数分解の結果を格納", \t -> do
+	text t "* Identity Charは文字の種類と何番目かを格納", \t -> do
+	text t "* data Identity a = ...のような定義は不可能", \t -> do
+	text t "* 型族を使う"
+ ]
+
+identityFamily :: Page
+identityFamily = [\t -> do
+	writeTopTitle t "データ族"
+	text t "", \t -> do
+	text t "data family Identity a"
+	text t "", \t -> do
+	text t "* データ族を宣言した", \t -> do
+	itext t 1 "- Identity IntやIdentity Charを別々に定義できる"
+	text t "", \t -> do
+	text t "data instance Identity Int = PrimeFactors [Int]"
+	text t "", \t -> do
+	text t "data CharClass = Upper | Lower | Digit"
+	text t "data instance Identity Char = CharID CharClass Int"
  ]
