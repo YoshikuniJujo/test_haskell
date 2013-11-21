7c813cefd04c7c2d58a783eeed259c20a6af8592
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2013-11-21 10:13:36 +0900

    edit lecture: simplify type hierarchy example

--- tools/slide/yjslide/examples/typeHierarchy.hs
+++ tools/slide/yjslide/examples/typeHierarchy.hs
@@ -1,22 +1,16 @@
 {-# LANGUAGE ExistentialQuantification, DeriveDataTypeable #-}
 
 import Data.Typeable
-import Control.Applicative
 import Data.Maybe
-
-class HasName n where
-	name :: n -> String
 
 data SomeLife = forall l . Life l => SomeLife l deriving Typeable
 
-class (Typeable l, HasName l, Show l) => Life l where
+class (Typeable l, Show l) => Life l where
 	toLife :: l -> SomeLife
 	fromLife :: SomeLife -> Maybe l
+
 	toLife = SomeLife
 	fromLife (SomeLife l) = cast l
-
-instance HasName SomeLife where
-	name (SomeLife l) = name l
 
 instance Show SomeLife where
 	showsPrec d (SomeLife l) = showParen (d > app_prec) $
@@ -29,9 +23,6 @@ instance Life SomeLife where
 	fromLife = Just
 
 data Animal = forall a . Life a => Animal a deriving Typeable
-
-instance HasName Animal where
-	name (Animal a) = name a
 
 instance Show Animal where
 	showsPrec d (Animal a) = showParen (d > app_prec) $
@@ -51,25 +42,15 @@ animalFromLife l = do
 
 data Dog = Dog String deriving (Typeable, Show)
 
-instance HasName Dog where
-	name (Dog n) = n
-
 instance Life Dog where
 	toLife = animalToLife
 	fromLife = animalFromLife
 
 data Cat = Cat String deriving (Typeable, Show)
 
-instance HasName Cat where
-	name (Cat n) = n
-
 instance Life Cat where
 	toLife = animalToLife
 	fromLife = animalFromLife
-
-showName :: Life l => l -> String
-showName l = case toLife l of
-	SomeLife l' -> name l'
 
 castLife :: (Life l, Life m) => l -> Maybe m
 castLife = fromLife . toLife
