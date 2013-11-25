4649e114ce53fd74d91b1a045c0a3756ee8c1f13
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2013-11-25 17:22:16 +0900

    edit lecture: finish lecture 11

--- tools/slide/yjslide/lecture11.hs
+++ tools/slide/yjslide/lecture11.hs
@@ -14,7 +14,10 @@ pages = [
 	useGetContents, useGetContents2, useGetContents3, useGetContents4,
 	useGetContents5,
 	usePutStr, badReadWrite, badReadWrite2,
-	useGetLine, useOpenFile, useOpenFile2, useHIsEOF
+	useGetLine, useOpenFile, useOpenFile2, useHIsEOF,
+	charCode, newLine, buffering, binaryMode, useSeek, useWait,
+	basic, basic2, basic3,
+	summary
  ]
 
 titlePage :: Page
@@ -38,7 +41,6 @@ useInteract = [\t -> do
 	itext t 1 "- 標準入力から入力を受け取り", \t -> do
 	itext t 1 "- 何らかの加工を行い", \t -> do
 	itext t 1 "- 標準出力に書き出す", \t -> do
-	itext t 1 "- 入力と出力は並行して行われる", \t -> do
 	text t "* 標準入力はデフォルトではキーボードからのインプット", \t -> do
 	text t "* 標準出力はデフォルトではモニタへの出力", \t -> do
 	text t "* 標準入出力はファイルにつなぎかえることができる", \t -> do
@@ -281,7 +283,7 @@ useOpenFile2 = [\t -> do
 
 useHIsEOF :: Page
 useHIsEOF = [\t -> do
-	writeTopTitle t "ファイルの終了をチェック"
+	writeTopTitle t "ファイルの内容を表示する"
 	text t "", \t -> do
 	text t "* ファイルの終了をチェックする関数", \t -> do
 	itext t 1 "hIsEOF :: Handle -> IO Bool", \t -> do
@@ -294,4 +296,147 @@ useHIsEOF = [\t -> do
 	itext t 2 "if eof"
 	itext t 3 "then return ()"
 	itext t 3 "else hGetLine h >>= putStrLn"
+ ]
+
+charCode :: Page
+charCode = [\t -> do
+	writeTopTitle t "文字コード"
+	text t "", \t -> do
+	text t "* 文字(列)の読み込み、書き出し関数は文字コードを考慮", \t -> do
+	text t "* Haskellの文字はユニコードで保存されているので", \t -> do
+	itext t 1 "- 読み込み時にはユニコードへ変換", \t -> do
+	itext t 1 "- 書き出し時にはユニコードから変換", \t -> do
+	text t "* システムのデフォルトのエンコードを調べる", \t -> do
+	itext t 1 "> localeEncoding"
+	itext t 1 "UTF-8", \t -> do
+	text t "* Handleごとにエンコードを設定できる", \t -> do
+	itext t 1 "hSetEncoding :: Handle -> TextEncoding -> IO ()", \t -> do
+	text t "* Handleごとのエンコードを調べる", \t -> do
+	itext t 1 "hGetEncoding :: Handle -> IO (Maybe TextEncoding)"
+ ]
+
+newLine :: Page
+newLine = [\t -> do
+	writeTopTitle t "改行文字"
+	text t "", \t -> do
+	text t "* 改行文字はウィンドウズとユニックス系OSとで異なる", \t -> do
+	text t "* Haskellは基本的に改行を'\\n'で表現する", \t -> do
+	text t "* よってウィンドウズでは", \t -> do
+	itext t 1 "- 読み込み時には'\\r\\n' -> '\\n'の変換を", \t -> do
+	itext t 1 "- 書き出し時には'\\n' -> '\\r\\n'の変換を行う", \t -> do
+	text t "* 改行の変換を設定する関数がある", \t -> do
+	itext t 1 "hSetNewLineMode :: Handle -> NewlineMode -> IO ()", \t -> do
+	text t "* 変換を行わない場合", \t -> do
+	itext t 1 "hSetNewLineMode h noNewlineTranslation"
+ ]
+
+buffering :: Page
+buffering = [\t -> do
+	writeTopTitle t "バッファリング"
+	text t "", \t -> do
+	text t "* バッファリングを設定することができる", \t -> do
+	itext t 1 "hSetBuffering :: Handle -> BufferMode -> IO ()", \t -> do
+	text t "* バッファリングの設定を調べる", \t -> do
+	itext t 1 "hGetBuffering :: Handle -> IO BufferMode", \t -> do
+	text t "* BufferModeには何があるか?", \t -> do
+	itext t 1 "NoBuffering", \t -> do
+	itext t 1 "LineBuffering", \t -> do
+	itext t 1 "BlockBuffering (Maybe Int)", \t -> do
+	text t "* BlockBufferingの引数は", \t -> do
+	itext t 1 "- Just nのときはn個のアイテムをバッファリングし", \t -> do
+	itext t 1 "- Nothingのときは実装依存となる"
+ ]
+
+binaryMode :: Page
+binaryMode = [\t -> do
+	writeTopTitle t "バイナリモード"
+	text t "", \t -> do
+	text t "* バイナリモードを設定する関数", \t -> do
+	itext t 1 "hSetBinaryMode :: Handle -> Bool -> IO ()", \t -> 
+	itext t 1 "- hSetBinaryMode h Trueとするのは以下と同じ", \t -> do
+	itext t 2 "hSetEncoding h char8"
+	itext t 2 "hSetNewlineMode h noNewlineTranslation", \t -> do
+	text t "* バイナリモードでファイルを開く関数が用意されている", \t -> do
+	itext t 1 "openBinaryFile :: FilePath -> IOMode -> IO Handle", \t -> do
+	itext t 1 "withBinaryFile :: FilePath -> IOMode ->"
+	itext t 4 "(Handle -> IO r) -> IO r"
+ ]
+
+useSeek :: Page
+useSeek = [\t -> do
+	writeTopTitle t "シーク"
+	text t "", \t -> do
+	text t "* ファイル中を自由に移動できる", \t -> do
+	itext t 1 "hSeek :: Handle -> SeekMode -> Integer -> IO ()", \t -> do
+	text t "* SeekModeは以下のどれか", \t -> do
+	itext t 1 "AbsoluteSeek", \t -> do
+	itext t 1 "RelativeSeek", \t -> do
+	itext t 1 "SeekFromEnd", \t -> do
+	text t "* 読んで字のごとし"
+ ]
+
+useWait :: Page
+useWait = [\t -> do
+	writeTopTitle t "入力値が使用可能かチェック"
+	text t "", \t -> do
+	text t "* チェックのときに指定された時間だけ待つ関数", \t -> do
+	itext t 1 "hWaitForInput :: Handle -> Int -> IO Bool", \t -> do
+	itext t 1 "- 指定の時間内に入力があればただちにTrueを返す", \t -> do
+	itext t 1 "- 指定の時間内に入力がなければFalseを返す", \t -> do
+	itext t 1 "- 負の時間が指定されれば入力が来るまで待つ", \t -> do
+	itext t 1 "- 時間はミリ秒で指定する", \t -> do
+	text t "* その時点での使用可能な入力値の有無を返す関数", \t -> do
+	itext t 1 "hReady :: Handle -> IO Bool"
+ ]
+
+basic :: Page
+basic = [\t -> do
+	writeTopTitle t "より低レベルな入出力"
+	text t "", \t -> do
+	text t "* よりプリミティブな入出力関数がある", \t -> do
+	text t "* ポインタで指定された番地から指定のバイト数を出力する", \t -> do
+	itext t 1 "hPutBuf :: Handle -> Ptr a -> Int -> IO ()", \t -> do
+	text t "* メモリのアロケーションとメモリへの書き込みが必要", \t -> do
+	itext t 0 "putBytes :: Handle -> [Word8] -> IO ()"
+	itext t 0 "putBytes h ws = allocaBytes (length ws) $ \\p -> do"
+	itext t 1 "pokeArray p ws"
+	itext t 1 "hPutBuf h p (length ws)"
+ ]
+
+basic2 :: Page
+basic2 = [\t -> do
+	writeTopTitle t "より低レベルな入出力"
+	text t "", \t -> do
+	text t "* ポインタで指定された番地へ指定のバイト数を入力する", \t -> do
+	itext t 1 "hGetBuf :: Handle -> Ptr a -> Int -> IO Int", \t -> do
+	itext t 1 "- 出力は読み込んだバイト数", \t -> do
+	text t "* メモリのアロケーションとメモリからの読み出しが必要", \t -> do
+	itext t 0 "getBytes :: Handle -> Int -> IO [Word8]"
+	itext t 0 "getBytes h n = allocaBytes n $ \\p -> do"
+	itext t 1 "s <- hGetBuf h p n"
+	itext t 1 "peekArray s p"
+ ]
+
+basic3 :: Page
+basic3 = [\t -> do
+	writeTopTitle t "より低レベルな入出力"
+	text t "", \t -> do
+	text t "* 入力関数は入力待ちの種類によって3種類ある", \t -> do
+	text t "* hGetBufは入力が指定されたバイト数に達するまで待つ", \t -> do
+	text t "* hGetBufSomeは入力が1バイトでもあれば実行される", \t -> do
+	text t "* hGetBufNonBlockingは入力が無くても直ちに実行される"
+ ]
+
+summary :: Page
+summary = [\t -> do
+	writeTopTitle t "まとめ"
+	text t "", \t -> do
+	text t "* interactで十分ならそれを使う", \t -> do
+	text t "* readFileは簡単だが複雑なプログラムでは避けるべき", \t -> do
+	text t "* テキストファイルならばhGetLineも選択肢になる", \t -> do
+	text t "* 文字コードや改行文字の変換を制御する関数がある", \t -> do
+	text t "* 文字コードと改行文字をバイナリモードにする関数がある", \t -> do
+	text t "* ファイルのシークについて見た", \t -> do
+	text t "* 入力値の存在をチェックする関数を見た", \t -> do
+	text t "* 細かい制御のためのよりプリミティブな関数を学んだ"
  ]
