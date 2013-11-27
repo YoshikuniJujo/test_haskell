b519561709d4737a39f89907f697dd5f51904682
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2013-11-27 15:11:35 +0900

    edit lecture: lecture 28: lazy pattern

--- tools/slide/yjslide/lecture28.hs
+++ tools/slide/yjslide/lecture28.hs
@@ -7,7 +7,8 @@ main :: IO ()
 main = runLecture [
 	[flip writeTitle subtitle], prelude,
 	refutable, irrefutable,
-	refutablePatterns, irrefutablePatterns
+	refutablePatterns, irrefutablePatterns,
+	powers, powers2, powers3, powers4, lazyPatternSummary
  ]
 
 prelude :: Page
@@ -69,4 +70,77 @@ irrefutablePatterns = [\t -> do
 	text t "* 上記以外でもパターン束縛のなかで使われている場合", \t -> do
 	itext t 1 "例: x : xs = lst, Just x <- someIO", \t -> do
 	text t "* このような場合、照合は必ず成功する"
+ ]
+
+powers :: Page
+powers = [\t -> do
+	writeTopTitle t "2の累乗を求める"
+	text t "", \t -> do
+	text t "* 2の累乗のリストを求めたいと思ったとする", \t -> do
+	text t "* いいやりかたを考えた!", \t -> do
+	text t "* 2の累乗のリストの差分リストは2の累乗のリストに等しい", \t -> do
+	itext t 1 "powers2 :: [Integer]"
+	itext t 1 "powers2 = rediff 1 powers2", \t -> do
+	text t "* 初期値と差分リストを与えるともとのリストを返す関数", \t -> do
+	itext t 1 "rediff :: Integer -> [Integer] -> [Integer]"
+	itext t 1 "rediff x0 (d : ds) = x0 : rediff (x0 + d) ds", \t -> do
+	text t "* しかし、以下は値が返らない", \t -> do
+	itext t 1 "> take 10 powers2"
+ ]
+
+powers2 :: Page
+powers2 = [\t -> do
+	writeTopTitle t "2の累乗を求める"
+	text t "", \t -> do
+	text t "* 頭のなかで考えてみると問題なく動きそうだ", \t -> do
+	itext t 1 "rediff x0 (d : ds) = x0 : rediff (x0 + d) ds", \t -> do
+	text t "* powers2の1番目の要素は1であることは明らか", \t -> do
+	itext t 1 "- よってrediff 1 powers2はrediff 1 [1, ...]となる", \t -> do
+	itext t 1 "- 変形すると1 : rediff (1 + 1) [(2番目), ...]", \t -> do
+	itext t 1 "- よって2番目の要素は2となる", \t -> do
+	itext t 1 "- 2番目の要素が2なので1 : rediff 2 [2, ...]", \t -> do
+	itext t 1 "- 変形すると1 : 2 : rediff (2 + 2) [(3番目), ...]", \t -> do
+	itext t 1 "- 3番目の要素は4となる", \t -> do
+	text t "* powers2のn番目の要素はn - 1番目の要素のみから決まる", \t -> do
+	text t "* 原理的には問題ないはずだ"
+ ]
+
+powers3 :: Page
+powers3 = [\t -> do
+	writeTopTitle t "2の累乗を求める"
+	text t "", \t -> do
+	text t "* しかし実際には動かない", \t -> do
+	text t "* 理由は?", \t -> do
+	itext t 1 "- rediffの第二引数の評価が早すぎる", \t -> do
+	itext t 1 "- rediff 1 powers2の1番目の要素は明らかに1である", \t -> do
+	itext t 1 "- しかし結果を計算する前に"
+	itext t 2 "rediffの第二引数を(d : ds)に照合する必要が", \t -> do
+	itext t 1 "- (d : ds)に照合するためにrediff 1 powers2を評価", \t -> do
+	itext t 1 "- 循環してしまう", \t -> do
+	text t "* 解決するには?", \t -> do
+	itext t 1 "- 照合を変数が使われるところまで遅らせる"
+ ]
+
+powers4 :: Page
+powers4 = [\t -> do
+	writeTopTitle t "2の累乗を求める"
+	text t "", \t -> do
+	text t "* 遅延パターンを使う", \t -> do
+	itext t 1 "- ~patという形", \t -> do
+	text t "* rediffを書き換えてみよう", \t -> do
+	itext t 1 "rediff x0 ~(d : ds) = x0 : rediff (x0 + d) ds", \t -> do
+	text t "* これを使えば良い", \t -> do
+	itext t 1 "powers2 = rediff 1 powers2"
+	itext t 1 "> take 10 powers2"
+	itext t 1 "[1,2,4,8,16,32,64,128,256,512]"
+ ]
+
+lazyPatternSummary :: Page
+lazyPatternSummary = [\t -> do
+	writeTopTitle t "遅延パターン(まとめ)"
+	text t "", \t -> do
+	text t "* ~patという形で書くと遅延パターンになる", \t -> do
+	text t "* 不可反駁パターンの名の通りこの照合は必ず成功する", \t -> do
+	text t "* 値の評価はパターン内の変数の使用時まで遅らされる", \t -> do
+	text t "* パターン束縛はデフォルトで遅延パターン"
  ]
