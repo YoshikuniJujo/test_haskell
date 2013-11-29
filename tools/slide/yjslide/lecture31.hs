aa17c958b434324561fc153550a3d9b7c1f51f03
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2013-11-29 17:26:22 +0900

    edit lecture: add lecture 31

---
+++ tools/slide/yjslide/lecture31.hs
@@ -0,0 +1,22 @@
+import Lecture
+
+subtitle :: String
+subtitle = "第31回 Map型"
+
+main :: IO ()
+main = runLecture [
+	[flip writeTitle subtitle], prelude
+ ]
+
+prelude :: Page
+prelude = [\t -> do
+	writeTopTitle t "はじめに"
+	text t "", \t -> do
+	text t "* キーと値を組にする辞書という構造がある", \t -> do
+	text t "* 最も素朴なアルゴリズムは以下の通り", \t -> do
+	itext t 1 "- (キー, 値)ペアのリストを作り", \t -> do
+	itext t 1 "- キーをひとつずつ見ていき探したいキーを見つける", \t -> do
+	itext t 1 "- それとペアになっていう値を得る", \t -> do
+	text t "* これにはO(n)時間がかかる", \t -> do
+	text t "* より効率の良い方法としてハッシュテーブルがある"
+ ]
