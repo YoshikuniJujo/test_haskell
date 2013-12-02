47d580c03144f711f8e901cf25c736a574cbd505
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2013-12-02 17:22:44 +0900

    edit lecture: finish lecture 32

--- tools/slide/yjslide/lecture32.hs
+++ tools/slide/yjslide/lecture32.hs
@@ -10,7 +10,9 @@ main = runLecture [
 	listDictSummary,
 	hashTable, hashTable2, hashTable3, hashTable4, hashTable5, hashTable6,
 	hashTable7, hashTable8, hashTableSummary,
-	treeDict
+	treeDict, treeDict2, treeDict3, treeDict4, treeDict5, treeDict6,
+	treeDictSummary,
+	attention, summary
  ]
 
 prelude :: Page
@@ -87,7 +89,7 @@ listDict5 :: Page
 listDict5 = [\t -> do
 	writeTopTitle t "リストを辞書として使う"
 	text t "", \t -> do
-	text t "* 人工的な例"
+	text t "* プロファイリング用のコード", \t -> do
 	itext t 1 "dict :: [(String, Int)]"
 	itext t 1 "dict = map (\\i -> (show i, i)) [0 .. 10 ^ 6]"
 	itext t 1 ""
@@ -216,10 +218,10 @@ hashTable7 :: Page
 hashTable7 = [\t -> do
 	writeTopTitle t "ハッシュテーブル"
 	text t "", \t -> do
-	text t "* 人工的な例", \t -> do
+	text t "* プロファイリング用のコード", \t -> do
 	itext t 1 "randomDict ::"
 	itext t 2 "HashTable String Int -> IO (Maybe Int)"
-	itext t 1 "randomDict = do"
+	itext t 1 "randomDict dict = do"
 	itext t 2 "k <- show <$> randomRIO (0, 10 ^ 6)"
 	itext t 2 "lookup k dict"
 	itext t 1 "main :: IO ()"
@@ -269,5 +271,125 @@ treeDict = [\t -> do
 	text t "", \t -> do
 	text t "* 十分に実用的な効率で", \t -> do
 	text t "* 状態変化を伴わずに辞書を実現するには", \t -> do
-	text t "* 木構造を使う"
+	text t "* 二分探索木を使う", \t -> do
+	text t "* キーは大小比較ができる必要がある", \t -> do
+	itext t 1 "- Ord k => ...ということ", \t -> do
+	text t "* 二分探索木はソート済みの構造なので", \t -> do
+	itext t 1 "- 木のトップから値の比較を始め", \t -> do
+	itext t 1 "- 探している値のほうが大きければ右へ", \t -> do
+	itext t 1 "- 小さければ左へ木を降りていき", \t -> do
+	itext t 1 "- 等しいところの値を返せば良い"
+ ]
+
+treeDict2 :: Page
+treeDict2 = [\t -> do
+	writeTopTitle t "辞書に木を使う"
+	text t "", \t -> do
+	text t "* 平衡の保たれた木であれば", \t -> do
+	text t "* n個の値に対して木の高さはlog nとなる", \t -> do
+	text t "* よって値の探索にはlog n回の比較をすれば良い", \t -> do
+	text t "* HaskellではData.MapモジュールにMap型が定義されている", \t -> do
+	text t "* Map型には前回の「重み平衡木」のアルゴリズムを採用"
+ ]
+
+treeDict3 :: Page
+treeDict3 = [\t -> do
+	writeTopTitle t "辞書に木を使う"
+	text t "", \t -> do
+	text t "* リストからの変換で新しい木を作る", \t -> do
+	itext t 1 "dict = fromList [(\"Tarou\", 33), (\"Hanako\", 28)]", \t -> do
+	text t "* 値の検索にはData.Map.lookupを使う", \t -> do
+	itext t 1 "lookup \"Hanako\" dict", \t -> do
+	text t "* 値の追加にはinsertを使う", \t -> do
+	itext t 1 "insert \"Jirou\" 29 dict", \t -> do
+	text t "* 値の削除にはdeleteを使う", \t -> do
+	itext t 1 "delete \"Hanako\" dict", \t -> do
+	text t "* 値の更新にもinsertが使える", \t -> do
+	itext t 1 "insert \"Hanako\" 29 dict"
+ ]
+
+treeDict4 :: Page
+treeDict4 = [\t -> do
+	writeTopTitle t "辞書に木を使う"
+	text t "", \t -> do
+	text t "* 前値を利用した値の更新にはadjustが使える", \t -> do
+	itext t 1 "adjust (+ 1) \"Hanako\" dict", \t -> do
+	text t "* その他、値の更新にkeyを利用する等のバリエーションが"
+ ]
+
+treeDict5 :: Page
+treeDict5 = [\t -> do
+	writeTopTitle t "辞書に木を使う"
+	text t "", \t -> do
+	text t "* プロファイリング用のコード", \t -> do
+	itext t 1 "dict :: Map String Int"
+	itext t 1 "dict = fromList $"
+	itext t 2 "map (\\i -> (show i, i)) [0 .. 10 ^ 6]"
+	itext t 1 "randomDict :: IO (Maybe Int)"
+	itext t 1 "randomDict = do"
+	itext t 2 "k <- show <$> randomRIO (0, 10 ^ 6)"
+	itext t 2 "return $ M.lookup k dict"
+	itext t 1 "main :: IO ()"
+	itext t 1 "main = (10 ^ 6) `timesDo` (randomDict >>= print)"
+ ]
+
+treeDict6 :: Page
+treeDict6 = [\t -> do
+	writeTopTitle t "辞書に木を使う"
+	text t "", \t -> do
+	text t "* 0から100万までの文字列から数への辞書を作り", \t -> do
+	text t "* ランダム値で100万回検索している", \t -> do
+	text t "* これにかかる時間が", \t -> do
+	itext t 1 "- 54.79秒で、そのうちの17.3%がrandomDict", \t -> do
+	itext t 1 "- よってrandomDictに費された時間は9.48秒", \t -> do
+	itext t 1 "- ハッシュリストを使ったものは5.69秒", \t -> do
+	text t "* O(log n)なので単純計算すると", \t -> do
+	itext t 1 "- 項目数が1兆になっても100万回の検索が20秒", \t -> do
+	itext t 1 "- ハッシュリストでその数だとO(n)に近くなる", \t -> do
+	itext t 1 "- むしろ木を使ったほうが速くなるかもしれない", \t -> do
+	text t "* つまりHashよりもMapのほうがスケールする"
+ ]
+
+treeDictSummary :: Page
+treeDictSummary = [\t -> do
+	writeTopTitle t "辞書に木を使う(まとめ)"
+	text t "", \t -> do
+	text t "* 状態変化を伴わない効率的な辞書が作れた", \t -> do
+	text t "* 平衡2分探索木を利用している", \t -> do
+	text t "* 平衡を保つアルゴリズムには「重み平衡木」を利用", \t -> do
+	text t "* 一般的なレベルの項目数ではハッシュリストに勝てない", \t -> do
+	itext t 1 "- しかし十分に競合可能な効率ではある", \t -> do
+	text t "* モンスターマシンでの作業ではハッシュリストに勝てる", \t -> do
+	itext t 1 "- とくに大量のメモリを積んだマシンでの作業", \t -> do
+	itext t 1 "- ハッシュリストも実装を変えればスケールする", \t -> do
+	itext t 1 "- Mapを使えば「そのままの」実装でスケールする", \t -> do
+	text t "* 何よりも不要な「状態変化」から逃れられたことが大きい"
+ ]
+
+attention :: Page
+attention = [\t -> do
+	writeTopTitle t "注意事項"
+	text t "", \t -> do
+	text t "* Data.MapはPreludeと同名の関数を多くエクスポート", \t -> do
+	text t "* このタイプのモジュールを使うときは修飾名を使うと良い", \t -> do
+	itext t 1 "import qualified Data.Map as M"
+	itext t 1 "M.lookup ...", \t -> do
+	text t "* あるいはより説明的に", \t -> do
+	itext t 1 "import qualified Data.Map as Map"
+	itext t 1 "Map.lookup ..."
+ ]
+
+summary :: Page
+summary = [\t -> do
+	writeTopTitle t "まとめ"
+	text t "", \t -> do
+	text t "* リストによる実装は純粋だが効率が悪い", \t -> do
+	text t "* ハッシュテーブルによる実装は効率は良いが不純だ", \t -> do
+	text t "* 平衡2分探索木による実装は純粋でしかも効率が良い", \t -> do
+	text t "* 辞書を使いたいとき", \t -> do
+	itext t 1 "- まずはリストを使って実装", \t -> do
+	itext t 1 "- そのとき十分に抽象化しておく", \t -> do
+	itext t 1 "- 効率が問題になった段階でMapにさしかえる", \t -> do
+	text t "* 関数型言語において純粋さと効率を両立できるのは木構造", \t -> do
+	text t "* 木は関数型言語の陰の主役と言える"
  ]
