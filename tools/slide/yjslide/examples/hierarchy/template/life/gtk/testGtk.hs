706ebfef7acf22b23adc044630c029211aa29e47
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2013-12-19 11:47:23 +0900

    edit lecture: hierarchy example: edit gtk

--- tools/slide/yjslide/examples/hierarchy/template/life/gtk/testGtk.hs
+++ tools/slide/yjslide/examples/hierarchy/template/life/gtk/testGtk.hs
@@ -19,21 +19,31 @@ gClass "GtkContainer" "GtkBin"
 gClass "GtkBin" "GtkButton"
 gClass "GtkBin" "GtkWindow"
 
-foreign import ccall "gtk/gtk.h gtk_init" c_gtkInit ::
-	Ptr CInt -> Ptr (Ptr CString) -> IO ()
-foreign import ccall "gtk/gtk.h gtk_main" c_gtkMain :: IO ()
-foreign import ccall "gtk/gtk.h gtk_window_new" c_gtkWindowNew ::
-	CInt -> IO (Ptr SomeGtkWindow)
-foreign import ccall "gtk/gtk.h gtk_widget_show" c_gtkWidgetShow ::
-	Ptr GtkWidget -> IO ()
-foreign import ccall "gtk/gtk.h g_signal_connect_data" c_gSignalConnectData ::
-	Ptr GtkWidget -> CString -> FunPtr (Ptr GtkWidget -> Ptr () -> IO ()) ->
-		Ptr () -> Ptr () -> CInt -> IO ()
-foreign import ccall "gtk/gtk.h gtk_main_quit" c_gtkMainQuit ::
-	Ptr GtkWidget -> Ptr () -> IO ()
 foreign import ccall "wrapper" wrapCallback ::
 	(Ptr GtkWidget -> Ptr () -> IO ()) ->
 		IO (FunPtr (Ptr GtkWidget -> Ptr () -> IO ()))
+
+foreign import ccall "gtk/gtk.h gtk_init" c_gtkInit ::
+	Ptr CInt -> Ptr (Ptr CString) -> IO ()
+foreign import ccall "gtk/gtk.h gtk_main" c_gtkMain :: IO ()
+foreign import ccall "gtk/gtk.h gtk_main_quit" c_gtkMainQuit ::
+	Ptr GtkWidget -> Ptr () -> IO ()
+foreign import ccall "gtk/gtk.h g_signal_connect_data" c_gSignalConnectData ::
+	Ptr GtkWidget -> CString -> FunPtr (Ptr GtkWidget -> Ptr () -> IO ()) ->
+		Ptr () -> Ptr () -> CInt -> IO ()
+
+foreign import ccall "gtk/gtk.h gtk_widget_show" c_gtkWidgetShow ::
+	Ptr GtkWidget -> IO ()
+foreign import ccall "gtk/gtk.h gtk_widget_show_all" c_gtkWidgetShowAll ::
+	Ptr GtkWidget -> IO ()
+
+foreign import ccall "gtk/gtk.h gtk_window_new" c_gtkWindowNew ::
+	CInt -> IO (Ptr SomeGtkWindow)
+foreign import ccall "gtk/gtk.h gtk_button_new_with_label" c_gtkButtonNewWithLabel ::
+	CString -> IO (Ptr SomeGtkButton)
+
+foreign import ccall "gtk/gtk.h gtk_container_add" c_gtkContainerAdd ::
+	Ptr GtkContainer -> Ptr GtkWidget -> IO ()
 
 withCStrings :: [String] -> ([CString] -> IO ()) -> IO ()
 withCStrings str f = bracket (mapM newCString str) (mapM_ free) f
@@ -69,8 +79,14 @@ gtkMainQuit = c_gtkMainQuit . pointer
 gtkWindowNew :: IO SomeGtkWindow
 gtkWindowNew = SomeGtkWindow <$> c_gtkWindowNew 0
 
-gtkWidgetShow :: GtkWidget -> IO ()
+gtkButtonNewWithLabel :: String -> IO SomeGtkButton
+gtkButtonNewWithLabel l = do
+	cl <- newCString l
+	SomeGtkButton <$> c_gtkButtonNewWithLabel cl
+
+gtkWidgetShow, gtkWidgetShowAll :: GtkWidget -> IO ()
 gtkWidgetShow = c_gtkWidgetShow . pointer
+gtkWidgetShowAll = c_gtkWidgetShowAll . pointer
 
 gSignalConnect :: GtkWidget -> String -> (GtkWidget -> Ptr () -> IO ()) -> IO ()
 gSignalConnect w s f = do
@@ -78,12 +94,18 @@ gSignalConnect w s f = do
 	cs <- newCString s
 	c_gSignalConnectData (pointer w) cs cb nullPtr nullPtr 0
 
+gtkContainerAdd :: GtkContainer -> GtkWidget -> IO ()
+gtkContainerAdd c w = c_gtkContainerAdd (pointer c) (pointer w)
+
 main = do
 	gtkInit
 	win <- gtkWindowNew
-	case (castGObject win) of
-		Just w -> do
-			gtkWidgetShow w
+	btn <- gtkButtonNewWithLabel "button"
+	case (castGObject win, castGObject win, castGObject btn) of
+		(Just w, Just cw, Just b) -> do
+			gtkContainerAdd cw b
+			gtkWidgetShowAll w
 			gSignalConnect w "destroy" gtkMainQuit
+			gSignalConnect b "clicked" gtkMainQuit
 			gtkMain
 		_ -> error "bad"
