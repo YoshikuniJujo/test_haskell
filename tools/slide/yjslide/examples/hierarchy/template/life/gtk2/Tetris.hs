6adc38576ceb395da0b5d7b48c0f9c1a46e1f59d
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-01-07 12:53:26 +0900

    lecture: tetris: change key bind

--- tools/slide/yjslide/examples/hierarchy/template/life/gtk2/Tetris.hs
+++ tools/slide/yjslide/examples/hierarchy/template/life/gtk2/Tetris.hs
@@ -219,12 +219,12 @@ processKey :: Keyval -> Maybe (Either LR Tick)
 processKey kv
 	| kv == char2keyval 'h' = Just $ Left L
 	| kv == char2keyval 'l' = Just $ Left R
-	| kv == char2keyval 'j' = Just $ Left RotateL
-	| kv == char2keyval 'k' = Just $ Left RotateR
+	| kv == char2keyval 'j' = Just $ Left RotateR
+	| kv == char2keyval 'k' = Just $ Left RotateL
 	| kv == char2keyval ';' = Just $ Left Hold
 	| kv == char2keyval ' ' = Just $ Left Landing
 	| kv == char2keyval '.' = Just $ Left L
 	| kv == char2keyval '/' = Just $ Left R
-	| kv == char2keyval 'z' = Just $ Left RotateL
-	| kv == char2keyval 'x' = Just $ Left RotateR
+	| kv == char2keyval 'z' = Just $ Left RotateR
+	| kv == char2keyval 'x' = Just $ Left RotateL
 	| otherwise = Nothing
