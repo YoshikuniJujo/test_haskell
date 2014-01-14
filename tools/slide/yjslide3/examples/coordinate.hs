13994a397b36b96614067909d1ddf40cb6c947f0
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-01-14 11:45:30 +0900

    lecture: lecture 03: finished

--- tools/slide/yjslide3/examples/coordinate.hs
+++ tools/slide/yjslide3/examples/coordinate.hs
@@ -6,3 +6,7 @@ dist0' (x, y) = sqrt $ x ^ (2 :: Int) + y ^ (2 :: Int)
 
 p :: (Double, Double)
 p = (6.0, 4.0)
+
+slope :: (Double, Double) -> Double
+slope (0, y) = 0
+slope (x, y) = y / x
---
+++ tools/slide/yjslide3/examples/number.hs
@@ -0,0 +1,5 @@
+intName :: Int -> String
+intName 0 = "The name is zero."
+intName 1 = "The name is one."
+intName 2 = "The name is two."
+intName _ = "I don't know."
