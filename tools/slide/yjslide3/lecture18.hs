c2d74c427b4d728522cb0c2515679debbdcc6376
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-01-30 12:18:12 +0900

    lecture: lecture 18: begin IOMcn

--- tools/slide/yjslide3/lecture18.hs
+++ tools/slide/yjslide3/lecture18.hs
@@ -1,11 +1,12 @@
 import Lecture
 
 subtitle :: String
-subtitle = "第17回 IOモナド"
+subtitle = "第18回 IOモナド"
 
 main :: IO ()
 main = runLecture [
-	[flip writeTitle subtitle], prelude, prelude2, machine
+	[flip writeTitle subtitle], prelude, prelude2,
+	machine, machine2, machine3, machine4, machine5, machine6
  ]
 
 prelude :: Page
@@ -41,5 +42,78 @@ machine = [\t -> do
 	writeTopTitle t "Machine"
 	text t "", \t -> do
 	text t "* たとえばMachine型という型があったとする", \t -> do
-	text t "* Machine型の値としてputHelloとputWorldがあったとする"
+	text t "* Machine型の値としてputHelloとputWorldがあり", \t -> do
+	itext t 1 "- それぞれ\"Hello\"と\"World\"を表示するとする", \t -> do
+	itext t 1 "putHello :: Machine", \t -> do
+	itext t 1 "putWorld :: Machine", \t -> do
+	text t "* Machine型の値をつなぐ関数nextがあると", \t -> do
+	itext t 1 "next :: Machine -> Machine -> Machine", \t -> do
+	text t "* \"HelloWorld\"と表示する関数は以下のように書ける", \t -> do
+	itext t 1 "putHelloWorld :: Machine", \t -> do
+	itext t 1 "putHelloWorld = putHello `next` putWorld"
+ ]
+
+machine2 :: Page
+machine2 = [\t -> do
+	writeTopTitle t "Machine"
+	text t "", \t -> do
+	text t "* 出力についてはMachine型でうまくいく", \t -> do
+	text t "* つまり「出力」というものは本質的に", \t -> do
+	itext t 1 "- 「これやって」", \t -> do
+	itext t 1 "- 「次にこれやって」", \t -> do
+	text t "* それの連続なので。", \t -> do
+	text t "* 入力がからむとこれはうまくいかなくなる", \t -> do
+	text t "* 入力値を次の機械にわたす必要が出てくるからだ", \t -> do
+	text t "* つまり機械のあいだで値をわたす仕組みが必要だ", \t -> do
+	text t "* ひとつめの機械からふたつめの機械に値をさたす関数", \t -> do
+	itext t 1 "pipe :: Machine -> Machine -> Machine"
+ ]
+
+machine3 :: Page
+machine3 = [\t -> do
+	writeTopTitle t "Machine"
+	text t "", \t -> do
+	text t "* 以下の機械があるとする", \t -> do
+	itext t 1 "- getLine: 入力を一行読み、次の機械に渡す機械", \t -> do
+	itext t 1 "- putLine: 渡された値を表示する機械", \t -> do
+	text t "* 読み込んだ行を表示する機械は以下のように作れる", \t -> do
+	itext t 1 "getLine `pipe` putLine"
+ ]
+
+machine4 :: Page
+machine4 = [\t -> do
+	writeTopTitle t "Machine"
+	text t "", \t -> do
+	text t "* しかしこのやりかたには問題がある", \t -> do
+	text t "* 以下の機械があるとする", \t -> do
+	itext t 1 "- getInt: 入力を読み、数に変換し次の機械に渡す機械", \t -> do
+	text t "* そして次のようにすると", \t -> do
+	itext t 1 "getInt `pipe` putLine", \t -> do
+	text t "* putLineは文字列が来ることを期待しているので", \t -> do
+	itext t 1 "- 数を渡されると予測出来ない動作をするだろう"
+ ]
+
+machine5 :: Page
+machine5 = [\t -> do
+	writeTopTitle t "IOMcn"
+	text t "", \t -> do
+	text t "* つまり型の不一致が生じる可能性がある", \t -> do
+	text t "* 静的型付け言語であるHaskellでは", \t -> do
+	itext t 1 "- 型の不一致は型チェックの段階で検出したい", \t -> do
+	text t "* Machine型に渡される値と渡す型の値の型を含めれば良い", \t -> do
+	text t "* これをIOMcn型としよう", \t -> do
+	itext t 1 "IOMcn a b"
+ ]
+
+machine6 :: Page
+machine6 = [\t -> do
+	writeTopTitle t "IOMcn"
+	text t "", \t -> do
+	text t "* 今まで出てきた機械の型は以下のようになる", \t -> do
+	itext t 1 "putHello, putWorld :: IOMcn () ()", \t -> do
+	itext t 1 "getLine :: IOMcn () String", \t -> do
+	itext t 1 "getInt :: IOMcn () Int", \t -> do
+	itext t 1 "putLine :: IOMcn String ()", \t -> do
+	text t "* これらをつなぐ関数は以下のような型となる", \t -> do
+	itext t 1 "pipe :: IOMcn a b -> IOMcn b c -> IOMcn a c"
  ]
