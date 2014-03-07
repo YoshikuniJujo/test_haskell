e4442c39e1e72b349458c324dcf74329f831a9d1
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-03-08 07:40:19 +0900

    lecture: lecture 14: finish each map

--- tools/slide/yjslide3/lecture14.hs
+++ tools/slide/yjslide3/lecture14.hs
@@ -29,44 +29,6 @@ prelude = [\t -> do
 	text t "* 「中身に関数を適用できる」という性質を型クラスにした"
  ]
 
-{-
-prelude'' :: Page
-prelude'' = [\t -> do
-	writeTopTitle t "はじめに"
-	text t "", \t -> do
-	text t "* リストに対するmap関数について考えてみる", \t -> do
-	itext t 1 "map :: (a -> b) -> [a] -> [b]", \t -> do
-	text t "* リストに格納されている値すべてに対して関数を適用する", \t -> do
-	text t "* リスト[a]は([] a)という意味である", \t -> do
-	text t "* つまり、リストは型変数をひとつとる多相型である"
- ]
-
-prelude' :: Page
-prelude' = [\t -> do
-	writeTopTitle t "はじめに"
-	text t "", \t -> do
-	text t "* Functorと呼ばれる型クラスがある", \t -> do
-	text t "* 型変数をひとつとる多相型の性質を表す型クラスである", \t -> do
-	text t "* 型変数をひとつとる多相型には以下のものがある", \t -> do
-	itext t 1 "- リスト", \t -> do
-	itext t 1 "- Maybe型", \t -> do
-	text t "* これらは実際にFunctorクラスのインスタンスとなっている", \t -> do
-	text t "* 型変数をひとつとる多相型は値を格納するものと考えられる", \t -> do
-	itext t 1 "- リスト: 0以上の値を格納するもの", \t -> do
-	itext t 1 "- Maybe型: 0または1個の値を格納するもの", \t -> do
-	text t "* Functorは「中身の値に関数を適用可」という性質を表す"
- ]
-
-definition :: Page
-definition = [\t -> do
-	writeTopTitle t "Functorクラスの定義"
-	text t "", \t -> do
-	text t "* Functorクラスの定義は以下のようになっている", \t -> do
-	itext t 1 "class Functor f where", \t -> do
-	itext t 2 "fmap :: (a -> b) -> f a -> f b"
- ]
- -}
-
 list :: Page
 list = [\t -> do
 	writeTopTitle t "リスト"
--- tools/slide/yjslide3/markdown/lecture14.md
+++ tools/slide/yjslide3/markdown/lecture14.md
@@ -21,9 +21,56 @@ Functorはそのような型クラスのひとつであり、
 
 ### リスト
 
+リストの中身の値に関数を適用する関数は
+
+    map :: (a -> b) -> [a] -> [b]
+
+lectures/lecture14ディレクトリを作ってそこに移動しよう。
+
+試してみる。
+
+    Prelude> map even [1 .. 5]
+    [False,True,False,True,False]
+
 ### Maybe
 
+Maybe型の中身の値に関数を適用する関数を書く。
+
+    appMaybe :: (a -> b) -> Maybe a -> Maybe b
+    appMaybe _ Nothing = Nothing
+    appMaybe f (Just x) = Just $ f x
+
+functor.hsに書きこみ、試してみる。
+
+    Prelude> :load functor.hs
+    *Main> appMaybe even Nothing
+    Nothing
+    *Main> appMaybe even (Just 4)
+    Just True
+
 ### 木
+
+まずは二分木を作る。
+
+    data BinTree a
+        = Node (BinTree a) (BinTree a)
+        | Leaf a
+        deriving Show
+
+二分木のすべての葉に関数を適用する関数は以下のようになる。
+
+    mapTree :: (a -> b) -> BinTree a -> BinTree b
+    mapTree f (Leaf x) = Leaf $ f x
+    mapTree f (Node l r) = Node (mapTree f l) (mapTree f r)
+
+これをfunctor.hsに書きこみ、試してみる。
+
+    *Main> :reload
+    *Main> let t = Node (Leaf 3) (Node (Leaf 4) (Leaf 5))
+    *Main> mapTree even t
+    Node (Leaf False) (Node (Leaf True) (Leaf False))
+    *Main> mapTree (* 3) t
+    Node (Leaf 9) (Node (Leaf 12) (Leaf 15))
 
 型の比較
 --------
