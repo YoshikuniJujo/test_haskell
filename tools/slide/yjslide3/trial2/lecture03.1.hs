dc105356e2b319022a415538994fe40afec401c0
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-04-07 07:12:04 +0900

    lecture: trial lecture 3.1: add keyboard practice '=', '+', and partially '-'

--- tools/slide/yjslide3/trial2/lecture03.1.hs
+++ tools/slide/yjslide3/trial2/lecture03.1.hs
@@ -6,7 +6,8 @@ subtitle = "トライアル 第3.1回 環境に慣れよう"
 main :: IO ()
 main = runLecture [
 	[flip writeTitle subtitle], prelude, whats,
-	keyboard, editor, nano, backspace, backspace2, backspaceSummary
+	keyboard, editor, nano, backspace, backspace2, backspaceSummary,
+	equal, plus, equalPlus, minus
  ]
 
 prelude :: Page
@@ -116,4 +117,78 @@ backspaceSummary = [\t -> do
 	itext t 1 "1. Fn + Delete", \t -> do
 	itext t 1 "2. Control + 'h'", \t -> do
 	text t "* 好きなほうを使おう"
+ ]
+
+equal :: Page
+equal = [\t -> do
+	writeTopTitle t "'='"
+	text t "", \t -> do
+	text t "* 今回用意したのは英語配列のキーボードなので", \t -> do
+	itext t 1 "記号の位置が日本語配列とは異なる", \t -> do
+	text t "* まずは'='の位置を学ぼう", \t -> do
+	text t "* 1234567890と打ってみよう", \t -> do
+	text t "* さらに0を打った右手小指を右にずらしながら入力を続ける", \t -> do
+	itext t 1 "1234567890右へ右へ右へ右へ", \t -> do
+	text t "* もう一度くりかえす", \t -> do
+	itext t 1 "1234567890右へ右へ右へ右へ", \t -> do
+	text t "* 以下のように入力されたはずだ", \t -> do
+	itext t 1 "1234567890-=\\`1234567890-=\\`"
+ ]
+
+plus :: Page
+plus = [\t -> do
+	writeTopTitle t "'+'"
+	text t "", \t -> do
+	text t "* '+'の入力を見てみよう", \t -> do
+	text t "* Shiftキーを押しながら1234567890-=\\`を入力する", \t -> do
+	itext t 1 "1234567890右へ右へ右へ右へ", \t -> do
+	text t "* もう一度くりかえす", \t -> do
+	itext t 1 "1234567890右へ右へ右へ右へ", \t -> do
+	text t "* 以下のように入力されたはずだ", \t -> do
+	itext t 1 "!@#$%^&*()_+|~!@#$%^&*()_+|~", \t -> do
+	text t "* '+'を入力するときには以下のようにすれば良い", \t -> do
+	itext t 1 "Shift + '='"
+ ]
+
+equalPlus :: Page
+equalPlus = [\t -> do
+	writeTopTitle t "'='と'+'の練習"
+	text t "", \t -> do
+	text t "* '='と'+'の練習をしてみよう", \t -> do
+	text t "* 以下のように打ち込もう", \t -> do
+	itext t 1 "===+++===+++===+++===+++===+++", \t -> do
+	itext t 1 "=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+", \t -> do
+	itext t 1 "x+=1x+=1x+=1x+=1", \t -> do
+	itext t 1 "1+1=21+1=21+1=2", \t -> do
+	itext t 1 "2+2=42+2=42+2=4", \t -> do
+	itext t 1 "4+4=84+4=84+4=8", \t -> do
+	itext t 1 "1+1+1=31+1+1=31+1+1=3"
+ ]
+
+minus :: Page
+minus = [\t -> do
+	writeTopTitle t "'-'"
+	text t "", \t -> do
+	text t "* '-'の位置を学ぶ", \t -> do
+	text t "* 以下のように打ち込もう", \t -> do
+	itext t 1 "1234567890-=\\`1234567890-=\\`", \t -> do
+	text t "* '-'は'0'のひとつ右にあることがわかる", \t -> do
+	text t "* 以下の練習をしよう", \t -> do
+	itext t 1 "1-1+1=11-1+1=11-1+1=1"
+ ]
+
+equalX :: Page
+equalX = [\t -> do
+	writeTopTitle t "'='"
+	text t "", \t -> do
+	text t "* 以下の練習をしてみよう", \t -> do
+	itext t 1 "==========++++++++++==========", \t -> do
+	itext t 1 "p-=0p-=0p-=0", \t -> do
+	itext t 1 "o-=0o-=0o-=0", \t -> do
+	itext t 1 "x-=9x-=9x-=9", \t -> do
+	itext t 1 "0-0=00-0=00-0=0", \t -> do
+	itext t 1 "3-4=-13-4=-13-4=-1", \t -> do
+	itext t 1 "2-5=-32-5=-32-5=-3", \t -> do
+	itext t 1 "9-19=-109-19=-109-19=-10", \t -> do
+	itext t 1 "8-5=38-5=38-5=38-5=3"
  ]
