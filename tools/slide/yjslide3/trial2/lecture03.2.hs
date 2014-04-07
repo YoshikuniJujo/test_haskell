7c511acdf923861757eb35806565b6b38b8184d1
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-04-07 15:44:33 +0900

    lecture: trial 03.2: finish zsh

--- tools/slide/yjslide3/trial2/lecture03.2.hs
+++ tools/slide/yjslide3/trial2/lecture03.2.hs
@@ -6,7 +6,8 @@ subtitle = "トライアル 第3.2回 エディタ、シェル、対話環境"
 main :: IO ()
 main = runLecture [
 	[flip writeTitle subtitle], prelude,
-	nano, nano2, nano3, nano4, nano5, nano6, nano7
+	nano, nano2, nano3, nano4, nano5, nano6, nano7,
+	shell, shell2, shell3, shell4, shell5, shell6, shell7, shellSummary
  ]
 
 prelude :: Page
@@ -120,4 +121,119 @@ nano7 = [\t -> do
 	text t "* 切り取りはCtrl + k", \t -> do
 	text t "* 貼りつけはCtrl + u", \t -> do
 	text t "* 範囲選択はCtrl + 6"
+ ]
+
+shell :: Page
+shell = [\t -> do
+	writeTopTitle t "zsh"
+	text t "", \t -> do
+	text t "* 今回用意した環境のシェルはzsh", \t -> do
+	text t "* モードはViモードとしてある", \t -> do
+	text t "* このモードでの簡単な操作方法を学ぼう"
+ ]
+
+shell2 :: Page
+shell2 = [\t -> do
+	writeTopTitle t "zsh: バックスペース"
+	text t "", \t -> do
+	text t "* まずは普通にコマンドを打ち込んでみる", \t -> do
+	itext t 1 "% pwd", \t -> do
+	itext t 1 "/home/guest/lectures/lecture00", \t -> do
+	itext t 1 "% ls", \t -> do
+	itext t 1 "test.txt", \t -> do
+	itext t 1 "% cat test.txtt"
+	itext t 1 "(まだreturnは押さない)", \t -> do
+	text t "* ひとつtを余分に入力してしまった", \t -> do
+	text t "* バックスペースを入力する", \t -> do
+	itext t 1 "Ctrl + h", \t -> do
+	text t "* 正しいコマンドになったのでreturnを入力"
+ ]
+
+shell3 :: Page
+shell3 = [\t -> do
+	writeTopTitle t "zsh: カーソルの移動"
+	text t "", \t -> do
+	text t "* 以下のようにコマンドを打つ", \t -> do
+	itext t 1 "% cat ttest.txt"
+	itext t 1 "(まだreturnは押さない)", \t -> do
+	text t "* ここでEscキーを押す"
+	itext t 1 "(Escキーは1の左にある)", \t -> do
+	text t "* Escキーを押すとカーソルの移動モードになる", \t -> do
+	text t "* hキーで左にlキーで右にカーソルが移動する", \t -> do
+	itext t 1 "hとlを利用して重複したtの上にカーソルを動かす", \t -> do
+	text t "* xキーを押すと1文字削除される", \t -> do
+	text t "* 正しいコマンドになったのでreturnを入力"
+ ]
+
+shell4 :: Page
+shell4 = [\t -> do
+	writeTopTitle t "zsh: 文字の挿入"
+	text t "", \t -> do
+	text t "* 以下のようにコマンドを打つ", \t -> do
+	itext t 1 "% cat st.t"
+	itext t 1 "(まだreturnは押さない)", \t -> do
+	text t "* ここでEscキーを押す", \t -> do
+	text t "* h, lキーでsのうえにカーソルを移動させる", \t -> do
+	text t "* iキーを押すと挿入モードになる", \t -> do
+	itext t 1 "teを入力", \t -> do
+	text t "* Escキーでカーソル移動モードにもどる", \t -> do
+	text t "* lキーで文字列の最後まで移動する", \t -> do
+	text t "* aキーを押すと挿入モードになる", \t -> do
+	itext t 1 "xtを入力", \t -> do
+	text t "* 正しいコマンドになったのでreturnを入力"
+ ]
+
+shell5 :: Page
+shell5 = [\t -> do
+	writeTopTitle t "zsh: 文字の挿入"
+	text t "", \t -> do
+	text t "* 文字を挿入するためには", \t -> do
+	itext t 1 "移動モードから挿入モードにする必要がある", \t -> do
+	itext t 1 "- iキーはカーソル位置の直前に挿入", \t -> do
+	itext t 1 "- aキーはカーソル位置の直後に挿入"
+ ]
+
+shell6 :: Page
+shell6 = [\t -> do
+	writeTopTitle t "zsh: 履歴を使用する"
+	text t "", \t -> do
+	text t "* echoコマンドで文字列を表示させる", \t -> do
+	itext t 1 "% echo 'Hello, world!'", \t -> do
+	itext t 1 "Hello, world!", \t -> do
+	text t "* 次に\"Hello, my friend!\"と表示させたい", \t -> do
+	text t "* このようなときに履歴機能が使える", \t -> do
+	text t "* Escキーで移動モードに入る", \t -> do
+	text t "* kキーで履歴をさかのぼりjキーで履歴を順送りする", \t -> do
+	itext t 1 "kキーを3回押し、jキーを2回押す", \t -> do
+	text t "* 履歴上の逆送りと順送りがされているのがわかるだろうか"
+ ]
+
+shell7 :: Page
+shell7 = [\t -> do
+	writeTopTitle t "zsh: 履歴を使用する"
+	text t "", \t -> do
+	text t "* 今、コマンド行は以下のようになっているはずだ", \t -> do
+	itext t 1 "% echo 'Hello, world!'", \t -> do
+	text t "* worldの部分をmy friendに変えれば良い", \t -> do
+	text t "* hキーとlキーでカーソルをwのところに動かし", \t -> do
+	itext t 1 "xキーを5回入力する", \t -> do
+	text t "* これでworldが消える", \t -> do
+	text t "* iキーで挿入モードに移行し以下を入力する", \t -> do
+	itext t 1 "my friend", \t -> do
+	text t "* これで正しいコマンドになったのでreturnを入力する"
+ ]
+
+shellSummary :: Page
+shellSummary = [\t -> do
+	writeTopTitle t "zsh: まとめ"
+	text t "", \t -> do
+	text t "* zshのViモードにおける"
+	itext t 1 "コマンド行の編集と履歴の利用について学んだ", \t -> do
+	text t "* 挿入モードとカーソル移動モードがある", \t -> do
+	text t "* Escキーでカーソル移動モードに移行すると", \t -> do
+	text t "* h, lキーでカーソルが移動する", \t -> do
+	text t "* xキーで1文字削除が可能", \t -> do
+	text t "* i, aキーで挿入モードに移行できる", \t -> do
+	text t "* 移動モードでk, jキーを使って履歴内の移動が可能", \t -> do
+	text t "* 履歴も、その場で打ち込んだコマンド同様に編集できる"
  ]
