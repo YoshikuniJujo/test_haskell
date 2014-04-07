6654ef9d0405556d1181851d6c9c9d0136f15892
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-04-07 18:16:30 +0900

    lecture: trial 3.3, 3.4: copy and edit from lecture 01, 02

---
+++ tools/slide/yjslide3/trial2/lecture03.3.hs
@@ -0,0 +1,401 @@
+import System.Random
+import System.IO.Unsafe
+
+import Lecture
+
+subtitle :: String
+subtitle = "トライアル 第3.3回(元1回) 関数"
+
+main :: IO ()
+main = runLecture [
+	[flip writeTitle subtitle], prelude, function, apply,
+	defineFun, defineFun2, defineFun3, funSummary,
+	partial, partial2, partial3, partial4, partialSummary,
+	literal, literal2, literal3, literal4, literalSummary,
+	higherOrder, higherOrder2, higherOrderSummary,
+	operator, operator2, operator3,
+	dollar, summary
+ ]
+
+prelude :: Page
+prelude = [\t -> do
+	writeTopTitle t "はじめに"
+	text t "", \t -> do
+	text t "* Haskellでプログラミングをするということは", \t -> do
+	itext t 1 "関数を様々なやりかたで組み合わせるということ", \t -> do
+	text t "* 関数の定義のしかたを学ぶ", \t -> do
+	text t "* 関数への引数の与えかたを学ぶ", \t -> do
+	text t "* 関数の部分適用について見る", \t -> do
+	text t "* リテラルとして関数を書く記法を学ぶ", \t -> do
+	text t "* 高階関数について学ぶ", \t -> do
+	text t "* Haskellでは演算子と関数が同じものであることを見る"
+ ]
+
+function :: Page
+function = [\t -> do
+	writeTopTitle t "関数の定義"
+	text t "", \t -> do
+	text t "* C言語では以下のようにする", \t -> do
+	itext t 1 "int add(int x, int y) { return x + y; }", \t -> do
+	text t "* 同じことをHaskellでは以下のようにする", \t -> do
+	itext t 1 "add x y = x + y", \t -> do
+	text t "* [関数名] [仮引数1] [仮引数2] ... = [表現]という形", \t -> do
+	text t "* '='を使っているのはなぜか?", \t -> do
+	itext t 1 "add x yはx + yに置き換えられるということ", \t -> do
+	itext t 1 "プログラム中のadd 3 8は3 + 8に置き換え可能"
+ ]
+
+apply :: Page
+apply = [\t -> do
+	writeTopTitle t "関数の適用"
+	text t "", \t -> do
+	text t "* addの仮引数x, yに実引数3, 8を入れるには以下のように", \t -> do
+	itext t 1 "add 3 8", \t -> do
+	text t "* [関数名] [実引数1] [実引数2] ...という形", \t -> do
+	text t "* [関数名]([実引数1], [実引数2], ...)のようにしない", \t -> do
+	text t "* 空白でくぎった引数をならべるだけ", \t -> do
+	text t "* この記法の合理性についてはそのうち明らかになる"
+ ]
+
+defineFun :: Page
+defineFun = [\t -> do
+	writeTopTitle t "やってみよう"
+	text t "", \t -> do
+	text t "* ghciの対話環境内でも関数を定義できる", \t -> do
+	text t "* しかし、関数が長くなるとわずらわしいので", \t -> do
+	text t "* 別ファイルで関数を定義し対話環境で使うことにする", \t -> do
+	text t "* お好きなエディタを選んで、と言いたいところだが", \t -> do
+	itext t 1 "nano、vim、Emacsしか用意していない", \t -> do
+	text t "* vimやEmacsを使う人は「いつも通りに」編集してほしい", \t -> do
+	text t "* 演者はvim使いなのでvimについては教えられるが", \t -> do
+	itext t 1 "Emacsについてはわからない", \t -> do
+	text t "* これらのどのエディタも使ったことがないという人は", \t -> do
+	itext t 1 "とりあえずnanoを使っておこう"
+ ]
+
+defineFun2 :: Page
+defineFun2 = [\t -> do
+	writeTopTitle t "やってみよう"
+	text t "", \t -> do
+	text t "* 例題: 身長と体重を入力するとBMIを返す関数bmiを作る", \t -> do
+	itext t 1 "BMI = [体重(kg)] / [身長(m)]の2乗", \t -> do
+	text t "* bmi.hsファイルを作ろう", \t -> do
+	itext t 1 "% cd ~/lectures/lecture01", \t -> do
+	itext t 1 "% [エディタ] bmi.hs", \t -> do
+	itext t 2 "[エディタ]はnano -w, vim, emacsのどれか", \t -> do
+	itext t 2 "nanoを使う場合は-wオプションを忘れないように", \t -> do
+	text t "* 今後は[エディタ]と書く代わりにnano -wとする", \t -> do
+	text t "* それぞれviやemacsに読み換えてほしい"
+ ]
+
+bmi :: Double -> Double -> Double
+bmi h w = w / (h / 100) ^ (2 :: Int)
+
+defineFun3 :: Page
+defineFun3 = [\t -> do
+	writeTopTitle t "やってみよう"
+	text t "", \t -> do
+	text t "* 以下の内容を書き込む", \t -> do
+	itext t 1 "bmi h w = w / (h / 100) ^ 2", \t -> do
+	text t "* 別のターミナルでghciにこのファイルを読み込ませる", \t -> do
+	itext t 1 "% cd ~/lectures/lecture01/", \t -> do
+	itext t 1 "% ghci bmi.hs", \t -> do
+	itext t 1 "*Main> ", \t -> do
+	itext t 2 "- \"Prelude>\"ではなく\"*Main>\"になった", \t -> do
+	itext t 2 "- 理由については後々明らかになる", \t -> do
+	text t "* とりあえずチェ・ホンマンのBMIを求めてみよう", \t -> do
+	itext t 1 "*Main> bmi 218 164", \t -> do
+	itext t 1 $ show $ bmi 218 164
+ ]
+
+funSummary :: Page
+funSummary = [\t -> do
+	writeTopTitle t "ここまでのまとめ"
+	text t "", \t -> do
+	text t "* 関数とは入力を取り出力を返すもの", \t -> do
+	text t "* Haskellでは以下のように定義する", \t -> do
+	itext t 1 "[関数名] [仮引数1] [仮引数2] ... = [表現]", \t -> do
+	text t "* 関数fに値xを与えることをxにfを適用するという", \t -> do
+	text t "* 関数適用は以下のようにする", \t -> do
+	itext t 1 "[関数名] [実引数1] [実引数2] ..."
+ ]
+
+partial :: Page
+partial = [\t -> do
+	writeTopTitle t "関数の部分適用"
+	text t "", \t -> do
+	text t "* 例えばチェホンマンのbmiの変化を見たいと思ったとする", \t -> do
+	itext t 1 "% ghci bmi.hs", \t -> do
+	itext t 1 "*Main> bmi 218 164", \t -> do
+	itext t 1 $ show $ bmi 218 164, \t -> do
+	itext t 2 "2012年4月ごろには24kg減量したらしい", \t -> do
+	itext t 1 "*Main> bmi 218 140", \t -> do
+	itext t 1 $ show $ bmi 218 140, \t -> do
+	text t "* 今後もチェホンマンのbmiを追っていきたいとする", \t -> do
+	text t "* チェホンマン専用の関数を作ろう"
+ ]
+
+partial2 :: Page
+partial2 = [\t -> do
+	writeTopTitle t "関数の部分適用"
+	text t "", \t -> do
+	text t "* bmi.hsに以下を追加", \t -> do
+	itext t 1 "bmiCHM w = bmi 218 w", \t -> do
+	text t "* もとのプロンプトにもどり以下を入力", \t -> do
+	itext t 1 "*Main> :reload", \t -> do
+	itext t 2 "これでファイルの再読み込みができる", \t -> do
+	itext t 2 "エディタと対話環境を両方開いておける"
+ ]
+
+bmiCHM :: Double -> Double
+bmiCHM w = bmi 218 w
+
+partial3 :: Page
+partial3 = [\t -> do
+	writeTopTitle t "関数の部分適用"
+	text t "", \t -> do
+	text t "* 使ってみる", \t -> do
+	itext t 1 "*Main> bmiCHM 164", \t -> do
+	itext t 1 $ show $ bmiCHM 164, \t -> do
+	itext t 1 "*Main> bmiCHM 140", \t -> do
+	itext t 1 $ show $ bmiCHM 140, \t -> do
+	text t "* bmiCHMの定義をもう一度見てみよう", \t -> do
+	itext t 1 "bmiCHM w = bmi 218 w", \t -> do
+	text t "* bmiCHMはbmiの第一引数のみを指定したものと言える", \t -> do
+	text t "* 以下のように書くこともできる", \t -> do
+	itext t 1 "bmiCHM = bmi 218"
+ ]
+
+partial4 :: Page
+partial4 = [\t -> do
+	writeTopTitle t "関数の部分適用"
+	text t "", \t -> do
+	text t "* bmi 218のような形を「部分適用」と呼ぶ", \t -> do
+	text t "* 関数の一部の引数だけを指定した形", \t -> do
+	text t "* bmi 218自体も関数なので", \t -> do
+	itext t 1 "(bmi 218) 164のような形で引数を与えることが可能", \t -> do
+	text t "* 今まで2つの引数を与えていたと思っていたが", \t -> do
+	itext t 1 "本当はbmi 218 164は(bmi 218) 164の括弧を省略した形だった", \t -> do
+	text t "* Haskellに2引数関数はない!", \t -> do
+	text t "* bmiは引数を与えると「引数をひとつ取る関数」を返す関数", \t -> do
+	text t "* やってみよう", \t -> do
+	itext t 1 "*Main> (bmi 218) 164", \t -> do
+	itext t 1 $ show $ (bmi 218) 164
+ ]
+
+partialSummary :: Page
+partialSummary = [\t -> do
+	writeTopTitle t "関数の部分適用(まとめ)"
+	text t "", \t -> do
+	text t "* Haskellの2引数関数のように見えるものは", \t -> do
+	itext t 1 "引数を取って「ひとつ引数を取る関数」を返す関数", \t -> do
+	text t "* 関数適用は左結合", \t -> do
+	itext t 1 "bmi 218 164は(bmi 218) 164と解釈される", \t -> do
+	text t "* 「型」について学ぶとより明らかになる"
+ ]
+
+fun :: Int -> Int
+fun x = x ^ x
+
+l1Number1 :: Int
+l1Number1 = unsafePerformIO $ randomRIO (3, 8)
+
+literal :: Page
+literal = [\t -> do
+	writeTopTitle t "関数リテラル"
+	text t "", \t -> do
+	text t "* たとえば数を利用するときにはいちいち名前をつけない", \t -> do
+	text t "* 2とか3とかそのまま書けばいい", \t -> do
+	text t "* これをリテラルと呼ぶ", \t -> do
+	text t "* 今までのやりかただと名前のない関数は作れない", \t -> do
+	text t "* 「引数xに対してxのx乗を返す関数」を作りたいとする", \t -> do
+	text t "* その関数は一度しか使わないとする", \t -> do
+	text t "* 今までのやりかただと以下のようになる", \t -> do
+	itext t 1 "fun x = x ^ x", \t -> do
+	itext t 1 $ "*Main> fun " ++ show l1Number1, \t -> do
+	itext t 1 $ show $ fun l1Number1
+ ]
+
+literal2 :: Page
+literal2 = [\t -> do
+	writeTopTitle t "関数リテラル"
+	text t "", \t -> do
+	text t "* 一回しか使わないのに名前をつける必要はない", \t -> do
+	text t "* 関数リテラルの記法を使うと無名関数を作ることができる", \t -> do
+	text t "* やってみよう", \t -> do
+	itext t 1 $ "*Main> (\\x -> x ^ x) " ++ show l1Number1, \t -> do
+	itext t 1 $ show $ (\x -> x ^ x) l1Number1, \t -> do
+	text t "* \\[仮引数] -> [表現]という形"
+ ]
+
+literal3 :: Page
+literal3 = [\t -> do
+	writeTopTitle t "関数リテラル"
+	text t "", \t -> do
+	text t "* 2引数関数が「関数を返す関数」であることを", \t -> do
+	itext t 1 "より直接的に表現すると以下のようになる", \t -> do
+	itext t 1 "bmi h = \\w -> w / (h / 100) ^ 2", \t -> do
+	itext t 1 "- 「wをとってw / (h / 100) ^ 2を返す関数」を返す", \t -> do
+	text t "* さらに同様に以下のように書き換えることが可能", \t -> do
+	itext t 1 "bmi = \\h -> \\w -> w / (h / 100) ^ 2", \t -> do
+	text t "* 右辺のリテラル表記で作られた関数でbmiを束縛している", \t -> do
+	itext t 1 "- 「[値]が[変数]を束縛する」は", \t -> do
+	itext t 1 "- 「[値]を[変数]に代入する」とほぼ同じと考えてよい", \t -> do
+	text t "* リテラルを使わない関数定義は2つのことを同時にしていた", \t -> do
+	itext t 1 "1. 関数の作成", \t -> do
+	itext t 1 "2. 作成した関数での変数の束縛"
+ ]
+
+literal4 :: Page
+literal4 = [\t -> do
+	writeTopTitle t "関数リテラル"
+	text t "", \t -> do
+	text t "* 複数の引数をとる関数リテラルは省略記法がある", \t -> do
+	itext t 1 "\\h -> \\w -> w / (h / 100) ^ 2", \t -> do
+	arrowIText t 1 "\\h w -> w / (h / 100) ^ 2", \t -> do
+	text t "* \\[引数1] -> \\[引数2] -> ... -> [表現]を省略して", \t -> do
+	itext t 1 "\\[引数1] [引数2] ... -> [表現]"
+ ]
+
+literalSummary :: Page
+literalSummary = [\t -> do
+	writeTopTitle t "関数リテラル(まとめ)"
+	text t "", \t -> do
+	text t "* 関数リテラルの記法を使うと無名関数を書くことができる", \t -> do
+	text t "* 記法は以下の通り", \t -> do
+	itext t 1 "\\[引数1] [引数2] ... -> [表現]", \t -> do
+	text t "* 普通の関数定義は構文糖と考えることができる", \t -> do
+	itext t 1 "[関数名] [引数1] [引数2] ... = [表現]", \t -> do
+	arrowIText t 1 "[関数名] = \\[引数1] [引数2] ... -> [表現]"
+ ]
+
+higherOrder :: Page
+higherOrder = [\t -> do
+	writeTopTitle t "高階関数"
+	text t "", \t -> do
+	text t "* 以下のどちらかを満たす関数を高階関数と呼ぶ", \t -> do
+	itext t 1 "- 引数として関数をとる", \t -> do
+	itext t 1 "- 返り値として関数を返す", \t -> do
+	text t "* 高階関数を使うと", \t -> do
+	itext t 1 "- くりかえし等の「構造」を関数で表現できる", \t -> do
+	itext t 1 "- 他の言語での「構文」を普通の関数として定義できる", \t -> do
+	text t "* 返り値として関数を返す関数は", \t -> do
+	itext t 1 "- 高階関数として考える場合もあるが", \t -> do
+	itext t 1 "- 複数の引数をとる関数として見る場合もある"
+	{-
+	text t "* Haskellでの2引数関数は本当は関数を返す関数なので", \t -> do
+	itext t 1 "- 高階関数と呼ぶことができる", \t -> do
+	itext t 1 "- 同じ関数でも2引数関数としてとらえる"
+	-}
+--	text t "* 引数として関数をとる関数について見ていこう"
+ ]
+
+twice :: (a -> a) -> a -> a
+twice f = f . f
+
+ho2number1 :: Int
+ho2number1 = unsafePerformIO $ randomRIO (2, 5)
+
+higherOrder2 :: Page
+higherOrder2 = [\t -> do
+	writeTopTitle t "高階関数"
+	text t "", \t -> do
+	text t "* 与えられた関数を与えられた値に2回適用する関数", \t -> do
+	text t "* やってみよう", \t -> do
+	itext t 1 "% nano -w higher.hs", \t -> do
+	itext t 1 "twice f x = f (f x)", \t -> do
+	itext t 1 "*Main> :load higher.hs", \t -> do
+	itext t 1 $ "*Main> twice (\\x -> x * (x + 1)) " ++ show ho2number1, \t -> do
+	itext t 1 $ show $ twice (\x -> x * (x + 1)) ho2number1, \t -> do
+	text t "* twiceは第一引数として関数を取るので高階関数である"
+ ]
+
+higherOrderSummary :: Page
+higherOrderSummary = [\t -> do
+	writeTopTitle t "高階関数(まとめ)"
+	text t "", \t -> do
+	text t "* 引数または返り値が関数である関数を高階関数と呼ぶ", \t -> do
+	text t "* 返り値が関数である関数は", \t -> do
+	itext t 1 "Haskellでは複数の引数をとる関数と同じこと", \t -> do
+	text t "* 引数が関数である関数を使うと", \t -> do
+	itext t 1 "プログラムの「構造」を関数で表現することができる"
+ ]
+
+operator :: Page
+operator = [\t -> do
+	writeTopTitle t "演算子"
+	text t "", \t -> do
+	text t "* Haskellの演算子は本質的には関数と同じもの", \t -> do
+	text t "* 3 + 8は(+)という関数に引数として3と8を与えたということ", \t -> do
+	text t "* 演算子は()でかこってやることで普通の関数として扱える", \t -> do
+	itext t 1 "*Main> 3 + 8", \t -> do
+	itext t 1 $ show (3 + 8 :: Int), \t -> do
+	itext t 1 "*Main> (+) 3 8", \t -> do
+	itext t 1 $ show ((+) 3 8 :: Int), \t -> do
+	text t "* 逆に関数は`(バッククォート)でかこむと演算子となる", \t -> do
+	itext t 1 "*Main> :load bmi.hs", \t -> do
+	itext t 1 "*Main> 218 `bmi` 164", \t -> do
+	itext t 1 $ show $ 218 `bmi` 164
+ ]
+
+operator2 :: Page
+operator2 = [\t -> do
+	writeTopTitle t "演算子"
+	text t "", \t -> do
+	text t "* 演算子についても部分適用が可能", \t -> do
+	text t "* 第1引数と第2引数の両者について部分適用ができる", \t -> do
+	itext t 1 "*Main> (3 +) 8", \t -> do
+	itext t 1 $ show ((3 +) 8 :: Int), \t -> do
+	itext t 1 "*Main> (+ 8) 3", \t -> do
+	itext t 1 $ show ((+ 8) 3 :: Int), \t -> do
+	text t "* 関数由来の演算子についても同じことができる", \t -> do
+	itext t 1 "*Main> (218 `bmi`) 164", \t -> do
+	itext t 1 $ show $ (218 `bmi`) 164, \t -> do
+	itext t 1 "*Main> (`bmi` 164) 218", \t -> do
+	itext t 1 $ show $ (`bmi` 164) 218
+ ]
+
+operator3 :: Page
+operator3 = [\t -> do
+	writeTopTitle t "演算子"
+	text t "", \t -> do
+	text t "* 演算子は関数と同様に自分で定義できる", \t -> do
+	text t "* 演算子に使える記号はアスキーの範囲では以下のものがある", \t -> do
+	itext t 1 "!#$%&*+./<=>?@\\^|-~:", \t -> do
+	itext t 1 "(ただし、':'は普通の演算子の先頭にはこない)", \t -> do
+	text t "* 以下のように定義する", \t -> do
+	itext t 1 "y ./^ x = sqrt (x ^ 2 + y ^ 2)", \t -> do
+	text t "* 関数と同じような形での定義も可能", \t -> do
+	itext t 1 "(./^) y x = sqrt (x ^ 2 + y ^ 2)", \t -> do
+	text t "* 逆に関数を演算子と同じ形で定義することも", \t -> do
+	itext t 1 "h `bmi` w = w / (h / 100) ^ 2"
+ ]
+
+dollar :: Page
+dollar = [\t -> do
+	writeTopTitle t "$演算子"
+	text t "", \t -> do
+	text t "* 以下のような演算子が用意されている", \t -> do
+	itext t 1 "f $ x = f x", \t -> do
+	text t "* f $ xで値xに関数fを適用する", \t -> do
+	text t "* つまり、f $ xはf xと同じこと", \t -> do
+	text t "* 意味のない演算子のように見えるが", \t -> do
+	itext t 1 "- 優先順位が低く右結合するように作られているので", \t -> do
+	itext t 1 "- ()を省略したいときに使える", \t -> do
+	text t "* 以下のような書き換えが可能になる", \t -> do
+	itext t 1 "f (g (h (i (j (k x)))))", \t -> do
+	arrowIText t 1 "f $ g $ h $ i $ j $ k x"
+ ]
+
+summary :: Page
+summary = [\t -> do
+	writeTopTitle t "まとめ"
+	text t "", \t -> do
+	text t "* 関数定義: [関数名] [仮引数1] [仮引数2] ... = [表現]", \t -> do
+	text t "* 関数適用: [関数名] [実引数1] [実引数2] ...", \t -> do
+	text t "* 複数の引数をとる関数は「関数を返す関数」である", \t -> do
+	text t "* 関数リテラル: \\[仮引数1] [仮引数2] ... -> [表現]", \t -> do
+	text t "* 引数か返り値が関数であるような関数を高階関数と呼ぶ", \t -> do
+	text t "* 演算子と関数は()と``で相互に変換可能", \t -> do
+	text t "* 演算子も関数も部分適用が可能"
+ ]
---
+++ tools/slide/yjslide3/trial2/lecture03.4.hs
@@ -0,0 +1,280 @@
+import System.Random
+import System.IO.Unsafe
+import Data.Char
+
+import Lecture
+
+subtitle :: String
+subtitle = "トライアル 第3.4回(元第2回) 型"
+
+main :: IO ()
+main = runLecture [
+	[flip writeTitle subtitle], prelude,
+	{- aboutType, aboutType2, aboutType3, -} aboutType4, aboutType5,
+	aboutType6, aboutType7, aboutType9, aboutType8, aboutType10,
+	aboutType11, aboutType12, aboutType13, aboutType14, operator,
+	polymorphism, polymorphism2,
+	summary
+ ]
+
+prelude :: Page
+prelude = [\t -> do
+	writeTopTitle t "はじめに"
+	text t "", \t -> do
+	text t "* 値には型がある", \t -> do
+	text t "* 型は以下を表す", \t -> do
+	itext t 1 "- その値にどの関数を適用できるか", \t -> do
+	itext t 1 "- どの関数がその値を返すか", \t -> do
+	text t "* つまり、型はその値で「何ができるか」を表す", \t -> do
+	text t "* 関数にも型があり、入力と出力の型で定義される", \t -> do
+	text t "* 複数の型に適用できる関数があり多相関数と呼ばれる", \t -> do
+	text t "* 多相関数の型は型変数を使って表現する", \t -> do
+	text t "* 型の導出はHaskellを学ぶ際のよい練習になる"
+ ]
+
+
+at4char1 :: Char
+at4char1 = unsafePerformIO $ randomRIO ('A', 'Z')
+
+aboutType4 :: Page
+aboutType4 = [\t -> do
+	writeTopTitle t "「何ができるか」"
+	text t "", \t -> do
+	text t "* 値には型があり、型によって「何ができるか」が決まる", \t -> do
+	text t "* Haskellには文字を表すChar型が用意されている", \t -> do
+	text t "* 文字はtoLowerで小文字にすることができる", \t -> do
+	itext t 1 "Prelude> :m Data.Char", \t -> do
+	itext t 2 "- toLowerを含むData.Charモジュールの読み込み", \t -> do
+	itext t 1 $ "Prelude Data.Char> toLower " ++ show at4char1, \t -> do
+	itext t 1 $ show $ toLower at4char1
+ ]
+
+at5bool1 :: Bool
+at5bool1 = unsafePerformIO randomIO
+
+aboutType5 :: Page
+aboutType5 = [\t -> do
+	writeTopTitle t "型の不適合"
+	text t "", \t -> do
+	text t "* 真偽を表現するBool型がある", \t -> do
+	itext t 1 "- False, Trueの2つの値のみを持つ", \t -> do
+	text t $ "* " ++ show at5bool1 ++ "を小文字にすることはできない", \t -> do
+	itext t 1 $ "Prelude Data.Char> toLower " ++ show at5bool1, \t -> do
+	itext t 1 "..."
+	itext t 1 "Couldn't match expected type `Char' with"
+	itext t 1 "actual type `Bool'"
+	itext t 1 $ "In the first argument of `toLower', namely `" ++ show at5bool1 ++ "'"
+	itext t 1 "...", \t -> do
+	text t $ "* 「" ++ show at5bool1 ++ "はBoolであってCharじゃないよ」と言われた"
+ ]
+
+aboutType6 :: Page
+aboutType6 = [\t -> do
+	writeTopTitle t "関数の型"
+	text t "", \t -> do
+	text t "* 関数の型は入力の型と出力の型によって決まる", \t -> do
+	text t "* たとえばtoLowerは文字を取って文字を返す関数で", \t -> do
+	itext t 1 "型はChar -> Charとなる", \t -> do
+	text t "* 例の関数bmiの型はどうなるだろうか", \t -> do
+	text t "* 入力と出力を倍精度浮動小数点数とする", \t -> do
+	text t "* 倍精度浮動小数点数を表す型はDoubleなので", \t -> do
+	text t "* bmiはDoubleを2こ取ってDoubleを返す関数", \t -> do
+	text t "* 2引数関数は本当は「関数を返す関数」なので", \t -> do
+	itext t 1 "Double -> (Double -> Double)", \t -> do
+	text t "* (->)は右結合なので()は省略できる", \t -> do
+	itext t 1 "Double -> Double -> Double"
+ ]
+
+aboutType7 :: Page
+aboutType7 = [\t -> do
+	writeTopTitle t "関数の型"
+	text t "", \t -> do
+	text t "* 関数の型は以下のように表される", \t -> do
+	itext t 1 "[引数1の型] -> [引数2の型] -> ... -> [返り値の型]"
+ ]
+
+aboutType8 :: Page
+aboutType8 = [\t -> do
+	writeTopTitle t "型の重要性"
+	text t "", \t -> do
+	text t "* 関数を考えるときにはまず型を考える", \t -> do
+	text t "* 関数の中身を作る前に以下を考える", \t -> do
+	itext t 1 "- 出力としてほしいのはどういう種類の値か", \t -> do
+	itext t 1 "- 関数の入力には何が必要か", \t -> do
+	text t "* bmi関数の例では", \t -> do
+	itext t 1 "- ほしいのはBMIを表す浮動小数点数", \t -> do
+	itext t 1 "- 入力は身長と体重を表す2つの浮動小数点数", \t -> do
+	text t "* ここでBMI値によって肥満かどうかを判定する関数を考える", \t -> do
+	itext t 1 "- ほしいのは肥満かどうかを表すBool値", \t -> do
+	itext t 1 "- 入力は同上の2つの浮動小数点数"
+ ]
+
+aboutType9 :: Page
+aboutType9 = [\t -> do
+	writeTopTitle t "型の宣言"
+	text t "", \t -> do
+	text t "* bmi関数を定義したときには型を宣言しなかった", \t -> do
+	text t "* 型を宣言しない場合、型推論によって自動的に決まる", \t -> do
+	text t "* しかし、型は宣言したほうが良い", \t -> do
+	itext t 1 "- 自分がどういう関数を作っているのかを明確にする", \t -> do
+	itext t 1 "- バグの多くは型チェックによって検出可能", \t -> do
+	itext t 1 "- 型宣言は優れたドキュメントとなる", \t -> do
+	itext t 1 "- こみいった型だと型推論が利かない場合がある", \t -> do
+	text t "* 型宣言は以下のような記法となる", \t -> do
+	itext t 1 "[関数名] :: [型]"
+ ]
+
+aboutType10 :: Page
+aboutType10 = [\t -> do
+	writeTopTitle t "型駆動開発"
+	text t "", \t -> do
+	text t "* 「型駆動開発」とは今思いついた言葉", \t -> do
+	text t "* まずは型を決めてそれから中身をうめていくやりかた", \t -> do
+	text t "* lectures/lecture02/bmi.hsを作ろう", \t -> do
+	itext t 1 "% cd ~/lectures/lecture02", \t -> do
+	itext t 1 "% nano -w bmi.hs", \t -> do
+	itext t 1 "bmi :: Double -> Double -> Double"
+	itext t 1 "bmi = undefined"
+	itext t 1 ""
+	itext t 1 "isObese :: Double -> Double -> Bool"
+	itext t 1 "isObese = undefined", \t -> do
+	text t "* undefinedは評価されるとエラーとなる値", \t -> do
+	text t "* スタブとして使用した"
+ ]
+
+aboutType11 :: Page
+aboutType11 = [\t -> do
+	writeTopTitle t "型駆動開発", \t -> do
+	text t "* エディタとは別のターミナルで", \t -> do
+	itext t 0 "% cd ~/lectures/lecture02", \t -> do
+	itext t 0 "% ghci bmi", \t -> do
+	itext t 0 "*Main> bmi 218 164", \t -> do
+	itext t 0 "*** Exception: Prelude.undefined", \t -> do
+	itext t 0 "*Main> isObese 218 164", \t -> do
+	itext t 0 "*** Exception: Prelude.undefined", \t -> do
+	itext t 1 "- 当然エラーとなる", \t -> do
+	itext t 0 "*Main> :type bmi", \t -> do
+	itext t 0 "bmi :: Double -> Double -> Double", \t -> do
+	itext t 0 "*Main> :type isObese", \t -> do
+	itext t 0 "isObese :: Double -> Double -> Bool", \t -> do
+	itext t 1 "- 対話環境では:typeで関数や値の型を表示できる"
+ ]
+
+aboutType12 :: Page
+aboutType12 = [\t -> do
+	writeTopTitle t "型駆動開発"
+	text t "", \t -> do
+	text t "* 開発のしかたにはボトムアップとトップダウンがある", \t -> do
+	text t "* 今回の場合", \t -> do
+	itext t 1 "- bmi関数を先に定義すればボトムアップとなり", \t -> do
+	itext t 1 "- isObese関数を先に定義すればトップダウンとなる", \t -> do
+	text t "* 今回はトップダウンで開発する", \t -> do
+	text t "* BMIが30以上で肥満とされるので", \t -> do
+	itext t 1 "isObese h w = bmi h w >= 30", \t -> do
+	text t "* ここでの思考は以下のようになる", \t -> do
+	itext t 1 "- 身長と体重からBMIを求める関数bmiがあると仮定", \t -> do
+	itext t 1 "- するとisObese h wはbmi h w >= 30で定義できる"
+ ]
+
+aboutType13 :: Page
+aboutType13 = [\t -> do
+	writeTopTitle t "型駆動開発"
+	text t "", \t -> do
+	text t "* bmi関数が定義されているという仮定のもとで", \t -> do
+	itext t 1 "isObese関数が定義された", \t -> do
+	text t "* よってbmi関数を定義すれば仕事は終わる", \t -> do
+	itext t 1 "bmi h w = w / (h / 100) ^ 2"
+ ]
+
+aboutType14 :: Page
+aboutType14 = [\t -> do
+	writeTopTitle t "型駆動開発"
+	text t "", \t -> do
+	text t "* 動かしてみよう", \t -> do
+	itext t 1 "% ghci bmi.hs", \t -> do
+	itext t 1 "*Main> isObese 218 164", \t -> do
+	itext t 1 "True", \t -> do
+	itext t 1 "*Main> isObese 218 140", \t -> do
+	itext t 1 "False", \t -> do
+	text t "* チェ・ホンマンはかつて肥満であったと判定された、ただし", \t -> do
+	itext t 1 "- BMIは高身長者には厳しい", \t -> do
+	itext t 1 "- 脂肪と筋肉を区別できない"
+ ]
+
+operator :: Page
+operator = [\t -> do
+	writeTopTitle t "演算子"
+	text t "", \t -> do
+	text t "* 演算子についても型宣言ができる", \t -> do
+	text t "* ()でくくって「関数」にすればよい", \t -> do
+	text t "* (&&)演算子を見てみよう", \t -> do
+	itext t 1 "*Main> False && True", \t -> do
+	itext t 1 $ show $ False && True, \t -> do
+	itext t 1 "*Main> True && True", \t -> do
+	itext t 1 $ show $ True && True, \t -> do
+	text t "* Bool値を2つとりBool値を返す関数(演算子)なので", \t -> do
+	itext t 1 "(&&) :: Bool -> Bool -> Bool"
+ ]
+
+pm1char1 :: Char
+pm1char1 = unsafePerformIO $ randomRIO ('a', 'z')
+pm1bool1 :: Bool
+pm1bool1 = unsafePerformIO randomIO
+
+polymorphism :: Page
+polymorphism = [\t -> do
+	writeTopTitle t "多相関数"
+	text t "", \t -> do
+	text t "* 複数の型に適用可能な関数がある", \t -> do
+	text t "* 非常に単純な関数としてidがある", \t -> do
+	itext t 1 $ "*Main> id " ++ show pm1char1, \t -> do
+	itext t 1 $ show $ id pm1char1, \t -> do
+	itext t 1 $ "*Main> id " ++ show pm1bool1, \t -> do
+	itext t 1 $ show $ id pm1bool1, \t -> do
+	text t "* 定義は以下のようになる", \t -> do
+	itext t 1 "id x = x", \t -> do
+	text t "* つまり引数をそのまま返す関数である", \t -> do
+	text t "* 引数が何であれ自分自身を返すことは可能", \t -> do
+	arrowIText t 1 "すべての型の値を引数とすることができる"
+ ]
+
+polymorphism2 :: Page
+polymorphism2 = [\t -> do
+	writeTopTitle t "多相関数"
+	text t "", \t -> do
+	text t "* idの型宣言を書きたい", \t -> do
+	text t $ "* id " ++ show pm1char1 ++ "のときはidの型は", \t -> do
+	itext t 1 "Char -> Char", \t -> do
+	text t $ "* id " ++ show pm1bool1 ++ "のときはidの型は", \t -> do
+	itext t 1 "Bool -> Bool", \t -> do
+	text t "* つまり[型X] -> [型X]という形", \t -> do
+	text t "* このようなとき型変数が使える", \t -> do
+	text t "* 型変数は小文字ではじまる識別子である", \t -> do
+	text t "* よって以下のようにできる", \t -> do
+	itext t 1 "id :: a -> a", \t -> do
+	text t "* idは型aの値をとり、同じ型aの値を返す関数"
+ ]
+
+pm3int1, pm3int2 :: Int
+[pm3int1, pm3int2] = unsafePerformIO $ mapM randomRIO [(97, 122), (65, 90)]
+
+higherOrder :: Page
+higherOrder = [\t -> do
+	writeTopTitle t "高階関数"
+	text t ""
+ ]
+
+summary :: Page
+summary = [\t -> do
+	writeTopTitle t "まとめ"
+	text t "", \t -> do
+	text t "* 型とは値の種類", \t -> do
+	text t "* 型が同じなら値によらずできることは同じ", \t -> do
+	text t "* 関数を作るときにはまず型を考える", \t -> do
+	itext t 1 "- 型が決まれば仕事は半分終わったようなもの", \t -> do
+	itext t 1 "- 型チェックによってバグの多くは検出可", \t -> do
+	itext t 1 "- 型はドキュメントとしても優れる", \t -> do
+	text t "* 型の宣言は[関数名] :: [型]という形", \t -> do
+	text t "* 多相関数の型宣言には型変数を使う", \t -> do
+	text t "* 型の導出は勉強になる"
+ ]
