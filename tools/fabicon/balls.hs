4161b398c1d843fbf7c885bdeeb8c5b69bca66a9
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-12-01 17:57:23 +0900

    dot editor: show colors

--- tools/fabicon/balls.hs
+++ tools/fabicon/balls.hs
@@ -16,25 +16,75 @@ blank = newIORef . replicate 16 $ replicate 16 False
 ballsFrame :: FilePath -> IO ()
 ballsFrame fp = do
 	pic <- blank
+	clr <- newIORef $ PixelRGB8 0 0 0
 	f <- frame [ text := "hoge" ]
 	p <- panel f []
 	v <- panel f [
-		on keyboard := onKey fp pic f,
 		on paint := preview pic ]
-	set p [	on keyboard := onKey fp pic f,
-		on click := onClick p v pic,
+	c <- panel f [
+		on paint := showColor clr
+		]
+	set c [	on keyboard := onKey clr c fp pic f ]
+	set p [	on click := onClick p v pic,
 		on clickRight := onClickRight p v pic,
 		on paint := onPaint pic,
 		on resize := putStrLn "resize" ]
 	set f [ layout := row 10 [
 			minsize (sz 162 162) $ widget p,
-			minsize (sz 16 16) $ widget v ] ]
+			column 10 [
+				minsize (sz 16 16) $ widget v,
+				minsize (sz 16 16) $ widget c ] ] ]
 
-onKey fp pic f (EventKey (KeyChar 'q') _ _) = do
+scc x	| x < maxBound = succ x
+	| otherwise = x
+prd x	| x > minBound = pred x
+	| otherwise = x
+
+onKey _ c fp pic f (EventKey (KeyChar 'q') _ _) = do
 	p <- readIORef pic
 	close f
 	writePng fp $ toImage p
-onKey _ _ _ k = print k
+onKey clr c _ _ _ (EventKey (KeyChar 'u') _ _) = do
+	PixelRGB8 r g b <- readIORef clr
+	writeIORef clr $ PixelRGB8 (scc r) (scc g) (scc b)
+	readIORef clr >>= print
+	repaint c
+onKey clr c _ _ _ (EventKey (KeyChar 'j') _ _) = do
+	PixelRGB8 r g b <- readIORef clr
+	writeIORef clr $ PixelRGB8 (prd r) (prd g) (prd b)
+	readIORef clr >>= print
+	repaint c
+onKey clr c _ _ _ (EventKey (KeyChar 'i') _ _) = do
+	PixelRGB8 r g b <- readIORef clr
+	writeIORef clr $ PixelRGB8 (scc r) g b
+	readIORef clr >>= print
+	repaint c
+onKey clr c _ _ _ (EventKey (KeyChar 'k') _ _) = do
+	PixelRGB8 r g b <- readIORef clr
+	writeIORef clr $ PixelRGB8 (prd r) g b
+	readIORef clr >>= print
+	repaint c
+onKey clr c _ _ _ (EventKey (KeyChar 'o') _ _) = do
+	PixelRGB8 r g b <- readIORef clr
+	writeIORef clr $ PixelRGB8 r (scc g) b
+	readIORef clr >>= print
+	repaint c
+onKey clr c _ _ _ (EventKey (KeyChar 'l') _ _) = do
+	PixelRGB8 r g b <- readIORef clr
+	writeIORef clr $ PixelRGB8 r (prd g) b
+	readIORef clr >>= print
+	repaint c
+onKey clr c _ _ _ (EventKey (KeyChar 'p') _ _) = do
+	PixelRGB8 r g b <- readIORef clr
+	writeIORef clr $ PixelRGB8 r g (scc b)
+	readIORef clr >>= print
+	repaint c
+onKey clr c _ _ _ (EventKey (KeyChar ';') _ _) = do
+	PixelRGB8 r g b <- readIORef clr
+	writeIORef clr $ PixelRGB8 r g (prd b)
+	readIORef clr >>= print
+	repaint c
+onKey _ _ _ _ _ k = print k
 
 toImage p = generateImage (\x y -> if p !! y !! x
 	then PixelRGB8 0 0 0
@@ -68,6 +118,11 @@ onClickRight pnl v pic p@(Point x y) = do
 	repaint v
 	print p
 	print p
+
+showColor clr dc rect = do
+	PixelRGB8 r g b <- readIORef clr
+	set dc [ brushColor := rgb r g b ]
+	drawRect dc rect []
 
 preview pic dc _ = do
 	p <- readIORef pic
