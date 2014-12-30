0d7b95a2e1ef544d84d32dd8285e929910af1348
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-12-30 16:49:22 +0900

    tribial refactoring module Analizer and module ListLike

--- tools/analyzer/Analyzer.hs
+++ tools/analyzer/Analyzer.hs
@@ -1,36 +1,32 @@
 module Analyzer (
-	Analyzer, runAnalyzer, eof, spot, token, tokens, tokensWhile, listAll) where
+	Analyzer, runAnalyzer,
+	eof, spot, token, tokens, tokensWhile, listAll
+	) where
 
-import Control.Applicative
-import Control.Monad
-import Data.Maybe
+import Control.Applicative (Applicative(..), (<$>))
+import Control.Monad (liftM, ap)
 
 import qualified ListLike as LL
 
-data Analyzer a b = Analyzer { runAnalyzer :: a -> Maybe (b, a) }
+data Analyzer a b =
+	Analyzer { runAnalyzer :: a -> Maybe (b, a) }
 
 instance Monad (Analyzer a) where
 	return = Analyzer . (Just .) . (,)
-	a1 >>= a2 = Analyzer $ \x -> case runAnalyzer a1 x of
-		Just (x', s) -> runAnalyzer (a2 x') s
-		_ -> Nothing
+	a1 >>= a2 = Analyzer $ \s ->
+		case runAnalyzer a1 s of
+			Just (x, s') -> runAnalyzer (a2 x) s'
+			_ -> Nothing
 	fail _ = Analyzer $ const Nothing
-
-instance Functor (Analyzer a) where
-	fmap = liftM
-
-instance Applicative (Analyzer a) where
-	pure = return
-	mf <*> mx = do
-		f <- mf
-		x <- mx
-		return $ f x
+instance Functor (Analyzer a) where fmap = liftM
+instance Applicative (Analyzer a) where pure = return; (<*>) = ap
 
 eof :: LL.ListLike a => Analyzer a Bool
-eof = Analyzer $ \x -> Just (isNothing $ LL.uncons x, x)
+eof = Analyzer $ Just . ((,) <$> LL.null <*> id)
 
-spot :: LL.ListLike a => (LL.Element a -> Bool) -> Analyzer a (LL.Element a)
-spot p = Analyzer $ \x -> case LL.uncons x of
+spot :: LL.ListLike a =>
+	(LL.Element a -> Bool) -> Analyzer a (LL.Element a)
+spot p = Analyzer $ \s -> case LL.uncons s of
 	Just (h, t) | p h -> Just (h, t)
 	_ -> Nothing
 
@@ -40,7 +36,8 @@ token = spot $ const True
 tokens :: LL.ListLike a => Integer -> Analyzer a a
 tokens = Analyzer . (Just .) . LL.splitAt
 
-tokensWhile :: LL.ListLike a => (LL.Element a -> Bool) -> Analyzer a a
+tokensWhile :: LL.ListLike a =>
+	(LL.Element a -> Bool) -> Analyzer a a
 tokensWhile = Analyzer . (Just .) . LL.span
 
 listAll :: LL.ListLike a => Analyzer a b -> Analyzer a [b]
--- tools/analyzer/ListLike.hs
+++ tools/analyzer/ListLike.hs
@@ -1,30 +1,47 @@
 {-# LANGUAGE TypeFamilies #-}
 
-module ListLike (ListLike(..)) where
+module ListLike (ListLike(..), null, span) where
 
-import Prelude hiding (splitAt, span)
+import Prelude hiding (splitAt, null, span)
 import qualified Prelude as P
 
-import Data.List
-import Data.Word8
+import Data.Maybe (isNothing)
+import Data.Word8 (Word8)
 
 import qualified Data.ByteString as BS
 
 class ListLike a where
 	type Element a
+	empty :: a
+	cons :: Element a -> a -> a
 	uncons :: a -> Maybe (Element a, a)
 	splitAt :: Integer -> a -> (a, a)
-	span :: (Element a -> Bool) -> a -> (a, a)
+	splitAt n xs
+		| n <= 0 = (empty, xs)
+		| otherwise = case uncons xs of
+			Just (h, t) -> let
+				(t1, t2) = splitAt (n - 1) t in
+				(cons h t1, t2)
+			_ -> (empty, empty)
 
 instance ListLike [a] where
 	type Element [a] = a
+	empty = []
+	cons = (:)
 	uncons (x : xs) = Just (x, xs)
 	uncons _ = Nothing
-	splitAt = genericSplitAt
-	span = P.span
 
 instance ListLike BS.ByteString where
 	type Element BS.ByteString = Word8
+	empty = BS.empty
+	cons = BS.cons
 	uncons = BS.uncons
 	splitAt = BS.splitAt . fromIntegral
-	span = BS.span
+
+null :: ListLike a => a -> Bool
+null = isNothing . uncons
+
+span :: ListLike a => (Element a -> Bool) -> a -> (a, a)
+span p s = case uncons s of
+	Just (h, t) | p h -> let (t1, t2) = span p t in (cons h t1, t2)
+	_ -> (empty, s)
