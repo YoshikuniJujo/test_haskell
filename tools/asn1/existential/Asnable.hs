a98bfbf3f53046e8d21b6d97a56ebd966fd1d231
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-01-07 16:45:38 +0900

    add ASN.1 decoder which uses existential type

---
+++ tools/asn1/existential/Asnable.hs
@@ -0,0 +1,185 @@
+{-# LANGUAGE ExistentialQuantification, DeriveDataTypeable #-}
+
+module Asnable (
+	runAnalyzer,
+	AsnableBox(..), Raw(..), RawConstructed(..), Rule(..),
+	decodeRec, getAsnable,
+	decodeSel, rawSel, recSel,
+	sequenceSel,
+	Asn1Tag(..), DataClass(..),
+	Asnable(..),
+) where
+
+import Control.Applicative
+import Control.Monad
+import Data.Typeable
+import Data.Maybe
+import Data.List
+import Data.Bits
+import Data.Word8
+
+import qualified Data.ByteString as BS
+
+import Analyzer
+
+class Asnable a where
+	getTag :: a -> Asn1Tag
+
+instance Asnable [a] where
+	getTag _ = Asn1Tag Universal Constructed 16
+
+data AsnableBox = forall a . (Typeable a, Asnable a) =>
+	AsnableBox a deriving Typeable
+
+instance Asnable AsnableBox where
+	getTag (AsnableBox a) = getTag a
+
+getAsnable :: Typeable a => AsnableBox -> Maybe a
+getAsnable (AsnableBox a) = cast a
+
+data Raw = Raw Asn1Tag BS.ByteString
+	deriving (Show, Typeable)
+
+instance Asnable Raw where
+	getTag (Raw t _) = t
+
+data RawConstructed = RawConstructed Asn1Tag [AsnableBox]
+	deriving Typeable
+
+instance Asnable RawConstructed where
+	getTag (RawConstructed t _) = t
+
+instance Show RawConstructed where
+	show (RawConstructed t _) = "RawConstructed " ++ show t ++ " [...]"
+
+decodeRaw :: Analyzer BS.ByteString Raw
+decodeRaw = decodeTag >>= \t ->
+	Raw t <$> (decodeLength >>= maybe (fail "Raw needs length") tokens)
+
+decodeRec :: Analyzer BS.ByteString AsnableBox
+decodeRec = do
+	r@(Raw t@(Asn1Tag _ dc _) s) <- decodeRaw
+	case dc of
+		Primitive -> return $ AsnableBox r
+		_ -> do	let eas = runAnalyzer (listAll decodeRec) s
+			case eas of
+				Left em -> fail em
+				Right (as, "") -> return $
+					AsnableBox $ RawConstructed t as
+				_ -> error "never occur"
+
+runRule :: Rule -> RuleType
+runRule (Rule r) = r
+
+data Rule = Rule RuleType
+
+type RuleType = [Rule] ->
+	Asn1Tag -> Maybe Integer -> Maybe (Analyzer BS.ByteString AsnableBox)
+
+decodeSel :: [Rule] -> Analyzer BS.ByteString AsnableBox
+decodeSel sel = do
+	t <- decodeTag
+	l <- decodeLength
+	fromJust . fromJust . find isJust $
+		map (($ l) . ($ t) . ($ sel) . runRule) sel
+
+rawSel :: RuleType
+rawSel _ t (Just l) = Just $ AsnableBox . Raw t <$> tokens l
+rawSel _ _ _ = Just $ fail "Raw needs length"
+
+recSel :: RuleType
+recSel _ t@(Asn1Tag _ Primitive _) (Just l) = Just $ AsnableBox . Raw t <$> tokens l
+recSel r t (Just l) = Just $ do
+	s <- tokens l
+	let eas = runAnalyzer (listAll $ decodeSel r) s
+	case eas of
+		Left em -> fail em
+		Right (as, "") -> return $ AsnableBox $ RawConstructed t as
+		_ -> error "never occur"
+recSel _ _ _ = fail "No length is not yet implemented"
+
+sequenceSel :: RuleType
+sequenceSel r t@(Asn1Tag Universal Constructed 16) ln@(Just _) = Just $ do
+	rc <- fromJust $ recSel r t ln
+	let Just (RawConstructed _ as) = getAsnable rc
+	return $ AsnableBox as
+sequenceSel _ _ _ = Nothing
+
+---------------------------------------------------------------------------
+
+data Asn1Tag
+	= Asn1Tag TagClass DataClass Integer
+	deriving (Show, Eq)
+
+data TagClass
+	= Universal
+	| Application
+	| ContextSpecific
+	| Private
+	deriving (Show, Eq)
+
+data DataClass
+	= Primitive
+	| Constructed
+	deriving (Show, Eq)
+
+decodeTag :: Analyzer BS.ByteString Asn1Tag
+decodeTag = decodeTag1 >>= \(tc, dc, mtn) -> maybe
+	(Asn1Tag tc dc <$> decodeTagR0)
+	(return . Asn1Tag tc dc . fromIntegral)
+	mtn
+
+decodeTag1 :: Analyzer BS.ByteString
+	(TagClass, DataClass, Maybe Word8)
+decodeTag1 = flip fmap token $ \w -> let
+	tc = case w `shiftR` 6 of
+		0 -> Universal
+		1 -> Application
+		2 -> ContextSpecific
+		3 -> Private
+		_ -> error "never occur"
+	dc = if testBit w 5 then Constructed else Primitive
+	tn = case w .&. 0x1f of
+		0x1f -> Nothing
+		n	| n < 0x1f -> Just n
+			| otherwise ->
+				error "never occur" in
+	(tc, dc, tn)
+
+decodeTagR0 :: Analyzer BS.ByteString Integer
+decodeTagR0 = decodeTagR 0 >>= \n -> do
+	when (n <= 30) $ fail
+		"Use single byte for tag number 0 - 30"
+	return n
+
+decodeTagR :: Integer -> Analyzer BS.ByteString Integer
+decodeTagR 0 = token >>= \w -> do
+	when (w == 0x80) $ fail
+		"Redundant byte for tag number"
+	if testBit w 7
+		then decodeTagR $ fromIntegral (w .&. 0x7f)
+		else return $ fromIntegral w
+decodeTagR n = token >>= \w -> if testBit w 7
+	then decodeTagR $
+		n `shiftL` 7 .|. fromIntegral (w .&. 0x7f)
+	else return $ n `shiftL` 7 .|. fromIntegral w
+
+decodeLength :: Analyzer BS.ByteString (Maybe Integer)
+decodeLength = decodeLength1 >>= \ln1 -> case ln1 of
+	Just (Right ln) -> return . Just $ fromIntegral ln
+	Just (Left n) -> Just <$> decodeLengthR 0 n
+	_ -> return Nothing
+
+decodeLength1 :: Analyzer BS.ByteString
+	(Maybe (Either Word8 Word8))
+decodeLength1 = flip fmap token $ \w -> if not (testBit w 7)
+	then Just $ Right w
+	else let ln = w .&. 0x7f in
+		if ln /= 0 then Just $ Left ln else Nothing
+
+decodeLengthR ::
+	Integer -> Word8 -> Analyzer BS.ByteString Integer
+decodeLengthR ln 0 = return ln
+decodeLengthR ln n = token >>= \w -> decodeLengthR
+	(ln `shiftL` 8 .|. fromIntegral w)
+	(n - 1)
---
+++ tools/asn1/existential/testAsnable.hs
@@ -0,0 +1,21 @@
+import Control.Applicative
+import System.IO.Unsafe
+
+import qualified Data.ByteString as BS
+
+import Asnable
+
+cert :: BS.ByteString
+cert = unsafePerformIO $ BS.readFile "test_ASN_1_cert.der"
+
+data Rawest = RC Asn1Tag [Rawest] | RP Asn1Tag BS.ByteString
+	deriving Show
+
+toRawest :: AsnableBox -> Rawest
+toRawest ab = case getTag ab of
+	t@(Asn1Tag _ Constructed _) -> let
+		Just (RawConstructed _ as) = getAsnable ab in
+		RC t $ map toRawest as
+	t -> let
+		Just (Raw _ bs) = getAsnable ab in
+		RP t bs
