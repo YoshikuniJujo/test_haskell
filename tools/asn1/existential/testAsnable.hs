db573b278e481b4f9e0edf0fd315d8f2332e1e5d
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-01-09 21:26:46 +0900

    ASN.1: ExistentialQuantification: refactoring testAsnable.hs

--- tools/asn1/existential/testAsnable.hs
+++ tools/asn1/existential/testAsnable.hs
@@ -10,17 +10,22 @@ import Asnable
 cert :: BS.ByteString
 cert = unsafePerformIO $ BS.readFile "test_ASN_1_cert.der"
 
-test :: BS.ByteString
-test = "\x30\x80\x01\x01\x88\x00\x00"
+ind_test :: BS.ByteString
+ind_test = "\x30\x80\x01\x01\x88\x00\x00"
 
-data Rawest = RC Asn1Tag [Rawest] | RP Asn1Tag BS.ByteString
+data Rw = RC Asn1Tag [Rw] | RP Asn1Tag BS.ByteString
 	deriving Show
 
-toRawest :: AsnableBox -> Rawest
-toRawest ab = case getAsn1Tag ab of
+toRw :: AsnableBox -> Rw
+toRw ab = case getAsn1Tag ab of
 	t@(Asn1Tag _ Constructed _) -> let
 		Just (RawConstructed _ as) = getAsnable ab in
-		RC t $ map toRawest as
+		RC t $ map toRw as
 	t -> let
 		Just (Raw _ bs) = getAsnable ab in
 		RP t bs
+
+main :: IO ()
+main = let
+	Right (ab, "") = runAnalyzer (decodeWith [Rule recRule]) ind_test in
+	print $ toRw ab
