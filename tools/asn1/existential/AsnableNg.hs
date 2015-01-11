303de10d67d704e0f0df61dd206d0172142fcaf9
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-01-11 10:34:47 +0900

    ASN.1: Existential decode/encode: tribial refactoring AsnableNg.hs

--- tools/asn1/existential/AsnableNg.hs
+++ tools/asn1/existential/AsnableNg.hs
@@ -212,8 +212,8 @@ encodeTag (Asn1Tag tc dc n)
 	| n < 31 = BS.pack [c .|. fromIntegral n]
 	| otherwise = BS.pack $ (c .|. 0x1f) : encodeTagR n
 	where
-	c = fromIntegral $
-		fromEnum tc `shiftL` 6 .|. fromEnum dc `shiftL` 5
+	c = fromIntegral $ fromEnum tc `shiftL` 6 .|.
+		fromEnum dc `shiftL` 5
 
 encodeTagR :: Integer -> [Word8]
 encodeTagR n = map (0x80 .|.) (reverse ws) ++ [w]
@@ -229,7 +229,8 @@ encodeLength :: Maybe Integer -> BS.ByteString
 encodeLength (Just n)
 	| n < 0 = error "No negative length"
 	| n < 128 = BS.pack [fromIntegral n]
-	| otherwise = BS.pack $ (0x80 .|. fromIntegral (length ws)) : ws
+	| otherwise = BS.pack $
+		(0x80 .|. fromIntegral (length ws)) : ws
 	where
 	ws = reverse $ integerToWord8s n
 encodeLength _ = "\x80"
