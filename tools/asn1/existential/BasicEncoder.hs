486b38e8a085698f18288de4e7c31267f9a37a8e
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-01-13 13:43:47 +0900

    ASN.1: Existential: select length field's length

--- tools/asn1/existential/BasicEncoder.hs
+++ tools/asn1/existential/BasicEncoder.hs
@@ -33,15 +33,24 @@ integerToWord7s 0 = []
 integerToWord7s n = fromIntegral (n .&. 0x7f) :
 	integerToWord7s (n `shiftR` 7)
 
-encodeLength :: Maybe Integer -> BS.ByteString
-encodeLength (Just n)
-	| n < 0 = error "No negative length"
-	| n < 128 = BS.pack [fromIntegral n]
+encodeLength :: Int -> Maybe Integer -> BS.ByteString
+encodeLength _ (Just l)
+	| l < 0 = error "No negative length"
+encodeLength 0 (Just l)
+	| l < 128 = BS.pack [fromIntegral l]
 	| otherwise = BS.pack $
 		(0x80 .|. fromIntegral (length ws)) : ws
 	where
-	ws = reverse $ integerToWord8s n
-encodeLength _ = "\x80"
+	ws = reverse $ integerToWord8s l
+encodeLength n (Just l)
+	| ln >= n = BS.pack $
+		(0x80 .|. fromIntegral ln) : ws
+	| otherwise = BS.pack $
+		(0x80 .|. fromIntegral n) : replicate (n - ln) 0x00 ++ ws
+	where
+	ws = reverse $ integerToWord8s l
+	ln = length ws
+encodeLength _ _ = "\x80"
 
 integerToWord8s :: Integer -> [Word8]
 integerToWord8s 0 = []
