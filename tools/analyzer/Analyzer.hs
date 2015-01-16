fd6dfbdd6a378f310508399ce8e4b985c2730864
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-01-16 12:06:14 +0900

    binary analyzer: refactoring Analyzer.hs

--- tools/analyzer/Analyzer.hs
+++ tools/analyzer/Analyzer.hs
@@ -1,61 +1,67 @@
 module Analyzer (
-	Analyzer, runAnalyzer,
-	eof, spot, token, tokens, tokensWhile, listAll, listMap,
-	) where
+	Analyzer, runAnalyzer, eof, spot, token,
+	tokens, tokensWhile, listAll, listMap ) where
 
 import Control.Applicative (Applicative(..), (<$>))
 import Control.Monad (liftM, ap)
 
 import qualified ListLike as LL
 
-data Analyzer a b =
-	Analyzer { runAnalyzer :: a -> Either String (b, a) }
+moduleName :: String
+moduleName = "Analyzer"
 
-instance Monad (Analyzer a) where
+data Analyzer s a = Analyzer {
+	runAnalyzer :: s -> Either String (a, s) }
+
+instance Monad (Analyzer s) where
 	return = Analyzer . (Right .) . (,)
-	a1 >>= a2 = Analyzer $ \s ->
-		case runAnalyzer a1 s of
-			Right (x, s') -> runAnalyzer (a2 x) s'
+	a >>= b = Analyzer $ \s ->
+		case runAnalyzer a s of
+			Right (x, t) -> runAnalyzer (b x) t
 			Left err -> Left err
 	fail = Analyzer . const . Left
-instance Functor (Analyzer a) where fmap = liftM
-instance Applicative (Analyzer a) where pure = return; (<*>) = ap
 
-eof :: LL.ListLike a => Analyzer a Bool
+instance Functor (Analyzer s) where
+	fmap = liftM
+
+instance Applicative (Analyzer s) where
+	pure = return; (<*>) = ap
+
+eof :: LL.ListLike s => Analyzer s Bool
 eof = Analyzer $ Right . ((,) <$> LL.null <*> id)
 
-spot :: LL.ListLike a =>
-	(LL.Element a -> Bool) -> Analyzer a (LL.Element a)
+spot :: LL.ListLike s =>
+	(LL.Element s -> Bool) -> Analyzer s (LL.Element s)
 spot p = Analyzer $ \s -> case LL.uncons s of
 	Just (h, t) | p h -> Right (h, t)
-	_ -> Left "spot error"
+	_ -> Left $ moduleName ++ ".spot: parse error"
 
-token :: LL.ListLike a => Analyzer a (LL.Element a)
+token :: LL.ListLike s => Analyzer s (LL.Element s)
 token = spot $ const True
 
-tokens :: LL.ListLike a => Integer -> Analyzer a a
+tokens :: LL.ListLike s => Integer -> Analyzer s s
 tokens = Analyzer . (Right .) . LL.splitAt
 
-tokensWhile :: LL.ListLike a =>
-	(LL.Element a -> Bool) -> Analyzer a a
+tokensWhile :: LL.ListLike s =>
+	(LL.Element s -> Bool) -> Analyzer s s
 tokensWhile = Analyzer . (Right .) . LL.span
 
-listAll :: LL.ListLike a => Analyzer a b -> Analyzer a [b]
-listAll = loopWhile eof
+listAll :: LL.ListLike s => Analyzer s a -> Analyzer s [a]
+listAll = loopUntil eof
 
-loopWhile :: Monad m => m Bool -> m a -> m [a]
-loopWhile p m = do
+loopUntil :: Monad m => m Bool -> m a -> m [a]
+loopUntil p m = do
 	e <- p
 	if e then return [] else
-		(:) `liftM` m `ap` loopWhile p m
+		(:) `liftM` m `ap` loopUntil p m
 
 listMap :: LL.ListLike b =>
 	(a -> Analyzer b c) -> [a] -> Analyzer b [c]
-listMap = mapWhile eof
+listMap = mapUntil eof
 
-mapWhile :: Monad m => m Bool -> (a -> m b) -> [a] -> m [b]
-mapWhile _ _ [] = return []
-mapWhile p m (x : xs) = do
+mapUntil :: Monad m => m Bool -> (a -> m b) -> [a] -> m [b]
+mapUntil _ _ [] = return []
+mapUntil p m (x : xs) = do
 	e <- p
 	if e then return [] else
-		(:) `liftM` m x `ap` mapWhile p m xs
+		(:) `liftM` m x `ap` mapUntil p m xs
