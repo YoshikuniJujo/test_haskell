fb8190da48597795b2367ea793b5d703bf557594
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-05-21 13:11:15 +0900

    shorten binary count golf

--- tribial/codeiq/binary_count/binaryCount_golf.hs
+++ tribial/codeiq/binary_count/binaryCount_golf.hs
@@ -1,4 +1,5 @@
-import Data.List
 main=putStr$show(f$10^3)++" "++show(f$10^10)
 f 0=0
-f n|odd n=2*f(div n 2)+(n+1)`div`2|1>0=f(n-1)+sum(unfoldr(\n->case n of 0->Nothing;_->Just(n`mod`2,n`div`2))n)
+f n|odd n=2*f(div n 2)+(n+1)`div`2|1>0=f(n-1)+b n
+b 0=0
+b n=n`mod`2+b(n`div`2)
