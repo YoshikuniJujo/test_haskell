4865d5b91b81d577f3553034a0189c5c973272bc
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-06-17 11:09:17 +0900

    golf: shorten factoring2S.hs

--- tribial/golf/factoring2S.hs
+++ tribial/golf/factoring2S.hs
@@ -1,2 +1,4 @@
+(!)=div
+d=fromIntegral
 main=interact$unwords.map show.f.map read.words
-f[p,q,r]=[p`div`i`div`k,i,j,k,l]where z=round$(fromIntegral q^2-4*fromIntegral p*fromIntegral r)**0.5;[b,c,e]=[2*p,q+z,q-z];[g,h]=[gcd c b*signum p,gcd e b*signum p];[v,w,x,y]=[b`div`g,c`div`g,b`div`h,e`div`h];[i,j,k,l]=if(x,y)>(v,w)then[x,y,v,w]else[v,w,x,y]
+f[p,q,r]=[p!i!k,i,j,k,l]where z=round$(d q^2-4*d p*d r)**0.5;[b,c,e]=[2*p,q+z,q-z];s=signum p;[g,h]=[gcd c b*s,gcd e b*s];[v,w,x,y]=[b!g,c!g,b!h,e!h];[i,j,k,l]=if(x,y)>(v,w)then[x,y,v,w]else[v,w,x,y]
