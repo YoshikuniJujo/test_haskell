7005bc7791c83d1a580ad8e01cbe758c8e459ef2
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-08-21 20:37:35 +0900

    add example of let ... in ... and recursive with 3 functions

--- tribial/letin.hs
+++ tribial/letin.hs
@@ -27,3 +27,15 @@ fg'' = (\fx f g -> fix fx f g 10)
 	(\fx f g -> f (fx f g) (fx g f))
 	(\f g n -> case n of 0 -> 0; _ -> 2 + g (n - 1))
 	(\g f n -> case n of 0 -> 0; _ -> 3 + f (n - 1))
+
+fgh = let
+	f n = case n of 0 -> 0; _ -> 2 + g (n - 1)
+	g n = case n of 0 -> 0; _ -> 3 + h (n - 1)
+	h n = case n of 0 -> 0; _ -> 7 + f (n - 1) in
+	f 10
+
+fgh' = (\fx f g h -> fix fx f g h 10)
+	(\fx f g h -> f (fx f g h) (fx g h f) (fx h f g))
+	(\f g h n -> case n of 0 -> 0; _ -> 2 + g (n - 1))
+	(\g h f n -> case n of 0 -> 0; _ -> 3 + h (n - 1))
+	(\h f g n -> case n of 0 -> 0; _ -> 7 + f (n - 1))
