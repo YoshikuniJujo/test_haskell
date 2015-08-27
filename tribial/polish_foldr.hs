487021e4c20db8d81e9a8b018702888c41c5b7ea
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-08-27 11:52:33 +0900

    correct polish calculator using foldr

--- tribial/polish_foldr.hs
+++ tribial/polish_foldr.hs
@@ -1,4 +1,5 @@
 import Control.Applicative
+import Text.Read
 
 example1, example2, example3, example4 :: [String]
 example1 = ["+", "3", "4"]
@@ -6,11 +7,23 @@ example2 = ["*", "+", "1", "5", "+", "2", "3"]
 example3 = ["/", "*", "+", "1", "5", "+", "2", "3", "4"]
 example4 = ["/", "*", "+", "8", "5", "-", "2", "5", "4"]
 
-type State = [Integer]
+operators :: [(String, Integer -> Integer -> Integer)]
+operators = [("+", (+)), ("-", (-)), ("*", (*)), ("/", div)]
 
-update :: String -> Maybe State -> Maybe State
-update "+" (Just (x : y : ns)) = Just $ x + y : ns
-update "*" (Just (x : y : ns)) = Just $ x * y : ns
-update s ns = case reads s of
-	(n, "") : _ -> (n :) <$> ns
-	_ -> Nothing
+polish1 :: String -> Maybe [Integer] -> Maybe [Integer]
+polish1 s (Just ns) = case lookup s operators of
+	Just o -> case ns of
+		x : y : ns -> Just $ x `o` y : ns
+		_ -> Nothing
+	_ -> maybe Nothing (Just . (: ns)) $ readMaybe s
+polish1 _ _ = Nothing
+
+polish :: [String] -> Maybe [Integer]
+polish = foldr polish1 $ Just []
+
+polish1' :: String -> [Integer] -> Maybe [Integer]
+polish1' s (x : y : ns) | Just o <- lookup s operators = Just $ x `o` y : ns
+polish1' s ns = (: ns) <$> readMaybe s
+
+polish' :: [String] -> Maybe [Integer]
+polish' = foldr ((=<<) . polish1') $ Just []
