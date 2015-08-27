281e8fe4a49a4df9fd6d7f6e76f1a9fc4377fe4f
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-08-27 09:53:10 +0900

    correct polish calculator

--- tribial/polish.hs
+++ tribial/polish.hs
@@ -1,4 +1,7 @@
+{-# LANGUAGE MonadComprehensions #-}
+
 import Control.Applicative
+import Text.Read
 
 example1, example2, example3, example4 :: [String]
 example1 = ["+", "3", "4"]
@@ -15,6 +18,13 @@ polish (s : ss) = case lookup s operators of
 	Just o -> case polish ss of
 		Just (x : y : ns) -> Just $ x `o` y : ns
 		_ -> Nothing
-	_ -> case reads s of
-		(n, "") : _ -> (n :) <$> polish ss
+	_ -> case readMaybe s of
+		Just n -> maybe Nothing (Just . (n :)) $ polish ss
 		_ -> Nothing
+
+polish' :: [String] -> Maybe [Integer]
+polish' (s : ss) = maybe
+	((:) <$> readMaybe s <*> polish' ss)
+	(\o -> [ x `o` y : ns | x : y : ns <- polish' ss ])
+	(lookup s operators)
+polish' _ = return []
