bd9c2ad5395e7c96b988c8aebcf8566f84b8076a
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-09-20 05:54:02 +0900

    refactoring churchNaive.hs

--- tribial/church/churchNaive.hs
+++ tribial/church/churchNaive.hs
@@ -1,17 +1,24 @@
+toInt n = n (+ 1) 0
+toChurch 0 = zero
+toChurch n = s . toChurch $ n - 1
+
 false x y = y
 true = \x -> \y -> x
 _if b x y = b x y
 
 zero = \f x -> x
-one' f x = f x
+s n f x = f $ n f x
+one = s zero
+two = s one
+three = s two
+four = s three
+five = s four
+six = s five
 
-s n f x = f (n f x)
 isZero n = n (\x -> false) true
-add m n f x = m f (n f x)
-mult m n f = m (n f)
-toInt n = n (+ 1) 0
-toChurch 0 = zero
-toChurch n = s (toChurch (n - 1))
-pre n f x = (n (\g h -> h (g f)) (\u -> x)) (\u -> u)
+add m n f x = m f $ n f x
+mult m n f = m $ n f
+pre n f x = n (\g h -> h $ g f) (\u -> x) (\u -> u)
 
--- hoge n = _if (isZero n) one n
+zeroToOne n = _if (isZero n) one zero
+-- zeroToOne n = _if (isZero n) one n
