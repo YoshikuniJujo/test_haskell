d8e27327fc12730291e5c80d8344602ce1521b6a
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-09-19 12:45:56 +0900

    refactoring church.hs

--- tribial/church.hs
+++ tribial/church.hs
@@ -1,4 +1,4 @@
-data C = C { unC :: C -> C } | A C | Z
+data C = C { (.$.) :: C -> C } | A C | Z
 
 toInt :: C -> Int
 toInt Z = 0
@@ -9,28 +9,24 @@ toChurch :: Int -> C
 toChurch 0 = zero
 toChurch n = s .$. toChurch (n - 1)
 
-(.$.) :: C -> C -> C
-(.$.) = unC
-
 false, true, _if :: C
-false = C (\x -> C (\y -> y))
-true = C (\x -> C (\y -> x))
-_if = C (\b -> C (\x -> C (\y -> b .$. x .$. y)))
+false = C $ \x -> C $ \y -> y
+true = C $ \x -> C $ \y -> x
+_if = C $ \b -> C $ \x -> C $ \y -> b .$. x .$. y
 
 zero, one, s :: C
-zero = C (\f -> C (\x -> x))
-one = C (\f -> C (\x -> f .$. x))
-s = C (\n -> C (\f -> C (\x -> f .$. (n .$. f .$. x))))
+zero = C $ \f -> C $ \x -> x
+one = C $ \f -> C $ \x -> f .$. x
+s = C $ \n -> C $ \f -> C $ \x -> f .$. (n .$. f .$. x)
 
 isZero :: C
-isZero = C (\n -> n .$. (C (\x -> false)) .$. true)
+isZero = C $ \n -> n .$. C (\x -> false) .$. true
 
 add, mult, pre :: C
-add = C (\m -> C (\n -> C (\f -> C (\x -> m .$. f .$. (n .$. f .$. x)))))
-mult = C (\m -> C (\n -> C (\f -> m .$. (n .$. f))))
-pre = C (\n -> C (\f -> C (\x ->
-	(n .$. (C (\g -> C (\h -> h .$. (g .$. f))))
-		.$. (C (\u -> x))) .$. (C (\u -> u)))))
+add = C $ \m -> C $ \n -> C $ \f -> C $ \x -> m .$. f .$. (n .$. f .$. x)
+mult = C $ \m -> C $ \n -> C $ \f -> m .$. (n .$. f)
+pre = C $ \n -> C $ \f -> C $ \x ->
+	n .$. C (\g -> C $ \h -> h .$. (g .$. f)) .$. C (\u -> x) .$. C (\u -> u)
 
 hoge :: C
-hoge = C (\n -> _if .$. (isZero .$. n) .$. one .$. n)
+hoge = C $ \n -> _if .$. (isZero .$. n) .$. one .$. n
