44332762f7fe47ab15e508171325cfd1b64fc871
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-10-21 12:19:21 +0900

    refactoring eight queens

--- tribial/eightqueen.hs
+++ tribial/eightqueen.hs
@@ -1,12 +1,13 @@
+-- % echo 8 | runhaskell eightqueen.hs
+
 import Control.Applicative
 import Data.List
 
+main :: IO ()
+main = interact $ (++ "\n") . show . (\n -> queens n n [] [] []) . read
+
 queens :: Int -> Int -> [Int] -> [Int] -> [Int] -> [[Int]]
 queens _ 0 _ _ _ = [[]]
-queens n i ls hs rs = qs >>= \q ->
-	map (q :) $ queens n (i - 1)
-		(map (subtract 1) $ q : ls)
-		(q : hs)
-		(map (+ 1) $ q : rs)
-	where
-	qs = [0 .. n - 1] \\ (ls ++ hs ++ rs)
+queens n i ls hs rs = [ q : qs |
+	q <- [0 .. n - 1] \\ (ls ++ hs ++ rs),
+	qs <- queens n (i - 1) (pred <$> q : ls) (q : hs) (succ <$> q : rs) ]
