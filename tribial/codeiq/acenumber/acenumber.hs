af3dd84e6c339c8e3f0861c712d6e13f966fb3fe
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-10-23 11:39:59 +0900

    refactoring acenumber

--- tribial/codeiq/acenumber/acenumber.hs
+++ tribial/codeiq/acenumber/acenumber.hs
@@ -3,25 +3,26 @@ import Data.List
 main :: IO ()
 main = interact $ (++ "\n") . show . f . read
 
-as :: Integral n => [n]
-as = iterate (modulo . (* 16)) 0xA
-
-modulo :: Integral n => n -> n
-modulo = (`mod` 10 ^ 6)
-
 f :: Integral n => n -> n
-f n	| n' < 0 = modulo . sum' $ take (fromIntegral n) xs
-	| otherwise = modulo $ sum' xs
-		+ sum' ys `mul` (n' `div` m)
-		+ sum' (take (fromIntegral $ n' `mod` m) ys)
-	where
-	sum' = foldl' ((modulo .) . (+)) 0
-	mul = (modulo .) . (*)
-	(xs, ys) = period [] as
-	m = fromIntegral $ length ys
-	n' = n - fromIntegral (length xs)
+f n	| n' < 0 = sum' $ take (fromIntegral n) bs
+	| otherwise = modulo $ sum' bs
+		+ sum' rs `mul` (n' `div` fromIntegral m)
+		+ sum' (take (fromIntegral n' `mod` m) rs)
+	where (bs, rs) = reps; m = length rs; n' = n - fromIntegral (length bs)
 
-period :: Integral n => [n] -> [n] -> ([n], [n])
+reps :: Integral n => ([n], [n])
+reps = period [] $ iterate (modulo . (* 16)) 0xA
+
+period :: Eq n => [n] -> [n] -> ([n], [n])
 period ps (x : xs)
 	| Just p <- x `elemIndex` ps = splitAt (length ps - p - 1) $ reverse ps
 	| otherwise = period (x : ps) xs
+
+sum' :: Integral n => [n] -> n
+sum' = foldl' ((modulo .) . (+)) 0
+
+mul :: Integral n => n -> n -> n
+mul = (modulo .) . (*)
+
+modulo :: Integral n => n -> n
+modulo = (`mod` 10 ^ (6 :: Int))
