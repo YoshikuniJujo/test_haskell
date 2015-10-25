59fd5b084d55095381ee886645c47bf0e3f1cc29
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-10-26 07:14:53 +0900

    refactoring code sample: NML parser

--- tribial/lecture/NmlParse.hs
+++ tribial/lecture/NmlParse.hs
@@ -1,3 +1,5 @@
+module NmlParse (nml) where
+
 import Data.List (unfoldr)
 import Data.Tree (Tree(..))
 import Data.Char (isSpace)
@@ -8,9 +10,9 @@ nml :: String -> Maybe Nml
 nml s = case parse $ tokens s of (Just n, []) -> Just n; _ -> Nothing
 
 parse :: [Token] -> (Maybe Nml, [Token])
-parse (Right tx : ts) = (Just $ Node tx [], ts)
-parse (Left (Open, tg) : s) = case parses s of
-	(ns, Left (Close, tg') : r) | tg == tg' -> (Just $ Node tg ns, r)
+parse (Text tx : ts) = (Just $ Node tx [], ts)
+parse (Open tg : s) = case parses s of
+	(ns, Close tg' : r) | tg == tg' -> (Just $ Node tg ns, r)
 	(ns, r) -> (Just $ Node tg ns, r)
 parse ts = (Nothing, ts)
 
@@ -19,19 +21,18 @@ parses ts = case parse ts of
 	(Just n, r) -> let (ns, r') = parses r in (n : ns, r')
 	(_, r) -> ([], r)
 
-type Token = Either (OC, String) String
+data Token = Open String | Close String | Text String deriving Show
 
 tokens :: String -> [Token]
 tokens = concatMap emptyTag . spaces . catTexts . unfoldr token
 
 emptyTag :: Token -> [Token]
-emptyTag (Left (Open, tg)) | last tg == '/' =
-	let tg' = init tg in [Left (Open, tg'), Left (Close, tg')]
+emptyTag (Open tg) | last tg == '/' = map ($ init tg) [Open, Close]
 emptyTag tg = [tg]
 
 spaces :: [Token] -> [Token]
-spaces (Right tx : ts) | all isSpace tx = spaces ts
-spaces (Right tx : ts) = Right tx' : spaces ts
+spaces (Text tx : ts) | all isSpace tx = spaces ts
+spaces (Text tx : ts) = Text tx' : spaces ts
 	where tx' = takeWhileR (not . isSpace) $ dropWhile isSpace tx
 spaces (t : ts) = t : spaces ts
 spaces _ = []
@@ -44,30 +45,26 @@ takeWhileR = twr [] where
 	twr _ _ _ = []
 
 catTexts :: [Token] -> [Token]
-catTexts (Right tx1 : Right tx2 : ts) = catTexts $ Right (tx1 ++ tx2) : ts
+catTexts (Text tx1 : Text tx2 : ts) = catTexts $ Text (tx1 ++ tx2) : ts
 catTexts (t : ts) = t : catTexts ts
 catTexts _ = []
 
 token :: String -> Maybe (Token, String)
 token "" = Nothing
-token ('<' : s) = Just $ let (tg, r) = tag s in (Left tg, r)
-token ('&' : s) = case entity s of
-	Just (e, r) -> Just (Right [e], r)
-	_ -> Nothing
-token s = Just $ let (tx, r) = span (`notElem` "<&") s in (Right tx, r)
+token ('<' : s) = Just $ tag s
+token ('&' : s) = entity s
+token s = Just $ let (tx, r) = span (`notElem` "<&") s in (Text tx, r)
 
-data OC = Open | Close deriving Show
-
-tag :: String -> ((OC, String), String)
+tag :: String -> (Token, String)
 tag ('/' : s) = case span (/= '>') s of
-	(tg, _ : r) -> ((Close, tg), r)
-	(tg, _) -> ((Close, tg), "")
+	(tg, _ : r) -> (Close tg, r)
+	(tg, _) -> (Close tg, "")
 tag s = case span (/= '>') s of
-	(tg, _ : r) -> ((Open, tg), r)
-	(tg, _) -> ((Open, tg), "")
+	(tg, _ : r) -> (Open tg, r)
+	(tg, _) -> (Open tg, "")
 
-entity :: String -> Maybe (Char, String)
-entity ('l' : 't' : ';' : s) = Just ('<', s)
-entity ('g' : 't' : ';' : s) = Just ('>', s)
-entity ('a' : 'm' : 'p' : ';' : s) = Just ('&', s)
+entity :: String -> Maybe (Token, String)
+entity ('l' : 't' : ';' : s) = Just (Text "<", s)
+entity ('g' : 't' : ';' : s) = Just (Text ">", s)
+entity ('a' : 'm' : 'p' : ';' : s) = Just (Text "&", s)
 entity _ = Nothing
