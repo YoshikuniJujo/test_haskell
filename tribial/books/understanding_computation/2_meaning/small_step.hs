16ffd5a634b4acdd326906effebbaeca8a7704cc
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-11-02 10:25:31 +0900

    add to small_step.hs: Bool

--- tribial/books/understanding_computation/2_meaning/small_step.hs
+++ tribial/books/understanding_computation/2_meaning/small_step.hs
@@ -4,17 +4,22 @@ import Data.List
 
 data Expression
 	= N Integer | Expression :+: Expression | Expression :*: Expression
+	| B Bool | Expression :<: Expression
 	deriving Show
 
 reduce :: Expression -> Maybe Expression
 reduce (N l :+: N r) = Just . N $ l + r
 reduce (N l :*: N r) = Just . N $ l * r
+reduce (N l :<: N r) = Just . B $ l < r
 reduce (l :+: r)
 	| Just l' <- reduce l = Just $ l' :+: r
 	| Just r' <- reduce r = Just $ l :+: r'
 reduce (l :*: r)
 	| Just l' <- reduce l = Just $ l':*:  r
 	| Just r' <- reduce r = Just $ l :*: r'
+reduce (l :<: r)
+	| Just l' <- reduce l = Just $ l' :<: r
+	| Just r' <- reduce r = Just $ l :<: r'
 reduce _ = Nothing
 
 run :: Expression -> [Expression]
