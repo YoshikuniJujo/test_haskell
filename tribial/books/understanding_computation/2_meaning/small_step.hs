e2d8ffc51c5568fd38d9c4ed64dafa90f5a269e1
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-11-04 12:16:29 +0900

    enhance small_step.hs

--- tribial/books/understanding_computation/2_meaning/small_step.hs
+++ tribial/books/understanding_computation/2_meaning/small_step.hs
@@ -1,26 +1,47 @@
 -- import Control.Applicative
 import Data.Maybe
-import Data.List
+-- import Data.List
+import qualified Data.Map as Map
+
+data Statement
+	= DoNothing
+	| Assign String Expression
+	deriving Show
+
+reduceSt :: Statement -> Environment -> Maybe (Statement, Environment)
+reduceSt DoNothing _ = Nothing
+reduceSt (Assign v ex) env
+	| Just ex' <- reduceEx ex env = Just (Assign v ex', env)
+	| otherwise = Just (DoNothing, Map.insert v ex env)
+
+runSt :: Statement -> Environment -> [(Statement, Environment)]
+runSt ex env = map fromJust . takeWhile isJust
+	. iterate (>>= uncurry reduceSt) $ Just (ex, env)
 
 data Expression
 	= N Integer | Expression :+: Expression | Expression :*: Expression
 	| B Bool | Expression :<: Expression
+	| V String
 	deriving Show
 
-reduce :: Expression -> Maybe Expression
-reduce (N l :+: N r) = Just . N $ l + r
-reduce (N l :*: N r) = Just . N $ l * r
-reduce (N l :<: N r) = Just . B $ l < r
-reduce (l :+: r)
-	| Just l' <- reduce l = Just $ l' :+: r
-	| Just r' <- reduce r = Just $ l :+: r'
-reduce (l :*: r)
-	| Just l' <- reduce l = Just $ l':*:  r
-	| Just r' <- reduce r = Just $ l :*: r'
-reduce (l :<: r)
-	| Just l' <- reduce l = Just $ l' :<: r
-	| Just r' <- reduce r = Just $ l :<: r'
-reduce _ = Nothing
+type Environment = Map.Map String Expression
 
-run :: Expression -> [Expression]
-run = map fromJust . takeWhile isJust . iterate (>>= reduce) . Just
+reduceEx :: Expression -> Environment -> Maybe Expression
+reduceEx (N l :+: N r) _ = Just . N $ l + r
+reduceEx (N l :*: N r) _ = Just . N $ l * r
+reduceEx (N l :<: N r) _ = Just . B $ l < r
+reduceEx (l :+: r) env
+	| Just l' <- reduceEx l env = Just $ l' :+: r
+	| Just r' <- reduceEx r env = Just $ l :+: r'
+reduceEx (l :*: r) env
+	| Just l' <- reduceEx l env = Just $ l':*:  r
+	| Just r' <- reduceEx r env = Just $ l :*: r'
+reduceEx (l :<: r) env
+	| Just l' <- reduceEx l env = Just $ l' :<: r
+	| Just r' <- reduceEx r env = Just $ l :<: r'
+reduceEx (V v) env = Map.lookup v env
+reduceEx _ _ = Nothing
+
+runEx :: Expression -> Environment -> [Expression]
+runEx ex env = map fromJust . takeWhile isJust
+	. iterate (>>= (`reduceEx` env)) $ Just ex
