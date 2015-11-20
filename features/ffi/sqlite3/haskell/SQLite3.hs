a9542dfd51ebdb6ce333f941f9916da2e78851bb
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-11-20 23:26:49 +0900

    simple C bindings to SQLite3

--- features/ffi/sqlite3/haskell/SQLite3.hs
+++ features/ffi/sqlite3/haskell/SQLite3.hs
@@ -1,8 +1,13 @@
-module SQLite3 (SQLite3, withSQLite3) where
+{-# LANGUAGE TupleSections #-}
 
+module SQLite3 (SQLite3, withSQLite3, withPrepared, step, column) where
+
+import Control.Applicative
 import Control.Monad
 import Control.Exception
-import System.IO.Error
+import qualified Data.ByteString as BS
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
 import Foreign
 import Foreign.C.Types
 import Foreign.C.String
@@ -10,6 +15,7 @@ import Foreign.C.String
 import SQLite3.Constants
 
 data SQLite3 = SQLite3 (Ptr SQLite3) deriving Show
+data SQLite3Stmt = SQLite3Stmt (Ptr SQLite3Stmt) deriving Show
 
 foreign import ccall unsafe "sqlite3.h sqlite3_open" c_sqlite3_open ::
 	CString -> Ptr (Ptr SQLite3) -> IO CInt
@@ -41,3 +47,77 @@ sqlite3Close sq@(SQLite3 db) = do
 
 sqlite3Errmsg :: SQLite3 -> IO String
 sqlite3Errmsg (SQLite3 db) = peekCString =<< c_sqlite3_errmsg db
+
+foreign import ccall unsafe "sqlite3.h sqlite3_prepare_v2"
+	c_sqlite3_prepare_v2 ::
+		Ptr SQLite3 -> CString -> CInt -> Ptr (Ptr SQLite3Stmt) ->
+		(Ptr CString) -> IO CInt
+foreign import ccall unsafe "sqlite3.h sqlite3_finalize" c_sqlite3_finalize ::
+	Ptr SQLite3Stmt -> IO CInt
+
+withPrepared :: SQLite3 -> String -> (SQLite3Stmt -> IO a) -> IO (a, String)
+withPrepared db sql act =
+	bracket (sqlite3PrepareV2 db sql) (sqlite3Finalize . fst) $
+		\(sm, t) -> (, t) <$> act sm
+
+sqlite3PrepareV2 :: SQLite3 -> String -> IO (SQLite3Stmt, String)
+sqlite3PrepareV2 db@(SQLite3 pdb) sql =
+	withCString sql $ \csql -> alloca $ \psm -> alloca $ \pt -> do
+		ret <- c_sqlite3_prepare_v2 pdb csql (- 1) psm pt
+		sm <- SQLite3Stmt <$> peek psm
+		when (ret /= sQLITE_OK) $ do
+			em <- sqlite3Errmsg db
+			sqlite3Finalize sm
+			ioError . userError $ "Cannot prepare: " ++ em
+		(sm ,) <$> (peekCString =<< peek pt)
+
+sqlite3Finalize :: SQLite3Stmt -> IO ()
+sqlite3Finalize (SQLite3Stmt psm) = do
+	ret <- c_sqlite3_finalize psm
+	when (ret /= sQLITE_OK) $
+		ioError . userError $
+			"Cannot finalize stmt: error code (" ++ show ret ++ ")"
+
+data ResultCode = SQLiteBusy | SQLiteRow | SQLiteDone deriving Show
+
+foreign import ccall unsafe "sqlite3.h sqlite3_step" c_sqlite3_step ::
+	Ptr SQLite3Stmt -> IO CInt
+
+step :: SQLite3Stmt -> IO ResultCode
+step (SQLite3Stmt psm) = do
+	ret <- c_sqlite3_step psm
+	case ret of
+		_	| ret == sQLITE_BUSY -> return SQLiteBusy
+			| ret == sQLITE_ROW -> return SQLiteRow
+			| ret == sQLITE_DONE -> return SQLiteDone
+		_ -> ioError . userError $
+			"Error while step: error code (" ++ show ret ++ ")"
+
+foreign import ccall unsafe "sqlite3.h sqlite3_column_int"
+	c_sqlite3_column_int :: Ptr SQLite3Stmt -> CInt -> IO CInt
+foreign import ccall unsafe "sqlite3.h sqlite3_column_text"
+	c_sqlite3_column_text :: Ptr SQLite3Stmt -> CInt -> IO CString
+
+class SQLite3DataList a where
+	columnList :: SQLite3Stmt -> Int -> IO [a]
+
+class SQLite3Data a where
+	column :: SQLite3Stmt -> Int -> IO a
+
+instance SQLite3DataList a => SQLite3Data [a] where
+	column = columnList
+
+instance SQLite3Data Int where
+	column (SQLite3Stmt sm) i =
+		fromIntegral <$> c_sqlite3_column_int sm (fromIntegral i)
+
+instance SQLite3DataList Char where
+	columnList (SQLite3Stmt sm) i =
+		peekCString =<< c_sqlite3_column_text sm (fromIntegral i)
+
+instance SQLite3Data BS.ByteString where
+	column (SQLite3Stmt sm) i =
+		BS.packCString =<< c_sqlite3_column_text sm (fromIntegral i)
+
+instance SQLite3Data T.Text where
+	column sm i = T.decodeUtf8 <$> column sm i
--- features/ffi/sqlite3/haskell/SQLite3/Constants.hsc
+++ features/ffi/sqlite3/haskell/SQLite3/Constants.hsc
@@ -1,10 +1,15 @@
 #include <sqlite3.h>
 
 module SQLite3.Constants (
-	sQLITE_OK
+	sQLITE_OK, sQLITE_ERROR, sQLITE_BUSY,
+	sQLITE_ROW, sQLITE_DONE
 	) where
 
 import Foreign.C.Types
 
-sQLITE_OK :: CInt
+sQLITE_OK, sQLITE_ERROR, sQLITE_BUSY, sQLITE_ROW, sQLITE_DONE :: CInt
 sQLITE_OK = #const SQLITE_OK
+sQLITE_ERROR = #const SQLITE_ERROR
+sQLITE_BUSY = #const SQLITE_BUSY
+sQLITE_ROW = #const SQLITE_ROW
+sQLITE_DONE = #const SQLITE_DONE
