9cc50f0f1a513b5fff09fd7d17871408485a07ec
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-11-29 09:36:51 +0900

    make scheme: correct fib_fast.hs

--- themes/languages/make_scheme/haskell_codes/fib_fast.hs
+++ themes/languages/make_scheme/haskell_codes/fib_fast.hs
@@ -1,12 +1,14 @@
-tpq :: Integral n => n -> n -> Int -> (n, n) -> (n, n)
-tpq p q n
+tpq :: Integral n => n -> n -> (n, n) -> (n, n)
+tpq p q (a, b) = (p * a + q * b, q * a + (p + q) * b)
+
+tpqn :: Integral n => n -> n -> Int -> (n, n) -> (n, n)
+tpqn p q n
 	| n < 1 = id
-	| even n = tpq (p ^ 2 + q ^ 2) (2 * p * q + q ^ 2) (n `div` 2)
-	| otherwise = (\(a, b) -> (a * (p + q) + b * q, a * q + b * p))
-		. tpq p q (n - 1)
+	| even n = tpqn (p ^ 2 + q ^ 2) (2 * p * q + q ^ 2) (n `div` 2)
+	| otherwise = tpqn p q (n - 1) . tpq p q
 
 fastFib :: Integral n => Int -> n
-fastFib n = snd $ tpq 0 1 n (1, 0)
+fastFib n = fst $ tpqn 0 1 n (0, 1)
 
 fib :: Int -> Integer
 fib = (fibs !!) . fromIntegral
