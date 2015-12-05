d82a5f0d02a907069c9bbd1419f4d68a570709dd
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-12-06 08:09:23 +0900

    Nano Scheme: for Hubot

--- tribial/lecture/nano_scheme/nsc.hs
+++ tribial/lecture/nano_scheme/nsc.hs
@@ -6,7 +6,13 @@ import Environment
 import Maybe
 
 main :: IO ()
-main = interact $ maybe "err" (unlines . map showValue . fst) . (`scheme` env0)
+main = interact $ maybe "err" (unlines . map showValue . fst)
+	. (`scheme` env0) . rmvPrfx . head . lines
 
 scheme :: String -> Env -> Maybe ([Value], Env)
 scheme src e = (, e) `mapply` (parse `mbind` tokens src)
+
+rmvPrfx :: String -> String
+rmvPrfx ('n' : 's' : 'c' : s) = s
+rmvPrfx (_ : s) = rmvPrfx s
+rmvPrfx _ = ""
