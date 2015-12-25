63f82196dc31d8338921cd7e62c7a9feb2c86735
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-12-25 21:09:57 +0900

    refactoring module CRC of maybeState

--- packages/monad-control/state/maybeState/CRC.hs
+++ packages/monad-control/state/maybeState/CRC.hs
@@ -1,27 +1,21 @@
-{-# LANGUAGE TupleSections #-}
+module CRC (check, calc) where
 
-module CRC (check, crc) where
-
+import Control.Applicative ((<$>))
 import Control.Arrow (first, (&&&))
 import Data.Array (Array, (!), listArray)
-import Data.Bits (Bits, complement, xor, shiftR, testBit)
+import Data.Bits (complement, xor, shiftR, testBit)
 import Data.Bool (bool)
 import Data.Word (Word8, Word32)
 import qualified Data.ByteString as BS (ByteString, foldl')
 
 check :: BS.ByteString -> Bool
-check = (== 0x2144df1c) . crc
+check = (== 0x2144df1c) . calc
 
-crc :: BS.ByteString -> Word32
-crc = complement . BS.foldl' st 0xffffffff
-	where st n b = uncurry xor . first ((table !) . (`xor` b)) $ popByte n
-
-popByte :: (Integral a, Bits a) => a -> (Word8, a)
-popByte = fromIntegral &&& (`shiftR` 8)
+calc :: BS.ByteString -> Word32
+calc = complement . BS.foldl' (flip st) 0xffffffff
+	where st b = uncurry xor . first ((table !) . (xor b))
+		. (fromIntegral &&& (`shiftR` 8))
 
 table :: Array Word8 Word32
-table = listArray (0, 255) $ map crc8 [0 .. 255]
-	where
-	crc8 = (!! 8) . iterate crc1
-	crc1 = uncurry (bool id (`xor` 0xedb88320)) . popBit
-	popBit = (`testBit` 0) &&& (`shiftR` 1)
+table = listArray (0, 255) . (<$> [0 .. 255]) . ((!! 8) .) . iterate $
+	uncurry (bool id $ xor 0xedb88320) . ((`testBit` 0) &&& (`shiftR` 1))
--- packages/monad-control/state/maybeState/encode.hs
+++ packages/monad-control/state/maybeState/encode.hs
@@ -6,7 +6,7 @@ import Data.Bits (shiftR)
 import qualified Data.ByteString as BS
 
 import qualified YjChunks as Yc
-import CRC (crc)
+import CRC (calc)
 
 main :: IO ()
 main = BS.writeFile "hello.yjcs" sample
@@ -18,7 +18,7 @@ encode :: Yc.YjChunks -> BS.ByteString
 encode = (Yc.magic `BS.append`) . BS.concat . map encode1 . (++ [Yc.dend])
 
 encode1 :: Yc.YjChunk -> BS.ByteString
-encode1 = uncurry BS.append . first (le 4) . Yc.encode (le 4 . crc)
+encode1 = uncurry BS.append . first (le 4) . Yc.encode (le 4 . calc)
 	where
 	le 0 _ = ""
 	le c n = BS.cons (fromIntegral n) $ le (c - 1 :: Int) (n `shiftR` 8)
