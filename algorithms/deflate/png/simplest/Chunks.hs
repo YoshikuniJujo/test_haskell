eed137edfc8f7e250918bb37b6674a5f260222f0
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-12-28 14:38:32 +0900

    refactoring Chunk of no compressed PNG

--- algorithms/deflate/png/simplest/Chunks.hs
+++ algorithms/deflate/png/simplest/Chunks.hs
@@ -3,41 +3,28 @@
 module Chunks (Chunks, Chunk(..), frPng, toPng, idat) where
 
 import Control.Applicative
-import Control.Arrow
 import "monads-tf" Control.Monad.State
 import Data.Bool
 import qualified Data.ByteString as BS
 
 import CRC (check, calc)
+import Item
 import Bits
 
 magic :: BS.ByteString
 magic = "\x89PNG\r\n\SUB\n"
-
-frPng :: BS.ByteString -> Maybe [Chunk]
-frPng = evalStateT $ do
-	item 8 (bool Nothing (Just ()) . (== magic))
-	untilM (== Chunk "IEND" "") chunk
-
-toPng :: [Chunk] -> BS.ByteString
-toPng cs = (magic `BS.append`) . BS.concat . (<$> cs ++ [Chunk "IEND" ""]) $ \c ->
-	be (BS.length $ dat c)
-		`BS.append` typ c 
-		`BS.append` dat c
-		`BS.append` be (calc $ typ c `BS.append` dat c)
-	where
-	be :: (Bits a, Integral a) => a -> BS.ByteString
-	be = beToByteString 4
 
 idat :: [Chunk] -> BS.ByteString
 idat = BS.concat . map dat . filter ((== "IDAT") . typ)
 
 type Chunks = [Chunk]
 
-data Chunk = Chunk {
-	typ :: BS.ByteString,
-	dat :: BS.ByteString
-	} deriving (Show, Eq)
+data Chunk = Chunk { typ :: BS.ByteString, dat :: BS.ByteString }
+	deriving (Show, Eq)
+
+frPng :: BS.ByteString -> Maybe [Chunk]
+frPng = evalStateT $ item 8 (bool Nothing (Just ()) . (== magic)) >>
+	untilM (== Chunk "IEND" "") chunk
 
 chunk :: StateT BS.ByteString Maybe Chunk
 chunk = do
@@ -46,9 +33,8 @@ chunk = do
 	unless (check td cs) $ fail "error"
 	return . uncurry Chunk $ BS.splitAt 4 td
 
-untilM :: (Monad m, Functor m) => (a -> Bool) -> m a -> m [a]
-untilM p m = bool (return []) <$> ((<$> untilM p m) . (:)) <*> not . p =<< m
-
-item :: Int -> (BS.ByteString -> Maybe a) -> StateT BS.ByteString Maybe a
-item l f = gets (BS.splitAt l) >>=
-	uncurry (flip . maybe $ fail "error") . (f *** (. return) . (>>) . put)
+toPng :: [Chunk] -> BS.ByteString
+toPng cs = (magic `BS.append`) . BS.concat . (<$> cs ++ [Chunk "IEND" ""]) $
+	\c -> BS.concat [
+		beToByteString 4 . BS.length $ dat c, typ c, dat c,
+		beToByteString 4 . calc $ typ c `BS.append` dat c ]
--- algorithms/deflate/png/simplest/Item.hs
+++ algorithms/deflate/png/simplest/Item.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE OverloadedStrings, PackageImports #-}
 
-module Item (item, byte) where
+module Item (item, byte, untilM) where
 
+import Control.Applicative
 import Control.Arrow
 import "monads-tf" Control.Monad.State
+import Data.Bool
 import Data.Word
 import qualified Data.ByteString as BS
 
@@ -13,3 +15,6 @@ item l f = gets (BS.splitAt l) >>=
 
 byte :: BS.ByteString -> Maybe Word8
 byte bs = case BS.uncons bs of Just (w, "") -> Just w; _ -> Nothing
+
+untilM :: (Monad m, Functor m) => (a -> Bool) -> m a -> m [a]
+untilM p m = bool (return []) <$> ((<$> untilM p m) . (:)) <*> not . p =<< m
