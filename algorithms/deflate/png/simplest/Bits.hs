9a44b81d26d957bcc9ff3bc8bcd0781d52462085
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-12-28 15:15:55 +0900

    refactoring no compressed PNG

--- algorithms/deflate/png/simplest/Bits.hs
+++ algorithms/deflate/png/simplest/Bits.hs
@@ -2,35 +2,36 @@
 
 module Bits (
 	Bits, complement, (.&.), (.|.), xor, shiftL, shiftR, testBit,
+	popBit, popBits, popByte,
 	beToByteString, beFromByteString,
-	leToByteString,
-	popByte, popBits, popBit) where
+	leToByteString) where
 
-import Control.Arrow
-import Data.List
-import Data.Bits
-import Data.Bool
-import Data.Word
+import Control.Applicative ((<$>), (<*>))
+import Control.Arrow ((&&&), (***))
+import Data.Bits (Bits, complement, testBit, (.&.), (.|.), xor, shiftL, shiftR)
+import Data.Word (Word8)
 
-import qualified Data.ByteString as BS
+import qualified Data.ByteString as BS (ByteString, cons, foldl')
 
 beToByteString :: (Integral a, Bits a) => Int -> a -> BS.ByteString
-beToByteString c = ((BS.pack . reverse) .) . flip curry c . unfoldr $ \(i, n) ->
-	bool Nothing (Just . second (i - 1 ,) $ popByte n) (i > 0)
+beToByteString = btb "" where
+	btb s 0 _ = s
+	btb s c n = btb (BS.cons (fromIntegral n) s) (c - 1) (n `shiftR` 8)
 
 beFromByteString :: (Num a, Bits a) => BS.ByteString -> a
 beFromByteString =
 	BS.foldl' (curry $ uncurry (.|.) . ((`shiftL` 8) *** fromIntegral)) 0
 
 leToByteString :: (Integral a, Bits a) => Int -> a -> BS.ByteString
-leToByteString 0 _ = ""
-leToByteString c n = fromIntegral n `BS.cons` leToByteString (c - 1) (n `shiftR` 8)
+leToByteString 0 = const ""
+leToByteString c =
+	BS.cons <$> fromIntegral <*> leToByteString (c - 1) . (`shiftR` 8)
 
 popByte :: (Integral a, Bits a) => a -> (Word8, a)
 popByte = fromIntegral &&& (`shiftR` 8)
 
 popBits :: Bits n => n -> Int -> ([Bool], n)
-popBits n c = (map (n `testBit`) [0 .. c - 1], n `shiftR` c)
+popBits n c = (<$> [0 .. c - 1]) . testBit &&& (`shiftR` c) $ n
 
 popBit :: Bits a => a -> (Bool, a)
 popBit = (`testBit` 0) &&& (`shiftR` 1)
