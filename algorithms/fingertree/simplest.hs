1136f09f183c69143015bcc874398eb53a934e4e
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2016-01-06 17:24:25 +0900

    tribial

--- algorithms/fingertree/simplest.hs
+++ algorithms/fingertree/simplest.hs
@@ -1,18 +1,11 @@
 data FingerTree a
-	= Empty
-	| Single a
+	= Empty | Single a
 	| Deep (Digit a) (FingerTree (Node a)) (Digit a)
 	deriving Show
 
-data Digit a
-	= One a
-	| Two a a
-	deriving Show
+data Digit a = One a | Two a a deriving Show
 
-data Node a
-	= Node2 a a
-	| Node3 a a a
-	deriving Show
+data Node a = Node2 a a | Node3 a a a deriving Show
 
 (<|) :: a -> FingerTree a -> FingerTree a
 a <| Empty = Single a
