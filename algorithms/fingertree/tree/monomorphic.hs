9a8ac001f3812a09a1c2d65996d9501e52797754
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2016-01-07 12:50:24 +0900

    deleteMin for monomorphic Binary B-tree

--- algorithms/fingertree/tree/monomorphic.hs
+++ algorithms/fingertree/tree/monomorphic.hs
@@ -1,5 +1,7 @@
 module SetBM (Set, empty, member, insert) where
 
+-- import Data.Maybe
+
 data Set a = Zero a | Succ (Set (Node a)) deriving Show
 data Node a = Node2 a Int a | Node3 a Int a Int a deriving Show
 
@@ -9,10 +11,20 @@ empty = Zero ()
 class IsNode a where
 	memberN :: Int -> a -> Bool
 	insertN :: Int -> a -> Either a (a, Int, a)
+--	deleteN :: Int -> a -> Either a 
+--	popMinN :: Node a -> Maybe (Int, Either (Node a) a)
+--	maxN :: a -> Maybe Int
+	deleteMinN :: Node a -> Either (Node a) a
 
 instance IsNode () where
 	memberN _ _ = False
 	insertN a _ = Right ((), a, ())
+--	popMinN _ = Nothing
+	deleteMinN (Node2 _ _ _) = Right ()
+	deleteMinN (Node3 _ _ _ b _) = Left $ Node2 () b ()
+--	deleteN _ _ = ()
+--	minN _ = Nothing
+--	maxN _ = Nothing
 
 instance IsNode a => IsNode (Node a) where
 	memberN a (Node2 l b r)
@@ -48,6 +60,26 @@ instance IsNode a => IsNode (Node a) where
 			Left r' -> Left $ Node3 l b m c r'
 			Right (m', d, r') ->
 				Right (Node2 l b m, c, Node2 m' d r')
+	deleteMinN (Node2 l a r) = case deleteMinN l of
+		Left l' -> Left $ Node2 l' a r
+		Right l' -> case r of
+			Node2 m b r' -> Right $ Node3 l' a m b r'
+			Node3 m b m' c r' -> Left $
+				Node2 (Node2 l' a m) b (Node2 m' c r')
+	deleteMinN (Node3 l a m b r) = case deleteMinN l of
+		Left l' -> Left $ Node3 l' a m b r
+		Right l' -> case m of
+			Node2 m' c m'' -> Left $
+				Node2 (Node3 l' a m' c m'') b r
+			Node3 m' c m'' d m''' -> Left $
+				Node3 (Node2 l' a m') c (Node2 m'' d m''') b r
+--	popMin (Node2 l a r)
+--	deleteN a t@(Node2 l b r)
+--		| a < b = 
+--	minN (Node2 l a _) = Just . fromMaybe a $ minN l
+--	minN (Node3 l a _ _ _) = Just . fromMaybe a $ minN l
+--	maxN (Node2 _ a r) = Just . fromMaybe a $ maxN r
+--	maxN (Node3 _ _ _ b r) = Just . fromMaybe b $ maxN r
 
 member :: IsNode a => Int -> Set a -> Bool
 member a (Zero n) = memberN a n
@@ -58,3 +90,22 @@ insert a (Zero n) = case insertN a n of
 	Left n' -> Zero n'
 	Right (l, b, r) -> Succ . Zero $ Node2 l b r
 insert a (Succ t) = Succ $ insert a t
+
+deleteMin :: IsNode a => Set a -> Set a
+deleteMin (Succ (Zero n)) = case deleteMinN n of
+	Left n' -> Succ $ Zero n'
+	Right n' -> Zero n'
+deleteMin (Succ t) = Succ $ deleteMin t
+deleteMin t = t
+
+-- minT, maxT :: IsNode a => Set a -> Maybe Int
+-- minT (Zero n) = minN n
+-- minT (Succ t) = minT t
+
+-- maxT (Zero n) = maxN n
+-- maxT (Succ t) = maxT t
+
+{-
+checkBalance :: (Node (Node (Node ()))) -> Either (Node (Node (Node ()))) (Node (Node ()))
+checkBalance (Node2 l a r)
+-}
