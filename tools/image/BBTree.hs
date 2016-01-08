968e319db7821ae7bccf36b33200698c651388e7
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2016-01-08 18:49:12 +0900

    add Binary B-tree image

---
+++ tools/image/BBTree.hs
@@ -0,0 +1,97 @@
+{-# LANGUAGE TypeFamilies #-}
+
+module BBTree (
+	Tree, Tip,
+	BBTree.null, height1, decompose, value, children, sample) where
+
+data Tree v a = Zero a | Succ (Tree v (Node v a)) deriving Show
+data Node v a = Node2 a v a | Node3 a v a v a deriving Show
+data Tip v = Tip deriving Show
+
+null :: Tree v a -> Bool
+null (Zero _) = True
+null _ = False
+
+height1 :: Tree v a -> Bool
+height1 (Succ (Zero _)) = True
+height1 _ = False
+
+sample :: Tree Int (Tip Int)
+sample = Succ . Succ . Succ . Succ . Zero $ Node2
+	(Node2
+		(Node3
+			(Node2 Tip 0 Tip)
+			1
+			(Node3 Tip 2 Tip 3 Tip)
+			4
+			(Node2 Tip 5 Tip))
+		6
+		(Node2
+			(Node2 Tip 7 Tip)
+			8
+			(Node3 Tip 9 Tip 10 Tip)))
+	11
+	(Node3
+		(Node3
+			(Node2 Tip 12 Tip)
+			13
+			(Node3 Tip 14 Tip 15 Tip)
+			16
+			(Node2 Tip 17 Tip))
+		18
+		(Node2
+			(Node2 Tip 19 Tip)
+			20
+			(Node3 Tip 21 Tip 22 Tip))
+		23
+		(Node2
+			(Node2 Tip 24 Tip)
+			25
+			(Node2 Tip 26 Tip)))
+
+class IsNode a where
+	type Elem a
+	decomposeN :: Node (Elem a) a ->
+		Either (a, Elem a, a) (a, Elem a, a, Elem a, a)
+	valueN :: Node (Elem a) a -> Either (Elem a) (Elem a, Elem a)
+	childrenN :: Node (Elem a) a -> Either (a, a) (a, a, a)
+
+instance IsNode (Tip v) where
+	type Elem (Tip v) = v
+	decomposeN (Node2 _ d _) = Left (Tip, d, Tip)
+	decomposeN (Node3 _ c _ f _) = Right (Tip, c, Tip, f, Tip)
+	valueN (Node2 _ d _) = Left d
+	valueN (Node3 _ c _ f _) = Right (c, f)
+	childrenN (Node2 _ _ _) = Left (Tip, Tip)
+	childrenN (Node3 _ _ _ _ _) = Right (Tip, Tip, Tip)
+
+instance IsNode (Node v a) where
+	type Elem (Node v a) = v
+	decomposeN (Node2 l d r) = Left (l, d, r)
+	decomposeN (Node3 l c o f r) = Right (l, c, o, f, r)
+	valueN (Node2 _ d _) = Left d
+	valueN (Node3 _ c _ f _) = Right (c, f)
+	childrenN (Node2 l _ r) = Left (l, r)
+	childrenN (Node3 l _ o _ r) = Right (l, o, r)
+
+decompose :: (IsNode a, v ~Elem a) => Tree v a ->
+	Either (Tree v a, v, Tree v a) (Tree v a, v, Tree v a, v, Tree v a)
+decompose (Succ (Zero n)) = case decomposeN n of
+	Left (l, d, r) -> Left (Zero l, d, Zero r)
+	Right (l, c, o, f, r) -> Right (Zero l, c, Zero o, f, Zero r)
+decompose (Succ t) = case decompose t of
+	Left (l, d, r) -> Left (Succ l, d, Succ r)
+	Right (l, c, o, f, r) -> Right (Succ l, c, Succ o, f, Succ r)
+
+value :: (IsNode a, v ~ Elem a) => Tree v a -> Either v (v, v)
+value (Succ (Zero n)) = valueN n
+value (Succ t) = value t
+
+children :: (IsNode a, v ~ Elem a) =>
+	Tree v a -> Either (Tree v a, Tree v a) (Tree v a, Tree v a, Tree v a)
+children (Succ (Zero n)) = case childrenN n of
+	Left (l, r) -> Left (Zero l, Zero r)
+	Right (l, o, r) -> Right (Zero l, Zero o, Zero r)
+children (Succ t) = case children t of
+	Left (l, r) -> Left (Succ l, Succ r)
+	Right (l, o, r) -> Right (Succ l, Succ o, Succ r)
---
+++ tools/image/bbtree.hs
@@ -0,0 +1,88 @@
+import Control.Monad
+import Graphics.X11.Turtle
+import Text.XML.YJSVG hiding (topleft)
+
+import qualified BBTree as B
+
+main :: IO ()
+main = do
+	f <- openField
+	topleft f
+	onkeypress f (return . (/= 'q'))
+	t <- newTurtle f
+	penup t
+	goto t 230 20
+	pensize t 3
+
+	tree t B.sample 180 55 2
+
+	svg <- getSVG t
+	putStrLn $ showSVG 480 250 svg
+
+	waitField f
+
+tree :: Show a =>
+	Turtle -> B.Tree a (B.Tip a) -> Double -> Double -> Double -> IO ()
+tree t bbt w h rt
+	| B.null bbt = return ()
+	| otherwise = case B.decompose bbt of
+		Left (l, d, r) -> tree2 t l d r w h rt
+		Right (l, c, o, f, r) -> tree3 t l c o f r w h rt
+
+tree2 :: Show a => Turtle ->
+	B.Tree a (B.Tip a) -> a -> B.Tree a (B.Tip a) ->
+	Double -> Double -> Double -> IO ()
+tree2 t l d r w h rt = do
+	(x0, y0) <- position t
+	goto t (x0 - 7) (y0 - 3)
+	write t "KochiGothic" 15 $ show d
+
+	when (not $ B.null l) $ do
+		goto t (x0 - 10) (y0 + 4)
+		pendown t
+		goto t (x0 - w * 2 / 3) (y0 + h)
+		penup t
+		goto t (x0 - w * 2 / 3 - 3) (y0 + h + 20)
+		tree t l (w / rt) h (rt * 3 / 2)
+
+	when (not $ B.null r) $ do
+		goto t (x0 + 10) (y0 + 4)
+		pendown t
+		goto t (x0 + w * 2 / 3) (y0 + h)
+		penup t
+		goto t (x0 + w * 2 / 3 + 3) (y0 + h + 20)
+		tree t r (w / rt) h (rt * 3 / 2)
+
+tree3 :: Show a => Turtle ->
+	B.Tree a (B.Tip a) -> a -> B.Tree a (B.Tip a) -> a -> B.Tree a (B.Tip a) ->
+	Double -> Double -> Double -> IO ()
+tree3 t l c o f r w h rt = do
+	(x0, y0) <- position t
+	goto t (x0 - 14) (y0 - 3)
+	write t "KochiGothic" 15 $ show c
+	goto t (x0 + 3) (y0 - 3)
+	write t "KochiGothic" 15 $ show f
+
+	when (not $ B.null l) $ do
+		goto t (x0 - 20) (y0 + 4)
+		pendown t
+		goto t (x0 - w) (y0 + h)
+		penup t
+		goto t (x0 - w - 3) (y0 + h + 20)
+		tree t l (w / rt) h (rt * 3 / 2)
+
+	when (not $ B.null o) $ do
+		goto t x0 (y0 + 4)
+		pendown t
+		goto t x0 (y0 + h)
+		penup t
+		goto t x0 (y0 + h + 20)
+		tree t o (w / rt) h (rt * 3 / 2)
+
+	when (not $ B.null r) $ do
+		goto t (x0 + 20) (y0 + 4)
+		pendown t
+		goto t (x0 + w) (y0 + h)
+		penup t
+		goto t (x0 + w + 3) (y0 + h + 20)
+		tree t r (w / rt) h (rt * 3 / 2)
