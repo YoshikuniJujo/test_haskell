46fc42243f86d2aa19184fff2b9891c692149ec3
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2016-01-20 17:36:23 +0900

    add Data.Set to speed check code for trees

--- algorithms/fingertree/tree/nth.hs
+++ algorithms/fingertree/tree/nth.hs
@@ -1,11 +1,19 @@
 import Data.Time
 import System.Random
 
+import qualified Data.Set as S
 import qualified SetT as T
 import qualified SetB as B
 
 main :: IO ()
 main = do
+	time (nthS S.empty (randoms $ mkStdGen 8) 20000 :: Maybe Int)
+	time (nthS S.empty (randoms $ mkStdGen 8) 40000 :: Maybe Int)
+	time (nthS S.empty (randoms $ mkStdGen 8) 80000 :: Maybe Int)
+	time (nthS S.empty (randoms $ mkStdGen 8) 160000 :: Maybe Int)
+	time (nthS S.empty (randoms $ mkStdGen 8) 320000 :: Maybe Int)
+	time (nthS S.empty (randoms $ mkStdGen 8) 640000 :: Maybe Int)
+	putStrLn ""
 	time (nthT T.empty (randoms $ mkStdGen 8) 20000 :: Maybe Int)
 	time (nthT T.empty (randoms $ mkStdGen 8) 40000 :: Maybe Int)
 	time (nthT T.empty (randoms $ mkStdGen 8) 80000 :: Maybe Int)
@@ -26,6 +34,13 @@ time x = do
 	print x
 	t1 <- getCurrentTime
 	print $ t1 `diffUTCTime` t0
+
+nthS :: Ord a => S.Set a -> [a] -> Int -> Maybe a
+nthS s (x : xs) n
+	| x `S.member` s = nthS s xs n
+	| n < 1 = Just x
+	| otherwise = nthS (S.insert x s) xs (n - 1)
+nthS _ _ _ = Nothing
 
 nthT :: Ord a => T.Set a -> [a] -> Int -> Maybe a
 nthT s (x : xs) n
