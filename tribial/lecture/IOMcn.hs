30544484e4d9e2c51b8aef92f68ce69a6a2627f3
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2016-03-20 22:31:50 +0900

    sample codes to explain IO

---
+++ tribial/lecture/IOMcn.hs
@@ -0,0 +1,31 @@
+module IOMcn (
+	IOMcn, runIOMcn, (>>>), arr,
+	getLine, getInt, putLine, isEven) where
+
+import Prelude hiding (getLine)
+import qualified Prelude
+import Control.Applicative
+import Data.Time
+
+newtype IOMcn a b = IOMcn { getIOMcn :: a -> IO b }
+
+runIOMcn :: IOMcn () () -> IO ()
+runIOMcn m = getIOMcn m ()
+
+(>>>) :: IOMcn a b -> IOMcn b c -> IOMcn a c
+m1 >>> m2 = IOMcn $ \x -> getIOMcn m1 x >>= getIOMcn m2
+
+arr :: (a -> b) -> IOMcn a b
+arr f = IOMcn $ return . f
+
+getLine :: IOMcn () String
+getLine = IOMcn $ const Prelude.getLine
+
+getInt :: IOMcn () Int
+getInt = IOMcn . const $ fmap read Prelude.getLine
+
+putLine :: IOMcn String ()
+putLine = IOMcn putStrLn
+
+isEven :: IOMcn () Bool
+isEven = IOMcn . const $ even . floor . utctDayTime <$> getCurrentTime
