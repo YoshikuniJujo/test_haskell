0a53dcd7e6c515cf944a436175a6f485266fbd5d
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-05-31 11:21:24 +0900

    threepenny editor textarea: show file contents

--- packages/threepenny/testThreepenny/app/editorTextarea.hs
+++ packages/threepenny/testThreepenny/app/editorTextarea.hs
@@ -1,3 +1,4 @@
+import Control.Monad
 import Data.List
 import System.Directory
 import System.FilePath
@@ -15,24 +16,51 @@ setup window = do
 	ta <- UI.textarea #. "textarea"
 		# set style [
 			("width", "450px"), ("height", "400px") ]
-	getBody window #+ [element ta]
+	gub <- UI.button # set UI.text ".."
+	getBody window #+ [element ta, element gub]
 
 	(cde, cdh) <- liftIO newEvent
 	cdb <- stepper "/" cde
+	(ope, oph) <- liftIO newEvent
+	opd <- stepper False ope
 	showDirectory ta =<< currentValue cdb
 	b <- liftIO $ stepper (- 1, - 1) (UI.mousemove ta)
 	liftIO . register (UI.click ta) . const $ do
+		o <- currentValue opd
+		when (not o) $ clicked window ta cdb cdh b oph
+	liftIO . register (UI.click gub) . const $ do
+		cdh . goUp =<< currentValue cdb
+		runUI window . showDirectory ta =<< currentValue cdb
+		oph False
+	return ()
+
+clicked :: Window -> Element ->
+	Behavior FilePath -> (FilePath -> IO ()) ->
+	Behavior (Int, Int) -> (Bool -> IO ()) -> IO ()
+clicked window ta cdb cdh b oph = do
 		y <- currentValue b
 		i <- (`div` 69) . (* 4) . subtract 0 . snd <$> currentValue b
 		print y
 		print i
 		fps <- getDirCont =<< currentValue cdb
 		flip (maybe $ return ()) (fps `index` i) $ \fp -> do
-			cdh . (</> fp) =<< currentValue cdb
-			runUI window . showDirectory ta
-				=<< currentValue cdb
+			fpn <- (</> fp) <$> currentValue cdb
+			b <- doesDirectoryExist fpn
+			if b
+				then do	cdh fpn
+					runUI window . showDirectory ta
+						=<< currentValue cdb
+				else if takeExtension fp `elem` [".hs"]
+					then do	print $ "HS: " ++ fpn
+						cdh fpn
+						oph True
+						runUI window
+							$ (return ta #)
+							. set value
+							=<< liftIO (readFile fpn)
+						return ()
+					else return ()
 		return ()
-	return ()
 
 showDirectory :: UI.Element -> FilePath -> UI ()
 showDirectory ta fp = do
@@ -49,3 +77,9 @@ index (x : _) 0 = Just x
 index [] _ = Nothing
 index _ n | n < 0 = Nothing
 index (_ : xs) n = index xs (n - 1)
+
+goUp :: FilePath -> FilePath
+goUp fp = case splitDirectories fp of
+	[] -> fp
+	["/"] -> fp
+	ds -> joinPath $ init ds
