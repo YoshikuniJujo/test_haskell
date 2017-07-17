eb72d35c9bb91ccafd0179987eb2476c18923382
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-07-17 13:00:59 +0900

    refactoring bad ZipList monad

--- tribial/zipMonad.hs
+++ tribial/zipMonad.hs
@@ -12,35 +12,17 @@ safeTail :: [a] -> Maybe [a]
 safeTail [] = Nothing
 safeTail (_ : xs) = Just xs
 
-fun :: Int -> Int -> ZipList (Int, Int)
-fun x y = ZipList $ replicate (x * y) (x, y)
+fun :: Int -> ZipList Int
+fun n = ZipList $ replicate n n
 
-check :: ZipList (Int, Int)
+check :: ZipList Int
 check = do
 	x <- ZipList [3, 2, 1]
-	y <- ZipList [3, 1, 2]
-	z <- fun x y
-	return z
+	y <- (x *) <$> ZipList [2, 1, 3]
+	fun y
 
-check1 :: ZipList (Int, Int)
-check1 =
-	ZipList [3, 2, 1] >>= \x ->
-	ZipList [2, 1, 3] >>= \y ->
-	fun x y >>= \z ->
-	return z
+check1 :: ZipList Int
+check1 = ZipList [3, 2, 1] >>= \x -> (x *) <$> ZipList [2, 1, 3] >>= fun
 
-check2 :: ZipList (Int, Int)
-check2 =
-	(ZipList [3, 2, 1] >>= \x ->
-	ZipList [2, 1, 3] >>= \y ->
-	return (x, y)) >>= \(x, y) ->
-	fun x y >>= \z ->
-	return z
-
-check3 :: ZipList (Int, Int)
-check3 =
-	ZipList [3, 2, 1] >>= \x ->
-	ZipList [2, 1, 3] >>= \y ->
-	return (x, y) >>= \(x, y) ->
-	fun x y >>= \z ->
-	return z
+check2 :: ZipList Int
+check2 = (ZipList [3, 2, 1] >>= \x -> (x*) <$> ZipList [2, 1, 3]) >>= fun
