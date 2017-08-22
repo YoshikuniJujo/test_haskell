e276caa19dbd3005d22d2ec5c0b3c9599e91e7ef
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-08-22 12:10:12 +0900

    tar file analyzer: prepare some properties

--- tribial/analyzeTar.hs
+++ tribial/analyzeTar.hs
@@ -1,10 +1,17 @@
+{-# LANGUAGE LambdaCase #-}
 {-# OPTIONS_GHC -fno-warn-tabs #-}
 
+import Numeric
+import Control.Monad
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.State
+import Data.Bits
 import Data.Word
 import Data.Char
+import Data.Time
+import Data.Time.Clock.POSIX
 import System.IO
+import System.Posix
 import Foreign.Ptr
 import Foreign.Marshal
 import Foreign.Storable
@@ -17,21 +24,46 @@ type PtrIO = StateT (Ptr Word8) IO
 
 data Header = Header {
 	name :: FilePath,
-	mode :: BS.ByteString,
-	uid :: BS.ByteString,
-	gid :: BS.ByteString,
-	size :: BS.ByteString,
-	mtime :: BS.ByteString,
-	chksum :: BS.ByteString,
-	typeflag :: BS.ByteString,
+	mode :: FileMode,
+	uid :: UserID,
+	gid :: GroupID,
+	size :: FileOffset,
+	mtime :: EpochTime,
+	chksum :: Word32,
+	typeflag :: TypeFlag,
 	linkname :: FilePath,
 	magic :: BS.ByteString,
-	uname :: BS.ByteString,
-	gname :: BS.ByteString,
+	uname :: String,
+	gname :: String,
 	devmajor :: BS.ByteString,
 	devminor :: BS.ByteString,
 	prefix :: FilePath
 	} deriving Show
+
+data TypeFlag
+	= RegularFile
+	| Link
+	| SymLink
+	| CharDevice
+	| BlockDevice
+	| Directory
+	| Fifo
+	| ContiguousFile
+	| UnknownType Word8
+	deriving Show
+
+typeFlag :: Word8 -> TypeFlag
+typeFlag = \case
+	0 -> RegularFile
+	0x30 -> RegularFile
+	0x31 -> Link
+	0x32 -> SymLink
+	0x33 -> CharDevice
+	0x34 -> BlockDevice
+	0x35 -> Directory
+	0x36 -> Fifo
+	0x37 -> ContiguousFile
+	w -> UnknownType w
 
 instance Storable Header where
 	sizeOf _ = 512
@@ -43,24 +75,28 @@ peekHeader = do
 	p <- get
 	h <- Header
 		<$> (nullTermString <$> readByteString 100)
-		<*> readByteString 8
-		<*> readByteString 8
-		<*> readByteString 8
-		<*> readByteString 12
-		<*> readByteString 12
-		<*> readByteString 8
-		<*> readByteString 1
+		<*> (octal <$> readByteString 8)
+		<*> (octal <$> readByteString 8)
+		<*> (octal <$> readByteString 8)
+		<*> (octal <$> readByteString 12)
+		<*> (octal <$> readByteString 12)
+		<*> (octal <$> readByteString 8)
+		<*> (typeFlag . head . BS.unpack <$> readByteString 1)
 		<*> (nullTermString <$> readByteString 100)
 		<*> readByteString 8
-		<*> (BS.takeWhile (/= 0) <$> readByteString 32)
-		<*> (BS.takeWhile (/= 0) <$> readByteString 32)
+		<*> (nullTermString <$> readByteString 32)
+		<*> (nullTermString <$> readByteString 32)
 		<*> (BS.takeWhile (/= 0) <$> readByteString 8)
 		<*> (BS.takeWhile (/= 0) <$> readByteString 8)
 		<*> (nullTermString <$> readByteString 155)
 	lift $ do
 		pokeArray (p `plusPtr` 148) $ replicate 8 (32 :: Word8)
-		sumBytes 512 p >>= print
+		cs <- sumBytes 512 p
+		when (cs /= chksum h) $ error "not match checksum"
 	return h
+
+octal :: (Num a, Eq a) => BS.ByteString -> a
+octal = fst . head . readOct . BSC.unpack
 
 nullTermString :: BS.ByteString -> String
 nullTermString = BSC.unpack . BSC.takeWhile (/= '\0')
@@ -73,3 +109,13 @@ readByteString n_ = get >>= \p -> BS.takeWhile (/= 0)
 sumBytes :: Word16 -> Ptr a -> IO Word32
 sumBytes n p = sum . map fromIntegral
 	<$> peekArray (fromIntegral n) (castPtr p :: Ptr Word8)
+
+{-
+
+1. make directory
+2. set user and group from name
+	-> if no name then set user and group from id
+3. set mode
+4. set modification time
+
+-}
