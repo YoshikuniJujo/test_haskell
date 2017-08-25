5c1431e19d73f7b565c352859f39a4ec68a424d9
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-08-26 08:15:32 +0900

    kinoko takenoko: print results

--- tribial/kinoko.hs
+++ tribial/kinoko.hs
@@ -6,4 +6,5 @@ main = do
 	h <- connectTo "skami.iocikun.jp" $ PortNumber 4492
 	hPutStrLn h "Yoshio"
 	hPutStrLn h "kinoko"
+	hGetLine h >>= putStrLn
 	hClose h
--- tribial/takenoko.hs
+++ tribial/takenoko.hs
@@ -6,4 +6,5 @@ main = do
 	h <- connectTo "skami.iocikun.jp" $ PortNumber 4492
 	hPutStrLn h "Yoshio"
 	hPutStrLn h "takenoko"
+	hGetLine h >>= putStrLn
 	hClose h
