4818c0c4eb3caaafe33ee14e19c2f2d420bf60d8
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-09-02 15:50:15 +0900

    reading extensible effects: existRSCont.hs: add State

--- tribial/extensible-effect/existRSCont.hs
+++ tribial/extensible-effect/existRSCont.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE DeriveFunctor #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 import Control.Monad.Cont
@@ -7,10 +8,10 @@ import Data.Typeable
 
 data VE a
 	= Val a
-	| forall t . (Typeable t, Typeable a) => E (t (VE a))
+	| forall t . (Functor t, Typeable t, Typeable a) => E (t (VE a))
 	deriving Typeable
 
-class Typeable e => Effect e where
+class (Functor e, Typeable e) => Effect e where
 	toEffect :: Typeable a => e (VE a) -> VE a
 	fromEffect :: VE a -> Maybe (e (VE a))
 
@@ -20,7 +21,7 @@ class Typeable e => Effect e where
 		Val _ -> Nothing
 
 newtype Reader e v = Reader (e -> v)
-	deriving Typeable
+	deriving (Typeable, Functor)
 
 instance Typeable e => Effect (Reader e) where
 
@@ -30,13 +31,45 @@ ask = cont $ toEffect . Reader
 runReader :: Typeable e => Cont (VE a) a -> e -> VE a
 runReader m e = rloop (runCont m Val) e
 
+runReader2 :: Typeable e => Cont (VE a) a -> e -> Cont (VE a) a
+runReader2 m = cont . const . runReader m
+
 rloop :: Typeable e => VE a -> e -> VE a
 rloop m e = case m of
 	Val x -> Val x
-	E _u -> case fromEffect m of
+	E u -> case fromEffect m of
 		Just (Reader r) -> rloop (r e) e
-		Nothing -> undefined
+		Nothing -> E $ fmap (`rloop` e) u
 
 run :: VE a -> a
 run (Val x) = x
 run _ = undefined
+
+run2 :: Cont (VE a) a -> a
+run2 m = case runCont m Val of
+	Val x -> x
+	_ -> undefined
+
+data State s a = State (s -> s) (s -> a)
+	deriving (Typeable, Functor)
+
+instance Typeable s => Effect (State s) where
+
+modify :: (Typeable s, Typeable a) => (s -> s) -> Cont (VE a) s
+modify f = cont $ toEffect . State f
+
+get :: (Typeable s, Typeable a) => Cont (VE a) s
+get = cont $ toEffect . State id
+
+runState :: Typeable s => Cont (VE a) a -> s -> VE a
+runState m s = sloop (runCont m Val) s
+
+runState2 :: Typeable s => Cont (VE a) a -> s -> Cont (VE a) a
+runState2 m = cont . const . runState m
+
+sloop :: Typeable s => VE a -> s -> VE a
+sloop m s = case m of
+	Val x -> Val x
+	E u -> case fromEffect m of
+		Just (State f k) -> sloop (k s) (f s)
+		Nothing -> E $ fmap (`sloop` s) u
