d65997d48f6182657aa5a32713fa6d5e8a8919df
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-09-02 13:39:48 +0900

    reading extensible effect: fix: runReader

--- tribial/extensible-effect/contReader.hs
+++ tribial/extensible-effect/contReader.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
 import Control.Monad.Cont
 
 data VE e a = Val a | E (Reader e (VE e a))
@@ -8,8 +10,9 @@ ask :: Cont (VE e a) e
 ask = cont $ E . Reader
 
 runReader :: Cont (VE e a) a -> e -> a
-runReader m e = case runCont m Val of
+runReader m e = loop (runCont m Val) e
+
+loop :: VE e a -> e -> a
+loop m e = case m of
 	Val x -> x
-	E (Reader u) -> case u e of
-		Val y -> y
-		E (Reader v) -> undefined
+	E (Reader u) -> loop (u e) e
