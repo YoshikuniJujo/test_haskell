c7ea286fe607234cfce810f749b8a70833de2745
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-09-04 15:27:13 +0900

    reading extensible effects: trying forall w . Cont w a

--- tribial/extensible-effect/MyEff.hs
+++ tribial/extensible-effect/MyEff.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE LambdaCase, ExistentialQuantification #-}
+-- {-# LANGUAGE LambdaCase, ExistentialQuantification, RankNTypes #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module MyEff (
@@ -12,6 +13,8 @@ import Data.Void
 import TypeElem ((:>), Member, MemberU2)
 import Cast (cast1)
 
+-- type Eff r = forall w . Cont (VE w r)
+
 data VE r a
 	= V a
 	| forall t . (Functor t, Typeable t) => E (t (VE r a))
@@ -24,4 +27,5 @@ fromEffect :: (Functor t, Typeable t) => VE r a -> Maybe (t (VE r a))
 fromEffect = \case E e -> cast1 e; V _ -> Nothing
 
 run :: Cont (VE Void a) a -> a
+-- run :: Eff Void a -> a
 run m = case runCont m V of V x -> x; _ -> undefined
