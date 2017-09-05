9c96d33f19c8840d1f2855c3d1585e1064533745
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-09-05 10:01:40 +0900

    reading extensible effects: add Reader, Writer, State by Free

---
+++ tribial/extensible-effect/steps/free/rwsFree.hs
@@ -0,0 +1,76 @@
+{-# LANGUAGE LambdaCase, DeriveFunctor #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+import Control.Arrow
+import Data.Monoid
+
+data Free f a = Pure a | Free (f (Free f a))
+
+instance Functor f => Functor (Free f) where
+	fmap f = go where
+		go (Pure a) = Pure (f a)
+		go (Free fa) = Free (go <$> fa)
+
+instance Functor f => Applicative (Free f) where
+	pure = Pure
+	Pure a <*> Pure b = Pure $ a b
+	Pure a <*> Free mb = Free $ fmap a <$> mb
+	Free ma <*> b = Free $ (<*> b) <$> ma
+
+instance Functor f => Monad (Free f) where
+	return = pure
+	Pure a >>= f = f a
+	Free m >>= f = Free ((>>= f) <$> m)
+
+data Reader r w = Reader (r -> w) deriving Functor
+
+ask :: Free (Reader r) r
+ask = Free $ Reader Pure
+
+runReader :: Free (Reader r) a -> r -> a
+runReader f r = case f of
+	Pure x -> x
+	Free (Reader f') -> runReader (f' r) r
+
+data Writer wr w = Writer wr w deriving Functor
+
+tell :: wr -> Free (Writer wr) ()
+tell wr = Free $ Writer wr (Pure ())
+
+runWriter :: Monoid wr => Free (Writer wr) a -> (a, wr)
+runWriter f = case f of
+	Pure x -> (x, mempty)
+	Free (Writer wr f') -> second (wr <>) $ runWriter f'
+
+data State s w = State (s -> s) (s -> w) deriving Functor
+
+getModify :: (s -> s) -> Free (State s) s
+getModify f = Free $ State f Pure
+
+get :: Free (State s) s
+get = getModify id
+
+modify :: (s -> s) -> Free (State s) ()
+modify = (>> return ()) . getModify
+
+put :: s -> Free (State s) ()
+put = modify . const
+
+runState :: Free (State s) a -> s -> (a, s)
+runState f s = case f of
+	Pure x -> (x, s)
+	Free (State u f') -> runState (f' s) (u s)
+
+data RW r wr w = R (r -> w) | W wr w deriving Functor
+
+rwAsk :: Free (RW r wr) r
+rwAsk = Free $ R Pure
+
+rwTell :: wr -> Free (RW r wr) ()
+rwTell wr = Free $ W wr (Pure ())
+
+runRW :: Monoid wr => Free (RW r wr) a -> r -> (a, wr)
+runRW f r = case f of
+	Pure x -> (x, mempty)
+	Free (R f') -> runRW (f' r) r
+	Free (W wr f') -> second (wr <>) $ runRW f' r
