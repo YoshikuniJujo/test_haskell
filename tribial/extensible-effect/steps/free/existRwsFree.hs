3aa97207b5c65d7a72ff9ded5218f7a5b22405dc
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-09-05 11:07:55 +0900

    reading extensible effects: existential and free: add State

--- tribial/extensible-effect/steps/free/existRwsFree.hs
+++ tribial/extensible-effect/steps/free/existRwsFree.hs
@@ -25,17 +25,40 @@ ask = Free . U $ Reader Pure
 
 run :: Free Union a -> a
 run (Pure x) = x
+run _ = error "Oh! No!"
 
-runReader :: Typeable r => Free Union a -> r -> a
+runReader :: Typeable r => Free Union a -> r -> Free Union a
 runReader f r = case f of
-	Pure x -> x
-	Free u -> case fromUnion u of
-		Just (Reader f') -> runReader (f' r) r
-		Nothing -> undefined
-
-runReader2 :: Typeable r => Free Union a -> r -> Free Union a
-runReader2 f r = case f of
 	Pure x -> Pure x
 	Free u -> case fromUnion u of
-		Just (Reader f') -> runReader2 (f' r) r
-		Nothing -> Free $ fmap (`runReader2` r) u
+		Just (Reader f') -> runReader (f' r) r
+		Nothing -> Free $ fmap (`runReader` r) u
+
+data State s w = State (s -> s) (s -> w) deriving Functor
+
+getModify :: Typeable s => (s -> s) -> Free Union s
+getModify f = Free . U $ State f Pure
+
+get :: Typeable s => Free Union s
+get = getModify id
+
+modify :: Typeable s => (s -> s) -> Free Union ()
+modify = (>> return ()) . getModify
+
+put :: Typeable s => s -> Free Union ()
+put = modify . const
+
+runState :: Typeable s => Free Union a -> s -> Free Union (a, s)
+runState f s = case f of
+	Pure x -> Pure (x, s)
+	Free u -> case fromUnion u of
+		Just (State m f') -> runState (f' s) (m s)
+		Nothing -> Free $ fmap (`runState` s) u
+
+testRS :: Free Union (Char, Integer)
+testRS = do
+	r <- ask
+	modify (+ (15 :: Integer))
+	s <- get
+	modify (+ (123 :: Integer))
+	return (r, s)
