069aab85cb24e20153b5a737a14c239f2e5c5e3b
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-09-06 17:55:44 +0900

    freer effects: refactoring MyEff: en route

--- tribial/freer-effects/FTCQueue/MyEff.hs
+++ tribial/freer-effects/FTCQueue/MyEff.hs
@@ -1,6 +1,4 @@
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE DataKinds, TypeOperators #-}
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DataKinds #-}
 
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 {-# OPTIONS_GHC -fno-warn-simplifiable-class-constraints #-}
@@ -9,8 +7,8 @@ module MyEff (
 	Eff, Freer(..), Member,
 	run, runM, send, decomp, tsingleton, qApp, qComp) where
 
-import Freer
-import OpenUnion
+import Freer (Freer(..), tsingleton, qApp, qComp)
+import OpenUnion (Union, Member, inj, decomp, extract)
 
 type Eff effs = Freer (Union effs)
 
@@ -25,3 +23,7 @@ runM :: Monad m => Eff '[m] a -> m a
 runM (Pure x) = return x
 runM (Join u q) = case extract u of
 	mb -> mb >>= runM . (q `qApp`)
+
+-- あとですること
+-- * decomp, tsingleton, qApp, qCompの代わりに、
+-- 	もっと高レベルの関数を作って公開する
