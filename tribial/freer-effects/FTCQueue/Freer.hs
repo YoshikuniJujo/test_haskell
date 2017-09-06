1a602489f4adc58da47174379d8f3896b4942812
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-09-07 04:53:26 +0900

    freer effects: refactoring Freer

--- tribial/freer-effects/FTCQueue/Freer.hs
+++ tribial/freer-effects/FTCQueue/Freer.hs
@@ -11,25 +11,25 @@ data Freer t a = Pure a | forall x . Join (t x) (FTCQueue (Freer t) x a)
 
 instance Functor (Freer t) where
 	fmap f (Pure x) = Pure $ f x
-	fmap f (Join m q) = Join m $ q |> (Pure . f)
+	fmap f (Join tx q) = Join tx $ q |> (Pure . f)
 
 instance Applicative (Freer t) where
 	pure = Pure
 	Pure f <*> Pure x = Pure $ f x
-	Pure f <*> Join m q = Join m $ q |> (Pure . f)
-	Join m q <*> Pure x = Join m $ q |> (Pure . ($ x))
-	Join m q <*> n = Join m $ q |> (<$> n)
+	Pure f <*> Join tx q = Join tx $ q |> (Pure . f)
+	Join tx q <*> Pure x = Join tx $ q |> (Pure . ($ x))
+	Join tx q <*> m = Join tx $ q |> (<$> m)
 
 instance Monad (Freer t) where
 	Pure x >>= f = f x
-	Join m q >>= f = Join m $ q |> f
+	Join tx q >>= f = Join tx $ q |> f
 
 qApp :: FTCQueue (Freer t) a b -> a -> Freer t b
 q `qApp` x = case tviewl q of
 	TOne f -> f x
 	f :| r -> case f x of
 		Pure y -> r `qApp` y
-		Join m q' -> Join m (q' >< r)
+		Join tx q' -> Join tx (q' >< r)
 
 qComp :: FTCQueue (Freer t) a b -> (Freer t b -> c) -> a -> c
 qComp = flip (.) . qApp
