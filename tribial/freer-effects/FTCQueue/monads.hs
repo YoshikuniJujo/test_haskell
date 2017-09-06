41d9ea7fc7d615810ab7d39a2924eb4b27c952a0
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-09-06 15:11:24 +0900

    freer effects: FTCQueue: State

--- tribial/freer-effects/FTCQueue/monads.hs
+++ tribial/freer-effects/FTCQueue/monads.hs
@@ -65,3 +65,24 @@ runWriter = \case
 	Join u q -> case decomp u of
 		Right (Writer w) -> second (w <>) <$> runWriter (q `qApp` ())
 		Left u' -> Join u' . tsingleton $ q `qComp` runWriter
+
+data State s a where
+	Get :: State s s
+	Put :: !s -> State s ()
+
+get :: Member (State s) effs => Eff effs s
+get = send Get
+
+put :: Member (State s) effs => s -> Eff effs ()
+put s = send (Put s)
+
+modify :: Member (State s) effs => (s -> s) -> Eff effs ()
+modify f = fmap f get >>= put
+
+runState :: Eff (State s ': effs) a -> s -> Eff effs (a, s)
+runState m s = case m of
+	Pure x -> Pure (x, s)
+	Join u q -> case decomp u of
+		Right Get -> runState (q `qApp` s) s
+		Right (Put s') -> runState (q `qApp` ()) s'
+		Left u' -> Join u' . tsingleton $ q `qComp` (`runState` s)
