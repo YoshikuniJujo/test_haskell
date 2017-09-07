518d485ed86246f80794510e57cd60e45b034d79
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-09-07 11:59:32 +0900

    freer effects: refactoring MyEff

--- tribial/freer-effects/FTCQueue/MyEff.hs
+++ tribial/freer-effects/FTCQueue/MyEff.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE DataKinds, TypeOperators #-}
 
@@ -12,36 +13,32 @@ import OpenUnion (Union, Member, inj, decomp, extract)
 
 type Eff effs = Freer (Union effs)
 
-send :: Member eff effs => eff a -> Eff effs a
-send t = Join (inj t) (tsingleton Pure)
-
 run :: Eff '[] a -> a
 run (Pure x) = x
-run _ = error "bad"
+run _ = error "MyEff.run: expect Pure"
 
 runM :: Monad m => Eff '[m] a -> m a
 runM (Pure x) = return x
-runM (Join u q) = case extract u of
-	mb -> mb >>= runM . (q `qApp`)
+runM (Join u q) = runM . (q `qApp`) =<< extract u
 
-type Arr effs a b = a -> Eff effs b
+send :: Member eff effs => eff a -> Eff effs a
+send = (`Join` tsingleton Pure) . inj
 
 handleRelay ::
-	Arr effs a b ->
-	(forall v . eff v -> Arr effs v b -> Eff effs b) ->
+	(a -> Eff effs b) ->
+	(forall x . eff x -> (x -> Eff effs b) -> Eff effs b) ->
 	Eff (eff ': effs) a -> Eff effs b
 handleRelay ret h = handleRelayS () (const ret) h'
 	where h' () m k = h m (k ())
 
 handleRelayS ::
-	s -> (s -> Arr effs a b) ->
-	(forall v . s -> eff v -> (s ->  Arr effs v b) -> Eff effs b) ->
+	s -> (s -> a -> Eff effs b) ->
+	(forall x . s -> eff x -> (s -> x -> Eff effs b) -> Eff effs b) ->
 	Eff (eff ': effs) a -> Eff effs b
-handleRelayS s' ret h = loop s'
-	where
-	loop s (Pure x) = ret s x
-	loop s (Join u' q) = case decomp u' of
-		Right x -> h s x k
-		Left u -> Join u (tsingleton (k s))
-		where
-		k s'' x = loop s'' $ q `qApp` x
+handleRelayS s0 ret h = loop s0
+	where loop s = \case
+		Pure x -> ret s x
+		Join u q -> case decomp u of
+			Right tx -> h s tx k
+			Left u' -> Join u' . tsingleton $ k s
+			where k s' = loop s' . (q `qApp`)
