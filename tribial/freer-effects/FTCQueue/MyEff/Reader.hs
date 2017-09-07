95ca04b7017ee81e9c08e11f9670e2df98182c29
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-09-07 12:04:45 +0900

    freer effects: refactoring Reader

--- tribial/freer-effects/FTCQueue/MyEff/Reader.hs
+++ tribial/freer-effects/FTCQueue/MyEff/Reader.hs
@@ -1,21 +1,17 @@
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE DataKinds, TypeOperators #-}
+{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE FlexibleContexts #-}
 
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
-{-# OPTIONS_GHC -fno-warn-simplifiable-class-constraints #-}
 
-module MyEff.Reader where
+module MyEff.Reader (Reader, runReader, ask) where
 
-import MyEff
--- import Freer
--- import OpenUnion
+import MyEff (Eff, Member, send, handleRelayS)
 
-data Reader e a where
-	Reader :: Reader e e
+data Reader e a where Reader :: Reader e e
 
 ask :: Member (Reader e) effs => Eff effs e
 ask = send Reader
 
 runReader :: Eff (Reader e ': effs) a -> e -> Eff effs a
-runReader m0 e0 = handleRelayS e0 (const pure) (\e Reader k -> k e e) m0
+runReader m e0 = handleRelayS e0 (const pure) (\e Reader f -> f e e) m
