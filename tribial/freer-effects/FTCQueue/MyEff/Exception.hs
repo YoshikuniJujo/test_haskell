23a4f374edb41953ca614d9523843615dfd8ae45
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-09-08 12:16:11 +0900

    freer effects: refactoring Exception

--- tribial/freer-effects/FTCQueue/MyEff/Exception.hs
+++ tribial/freer-effects/FTCQueue/MyEff/Exception.hs
@@ -4,9 +4,9 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 {-# OPTIONS_GHC -fno-warn-simplifiable-class-constraints #-}
 
-module MyEff.Exception where
+module MyEff.Exception (Exc, runError, throwError, catchError) where
 
-import MyEff
+import MyEff (Eff, Member, send, handleRelay, interpose)
 
 newtype Exc e a = Exc e
 
@@ -14,8 +14,8 @@ throwError :: Member (Exc e) effs => e -> Eff effs a
 throwError = send . Exc
 
 runError :: Eff (Exc e ': effs) a -> Eff effs (Either e a)
-runError = handleRelay (pure . Right) (\(Exc e) _k -> pure (Left e))
+runError = handleRelay (pure . Right) $ \(Exc e) _ -> pure $ Left e
 
 catchError :: Member (Exc e) effs =>
 	Eff effs a -> (e -> Eff effs a) -> Eff effs a
-catchError m handle = interpose pure (\(Exc e) _k -> handle e) m
+m `catchError` handle = interpose pure (\(Exc e) _ -> handle e) m
