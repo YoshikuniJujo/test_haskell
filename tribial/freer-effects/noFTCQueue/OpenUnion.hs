97e39ba6b59f50740833e0445b975e74a8dd49c3
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-09-08 19:18:09 +0900

    freer effects: no FTCQueue: refactoring: OpenUnion

--- tribial/freer-effects/noFTCQueue/OpenUnion.hs
+++ tribial/freer-effects/noFTCQueue/OpenUnion.hs
@@ -1,52 +1,42 @@
 {-# LANGUAGE ExistentialQuantification, ScopedTypeVariables #-}
 {-# LANGUAGE KindSignatures, DataKinds, TypeOperators #-}
 {-# LANGUAGE
-	MultiParamTypeClasses, FlexibleInstances,
-	UndecidableInstances #-}
+	MultiParamTypeClasses,
+	FlexibleInstances, UndecidableInstances #-}
 
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module OpenUnion where
+module OpenUnion (Union, Member, inj, prj, decomp, extract) where
 
-import Unsafe.Coerce
+import Unsafe.Coerce (unsafeCoerce)
 
-data Union (r :: [* -> *]) a = forall t . Union !Word (t a)
+data Union (ts :: [* -> *]) a = forall t . Union !Word (t a)
 
-{-
-data Union (r :: [* -> *]) a where
-	Union :: !Word -> t a -> Union r a
--}
+inj :: forall t ts a . Member t ts => t a -> Union ts a
+inj = unsafeInj $ unP (elemNo :: P t ts)
 
-unsafeInj :: Word -> t a -> Union r a
+prj :: forall t ts a . Member t ts => Union ts a -> Maybe (t a)
+prj = unsafePrj $ unP (elemNo :: P t ts)
+
+newtype P (t :: * -> *) (ts :: [* -> *]) = P { unP :: Word }
+
+class Member (t :: * -> *) (ts :: [* -> *]) where elemNo :: P t ts
+instance Member t (t ': ts) where
+	elemNo = P 0
+instance {-# OVERLAPPABLE #-} Member t ts => Member t (_t' ': ts) where
+	elemNo = P $ 1 + unP (elemNo :: P t ts)
+
+unsafeInj :: Word -> t a -> Union ts a
 unsafeInj = Union
 
-unsafePrj :: Word -> Union r a -> Maybe (t a)
-unsafePrj n (Union n' x)
-	| n == n' = Just (unsafeCoerce x)
+unsafePrj :: Word -> Union ts a -> Maybe (t a)
+unsafePrj i (Union j x)
+	| i == j = Just $ unsafeCoerce x
 	| otherwise = Nothing
 
-newtype P (t :: * -> *) (r :: [* -> *]) = P { unP :: Word }
-
-class FindElem (t :: * -> *) (r :: [* -> *]) where
-	elemNo :: P t r
-
-instance FindElem t (t ': r) where
-	elemNo = P 0
-
-instance {-# OVERLAPPABLE #-} FindElem t r => FindElem t (t' ': r) where
-	elemNo = P $ 1 + unP (elemNo :: P t r)
-
-class FindElem t r => Member (t :: * -> *) r where
-	inj :: t a -> Union r a
-	prj :: Union r a -> Maybe (t a)
-
-instance FindElem t r => Member t r where
-	inj = unsafeInj $ unP (elemNo :: P t r)
-	prj = unsafePrj $ unP (elemNo :: P t r)
-
-decomp :: Union (t ': r) a -> Either (Union r a) (t a)
-decomp (Union 0 a) = Right $ unsafeCoerce a
-decomp (Union n a) = Left $ Union (n - 1) a
+decomp :: Union (t ': ts) a -> Either (Union ts a) (t a)
+decomp (Union 0 tx) = Right $ unsafeCoerce tx
+decomp (Union i tx) = Left $ Union (i - 1) tx
 
 extract :: Union '[t] a -> t a
-extract (Union _ a) = unsafeCoerce a
+extract (Union _ tx) = unsafeCoerce tx
