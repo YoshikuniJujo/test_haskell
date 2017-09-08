989a69f6fcb5538c2825f495f78a42d2849af78f
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-09-08 19:26:27 +0900

    freer effects: no FCTQueue: refactoring: Reader

--- tribial/freer-effects/noFTCQueue/MyEff/Reader.hs
+++ tribial/freer-effects/noFTCQueue/MyEff/Reader.hs
@@ -6,10 +6,9 @@
 
 module MyEff.Reader where
 
-import MyEff
+import MyEff (Eff, Member, send, Freer(..), decomp)
 
-data Reader e a where
-	Reader :: Reader e e
+data Reader e a where Reader :: Reader e e
 
 ask :: Member (Reader e) effs => Eff effs e
 ask = send Reader
@@ -17,6 +16,6 @@ ask = send Reader
 runReader :: Eff (Reader e ': effs) a -> e -> Eff effs a
 runReader m e = case m of
 	Pure x -> Pure x
-	Join u q -> case decomp u of
-		Right Reader -> runReader (q e) e
-		Left u' -> Join u' $ (`runReader` e) . q
+	Join u k -> case decomp u of
+		Right Reader -> runReader (k e) e
+		Left u' -> Join u' $ (`runReader` e) . k
