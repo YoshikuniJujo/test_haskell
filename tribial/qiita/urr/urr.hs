318695b8c33e602733f9b77ace87d36436bb6a71
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2019-01-09 13:19:19 +0900

    Hamada's URR for Qiita: add Integer examples

--- tribial/qiita/urr/urr.hs
+++ tribial/qiita/urr/urr.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 import Data.Word
@@ -5,8 +6,10 @@ import Data.Word
 data Bit = O | I deriving Show
 
 fromString :: String -> [Bit]
-fromString = map fc
-	where fc '0' = O; fc '1' = I; fc _ = error "Oops!"
+fromString = map fc where fc '0' = O; fc '1' = I; fc _ = error "Oops!"
+
+toString :: [Bit] -> String
+toString = map $ \case O -> '0'; I -> '1'
 
 toValue :: (r -> r -> r) -> (r -> a) -> r -> r -> [Bit] -> a
 toValue _ gv mn _ [] = gv mn
@@ -30,3 +33,43 @@ fromWord8 = fromValue Nothing
 	(\a b -> a + 1 >= b)
 	(\a b -> (a + b) `div` 2)
 	(\w s -> fromIntegral w < s)
+
+positiveIntegerSplitter :: Maybe Integer -> Maybe Integer -> Maybe Integer
+positiveIntegerSplitter (Just mn) Nothing = Just $ mn * 2
+positiveIntegerSplitter (Just mn) (Just mx) = Just $ (mn + mx) `div` 2
+positiveIntegerSplitter _ _ = error "Oops!"
+
+toPositiveInteger :: [Bit] -> Integer
+toPositiveInteger = toValue
+	positiveIntegerSplitter
+	(\case	Just mn -> mn; _ -> error "Oops!")
+	(Just 1) Nothing
+
+fromPositiveInteger :: Integer -> [Bit]
+fromPositiveInteger = fromValue Nothing
+	(\a b -> case (a, b) of
+		(Just mn, Just mx) -> mn + 1 >= mx
+		_ -> False)
+	positiveIntegerSplitter
+	(\n -> maybe True (n <))
+	(Just 1) Nothing
+
+integerSplitter :: Maybe Integer -> Maybe Integer -> Maybe Integer
+integerSplitter Nothing Nothing = Just 0
+integerSplitter Nothing (Just 0) = Just $ - 1
+integerSplitter (Just 0) Nothing = Just 1
+integerSplitter Nothing (Just mx) = Just $ mx * 2
+integerSplitter (Just mn) Nothing = Just $ mn * 2
+integerSplitter (Just mn) (Just mx) = Just $ (mn + mx) `div` 2
+
+toWholeInteger :: [Bit] -> Maybe Integer
+toWholeInteger = toValue integerSplitter id Nothing Nothing
+
+fromWholeInteger :: Integer -> [Bit]
+fromWholeInteger = fromValue Nothing
+	(\a b -> case (a, b) of
+		(Just mn, Just mx) -> mn + 1 >= mx
+		_ -> False)
+	integerSplitter
+	(\n -> maybe (error "Oops!") (n <))
+	Nothing Nothing
