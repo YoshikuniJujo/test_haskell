2cf068c83ed42135b82487bc7610becfb3a30988
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2019-01-21 14:04:23 +0900

    circuit: PLA example

--- tribial/circuit/src/TestCircuit.hs
+++ tribial/circuit/src/TestCircuit.hs
@@ -4,6 +4,7 @@
 module TestCircuit where
 
 import Control.Arrow
+import Control.Monad
 
 import Circuit
 
@@ -101,3 +102,55 @@ peekDecoder (i1, i2, i3, o0, o1, o2, o3, o4, o5, o6, o7) cct = (
 	peekOWire o0 cct, peekOWire o1 cct, peekOWire o2 cct,
 	peekOWire o3 cct, peekOWire o4 cct, peekOWire o5 cct,
 	peekOWire o6 cct, peekOWire o7 cct )
+
+multiOrGate :: Int -> CircuitBuilder ([IWire], OWire)
+multiOrGate n | n < 1 = error "Oops!"
+multiOrGate 1 = first (: []) <$> idGate
+multiOrGate 2 = (\(i1, i2, o) -> ([i1, i2], o)) <$> orGate
+multiOrGate n = do
+	(is1, o1) <- multiOrGate (n `div` 2)
+	(is2, o2) <- multiOrGate (n - n `div` 2)
+	(i1, i2, o) <- orGate
+	connectWire o1 i1
+	connectWire o2 i2
+	return (is1 ++ is2, o)
+
+patterns :: a -> a -> Int -> [[a]]
+patterns _ _ n | n < 1 = [[]]
+patterns a b n = [x : xs | x <- [a, b], xs <- patterns a b (n - 1)]
+
+all8AndGate :: OWire -> OWire -> OWire -> CircuitBuilder [OWire]
+all8AndGate o1 o2 o3 = do
+	(is, os) <- unzip . map (\(a, b, c, d) -> ([a, b, c], d))
+		<$> replicateM 8 and3Gate
+	sequence_ . concat $ zipWith
+		(flip (zipWith3 id) [o1, o2, o3]) (patterns invert obvert 3) is
+	return os
+
+test8AndGate :: CircuitBuilder (IWire, IWire, IWire, [OWire])
+test8AndGate = do
+	((i1, o1), (i2, o2), (i3, o3)) <- (,,) <$> idGate <*> idGate <*> idGate
+	os <- all8AndGate o1 o2 o3
+	return (i1, i2, i3, os)
+
+pla :: CircuitBuilder (IWire, IWire, IWire, OWire, OWire, OWire)
+pla = do
+	((a, oa), (b, ob), (c, oc)) <- (,,) <$> idGate <*> idGate <*> idGate
+	dcs <- all8AndGate oa ob oc
+	(ids, d) <- multiOrGate 7
+	(ies, e) <- multiOrGate 3
+	(if1, f) <- idGate
+	zipWithM_ connectWire (tail dcs) ids
+	zipWithM_ connectWire (map (dcs !!) [3, 5, 6]) ies
+	connectWire (last dcs) if1
+	return (a, b, c, d, e, f)
+
+peekPla :: (IWire, IWire, IWire, OWire, OWire, OWire) -> Circuit ->
+	(Bit, Bit, Bit, Bit, Bit, Bit)
+peekPla (a, b, c, d, e, f) cct = (\[x, y, z, u, v, w] -> (x, y, z, u, v, w))
+	$ map (`peekIWire` cct) [a, b, c] ++ map (`peekOWire` cct) [d, e, f]
+
+setPla :: (IWire, IWire, IWire, OWire, OWire, OWire) ->
+	(Bit, Bit, Bit) -> Circuit -> Circuit
+setPla (a, b, c, _, _, _) (ba, bb, bc) cct =
+	setBit a ba . setBit b bb $ setBit c bc cct
