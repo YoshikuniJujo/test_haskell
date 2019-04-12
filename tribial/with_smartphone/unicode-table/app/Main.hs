00833b49b709d343b442af8433da02c346155591
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2019-04-13 06:04:07 +0900

    unicode table

--- tribial/with_smartphone/unicode-table/app/Main.hs
+++ tribial/with_smartphone/unicode-table/app/Main.hs
@@ -4,10 +4,14 @@ module Main where
 
 import Control.Arrow
 import Data.Char
+import System.Environment
 import Numeric
 
 main :: IO ()
-main = putStrLn "Slozsoft"
+main = do
+	n_ : _ <- getArgs
+	let	n = read n_
+	mapM_ (putStrLn . unwords) . sepWords n $ take 30 tableLines
 
 tableLines :: [String]
 tableLines = map (\(i, c) -> ($ "") $ showHex i . ((" " ++ [c]) ++)) table
@@ -15,8 +19,11 @@ tableLines = map (\(i, c) -> ($ "") $ showHex i . ((" " ++ [c]) ++)) table
 table :: [(Int, Char)]
 table = map (id &&& chr) [0x3041 ..]
 
+sepWords :: Int -> [String] -> [[String]]
+sepWords n = sepBy (\c s -> if c + length s > n then Nothing else Just $ c + length s + 1) 0 0
+
 sepBy :: (a -> b -> Maybe a) -> a -> a -> [b] -> [[b]]
-sepBy f i s (x : xs) = case f s x of
-	Just s' -> let ys : yss = sepBy f i s' ys in (x : ys) : yss
-	Nothing -> [x] : sepBy f i i xs
-sepBy _ _ _ _ = []
+sepBy f i s xa@(x : xs) = case f s x of
+	Just s' -> let ys : yss = sepBy f i s' xs in (x : ys) : yss
+	Nothing -> [] : sepBy f i i xa
+sepBy _ _ _ _ = [[]]
