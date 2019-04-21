a51d919e3de0b5dec69c9e8ba9fb0fae0ee98523
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2019-04-21 16:24:59 +0900

    validate client cert

--- packages/servant/helloworld-client-auth/app/Main.hs
+++ packages/servant/helloworld-client-auth/app/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DataKinds, TypeApplications #-}
+{-# LANGUAGE OverloadedStrings, DataKinds, TypeApplications #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Main where
@@ -6,6 +6,7 @@ module Main where
 import Prelude hiding (head)
 import Control.Monad.IO.Class
 import Data.Default.Class
+import Data.X509.Validation
 import System.Environment
 import System.FilePath
 import Network.TLS
@@ -14,6 +15,8 @@ import Network.Wai.Handler.WarpTLS
 import Text.Blaze.Html5 hiding (main)
 import Servant
 import Servant.HTML.Blaze
+
+import Lib
 
 curubaCertFile, curubaIntFile, curubaKeyFile :: String -> String -> FilePath
 curubaCertFile d n = d </> n <.> "cert"
@@ -24,8 +27,8 @@ data HelloWorld = HelloWorld deriving Show
 
 instance ToMarkup HelloWorld where
 	toMarkup _ = docTypeHtml $ do
-		head . title $ toHtml "Say Hello"
-		body $ toHtml "Hello, world! Oh, no!"
+		head . title $ toHtml ("Say Hello" :: String)
+		body $ toHtml ("Hello, world! Oh, no!" :: String)
 
 type Api = Get '[HTML] HelloWorld
 
@@ -34,6 +37,7 @@ server = return HelloWorld
 
 main :: IO ()
 main = do
+	cs <- getMyCertStore
 	crtd : crtn : _ <- getArgs
 	let	ccf = curubaCertFile crtd crtn
 		cif = curubaIntFile crtd crtn
@@ -43,6 +47,8 @@ main = do
 			tlsServerHooks = def {
 				onClientCertificate = \cc -> do
 					print cc
+					print =<< validateDefault cs def
+						("yoshio", "") cc
 					return CertificateUsageAccept } } )
 		(setPort 8080 defaultSettings)
 		$ serve @Api Proxy server
--- packages/servant/helloworld-client-auth/src/Lib.hs
+++ packages/servant/helloworld-client-auth/src/Lib.hs
@@ -1,6 +1,11 @@
-module Lib
-    ( someFunc
-    ) where
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib (getMyCertStore) where
+
+import Data.Maybe
+import Data.X509.CertificateStore
+import Paths_helloworld_client_auth
+
+getMyCertStore :: IO CertificateStore
+getMyCertStore =
+	(fromJust <$>) $ readCertificateStore =<< getDataFileName "cacert.pem"
