a382899e94a768f77460162f6c037fec3176f966
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2019-05-02 10:30:26 +0900

    HRR: use defineTableDefault

--- packages/hrr/try-hrr-sqlite/src/Greeting.hs
+++ packages/hrr/try-hrr-sqlite/src/Greeting.hs
@@ -1,9 +1,22 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE DataKinds, DeriveGeneric #-}
 {-# LANGUAGE MultiParamTypeClasses, FlexibleInstances #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Greeting where
 
-import Database.Record.TH.SQLite3 (defineTable)
+-- import Database.Record.TH.SQLite3 (defineTable)
 
-$(defineTable "test.sqlite3" "greeting")
+import Database.HDBC.Record
+import Database.HDBC.Sqlite3
+import Database.Relational
+
+import Lib
+
+-- $(defineTable "test.sqlite3" "greeting")
+greeting2
+
+selectAll :: IO [Greeting]
+selectAll = do
+	conn <- connectSqlite3 "test.sqlite3"
+	runQuery' conn (relationalQuery Greeting.greeting) ()
--- packages/hrr/try-hrr-sqlite/src/Lib.hs
+++ packages/hrr/try-hrr-sqlite/src/Lib.hs
@@ -4,12 +4,15 @@
 module Lib where
 
 import GHC.Generics
+import Data.Maybe
 import Language.Haskell.TH
+import Database.HDBC.Session
 import Database.HDBC.Query.TH
 import Database.HDBC.Sqlite3
 import Database.HDBC.Schema.SQLite3
-
 import Database.HDBC.Schema.Driver
+
+import qualified Data.Map as Map
 
 greeting :: Q [Dec]
 greeting = defineTableFromDB
@@ -21,6 +24,27 @@ greeting = defineTableFromDB
 
 greeting2 :: Q [Dec]
 greeting2 = do
-	let	config = driverConfig (driverSQLite3 :: Driver Connection)
-	defineTableDefault config undefined undefined undefined undefined
-		undefined undefined
+	let	config = driverConfig drv
+	(((cols, notNullIdxs), primaryCols), _logs) <- runIO getDbInfo
+	let	cols1 = [ (,) cn . maybe ty (liftMaybe ty) . Map.lookup cn $ Map.fromList [] | (cn, ty) <- cols ]
+		colIxMap = Map.fromList $ zip [c | (c, _) <- cols] [(0 :: Int) .. ]
+		ixLookups = [ (k, Map.lookup k colIxMap) | k <- primaryCols ]
+		primaryIxs = fromMaybe [] . sequence $ map snd ixLookups
+	defineTableDefault config scm tbl cols1 [''Show, ''Generic]
+		primaryIxs (listToMaybe notNullIdxs)
+	where
+	drv = driverSQLite3 :: Driver Connection
+	scm = "main"
+	tbl = "greeting"
+	conn = connectSqlite3 "test.sqlite3"
+	getDbInfo = do
+		logChan <- emptyLogChan
+		infoP <- withConnectionIO conn $ \c -> (,)
+			<$> getFields drv c logChan scm tbl
+			<*> getPrimaryKey drv c logChan scm tbl
+		(,) infoP <$> takeLogs logChan
+	liftMaybe tyQ sty = do
+		ty <- tyQ
+		case ty of
+			(AppT (ConT n) _) | n == ''Maybe -> [t| Maybe $(sty) |]
+			_ -> sty
