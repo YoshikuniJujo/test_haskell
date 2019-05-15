2f0bd0195f1c5a3941885c1688e49dffe3f322ac
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2019-05-15 14:35:42 +0900

    mandelbrot: refactoring

--- tribial/qiita/parallel-mandelbrot/parallel-mandelbrot-original/app/Main.hs
+++ tribial/qiita/parallel-mandelbrot/parallel-mandelbrot-original/app/Main.hs
@@ -2,47 +2,34 @@
 
 module Main where
 
-import Data.Word
-import Data.Complex
-import Codec.Picture
+import Data.Word (Word8)
+import Data.Complex (Complex(..), magnitude)
+import Codec.Picture (Pixel8, writePng, generateImage)
 
 trial :: Word8
 trial = 255
 
 black, white :: Pixel8
-black = 0
-white = 255
+black = 0; white = 255
 
 width, height :: Int
-width = 1024
-height = 1024
--- width = 256
--- height = 256
+width = 1024; height = 1024
 
 lefttop, rightbottom :: Complex Double
--- lefttop = (- 1.20) :+ 0.35
--- rightbottom = (- 1.0) :+ 0.15
--- lefttop = (- 0.875) :+ 0.25
--- rightbottom = (- 0.625) :+ 0
-lefttop = (- 1.5) :+ 1
-rightbottom = 0.5 :+ (- 1)
+lefttop = (- 1.20) :+ 0.40; rightbottom = (- 1.00) :+ 0.20
 
 main :: IO ()
 main = writePng "mandelbrot.png"
-	$ generateImage (((toPixel . result) .) . toComplex) width height
-	
+	$ generateImage (((toPixel . try) .) . toComplex) width height
 
-result :: Complex Double -> Maybe Word8
-result c = case dropWhile (check . snd)
-		$ zip [0 .. trial - 1] (tail $ iterate (step c) 0) of
+try :: Complex Double -> Maybe Word8
+try c = case dropWhile (check . snd)
+		$ zip [0 .. trial - 1] (tail $ iterate step 0) of
 	[] -> Nothing
 	(i, _) : _ -> Just i
-
-check :: Complex Double -> Bool
-check z = magnitude z <= 2
-
-step :: Complex Double -> Complex Double -> Complex Double
-step c z = z * z + c
+	where
+	check z = magnitude z <= 2
+	step z = z * z + c
 
 toPixel :: Maybe Word8 -> Pixel8
 toPixel Nothing = black
