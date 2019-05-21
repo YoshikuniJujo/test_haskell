f867deaa52c1f15679db26aa97108870cbb9eb6b
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-05-21 17:06:59 +0900

    circuit: ALU RISC-V: try compiled

--- tribial/circuit/app/Main.hs
+++ tribial/circuit/app/Main.hs
@@ -7,6 +7,6 @@ import TestCircuit
 
 main :: IO ()
 main = let
-	(ws, cct) = makeCircuit $ alu_s 64
-	cct0 = setAlu_s ws I 2 I 10 3 cct in
-	print . peekAlu_s ws . (!! 700) $ iterate step cct0
+	(ws, cct) = makeCircuit $ alu_riscv 64
+	cct0 = setAlu_riscv ws O I 2 10 3 cct in
+	print . peekAlu_riscv ws . (!! 700) $ iterate step cct0
