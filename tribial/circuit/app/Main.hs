8bec595b1e7fe126e80f8a09cfb4a215ea94869b
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-05-21 13:44:36 +0900

    circuit: alu_s: compiled

--- tribial/circuit/app/Main.hs
+++ tribial/circuit/app/Main.hs
@@ -1,4 +1,12 @@
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
 module Main where
 
+import Circuit
+import TestCircuit
+
 main :: IO ()
-main = putStrLn "Slozsoft"
+main = let
+	(ws, cct) = makeCircuit $ alu_s 64
+	cct0 = setAlu_s ws I 2 I 10 3 cct in
+	print . peekAlu_s ws . (!! 700) $ iterate step cct0
