ba5ea7904469316e811c8d2e1e5f1cdf760a9dcb
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-05-22 09:03:27 +0900

    circuit: try compiled

--- tribial/circuit/app/Main.hs
+++ tribial/circuit/app/Main.hs
@@ -7,6 +7,6 @@ import TestCircuit
 
 main :: IO ()
 main = let
-	(ws, cct) = makeCircuit $ alu_riscv 64
-	cct0 = setAlu_riscv ws O I 2 10 3 cct in
-	print . peekAlu_riscv ws . (!! 700) $ iterate step cct0
+	(ws, cct) = makeCircuit $ riscv_alu 64
+	cct0 = setRiscvAlu ws 6 10 3 cct in
+	print . peekRiscvAlu ws . (!! 700) $ iterate step cct0
