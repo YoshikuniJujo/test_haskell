b12e33bc934a979f433c1d963141d462318814ee
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-05-24 16:56:16 +0900

    circuit: clock: edge

--- tribial/circuit/src/Clock.hs
+++ tribial/circuit/src/Clock.hs
@@ -14,3 +14,15 @@ clock n = do
 	(dli, dlo) <- delay $ n - 2
 	zipWithM_ connectWire [no, dlo] [dli, ni]
 	return no
+
+edge :: Word8 -> Word8 -> CircuitBuilder OWire
+edge _ n | n < 3 = error "clock: span should be more than 2"
+edge m _ | m < 3 = error "edge: edge width should be more than 2"
+edge m n = do
+	c1 <- clock n
+	c2 <- clock n
+	(dli, dlo) <- delay (m - 2)
+	(ni, no) <- notGate
+	(a1, a2, ao) <- andGate
+	zipWithM_ connectWire [c2, dlo, no, c1] [dli, ni, a2, a1]
+	return ao
