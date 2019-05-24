fe0890ff9e709e052b03460d5d1fa9515a21d7e9
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-05-24 16:12:22 +0900

    circuit: RISC-V ALU: try with compiled

--- tribial/circuit/app/Main.hs
+++ tribial/circuit/app/Main.hs
@@ -1,12 +1,26 @@
+{-# LANGUAGE BinaryLiterals #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Main where
 
+import RiscvAlu
 import Circuit
-import TestCircuit
+import Tools
 
 main :: IO ()
 main = let
-	(ws, cct) = makeCircuit $ riscv_alu 64
-	cct0 = setRiscvAlu ws 6 10 3 cct in
-	print . peekRiscvAlu ws . (!! 700) $ iterate step cct0
+	(ws, cct) = makeCircuit $ riscvAlu 6
+	cct0 = setBitsRiscvAlu ws 0b0000 0xff00 0x0ff0 cct
+	cct1 = setBitsRiscvAlu ws 0b0001 0xff00 0x0ff0 cct
+	cct2 = setBitsRiscvAlu ws 0b0010 0xff00 0x0ff0 cct
+	cct3 = setBitsRiscvAlu ws 0b0110 0xff00 0x0ff0 cct
+	cct4 = setBitsRiscvAlu ws 0b0111 0xff00 0x0ff0 cct
+	cct5 = setBitsRiscvAlu ws 0b0111 0x0ff0 0xff00 cct
+	cct6 = setBitsRiscvAlu ws 0b1100 0x0ff0 0xff00 cct in do
+		putStrLn $ "AND\t0xff00 0x0ff0 : " ++ show (peekBitsRiscvAlu ws $ run 150 cct0)
+		putStrLn $ "OR\t0xff00 0x0ff0 : " ++ show (peekBitsRiscvAlu ws $ run 150 cct1)
+		putStrLn $ "add\t0xff00 0x0ff0 : " ++ show (peekBitsRiscvAlu ws $ run 150 cct2)
+		putStrLn $ "sub\t0xff00 0x0ff0 : " ++ show (peekBitsRiscvAlu ws $ run 150 cct3)
+		putStrLn $ "slt\t0xff00 0x0ff0 : " ++ show (peekBitsRiscvAlu ws $ run 150 cct4)
+		putStrLn $ "slt\t0x0ff0 0xff00 : " ++ show (peekBitsRiscvAlu ws $ run 150 cct5)
+		putStrLn $ "NOR\t0xff00 0x0ff0 : " ++ show (peekBitsRiscvAlu ws $ run 150 cct6)
