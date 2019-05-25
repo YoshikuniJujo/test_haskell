ad2dd7ef1a6f7fa2a3fc02fb17a3de89299444a6
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-05-25 13:19:38 +0900

    circuit: multi bit dflipflop

--- tribial/circuit/src/Memory.hs
+++ tribial/circuit/src/Memory.hs
@@ -3,6 +3,7 @@
 module Memory where
 
 import Control.Monad
+import Data.List
 import Data.Word
 
 import Circuit
@@ -40,6 +41,13 @@ temporary w dr = do
 	zipWithM_ connectWire [on, on, wo, dro] [wi, dri, i1, i2]
 	return o
 
+temporaryData :: Word8 -> Word8 -> Word8 -> CircuitBuilder ([IWire], [OWire])
+temporaryData n w dr = do
+	t <- temporary w dr
+	(a1s, ds, rs) <- unzip3 <$> replicateM (fromIntegral n) andGate
+	mapM_ (connectWire t) a1s
+	return (ds, rs)
+
 testDlatch :: CircuitBuilder (OWire, OWire)
 testDlatch = do
 	c <- edge 10 100
@@ -51,8 +59,8 @@ testDlatch = do
 	connectWire d d'
 	return (q, q_)
 
-dflipflop :: CircuitBuilder (IWire, IWire, OWire, OWire)
-dflipflop = do
+dflipflop1 :: CircuitBuilder (IWire, IWire, OWire, OWire)
+dflipflop1 = do
 	(cin, cout) <- idGate
 	(mc, md, mq, _mq_) <- dlatch
 	(ni, no) <- notGate
@@ -60,11 +68,37 @@ dflipflop = do
 	zipWithM_ connectWire [cout, cout, no, mq] [mc, ni, sc, sd]
 	return (cin, md, sq, sq_)
 
-testDflipflop :: CircuitBuilder (OWire, OWire)
-testDflipflop = do
+testDflipflop1 :: CircuitBuilder (OWire, OWire)
+testDflipflop1 = do
 	c <- clock 100
 	d <- temporary 80 40
-	(c', d', q, q_) <- dflipflop
+	(c', d', q, q_) <- dflipflop1
 	connectWire c c'
 	connectWire d d'
 	return (q, q_)
+
+dflipflop :: Word8 -> CircuitBuilder (IWire, [IWire], [OWire], [OWire])
+dflipflop n = do
+	(cin, cout) <- idGate
+	(cs, ds, qs, q_s) <- unzip4 <$> replicateM (fromIntegral n) dflipflop1
+	mapM_ (connectWire cout) cs
+	return (cin, ds, qs, q_s)
+
+type TestDflipflopWires = ([IWire], [OWire], [OWire])
+
+testDflipflop :: Word8 -> CircuitBuilder TestDflipflopWires
+testDflipflop n = do
+	c <- clock 100
+	(din, dout) <- temporaryData n 80 40
+	(c', ds', qs, q_s) <- dflipflop n
+	connectWire c c'
+	zipWithM_ connectWire dout ds'
+	return (din, qs, q_s)
+
+setBitsTestDflipflop :: TestDflipflopWires -> Word64 -> Circuit -> Circuit
+setBitsTestDflipflop (ds, _, _) d =
+	flip (foldr $ uncurry setBit) . zip ds $ wordToBits 64 d
+
+peekBitsTestDflipflop :: TestDflipflopWires -> Circuit -> (Word64, Word64)
+peekBitsTestDflipflop (_, qs, q_s) cct =
+	(bitsToWord $ (`peekOWire` cct) <$> qs, bitsToWord $ (`peekOWire` cct) <$> q_s)
