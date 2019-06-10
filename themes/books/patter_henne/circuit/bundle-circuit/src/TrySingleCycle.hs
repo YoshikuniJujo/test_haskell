08bc3a113e47654919e581d97f2006c46a5d8b88
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-06-10 17:16:43 +0900

    bundle-circuit: sample instructions

--- themes/books/patter_henne/circuit/bundle-circuit/src/TrySingleCycle.hs
+++ themes/books/patter_henne/circuit/bundle-circuit/src/TrySingleCycle.hs
@@ -2,6 +2,9 @@
 
 module TrySingleCycle where
 
+import Data.Bits
+import Data.Word
+
 import Circuit
 import Clock
 import Memory
@@ -39,3 +42,43 @@ tryInstMem = do
 	rim <- riscvInstMem 64
 	connectWire64 (pcOutput pc) (rimReadAddress rim)
 	return (cl, pc, rim)
+
+sampleInstructions :: [Word64]
+sampleInstructions = fromIntegral <$> [
+	0x00f507b3,		-- add a5, a0, a5
+	0x40208f33,		-- sub r30, r01, r02
+	0x004787b3,		-- add a5, a5, tp
+	0x015a04b3,		-- add x9, x20, x21
+	0x009a84b3 :: Word32	-- add x9, x21, x9
+	]
+
+-- R type: 7 5 5 3 5 7
+
+packRType :: [Word8] -> Word32
+packRType ws = f7 .|. r2 .|. r1 .|. f3 .|. rd .|. op
+	where
+	[f7_, r2_, r1_, f3_, rd_, op] = fromIntegral <$> ws
+	f7 = f7_ `shiftL` 25; r2 = r2_ `shiftL` 20; r1 = r1_ `shiftL` 15
+	f3 = f3_ `shiftL` 12; rd = rd_ `shiftL` 7
+
+unpackRType :: Word64 -> [Word8]
+unpackRType w = fromIntegral <$> [
+	f7_ `shiftR` 25, r2_ `shiftR` 20, r1_ `shiftR` 15,
+	f3_ `shiftR` 12, rd_ `shiftR` 7, op ]
+	where
+	[f7_, r2_, r1_, f3_, rd_, op] = map (w .&.) [
+		0xfe000000,
+		0x01f00000,
+		0x000f8000,
+		0x00007000,
+		0x00000f80,
+		0x0000007f ]
+
+data ReadReg = ReadReg Word8 deriving Show
+data WriteReg = WriteReg Word8 deriving Show
+
+decodeRTypeFromWords ::
+	[Word8] -> (Word8, ReadReg, ReadReg, Word8, WriteReg, Word8)
+decodeRTypeFromWords [f7, r2, r1, f3, rd, op] =
+	(f7, ReadReg r2, ReadReg r1, f3, WriteReg rd, op)
+decodeRTypeFromWords _ = error "Oops!"
