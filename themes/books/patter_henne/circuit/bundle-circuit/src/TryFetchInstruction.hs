97c2054873a34950418688dd247c0f073d83884d
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-06-10 15:49:23 +0900

    bundle-circuit: tryFetchInstructionTryRun: more

--- themes/books/patter_henne/circuit/bundle-circuit/src/TryFetchInstruction.hs
+++ themes/books/patter_henne/circuit/bundle-circuit/src/TryFetchInstruction.hs
@@ -11,11 +11,12 @@ cct1 = storeRiscvInstMem rim 0 1234567890 cct
 cct2 = storeRiscvInstMem rim 4 9876543210 cct1
 cct3 = storeRiscvInstMem rim 8 9999999999 cct2
 cct4 = storeRiscvInstMem rim 12 1111111111 cct3
-cct5 = resetProgramCounter pc cct4
-cct6 = clockOn cl cct5
+cct5 = storeRiscvInstMem rim 16 7777777777 cct4
+cct6 = resetProgramCounter pc cct5
+cct7 = clockOn cl cct6
 
-middle = readRiscvInstMem rim cct3
+middle = readRiscvInstMem rim cct4
 
-tryRun = readRiscvInstMem rim <$> iterate step cct6
+tryRun = readRiscvInstMem rim <$> iterate step cct7
 
 tryFetchInstructionTryRun = tryRun
