2a6ea5173ae035e7c439eeb7a35ec8d42bb1523b
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-06-19 17:53:39 +0900

    bundle-circuit: control: dispatch2

--- themes/books/patter_henne/circuit/bundle-circuit/src/Control.hs
+++ themes/books/patter_henne/circuit/bundle-circuit/src/Control.hs
@@ -20,6 +20,20 @@ dispatch1 = do
 	connectWire (instout, 1, 6) (nstin, 1, 3)
 	return (instin, nstout)
 
+dispatch2 :: CircuitBuilder (IWire, OWire)
+dispatch2 = do
+	(instin, instout) <- idGate64
+	(nstin, nstout) <- idGate64
+	b0 <- constGate0 $ Bits 1
+	connectWire (b0, 1, 0) (nstin, 1, 0)
+	(ni, no) <- notGate0
+	connectWire (instout, 1, 5) (ni, 1, 0)
+	connectWire (no, 1, 0) (nstin, 1, 1)
+	connectWire (instout, 1, 5) (nstin, 1, 2)
+	b3 <- constGate0 $ Bits 0
+	connectWire (b3, 1, 0) (nstin, 1, 3)
+	return (instin, nstout)
+
 sampleLoadInst, sampleStoreInst, sampleAddInst, sampleBeqInst :: Word64
 sampleLoadInst = encodeInst $ Load (Reg 10) 56 (Reg 15)
 sampleStoreInst = encodeInst $ Store (Reg 1) 8 (Reg 2)
