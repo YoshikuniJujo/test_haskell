a1480bac59ad39a3366d9b63fae74b13d8c337d4
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-06-19 17:30:57 +0900

    bundle-circuit: control: dispatch1

---
+++ themes/books/patter_henne/circuit/bundle-circuit/src/Control.hs
@@ -0,0 +1,27 @@
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Control where
+
+import Data.Word
+
+import Circuit
+import MakeInstruction
+
+dispatch1 :: CircuitBuilder (IWire, OWire)
+dispatch1 = do
+	(instin, instout) <- idGate64
+	(nstin, nstout) <- idGate64
+	b0 <- constGate0 $ Bits 0
+	connectWire (b0, 1, 0) (nstin, 1, 0)
+	(ni, no) <- notGate0
+	connectWire (instout, 1, 6) (ni, 1, 0)
+	connectWire (no, 1, 0) (nstin, 1, 1)
+	connectWire (instout, 1, 4) (nstin, 1, 2)
+	connectWire (instout, 1, 6) (nstin, 1, 3)
+	return (instin, nstout)
+
+sampleLoadInst, sampleStoreInst, sampleAddInst, sampleBeqInst :: Word64
+sampleLoadInst = encodeInst $ Load (Reg 10) 56 (Reg 15)
+sampleStoreInst = encodeInst $ Store (Reg 1) 8 (Reg 2)
+sampleAddInst = encodeInst $ Add (Reg 15) (Reg 10) (Reg 15)
+sampleBeqInst = encodeInst $ Beq (Reg 30) (Reg 31) 20
