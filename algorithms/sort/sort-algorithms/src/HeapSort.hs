458919342c1a98e6f1129e739389a5e3a8f5b892
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-07-25 10:37:44 +0900

    heap sort: refactoring and so on

--- algorithms/sort/sort-algorithms/src/HeapSort.hs
+++ algorithms/sort/sort-algorithms/src/HeapSort.hs
@@ -9,64 +9,50 @@ import Data.Array.MArray
 import Data.Array.ST
 import Data.Bool
 
-swapArray :: (MArray a e m, Ix i) => a i e -> i -> i -> m ()
-swapArray arr i j = do
+heapSort :: forall a . Ord a => [a] -> [a]
+heapSort xs = runST $ (>>) <$> heapSortArray <*> getElems
+	=<< (newListArray (0, length xs - 1) xs :: ST s (STArray s Int a))
+
+heapSortArray :: (MArray a e m, Ix i, Integral i, Ord e) => a i e -> m ()
+heapSortArray = (>>) <$> construct <*> destruct
+
+construct :: (MArray a e m, Ix i, Integral i, Ord e) => a i e -> m ()
+construct arr = do
+	(i0, ix) <- getBounds arr
+	flip (correct arr i0) ix `mapM_` reverse (range (i0, parent i0 ix))
+
+destruct :: (MArray a e m, Ix i, Integral i, Ord e) => a i e -> m ()
+destruct arr = do
+	bs@(i0, _) <- getBounds arr
+	for_ (init . reverse $ range bs)
+		$ (>>) <$> swap arr i0 <*> correct arr i0 i0 . subtract 1
+
+correct ::
+	(MArray a e m, Ix i, Integral i, Ord e) => a i e -> i -> i -> i -> m ()
+correct arr i0 b e
+	| li > e = return ()
+	| ri > e = do
+		p <- arr `readArray` b
+		lc <- arr `readArray` li
+		bool (return ()) (swap arr b li >> correct arr i0 li e) (lc > p)
+	| otherwise = do
+		p <- arr `readArray` b
+		lc <- arr `readArray` li
+		rc <- arr `readArray` ri
+		if p >= lc && p >= rc then return () else bool
+			(swap arr b li >> correct arr i0 li e)
+			(swap arr b ri >> correct arr i0 ri e)
+			(lc < rc)
+	where li = leftChild i0 b; ri = rightChild i0 b
+
+parent, leftChild, rightChild :: Integral i => i -> i -> i
+parent i0 i = (i + i0 - 1) `div` 2
+leftChild i0 i = i * 2 - i0 + 1
+rightChild i0 i = i * 2 - i0 + 2
+
+swap :: (MArray a e m, Ix i) => a i e -> i -> i -> m ()
+swap arr i j = do
 	x <- arr `readArray` i
 	y <- arr `readArray` j
 	(arr `writeArray` i) y
 	(arr `writeArray` j) x
-
-parent :: Integral i => i -> i
-parent i = (i + 1) `div` 2 - 1
-
-leftChild, rightChild :: Integral i => i -> i
-leftChild i = (i + 1) * 2 - 1
-rightChild i = (i + 1) * 2
-
-heapSortList :: forall a . Ord a => [a] -> [a]
-heapSortList xs = runST $ do
-	a <- newListArray (0, length xs - 1) xs :: ST s (STArray s Int a)
-	heapSort a
-	getElems a
-
-heapSort :: (MArray a e m, Ix i, Integral i, Ord e) => a i e -> m ()
-heapSort = (>>) <$> makeTree <*> fromTree
-
-makeTree :: (MArray a e m, Ix i, Integral i, Ord e) => a i e -> m ()
-makeTree arr = do
-	bs <- getBounds arr
-	mapM_ (arr `upheap`) . tail $ range bs
-
-fromTree :: (MArray a e m, Ix i, Integral i, Ord e) => a i e -> m ()
-fromTree arr = do
-	bs <- getBounds arr
-	for_ (init . reverse $ range bs) $ \i -> do
-		swapArray arr 0 i
-		downheap arr i 0
-
-upheap :: (MArray a e m, Ix i, Integral i, Ord e) => a i e -> i -> m ()
-upheap _ n | n < 1 = return ()
-upheap arr n = do
-	p <- arr `readArray` m
-	c <- arr `readArray` n
-	bool (return ()) (swapArray arr m n >> upheap arr m) (p < c)
-	where m = parent n
-
-downheap :: (MArray a e m, Ix i, Integral i, Ord e) => a i e -> i -> i -> m ()
-downheap arr n m
-	| lc >= n = return ()
-	| rc >= n = do
-		p <- arr `readArray` m
-		l <- arr `readArray` lc
-		bool (return ()) (swapArray arr lc m >> downheap arr n lc) (l > p)
-	| otherwise = do
-		p <- arr `readArray` m
-		l <- arr `readArray` lc
-		r <- arr `readArray` rc
-		if p >= l && p >= r then return () else
-			bool	(swapArray arr lc m >> downheap arr n lc)
-				(swapArray arr rc m >> downheap arr n rc)
-				(l < r)
-	where
-	lc = leftChild m
-	rc = rightChild m
