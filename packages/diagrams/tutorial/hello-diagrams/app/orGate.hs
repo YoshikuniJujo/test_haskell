e02e85dac0a6c37d27c575536dad7a07bf365ce6
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-07-30 07:48:42 +0900

    hello-diagrams: correct or gate

--- packages/diagrams/tutorial/hello-diagrams/app/orGate.hs
+++ packages/diagrams/tutorial/hello-diagrams/app/orGate.hs
@@ -11,21 +11,25 @@ import Diagrams.Backend.SVG.CmdLine
 example :: Diagram B
 example = alignY 0 (vcat' (with & sep .~ 1) [line, line]) {- # showOrigin -} ||| orGate ||| line
 
+orGateD :: Diagram B
+orGateD = moveTo ((- 1.5) ^& 0) (orGate ||| line' 0.2)
+	`withEnvelope'` (rect 3 3 :: Diagram B)
+
 line :: Diagram B
 line = (strokeT (fromOffsets [unitX]) # lwL 0.08)
 
 line2 :: Diagram B
-line2 = (strokeT (fromOffsets [zero & _x .~ 0.2]) # lwL 0.08)
+line2 = (strokeT (fromOffsets [zero & _x .~ 0.3]) # lwL 0.08)
 
 line' :: Double -> Diagram B
 line' l = (strokeT (fromOffsets [zero &_x .~ l]) # lwL 0.08)
 
 orGate, orGate0, orGate1, orGate2, orGate3 :: Diagram B
-orGate = (orGate0 <> orGate1 <> orGate2) ||| orGate3
-orGate0 = alignY 0 (vcat' (with & sep .~ 1) [line2, line2])
-orGate1 = arcBetween (p2 (0, -1)) (p2 (0, 1)) (- 0.3) # lwL 0.08
-orGate2 = alignY 0 $ vcat' (with & sep .~ 2) [line' 1.7, line' 1.7]
-orGate3 = (arcBetween (p2 (0, 1)) (p2 (1, 0)) 0.2 === arcBetween (p2 (0, -1)) (p2 (1, 0)) (- 0.2)) #
+orGate = orGate0 <> orGate1 <> orGate2 <> moveTo (1.7 ^& 0) orGate3
+orGate0 = alignY 0 (vcat' (with & sep .~ 2) [line2, line2])
+orGate1 = arcBetween (p2 (0.2, -1.3)) (p2 (0.2, 1.3)) (- 0.3) # lwL 0.08
+orGate2 = moveTo (0.169 ^& 0) $ alignY 0 $ vcat' (with & sep .~ 2.6) [line' 1.545, line' 1.545]
+orGate3 = (arcBetween (p2 (0, 1.3)) (p2 (1.1, 0)) 0.2 === arcBetween (p2 (0, -1.3)) (p2 (1.1, 0)) (- 0.2)) #
 	lwL 0.08 -- arc (dir unit_Y) (1 / 2 @@ turn) # lwL 0.08
 
 withEnvelope' :: (InSpace v n a, Monoid' m, Enveloped a) =>
@@ -42,4 +46,4 @@ squareB :: Double -> Diagram B
 squareB = square
 
 main :: IO ()
-main = mainWith example
+main = mainWith orGateD
