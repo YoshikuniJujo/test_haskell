986b082826e2b8a6832a7f066f941fa0fea7420c
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-08-02 05:14:51 +0900

    zundoko-kiyoshi

--- tribial/tribial-trials/src/Lib.hs
+++ tribial/tribial-trials/src/Lib.hs
@@ -1,6 +1,45 @@
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
 module Lib
-    ( someFunc
+    ( someFunc, runZundoko
     ) where
+
+import System.Random
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+data ZunDoko = Z | D deriving (Show, Bounded)
+
+instance Random ZunDoko where
+	randomR (a, b) g =
+		let (x, g') = (randomR (zunDokoToInt a, zunDokoToInt b) g) in
+			(intToZunDoko x, g')
+		where
+		zunDokoToInt :: ZunDoko -> Int
+		zunDokoToInt Z = 0
+		zunDokoToInt D = 1
+		intToZunDoko :: Int -> ZunDoko
+		intToZunDoko 0 = Z
+		intToZunDoko _ = D
+	random = randomR (minBound, maxBound)
+
+runZundoko :: IO ()
+runZundoko = do
+	g <- newStdGen
+	putStrLn $ zundoko g
+
+zundoko :: StdGen -> String
+zundoko g = zundokoStr `concatMap` zundokoGen g ++ "キ・ヨ・シ!"
+
+zundokoStr :: ZunDoko -> String
+zundokoStr Z = "ズン"
+zundokoStr D = "ドコ"
+
+zundokoGen :: StdGen -> [ZunDoko]
+zundokoGen g = zundokoFinish $ randoms g
+
+zundokoFinish :: [ZunDoko] -> [ZunDoko]
+zundokoFinish (Z : Z : Z : Z : D : _) = [Z, Z, Z, Z, D]
+zundokoFinish (zd : zds) = zd : zundokoFinish zds
+zundokoFinish _ = []
