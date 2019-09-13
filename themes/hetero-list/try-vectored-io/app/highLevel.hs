a2e552c1f999d536537ac4dcc98f566cad5fc88e
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-09-13 16:02:31 +0900

    try-vectored-io: highLevel.hs: change example file name

--- themes/hetero-list/try-vectored-io/app/highLevel.hs
+++ themes/hetero-list/try-vectored-io/app/highLevel.hs
@@ -3,42 +3,49 @@
 
 module Main where
 
-import Foreign.C.String
-import Foreign.C.Types
-import System.IO
-import Numeric
+import Foreign.C.Types (CChar)
+import Foreign.C.String (castCharToCChar, castCCharToChar)
+import System.IO (IOMode(..), withFile)
+import System.FilePath ((</>))
+import Numeric (showHex)
 
 import qualified Data.ByteString as BS
 
-import VectoredIo
-import Iovec
+import VectoredIo (readVector, writeVector)
+import Iovec (ListTuple(..))
+
+tmpDir, tryHlTupleFile, tryHlListFile, tryInadequateReadFile :: FilePath
+tmpDir = "tmp"
+tryHlTupleFile = tmpDir </> "tryHlTuple.txt"
+tryHlListFile = tmpDir </> "tryHlList.txt"
+tryInadequateReadFile = tmpDir </> "tryInadequateReadFile.txt"
 
 main :: IO ()
-main = main1 >> main2 >> main3
+main = tryTuple >> tryList >> tryInadequateRead
 
-main1 :: IO ()
-main1 = do
-	withFile "tmp_hl.txt" WriteMode $ \h ->
+tryTuple :: IO ()
+tryTuple = do
+	withFile tryHlTupleFile WriteMode $ \h ->
 		writeVector h ([0x3132333435363738 :: Int] :. (castCharToCChar <$> "w\n") :. ListTupleNil)
-	withFile "tmp_hl.txt" ReadMode $ \h -> do
+	withFile tryHlTupleFile ReadMode $ \h -> do
 		Right (n :. str :. ListTupleNil :: ListTuple [Int, CChar]) <- readVector h [1, 2]
 		mapM_ (putStrLn . ("0x" ++)) $ (`showHex` "") <$> n
 		print $ castCCharToChar <$> str
 
-main2 :: IO ()
-main2 = do
-	withFile "tmp_hl2.txt" WriteMode $ \h ->
+tryList :: IO ()
+tryList = do
+	withFile tryHlListFile WriteMode $ \h ->
 		writeVector h [castCharToCChar <$> "Hello, ", castCharToCChar <$> "world!\n"]
-	withFile "tmp_hl2.txt" ReadMode $ \h -> do
+	withFile tryHlListFile ReadMode $ \h -> do
 		Right [s1, s2] <- readVector h [7, 7]
 		print (castCCharToChar <$> s1 :: String)
 		print (castCCharToChar <$> s2 :: String)
 
-main3 :: IO ()
-main3 = do
-	withFile "dummy.txt" WriteMode $ \h ->
+tryInadequateRead :: IO ()
+tryInadequateRead = do
+	withFile tryInadequateReadFile WriteMode $ \h ->
 		writeVector h [castCharToCChar <$> "12345678123456"]
-	withFile "dummy.txt" ReadMode $ \h -> do
+	withFile tryInadequateReadFile ReadMode $ \h -> do
 		(Left [s1, s2] :: Either [BS.ByteString] [[Int]]) <- readVector h [1, 1]
 		print s1
 		print s2
