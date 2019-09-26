c0f1a8f59c4c40a4f4396fcd74aa21908f93df80
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-09-26 16:08:14 +0900

    try-freer-effects-ftcq: module Freer and FTCQueue

---
+++ tribial/qiita/try-freer-effects-ftcq/ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for try-freer-effects-ftcq
+
+## Unreleased changes
---
+++ tribial/qiita/try-freer-effects-ftcq/src/FTCQueue.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE ExistentialQuantification #-}
+
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module FTCQueue (FTCQueue, ViewL(..), tsingleton, (|>), (><), tviewl) where
+
+data FTCQueue t a b
+	= Leaf (a -> t b)
+	| forall x . Node (FTCQueue t a x) (FTCQueue t x b)
+
+tsingleton :: (a -> t b) -> FTCQueue t a b
+tsingleton = Leaf
+
+(|>) :: FTCQueue t a b -> (b -> t c) -> FTCQueue t a c
+(|>) = (. Leaf) . Node
+
+(><) :: FTCQueue t a b -> FTCQueue t b c -> FTCQueue t a c
+(><) = Node
+
+data ViewL t a b
+	= TOne (a -> t b)
+	| forall x . (a -> t x) :| FTCQueue t x b
+
+tviewl :: FTCQueue t a b -> ViewL t a b
+tviewl (Leaf f) = TOne f
+tviewl (Node l0 r0) = go l0 r0
+	where
+	go :: FTCQueue t a x -> FTCQueue t x b -> ViewL t a b
+	go (Leaf f) r = f :| r
+	go (Node ll lr) r = go ll (Node lr r)
