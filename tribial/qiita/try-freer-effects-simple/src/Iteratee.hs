89965d7fc019cfc1ca3ee841e7e2123dccf5d9ad
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-10-04 16:50:24 +0900

    try-freer-effects-simple: add par

--- tribial/qiita/try-freer-effects-simple/src/Iteratee.hs
+++ tribial/qiita/try-freer-effects-simple/src/Iteratee.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GADTs, DataKinds, TypeOperators, FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables, GADTs, DataKinds, TypeOperators, FlexibleContexts, AllowAmbiguousTypes, TypeApplications #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Iteratee where
@@ -18,3 +18,20 @@ runIteratee m ia@(~(i : is)) = case m of
 			[] -> Pure Nothing
 			_ -> runIteratee (k i) is
 		Left u' -> u' `Bind` ((`runIteratee` ia) . k)
+
+viewIteratee :: forall effs i a . Member (Iteratee i) effs => Eff effs a -> Eff effs (Maybe (i -> Eff effs a))
+viewIteratee m = case m of
+	Pure _ -> return Nothing
+	u `Bind` k -> case prj u of
+		Just (Get :: Iteratee i x) -> return $ Just k
+		Nothing -> u `Bind` (viewIteratee . k)
+
+par :: forall effs i a b . Member (Iteratee i) effs =>
+	Eff effs a -> Eff effs b -> Eff effs (Eff effs a, Eff effs b)
+par l r = do
+	(vl :: Maybe (i -> Eff effs a)) <- viewIteratee l
+	(vr :: Maybe (i -> Eff effs b)) <- viewIteratee r
+	case (vl, vr) of
+		(Nothing, _) -> return (l, r)
+		(_, Nothing) -> return (l, r)
+		(Just l', Just r') -> get >>= \x -> par @_ @i (l' x) (r' x)
