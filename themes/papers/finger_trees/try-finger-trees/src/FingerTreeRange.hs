501a92425cf46ab9af893ec0a2792faf4428f3ee
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-10-09 10:33:48 +0900

    try-finger-trees: define (><) with Range 0 4

--- themes/papers/finger_trees/try-finger-trees/src/FingerTreeRange.hs
+++ themes/papers/finger_trees/try-finger-trees/src/FingerTreeRange.hs
@@ -138,6 +138,14 @@ app3 xs ts (Single x) = (xs |>. ts) |> x
 app3 (Deep pr1 m1 sf1) ts (Deep pr2 m2 sf2) =
 	Deep pr1 (app3 m1 (nodes (digitToList sf1 ++ foldr (:) [] ts ++ digitToList pr2)) m2) sf2
 
+app3' :: forall a . FingerTree a -> Range 0 4 a -> FingerTree a -> FingerTree a
+app3' Empty ts xs = ts <|. xs
+app3' xs ts Empty = xs |>. ts
+app3' (Single x) ts xs = x <| (ts <|. xs)
+app3' xs ts (Single x) = (xs |>. ts) |> x
+app3' (Deep pr1 m1 sf1) ts (Deep pr2 m2 sf2) =
+	Deep pr1 (app3' m1 (loosen (nodes' (sf1 ++. ts ++.. pr2) :: Range 1 4 (Node a))) m2) sf2
+
 digitToList :: Digit a -> [a]
 digitToList = foldr (:) []
 
@@ -160,10 +168,12 @@ nodes [a, b, c] = [a :. b :. c :.. Nil]
 nodes [a, b, c, d] = [a :. b :. Nil, c :. d :. Nil]
 nodes (a : b : c : xs) = a :. b :. c :.. Nil : nodes xs
 
-nodesTest :: Range 2 4 a -> Range 1 2 (Node a)
+nodesTest :: Range 2 6 a -> Range 1 2 (Node a)
 nodesTest (a :. b :. Nil) = (a :. b :. Nil) :. Nil
 nodesTest (a :. b :. c :.. Nil) = (a :. b :. c :.. Nil) :. Nil
 nodesTest (a :. b :. c :.. d :.. Nil) = (a :. b :. Nil) :. (c :. d :. Nil) :.. Nil
+nodesTest (a :. b :. c :.. d :.. e :.. Nil) = (a :. b :. c :.. Nil) :. (d :. e :. Nil) :.. Nil
+nodesTest (a :. b :. c :.. d :.. e :.. f :.. Nil) = (a :. b :. c :.. Nil) :. (d :. e :. f :.. Nil) :.. Nil
 
 class Nodes m m' where
 	nodes' :: Range 2 m a -> Range 1 m' (Node a)
@@ -175,10 +185,10 @@ class Nodes m m' where
 -- instance Nodes 2 5 m' where
 -- instance Nodes 2 6 m' where
 
-instance Nodes 1 1 where
+-- instance Nodes 1 1 where
 
 -- instance {-# INCOHERENT #-} Nodes 2 4 2 where
-instance Nodes (4 - 3) (2 - 1)  => Nodes 4 2 where
+instance Nodes 6 2 where
 	nodes' = loosen . nodesTest
 
 {-
@@ -199,5 +209,6 @@ instance {-# OVERLAPPABLE #-} Nodes (m - 3) (m' - 1) => Nodes m m' where
 	nodes' (a :. b :. c :.. d :.. e :.. xs :: Range 2 m a) = (a :. b :. c :.. Nil) .:.. (nodes' (d :. e :. xs :: Range 2 (m - 3) a) :: Range 1 (m' - 1) (Node a))
 --	nodes' (a :. b :. c :.. xs) = (a :. b :. c :.. Nil) .:.. nodes' xs
 
-(><) :: FingerTree a -> FingerTree a -> FingerTree a
+(><), (><.) :: FingerTree a -> FingerTree a -> FingerTree a
 xs >< ys = app3 xs [] ys
+xs ><. ys = app3' xs Nil ys
