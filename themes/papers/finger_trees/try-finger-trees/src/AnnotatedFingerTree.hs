8921a1fda92d9b784ffe52981c6b57d21b2edab0
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-10-09 11:42:32 +0900

    try-finger-trees: AnnotatedFingerTree: add (|>)

--- themes/papers/finger_trees/try-finger-trees/src/AnnotatedFingerTree.hs
+++ themes/papers/finger_trees/try-finger-trees/src/AnnotatedFingerTree.hs
@@ -8,6 +8,9 @@ module AnnotatedFingerTree where
 
 import Range
 
+reducer :: Foldable t => (a -> b -> b) -> t a -> b -> b
+reducer = flip . foldr
+
 reducel :: Foldable t => (b -> a -> b) -> b -> t a -> b
 reducel = foldl
 
@@ -45,7 +48,7 @@ instance Measured a v => Measured (FingerTree v a) v where
 	measure (Single x) = measure x
 	measure (Deep v _ _ _) = v
 
-infixr 5 <||, <|
+infixr 5 <||, <|, <|.
 
 (<||) :: Measured a v => a -> Digit a -> Either (Digit a) (Digit a, Node v a)
 a <|| b :. Nil = Left $ a :. b :.. Nil
@@ -60,3 +63,28 @@ a <| Single b = deep (a :. Nil) Empty (b :. Nil)
 a <| Deep _ pr m sf = case a <|| pr of
 	Left pr' -> deep pr' m sf
 	Right (pr', n3) -> deep pr' (n3 <| m) sf
+
+(<|.) :: (Measured a v, Foldable t) => t a -> FingerTree v a -> FingerTree v a
+(<|.) = reducer (<|)
+
+toTree :: (Measured a v, Foldable t) => t a -> FingerTree v a
+toTree = (<|. Empty)
+
+infixl 5 ||>, |>, |>.
+
+(||>) :: Measured a v => Digit a -> a -> Either (Digit a) (Node v a, Digit a)
+a :. Nil ||> b = Left $ a :. b :.. Nil
+a :. b :.. Nil ||> c = Left $ a :. b :.. c :.. Nil
+a :. b :.. c :.. Nil ||> d = Left $ a :. b :.. c :.. d :.. Nil
+a :. b :.. c :.. d :.. Nil ||> e = Right (node3 a b c, d :. e :.. Nil)
+_ ||> _ = error "never occur"
+
+(|>) :: Measured a v => FingerTree v a -> a -> FingerTree v a
+Empty |> a = Single a
+Single a |> b = deep (a :. Nil) Empty (b :. Nil)
+Deep _ pr m sf |> a = case sf ||> a of
+	Left sf' -> deep pr m sf'
+	Right (n3, sf') -> deep pr (m |> n3) sf'
+
+(|>.) :: (Measured a v, Foldable t) => FingerTree v a -> t a -> FingerTree v a
+(|>.) = reducel (|>)
