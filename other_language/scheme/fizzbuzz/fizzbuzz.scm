e1d73f8bc4d3933197727f3f74ff12dbc42cd7d9
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-10-10 10:18:30 +0900

    Scheme: fizzbuzz string

---
+++ other_language/scheme/fizzbuzz/fizzbuzz.scm
@@ -0,0 +1,15 @@
+#!/usr/bin/env gosh
+
+(define (range init max)
+	(cond	((< max init) '())
+		(else (cons init (range (+ 1 init) max)))))
+
+(define fizzbuzz1 (lambda (n)
+	(cond	((= (modulo n 15) 0) "fizzbuzz")
+		((= (modulo n 3) 0) "fizz")
+		((= (modulo n 5) 0) "buzz")
+		(else (number->string n)))))
+
+(define fizzbuzz (string-append (string-join (map fizzbuzz1 (range 1 100)) "\n") "\n"))
+
+fizzbuzz
