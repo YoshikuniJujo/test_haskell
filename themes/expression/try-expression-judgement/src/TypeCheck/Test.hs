9bb265a23ff51dc444c0afe1be5fa03e4a8ea9cd
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-10-22 17:04:47 +0900

    try-Aexpression-judgement: function nodes: refactoring

--- themes/expression/try-expression-judgement/src/TypeCheck/Test.hs
+++ themes/expression/try-expression-judgement/src/TypeCheck/Test.hs
@@ -3,10 +3,6 @@
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleContexts, FlexibleInstances, UndecidableInstances #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeFamilyDependencies #-}
-{-# LANGUAGE AllowAmbiguousTypes #-}
-{-# LANGUAGE TypeApplications #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs -fplugin=TypeCheck.Nat #-}
 
 module TypeCheck.Test where
@@ -22,80 +18,29 @@ data RangeL :: Nat -> Nat -> * -> * where
 
 deriving instance Show a => Show (RangeL n m a)
 
-infixr 5 .:..
-
-{-
-class PushL m where
-	(.:..) :: a -> RangeL n m a -> RangeL n (m + 1) a
-
--- instance {-# INCOHERENT #-} PushL 0 where
-instance PushL 0 where
-	x .:.. NilL = x :.. NilL
-	_ .:.. _ = error "never occur"
-
-instance {-# OVERLAPPABLE #-} (1 <= m, 1 <= m + 1, PushL (m - 1)) => PushL m where
--- instance {-# INCOHERENT #-} (1 <= m, 1 <= m + 1, PushL (m - 1)) => PushL m where
--- instance {-# OVERLAPPABLE #-} (1 <= m, PushL (m - 1)) => PushL m where
-	x .:.. NilL = x :.. NilL
-	x .:.. ya@(_ :.. _) = x :.. ya
-	x .:.. (y :. ys) = x :. (y .:.. ys)
-	-}
-
-{-
-(.:..) :: (1 <= m, 1 <= m - 1, 1 <= (m - 1) - 1) => a -> RangeL n (m - 1) a -> RangeL n m a
-x .:.. NilL = x :.. NilL
-x .:.. ya@(_ :.. _) = x :.. ya
-x .:.. (y :. ys) = x :. (y .:.. ys)
--}
-
 type Node = RangeL 2 3
 
+class Nodes m m' where nodes :: RangeL 2 m a -> RangeL 1 m' (Node a)
 
--- type family Mm m' = r | r -> m' where
-type family Mm m' where
-	Mm 1 = 0
-	Mm 2 = 1
-	Mm m' = m' - 1
+instance Nodes 3 1 where nodes = (:. NilL)
 
-class Nodes m m' where
-	nodes :: RangeL 2 m a -> RangeL 1 m' (Node a)
-	(.:..) :: Node a -> RangeL 1 (m' - 1) (Node a) -> RangeL 1 m' (Node a)
-
-instance Nodes 3 1 where
---	type Mm 1 = 0
-	nodes = (:. NilL)
-	x .:.. _ = x :. NilL
-
-{-
-instance Nodes 4 1 where
-	nodes = (:. NilL)
-
-instance Nodes 5 1 where
-	nodes = (:. NilL)
-	-}
-
-{-
-instance Nodes 6 2 where
---	type Mm 2 = 1
-	nodes :: forall a . RangeL 2 6 a -> RangeL 1 2 (Node a)
-	nodes (a :. b :. NilL) = (a :. b :. NilL) :. NilL
-	nodes (a :. b :. c :.. NilL) = (a :. b :. c :.. NilL) :. NilL
-	nodes (a :. b :. c :.. d :.. NilL) = (a :. b :. NilL) :. (c :. d :. NilL) :.. NilL
-	nodes (a :. b :. c :.. d :.. e :.. xs) = (a :. b :. c :.. NilL) .:.. (nodes (d :. e :. xs :: RangeL 2 3 a) :: RangeL 1 1 (Node a))
-	-}
-
--- instance {-# OVERLAPPABLE #-} (1 <= m', 1 <= (m' - 1), Nodes (m - 3) (m' - 1)) => Nodes m m' where
-instance {-# OVERLAPPABLE #-} (1 <= (m' - 1), Nodes (m - 3) (m' - 1)) => Nodes m m' where
--- instance (1 <= (m' - 1), Nodes (m - 3) (m' - 1)) => Nodes m m' where
---	type Mm m' = m' - 1
+instance {-# OVERLAPPABLE #-}
+	(1 <= (m' - 1), Nodes (m - 3) (m' - 1)) => Nodes m m' where
 	nodes :: forall a . RangeL 2 m a -> RangeL 1 m' (Node a)
 	nodes (a :. b :. NilL) = (a :. b :. NilL) :. NilL
 	nodes (a :. b :. c :.. NilL) = (a :. b :. c :.. NilL) :. NilL
-	nodes (a :. b :. c :.. d :.. NilL) = (a :. b :. NilL) :. (c :. d :. NilL) :.. NilL
---	nodes (a :. b :. c :.. d :.. e :.. xs) = (a :. b :. c :.. NilL) .:.. (nodes (d :. e :. xs :: RangeL 2 (m - 3) a) :: RangeL 1 (m' - 1) (Node a))
-	nodes (a :. b :. c :.. d :.. e :.. xs) = (a :. b :. c :.. NilL) ..:.. (nodes (d :. e :. xs :: RangeL 2 (m - 3) a) :: RangeL 1 (m' - 1) (Node a))
-		where (..:..) = (.:..) @m
---	nodes (a :. b :. c :.. d :.. e :.. xs) = (a :. b :. c :.. NilL) .:.. nodes (d :. e :. xs)
---	x .:.. NilL = x :.. NilL
---	x .:.. ya@(_ :.. _) = x :.. ya
-	x .:.. (y :. ys) = x :. (y :.. ys)
+	nodes (a :. b :. c :.. d :.. NilL) =
+		(a :. b :. NilL) :. (c :. d :. NilL) :.. NilL
+	nodes (a :. b :. c :.. d :.. e :.. xs) =
+		(a :. b :. c :.. NilL) ..:..
+			(nodes (d :. e :. xs :: RangeL 2 (m - 3) a)
+				:: RangeL 1 (m' - 1) (Node a))
+		where x ..:.. (y :. ys) = x :. y :.. ys
+	nodes _ = error "never occur"
+
+sample0, sample1, sample2, sample3, sample4 :: RangeL 2 12 Int
+sample0	 = 1 :. 2 :. 3 :.. 4 :.. 5 :.. 6 :.. 7 :.. 8 :.. 9 :.. 10 :.. 11 :.. NilL
+sample1	 = 1 :. 2 :. 3 :.. 4 :.. 5 :.. 6 :.. 7 :.. 8 :.. 9 :.. 10 :.. NilL
+sample2 = 1 :. 2 :. 3 :.. 4 :.. 5 :.. 6 :.. 7 :.. 8 :.. 9 :.. NilL
+sample3 = 1 :. 2 :. 3 :.. 4 :.. 5 :.. 6 :.. 7 :.. 8 :.. NilL
+sample4 = 1 :. 2 :. 3 :.. 4 :.. 5 :.. 6 :.. 7 :.. NilL
