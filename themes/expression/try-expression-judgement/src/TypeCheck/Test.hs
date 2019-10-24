a83219132b055dd4b374afc10824f39134a4efb5
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-10-24 15:19:12 +0900

    try-expression-judgement: module TypeCheck.Test: class PushL where (.:..)

--- themes/expression/try-expression-judgement/src/TypeCheck/Test.hs
+++ themes/expression/try-expression-judgement/src/TypeCheck/Test.hs
@@ -18,6 +18,18 @@ data RangeL :: Nat -> Nat -> * -> * where
 
 deriving instance Show a => Show (RangeL n m a)
 
+infixr 5 .:..
+
+class PushL n m where
+	(.:..) :: a -> RangeL n m a -> RangeL n (m + 1) a
+
+instance {-# OVERLAPPABLE #-} 1 <= m + 1 => PushL 0 m where
+	(.:..) = (:..)
+
+instance {-# OVERLAPPABLE #-} (1 <= m + 1, PushL (n - 1) (m - 1)) => PushL n m where
+	x .:.. (y :. ys) = x :. (y .:.. ys)
+	_ .:.. _ = error "never occur"
+
 type Node = RangeL 2 3
 
 class Nodes m m' where nodes :: RangeL 2 m a -> RangeL 1 m' (Node a)
@@ -25,17 +37,16 @@ class Nodes m m' where nodes :: RangeL 2 m a -> RangeL 1 m' (Node a)
 instance Nodes 3 1 where nodes = (:. NilL)
 
 instance {-# OVERLAPPABLE #-}
-	(1 <= (m' - 1), Nodes (m - 3) (m' - 1)) => Nodes m m' where
+	(1 <= ((m' - 1) - 1) + 1, 1 <= (m' - 1) + 1, 1 <= (m' - 1), Nodes (m - 3) (m' - 1)) => Nodes m m' where
 	nodes :: forall a . RangeL 2 m a -> RangeL 1 m' (Node a)
 	nodes (a :. b :. NilL) = (a :. b :. NilL) :. NilL
 	nodes (a :. b :. c :.. NilL) = (a :. b :. c :.. NilL) :. NilL
 	nodes (a :. b :. c :.. d :.. NilL) =
 		(a :. b :. NilL) :. (c :. d :. NilL) :.. NilL
 	nodes (a :. b :. c :.. d :.. e :.. xs) =
-		(a :. b :. c :.. NilL) ..:..
+		(a :. b :. c :.. NilL) .:..
 			(nodes (d :. e :. xs :: RangeL 2 (m - 3) a)
 				:: RangeL 1 (m' - 1) (Node a))
-		where x ..:.. (y :. ys) = x :. y :.. ys
 	nodes _ = error "never occur"
 
 sample0, sample1, sample2, sample3, sample4 :: RangeL 2 12 Int
