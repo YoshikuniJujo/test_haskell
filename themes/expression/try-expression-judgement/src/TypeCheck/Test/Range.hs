a02a50e65296e2ef3c26aa8642941e8c65bc763a
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-10-24 16:09:00 +0900

    try-expression-judgement: TypeCheck.Test.Range: instance Foldable

--- themes/expression/try-expression-judgement/src/TypeCheck/Test/Range.hs
+++ themes/expression/try-expression-judgement/src/TypeCheck/Test/Range.hs
@@ -22,6 +22,28 @@ data RangeL :: Nat -> Nat -> * -> * where
 
 deriving instance Show a => Show (RangeL n m a)
 
+instance Foldable (RangeL 0 0) where
+	foldr _ z NilL = z
+	foldr _ _ _ = error "never occur"
+	foldl _ z NilL = z
+	foldl _ _ _ = error "never occur"
+
+instance {-# OVERLAPPABLE #-}
+	Foldable (RangeL 0 (m - 1)) => Foldable (RangeL 0 m) where
+	foldr _ z NilL = z
+	foldr (-<) z (x :.. xs) = x -< foldr (-<) z xs
+	foldr _ _ _ = error "never occur"
+	foldl _ z NilL = z
+	foldl (>-) z (x :.. xs) = foldl (>-) (z >- x) xs
+	foldl _ _ _ = error "never occur"
+
+instance {-# OVERLAPPABLE #-}
+	Foldable (RangeL (n - 1) (m - 1)) => Foldable (RangeL n m) where
+	foldr (-<) z (x :. xs) = x -< foldr (-<) z xs
+	foldr _ _ _ = error "never occur"
+	foldl (>-) z (x :. xs) = foldl (>-) (z >- x) xs
+	foldl _ _ _ = error "never occur"
+
 infixr 5 .:..
 
 class PushL n m where
@@ -46,6 +68,28 @@ data RangeR :: Nat -> Nat -> * -> * where
 	(:+) :: RangeR (n - 1) (m - 1) a -> a -> RangeR n m a
 
 deriving instance Show a => Show (RangeR n m a)
+
+instance Foldable (RangeR 0 0) where
+	foldr _ z NilR = z
+	foldr _ _ _ = error "never occur"
+	foldl _ z NilR = z
+	foldl _ _ _ = error "never occur"
+
+instance {-# OVERLAPPABLE #-}
+	Foldable (RangeR 0 (m - 1)) => Foldable (RangeR 0 m) where
+	foldr _ z NilR = z
+	foldr (-<) z (xs :++ x) = foldr (-<) (x -< z) xs
+	foldr _ _ _ = error "never occur"
+	foldl _ z NilR = z
+	foldl (>-) z (xs :++ x) = foldl (>-) z xs >- x
+	foldl _ _ _ = error "never occur"
+
+instance {-# OVERLAPPABLE #-}
+	Foldable (RangeR (n - 1) (m - 1)) => Foldable (RangeR n m) where
+	foldr (-<) z (xs :+ x) = foldr (-<) (x -< z) xs
+	foldr _ _ _ = error "never occur"
+	foldl (>-) z (xs :+ x) = foldl (>-) z xs >- x
+	foldl _ _ _ = error "never occur"
 
 infixl 5 .:++
 
