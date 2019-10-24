f6fe1268eba7991f95664df3ddc1bbdf219ee1cb
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-10-24 16:44:33 +0900

    try-expression-judgement: FingerTree: push item

--- themes/expression/try-expression-judgement/src/TypeCheck/Test/FingerTree.hs
+++ themes/expression/try-expression-judgement/src/TypeCheck/Test/FingerTree.hs
@@ -47,6 +47,47 @@ instance Foldable FingerTree where
 		(>-..) :: forall t t' . (Foldable t, Foldable t') => b -> t (t' a) -> b
 		(>-..) = reducel (>-.)
 
+infixr 5 <||, <|, <|.
+
+(<||) :: a -> DigitL a -> Either (DigitL a) (DigitL a, Node a)
+a <|| b :. NilL = Left $ a :. b :.. NilL
+a <|| b :. c :.. NilL = Left $ a :. b :.. c :.. NilL
+a <|| b :. c :.. d :.. NilL = Left $ a :. b :.. c :.. d :.. NilL
+a <|| b :. c :.. d :.. e :.. NilL = Right (a :. b :.. NilL, c :. d :. e :.. NilL)
+_ <|| _ = error "never occur"
+
+(<|) :: a -> FingerTree a -> FingerTree a
+a <| Empty = Single a
+a <| Single b = Deep (a :. NilL) Empty (NilR :+ b)
+a <| Deep pr m sf = case a <|| pr of
+	Left pr' -> Deep pr' m sf
+	Right (pr', n3) -> Deep pr' (n3 <| m) sf
+
+(<|.) :: Foldable t => t a -> FingerTree a -> FingerTree a
+(<|.) = reducer (<|)
+
+infixl 5 ||>, |>, |>.
+
+(||>) :: DigitR a -> a -> Either (DigitR a) (Node a, DigitR a)
+NilR :+ a ||> b = Left $ NilR :++ a :+ b
+NilR :++ a :+ b ||> c = Left $ NilR :++ a :++ b :+ c
+NilR :++ a :++ b :+ c ||> d = Left $ NilR :++ a :++ b :++ c :+ d
+NilR :++ a :++ b :++ c :+ d ||> e = Right (a :. b :. c :.. NilL, NilR :++ d :+ e)
+_ ||> _ = error "never occur"
+
+(|>) :: FingerTree a -> a -> FingerTree a
+Empty |> a = Single a
+Single a |> b = Deep (a :. NilL) Empty (NilR :+ b)
+Deep pr m sf |> a = case sf ||> a of
+	Left sf' -> Deep pr m sf'
+	Right (n3, sf') -> Deep pr (m |> n3) sf'
+
+(|>.) :: Foldable t => FingerTree a -> t a -> FingerTree a
+(|>.) = reducel (|>)
+
+toTree :: Foldable t => t a -> FingerTree a
+toTree = (<|. Empty)
+
 class Nodes m m' where nodes :: RangeL 2 m a -> RangeL 1 m' (Node a)
 
 instance Nodes 3 1 where nodes = (:. NilL)
