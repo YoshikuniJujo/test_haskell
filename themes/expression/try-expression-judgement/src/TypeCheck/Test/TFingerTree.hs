2712e8fff084ae65807ee17221c39a0f0df3ceb3
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-10-31 12:45:13 +0900

    try-expression-judgement: instance TFoldable TFingerTree

--- themes/expression/try-expression-judgement/src/TypeCheck/Test/TFingerTree.hs
+++ themes/expression/try-expression-judgement/src/TypeCheck/Test/TFingerTree.hs
@@ -1,13 +1,46 @@
+{-# LANGUAGE ScopedTypeVariables, RankNTypes, InstanceSigs #-}
+{-# LANGUAGE GADTs, DataKinds, KindSignatures #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module TypeCheck.Test.TFingerTree where
 
-{-
+import TypeCheck.Test.TFoldable
+import TypeCheck.Test.TRange
+
 data TFingerTree :: (* -> * -> *) -> * -> * -> * where
 	TEmpty :: TFingerTree c x x
 	TSingle :: c x y -> TFingerTree c x y
 	TDeep :: TDigitL c x y ->
 		TFingerTree (TNode c) y z -> TDigitR c z w -> TFingerTree c x w
 
-type TDigitL = TRange
--}
+type TDigitL = TRangeL 1 4
+type TDigitR = TRangeR 1 4
+type TNode = TRangeL 2 3
+
+instance TFoldable TFingerTree where
+	tfoldr :: forall c d x' y' z' .
+		(forall x y z . c x y -> d y z -> d x z) ->
+			d y' z' -> TFingerTree c x' y' -> d x' z'
+	tfoldr _ z TEmpty = z
+	tfoldr (-<) z (TSingle x) = x -< z
+	tfoldr (-<) z (TDeep pr m sf) = pr -<. (m -<.. (sf -<. z))
+		where
+		(-<.) :: forall t x y z .
+			TFoldable t => t c x y -> d y z -> d x z
+		(-<.) = treducer (-<)
+		(-<..) :: forall t t' x y z .  (TFoldable t, TFoldable t') =>
+			t (t' c) x y -> d y z -> d x z
+		(-<..) = treducer (-<.)
+	tfoldl :: forall c d x' y' z' .
+		(forall x y z . d x y -> c y z -> d x z) ->
+			d x' y' -> TFingerTree c y' z' -> d x' z'
+	tfoldl _ z TEmpty = z
+	tfoldl (>-) z (TSingle x) = z >- x
+	tfoldl (>-) z (TDeep pr m sf) = ((z >-. pr) >-.. m) >-. sf
+		where
+		(>-.) :: forall t x y z .
+			TFoldable t => d x y -> t c y z -> d x z
+		(>-.) = treducel (>-)
+		(>-..) :: forall t t' x y z . (TFoldable t, TFoldable t') =>
+			d x y -> t (t' c) y z -> d x z
+		(>-..) = treducel (>-.)
