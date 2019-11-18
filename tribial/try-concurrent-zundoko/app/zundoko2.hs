c46b0036af13ddfcd5a5bed18d0fc554439f635e
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-11-18 17:42:41 +0900

    try-concurrent-zundoko: zundoko2: refactoring

--- tribial/try-concurrent-zundoko/app/zundoko2.hs
+++ tribial/try-concurrent-zundoko/app/zundoko2.hs
@@ -9,15 +9,18 @@ import Control.Monad.Tips (doWhile)
 import Control.Concurrent (forkIO, threadDelay)
 import Control.Concurrent.STM.TVar (
 	TVar, newTVar, readTVar, writeTVar, modifyTVar )
-import Data.Bool (bool)
 import Data.RTQueue (RTQueue, empty, snoc, uncons, cons)
 import System.Random (randomRIO)
 
 data ZunDoko = Zun | Doko | Bye deriving Show
 data Kiyoshi
 	= Timeout | Pending
-	| Kiyoshi [ZunDoko] | NotKiyoshi [ZunDoko] (RTQueue ZunDoko)
-	| ZunOverflow ZunDoko (RTQueue ZunDoko)
+	| UntimelyDoko [ZunDoko] (RTQueue ZunDoko)
+	| ZunOverflow (RTQueue ZunDoko)
+	| Kiyoshi [ZunDoko]
+	deriving Show
+data Action =
+	Retry | Exit [ZunDoko] String | Continue [ZunDoko] (RTQueue ZunDoko)
 	deriving Show
 
 say :: ZunDoko -> String
@@ -29,15 +32,13 @@ main = do
 	void . forkIO . forever $ ruffle >> atomically (enqueue q Zun)
 	void . forkIO . forever $ ruffle >> atomically (enqueue q Doko)
 	void . forkIO $ delay 50000 >> atomically (modifyTVar q $ cons Bye)
-	(putStrLn . bool "<タイムアウト>" "キ・ヨ・シ!" =<<) . doWhile $ do
-		r <- atomically $ kiyoshiCheck 0 <$> readTVar q >>= \case
-			Timeout -> pure Nothing
-			Pending -> retry
-			NotKiyoshi zds q' -> Just (False, zds) <$ writeTVar q q'
-			ZunOverflow zun q' -> Just (False, [zun]) <$ writeTVar q q'
-			Kiyoshi zds -> pure $ Just (True, zds)
-		($ r) . maybe (pure $ Just False) $ \(k, z) ->
-			bool Nothing (Just True) k <$ (putStrLn . say) `mapM_` z
+	(putStrLn =<<) . doWhile $ do
+		(r, zds) <-
+			atomically $ (action . check 0) <$> readTVar q >>= \case
+				Retry -> retry
+				Exit z msg -> pure (Just msg, z)
+				Continue z q' -> (Nothing, z) <$ writeTVar q q'
+		r <$ (putStrLn . say) `mapM_` zds
 
 enqueue :: TVar (RTQueue a) -> a -> STM ()
 enqueue q x = modifyTVar q (`snoc` x)
@@ -48,17 +49,24 @@ ruffle = randomRIO (1, 1000) >>= delay
 delay :: Int -> IO ()
 delay = threadDelay . (* 100)
 
-kiyoshiCheck :: Int -> RTQueue ZunDoko -> Kiyoshi
-kiyoshiCheck n q = case uncons q of
+check :: Int -> RTQueue ZunDoko -> Kiyoshi
+check n q = case uncons q of
 	Nothing -> Pending
 	Just (Bye, _) -> Timeout
-	Just (Zun, zd)
-		| n < 4 -> case kiyoshiCheck (n + 1) zd of
-			NotKiyoshi zds zd' -> NotKiyoshi (Zun : zds) zd'
+	Just (Zun, q')
+		| n < 4 -> case check (n + 1) q' of
+			UntimelyDoko zds q'' -> UntimelyDoko (Zun : zds) q''
+			ZunOverflow q'' -> ZunOverflow $ cons Zun q''
 			Kiyoshi zds -> Kiyoshi (Zun : zds)
-			ZunOverflow zun zd' -> ZunOverflow Zun (cons zun zd')
 			k -> k
-		| otherwise -> ZunOverflow Zun zd
-	Just (Doko, zd)
-		| n < 4 -> NotKiyoshi [Doko] zd
+		| otherwise -> ZunOverflow q'
+	Just (Doko, q')
+		| n < 4 -> UntimelyDoko [Doko] q'
 		| otherwise -> Kiyoshi [Doko]
+
+action :: Kiyoshi -> Action
+action Timeout = Exit [] "<タイムアウト>"
+action Pending = Retry
+action (UntimelyDoko zds q) = Continue zds q
+action (ZunOverflow q) = Continue [Zun] q
+action (Kiyoshi zds) = Exit zds "キ・ヨ・シ!"
