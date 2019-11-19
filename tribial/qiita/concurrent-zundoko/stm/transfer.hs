cd3ea3e6a4fc7d3e5611ef6ac6556ec36ecc0d1c
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-11-19 15:01:14 +0900

    concurrent zundoko: STM: transfer

---
+++ tribial/qiita/concurrent-zundoko/stm/transfer.hs
@@ -0,0 +1,47 @@
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+import Control.Monad
+import Control.Monad.STM
+import Control.Concurrent
+import Control.Concurrent.STM
+
+type Account = TVar Int
+
+newAccount :: Int -> STM Account
+newAccount = newTVar
+
+withdraw :: Account -> Int -> STM ()
+withdraw acc amnt = do
+	bl <- readTVar acc
+	writeTVar acc $ bl - amnt
+
+deposit :: Account -> Int -> STM ()
+deposit acc amnt = do
+	bl <- readTVar acc
+	writeTVar acc $ bl + amnt
+
+inquiry :: Account -> STM Int
+inquiry = readTVar
+
+inquiryAll :: [Account] -> IO Int
+inquiryAll accs = atomically $ sum <$> (inquiry `mapM` accs)
+
+transfer :: Account -> Account -> Int -> IO ()
+transfer a b amnt = atomically $ withdraw a amnt >> deposit b amnt
+
+try :: IO ()
+try = do
+	a <- atomically $ newAccount 800
+	b <- atomically $ newAccount 300
+	void . forkIO $ transfer a b 500
+	inquiryAll [a, b] >>= print
+
+noDeadlock :: IO ()
+noDeadlock = do
+	a <- atomically $ newAccount 800
+	b <- atomically $ newAccount 300
+	void . forkIO $ transfer a b 500
+	transfer b a 200
+	atomically ((,) <$> inquiry a <*> inquiry b) >>= print
+	threadDelay 500000
+	atomically ((,) <$> inquiry a <*> inquiry b) >>= print
