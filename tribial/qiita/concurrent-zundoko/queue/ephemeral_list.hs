cc61098fa7c09daeed4bfef5bd143a76eb70e9da
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-11-20 14:11:27 +0900

    concurrent zundoko: ephemeral list

---
+++ tribial/qiita/concurrent-zundoko/queue/ephemeral_list.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE LambdaCase #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+import Control.Concurrent.STM
+
+type List a = TVar (ConsVar a)
+type ConsVar a = TVar (Cons a)
+data Cons a = Nil | Cons a (ConsVar a)
+
+newList :: STM (List a)
+newList = newTVar =<< newTVar Nil
+
+cons :: a -> List a -> STM ()
+cons x l = writeTVar l =<< newTVar . Cons x =<< readTVar l
+
+consAll :: [a] -> List a -> STM ()
+consAll xs l = (`cons` l) `mapM_` xs
+
+makeList :: [a] -> STM (List a)
+makeList xs = (>>) <$> consAll xs <*> pure =<< newList
+
+breakList :: List a -> STM [a]
+breakList = (bl =<<) . readTVar
+	where
+	bl l = readTVar l >>= \case Nil -> pure []; Cons x t -> (x :) <$> bl t
---
+++ tribial/qiita/concurrent-zundoko/queue/ephemeral_list2.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE LambdaCase #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+import Prelude hiding (head)
+
+import Control.Concurrent.STM
+
+type List a = TVar (Cons a)
+data Cons a = Nil | Cons a (List a)
+
+newList :: STM (List a)
+newList = newTVar Nil
+
+cons :: a -> List a -> STM ()
+cons x l = writeTVar l . Cons x =<< newTVar =<< readTVar l
+
+head :: List a -> STM a
+head l = readTVar l >>= \case
+	Nil -> retry
+	Cons x t -> x <$ (writeTVar l =<< readTVar t)
+
+consAll :: [a] -> List a -> STM ()
+consAll xs l = (`cons` l) `mapM_` xs
+
+makeList :: [a] -> STM (List a)
+makeList xs = (>>) <$> consAll xs <*> pure =<< newList
+
+breakList :: List a -> STM [a]
+breakList l =
+	readTVar l >>= \case Nil -> pure []; Cons x t -> (x :) <$> breakList t
