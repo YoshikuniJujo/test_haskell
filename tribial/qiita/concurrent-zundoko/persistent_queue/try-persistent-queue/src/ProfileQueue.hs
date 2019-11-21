d97379063936a18368d3645a01f65eec208f492a
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-11-21 13:06:52 +0900

    concurrent zundoko: ProfileQueue: refactoring

--- tribial/qiita/concurrent-zundoko/persistent_queue/try-persistent-queue/src/ProfileQueue.hs
+++ tribial/qiita/concurrent-zundoko/persistent_queue/try-persistent-queue/src/ProfileQueue.hs
@@ -1,8 +1,8 @@
-{-# LANGUAGE ScopedTypeVariables, TypeApplications, KindSignatures, AllowAmbiguousTypes #-}
+{-# LANGUAGE ScopedTypeVariables, KindSignatures #-}
+{-# LANGUAGE TypeApplications, AllowAmbiguousTypes #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
--- module ProfileQueue (profileWhole) where
-module ProfileQueue where
+module ProfileQueue (profileWhole) where
 
 import Data.List (unfoldr)
 
@@ -10,17 +10,14 @@ import Queue
 
 profileWhole :: forall q . Queue q => Int -> IO ()
 profileWhole n = do
-	putStrLn $ "make " ++ show n ++ " and " ++ show (n * 10) ++ " elem queues"
+	putStrLn $
+		"make " ++ show n ++ " and " ++
+		show (n * 10) ++ " elem queues"
 	print . sum $ {-# SCC "BUILD_AND_BREAK_N_ELEM_QUEUE" #-}
 		unfoldr uncons ({-# SCC "BUILD_N_ELEM_QUEUE" #-} queueN @q n)
 	print . sum $ {-# SCC "BUILD_AND_BREAK_N*10_ELEM_QUEUE" #-}
-		unfoldr uncons ({-# SCC "BUILD_N*10_ELEM_QUEUE" #-} queueN @q (n * 10))
+		unfoldr uncons ({-# SCC "BUILD_N*10_ELEM_QUEUE" #-}
+			queueN @q (n * 10))
 
 queueN :: Queue q => Int -> q Int
 queueN n = snocAll empty [1 .. n]
-
-queue1000 :: Queue q => q Int
-queue1000 = queueN 1000
-
-queue10000 :: Queue q => q Int
-queue10000 = queueN 10000
