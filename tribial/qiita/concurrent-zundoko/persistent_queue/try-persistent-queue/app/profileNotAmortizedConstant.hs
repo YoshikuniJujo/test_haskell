f246d14438198714ce450519d581a135f199eb07
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-11-21 16:27:33 +0900

    concurrent zundoko: BatchedQueue: not amortized constant

--- tribial/qiita/concurrent-zundoko/persistent_queue/try-persistent-queue/app/profileNotAmortizedConstant.hs
+++ tribial/qiita/concurrent-zundoko/persistent_queue/try-persistent-queue/app/profileNotAmortizedConstant.hs
@@ -1,9 +1,22 @@
+{-# LANGUAGE TypeApplications #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Main where
+
+import Prelude hiding (head)
 
 import ProfileQueue
 import BatchedQueue
 
 main :: IO ()
-main = putStrLn "Slozsoft"
+main = do
+	{-# SCC "2000_ELEM_QUEUE" #-} trial 2000
+	{-# SCC "4000_ELEM_QUEUE" #-} trial 4000
+
+trial :: Int -> IO ()
+trial n = print
+	$ sum <$> sequence (timesEvaluate n head $ queueN @BatchedQueue n)
+
+timesEvaluate :: Int -> (a -> b) -> a -> [b]
+timesEvaluate n _ _ | n < 1 = []
+timesEvaluate n f x = f x : timesEvaluate (n - 1) f x
