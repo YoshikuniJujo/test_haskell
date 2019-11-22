a16bef3152b77c2fb11f2f538dc8386944541a0a
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-11-22 09:11:07 +0900

    concurrent zundoko: BatchedQueue: use original defined reverse

--- tribial/qiita/concurrent-zundoko/persistent_queue/try-persistent-queue/src/BatchedQueue.hs
+++ tribial/qiita/concurrent-zundoko/persistent_queue/try-persistent-queue/src/BatchedQueue.hs
@@ -15,7 +15,13 @@ instance Queue BatchedQueue where
 	snoc (BatchedQueue f r) x = BatchedQueue f (x : r)
 	uncons (BatchedQueue [] []) = Nothing
 	uncons (BatchedQueue (x : f) r) = Just (x, BatchedQueue f r)
-	uncons (BatchedQueue [] r) = uncons $ BatchedQueue (reverse r) []
+	uncons (BatchedQueue [] r) = uncons $ BatchedQueue (rev r) []
 
 instance ConsQueue BatchedQueue where
 	cons x (BatchedQueue f r) = BatchedQueue (x : f) r
+
+rev :: [a] -> [a]
+rev = rv []
+	where
+	rv s [] = s
+	rv s (x : xs) = rv (x : s) xs
