a32e696c33b1502572fb0be9d42364c4f7bbae2c
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-11-25 16:41:10 +0900

    try-persistent-queue: showLazyList

--- tribial/qiita/concurrent-zundoko/persistent_queue/try-persistent-queue/src/ShowLazyList.hs
+++ tribial/qiita/concurrent-zundoko/persistent_queue/try-persistent-queue/src/ShowLazyList.hs
@@ -1,8 +1,20 @@
+{-# LANGUAGE LambdaCase #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module ShowLazyList where
 
-import GHC.Exts.Heap (ClosureType(..), getClosureData, tipe, info)
+import GHC.Exts.Heap (GenClosure(..), getClosureData)
+import Control.Arrow
+import Data.Bool
 
-getTipe :: a -> IO ClosureType
-getTipe = (tipe . info <$>) . getClosureData
+isEvaluated :: a -> IO Bool
+isEvaluated x = (<$> getClosureData x) $ \case
+	ConstrClosure {} -> True
+	BlackholeClosure {} -> True
+	_ -> False
+
+showLazyList :: Show a => [a] -> IO (Bool, [String])
+showLazyList xs = (=<< isEvaluated xs) . bool (return (False, []))
+	$ case xs of
+		[] -> return (True, [])
+		x : xs' -> second (show x :) <$> showLazyList xs'
--- tribial/qiita/concurrent-zundoko/persistent_queue/try-persistent-queue/src/ValueSamples.hs
+++
@@ -1,6 +0,0 @@
-{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
-
-module ValueSamples where
-
-sampleValue1 :: Int
-sampleValue1 = 123 + 321
