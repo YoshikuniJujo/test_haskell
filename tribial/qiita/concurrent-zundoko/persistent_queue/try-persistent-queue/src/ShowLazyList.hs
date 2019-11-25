3ece914f9aa8b8eed3c423cbf6d7be7dc3522296
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-11-25 16:43:21 +0900

    try-persistent-queue: showLazyList: refactoring

--- tribial/qiita/concurrent-zundoko/persistent_queue/try-persistent-queue/src/ShowLazyList.hs
+++ tribial/qiita/concurrent-zundoko/persistent_queue/try-persistent-queue/src/ShowLazyList.hs
@@ -1,11 +1,11 @@
 {-# LANGUAGE LambdaCase #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module ShowLazyList where
+module ShowLazyList (showLazyList) where
 
 import GHC.Exts.Heap (GenClosure(..), getClosureData)
-import Control.Arrow
-import Data.Bool
+import Control.Arrow (second)
+import Data.Bool (bool)
 
 isEvaluated :: a -> IO Bool
 isEvaluated x = (<$> getClosureData x) $ \case
@@ -14,7 +14,7 @@ isEvaluated x = (<$> getClosureData x) $ \case
 	_ -> False
 
 showLazyList :: Show a => [a] -> IO (Bool, [String])
-showLazyList xs = (=<< isEvaluated xs) . bool (return (False, []))
+showLazyList xs = (=<< isEvaluated xs) . bool (pure (False, []))
 	$ case xs of
-		[] -> return (True, [])
+		[] -> pure (True, [])
 		x : xs' -> second (show x :) <$> showLazyList xs'
