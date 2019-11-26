18f14c0d65d44c3881e7219e70f93dcfc3368ab0
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-11-26 10:36:32 +0900

    try-persistent-queue: Banker's Queue: refactoring

--- tribial/qiita/concurrent-zundoko/persistent_queue/try-persistent-queue/src/BankersQueue.hs
+++ tribial/qiita/concurrent-zundoko/persistent_queue/try-persistent-queue/src/BankersQueue.hs
@@ -1,39 +1,41 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module BankersQueue (BankersQueue, empty, snoc, uncons, head, tail) where
+module BankersQueue (BankersQueue, empty, snoc, uncons, cons, head, tail) where
 
 import Prelude hiding (head, tail)
 
-import Data.Bool
+import Data.Bool (bool)
 import Data.List (intercalate)
-import System.IO.Unsafe
+import System.IO.Unsafe (unsafePerformIO)
 
-import Queue
-import ShowLazyList
+import Queue (Queue(..), ConsQueue(..), head, tail)
+import ShowLazyList (showLazyList)
 
 data BankersQueue a = BankersQueue Int [a] Int ![a]
 
 instance Queue BankersQueue where
 	empty = BankersQueue 0 [] 0 []
 	snoc (BankersQueue lf f lr r) x
-		| lf <= lr = BankersQueue (lf + lr + 1) (f ++ reverse (x : r)) 0 []
+		| lf <= lr =
+			BankersQueue (lf + lr + 1) (f ++ reverse (x : r)) 0 []
 		| otherwise = BankersQueue lf f (lr + 1) (x : r)
 	uncons (BankersQueue 0 _ 0 _) = Nothing
 	uncons (BankersQueue lf (x : f) lr r)
-		| lf <= lr = Just (x, BankersQueue (lf + lr - 1) (f ++ reverse r) 0 [])
+		| lf <= lr = Just
+			(x, BankersQueue (lf + lr - 1) (f ++ reverse r) 0 [])
 		| otherwise = Just (x, BankersQueue (lf - 1) f lr r)
 	uncons (BankersQueue _ [] _ _) = error "never occur"
 
+instance ConsQueue BankersQueue where
+	cons x (BankersQueue lf f lr r) = BankersQueue (lf + 1) (x : f) lr r
+
 showBankersQueue :: Show a => BankersQueue a -> IO String
 showBankersQueue (BankersQueue _ f _ r) = do
-	(b1, sf) <- showLazyList f
-	(b2, sr) <- showLazyList r
-	pure $ "BankersQueue [" ++ intercalate "," sf ++ bool mc ".." (not (b1 && b2)) ++ intercalate "," (reverse sr) ++ "]"
-	where
-	mc = case (f, r) of
-		([], _) -> "|"
-		(_, []) -> "|"
-		_ -> "|"
+	(ef, sf) <- showLazyList f
+	(er, sr) <- showLazyList r
+	pure $ "BankersQueue [" ++ intercalate "," sf ++
+		bool "|" ".." (not (ef && er)) ++
+		intercalate "," (reverse sr) ++ "]"
 
 instance Show a => Show (BankersQueue a) where
 	show = unsafePerformIO . showBankersQueue
