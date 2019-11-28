15494d19df9c6cf39a25217f2f3f4339e5581a1b
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-11-28 09:25:26 +0900

    try-pearls-of-functional-algorithm-design: module Ch2ASurpassingProblem: remove warnings

--- themes/books/pearls_of_functional_algorithm_design/try-pearls-of-functional-algorithm-design/src/Ch2ASurpassingProblem.hs
+++ themes/books/pearls_of_functional_algorithm_design/try-pearls-of-functional-algorithm-design/src/Ch2ASurpassingProblem.hs
@@ -2,7 +2,7 @@
 
 module Ch2ASurpassingProblem where
 
-import Data.List
+-- import Data.List
 
 msc :: Ord a => [a] -> Int
 -- msc xs = maximum [scount z zs | z : zs <- tails xs]
@@ -20,8 +20,10 @@ table xs = join (m - n) (table ys) (table zs)
 	n = m `div` 2
 	(ys, zs) = splitAt n xs
 
+join :: (Num a1, Ord a2, Eq a1) => a1 -> [(a2, a1)] -> [(a2, a1)] -> [(a2, a1)]
 join 0 txs [] = txs
 join _ [] tys = tys
 join n txs@((x, c) : txs') tys@((y, d) : tys')
 	| x < y = (x, c + n) : join n txs' tys
 	| x >= y = (y, d) : join (n - 1) txs tys'
+join _ _ _ = error "Oops"
