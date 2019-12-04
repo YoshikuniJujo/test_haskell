85655d1adcfea8b3aaee5b6c9287d204a44c19bf
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2019-12-04 15:34:07 +0900

    Qiita: Concurrent Zundoko: concurrent-zundoko

---
+++ tribial/qiita/concurrent-zundoko/concurrent_zundoko/concurrent-zundoko/src/ZunDoko.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TypeFamilies, DefaultSignatures #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module ZunDoko where
+
+import Control.Arrow
+import Control.Concurrent
+import Control.Concurrent.STM
