c0fb88a85c7b777f27ff983799ae62f1484df141
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2019-12-04 11:56:31 +0900

    Qiita: try-knuth-morris-pratt-algorithm: KmpState

--- tribial/qiita/knuth_morris_pratt_algorithm/try-knuth-morris-pratt-algorithm/src/KnuthMorrisPrattAlgorithm.hs
+++ tribial/qiita/knuth_morris_pratt_algorithm/try-knuth-morris-pratt-algorithm/src/KnuthMorrisPrattAlgorithm.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE LambdaCase #-}
-{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+-- {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs -fno-warn-unused-top-binds #-}
 
-module KnuthMorrisPrattAlgorithm where
+module KnuthMorrisPrattAlgorithm (
+	matches, KmpState, initialState, nextState, found ) where
 
 data Rep a = Null | Node a (Rep a) (Rep a)
 
@@ -33,4 +35,20 @@ run ws = scanl (step root) root where root = grep root Null ws
 
 matches :: Eq a => [a] -> [a] -> [Int]
 matches ws = (fst <$>) . filter (ok . snd) . zip [0 ..] . run ws
-	where ok = \case Null -> False; Node vs _ _ -> null vs
+
+ok :: Rep [a] -> Bool
+ok = \case Null -> False; Node vs _ _ -> null vs
+
+data KmpState a = KmpState { rootRep :: Rep [a], currentRep :: Rep [a] }
+
+initialState :: Eq a => [a] -> KmpState a
+initialState ws = KmpState root root
+	where root = grep root Null ws
+
+nextState :: Eq a => KmpState a -> a -> KmpState a
+nextState st x = KmpState {
+	rootRep = rootRep st,
+	currentRep = step (rootRep st) (currentRep st) x }
+
+found :: KmpState a -> Bool
+found = ok . currentRep
