59f66bd6ebf9a43396e30c299693eb1c09ebafde
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-12-10 15:19:10 +0900

    Qiita: Concurrent Zundoko: concurrent-zundoko

---
+++ tribial/qiita/concurrent-zundoko/2nd/concurrent-zundoko/src/Printable.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE FlexibleInstances, UndecidableInstances #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Printable where
+
+class Printable a where show' :: a -> IO String
+
+print' :: Printable a => a -> IO ()
+print' x = putStrLn =<< show' x
+
+instance {-# OVERLAPPABLE #-} Show a => Printable a where
+	show' = pure . show
+
+instance Printable a => Printable (Maybe a) where
+	show' (Just x) = ("Just (" ++) . (++ ")") <$> show' x
+	show' Nothing = pure "Nothing"
+
+instance (Printable a, Printable b) => Printable (a, b) where
+	show' (x, y) =
+		(\s t -> "(" ++ s ++ "," ++ t ++ ")") <$> show' x <*> show' y
