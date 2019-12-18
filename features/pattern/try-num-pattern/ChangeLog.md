6d0253be04a1c19569235dceaa1f6c196e23dbdf
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2019-12-18 17:10:13 +0900

    try-num-pattern

---
+++ features/pattern/try-num-pattern/ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for try-num-pattern
+
+## Unreleased changes
---
+++ features/pattern/try-num-pattern/src/Lib.hs
@@ -0,0 +1,22 @@
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Lib where
+
+newtype I = I Integer deriving Show
+
+instance Num I where
+	I m + I n = I $ m + n
+	I m * I n = I $ m * n
+	abs (I n) = I $ abs n
+	signum (I n) = I $ signum n
+	fromInteger = I
+	negate (I n) = I $ negate n
+
+instance Eq I where
+	I 0 == I 0 = False
+	I m == I n = m == n
+
+f :: I -> I
+f 0 = 123
+f 1 = 321
+f n = n
