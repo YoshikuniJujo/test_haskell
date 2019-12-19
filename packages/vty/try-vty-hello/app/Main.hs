58d4403a00b1530c42650a89d4c2d94110942e0d
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2019-12-19 12:13:02 +0900

    try-vty-hello: tetrimino with colors

--- packages/vty/try-vty-hello/app/Main.hs
+++ packages/vty/try-vty-hello/app/Main.hs
@@ -9,13 +9,13 @@ main = do
 	vty <- mkVty =<< standardIOConfig
 	let	line0 = string (defAttr `withForeColor` green) "first line"
 		line1 = string (defAttr `withBackColor` blue) "second line"
-		i = pad 0 1 0 0 $ foldr1 (<|>) (replicate 4 block) <|> space
-		o = block <|> block <|> space <-> block <|> block <|> space
-		s = space <|> block <|> block <|> space <-> block <|> block <|> space
-		z = block <|> block <|> space <-> space <|> block <|> block <|> space
-		j = block <|> space <-> foldl1 (<|>) (replicate 3 block) <|> space
-		l = space <|> space <|> block <|> space <-> foldl1 (<|>) (replicate 3 block) <|> space 
-		t = resize 6 2 $ space <|> block <|> space <-> block <|> block <|> block <|> space
+		i = pad 0 1 0 0 $ foldr1 (<|>) (replicate 4 blockC) <|> space
+		o = blockY <|> blockY <|> space <-> blockY <|> blockY <|> space
+		s = space <|> blockG <|> blockG <|> space <-> blockG <|> blockG <|> space
+		z = blockR <|> blockR <|> space <-> space <|> blockR <|> blockR <|> space
+		j = blockB <|> space <-> foldl1 (<|>) (replicate 3 blockB) <|> space
+		l = space <|> space <|> blockO <|> space <-> foldl1 (<|>) (replicate 3 blockO) <|> space 
+		t = resize 6 2 $ space <|> blockM <|> space <-> blockM <|> blockM <|> blockM <|> space
 	update vty . picForImage $ line0 <-> line1 <->
 		pad 2 1 0 0 i <|> pad 0 1 0 0 o <|> pad 0 1 0 0 s <|> pad 0 1 0 0 z <|>
 		pad 0 1 0 0 j <|> pad 0 1 0 0 l <|> pad 0 1 0 0 t
@@ -23,8 +23,12 @@ main = do
 	shutdown vty
 	print $ "Last event was: " ++ show e
 
-block :: Image
-block = string (defAttr `withBackColor` white) "  "
+blockW, blockC, blockY, blockG, blockR, blockB, blockM, blockO :: Image
+[blockW, blockC, blockY, blockG, blockR, blockB, blockM, blockO] =
+	block <$> [white, cyan, brightYellow, green, red, blue, magenta, white]
+
+block :: Color -> Image
+block c = string (defAttr `withBackColor` c) "  "
 
 space :: Image	
 space = string (defAttr `withBackColor` black) "  "
