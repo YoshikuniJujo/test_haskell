74661ce866bc9a9d95caa24f19baed1c570def32
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2019-12-19 11:54:23 +0900

    try-vty-hello: tetrimino

--- packages/vty/try-vty-hello/app/Main.hs
+++ packages/vty/try-vty-hello/app/Main.hs
@@ -9,7 +9,22 @@ main = do
 	vty <- mkVty =<< standardIOConfig
 	let	line0 = string (defAttr `withForeColor` green) "first line"
 		line1 = string (defAttr `withBackColor` blue) "second line"
-	update vty . picForImage $ line0 <-> line1
+		i = pad 0 1 0 0 $ foldr1 (<|>) (replicate 4 block) <|> space
+		o = block <|> block <|> space <-> block <|> block <|> space
+		s = space <|> block <|> block <|> space <-> block <|> block <|> space
+		z = block <|> block <|> space <-> space <|> block <|> block <|> space
+		j = block <|> space <-> foldl1 (<|>) (replicate 3 block) <|> space
+		l = space <|> space <|> block <|> space <-> foldl1 (<|>) (replicate 3 block) <|> space 
+		t = resize 6 2 $ space <|> block <|> space <-> block <|> block <|> block <|> space
+	update vty . picForImage $ line0 <-> line1 <->
+		pad 2 1 0 0 i <|> pad 0 1 0 0 o <|> pad 0 1 0 0 s <|> pad 0 1 0 0 z <|>
+		pad 0 1 0 0 j <|> pad 0 1 0 0 l <|> pad 0 1 0 0 t
 	e <- nextEvent vty
 	shutdown vty
 	print $ "Last event was: " ++ show e
+
+block :: Image
+block = string (defAttr `withBackColor` white) "  "
+
+space :: Image	
+space = string (defAttr `withBackColor` black) "  "
