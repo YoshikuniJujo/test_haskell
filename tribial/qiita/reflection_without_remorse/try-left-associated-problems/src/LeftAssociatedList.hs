0a3dbc7ca7b5f36c7301416070518f3fcf5e4bb4
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2020-01-07 14:57:01 +0900

    try-left-associated-problems: module LeftAssociatedList: refactoring

--- tribial/qiita/reflection_without_remorse/try-left-associated-problems/src/LeftAssociatedList.hs
+++ tribial/qiita/reflection_without_remorse/try-left-associated-problems/src/LeftAssociatedList.hs
@@ -1,11 +1,16 @@
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
 module LeftAssociatedList where
 
 hello :: String
 hello = "hello"
 
 helloL, helloR :: String
-helloL = {-# SCC "LeftAssociatedHellos" #-} foldl (++.) "" $ replicate 5000 hello
-helloR = {-# SCC "RightAssociatedHellos" #-} foldr (++.) "" $ replicate 5000 hello
+helloL = {-# SCC "LeftAssociatedHellos" #-}
+	foldl (++.) "" $ replicate 5000 hello
+
+helloR = {-# SCC "RightAssociatedHellos" #-}
+	foldr (++.) "" $ replicate 5000 hello
 
 (++.) :: [a] -> [a] -> [a]
 [] ++. ys = ys
