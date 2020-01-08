c4f8cbd7c980a04eca50785f7dc79712ab9201bd
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2020-01-08 11:15:26 +0900

    try-left-associated-problems: module LeftAssociatedWriterMonad: refactoring

--- tribial/qiita/reflection_without_remorse/try-left-associated-problems/src/LeftAssociatedWriterMonad.hs
+++ tribial/qiita/reflection_without_remorse/try-left-associated-problems/src/LeftAssociatedWriterMonad.hs
@@ -10,10 +10,9 @@ getLog :: Writer a -> String
 getLog (Writer _ w) = w
 
 bind :: Writer a -> (a -> Writer b) -> Writer b
-bind (Writer x w) f = let Writer y w' = f x in Writer y (w ++. w')
+Writer x w `bind` f = let Writer y w' = f x in Writer y $ w ++. w'
 
-instance Functor Writer where
-	f `fmap` Writer x w = Writer (f x) w
+instance Functor Writer where f `fmap` Writer x w = Writer (f x) w
 
 instance Applicative Writer where
 	pure = (`Writer` "")
@@ -28,8 +27,11 @@ sampleFun :: () -> Writer ()
 sampleFun = const sampleWriter
 
 sampleL, sampleR :: () -> Writer ()
-sampleL = {-# SCC "LeftAssociatedHellos" #-} foldl (>=>) pure $ replicate 8000 sampleFun
-sampleR = {-# SCC "WriteAssoociatedHellos" #-} foldr (>=>) pure $ replicate 8000 sampleFun
+sampleL = {-# SCC "LeftAssociatedHellos" #-}
+	foldl (>=>) pure $ replicate 8000 sampleFun
+
+sampleR = {-# SCC "RightAssoociatedHellos" #-}
+	foldr (>=>) pure $ replicate 8000 sampleFun
 
 (++.) :: [a] -> [a] -> [a]
 [] ++. ys = ys
