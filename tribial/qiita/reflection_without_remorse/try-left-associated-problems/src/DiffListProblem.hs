f86060fc0279965daa53a6970d9cafd7b2a86b11
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2020-01-17 17:44:27 +0900

    try-left-associated-problems: DiffListProblem

---
+++ tribial/qiita/reflection_without_remorse/try-left-associated-problems/src/DiffListProblem.hs
@@ -0,0 +1,15 @@
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module DiffListProblem where
+
+import Control.Arrow
+
+import DifferenceList
+
+par :: [a] -> [a] -> ([(a, a)], ([a], [a]))
+par xs [] = ([], (xs, []))
+par [] ys = ([], ([], ys))
+par (x : xs) (y : ys) = ((x, y) :) `first` par xs ys
+
+diffPar :: DiffList a -> DiffList a -> (DiffList (a, a), (DiffList a, DiffList a))
+diffPar xs ys = toDiffList *** (toDiffList *** toDiffList) $ par (fromDiffList xs) (fromDiffList ys)
