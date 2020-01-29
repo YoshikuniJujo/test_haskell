008c838b46c893d63e819410ffee920db8ba03a0
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-01-29 11:43:29 +0900

    try-x11: simple vibration

--- themes/gui/x11/try-x11/app/move.hs
+++ themes/gui/x11/try-x11/app/move.hs
@@ -13,9 +13,10 @@ main :: IO ()
 main = do
 	f <- openField "動くよ" [exposureMask]
 	forkIO $ for_ [0 ..] \x -> do
-		fillRect f 0x00ff00 (100 + 5 * (x `mod` 40)) 100 300 200
+--		fillRect f 0x00ff00 (100 + 5 * (x `mod` 40)) 100 300 200
+		fillRect f 0x00ff00 (100 + vibration x) 100 300 200
 		flushField f
-		threadDelay 50000
+		threadDelay 10000
 		clearField f
 	while $ withNextEvent f \case
 		DestroyWindowEvent {} -> False <$ closeField f
@@ -25,3 +26,9 @@ main = do
 
 while :: Monad m => m Bool -> m ()
 while act = (`when` while act) =<< act
+
+vibration :: Position -> Position
+vibration t_ = round . vb $ fromIntegral t_
+	where
+	vb :: Double -> Double
+	vb t = 8 * cos (t / 12)
