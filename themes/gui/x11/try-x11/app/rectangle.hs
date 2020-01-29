3c96dd5c0455323c2b2a39fada78561208901e95
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-01-29 18:00:55 +0900

    try-x11: new rectangle: draw red rectangle

--- themes/gui/x11/try-x11/app/rectangle.hs
+++ themes/gui/x11/try-x11/app/rectangle.hs
@@ -1,4 +1,69 @@
+{-# LANGUAGE LambdaCase, BlockArguments #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
 module Main where
 
+import Control.Monad
+import Control.Concurrent
+import Control.Concurrent.STM
+
+import Field
+import ButtonEvent
+import Rectangle
+
 main :: IO ()
-main = putStrLn "Slozsoft"
+main = do
+	fl <- atomically $ newTVar False
+	rcts <- atomically $ newTVar []
+	f <- openField "長方形" [
+		exposureMask,
+		buttonPressMask, buttonReleaseMask, button1MotionMask ]
+	void . forkIO $ loop do
+		atomically $ check =<< readTVar fl <* writeTVar fl False
+		clearField f
+		(>>) <$> drawRects f <*> print =<< atomically (readTVar rcts)
+		flushField f
+	while $ withNextEvent f \case
+		DestroyWindowEvent {} -> False <$ closeField f
+		ExposeEvent {} -> True <$ flushField f
+		ev@ButtonEvent {} -> True <$ case buttonEvent ev of
+			Just BtnEvent {
+				buttonNumber = Button1,
+				pressOrRelease = Press,
+				position = (x, y) } -> atomically do
+				modifyTVar rcts $ pushRectangle (fromIntegral x, fromIntegral y)
+			e -> print e
+		ev@MotionEvent {} -> True <$ case buttonEvent ev of
+			Just BtnEvent {
+				buttonNumber = ButtonX,
+				pressOrRelease = Move,
+				position = (x, y) } -> atomically do
+				modifyTVar rcts $ dragRectangle (fromIntegral x, fromIntegral y)
+				writeTVar fl True
+			e -> print e
+		ev	| isDeleteEvent f ev -> True <$ destroyField f
+			| otherwise -> print ev >> pure True
+
+while :: Monad m => m Bool -> m ()
+while act = (`when` while act) =<< act
+
+loop :: Monad m => m a -> m ()
+loop act = act >> loop act
+
+drawRects :: Field -> [Rectangle] -> IO ()
+drawRects f rs = drawRect f `mapM_` reverse rs
+
+drawRect :: Field -> Rectangle -> IO ()
+drawRect f r = fillRect f (color c) l u w h
+	where
+	((l_, u_, w_, h_), c) = calcRectangle 0 0 r
+	(l, u) = (fromIntegral l_, fromIntegral u_)
+	(w, h) = (fromIntegral w_, fromIntegral h_)
+
+color :: Color -> Pixel
+color Red = 0xff0000
+color Green = 0x00ff00
+color Blue = 0x0000ff
+color Yellow = 0xffff00
+color Cyan = 0x00ffff
+color Magenta = 0xff00ff
---
+++ themes/gui/x11/try-x11/src/Rectangle.hs
@@ -0,0 +1,61 @@
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Rectangle (
+	Rectangle, Color(..),
+	calcRectangle, pushRectangle, dragRectangle, rotColor, sink1, remove1
+	) where
+
+import Data.Bool
+import Numeric.Natural
+
+data Rectangle = Rectangle {
+	left :: Integer,
+	upper :: Integer,
+	right :: Integer,
+	bottom :: Integer,
+	color :: Color,
+	floating :: Bool
+	} deriving Show
+
+data Color = Red | Green | Blue | Yellow | Cyan | Magenta deriving (Show, Enum)
+
+rotate :: Color -> Color
+rotate Magenta = Red
+rotate c = succ c
+
+calcRectangle :: Double -> Double -> Rectangle ->
+	((Integer, Integer, Natural, Natural), Color)
+calcRectangle amp agl Rectangle {
+	left = l, upper = u, right = r, bottom = b, color = c, floating = f } =
+	(	(min l r + bool 0 (round $ amp * sin agl) f, min u b,
+			fromIntegral . abs $ r - l, fromIntegral . abs $ b - u),
+		c)
+
+pushRectangle :: (Integer, Integer) -> [Rectangle] -> [Rectangle]
+pushRectangle (l, u) rs = (: rs) $ Rectangle {
+	left = l, upper = u, right = l, bottom = u,
+	color = Red, floating = True }
+
+dragRectangle :: (Integer, Integer) -> [Rectangle] -> [Rectangle]
+dragRectangle (x, y) (r : rs) = r { right = x, bottom = y } : rs
+
+rotColor :: (Integer, Integer) -> [Rectangle] -> [Rectangle]
+rotColor _ [] = []
+rotColor p (r@Rectangle { floating = True } : rs) | p `inside` r =
+	r { color = rotate $ color r } : rs
+rotColor p (r : rs) = r : rotColor p rs
+
+sink1 :: (Integer, Integer) -> [Rectangle] -> [Rectangle]
+sink1 _ [] = []
+sink1 p (r@Rectangle { floating = True } : rs) | p `inside` r =
+	r { floating = False } : rs
+sink1 p (r : rs) = r : sink1 p rs
+
+remove1 :: (Integer, Integer) -> [Rectangle] -> [Rectangle]
+remove1 _ [] = []
+remove1 p (r : rs) | p `inside` r = rs | otherwise = r : remove1 p rs
+
+inside :: (Integer, Integer) -> Rectangle -> Bool
+inside (x, y) Rectangle { left = l, upper = u, right = r, bottom = b } =
+	(l <= x && x <= r || r <= x && x <= l) &&
+	(u <= y && y <= b || b <= y && y <= u)
