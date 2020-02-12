df03f8bfdb81530707724dc2c1807c6b67e7411f
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-02-12 10:46:35 +0900

    try-unique-identifier: UniqueMonad

---
+++ themes/unique_identifier/try-unique-identifier/src/UseUniqueMonadIO.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE BlockArguments #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module UseUniqueMonadIO where
+
+import System.IO.Unsafe
+
+import UniqueMonad
+
+add123 :: Int -> IO Int
+add123 n = do
+	putStrLn $ show n ++ " + 123 = " ++ show n'
+	pure n'
+	where
+	n' = n + 123
+
+mkAdd123 :: Int -> IO Int
+mkAdd123 n = unsafePerformIO do
+	putStrLn "make monad"
+	pure $ add123 n
+
+sample :: CountT s IO (Int, Int)
+sample = do
+	f <- mkMonad mkAdd123
+	ff <- par f f
+	apply ff 321
+
+sample2 :: CountT s IO (Int, Int)
+sample2 = do
+	f <- mkMonad mkAdd123
+	g <- mkMonad mkAdd123
+	fg <- par f g
+	apply fg 321
