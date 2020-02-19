917141c4db2af9a7ab6f5b98c9210ef8ba9e85ff
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2020-02-19 18:01:17 +0900

    try-tab-to-space: search directories recursively

--- other_language/purescript/try-tab-to-space/src/Lib.hs
+++ other_language/purescript/try-tab-to-space/src/Lib.hs
@@ -3,10 +3,21 @@
 module Lib where
 
 import Control.Arrow
+import Control.Monad
+import Data.List
 import System.Directory
 import System.FilePath
 
 purstAndPurs :: FilePath -> IO [(FilePath, FilePath)]
 purstAndPurs d = do
-	map (((d </>) . (<.> "purst") &&& (d </>) . (<.> "purs")) . dropExtension)
+	fps <- map (((d </>) . (<.> "purst") &&& (d </>) . (<.> "purs")) . dropExtension)
 		. filter (isExtensionOf "purst") <$> getDirectoryContents d
+	(fps ++) <$> (purstAndPursDirs =<< getSubDirectories d)
+
+purstAndPursDirs :: [FilePath] -> IO [(FilePath, FilePath)]
+purstAndPursDirs ds = concat <$> purstAndPurs `mapM` ds
+
+getSubDirectories :: FilePath -> IO [FilePath]
+getSubDirectories d = do
+	fps <- map (d </>) . filter (not . ("." `isPrefixOf`)) <$> getDirectoryContents d
+	filterM doesDirectoryExist fps
