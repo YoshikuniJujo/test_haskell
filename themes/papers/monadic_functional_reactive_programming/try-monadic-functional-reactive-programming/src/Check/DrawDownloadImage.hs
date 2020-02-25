89eab646a6eda02b9d8aebddf6f7149932313e5d
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-02-25 17:59:58 +0900

    try-monadic-functional-reactive-programming: separate drawImage from drawDownloadImage

--- themes/papers/monadic_functional_reactive_programming/try-monadic-functional-reactive-programming/src/Check/DrawDownloadImage.hs
+++ themes/papers/monadic_functional_reactive_programming/try-monadic-functional-reactive-programming/src/Check/DrawDownloadImage.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE BlockArguments, LambdaCase #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Check.DrawDownloadImage where
+module Check.DrawDownloadImage (downloadImage, drawImage, Check.DrawDownloadImage.sample) where
 
 import Control.Monad as M
 import Control.Monad.ST
@@ -18,15 +18,16 @@ import Check.DownloadImage
 import Check.Field.DrawImage
 
 drawDownloadImage :: Field -> Request -> IO ()
-drawDownloadImage f rq = downloadImage rq >>= \case
-	Left em -> error em
-	Right img -> do
-		let	w = fromIntegral $ imageWidth img
-			h = fromIntegral $ imageHeight img
-			dt = V.modify swap02s $ imageData img
-		V.unsafeWith (V.unsafeCast dt) \d -> do
-			i <- createImageSimple f d w h
-			putImage f i 100 100 w h
+drawDownloadImage f rq = downloadImage rq >>= either error (drawImage f)
+
+drawImage :: Field -> Image PixelRGBA8 -> IO ()
+drawImage f img = do
+	let	w = fromIntegral $ imageWidth img
+		h = fromIntegral $ imageHeight img
+		dt = V.modify swap02s $ imageData img
+	V.unsafeWith (V.unsafeCast dt) \d -> do
+		i <- createImageSimple f d w h
+		putImage f i 100 100 w h
 
 swap02s :: V.Storable a => V.MVector s a -> ST s ()
 swap02s v = M.zipWithM_ (MV.swap v) [0, 4 .. MV.length v] [2, 6 .. MV.length v]
