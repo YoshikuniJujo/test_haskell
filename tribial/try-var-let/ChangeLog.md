ad588ca61aec1de31dece6caff1d699882c8f780
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-03-12 09:18:38 +0900

    JavaScript var/let problem

---
+++ tribial/try-var-let/ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for try-var-let
+
+## Unreleased changes
---
+++ tribial/try-var-let/src/Lib.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE BlockArguments #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Lib where
+
+import Control.Monad
+import Data.Foldable
+import Control.Concurrent
+import Data.IORef
+
+{-
+
+for (var i = 0; i < 3; i ++) {
+	setTimeout(() => console.log(i), 1000);
+}
+
+-}
+
+act :: IO ()
+act = loop =<< newIORef (0 :: Int)
+	where loop i = do
+		n <- readIORef i
+		when (n < 3) $ do
+			void . forkIO $ do
+				threadDelay 1000
+				print =<< readIORef i
+			modifyIORef i (+ 1)
+			loop i
+
+{-
+
+for (let i = 0; i < 3; i ++) {
+	setTimeout(() => console.log(i), 1000);
+}
+
+-}
+
+act' :: IO ()
+act' = for_ [0 :: Int .. 2] \i -> forkIO $ do
+	threadDelay 1000
+	print i
