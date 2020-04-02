79b830ede1c7fa932f775c874a6b5127bfef38ef
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2020-04-02 15:46:26 +0900

    try-maybe-type-values: Monadic FRP: refactoring: TryMyInterface.TryReact

--- themes/open_union/try-maybe-type-values/src/TryMyInterface/TryReact.hs
+++ themes/open_union/try-maybe-type-values/src/TryMyInterface/TryReact.hs
@@ -4,20 +4,19 @@
 module TryMyInterface.TryReact (
 	tryLeftClick, trySameClick, trySleep, tryDoubler, tryFirstPoint ) where
 
-import Control.Monad.State
-import Data.Time.Clock.System
-import Data.Time.Clock.TAI
+import Control.Monad.State (runStateT)
+import Data.Time.Clock.System (getSystemTime, systemToTAITime)
+import Data.Time.Clock.TAI (AbsoluteTime)
 
-import MonadicFrp.MyInterface
-import TryMyInterface.Boxes
-import TryMyInterface.Boxes.Events
-import TryMyInterface.Boxes.Handlers
-import Field
+import TryMyInterface.Boxes (leftClick, sameClick, doubler, firstPoint)
+import TryMyInterface.Boxes.Handlers (handle, handleWithoutTime)
+import TryMyInterface.Boxes.Events (ReactG, sleep)
+import MonadicFrp.MyInterface (interpret, adjust)
+import Field (Field, openField, closeField, exposureMask, buttonPressMask)
 
 withField :: String -> (Field -> IO a) -> IO a
-withField fn act = do
-	f <- openField fn [exposureMask, buttonPressMask]
-	act f <* closeField f
+withField fn act = (<*) <$> act <*> closeField
+	=<< openField fn [exposureMask, buttonPressMask]
 
 tryLeftClick :: IO ()
 tryLeftClick = withField "tryLeftClick" \f ->
@@ -25,22 +24,22 @@ tryLeftClick = withField "tryLeftClick" \f ->
 
 trySameClick :: IO ()
 trySameClick = withField "trySameClick" \f ->
-	interpret (handleWithoutTime f) sameClick >>= print
+	print =<< interpret (handleWithoutTime f) sameClick
 
 trySleep :: IO ()
 trySleep = withField "trySleep" \f -> do
 	now <- getTAITime
-	interpret (handle 0.5 f) (adjust $ sleep 3) `runStateT` now >>= print
+	print =<< interpret (handle 0.5 f) (adjust $ sleep 3) `runStateT` now
 
 tryDoubler :: IO ()
 tryDoubler = withField "tryDoubler" \f -> do
 	now <- getTAITime
-	interpret (handle 0.05 f) doubler `runStateT` now >>= print
-
-getTAITime :: IO AbsoluteTime
-getTAITime = systemToTAITime <$> getSystemTime
+	print =<< interpret (handle 0.05 f) doubler `runStateT` now
 
 tryFirstPoint :: IO ()
 tryFirstPoint = withField "tryFirstPoint" \f -> do
 	now <- getTAITime
-	interpret (handle 0.05 f) firstPoint `runStateT` now >>= print
+	print =<< interpret (handle 0.05 f) firstPoint `runStateT` now
+
+getTAITime :: IO AbsoluteTime
+getTAITime = systemToTAITime <$> getSystemTime
