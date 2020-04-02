0266f9194e083a1b5185155dc65fa493f2a997bb
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2020-04-02 16:06:18 +0900

    try-maybe-type-values: Monadic FRP: refactoring TryMyInterface.TrySig

--- themes/open_union/try-maybe-type-values/src/TryMyInterface/TrySig.hs
+++ themes/open_union/try-maybe-type-values/src/TryMyInterface/TrySig.hs
@@ -3,26 +3,32 @@
 
 module TryMyInterface.TrySig (
 	tryCycleColor, tryCurRect, tryElapsed, tryWiggleRect, tryCompleteRect,
-	tryDefineRect, tryChooseBoxColor, tryChooseBoxColor', tryBoxes
-	) where
+	tryDefineRect, tryChooseBoxColor, tryChooseBoxColor', tryBoxes ) where
 
-import Control.Monad.State
-import Data.Time
-import Data.Time.Clock.System
+import Control.Monad.State (runStateT, liftIO)
+import Data.Time (DiffTime)
+import Data.Time.Clock.System (getSystemTime, systemToTAITime)
 
-import TryMyInterface.Boxes
-import TryMyInterface.Boxes.Events
-import TryMyInterface.Boxes.Handlers
-import MonadicFrp.MyInterface
-import Field
+import TryMyInterface.Boxes (
+	Box(..), Rect(..), Color(..),
+	cycleColor, curRect, elapsed, wiggleRect,
+	completeRect, defineRect, chooseBoxColor, chooseBoxColor', boxes )
+import TryMyInterface.Boxes.Handlers (handle)
+import TryMyInterface.Boxes.Events (SigG)
+import MonadicFrp.MyInterface (interpretSig)
+import Field (
+	Field, Pixel,
+	openField, closeField, flushField, clearField, fillRect, drawStr,
+	exposureMask, buttonPressMask, buttonReleaseMask, pointerMotionMask )
 
-withInterpretSig :: Show r => String -> (Field -> a -> IO ()) -> SigG a r -> IO ()
+withInterpretSig ::
+	Show r => String -> (Field -> a -> IO ()) -> SigG a r -> IO ()
 withInterpretSig fn op s = do
 	f <- openField fn [
 		exposureMask, buttonPressMask,
 		buttonReleaseMask, pointerMotionMask ]
 	now <- systemToTAITime <$> getSystemTime
-	interpretSig (handle 0.05 f) (liftIO . op f) s `runStateT` now >>= print
+	print =<< interpretSig (handle 0.05 f) (liftIO . op f) s `runStateT` now
 	closeField f
 
 tryCycleColor :: IO ()
@@ -32,24 +38,8 @@ tryCurRect :: IO ()
 tryCurRect = withInterpretSig "tryCurRect"
 	(\f -> withFlush f . drawRect f 0xff0000) $ curRect (300, 200)
 
-withFlush :: Field -> IO () -> IO ()
-withFlush f act = clearField f >> act >> flushField f
-
-drawRect :: Field -> Pixel -> Rect -> IO ()
-drawRect f clr (Rect (l_, u_) (r_, d_)) = fillRect f clr l u w h where
-	l = fromIntegral $ l_ `min` r_
-	u = fromIntegral $ u_ `min` d_
-	w = fromIntegral . abs $ r_ - l_
-	h = fromIntegral . abs $ d_ - u_
-
 tryElapsed :: IO ()
 tryElapsed = withInterpretSig "tryElapsed" drawElapsed elapsed
-
-drawElapsed :: Field -> DiffTime -> IO ()
-drawElapsed f dt = do
-	clearField f
-	drawStr f 0x00ff00 "sans" 30 100 100 $ show dt
-	flushField f
 
 tryWiggleRect :: IO ()
 tryWiggleRect = withInterpretSig "tryWiggleRect"
@@ -58,8 +48,7 @@ tryWiggleRect = withInterpretSig "tryWiggleRect"
 
 tryCompleteRect :: IO ()
 tryCompleteRect = withInterpretSig "tryCompleteRect"
-	(\f -> withFlush f . drawRect f 0xff0000)
-	(completeRect (200, 150))
+	(\f -> withFlush f . drawRect f 0xff0000) (completeRect (200, 150))
 
 tryDefineRect :: IO ()
 tryDefineRect = withInterpretSig "tryDefineRect"
@@ -75,18 +64,27 @@ tryChooseBoxColor' = withInterpretSig "tryChooseBoxColor'"
 	(\f -> withFlush f . drawBox f)
 	(chooseBoxColor' $ Rect (200, 150) (400, 300))
 
+tryBoxes :: IO ()
+tryBoxes = withInterpretSig "tryBoxes"
+	(\f -> withFlush f . (drawBox f `mapM_`) . reverse) boxes
+
+withFlush :: Field -> IO () -> IO ()
+withFlush f act = clearField f >> act >> flushField f
+
+drawElapsed :: Field -> DiffTime -> IO ()
+drawElapsed f dt = withFlush f . drawStr f 0x00ff00 "sans" 30 100 100 $ show dt
+
+drawRect :: Field -> Pixel -> Rect -> IO ()
+drawRect f clr (Rect (l_, u_) (r_, d_)) = fillRect f clr l u w h where
+	l = fromIntegral $ l_ `min` r_
+	u = fromIntegral $ u_ `min` d_
+	w = fromIntegral . abs $ r_ - l_
+	h = fromIntegral . abs $ d_ - u_
+
 drawBox :: Field -> Box -> IO ()
 drawBox f (Box rct clr) = drawRect f (colorToPixel clr) rct
 
 colorToPixel :: Color -> Pixel
 colorToPixel = \case
-	Red -> 0xff0000
-	Green -> 0x00ff00
-	Blue -> 0x0000ff
-	Yellow -> 0xffff00
-	Cyan -> 0xff00ff
-	Magenta -> 0x00ffff
-
-tryBoxes :: IO ()
-tryBoxes = withInterpretSig "tryBoxes"
-	(\f -> withFlush f . (drawBox f `mapM_`) . reverse) boxes
+	Red -> 0xff0000; Green -> 0x00ff00; Blue -> 0x0000ff
+	Yellow -> 0xffff00; Cyan -> 0xff00ff; Magenta -> 0x00ffff
