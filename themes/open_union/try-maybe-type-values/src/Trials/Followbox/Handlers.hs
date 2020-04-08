ad6db8706945d15d1de0ad9e4eb31ed45bbd43bc
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-04-08 14:47:21 +0900

    try-maybe-type-values: Followbox: add GitHub Basic Auth

--- themes/open_union/try-maybe-type-values/src/Trials/Followbox/Handlers.hs
+++ themes/open_union/try-maybe-type-values/src/Trials/Followbox/Handlers.hs
@@ -11,15 +11,20 @@ import qualified Data.ByteString as BS
 import qualified Network.HTTP.Simple as HTTP
 
 import Trials.Followbox.Event
+import Trials.Followbox.BasicAuth
 import MonadicFrp
 
-handle :: EvReqs (Singleton HttpGet) -> IO (EvOccs (Singleton HttpGet))
-handle reqs = case prj reqs of
+handle :: Maybe (BS.ByteString, FilePath) -> EvReqs (Singleton HttpGet) -> IO (EvOccs (Singleton HttpGet))
+handle mba reqs = case prj reqs of
 	Just (HttpGetReq u) -> do
-		r <- HTTP.httpLBS . setUserAgent "Yoshio" $ fromString u
+		r <- hg . setUserAgent "Yoshio" $ fromString u
 		print $ HTTP.getResponseHeader "X-RateLimit-Remaining" r
 		pure . singleton $ OccHttpGet u (HTTP.getResponseHeaders r) (HTTP.getResponseBody r)
 	Nothing -> undefined
+	where
+	hg = case mba of
+		Just (nm, tkn) -> httpBasicAuth nm tkn
+		Nothing -> HTTP.httpLBS
 
 setUserAgent :: BS.ByteString -> HTTP.Request -> HTTP.Request
 setUserAgent ua = HTTP.setRequestHeader "User-Agent" [ua]
--- themes/open_union/try-maybe-type-values/src/Trials/Followbox/Trials.hs
+++ themes/open_union/try-maybe-type-values/src/Trials/Followbox/Trials.hs
@@ -1,14 +1,26 @@
+{-# LANGUAGE BlockArguments, LambdaCase #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Trials.Followbox.Trials where
+
+import System.Environment
+
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BSC
 
 import MonadicFrp
 import Trials.Followbox
 import Trials.Followbox.Event
 import Trials.Followbox.Handlers
 
+getGithubToken :: IO (Maybe (BS.ByteString, FilePath))
+getGithubToken =
+	(<$> getArgs) \case [nm, tkn] -> Just (BSC.pack nm, tkn); _ -> Nothing
+
 tryHttpGet :: IO ()
-tryHttpGet = interpret handle (httpGet "https://api.github.com/users") >>= print
+tryHttpGet = getGithubToken >>= \mba ->
+	interpret (handle mba) (httpGet "https://api.github.com/users") >>= print
 
 tryGetUsersJson :: IO ()
-tryGetUsersJson = interpret handle ((take 3 <$>) <$> getUsersJson) >>= either putStrLn (print `mapM_`)
+tryGetUsersJson = getGithubToken >>= \mba ->
+	interpret (handle mba) ((take 3 <$>) <$> getUsersJson) >>= either putStrLn (print `mapM_`)
