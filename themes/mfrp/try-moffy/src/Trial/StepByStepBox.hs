dbf98101dd49f0807e83a2095ea6ded67d6a316a
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-06-26 09:13:45 +0900

    try-moffy: sameClick

--- themes/mfrp/try-moffy/src/Trial/StepByStepBox.hs
+++ themes/mfrp/try-moffy/src/Trial/StepByStepBox.hs
@@ -2,6 +2,7 @@
 
 module Trial.StepByStepBox where
 
+import Data.Type.Set
 import Moffy.React
 import Moffy.Event.Mouse
 import Moffy.XFieldHandle.Mouse
@@ -12,3 +13,14 @@ tryClick :: IO [MouseBtn]
 tryClick = do
 	f <- openField "TRY CLICK" [buttonPressMask]
 	interpretReact (retry $ handleMouse f) mouseDown <* closeField f
+
+sameClick :: React s (Singleton MouseDown) Bool
+sameClick = do
+	pressed1 <- mouseDown
+	pressed2 <- mouseDown
+	pure $ pressed1 == pressed2
+
+trySameClick :: IO Bool
+trySameClick = do
+	f <- openField "TRY SAME CLICK" [buttonPressMask]
+	interpretReact (retry $ handleMouse f) sameClick <* closeField f
