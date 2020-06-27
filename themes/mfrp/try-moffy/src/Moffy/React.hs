baf8c2ddd33c46014666290e846f20c0a463427f
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2020-06-28 08:51:02 +0900

    try-moffy: interpretReactSt

--- themes/mfrp/try-moffy/src/Moffy/React.hs
+++ themes/mfrp/try-moffy/src/Moffy/React.hs
@@ -15,6 +15,8 @@ import Data.Or
 import Data.Bits
 import Numeric.Natural
 
+import qualified Control.Arrow as A
+
 import Freer
 import FTCQueue
 import TaggableFunction
@@ -57,8 +59,22 @@ runReact ti hdl (Await rqs :>>= c) = runReact ti hdl . (c `qApp`) =<< hdl rqs
 interpretReact :: Monad m => Handle m es -> React s es a -> m a
 interpretReact hdl r = fst <$> runReact rootThreadId hdl r
 
+runReactSt :: Monad m => st -> ThreadId -> HandleSt st m es -> React s es a -> m ((a, ThreadId), st)
+runReactSt st ti _ (Pure x) = pure ((x, ti), st)
+runReactSt _ _ _ (Never :>>= _) = error "never end"
+runReactSt st ti hdl (GetThreadId :>>= c) = runReactSt st ti hdl (c `qApp` ti)
+runReactSt st _ hdl (PutThreadId ti' :>>= c) = runReactSt st ti' hdl (c `qApp` ())
+runReactSt st ti hdl (Await rqs :>>= c) = do
+	(x, st') <- hdl st rqs
+	runReactSt st' ti hdl (c `qApp` x)
+
+interpretReactSt :: Monad m => st -> HandleSt st m es -> React s es a -> m (a, st)
+interpretReactSt st0 hdl r = (fst `A.first`) <$> runReactSt st0 rootThreadId hdl r
+
 type Handle m es = EvReqs es -> m (EvOccs es)
 type Handle' m es = EvReqs es -> m (Maybe (EvOccs es))
+type HandleSt st m es = st -> EvReqs es -> m (EvOccs es, st)
+type HandleSt' st st' m es = st -> EvReqs es -> m (Maybe (EvOccs es), st')
 
 await :: a -> (Occurred a -> b) -> React s (Singleton a) b
 await r f = Await (singleton r) >>>= (pure . f . extract)
