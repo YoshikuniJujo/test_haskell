2e76619f99672fed86151c78585bcb94598ea331
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2020-06-28 09:15:10 +0900

    try-moffy: Moffy.Handle: with state

--- themes/mfrp/try-moffy/src/Moffy/Handle.hs
+++ themes/mfrp/try-moffy/src/Moffy/Handle.hs
@@ -1,15 +1,17 @@
+{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Moffy.Handle where
 
+import Control.Arrow
 import Data.Type.Set
 import Data.OneOrMore hiding (expand, collapse)
 
 import qualified Data.OneOrMore as OOM
 
-import Moffy.React
+import Moffy.React hiding (first)
 
 retry :: Monad m => Handle' m es -> Handle m es
 retry hdl rqs = maybe (retry hdl rqs) pure =<< hdl rqs
@@ -40,3 +42,22 @@ merge :: (
 	Collapsable (es :+: es') es, Collapsable (es :+: es') es' ) =>
 	Handle' m es -> Handle' m es' -> Handle' m (es :+: es')
 merge hdl1 hdl2 rqs = merge' <$> collapse hdl1 rqs <*> collapse hdl2 rqs
+
+retrySt :: Monad m => HandleSt' st st m es -> HandleSt st m es
+retrySt hdl st rqs = hdl st rqs >>= \(mocc, st') ->
+	maybe (retrySt hdl st' rqs) (pure . (, st')) mocc
+
+collapseSt :: (Applicative m, Collapsable es' es) =>
+	HandleSt' st st' m es -> (st -> m st') -> st -> EvReqs es' -> m (Maybe (EvOccs es), st')
+collapseSt hdl ot st = maybe ((Nothing ,) <$> ot st) (hdl st) . OOM.collapse
+
+expandSt :: (Applicative m, Collapsable es' es, ExpandableOccurred es es') =>
+	HandleSt' st st' m es -> (st -> m st') -> HandleSt' st st' m es'
+expandSt hdl o st rqs = first (OOM.expand <$>) <$> collapseSt hdl o st rqs
+
+-- beforeSt :: (Monad m, Beforable es es') =>
+beforeSt hdl1 ot1 hdl2 ot2 st rqs = expandSt hdl1 ot1 st rqs >>= \(mocc, st') ->
+	maybe (expandSt hdl2 ot2 st' rqs) ((<$> ot2 st') . (,) . Just) mocc
+
+mergeSt hdl1 ot1 hdl2 ot2 st rqs = collapseSt hdl1 ot1 st rqs >>= \(mocc1, st') ->
+	first (mocc1 `merge'`) <$> collapseSt hdl2 ot2 st' rqs
