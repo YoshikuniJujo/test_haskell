22257541349f4cb7690f9e9ddd8d5196f6cc2e18
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2020-06-29 13:48:16 +0900

    try-moffy: Moffy.Sig: instance Monad Sig, ISig

--- themes/mfrp/try-moffy/src/Moffy/Sig.hs
+++ themes/mfrp/try-moffy/src/Moffy/Sig.hs
@@ -14,8 +14,27 @@ isig e c = \case End x -> e x; h :| t -> c h t
 
 instance Functor (Sig s es a) where f `fmap` Sig s = Sig $ (f <$>) <$> s
 
+instance Applicative (Sig s es a) where
+	pure = emitAll . pure
+	Sig rf <*> mx = Sig $ isig
+		(unSig . (<$> mx)) (\h -> pure . (h :|) . ((<$> mx) =<<)) =<< rf
+
+instance Monad (Sig s es a) where
+	Sig r >>= f =
+		Sig $ isig (unSig . f) (\h -> pure . (h :|) . (f =<<)) =<< r
+
 instance Functor (ISig s es a) where
 	fmap f = isig (End . f) (\h -> (h :|) . (f <$>))
+
+instance Applicative (ISig s es a) where
+	pure = End
+	mf <*> mx = isig (<$> mx) (\h -> (h :|) . (emitAll . (<$> mx) =<<)) mf
+
+instance Monad (ISig s es a) where
+	m >>= f = isig f (\h -> (h :|) . (emitAll . f =<<)) m
+
+emitAll :: ISig s es a b -> Sig s es a b
+emitAll = Sig . pure
 
 {-
 hold :: Sig s es a r
