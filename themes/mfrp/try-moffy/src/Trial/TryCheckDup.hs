ea19749b758a80d932c104966422e4eca96056de
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2020-07-01 12:17:40 +0900

    try-moffy: tryChe4ckDup

---
+++ themes/mfrp/try-moffy/src/Trial/TryCheckDup.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE BlockArguments #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Trial.TryCheckDup where
+
+import Control.Monad.State
+import Data.Time.Clock.System
+
+import Moffy.React
+import Moffy.React.Common
+import Moffy.Event.Mouse
+import Moffy.XFieldHandle.Mouse
+import Trial.Boxes.Event
+import Trial.Boxes.Handle
+import Trial.StepByStepBox
+import Freer
+import Field
+
+tryCheckDup :: IO ()
+tryCheckDup = do
+	f <- openField "右クリックのあとに左クリックが2回あったら終了" [
+		exposureMask, buttonPressMask, pointerMotionMask ]
+	t <- systemToTAITime <$> getSystemTime
+	(print =<<) . (`runStateT` t) $ runCount $ do
+		cd <- addTag checkDup
+		pure $ interpretReactSt InitMode (handleBoxes 0.05 f) do
+			checkDup `first` checkDup
+			cd `first` cd
+	closeField f
+				
+
+checkDup :: ReactG s ()
+checkDup = doubler >> adjust leftClick
