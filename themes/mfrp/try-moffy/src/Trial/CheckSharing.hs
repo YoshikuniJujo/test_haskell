3da1ef9b5cb11f240cfa371f098ea730ca78feff
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2020-07-01 12:41:34 +0900

    try-moffy: module CheckSharing

---
+++ themes/mfrp/try-moffy/src/Trial/CheckSharing.hs
@@ -0,0 +1,37 @@
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Trial.CheckSharing where
+
+import Control.Concurrent
+import Data.Or
+import System.IO.Unsafe
+
+import Moffy.React
+import Moffy.React.Common
+import Moffy.Handle
+import Moffy.Event.Mouse
+import Moffy.XFieldHandle.Mouse
+import Freer
+import Field
+
+heavy :: () -> Int
+heavy _ = unsafePerformIO $ 123 <$ threadDelay 2000000
+
+heavyReact :: () -> React s MouseEv Int
+heavyReact u = pure $ heavy u
+
+runMouseReact :: React s MouseEv r -> IO r
+runMouseReact r = do
+	f <- openField "RUN REACT" [exposureMask]
+	interpretReact (retry $ handleMouse Nothing f) r
+		<* closeField f
+runSharingHeavy :: IO (Or Int Int)
+runSharingHeavy = do
+	f <- openField "RUN SHARING HEAVY" [exposureMask]
+	r <- runCount $ do
+		hr <- addTag $ heavyReact ()
+		pure $ interpretReact (retry $ handleMouse Nothing f) $ hr `first` hr
+	r <$ closeField f
+
+runNoSharingHeavy :: IO (Or Int Int)
+runNoSharingHeavy = runMouseReact $ heavyReact () `first` heavyReact ()
