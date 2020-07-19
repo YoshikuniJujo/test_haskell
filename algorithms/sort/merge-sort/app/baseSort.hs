dc268b23be40604dd539d6595131852a476311d0
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2020-07-19 18:09:11 +0900

    merge-sort: Original

--- algorithms/sort/merge-sort/app/baseSort.hs
+++ algorithms/sort/merge-sort/app/baseSort.hs
@@ -3,6 +3,9 @@
 module Main where
 
 import Data.List
+import RewriteSort
+import Original
+import BaseSortState
 import ForProfile
 
 import Control.DeepSeq
@@ -18,5 +21,11 @@ main = do
 	evaluate $ special `deepseq` ()
 	print $ last normal
 	print $ last special
-	print . last $ {-# SCC "normal_data" #-} sort normal
-	print . last $ {-# SCC "special_data" #-} sort special
+	print . last $ {-# SCC "normal_data" #-} Data.List.sort normal
+	print . last $ {-# SCC "special_data" #-} Data.List.sort special
+	print . last $ {-# SCC "normal_data_original" #-} Original.sort normal
+	print . last $ {-# SCC "special_data_original" #-} Original.sort special
+	print . last $ {-# SCC "normal_data_rewrite" #-} RewriteSort.sort normal
+	print . last $ {-# SCC "special_data_rewrite" #-} RewriteSort.sort special
+	print . last $ {-# SCC "normal_data_state" #-} baseSort normal
+	print . last $ {-# SCC "special_data_state" #-} baseSort special
