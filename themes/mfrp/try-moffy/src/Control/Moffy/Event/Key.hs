c35e6b415c8af0742237d8323c2b952dc0349be0
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2020-07-20 17:46:31 +0900

    try-moffy: Contgrol.Moffy.Event.Key: AsciiKey

--- themes/mfrp/try-moffy/src/Control/Moffy/Event/Key.hs
+++ themes/mfrp/try-moffy/src/Control/Moffy/Event/Key.hs
@@ -1,9 +1,20 @@
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE PatternSynonyms, ViewPatterns #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Control.Moffy.Event.Key (
+	pattern AsciiKey,
 	module Control.Moffy.Event.Key.XK
 	) where
 
 import Control.Moffy.Event.Key.XK
+import Control.Moffy.Event.Key.Internal
+import Data.Char
+
+pattern AsciiKey :: Char -> Key
+pattern AsciiKey c <- (toAsciiChar -> Just c)
+
+toAsciiChar :: Key -> Maybe Char
+toAsciiChar (Key k)
+	| 0x20 <= k && k <= 0x7e = Just . chr $ fromIntegral k
+	| otherwise = Nothing
