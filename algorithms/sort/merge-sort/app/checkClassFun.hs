19f9cbf92e6b04d2940198634ec586afee015f51
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2020-07-20 16:11:59 +0900

    merge-sort: CheckClassFun

--- algorithms/sort/merge-sort/app/checkClassFun.hs
+++ algorithms/sort/merge-sort/app/checkClassFun.hs
@@ -6,5 +6,5 @@ import CheckClassFun
 
 main :: IO ()
 main = do
-	print . last $ {-# SCC "check_insert" #-} insert (10 ^ (5 :: Int) + 1) [1 .. 10 ^ (5 :: Int) :: Int]
-	print . last $ {-# SCC "check_insert'" #-} insert' (10 ^ (5 :: Int) + 1) [1 .. 10 ^ (5 :: Int) :: Int]
+	print . last $ {-# SCC "check_insert" #-} insert (10 ^ (7 :: Int) + 1) [1 .. 10 ^ (7 :: Int) :: Int]
+	print . last $ {-# SCC "check_insert'" #-} insert' (10 ^ (7 :: Int) + 1) [1 .. 10 ^ (7 :: Int) :: Int]
--- algorithms/sort/merge-sort/src/CheckClassFun.hs
+++ algorithms/sort/merge-sort/src/CheckClassFun.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module CheckClassFun where
@@ -7,14 +8,22 @@ insert = ins
 	where
 	ins x [] = [x]
 	ins x xa@(y : xs)
-		| x <= y = x : xa
+		| x `compare` y /= GT = x : xa
+--		| x <= y = x : xa
 		| otherwise = y : ins x xs
 
-insert' :: Ord a => a -> [a] -> [a]
-insert' = ins
+insert' :: forall a . Ord a => a -> [a] -> [a]
+insert' = insertBy compare
+
+insertBy cmp = ins
 	where
-	(<=.) = (<=)
+--	(<=.) :: Ord a => a -> a -> Bool
+--	(<=.) :: a -> a -> Bool
+--	(<=.) = (<=)
+--	cmp :: a -> a -> Ordering
+--	cmp = compare
 	ins x [] = [x]
 	ins x xa@(y : xs)
-		| x <=. y = x : xa
+		| x `cmp` y /= GT = x : xa
+--		| x <=. y = x : xa
 		| otherwise = y : ins x xs
