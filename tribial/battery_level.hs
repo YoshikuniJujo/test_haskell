e640316c9e84679891230559c086e7ccbb3d88ea
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2020-07-27 13:41:26 +0900

    tribial: battery_level.hs: modify

--- tribial/battery_level.hs
+++ tribial/battery_level.hs
@@ -3,6 +3,18 @@
 module Main where
 
 import Control.Concurrent
+import Data.List
+import System.Directory
+import System.FilePath
+
+isBat :: FilePath -> Bool
+isBat = ("BAT" `isPrefixOf`)
+
+path :: FilePath
+path = "/sys/class/power_supply"
+
+getPath :: IO FilePath
+getPath = (path </>) . head . filter isBat <$> getDirectoryContents path
 
 main :: IO ()
 main = do
@@ -10,10 +22,26 @@ main = do
 	threadDelay 10000000
 	main
 
+isNow :: FilePath -> Bool
+isNow = (||) <$> (== "charge_now") <*> (== "energy_now")
+
+isFull :: FilePath -> Bool
+isFull = (||) <$> (== "charge_full") <*> (== "energy_full")
+
+getNow :: IO FilePath
+getNow = do
+	p <- getPath
+	(p </>) . head . filter isNow <$> getDirectoryContents p
+
+getFull :: IO FilePath
+getFull = do
+	p <- getPath
+	(p </>) . head . filter isFull <$> getDirectoryContents p
+
 run1 :: IO ()
 run1 = do
-	now <- read <$> readFile "/sys/class/power_supply/BAT0/charge_now"
-	full <- read <$> readFile "/sys/class/power_supply/BAT0/charge_full"
+	now <- read <$> (readFile =<< getNow)
+	full <- read <$> (readFile =<< getFull)
 	putStr $ '[' : showGraph 74 (now / full) ++ "] "
 	putStrLn $ show (round $ now / full * 100 :: Int) ++ "%"
 
