e8e89538f67811d95d1cb5617f0ff0e77d845451
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-09-01 17:54:06 +0900

    c/gtk/draw: try to use pixbuf

--- other_language/c/gtk/draw/drawRectangle.c
+++ other_language/c/gtk/draw/drawRectangle.c
@@ -2,13 +2,24 @@
 #include <gtk/gtk.h>
 
 void draw_callback(GtkWidget *, cairo_t *);
+static gint configure_callback(GtkWidget *, GdkEventConfigure *, GdkPixbuf **);
+
+gboolean
+foo (gpointer user_data)
+{
+	printf("hello\n");
+}
 
 int
 main(int argc, char *argv[])
 {
 	GtkWidget *window, *da;
+	GdkPixbuf *pb;
+//	GdkPixmap *pm;
 
 	gtk_init(&argc, &argv);
+
+	g_timeout_add(10000, (GSourceFunc) foo, NULL);
 
 	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
@@ -16,6 +27,9 @@ main(int argc, char *argv[])
 	da = gtk_drawing_area_new();
 	gtk_container_add(GTK_CONTAINER(window), da);
 	g_signal_connect(da, "draw", G_CALLBACK(draw_callback), NULL);
+	g_signal_connect(da, "configure_event", G_CALLBACK(configure_callback), &pb);
+
+//	pm = gdk_pixmap_new(
 
 	gtk_widget_show_all(window);
 
@@ -36,4 +50,13 @@ draw_callback(GtkWidget *da, cairo_t *cr)
 	cairo_rectangle(cr, 50.0, 50.0, 300.0, 200.0);
 	cairo_stroke_preserve(cr);
 	cairo_fill(cr);
+}
+
+static gint
+configure_callback(GtkWidget *widget, GdkEventConfigure *event, GdkPixbuf **pb)
+{
+	printf("configure event occur\n");
+	printf("width: %d\n", event->width);
+	printf("height: %d\n", event->height);
+	*pb = gdk_pixbuf_new(GDK_COLORSPACE_RGB, FALSE, 8, event->width, event->height);
 }
