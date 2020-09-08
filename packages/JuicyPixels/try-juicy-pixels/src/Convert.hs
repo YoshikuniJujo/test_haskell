87337411bc73d07738f882bbce00222f37438147
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-09-08 17:18:11 +0900

    try-juicy-pixels: convert

---
+++ packages/JuicyPixels/try-juicy-pixels/src/Convert.hs
@@ -0,0 +1,18 @@
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Convert where
+
+import Codec.Picture
+
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as LBS
+
+convert :: IO ()
+convert = do
+	eimg <- readImage "../../../files/images/flower.jpg"
+	case eimg of
+		Left err -> error err
+		Right img -> writePng "flower.png" $ convertRGB16 img
+
+convertByteString :: BS.ByteString -> Either String BS.ByteString
+convertByteString jpg = LBS.toStrict <$> (encodeDynamicPng =<< decodeImage jpg)
