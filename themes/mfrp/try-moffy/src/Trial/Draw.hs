7f62a9c77efff21816b9b774085100f368d7cc54
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-09-17 17:37:05 +0900

    try-moffy: draw: draw one line

--- themes/mfrp/try-moffy/src/Trial/Draw.hs
+++ themes/mfrp/try-moffy/src/Trial/Draw.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE TypeOperators #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Trial.Draw where
 
 import Control.Moffy
 import Control.Moffy.Event.Delete
+import Control.Moffy.Event.Mouse
 import Control.Moffy.Viewable.Basic
 import Control.Moffy.Viewable.Shape
 import Data.Type.Set
@@ -17,12 +19,19 @@ import Graphics.Gtk hiding (DeleteEvent)
 
 import Data.OneOfThem
 
-sampleLine :: Sig s (Singleton DeleteEvent) [OneOfThem (Singleton Line)] ()
-sampleLine = do
-	emit [Singleton $ Line' (Color 0 0 0) 2 (50, 50) (200, 200)]
-	waitFor deleteEvent
+maybeEither :: b -> Either a (Maybe b, ()) -> b
+maybeEither d (Left _) = d
+maybeEither d (Right (Nothing, ())) = d
+maybeEither _ (Right (Just x, ())) = x
 
-runDraw :: Monoid a => GtkDrawer a -> Sig s (Singleton DeleteEvent) a r -> IO r
+sampleLine :: Sig s (DeleteEvent :- MouseEv) [OneOfThem (Singleton Line)] ()
+sampleLine = do
+	(emit . (: []) . Singleton =<<) . waitFor $ Line' (Color 0 0 0) 2
+		<$> adjust (maybeEither (0, 0) <$> mousePos `at` leftClick)
+		<*> adjust (maybeEither (0, 0) <$> mousePos `at` leftUp)
+	waitFor $ adjust deleteEvent
+
+runDraw :: Monoid a => GtkDrawer a -> Sig s (DeleteEvent :- MouseEv) a r -> IO r
 runDraw dr s = do
 	([], (cr, c, c')) <- runGtkMain dr []
 	interpret (retry $ handle Nothing cr c) c' s <* gtkMainQuit
