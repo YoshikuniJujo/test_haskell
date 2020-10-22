a46fa66523375d7a1ddd293f88331bbf3cef4711
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2020-10-22 12:19:26 +0900

    try-trochoid: use simple-cairo

--- tribial/try-trochoid/src/Lib.hs
+++ tribial/try-trochoid/src/Lib.hs
@@ -3,14 +3,24 @@
 module Lib where
 
 import Control.Arrow
+import Control.Monad.ST
 import Data.Set
-import Data.Bool
 import Codec.Picture
+
+import Graphics.Cairo.Drawing.CairoT
+import Graphics.Cairo.ImageSurfaces
+import Graphics.Cairo.Paths
+import Graphics.Cairo.Types
+import Graphics.Cairo.Values
+import Graphics.Cairo.PngSupport
 
 dot :: Double -> (Double, Double)
 dot x = (
 	200 * cos x - 120 * sin (18 / 17 * x) + 400,
 	200 * sin x - 120 * cos (18 / 17 * x) + 400 )
+
+dotList :: [(Double, Double)]
+dotList = dot <$> [0, pi / 1024 .. 128 * pi]
 
 dot2 :: Double -> (Double, Double)
 dot2 x = (
@@ -35,3 +45,17 @@ img = generateImage pixel 800 800
 
 writeTrochoid :: IO ()
 writeTrochoid = writePng "tmp.png" img
+
+writeWithCairo :: IO ()
+writeWithCairo = do
+	s <- cairoImageSurfaceCreate cairoFormatArgb32 800 800
+	cr <- cairoCreate s
+	cairoSetSourceRgb cr 0.5 0.5 0
+	polygon cr (head dotList) (tail dotList)
+	cairoStroke cr
+	print =<< cairoSurfaceWriteToPng s "tmp_c.png"
+
+polygon :: CairoT RealWorld -> (Double, Double) -> [(Double, Double)] -> IO ()
+polygon cr (x0, y0) ps = do
+	cairoMoveTo cr x0 y0
+	uncurry (cairoLineTo cr) `mapM_` ps
