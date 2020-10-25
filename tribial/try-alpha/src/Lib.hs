0a4ac2004d7af7056089dd658d00e3c35e518aa8
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2020-10-25 18:32:14 +0900

    try-alpha: add more PNG

--- tribial/try-alpha/src/Lib.hs
+++ tribial/try-alpha/src/Lib.hs
@@ -28,9 +28,24 @@ readPngImageRGB8 fp = readPng fp >>= \case
 
 addAlpha :: PixelRGB8 -> PixelRGBA8
 addAlpha (PixelRGB8 r g b)
---	| b > 0x8f = PixelRGBA8 r g b 0xff
+	| b_ > (r_ * 13 `div` 16) && b_ > (g_ * 13 `div` 16) = PixelRGBA8 r g b 0xff
+--	| r_ > (b_ * 25 `div` 16) && g_ > (b_ * 25 `div` 16) = PixelRGBA8 r g b 0x00
+--	| r > 0x7f && g > 0x7f && b > 0x9f = PixelRGBA8 r g b 0xff
 	| r > 0x6f && g > 0x6f && b > 0x6f = PixelRGBA8 r g b 0x00
 	| otherwise = PixelRGBA8 r g b 0xff
+	where
+	[r_, g_, b_] = fromIntegral <$> [r, g, b] :: [Integer]
 
 convertPngFile :: FilePath -> FilePath -> IO ()
 convertPngFile src dst = writePng dst . pixelMap addAlpha =<< readPngImageRGB8 src
+
+convertPngFile2 :: FilePath -> FilePath -> IO ()
+convertPngFile2 src dst = writePng dst . pixelMap addAlpha2 =<< readPngImageRGB8 src
+
+addAlpha2 :: PixelRGB8 -> PixelRGBA8
+addAlpha2 (PixelRGB8 r g b)
+--	| r_ > (b_ * 24 `div` 16) && g_ > (b_ * 24 `div` 16) = PixelRGBA8 r g b 0xff
+	| r > 0x8f && g > 0x8f && b > 0x8f = PixelRGBA8 r g b 0x00
+	| otherwise = PixelRGBA8 r g b 0xff
+	where
+	[r_, g_, b_] = fromIntegral <$> [r, g, b] :: [Integer]
