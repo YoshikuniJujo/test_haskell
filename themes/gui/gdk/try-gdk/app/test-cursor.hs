9acd2dad2ad4d122c62d6fe91ba0eb16607d8877
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-10-27 14:30:39 +0900

    try-gdk: app/test-cursor.hs: try gdkCursorNewFromSurface

--- themes/gui/gdk/try-gdk/app/test-cursor.hs
+++ themes/gui/gdk/try-gdk/app/test-cursor.hs
@@ -4,6 +4,7 @@
 module Main where
 
 import Control.Monad
+import Control.Monad.Primitive
 import Data.Char
 import System.Environment
 
@@ -12,6 +13,12 @@ import Graphics.Gdk.Windows
 import Graphics.Gdk.Event
 import Graphics.Gdk.Cursors
 import Try.Tools
+
+import Graphics.Cairo.Drawing.CairoT
+import Graphics.Cairo.Paths
+import Graphics.Cairo.ImageSurfaces
+import Graphics.Cairo.Types
+import Graphics.Cairo.Values
 
 main :: IO ()
 main = do
@@ -24,5 +31,26 @@ main = do
 		GdkEventGdkDelete _d -> pure False
 		GdkEventGdkKeyPress k -> do
 			kv <- gdkEventKeyKeyval k
+			when (kv == fromIntegral (ord 'c'))
+				$ gdkWindowSetCursor w =<< (\s -> gdkCursorNewFromSurface d s 15 15) =<< drawCursor
+			when (kv == fromIntegral (ord 'd'))
+				$ gdkWindowSetCursor w =<< gdkCursorNewFromName d "crosshair"
 			pure $ kv /= fromIntegral (ord 'q')
 		e -> True <$ print e
+
+drawCursor :: PrimMonad m => m (CairoSurfaceT (PrimState m))
+drawCursor = do
+	s <- cairoImageSurfaceCreate cairoFormatArgb32 50 50
+	cr <- cairoCreate s
+	cairoSetSourceRgb cr 0 1 0
+	cairoSetLineWidth cr 3
+	cairoMoveTo cr 15 15
+	cairoLineTo cr 15 30
+	cairoStroke cr
+	cairoMoveTo cr 15 15
+	cairoLineTo cr 30 15
+	cairoStroke cr
+	cairoMoveTo cr 15 15
+	cairoLineTo cr 35 35
+	cairoStroke cr
+	pure s
