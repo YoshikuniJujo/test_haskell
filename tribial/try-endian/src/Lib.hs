40f08ec0c2d4efa415b36f20f83ec0547db072ee
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2020-11-20 15:06:33 +0900

    try-endian: word32ToBools and boolsToWord32

--- tribial/try-endian/src/Lib.hs
+++ tribial/try-endian/src/Lib.hs
@@ -7,6 +7,7 @@ import Foreign.Ptr
 import Foreign.Marshal
 import Foreign.Storable
 import Control.Monad
+import Data.Bits
 import Data.Word
 import System.IO.Unsafe
 
@@ -23,3 +24,19 @@ endian32 = unsafePerformIO $ b1234 >>= \case
 	0x04030201 -> pure LittleEndian
 	0x01020304 -> pure BigEndian
 	_ -> pure UnknownEndian
+
+bitIndices32 :: [Int]
+bitIndices32 = case endian32 of
+	LittleEndian -> [0 .. 31]
+	BigEndian -> [31, 30 .. 0]
+	UnknownEndian -> error "UnknownEndian"
+
+word32ToBools :: Word32 -> [Bool]
+word32ToBools w = (w `testBit`) <$> bitIndices32
+
+set :: Bits a => Int -> Bool -> a -> a
+set n False x = x `clearBit` n
+set n True x = x `setBit` n
+
+boolsToWord32 :: [Bool] -> Word32
+boolsToWord32 bs = foldr (.) id (zipWith set bitIndices32 bs) 0
