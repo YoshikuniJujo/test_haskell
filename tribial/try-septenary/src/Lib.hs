d152d35a6fdd22cb837b880f540d021893f2d7f1
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-11-21 22:35:14 +0900

    tribial: try-moffy: add foo

--- tribial/try-septenary/src/Lib.hs
+++ tribial/try-septenary/src/Lib.hs
@@ -11,3 +11,8 @@ septenary x = n : septenary (7 * x')
 unSeptenary :: RealFrac x => [Int] -> x
 unSeptenary [] = 0
 unSeptenary (n : ns) = fromIntegral n + unSeptenary ns / 7
+
+foo 0 = []
+foo x = n : foo (10 * x')
+	where
+	(n, x') = properFraction x
