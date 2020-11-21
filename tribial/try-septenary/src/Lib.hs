c1c9845417a476a8791fb183e2be6144ec4e5ff4
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-11-21 19:35:15 +0900

    tribial: try-septenary

---
+++ tribial/try-septenary/src/Lib.hs
@@ -0,0 +1,13 @@
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Lib where
+
+septenary :: RealFrac x => x -> [Int]
+septenary 0 = []
+septenary x = n : septenary (7 * x')
+	where
+	(n, x') = properFraction x
+
+unSeptenary :: RealFrac x => [Int] -> x
+unSeptenary [] = 0
+unSeptenary (n : ns) = fromIntegral n + unSeptenary ns / 7
