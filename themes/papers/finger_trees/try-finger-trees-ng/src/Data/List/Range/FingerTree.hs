6bdf341c8ae7e8e95ef61f13a22d9f368dc77009
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2020-11-24 16:18:39 +0900

    try-finger-trees-ng: instance Foldable FingerTree

--- themes/papers/finger_trees/try-finger-trees-ng/src/Data/List/Range/FingerTree.hs
+++ themes/papers/finger_trees/try-finger-trees-ng/src/Data/List/Range/FingerTree.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables, InstanceSigs #-}
 {-# LANGUAGE DataKinds #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
@@ -14,3 +15,29 @@ data FingerTree a
 type DigitL = RangeL 1 4
 type DigitR = RangeR 1 4
 type Node = RangeL 2 3
+
+reducer :: Foldable t => (a -> b -> b) -> t a -> b -> b
+reducer = flip . foldr
+
+reducel :: Foldable t => (b -> a -> b) -> b -> t a -> b
+reducel = foldl
+
+instance Foldable FingerTree where
+	foldr :: forall a b . (a -> b -> b) -> b -> FingerTree a -> b
+	foldr _ z Empty = z
+	foldr (-<) z (Single x) = x -< z
+	foldr (-<) z (Deep pr m sf) = pr -<. (m -<.. (sf -<. z))
+		where
+		(-<.) :: forall t . Foldable t => t a -> b -> b
+		(-<.) = reducer (-<)
+		(-<..) :: forall t t' . (Foldable t, Foldable t') => t (t' a) -> b -> b
+		(-<..) = reducer (-<.)
+	foldl :: forall a b . (b -> a -> b) -> b -> FingerTree a -> b
+	foldl _ z Empty = z
+	foldl (>-) z (Single x) = z >- x
+	foldl (>-) z (Deep pr m sf) = ((z >-. pr) >-.. m) >-. sf
+		where
+		(>-.) :: forall t . Foldable t => b -> t a -> b
+		(>-.) = reducel (>-)
+		(>-..) :: forall t t' . (Foldable t, Foldable t') => b -> t (t' a) -> b
+		(>-..) = reducel (>-.)
