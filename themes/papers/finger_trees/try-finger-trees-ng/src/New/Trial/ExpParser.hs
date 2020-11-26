86aa0620fa36a3a71a90c691aed6dc738c9284cc
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-11-26 13:41:29 +0900

    try-finger-trees-ng: New.Trial.ExpParser: parser for trial

--- themes/papers/finger_trees/try-finger-trees-ng/src/New/Trial/ExpParser.hs
+++ themes/papers/finger_trees/try-finger-trees-ng/src/New/Trial/ExpParser.hs
@@ -2,20 +2,68 @@
 
 module New.Trial.ExpParser where
 
+import Data.Maybe
+import Data.List
 import Data.Char
 
 import New.Expression
 
-parse :: String -> Maybe (Exp Char Bool, String)
-parse ('F': cs) = Just (Bool False, cs)
-parse ('T': cs) = Just (Bool True, cs)
+parseBool :: [String] -> Maybe (Exp String Bool, [String])
+parseBool ("F" : ts) = Just (Bool False, ts)
+parseBool ("T" : ts) = Just (Bool True, ts)
+parseBool ("(" : ts) = case parseLeq ts of
+	Just r -> Just r
+	Nothing -> parseEq ts
+parseBool _ = Nothing
 
-parseTerm :: String -> Maybe (Exp Char Term, String)
-parseTerm (c : cs)
-	| isDigit c = Just (Const $ read [c], cs)
-	| isLower c = Just (Var c, cs)
-	{-
-parseTerm ('(' : cs) = let
-	(t, cs') = parseTerm 
-	-}
+parseLeq :: [String] -> Maybe (Exp String Bool, [String])
+parseLeq ts = case parseTerm ts of
+	Just (t1, "<=" : ts') -> case parseTerm ts' of
+		Just (t2, ")" : ts'') -> Just (t1 :<= t2, ts'')
+		_ -> Nothing
+	_ -> Nothing
+
+parseEq :: [String] -> Maybe (Exp String Bool, [String])
+parseEq ts = case parseVar ts of
+	Just (v1, "==" : ts') -> case parseVar ts' of
+		Just (v2, ")" : ts'') -> Just (Var v1 :== Var v2, ts'')
+		_ -> case parseTerm ts' of
+			Just (t2, ")" : ts'') -> Just (Var v1 :== t2, ts'')
+			_ -> case parseBool ts' of
+				Just (b2, ")" : ts'') -> Just (Var v1 :== b2, ts'')
+				_ -> Nothing
+	_ -> case parseTerm ts of
+		Just (t1, "==" : ts') -> case parseTerm ts' of
+			Just (t2, ")" : ts'') -> Just (t1 :== t2, ts'')
+			_ -> Nothing
+		_ -> case parseBool ts of
+			Just (b1, "==" : ts') -> case parseBool ts' of
+				Just (b2, ")" : ts'') -> Just (b1 :== b2, ts'')
+				_ -> Nothing
+			_ -> Nothing
+
+parseVar :: [String] -> Maybe (String, [String])
+parseVar (t : ts)
+	| all isLower t = Just (t, ts)
+parseVar _ = Nothing
+
+parseTerm :: [String] -> Maybe (Exp String Term, [String])
+parseTerm (t : ts)
+	| all isLower t = Just (Var t, ts)
+	| all isDigit t = Just (Const $ read t, ts)
+parseTerm ("(" : ts) = case parseTerm ts of
+	Just (t1, "+" : ts') ->  case parseTerm ts' of
+		Just (t2, ")" : ts'') -> Just (t1 :+ t2, ts'')
+		_ -> Nothing
+	Just (t1, "-" : ts') -> case parseTerm ts' of
+		Just (t2, ")" : ts'') -> Just (t1 :- t2, ts'')
+		_ -> Nothing
+	_ -> Nothing
 parseTerm _ = Nothing
+
+tokens :: String -> [String]
+tokens = unfoldr lex'
+
+lex' :: String -> Maybe (String, String)
+lex' "" = Nothing
+lex' s = listToMaybe $ lex s
