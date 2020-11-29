ab3626dfe03c8ecdb21c868a5dfb24542cc9bb93
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2020-11-29 10:34:57 +0900

    try-finger-trees-ng: Data.List.Range.FingerTree: function app3 and (><)

--- themes/papers/finger_trees/try-finger-trees-ng/src/Data/List/Range/FingerTree.hs
+++ themes/papers/finger_trees/try-finger-trees-ng/src/Data/List/Range/FingerTree.hs
@@ -147,3 +147,14 @@ instance {-# OVERLAPPABLE #-}
 			(nodes (d :. e :. xs :: RangeL 2 (m - 3) a)
 				:: RangeL 1 (mmmmm' - 1) (Node a))
 	nodes _ = error "never occur"
+
+app3 :: forall a . FingerTree a -> RangeL 0 4 a -> FingerTree a -> FingerTree a
+app3 Empty ts xs = ts <|. xs
+app3 xs ts Empty = xs |>. ts
+app3 (Single x) ts xs = x <| (ts <|. xs)
+app3 xs ts (Single x) = (xs |>. ts) |> x
+app3 (Deep pr1 m1 sf1) ts (Deep pr2 m2 sf2) =
+	Deep pr1 (app3 m1 (loosenL (nodes (rightToLeft sf1 ++. ts ++. pr2) :: RangeL 1 4 (Node a))) m2) sf2
+
+(><) :: FingerTree a -> FingerTree a -> FingerTree a
+xs >< ys = app3 xs NilL ys
