72c3cc3280ab52df5a6b0fef97ce53abaf136be5
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-12-02 14:58:16 +0900

    try-finger-trees-ng: Internal.Tools.SampleParser: function calc

--- themes/papers/finger_trees/try-finger-trees-ng/src/Internal/Tools/SampleParser.hs
+++ themes/papers/finger_trees/try-finger-trees-ng/src/Internal/Tools/SampleParser.hs
@@ -9,3 +9,36 @@ import Internal.Tools.Parse
 
 number :: Parse Integer
 number = read <$> some (check isDigit)
+
+spaces1 :: Parse ()
+spaces1 = () <$ some (check isSpace)
+
+spaces :: Parse ()
+spaces = () <$ many (check isSpace)
+
+comma :: Parse ()
+comma = () <$ (spaces >*> char ',' >*> spaces)
+
+sep :: Parse ()
+sep = spaces1 <|> comma
+
+numbers :: Parse [Integer]
+numbers = uncurry (:) <$> (number >*> many (sep *> number))
+
+type Op = Integer -> Integer -> Integer
+
+op, ad, sb, ml, dv :: Parse Op
+op = ad <|> sb <|> ml <|> dv
+ad = (+) <$ char '+'
+sb = (-) <$ char '-'
+ml = (*) <$ char '*'
+dv = div <$ char '/'
+
+expr :: Parse Integer
+expr = (\x o y -> x `o` y) <$> term <*> op <*> term
+
+term :: Parse Integer
+term = number <|> (char '(' *> expr <* char ')')
+
+calc :: String -> Maybe Integer
+calc = parse expr
