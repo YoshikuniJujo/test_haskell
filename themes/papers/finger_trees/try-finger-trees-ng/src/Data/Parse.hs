b00a8436b00941facc5993cb5c77a5d2391704b2
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-12-08 09:18:54 +0900

    try-finger-trees-ng: Data.Parse: refactoring with hlint

--- themes/papers/finger_trees/try-finger-trees-ng/src/Data/Parse.hs
+++ themes/papers/finger_trees/try-finger-trees-ng/src/Data/Parse.hs
@@ -5,27 +5,27 @@ module Data.Parse (Parse(..), (>>!), maybeToParse) where
 
 import Control.Arrow (first)
 import Control.Applicative (Alternative(..))
+import Control.Monad ((>=>))
 
 newtype Parse s a = Parse { runParse :: s -> Maybe (a, s) }
 
 instance Functor (Parse s) where
-	f `fmap` Parse p = Parse \s -> (f `first`) <$> p s
+	f `fmap` Parse p = Parse $ ((f `first`) <$>) . p
 
 instance Applicative (Parse t) where
 	pure x = Parse \s -> Just (x, s)
-	Parse pf <*> mx =
-		Parse \s -> pf s >>= \(f, s') -> (f <$> mx) `runParse` s'
+	Parse pf <*> mx = Parse $ pf  >=> \(f, s') -> (f <$> mx) `runParse` s'
 
 instance Monad (Parse t) where
-	Parse p >>= f = Parse \s -> p s >>= \(x, s') -> f x `runParse` s'
+	Parse p >>= f = Parse $ p >=> \(x, s') -> f x `runParse` s'
 
 instance Alternative (Parse t) where
-	empty = Parse \_ -> Nothing
+	empty = Parse $ const Nothing
 	Parse l <|> Parse r = Parse \s -> l s <|> r s
 
 (>>!) :: Parse s a -> Parse s b -> Parse s a
 Parse p >>! Parse nla =
-	Parse \s -> p s >>= \r@(_, s') -> maybe (pure r) (const empty) $ nla s'
+	Parse $ p >=> \r@(_, s') -> maybe (pure r) (const empty) $ nla s'
 
 maybeToParse :: Maybe a -> Parse s a
 maybeToParse mx = Parse \s -> (, s) <$> mx
