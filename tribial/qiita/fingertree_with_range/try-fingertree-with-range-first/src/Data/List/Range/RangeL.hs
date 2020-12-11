c0a38c6aa2c8971159045be8bcfdfe8d3cfa50ab
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2020-12-11 11:48:30 +0900

    try-finger-tree-with-range-first: Data.List.Range.RangeL

---
+++ tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Data/List/Range/RangeL.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE ScopedTypeVariables, InstanceSigs #-}
+{-# LANGUAGE DataKinds, KindSignatures, TypeOperators #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses, FlexibleContexts, FlexibleInstances,
+	UndecidableInstances #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Data.List.Range.RangeL where
+
+import GHC.TypeNats (Nat, type (+), type (-), type (<=))
+
+infixr 6 :., :..
+
+data RangeL :: Nat -> Nat -> * -> * where
+	NilL :: RangeL 0 m a
+	(:..) :: 1 <= m => a -> RangeL 0 (m - 1) a -> RangeL 0 m a
+	(:.) :: a -> RangeL (n - 1) (m - 1) a -> RangeL n m a
+
+deriving instance Show a => Show (RangeL n m a)
