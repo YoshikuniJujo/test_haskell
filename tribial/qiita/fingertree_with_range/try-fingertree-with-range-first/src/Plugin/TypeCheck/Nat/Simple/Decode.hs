69eac086d29d9f8cfff72d9c1810d3ed978cb952
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-12-12 08:59:35 +0900

    try-fingertree-with-range-first: Plugin.TypeCheck.Nat.Simple.Decode: function exVar

--- tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Plugin/TypeCheck/Nat/Simple/Decode.hs
+++ tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Plugin/TypeCheck/Nat/Simple/Decode.hs
@@ -3,8 +3,16 @@
 
 module Plugin.TypeCheck.Nat.Simple.Decode where
 
+import GhcPlugins (Var)
+import TyCoRep
 import Outputable (Outputable(..), SDoc, (<+>), text)
+import Control.Monad.Trans.Except
 import Data.String
+
+import Data.Derivation.Expression
+
+exVar :: Type -> Except Message (Exp Var a)
+exVar = \case TyVarTy v -> pure $ Var v; _ -> throwE "exVar: fail"
 
 newtype Message = Message SDoc
 instance Semigroup Message where Message l <> Message r = Message $ l <+> r
