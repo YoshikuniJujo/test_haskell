7f3eda956a02f033ab263f6bd6d50c4049a385dd
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-12-15 09:33:24 +0900

    try-fingertree-with-range-first: Data.Derivation.Constraint: type Polynomial

---
+++ tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Data/Derivation/Constraint.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE BlockArguments, LambdaCase #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Data.Derivation.Constraint where
+
+import Prelude hiding (null, filter, (<>))
+
+import Outputable (Outputable(..), (<>), (<+>), text)
+import Control.Monad
+import Data.Foldable
+import Data.Maybe
+import Data.Map.Strict (Map, null, singleton, (!?), filter, lookupMin)
+import Data.Map.Merge.Strict
+
+import qualified Data.Map.Strict as M
+
+type Polynomial v = Map (Maybe v) Integer
