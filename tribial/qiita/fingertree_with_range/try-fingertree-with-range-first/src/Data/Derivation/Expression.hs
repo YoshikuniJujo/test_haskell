d43b75af9f3f163a16b1bf4e5a2996e1bf740d02
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-12-15 11:16:06 +0900

    try-fingertree-with-range-first: Data.Derivation.Expression: mkVarBool

--- tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Data/Derivation/Expression.hs
+++ tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Data/Derivation/Expression.hs
@@ -11,6 +11,10 @@ import Outputable (Outputable(..), (<>), (<+>), text)
 import Control.Monad.Writer (Writer, runWriter, tell)
 import Data.Map.Strict (Map, (!?), empty, singleton, insert)
 
+import Control.Arrow (first, second)
+import Data.Maybe
+import Data.List (find)
+
 import Data.Derivation.Constraint
 
 data Exp v t where
@@ -42,3 +46,23 @@ poly (Var v) = p <$ tell [p `greatEqualThan` empty]
 poly (l :+ r) = (.+) <$> poly l <*> poly r
 poly (l :- r) = (,) <$> poly l <*> poly r >>= \(pl, pr) ->
 	pl .- pr <$ tell [pl `greatEqualThan` pr]
+
+type VarBool v = Map v Bool
+
+vbInit :: Ord v => [Exp v Bool] -> ([(v, v)], VarBool v)
+vbInit [] = ([], empty)
+vbInit (Var l :== Var r : es) = ((l, r) :) `first` vbInit es
+vbInit (Var l :== Bool r : es) = insert l r `second` vbInit es
+vbInit (Bool l :== Var r : es) = insert r l `second` vbInit es
+vbInit (_ : es) = vbInit es
+
+vbStep :: Ord v => [(v, v)] -> VarBool v -> ([(v, v)], VarBool v)
+vbStep [] vb = ([], vb)
+vbStep ((l, r) : vs) vb = case (vb !? l, vb !? r) of
+	(Just bl, _) -> vbStep vs $ insert r bl vb
+	(Nothing, Just br) -> vbStep vs $ insert l br vb
+	(Nothing, Nothing) -> ((l, r) :) `first` vbStep vs vb
+
+untilFixed :: Eq a => (a -> a) -> a -> a
+untilFixed f x = fst . fromJust . find (uncurry (==)) $ zip xs (tail xs)
+	where xs = iterate f x
