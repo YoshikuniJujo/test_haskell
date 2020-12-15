2f2f08291de332eafe572429628c45cd2ce26d9c
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-12-15 14:19:29 +0900

    try-fingertree-with-range-first: Data.Derivation.CanDerive: Given

--- tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Data/Derivation/CanDerive.hs
+++ tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Data/Derivation/CanDerive.hs
@@ -13,3 +13,15 @@ import Data.Derivation.Constraint (
 import Data.Derivation.Expression
 
 import qualified Data.Derivation.Constraint as C
+
+newtype Given v = Given { unGiven :: [Constraint v] } deriving Show
+
+mkGiven :: Ord v => [Exp v Bool] -> Given v
+mkGiven es = given . concat
+	$ uncurry (maybe id (:)) . mkConstraint (mkVarBool es) <$> es
+
+given :: Ord v => [Constraint v] -> Given v
+given zs = Given . nub . sort $ zs ++ (rmNegative <$> zs)
+
+givenVars :: Ord v => Given v -> [Maybe v]
+givenVars = nub . sort . concat . (getVars <$>) . unGiven
