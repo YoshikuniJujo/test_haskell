aabfcd4ceb39c48fdd79840750b81c71a82d03b0
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-12-15 10:19:28 +0900

    try-fingertree-with-range-first: Data.Derivation.Constraint: constructors of Constraint

--- tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Data/Derivation/Constraint.hs
+++ tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Data/Derivation/Constraint.hs
@@ -27,3 +27,26 @@ rmZero = \case 0 -> Nothing; n -> Just n
 
 data Constraint v = Eq (Polynomial v) | Geq (Polynomial v)
 	deriving (Show, Eq, Ord)
+
+equal :: Ord v => Polynomial v -> Polynomial v -> Constraint v
+l `equal` r = Eq . formatEq $ l .- r
+
+greatEqualThan :: Ord v => Polynomial v -> Polynomial v -> Constraint v
+l `greatEqualThan` r = Geq . formatGeq $ l .- r
+
+greatThan :: Ord v => Polynomial v -> Polynomial v -> Constraint v
+l `greatThan` r = Geq $ formatGeq (l .- r) .- singleton Nothing 1
+
+formatEq :: Polynomial v -> Polynomial v
+formatEq p =
+	maybe p ((p `divide` divisor p `times`) . signum . snd) $ lookupMin p
+
+formatGeq :: Polynomial v -> Polynomial v
+formatGeq p = p `divide` divisor p
+
+times, divide :: Polynomial v -> Integer -> Polynomial v
+p `times` n = (* n) <$> p
+p `divide` n = (`div` n) <$> p
+
+divisor :: Polynomial v -> Integer
+divisor = gcdAll . toList where gcdAll = \case [] -> 1; n : ns -> foldr gcd n ns
