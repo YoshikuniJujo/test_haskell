a9525cee1c84df2ccbaa5abdd9150d9cf9331a89
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-12-15 17:40:35 +0900

    try-fingertree-with-range-first: Data.List.Range.RangeR

---
+++ tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Data/List/Range/RangeR.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE DataKinds, KindSignatures, TypeOperators #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses, FlexibleContexts, FlexibleInstances,
+	UndecidableInstances #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs -fplugin=Plugin.TypeCheck.Nat.Simple #-}
+
+module Data.List.Range.RangeR where
+
+import GHC.TypeLits
