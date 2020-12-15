ec95743a70bb4dfea468a1fa9b042d39bc500483
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-12-15 10:10:20 +0900

    try-fingertree-with-range-first: Data.Derivation.Constraint: operator (.+) and (.-)

--- tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Data/Derivation/Constraint.hs
+++ tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Data/Derivation/Constraint.hs
@@ -16,5 +16,14 @@ import qualified Data.Map.Strict as M
 
 type Polynomial v = Map (Maybe v) Integer
 
+(.+), (.-) :: Ord v => Polynomial v -> Polynomial v -> Polynomial v
+(.+) = merge preserveMissing preserveMissing
+	(zipWithMaybeMatched \_ a b -> rmZero $ a + b)
+(.-) = merge preserveMissing (mapMissing $ const negate)
+	(zipWithMaybeMatched \_ a b -> rmZero $ a - b)
+
+rmZero :: (Eq n, Num n) => n -> Maybe n
+rmZero = \case 0 -> Nothing; n -> Just n
+
 data Constraint v = Eq (Polynomial v) | Geq (Polynomial v)
 	deriving (Show, Eq, Ord)
