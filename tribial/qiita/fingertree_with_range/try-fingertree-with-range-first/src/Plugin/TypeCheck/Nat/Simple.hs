d360d5d7ca91e76addf443cad5dbe658558ffc9b
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-12-15 15:45:53 +0900

    try-fingertree-with-range-first: Plugin.TypeCheck.Nat.Simple: complete function plugin

--- tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Plugin/TypeCheck/Nat/Simple.hs
+++ tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Plugin/TypeCheck/Nat/Simple.hs
@@ -9,7 +9,13 @@ import TcRnTypes
 import Control.Monad
 import Control.Monad.Trans.Except
 
+import Data.Bool
+import Data.Either
+import TcEvidence
+import TyCoRep
+
 import Plugin.TypeCheck.Nat.Simple.Decode
+import Data.Derivation.CanDerive
 
 plugin :: Plugin
 plugin = defaultPlugin { tcPlugin = const $ Just TcPlugin {
@@ -24,7 +30,17 @@ solve gs ds ws = do
 	tcPluginTrace "Given: " . ppr $ runExcept . (uncurry decode <=< unNomEq) <$> gs
 	tcPluginTrace "Derived: " . ppr $ runExcept . (uncurry decode <=< unNomEq) <$> ds
 	tcPluginTrace "Wanted: " . ppr $ runExcept . (uncurry decode <=< unNomEq) <$> ws
-	pure $ TcPluginOk [] []
+	pure $ TcPluginOk (rights $ runExcept . result gs <$> ws) []
+
+result :: [Ct] -> Ct -> Except Message (EvTerm, Ct)
+result gs w = unNomEq w >>= \(l, r) ->
+	bool (throwE em) (pure (et l r, w)) . canDerive g =<< wnt =<< decode l r
+	where
+	em = "result: fail"
+	g = mkGiven . rights $ runExcept . (uncurry decode =<<) . unNomEq <$> gs
+	wnt = maybe (throwE "mkWanted: fail") pure . mkWanted
+	et l r = EvExpr . Coercion $
+		mkUnivCo (PluginProv "Plugin.TypeCheck.Nat.Simple") Nominal l r
 
 unNomEq :: Ct -> Except Message (Type, Type)
 unNomEq ct = case classifyPredType . ctEvPred $ ctEvidence ct of
