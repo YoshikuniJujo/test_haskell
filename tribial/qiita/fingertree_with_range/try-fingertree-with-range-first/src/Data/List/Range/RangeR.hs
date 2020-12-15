55fdaf7907f2adfbdf5f392163e78ebb38d651b0
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-12-15 17:52:17 +0900

    try-fingertree-with-range-first: Data.List.Range.RangeR: RangeR

--- tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Data/List/Range/RangeR.hs
+++ tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Data/List/Range/RangeR.hs
@@ -8,3 +8,12 @@
 module Data.List.Range.RangeR where
 
 import GHC.TypeLits
+
+infixl 6 :+, :++
+
+data RangeR :: Nat -> Nat -> * -> * where
+	NilR :: RangeR 0 m a
+	(:++) :: 1 <= m => RangeR 0 (m - 1) a -> a -> RangeR 0 m a
+	(:+) :: RangeR (n - 1) (m - 1) a -> a -> RangeR n m a
+
+deriving instance Show a => Show (RangeR n m a)
