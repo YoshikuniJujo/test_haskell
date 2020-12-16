5838da40bd40f42fc06c1ae612ee0c105665cd64
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-12-16 14:31:53 +0900

    try-fingertree-with-range-first: Data.FingerTree: instance Foldable FingerTree

--- tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Data/FingerTree.hs
+++ tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Data/FingerTree.hs
@@ -20,3 +20,21 @@ data FingerTree a
 type Node = RangeL 2 3
 type DigitL = RangeL 1 4
 type DigitR = RangeR 1 4
+
+reducer :: Foldable t => (a -> b -> b) -> t a -> b -> b
+reducer = flip . foldr
+
+reducel :: Foldable t => (b -> a -> b) -> b -> t a -> b
+reducel = foldl
+
+instance Foldable FingerTree where
+	foldr :: forall a b . (a -> b -> b) -> b -> FingerTree a -> b
+	foldr _ z Empty = z
+	foldr (-<) z (Single x) = x -< z
+	foldr (-<) z (Deep pr m sf) = pr -<. (m -<.. (sf -<. z))
+		where
+		(-<.) :: forall t . Foldable t => t a -> b -> b
+		(-<.) = reducer (-<)
+		(-<..) :: forall t t' .
+			(Foldable t, Foldable t') => t (t' a) -> b -> b
+		(-<..) = reducer (-<.)
