90e8adb21defb635b3f8c1fcf113e35c7dc450a4
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-12-16 10:00:27 +0900

    try-fingertree-with-range-first: Data.List.Range.RangeR: instance Foldable RangeR

--- tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Data/List/Range/RangeR.hs
+++ tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Data/List/Range/RangeR.hs
@@ -17,3 +17,18 @@ data RangeR :: Nat -> Nat -> * -> * where
 	(:+) :: RangeR (n - 1) (m - 1) a -> a -> RangeR n m a
 
 deriving instance Show a => Show (RangeR n m a)
+
+instance Foldable (RangeR 0 0) where
+	foldr _ z NilR = z
+	foldr _ _ _ = error "never occur"
+
+instance {-# OVERLAPPABLE #-}
+	Foldable (RangeR 0 (m - 1)) => Foldable (RangeR 0 m) where
+	foldr _ z NilR = z
+	foldr (-<) z (xs :++ x) = foldr (-<) (x -< z) xs
+	foldr _ _ _ = error "never occur"
+
+instance {-# OVERLAPPABLE #-}
+	Foldable (RangeR (n - 1) (m - 1)) => Foldable (RangeR n m) where
+	foldr (-<) z (xs :+ x) = foldr (-<) (x -< z) xs
+	foldr _ _ _ = error "never occur"
