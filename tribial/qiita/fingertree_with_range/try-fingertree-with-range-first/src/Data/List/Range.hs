2effd9709fa0fc5f21b5ce25bc1b0097e09d007c
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-12-16 12:56:44 +0900

    try-fingertree-with-range-first: Data.List.Range: leftToRight

--- tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Data/List/Range.hs
+++ tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Data/List/Range.hs
@@ -28,3 +28,24 @@ instance {-# OVERLAPPABLE #-}
 	n ++.+ NilL = loosenRMax n :: RangeR 0 (m + w) a
 	n ++.+ x :.. v = (n :++ x :: RangeR 0 (m + 1) a) ++.+ v
 	_ ++.+ _ = error "never occur"
+
+instance {-# OVERLAPPABLE #-} (
+	LoosenRMax n m (m + w), PushR (n - 1) (m - 1),
+	LeftToRight n (m + 1) 0 (w - 1) ) =>
+	LeftToRight n m 0 w where
+	(++.+) :: forall a . RangeR n m a -> RangeL 0 w a -> RangeR n (m + w) a
+	n ++.+ NilL = loosenRMax n :: RangeR n (m + w) a
+	n ++.+ x :.. v = (n .:++ x :: RangeR n (m + 1) a) ++.+ v
+	_ ++.+ _ = error "never occur"
+
+instance {-# OVERLAPPABLE #-}
+	LeftToRight (n + 1) (m + 1) (v - 1) (w - 1) =>
+	LeftToRight n m v w where
+	(++.+) :: forall a .
+		RangeR n m a -> RangeL v w a -> RangeR (n + v) (m + w) a
+	n ++.+ x :. v = (n :+ x :: RangeR (n + 1) (m + 1) a) ++.+ v
+	_ ++.+ _ = error "never occur"
+
+leftToRight ::
+	forall n m a . LeftToRight 0 0 n m => RangeL n m a -> RangeR n m a
+leftToRight = ((NilR :: RangeR 0 0 a) ++.+)
