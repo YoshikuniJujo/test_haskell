77efc27d660aea8dc021c14f7f2a4f97c39b5914
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-12-16 13:23:43 +0900

    try-fingertree-with-range-first: Data.List.Range: rightToLeft

--- tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Data/List/Range.hs
+++ tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Data/List/Range.hs
@@ -49,3 +49,41 @@ instance {-# OVERLAPPABLE #-}
 leftToRight ::
 	forall n m a . LeftToRight 0 0 n m => RangeL n m a -> RangeR n m a
 leftToRight = ((NilR :: RangeR 0 0 a) ++.+)
+
+infixr 5 ++..
+
+class RightToLeft n m v w where
+	(++..) :: RangeR n m a -> RangeL v w a -> RangeL (n + v) (m + w) a
+
+instance RightToLeft 0 0 0 w where _ ++.. v = v
+
+instance {-# OVERLAPPABLE #-} RightToLeft 0 0 v w where _ ++.. v = v
+
+instance {-# OVERLAPPABLE #-}
+	(LoosenLMax 0 w (m + w), RightToLeft 0 (m - 1) 0 (w + 1)) =>
+	RightToLeft 0 m 0 w where
+	(++..) :: forall a . RangeR 0 m a -> RangeL 0 w a -> RangeL 0 (m + w) a
+	NilR ++.. v = loosenLMax v :: RangeL 0 (m + w) a
+	n :++ x ++.. v = n ++.. (x :.. v :: RangeL 0 (w + 1) a)
+	_ ++.. _ = error "never occur"
+
+instance {-# OVERLAPPABLE #-} (
+	LoosenLMax v w (m + w) , PushL (v - 1) (w - 1),
+	RightToLeft 0 (m - 1) v (w + 1) ) =>
+	RightToLeft 0 m v w where
+	(++..) :: forall a . RangeR 0 m a -> RangeL v w a -> RangeL v (m + w) a
+	NilR ++.. v = loosenLMax v :: RangeL v (m + w) a
+	n :++ x ++.. v = n ++.. (x .:.. v :: RangeL v (w + 1) a)
+	_ ++.. _ = error "never occur"
+
+instance {-# OVERLAPPABLE #-}
+	RightToLeft (n - 1) (m - 1) (v + 1) (w + 1) =>
+	RightToLeft n m v w where
+	(++..) :: forall a .
+		RangeR n m a -> RangeL v w a -> RangeL (n + v) (m + w) a
+	n :+ x ++.. v = n ++.. (x :. v :: RangeL (v + 1) (w + 1) a)
+	_ ++.. _ = error "never occur"
+
+rightToLeft ::
+	forall n m a . RightToLeft n m 0 0 => RangeR n m a -> RangeL n m a
+rightToLeft = (++.. (NilL :: RangeL 0 0 a))
