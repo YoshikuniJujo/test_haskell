379d3bd6cde577562ddea741b3d510c89d50c5ed
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-12-16 14:04:06 +0900

    try-fingertree-with-range-first: Data.FingerTree: data FingerTree

--- tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Data/FingerTree.hs
+++ tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Data/FingerTree.hs
@@ -10,3 +10,13 @@ module Data.FingerTree where
 import GHC.TypeNats
 
 import Data.List.Range
+
+data FingerTree a
+	= Empty
+	| Single a
+	| Deep (DigitL a) (FingerTree (Node a)) (DigitR a)
+	deriving Show
+
+type Node = RangeL 2 3
+type DigitL = RangeL 1 4
+type DigitR = RangeR 1 4
