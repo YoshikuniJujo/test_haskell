52fe4bab97b566a56636b05add792e71134c0a5b
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-12-16 13:48:32 +0900

    try-fingertree-with-range-first: Data.FingerTree

---
+++ tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Data/FingerTree.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE ScopedTypeVariables, InstanceSigs #-}
+{-# LANGUAGE DataKinds, TypeOperators #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE MultiParamTypeClasses, FlexibleContexts, FlexibleInstances,
+	UndecidableInstances #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs -fplugin=Plugin.TypeCheck.Nat.Simple #-}
+
+module Data.FingerTree where
+
+import GHC.TypeNats
+
+import Data.List.Range
