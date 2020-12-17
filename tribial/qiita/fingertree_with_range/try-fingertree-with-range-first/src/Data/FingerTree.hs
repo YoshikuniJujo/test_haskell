48f492d787a42c77d5bcc796ae3c911d7c474889
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2020-12-17 11:36:45 +0900

    try-fingertree-with-range-first: Data.FingerTree: repair class and instance Nodes

--- tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Data/FingerTree.hs
+++ tribial/qiita/fingertree_with_range/try-fingertree-with-range-first/src/Data/FingerTree.hs
@@ -117,23 +117,24 @@ deepR _ _ _ = error "never occur"
 nodeToDigitR :: Node a -> DigitR a
 nodeToDigitR = loosenR . leftToRight
 
-class Nodes m m' where nodes :: RangeL 2 m a -> RangeL 1 m' (Node a)
+class Nodes m w where nodes :: RangeL 3 m a -> RangeL 1 w (Node a)
 
-instance Nodes 3 1 where nodes = (:. NilL)
+instance Nodes 3 1 where nodes = (:. NilL) . loosenL
 
-instance {-# OVERLAPPABLE #-} (2 <= m', Nodes (m - 3) (m' - 1)) =>
-	Nodes m m' where
-	nodes :: forall a . RangeL 2 m a -> RangeL 1 m' (Node a)
-	nodes (a :. b :. NilL) = (a :. b :. NilL) :. NilL
-	nodes (a :. b :. c :.. NilL) = (a :. b :. c :.. NilL) :. NilL
-	nodes (a :. b :. c :.. d :.. NilL) =
+instance {-# OVERLAPPABLE #-} (2 <= w, Nodes (m - 3) (w - 1)) => Nodes m w where
+	nodes :: forall a . RangeL 3 m a -> RangeL 1 w (Node a)
+	nodes (a :. b :. c :. NilL) = (a :. b :. c :.. NilL) :. NilL
+	nodes (a :. b :. c :. d :.. NilL) =
 		(a :. b :. NilL) :. (c :. d :. NilL) :.. NilL
-	nodes (a :. b :. c :.. d :.. e :.. xs) =
+	nodes (a :. b :. c :. d :.. e :.. NilL) =
+		(a :. b :. c :.. NilL) :. (d :. e :. NilL) :.. NilL
+	nodes (a :. b :. c :. d :.. e :.. f :.. xs) =
 		(a :. b :. c :.. NilL) .:..
-			(nodes (d :. e :. xs :: RangeL 2 (m - 3) a)
-				:: RangeL 1 (m' - 1) (Node a))
+			(nodes (d :. e :. f :. xs :: RangeL 3 (m - 3) a)
+				:: RangeL 1 (w - 1) (Node a))
 	nodes _ = error "never occur"
 
+{-
 app3 :: forall a . FingerTree a -> RangeL 0 4 a -> FingerTree a -> FingerTree a
 app3 Empty m xs = m <|. xs
 app3 xs m Empty = xs |>. m
@@ -146,3 +147,4 @@ app3 (Deep pr1 m1 sf1) m (Deep pr2 m2 sf2) = Deep pr1 (app3
 
 (><) :: FingerTree a -> FingerTree a -> FingerTree a
 l >< r = app3 l NilL r
+-}
