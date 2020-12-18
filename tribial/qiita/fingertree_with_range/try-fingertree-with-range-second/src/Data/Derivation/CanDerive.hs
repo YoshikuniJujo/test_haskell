bb00d28a2b78c2ab4c42bd2f0465946e4aa52894
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-12-18 17:06:02 +0900

    try-fingertree-with-range-second: Data.Derivation.CanDerive: Given and Wanted

---
+++ tribial/qiita/fingertree_with_range/try-fingertree-with-range-second/src/Data/Derivation/CanDerive.hs
@@ -0,0 +1,37 @@
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Data.Derivation.CanDerive where
+
+import Control.Arrow (first)
+import Data.Either
+import Data.List ((\\), nub, partition, sort)
+import Data.Map.Strict (empty)
+
+import Data.Derivation.Constraint (
+	Constraint, getVars, hasVar,
+	rmNegative, isDerivFrom, selfContained )
+import Data.Derivation.Expression
+
+import qualified Data.Derivation.Constraint as C
+
+newtype Given v = Given { unGiven :: [Constraint v] } deriving Show
+
+mkGiven :: Ord v => [Exp v Bool] -> Given v
+mkGiven es = given . concat
+	$ uncurry (maybe id (:)) . mkConstraint (mkVarBool es) <$> es
+
+given :: Ord v => [Constraint v] -> Given v
+given zs = Given . nub . sort $ zs ++ (rmNegative <$> zs)
+
+givenVars :: Ord v => Given v -> [Maybe v]
+givenVars = nub . sort . concat . (getVars <$>) . unGiven
+
+newtype Wanted v = Wanted { unWanted :: [Wanted1 v] } deriving Show
+
+type Wanted1 v = Constraint v
+
+mkWanted :: Ord v => Exp v Bool -> Maybe (Wanted v)
+mkWanted = uncurry wanted . mkConstraint empty
+
+wanted :: Maybe (Wanted1 v) -> [Wanted1 v] -> Maybe (Wanted v)
+wanted mw ws = Wanted . (: ws) <$> mw
