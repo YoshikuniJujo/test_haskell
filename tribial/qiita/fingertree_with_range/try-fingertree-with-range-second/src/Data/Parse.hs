c6b08b6c9250ef093bd7f908a8fa8316a5cc6c94
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-12-18 15:22:44 +0900

    try-fingertree-with-range-second: TryParse.Exponential

---
+++ tribial/qiita/fingertree_with_range/try-fingertree-with-range-second/src/Data/Parse.hs
@@ -0,0 +1,13 @@
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Data.Parse (Parse, parse, unparse) where
+
+import Control.Monad.State
+
+type Parse s = StateT s Maybe
+
+parse :: (s -> Maybe (a, s)) -> Parse s a
+parse = StateT
+
+unparse :: Parse s a -> s -> Maybe (a, s)
+unparse = runStateT
---
+++ tribial/qiita/fingertree_with_range/try-fingertree-with-range-second/src/TryParse/Exponential.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE BlockArguments, LambdaCase #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module TryParse.Exponential where
+
+import Control.Applicative
+import Data.Bool
+import Data.List
+import Data.Parse
+
+data Expression = One | Expression :+ Expression | Expression :- Expression
+	deriving Show
+
+pChar :: Parse String Char
+pChar = parse uncons
+
+pick :: Char -> Parse String Char
+pick = check . (==)
+
+check :: (Char -> Bool) -> Parse String Char
+check p = pChar >>= \c -> bool empty (pure c) (p c)
+
+pExpr, pTerm :: Parse String Expression
+pExpr = (:+) <$> pTerm <* pick '+' <*> pExpr <|> pTerm
+pTerm = pick '(' *> pExpr <* pick ')' <|> One <$ pick '1'
