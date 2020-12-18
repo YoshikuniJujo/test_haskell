383384580c3f82816e59dfdcc5f73fc5e622af22
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-12-18 17:20:43 +0900

    try-fingertree-with-range-second: Plugin.TypeCheck.Nat.Simple: complete type check plugin

--- tribial/qiita/fingertree_with_range/try-fingertree-with-range-second/src/Plugin/TypeCheck/Nat/Simple.hs
+++ tribial/qiita/fingertree_with_range/try-fingertree-with-range-second/src/Plugin/TypeCheck/Nat/Simple.hs
@@ -12,6 +12,13 @@ import Control.Monad
 
 import Plugin.TypeCheck.Nat.Simple.Decode
 
+import TcEvidence
+import TyCoRep
+import Data.Bool
+import Data.Either
+
+import Data.Derivation.CanDerive
+
 plugin :: Plugin
 plugin = defaultPlugin { tcPlugin = const $ Just TcPlugin {
 	tcPluginInit = pure (),
@@ -25,9 +32,19 @@ solve gs ds ws = do
 	tcPluginTrace "Given: " . ppr $ runExcept . (uncurry decode <=< unNomEq) <$> gs
 	tcPluginTrace "Derived: " . ppr $ runExcept . (uncurry decode <=< unNomEq) <$> ds
 	tcPluginTrace "Wanted: " . ppr $ runExcept . (uncurry decode <=< unNomEq) <$> ws
-	pure $ TcPluginOk [] []
+	pure $ TcPluginOk (rights $ runExcept . result gs <$> ws) []
 
 unNomEq :: Ct -> Except Message (Type, Type)
 unNomEq ct = case classifyPredType . ctEvPred $ ctEvidence ct of
 	EqPred NomEq l r -> pure (l, r)
 	_ -> throwE "Cannot unNomEq"
+
+result :: [Ct] -> Ct -> Except Message (EvTerm, Ct)
+result gs w = unNomEq w >>= \(l, r) ->
+	bool (throwE em) (pure (et l r, w)) . canDerive g =<< wnt =<< decode l r
+	where
+	em = "result: fail"
+	g = mkGiven . rights $ runExcept . (uncurry decode =<<) . unNomEq <$> gs
+	wnt = maybe (throwE "mkWanted: fail") pure . mkWanted
+	et l r = EvExpr . Coercion $
+		mkUnivCo (PluginProv "Plugin.TypeCheck.Nat.Simple") Nominal l r
