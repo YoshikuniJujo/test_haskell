5efb6404dee749f03bdce03dd0cda1f9a2ad8be5
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-12-19 19:10:01 +0900

    try-fingertree-with-range-second: Data.List.Range.RangeR

---
+++ tribial/qiita/fingertree_with_range/try-fingertree-with-range-second/src/Data/List/Range/RangeR.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE DataKinds, KindSignatures, TypeOperators #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses, FlexibleContexts, FlexibleInstances,
+	UndecidableInstances #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs -fplugin=Plugin.TypeCheck.Nat.Simple #-}
+
+module Data.List.Range.RangeR where
+
+import GHC.TypeLits
+
+infixl 6 :+, :++
+
+data RangeR :: Nat -> Nat -> * -> * where
+	NilR :: RangeR 0 m a
+	(:++) :: 1 <= m => RangeR 0 (m - 1) a -> a -> RangeR 0 m a
+	(:+) :: RangeR (n - 1) (m - 1) a -> a -> RangeR n m a
+
+deriving instance Show a => Show (RangeR n m a)
+
+instance Foldable (RangeR 0 0) where
+	foldr _ z NilR = z
+	foldr _ _ _ = error "never occur"
+
+instance {-# OVERLAPPABLE #-}
+	Foldable (RangeR 0 (m - 1)) => Foldable (RangeR 0 m) where
+	foldr _ z NilR = z
+	foldr (-<) z (xs :++ x) = foldr (-<) (x -< z) xs
+	foldr _ _ _ = error "never occur"
+
+instance {-# OVERLAPPABLE #-}
+	Foldable (RangeR (n - 1) (m - 1)) => Foldable (RangeR n m) where
+	foldr (-<) z (xs :+ x) = foldr (-<) (x -< z) xs
+	foldr _ _ _ = error "never occur"
