cca0127eaf0eee79b1992eeb5785a1422980aa15
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2020-12-24 04:42:02 +0900

    try-bits-to-word32-endian: module BitsAndWord32: word32ToBitsLittleE

--- themes/hardware/try-bits-to-word32-endian/memo.md
+++ themes/hardware/try-bits-to-word32-endian/memo.md
@@ -7,7 +7,7 @@ todo
 * [x] get environment variables in template haskell
 * [x] check endian
 * [x] encode bits to Word32 in little endian
-* [ ] decode bits from Word32 in little endian
+* [x] decode bits from Word32 in little endian
 * [ ] encode bits to Word32 in big endian
 * [ ] decode bits from Word32 in big endian
 * [ ] switch between the endians by environment variable or result of endian check
--- themes/hardware/try-bits-to-word32-endian/src/BitsAndWord32.hs
+++ themes/hardware/try-bits-to-word32-endian/src/BitsAndWord32.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DataKinds #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
@@ -5,14 +6,18 @@ module BitsAndWord32 where
 
 import Data.List.Length
 import Data.Bits
+import Data.Bool
 import Data.Word
 
 data Bit = O | I deriving (Show, Enum)
 
-bitsToWord32 :: LengthL 32 Bit -> Word32
-bitsToWord32 = foldr (\b w -> w `shift` 1 .|. fromIntegral (fromEnum b)) 0
+bitsToWord32LittleE :: LengthL 32 Bit -> Word32
+bitsToWord32LittleE = foldr (\b w -> w `shift` 1 .|. fromIntegral (fromEnum b)) 0
 
 exampleBits :: LengthL 32 Bit
 exampleBits =
 	(repeatL O :: LengthL 8 Bit) ++. (repeatL I :: LengthL 8 Bit) ++.
 	(repeatL O :: LengthL 8 Bit) ++. (repeatL I :: LengthL 8 Bit)
+
+word32ToBitsLittleE :: Word32 -> LengthL 32 Bit
+word32ToBitsLittleE = unfoldr \w -> (bool O I (w `testBit` 0), w `shiftR` 1)
