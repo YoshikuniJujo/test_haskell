4b193ced1abee47091c441ba6afb2525d3c05a71
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2020-12-25 15:16:46 +0900

    tribial-trials: module Zip: zipWith defined with using zipWithM

--- tribial/tribial-trials/src/Zip.hs
+++ tribial/tribial-trials/src/Zip.hs
@@ -2,8 +2,14 @@
 
 module Zip where
 
+import Control.Monad
+import Control.Monad.Identity
+
 zip' :: [a] -> [b] -> [(a, b)]
 zip' = zipWith (,)
 
 zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
 zipWith' f xs ys = uncurry f <$> zip xs ys
+
+zipWith'' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith'' f xs ys = runIdentity $ zipWithM (\x y -> Identity $ f x y) xs ys
