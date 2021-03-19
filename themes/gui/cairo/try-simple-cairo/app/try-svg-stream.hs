58eeb1f3134c847eb3e0e4e7bb79face37db1e64
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-03-19 14:03:32 +0900

    try-simple-cairo: use cairoSvgSurfaceCreateForStream

---
+++ themes/gui/cairo/try-simple-cairo/app/try-svg-stream.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE LambdaCase, OverloadedStrings #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Main where
+
+import Foreign.Ptr
+import Control.Monad.ST
+import Data.STRef
+import Data.Maybe
+import Data.Color
+import Graphics.Cairo.Drawing.CairoT
+import Graphics.Cairo.Surfaces.CairoSurfaceT
+import Graphics.Cairo.Surfaces.SvgSurfaces
+
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+
+main :: IO ()
+main = do
+	sr <- cairoSvgSurfaceCreateForStream (\_ t -> T.putStrLn t >> pure WriteSuccess) nullPtr 128 128
+	cr <- cairoCreate sr
+	cairoSetSourceRgb cr . fromJust $ rgbDouble 0.2 0.6 0.1
+	cairoPaint cr
+--	cairoSurfaceFlush sr
+	cairoSurfaceFinish sr
+	putStrLn "=== ST ==="
+	T.putStr $ runST st
+
+st :: ST s T.Text
+st = do
+	v <- newSTRef ""
+	sr <- cairoSvgSurfaceCreateForStream (\_ t -> modifySTRef v (<> t) >> pure WriteSuccess) nullPtr 128 128
+	cr <- cairoCreate sr
+	cairoSetSourceRgb cr . fromJust $ rgbDouble 0.2 0.6 0.1
+	cairoPaint cr
+	cairoSurfaceFinish sr
+	readSTRef v
