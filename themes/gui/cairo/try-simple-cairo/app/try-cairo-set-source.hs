d9e683c77deafc998b551f6ebabd0504f98464f3
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-03-23 11:36:41 +0900

    try-simple-cairo: app/try-cairo-set-source: try using a function cairoFillWithAlpha

--- themes/gui/cairo/try-simple-cairo/app/try-cairo-set-source.hs
+++ themes/gui/cairo/try-simple-cairo/app/try-cairo-set-source.hs
@@ -17,10 +17,15 @@ main = do
 	sr <- cairoImageSurfaceCreate cairoFormatArgb32 128 128
 	cr <- cairoCreate sr
 	pt <- cairoPatternCreateLinear 96 32 32 96
-	cairoPatternAddColorStopRgb pt 0.2 1 0 0
-	cairoPatternAddColorStopRgb pt 0.8 0 1 0
+	cairoPatternAddColorStopRgb pt 0.2 1.0 0.0 0.0
+	cairoPatternAddColorStopRgb pt 0.8 0.0 0.5 0.5
 	cairoSetSource cr pt
 	cairoPaint cr
+	pt' <- cairoPatternCreateLinear 32 32 96 96
+	cairoPatternAddColorStopRgb pt' 0.2 0.5 0.5 0.0
+	cairoPatternAddColorStopRgb pt' 0.8 0.0 0.0 1.0
+	cairoSetSource cr pt'
+	cairoPaintWithAlpha cr 0.5
 	cairoImageSurfaceGetCairoImage sr >>= \case
 		CairoImageArgb32 ci ->
 			writePng "try-cairo-set-source.png" $ cairoArgb32ToJuicyRGBA8 ci
