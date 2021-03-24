a75b9d7caf41cc1edea9530e8cbe29712cb472e5
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2021-03-24 17:43:51 +0900

    try-simple-cairo: app/try-cairo-operator.hs

---
+++ themes/gui/cairo/try-simple-cairo/app/try-cairo-operator.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE LambdaCase #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Main where
+
+import Control.Monad.Primitive
+import Data.Maybe
+import Data.CairoContext
+import Data.CairoImage
+import Data.JuicyCairo
+import Data.Color
+import Codec.Picture
+import Graphics.Cairo.Drawing.CairoT
+import Graphics.Cairo.Drawing.CairoT.Setting
+import Graphics.Cairo.Drawing.CairoT.CairoOperatorT
+import Graphics.Cairo.Drawing.Paths
+import Graphics.Cairo.Drawing.Transformations
+import Graphics.Cairo.Surfaces.ImageSurfaces
+import Graphics.Cairo.Values
+
+main :: IO ()
+main = do
+	sr <- cairoImageSurfaceCreate cairoFormatArgb32 256 512
+	cr <- cairoCreate sr
+
+	cairoTranslate cr 16 16
+	sample cr OperatorClear
+
+	cairoTranslate cr 0 128
+	sample cr OperatorSource
+
+	cairoTranslate cr 0 128
+	sample cr OperatorOver
+
+	cairoImageSurfaceGetCairoImage sr >>= \case
+		CairoImageArgb32 ci ->
+			writePng "try-cairo-operator.png" $ cairoArgb32ToJuicyRGBA8 ci
+		_ -> error "never occur"
+
+sample :: PrimMonad m => CairoT (PrimState m) -> Operator -> m ()
+sample cr o = do
+	cairoSet cr OperatorOver
+	cairoRectangle cr 0 0 120 90
+	cairoSetSourceRgba cr . fromJust $ rgbaDouble 0.7 0 0 0.8
+	cairoFill cr
+
+	cairoSet cr o
+
+	cairoRectangle cr 40 30 120 90
+	cairoSetSourceRgba cr . fromJust $ rgbaDouble 0 0 0.9 0.4
+	cairoFill cr
