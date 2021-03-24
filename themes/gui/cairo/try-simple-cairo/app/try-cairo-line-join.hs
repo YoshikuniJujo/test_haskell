09f8d145b78b18ee4fd9f1cad6ca396e79e7dd08
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2021-03-24 16:22:41 +0900

    try-simple-cairo: app/try-cairo-line-join.hs

---
+++ themes/gui/cairo/try-simple-cairo/app/try-cairo-line-join.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TypeApplications #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Main where
+
+import Control.Monad.Primitive
+import Data.Maybe
+import Data.CairoContext
+import Data.CairoImage
+import Data.JuicyCairo
+import Data.Color
+import Codec.Picture
+import Graphics.Cairo.Drawing.CairoT
+import Graphics.Cairo.Drawing.CairoT.Setting
+import Graphics.Cairo.Drawing.Paths
+import Graphics.Cairo.Drawing.Transformations
+import Graphics.Cairo.Surfaces.ImageSurfaces
+import Graphics.Cairo.Values
+
+main :: IO ()
+main = do
+	sr <- cairoImageSurfaceCreate cairoFormatArgb32 128 128
+	cr <- cairoCreate sr
+	cairoSetSourceRgb cr . fromJust $ rgbDouble 0.2 0.6 0.1
+	cairoSet cr $ LineWidth 12
+
+	print =<< cairoGet @LineJoin cr
+	sampleLines cr
+
+	cairoTranslate cr 0 40
+	cairoSet cr $ LineJoinRound
+	print =<< cairoGet @LineJoin cr
+	sampleLines cr
+
+	cairoTranslate cr 0 40
+	cairoSet cr $ LineJoinBevel
+	print =<< cairoGet @LineJoin cr
+	sampleLines cr
+
+	cairoImageSurfaceGetCairoImage sr >>= \case
+		CairoImageArgb32 ci ->
+			writePng "try-cairo-line-join.png" $ cairoArgb32ToJuicyRGBA8 ci
+		_ -> error "never occur"
+
+sampleLines :: PrimMonad m => CairoT (PrimState m) -> m ()
+sampleLines cr = do
+	cairoMoveTo cr 4 16
+	cairoLineTo cr 88 16
+	cairoLineTo cr 40 32
+	cairoStroke cr
