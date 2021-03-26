13fa56c2de349e5b8f0c2a4f322f4d3108189904
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-03-26 17:16:48 +0900

    try-simple-cairo: app/try-cairo-path-extents.hs

---
+++ themes/gui/cairo/try-simple-cairo/app/try-cairo-path-extents.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE LambdaCase #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Main where
+
+import Data.Maybe
+import Data.CairoImage
+import Data.JuicyCairo
+import Data.Color
+import Codec.Picture
+import Graphics.Cairo.Drawing.CairoT.Basic
+import Graphics.Cairo.Drawing.CairoT.Setting
+import Graphics.Cairo.Drawing.Paths.Basic
+import Graphics.Cairo.Surfaces.ImageSurfaces
+import Graphics.Cairo.Values
+
+main :: IO ()
+main = do
+	sr <- cairoImageSurfaceCreate cairoFormatArgb32 128 128
+	cr <- cairoCreate sr
+	cairoSetSourceRgb cr . fromJust $ rgbDouble 0.2 0.6 0.1
+	cairoSet cr $ LineWidth 8
+	cairoMoveTo cr 32 32
+	cairoLineTo cr 96 64
+	cairoLineTo cr 64 96
+	cairoClosePath cr
+	print =<< cairoStrokeExtents cr
+	print =<< cairoFillExtents cr
+	print =<< cairoPathExtents cr
+	cairoStroke cr
+	cairoImageSurfaceGetCairoImage sr >>= \case
+		CairoImageArgb32 ci ->
+			writePng "try-cairo-path-extents.png" $ cairoArgb32ToJuicyRGBA8 ci
+		_ -> error "never occur"
