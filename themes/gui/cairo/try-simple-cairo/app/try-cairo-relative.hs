5b66bc5722aad74de22f6aaa06b252946b7b60c9
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-03-26 18:33:59 +0900

    try-simple-cairo: app/try-cairo-relative.hs

---
+++ themes/gui/cairo/try-simple-cairo/app/try-cairo-relative.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE LambdaCase #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Main where
+
+import Data.Maybe
+import Data.CairoImage
+import Data.JuicyCairo
+import Data.Color
+import Codec.Picture
+import Graphics.Cairo.Drawing.CairoT
+import Graphics.Cairo.Drawing.Paths
+import Graphics.Cairo.Drawing.Paths.Relative
+import Graphics.Cairo.Surfaces.ImageSurfaces
+import Graphics.Cairo.Values
+
+main :: IO ()
+main = do
+	sr <- cairoImageSurfaceCreate cairoFormatArgb32 128 128
+	cr <- cairoCreate sr
+	cairoSetSourceRgb cr . fromJust $ rgbDouble 0.2 0.6 0.1
+	cairoMoveTo cr 16 16
+	cairoRelMoveTo cr 16 16
+	cairoRelLineTo cr 64 0
+	cairoRelCurveTo cr 16 16 0 32 0 48
+	cairoStroke cr
+	cairoImageSurfaceGetCairoImage sr >>= \case
+		CairoImageArgb32 ci ->
+			writePng "try-cairo-relative.png" $ cairoArgb32ToJuicyRGBA8 ci
+		_ -> error "never occur"
