b4408a64c912ef736e4c82cc78d880f9be0c26b8
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-03-26 17:32:27 +0900

    try-simple-cairo: app/try-cairo-rel-move-error.hs

---
+++ themes/gui/cairo/try-simple-cairo/app/try-cairo-rel-move-error.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE LambdaCase #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Main where
+
+import Data.Maybe
+import Data.CairoImage
+import Data.JuicyCairo
+import Data.Color
+import Codec.Picture
+import Graphics.Cairo.Drawing.CairoT
+import Graphics.Cairo.Drawing.Paths.Relative
+import Graphics.Cairo.Surfaces.ImageSurfaces
+import Graphics.Cairo.Values
+
+main :: IO ()
+main = do
+	sr <- cairoImageSurfaceCreate cairoFormatArgb32 128 128
+	cr <- cairoCreate sr
+	cairoSetSourceRgb cr . fromJust $ rgbDouble 0.2 0.6 0.1
+	cairoPaint cr
+	cairoRelMoveTo cr 16 16
+	cairoImageSurfaceGetCairoImage sr >>= \case
+		CairoImageArgb32 ci ->
+			writePng "try-cairo-rel-move-error.png" $ cairoArgb32ToJuicyRGBA8 ci
+		_ -> error "never occur"
