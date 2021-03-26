4d9359872f74e9522e230f3e447a4d703d90841c
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-03-26 16:14:54 +0900

    try-simple-cairo: app/try-cairo-close-path.hs

---
+++ themes/gui/cairo/try-simple-cairo/app/try-cairo-close-path.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE LambdaCase #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Main where
+
+import Control.Monad.Primitive
+import Data.Bool
+import Data.Maybe
+import Data.CairoContext
+import Data.CairoImage
+import Data.JuicyCairo
+import Data.Color
+import Codec.Picture
+import Graphics.Cairo.Drawing.CairoT
+import Graphics.Cairo.Drawing.CairoT.Setting
+import Graphics.Cairo.Drawing.Paths
+import Graphics.Cairo.Drawing.Transformations
+import Graphics.Cairo.Surfaces.ImageSurfaces
+import Graphics.Cairo.Values
+
+main :: IO ()
+main = do
+	sr <- cairoImageSurfaceCreate cairoFormatArgb32 256 128
+	cr <- cairoCreate sr
+	cairoSetSourceRgb cr . fromJust $ rgbDouble 0.2 0.6 0.1
+	cairoSet cr $ LineWidth 16
+
+	sample cr False
+	cairoTranslate cr 128 0
+	sample cr True
+
+	cairoImageSurfaceGetCairoImage sr >>= \case
+		CairoImageArgb32 ci ->
+			writePng "try-cairo-close-path.png" $ cairoArgb32ToJuicyRGBA8 ci
+		_ -> error "never occur"
+
+sample :: PrimMonad m => CairoT (PrimState m) -> Bool -> m ()
+sample cr b = do
+	cairoMoveTo cr 32 32
+	cairoLineTo cr 96 64
+	cairoLineTo cr 48 96
+	bool (cairoLineTo cr 32 32) (cairoClosePath cr) b
+	cairoStroke cr
