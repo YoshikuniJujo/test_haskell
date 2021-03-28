c752666342f2577f63422d5de97e05b4a7266f2e
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-03-28 09:34:52 +0900

    try-simple-cairo: app/try-cairo-pattern-create-rgb.hs

---
+++ themes/gui/cairo/try-simple-cairo/app/try-cairo-pattern-create-rgb.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE LambdaCase #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Main where
+
+import Data.Maybe
+import Data.CairoImage
+import Data.JuicyCairo
+import Data.Color
+import Codec.Picture
+import Graphics.Cairo.Drawing.CairoT
+import Graphics.Cairo.Surfaces.ImageSurfaces
+import Graphics.Cairo.Values
+
+import Graphics.Cairo.Drawing.CairoPatternT.Basic
+
+main :: IO ()
+main = do
+	sr <- cairoImageSurfaceCreate cairoFormatArgb32 128 128
+	cr <- cairoCreate sr
+	p <- cairoPatternCreateRgb 0.2 0.4 0.1
+	cairoSetSource cr p
+	cairoPaint cr
+	cairoImageSurfaceGetCairoImage sr >>= \case
+		CairoImageArgb32 ci ->
+			writePng "try-cairo-pattern-create-rgb.png" $ cairoArgb32ToJuicyRGBA8 ci
+		_ -> error "never occur"
