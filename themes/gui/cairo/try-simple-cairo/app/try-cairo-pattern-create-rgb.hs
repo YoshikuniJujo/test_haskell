1c169e3b52beda8b0a120c2a6aedee7f0b517e99
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2021-03-29 09:45:38 +0900

    try-simple-cairo: app/try-cairo-pattern-create-rgb.hs: try cairoPatternCreateRgba

--- themes/gui/cairo/try-simple-cairo/app/try-cairo-pattern-create-rgb.hs
+++ themes/gui/cairo/try-simple-cairo/app/try-cairo-pattern-create-rgb.hs
@@ -9,6 +9,7 @@ import Data.JuicyCairo
 import Data.Color
 import Codec.Picture
 import Graphics.Cairo.Drawing.CairoT
+import Graphics.Cairo.Drawing.Paths.Basic
 import Graphics.Cairo.Surfaces.ImageSurfaces
 import Graphics.Cairo.Values
 
@@ -18,7 +19,7 @@ main :: IO ()
 main = do
 	sr <- cairoImageSurfaceCreate cairoFormatArgb32 128 128
 	cr <- cairoCreate sr
-	pt <- cairoPatternCreateRgb . fromJust $ rgbDouble 0.2 0.4 0.1
+	pt <- cairoPatternCreateRgb . fromJust $ rgbDouble 0.2 0.8 0.1
 	ptt <- cairoPatternGetType pt
 	print ptt
 	print CairoPatternTypeSolid
@@ -26,6 +27,10 @@ main = do
 		CairoPatternTypeSolid -> putStrLn "CairoPatternTypeSolid"
 		_ -> putStrLn "other"
 	cairoSetSource cr pt
+	cairoRectangle cr 32 32 64 64
+	cairoFill cr
+	pt' <- cairoPatternCreateRgba . fromJust $ rgbaDouble 0.0 0.0 0.8 0.4
+	cairoSetSource cr pt'
 	cairoPaint cr
 	cairoImageSurfaceGetCairoImage sr >>= \case
 		CairoImageArgb32 ci ->
