d543e8492e1f485051effd12812f0fbd48bc27fa
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2021-03-30 15:29:57 +0900

    try-cairo-mesh-pattern: app/try-partially-defined-patch.hs

--- themes/gui/cairo/try-cairo-mesh-pattern/app/try-partially-defined-patch.hs
+++ themes/gui/cairo/try-cairo-mesh-pattern/app/try-partially-defined-patch.hs
@@ -3,14 +3,19 @@
 
 module Main where
 
+import Foreign.ForeignPtr
+import Data.CairoContext
 import Data.CairoImage
 import Data.JuicyCairo
 import Codec.Picture
 import Graphics.Cairo.Drawing.CairoT
 import Graphics.Cairo.Surfaces.ImageSurfaces
 import Graphics.Cairo.Values
+import Graphics.Cairo.Drawing.CairoPatternT.Basic
 
 import CairoMeshPattern
+
+import Graphics.Cairo.Exception
 
 main :: IO ()
 main = do
@@ -20,6 +25,7 @@ main = do
 	pt <- cairoPatternCreateMesh
 
 	cairoMeshPatternBeginPatch pt
+{-
 	cairoMeshPatternMoveTo pt 64 128
 	cairoMeshPatternLineTo pt 64 64
 	cairoMeshPatternLineTo pt 128 64
@@ -49,9 +55,18 @@ main = do
 	cairoMeshPatternSetCornerColorRgb pt 2 0 1 0
 	cairoMeshPatternSetCornerColorRgb pt 3 1 0 0
 	cairoMeshPatternEndPatch pt
+	-}
 
 	cairoSetSource cr pt
+	putStrLn "BEFORE PAINT"
 	cairoPaint cr
+
+	putStrLn "BEFORE CHECK CAIRO T ERROR"
+	let CairoT fcr = cr in
+		cairoStatusToThrowError =<< withForeignPtr fcr c_cairo_status
+	putStrLn "BEFORE CHECK CAIRO PATTERN T ERROR"
+	let CairoPatternT fpt = pt in
+		cairoStatusToThrowError =<< withForeignPtr fpt c_cairo_pattern_status
 
 	cairoImageSurfaceGetCairoImage sr >>= \case
 		CairoImageArgb32 ci ->
