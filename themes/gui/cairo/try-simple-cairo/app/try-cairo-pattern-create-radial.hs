fa67a0b16dfce8510d91616c64857dc09bafb70f
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-03-31 14:26:28 +0900

    try-simple-cairo: app/try-cairo-pattern-create-radial.hs: cairoPatternGetRadialCircles

--- themes/gui/cairo/try-simple-cairo/app/try-cairo-pattern-create-radial.hs
+++ themes/gui/cairo/try-simple-cairo/app/try-cairo-pattern-create-radial.hs
@@ -9,7 +9,7 @@ import Data.JuicyCairo
 import Data.Color
 import Codec.Picture
 import Graphics.Cairo.Drawing.CairoT
-import Graphics.Cairo.Drawing.CairoPatternT
+import Graphics.Cairo.Drawing.CairoPatternT.Basic
 import Graphics.Cairo.Surfaces.ImageSurfaces
 import Graphics.Cairo.Values
 
@@ -21,6 +21,8 @@ main = do
 	cairoPatternAddColorStopRgb pt 0.2 . fromJust $ rgbDouble 0.7 0.3 0.2
 	cairoPatternAddColorStopRgb pt 0.5 . fromJust $ rgbDouble 0.52 0.52 0.2
 	cairoPatternAddColorStopRgb pt 0.8 . fromJust $ rgbDouble 0.3 0.7 0.2
+	print =<< cairoPatternGetRadialCircles pt
+	print =<< cairoPatternGetColorStopRgbaList pt
 	cairoSetSource cr pt
 	cairoPaint cr
 	cairoImageSurfaceGetCairoImage sr >>= \case
