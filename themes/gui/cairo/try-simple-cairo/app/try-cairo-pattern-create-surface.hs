3cd305eb5bb59b477ff8e5db3205cae771df766b
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-04-02 17:15:31 +0900

    try-simple-cairo: app/try-cairo-pattern-create-surface.hs

---
+++ themes/gui/cairo/try-simple-cairo/app/try-cairo-pattern-create-surface.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE LambdaCase #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+import Data.CairoImage
+import Data.JuicyCairo
+import Codec.Picture
+import Graphics.Cairo.Values
+import Graphics.Cairo.Drawing.CairoT
+import Graphics.Cairo.Drawing.CairoPatternT
+import Graphics.Cairo.Drawing.Transformations
+import Graphics.Cairo.Surfaces.ImageSurfaces
+
+main :: IO ()
+main = readImage "data/HaskellLogo.png" >>= \case
+	Right (ImageRGBA8 i) -> do
+		let i' = juicyRGBA8ToCairoArgb32 i
+		putStrLn $ show (imageWidth i) ++ " " ++ show (imageHeight i)
+		sr0 <- cairoImageSurfaceCreate cairoFormatArgb32 256 256
+		sr1 <- cairoImageSurfaceCreateForCairoImage $ CairoImageArgb32 i'
+		cr <- cairoCreate sr0
+		pt <- cairoPatternCreateForSurface sr1
+		cairoTranslate cr 64 64
+		cairoSetSource cr pt
+		cairoPaint cr
+		cairoImageSurfaceGetCairoImage sr0 >>= \case
+			CairoImageArgb32 ci ->
+				writePng "try-cairo-pattern-create-surface.png" $ cairoArgb32ToJuicyRGBA8 ci
+			_ -> error "never occur"
+	_ -> error "bad"
