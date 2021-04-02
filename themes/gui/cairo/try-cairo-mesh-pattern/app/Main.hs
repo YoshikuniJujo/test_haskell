c193536b2b3b8b8fdf091e263e846cb5ad3bfd5f
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-04-02 09:54:37 +0900

    try-cairo-mesh-pattern: try using cairoMeshPatternAddPatch3

--- themes/gui/cairo/try-cairo-mesh-pattern/app/Main.hs
+++ themes/gui/cairo/try-cairo-mesh-pattern/app/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE PatternSynonyms #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Main where
@@ -7,6 +8,8 @@ import Data.CairoImage
 import Data.JuicyCairo
 import Codec.Picture
 import Graphics.Cairo.Drawing.CairoT
+import Graphics.Cairo.Drawing.Paths.CairoPathT (pattern CairoPathT)
+import Graphics.Cairo.Drawing.Paths
 import Graphics.Cairo.Surfaces.ImageSurfaces
 import Graphics.Cairo.Values
 
@@ -22,28 +25,6 @@ main = do
 
 	pt <- cairoPatternCreateMesh
 
-{-
-	cairoMeshPatternBeginPatch pt
-	cairoMeshPatternMoveTo pt 64 128
-	cairoMeshPatternLineTo pt 64 64
-	cairoMeshPatternLineTo pt 128 64
-	cairoMeshPatternLineTo pt 128 128
-	cairoMeshPatternSetCornerColorRgb pt 0 1 0 0
-	cairoMeshPatternSetCornerColorRgb pt 1 0 1 0
-	cairoMeshPatternSetCornerColorRgb pt 2 0 0 1
-	cairoMeshPatternSetCornerColorRgb pt 3 1 1 0
-	cairoMeshPatternEndPatch pt
-
-	cairoMeshPatternBeginPatch pt
-	cairoMeshPatternMoveTo pt 256 128
-	cairoMeshPatternLineTo pt 256 64
-	cairoMeshPatternLineTo pt 320 64
-	cairoMeshPatternSetCornerColorRgb pt 0 0 1 1
-	cairoMeshPatternSetCornerColorRgb pt 1 1 0 1
-	cairoMeshPatternSetCornerColorRgb pt 2 1 1 0
-	cairoMeshPatternEndPatch pt
-	-}
-
 	cairoMeshPatternAddPatch pt
 		(MoveTo 128 128) (LineTo 128 64) (LineTo 192 64) (LineTo 192 128) CloseLineTo
 		(ColorRgb . fromJust $ rgbDouble 1 1 0)
@@ -53,10 +34,8 @@ main = do
 		Nothing Nothing Nothing Nothing
 
 	cairoMeshPatternAddPatch pt
-		(MoveTo 256 128)
-		(CurveTo 224 106.6 224 85.3 256 64)
-		(LineTo 320 64)
-		(LineTo 320 128)
+		(MoveTo 256 128) (CurveTo 224 106.6 224 85.3 256 64)
+		(LineTo 320 64) (LineTo 320 128)
 		CloseLineTo
 		(ColorRgb . fromJust $ rgbDouble 0 1 1)
 		(ColorRgb . fromJust $ rgbDouble 1 0 1)
@@ -64,9 +43,42 @@ main = do
 		(ColorRgb . fromJust $ rgbDouble 1 1 1)
 		Nothing Nothing Nothing Nothing
 
-	print =<< cairoMeshPatternGetPatchList pt
+	cairoMeshPatternAddPatch3 pt
+		(MoveTo 384 128) (LineTo 448 128) (LineTo 416 72.57) CloseLineTo
+		(ColorRgb . fromJust $ rgbDouble 0 0 1)
+		(ColorRgb . fromJust $ rgbDouble 0 1 0)
+		(ColorRgb . fromJust $ rgbDouble 1 0 0)
+		Nothing Nothing Nothing
+
+	ps <- cairoMeshPatternGetPatchList pt
+	print ps
+	let	[
+			(_, _, (
+				Point a0 b0, Point a1 b1,
+				Point a2 b2, Point a3 b3 )),
+			(_, _, (
+				Point c0 d0, Point c1 d1,
+				Point c2 d2, Point c3 d3 )),
+			(CairoPathT cp, _, (
+				Point x0 y0, Point x1 y1,
+				Point x2 y2, Point x3 y3 )) ] = ps
+	print cp
 	cairoSetSource cr pt
 	cairoPaint cr
+
+	cairoSetSourceRgb cr . fromJust $ rgbDouble 0 0 0
+
+	cairoMoveTo cr a0 b0
+	uncurry (cairoLineTo cr) `mapM_` [(a1, b1), (a2, b2), (a3, b3)]
+
+	cairoMoveTo cr c0 d0
+	uncurry (cairoLineTo cr) `mapM_` [(c1, d1), (c2, d2), (c3, d3)]
+
+	cairoMoveTo cr x0 y0
+	cairoLineTo cr x1 y1
+	cairoLineTo cr x2 y2
+	cairoLineTo cr x3 y3
+	cairoStroke cr
 
 	cairoImageSurfaceGetCairoImage sr >>= \case
 		CairoImageArgb32 ci ->
