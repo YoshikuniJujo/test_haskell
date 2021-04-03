bc6684ea5580002518118fc81991510051e31e90
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-04-04 06:47:39 +0900

    try-cairo-transformations: app/try-cairo-matrix-t.hs: cairoMatrixInitIdentity and cairoMatrixInitTranslate

--- themes/gui/cairo/try-cairo-transformations/app/try-cairo-matrix-t.hs
+++ themes/gui/cairo/try-cairo-transformations/app/try-cairo-matrix-t.hs
@@ -7,11 +7,14 @@ import Control.Monad.ST
 import Graphics.Cairo.Utilities.CairoMatrixT
 
 main :: IO ()
-main = do
-	print sampleMatrix1
+main = print `mapM_` [sampleMatrix1, sampleMatrix2, sampleMatrix3]
 
-sampleMatrix1 :: Matrix
-sampleMatrix1 = runST do
-	m <- cairoMatrixNew
-	cairoMatrixInit m 5 8 10 18 3 2
-	cairoMatrixGet m
+sampleMatrix1, sampleMatrix2, sampleMatrix3 :: Matrix
+sampleMatrix1 = runST $ cairoMatrixNew >>= \m ->
+	cairoMatrixInit m 5 8 10 18 3 2 >> cairoMatrixGet m
+
+sampleMatrix2 = runST $ cairoMatrixNew >>= \m ->
+	cairoMatrixInitIdentity m >> cairoMatrixGet m
+
+sampleMatrix3 = runST $ cairoMatrixNew >>= \m ->
+	cairoMatrixInitTranslate m 15 25 >> cairoMatrixGet m
