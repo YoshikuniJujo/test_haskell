9d2a5991485f95977632483eabbdbc8d87d77d42
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-04-04 06:59:00 +0900

    try-cairo-transformations: app/try-cairo-matrix-t: cairoMatrixInitScale and cairoMatrixInitRotate

--- themes/gui/cairo/try-cairo-transformations/app/try-cairo-matrix-t.hs
+++ themes/gui/cairo/try-cairo-transformations/app/try-cairo-matrix-t.hs
@@ -7,9 +7,12 @@ import Control.Monad.ST
 import Graphics.Cairo.Utilities.CairoMatrixT
 
 main :: IO ()
-main = print `mapM_` [sampleMatrix1, sampleMatrix2, sampleMatrix3]
+main = print `mapM_` [
+	sampleMatrix1, sampleMatrix2, sampleMatrix3, sampleMatrix4,
+	sampleMatrix5, sampleMatrix6 ]
 
-sampleMatrix1, sampleMatrix2, sampleMatrix3 :: Matrix
+sampleMatrix1, sampleMatrix2, sampleMatrix3, sampleMatrix4, sampleMatrix5,
+	sampleMatrix6 :: Matrix
 sampleMatrix1 = runST $ cairoMatrixNew >>= \m ->
 	cairoMatrixInit m 5 8 10 18 3 2 >> cairoMatrixGet m
 
@@ -18,3 +21,12 @@ sampleMatrix2 = runST $ cairoMatrixNew >>= \m ->
 
 sampleMatrix3 = runST $ cairoMatrixNew >>= \m ->
 	cairoMatrixInitTranslate m 15 25 >> cairoMatrixGet m
+
+sampleMatrix4 = runST $ cairoMatrixNew >>= \m ->
+	cairoMatrixInitScale m 3 8 >> cairoMatrixGet m
+
+sampleMatrix5 = runST $ cairoMatrixNew >>= \m ->
+	cairoMatrixInitRotate m (pi / 2) >> cairoMatrixGet m
+
+sampleMatrix6 = runST $ cairoMatrixNew >>= \m ->
+	cairoMatrixInitRotate m (pi / 4) >> cairoMatrixGet m
