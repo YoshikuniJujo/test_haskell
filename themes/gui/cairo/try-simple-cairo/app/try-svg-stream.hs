de66c84034b8dbc8a55ea93b867fe1ca5ad9e8e6
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-04-06 13:19:12 +0900

    try-simple-cairo: try using cairoSvgSurfaceWithForStream

--- themes/gui/cairo/try-simple-cairo/app/try-svg-stream.hs
+++ themes/gui/cairo/try-simple-cairo/app/try-svg-stream.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE LambdaCase, OverloadedStrings #-}
+{-# LANGUAGE BlockArguments, LambdaCase, OverloadedStrings #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Main where
@@ -16,21 +16,22 @@ import qualified Data.Text.IO as T
 
 main :: IO ()
 main = do
-	sr <- cairoSvgSurfaceCreateForStream (\_ t -> T.putStrLn t >> pure WriteSuccess) nullPtr 128 128
-	cr <- cairoCreate sr
-	cairoSetSourceRgb cr . fromJust $ rgbDouble 0.2 0.6 0.1
-	cairoPaint cr
---	cairoSurfaceFlush sr
---	cairoSurfaceFinish sr
+	cairoSvgSurfaceWithForStream (\_ t -> T.putStrLn t >> pure WriteSuccess) nullPtr 128 128 \sr -> do
+	--	sr <- cairoSvgSurfaceCreateForStream (\_ t -> T.putStrLn t >> pure WriteSuccess) nullPtr 128 128
+		cr <- cairoCreate sr
+		cairoSetSourceRgb cr . fromJust $ rgbDouble 0.2 0.6 0.1
+		cairoPaint cr
+	--	cairoSurfaceFlush sr
+	--	cairoSurfaceFinish sr
 	putStrLn "=== ST ==="
 	T.putStr $ runST st
 
 st :: ST s T.Text
-st = do
-	v <- newSTRef ""
-	sr <- cairoSvgSurfaceCreateForStream (\_ t -> modifySTRef v (<> t) >> pure WriteSuccess) nullPtr 128 128
-	cr <- cairoCreate sr
-	cairoSetSourceRgb cr . fromJust $ rgbDouble 0.2 0.6 0.1
-	cairoPaint cr
---	cairoSurfaceFinish sr
+st = newSTRef "" >>= \v -> do
+	cairoSvgSurfaceWithForStream (\_ t -> modifySTRef v (<> t) >> pure WriteSuccess) nullPtr 128 128 \sr -> do
+	--	sr <- cairoSvgSurfaceCreateForStream (\_ t -> modifySTRef v (<> t) >> pure WriteSuccess) nullPtr 128 128
+		cr <- cairoCreate sr
+		cairoSetSourceRgb cr . fromJust $ rgbDouble 0.2 0.6 0.1
+		cairoPaint cr
+	--	cairoSurfaceFinish sr
 	readSTRef v
