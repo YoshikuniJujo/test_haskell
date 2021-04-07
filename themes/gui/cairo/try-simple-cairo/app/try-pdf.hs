19cea05f7816bfe0499d3fef6da5fdcdbbb04b3d
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-04-07 13:30:27 +0900

    try-simple-cairo: cairoPdfSurfaceWith

--- themes/gui/cairo/try-simple-cairo/app/try-pdf.hs
+++ themes/gui/cairo/try-simple-cairo/app/try-pdf.hs
@@ -11,9 +11,7 @@ import Graphics.Cairo.Surfaces.CairoSurfaceTypeT
 import Graphics.Cairo.Surfaces.PdfSurfaces
 
 main :: IO ()
-main = do
-	sr <- cairoPdfSurfaceCreate "try-pdf.pdf" 128 128
+main = cairoPdfSurfaceWith "try-pdf.pdf" 128 128 \sr -> do
 	cr <- cairoCreate sr
 	cairoSetSourceRgb cr . fromJust $ rgbDouble 0.2 0.6 0.1
 	cairoPaint cr
-	unsafeCairoSurfaceFinish sr
