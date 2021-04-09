2eef2b33848c2df2c2195af11d06f47b29d4ecc9
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2021-04-09 15:40:52 +0900

    try-cairo-pdf: cairoPdfGetVersions and cairoPdfVersionToString

--- themes/gui/cairo/try-cairo-pdf/app/Main.hs
+++ themes/gui/cairo/try-cairo-pdf/app/Main.hs
@@ -3,13 +3,16 @@
 
 module Main where
 
+import Control.Monad
 import Data.Maybe
 import Data.Color
 import Graphics.Cairo.Drawing.CairoT
 import Graphics.Cairo.Surfaces.PdfSurfaces
 
 main :: IO ()
-main = cairoPdfSurfaceWith "try-cairo-pdf.pdf" 595 842 \sr -> do
-	cr <- cairoCreate sr
-	cairoSetSourceRgb cr . fromJust $ rgbDouble 0.2 0.6 0.1
-	cairoPaint cr
+main = do
+	((putStrLn <=< cairoPdfVersionToString) `mapM_`) =<< cairoPdfGetVersions
+	cairoPdfSurfaceWith "try-cairo-pdf.pdf" 595 842 \sr -> do
+		cr <- cairoCreate sr
+		cairoSetSourceRgb cr . fromJust $ rgbDouble 0.2 0.6 0.1
+		cairoPaint cr
