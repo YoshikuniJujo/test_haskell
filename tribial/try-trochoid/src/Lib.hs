e72b6ce78687560aaeb043a936cf620d98400896
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2021-04-13 09:18:02 +0900

    try-trochoid: use a new version of simple-cairo

--- tribial/try-trochoid/src/Lib.hs
+++ tribial/try-trochoid/src/Lib.hs
@@ -2,28 +2,33 @@
 
 module Lib where
 
+import Foreign.C.Types
 import Control.Arrow
 import Control.Monad.ST
+import Data.Maybe
 import Data.Set
 import Codec.Picture
 
-import Graphics.Cairo.Drawing.CairoT
-import Graphics.Cairo.ImageSurfaces
-import Graphics.Cairo.Paths
-import Graphics.Cairo.CairoPatternT
-import Graphics.Cairo.Types
-import Graphics.Cairo.Values
-import Graphics.Cairo.PngSupport
+import Data.CairoContext
+import Data.Color
 
-dot :: Double -> Rational -> Double -> (Double, Double)
+import Graphics.Cairo.Drawing.CairoT
+import Graphics.Cairo.Surfaces.CairoSurfaceT.Internal
+import Graphics.Cairo.Surfaces.ImageSurfaces
+import Graphics.Cairo.Drawing.Paths
+import Graphics.Cairo.Drawing.CairoPatternT.Basic
+import Graphics.Cairo.Values
+import Graphics.Cairo.Surfaces.PngSupport
+
+dot :: CDouble -> Rational -> CDouble -> (CDouble, CDouble)
 dot r rt x = (
 	200 * cos x - r * sin (fromRational rt * x) + 400,
 	200 * sin x - r * cos (fromRational rt * x) + 400 )
 
-dotList :: Double -> Rational -> [(Double, Double)]
+dotList :: CDouble -> Rational -> [(CDouble, CDouble)]
 dotList r rt = dot r rt <$> [0, pi / 1024 .. 128 * pi]
 
-dot2 :: Double -> (Double, Double)
+dot2 :: CDouble -> (CDouble, CDouble)
 dot2 x = (
 	200 * cos x - 140 * sin (17 / 16 * x) + 400,
 	200 * sin x - 140 * cos (17 / 16 * x) + 400 )
@@ -50,7 +55,7 @@ writeTrochoid = writePng "tmp.png" img
 writeWithCairo1 :: IO ()
 writeWithCairo1 = writeWithCairo "tmp_c.png" 120 (18 / 17)
 
-writeWithCairo :: FilePath -> Double -> Rational -> IO ()
+writeWithCairo :: FilePath -> CDouble -> Rational -> IO ()
 writeWithCairo fp r rt = do
 	s <- cairoImageSurfaceCreate cairoFormatArgb32 800 800
 	cr <- cairoCreate s
@@ -59,21 +64,21 @@ writeWithCairo fp r rt = do
 	cairoSetSource cr =<< pat1
 	polygon cr (head ds) (tail ds)
 	cairoStroke cr
-	print =<< cairoSurfaceWriteToPng s fp
+	print =<< cairoSurfaceWriteToPng (toCairoSurfaceT s) fp
 	where
 	ds = dotList r rt
 
-polygon :: CairoT RealWorld -> (Double, Double) -> [(Double, Double)] -> IO ()
+polygon :: CairoT RealWorld -> (CDouble, CDouble) -> [(CDouble, CDouble)] -> IO ()
 polygon cr (x0, y0) ps = do
 	cairoMoveTo cr x0 y0
 	uncurry (cairoLineTo cr) `mapM_` ps
 
-pat1 :: IO (CairoPatternT RealWorld)
+pat1 :: IO (CairoPatternRadialT RealWorld)
 pat1 = do
 	p <- cairoPatternCreateRadial 400 400 140 400 400 250
-	cairoPatternAddColorStopRgb p 1.0 0.0 0.0 1.0
-	cairoPatternAddColorStopRgb p 0.5 1.0 1.0 0
-	cairoPatternAddColorStopRgb p 0.0 1.0 0.0 0.2
+	cairoPatternAddColorStopRgb p 1.0 . fromJust $ rgbDouble 0.0 0.0 1.0
+	cairoPatternAddColorStopRgb p 0.5 . fromJust $ rgbDouble 1.0 1.0 0
+	cairoPatternAddColorStopRgb p 0.0 . fromJust $ rgbDouble 1.0 0.0 0.2
 	pure p
 
 sample1 :: FilePath -> IO ()
