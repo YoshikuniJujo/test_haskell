9a3fd516ee0d8c596b537f7692d1aa2d7753df34
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2021-04-15 11:42:02 +0900

    try-pango: try more variable fonts

---
+++ themes/gui/pango/try-pango/app/try-pango-fonts-montserrat.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE LambdaCase #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Main where
+
+import Data.CairoImage
+import Data.JuicyCairo
+import Codec.Picture
+import Graphics.Cairo.Drawing.CairoT
+import Graphics.Cairo.Surfaces.ImageSurfaces
+import Graphics.Cairo.Values
+import Graphics.Pango.Basic.LayoutObjects
+import Graphics.Pango.Basic.LayoutObjects.PangoLayoutPrim
+import Graphics.Pango.Rendering.Cairo
+
+import Graphics.Pango.Basic.Fonts
+
+main :: IO ()
+main = do
+	s <- cairoImageSurfaceCreate cairoFormatArgb32 300 400
+	cr <- cairoCreate s
+
+	fd <- pangoFontDescriptionNew
+
+	pangoFontDescriptionSetFamily fd "Montserrat"
+	pangoFontDescriptionSet fd $ Size 20
+--	pangoFontDescriptionSetVariation fd "SKLA=1000,TRMG=750,WGHT=100"
+--	pangoFontDescriptionSetVariation fd "TRMG=750"
+	pangoFontDescriptionSetVariation fd "wght=500,slnt=40"
+
+	pl <- pangoCairoCreateLayout cr
+	pangoLayoutSetFontDescription pl fd
+	pangoLayoutSetText pl "Hello, world!\nこんにちは、世界!" 40
+	pangoCairoShowLayout cr =<< pangoLayoutFreeze pl
+
+	cairoImageSurfaceGetCairoImage s >>= \case
+		CairoImageArgb32 a -> writePng "try-pango-fonts-montserrat.png" $ cairoArgb32ToJuicyRGBA8 a
+		_ -> error "never occur"
