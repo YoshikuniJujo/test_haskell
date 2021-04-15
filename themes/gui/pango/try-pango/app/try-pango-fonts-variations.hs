1cdfa977fca971ab281ae87b33f68ca090924c8e
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-04-15 18:03:00 +0900

    try-pango: app/try-pango-fonts-variations.hs: Inline Skeleton

--- themes/gui/pango/try-pango/app/try-pango-fonts-variations.hs
+++ themes/gui/pango/try-pango/app/try-pango-fonts-variations.hs
@@ -1,10 +1,11 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE LambdaCase, OverloadedStrings #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Main where
 
 import Data.CairoImage
 import Data.JuicyCairo
+import System.Environment
 import Codec.Picture
 import Graphics.Cairo.Drawing.CairoT
 import Graphics.Cairo.Surfaces.ImageSurfaces
@@ -18,23 +19,33 @@ import Graphics.Pango.Basic.Fonts.PangoFontDescription.Variations
 import Graphics.Pango.Basic.Fonts.PangoFontDescription.Type
 
 main :: IO ()
-main = do
-	s <- cairoImageSurfaceCreate cairoFormatArgb32 300 400
-	cr <- cairoCreate s
+main = getArgs >>= \case
+	skla : _ -> do
+		s <- cairoImageSurfaceCreate cairoFormatArgb32 300 400
+		cr <- cairoCreate s
 
-	fd <- pangoFontDescriptionNew
+		fd <- pangoFontDescriptionNew
 
-	pangoFontDescriptionSetFamily fd "Decovar Alpha"
-	pangoFontDescriptionSet fd $ Size 20
+		pangoFontDescriptionSetFamily fd "Decovar Alpha"
+		pangoFontDescriptionSet fd $ Size 20
 --	pangoFontDescriptionSetVariation fd "SKLA=1000,TRMG=750,WGHT=100"
 --	pangoFontDescriptionSetVariation fd "TRMG=750"
-	pangoFontDescriptionSetVariation fd "TRMG=1000"
+--	pangoFontDescriptionSetVariation fd "TRMG=1000"
+		pangoFontDescriptionSetAxis fd . InlineSkeleton $ read skla
 
-	pl <- pangoCairoCreateLayout cr
-	pangoLayoutSetFontDescription pl fd
-	pangoLayoutSetText pl "Hello, world!\nこんにちは、世界!" 40
-	pangoCairoShowLayout cr =<< pangoLayoutFreeze pl
+		pl <- pangoCairoCreateLayout cr
+		pangoLayoutSetFontDescription pl fd
+		pangoLayoutSetText pl "Hello, world!\nこんにちは、世界!" 40
+		pangoCairoShowLayout cr =<< pangoLayoutFreeze pl
 
-	cairoImageSurfaceGetCairoImage s >>= \case
-		CairoImageArgb32 a -> writePng "try-pango-fonts-variations.png" $ cairoArgb32ToJuicyRGBA8 a
-		_ -> error "never occur"
+		cairoImageSurfaceGetCairoImage s >>= \case
+			CairoImageArgb32 a -> writePng "try-pango-fonts-variations.png" $ cairoArgb32ToJuicyRGBA8 a
+			_ -> error "never occur"
+	_ -> error "need skla"
+
+newtype InlineSkeleton = InlineSkeleton { getInlineSkeleton :: Double } deriving Show
+
+instance PangoFontDescriptionAxis InlineSkeleton where
+	pangoFontDescriptionAxisTag = "SKLA"
+	pangoFontDescriptionAxisToDouble = getInlineSkeleton
+	pangoFontDescriptionAxisFromDouble = InlineSkeleton
