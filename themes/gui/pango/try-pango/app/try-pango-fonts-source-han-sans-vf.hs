c0d434f8a76b7e3e76d15a198de3a9d536ca3ff8
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-04-16 10:05:55 +0900

    try-pango: app/try-pango-fonts-source-han-sans-vf.hs

---
+++ themes/gui/pango/try-pango/app/try-pango-fonts-source-han-sans-vf.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE BlockArguments, LambdaCase #-}
+{-# LANGUAGE TypeApplications #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Main where
+
+import Data.Foldable
+import Data.CairoImage
+import Data.JuicyCairo
+import Codec.Picture
+import Graphics.Cairo.Drawing.CairoT
+import Graphics.Cairo.Surfaces.ImageSurfaces
+import Graphics.Cairo.Values
+import Graphics.Pango.Basic.LayoutObjects
+import Graphics.Pango.Basic.LayoutObjects.PangoLayoutPrim
+import Graphics.Pango.Rendering.Cairo
+
+import Graphics.Pango.Basic.Fonts.PangoFontDescription
+import Graphics.Pango.Basic.Fonts.PangoFontDescription.Variations
+import Graphics.Pango.Basic.Fonts.PangoFontDescription.Type
+
+import Graphics.Cairo.Drawing.Paths
+
+main :: IO ()
+main = do
+	s <- cairoImageSurfaceCreate cairoFormatArgb32 300 1120
+	cr <- cairoCreate s
+
+	fd <- pangoFontDescriptionNew
+	pangoFontDescriptionSetFamily fd "Source Han Sans VF"
+	pangoFontDescriptionSet fd $ Size 20
+
+	pl <- pangoCairoCreateLayout cr
+	pangoLayoutSetText pl "Hello, world!\nこんにちは、世界!\n" 40
+	for_ (zip [0, 80 ..] [250, 300 .. 900]) \(y, w) -> do
+		pangoFontDescriptionSetAxis fd $ Weight w
+		print =<< pangoFontDescriptionGetAxis @Weight fd
+		pangoLayoutSetFontDescription pl fd
+		cairoMoveTo cr 0 y
+		pangoCairoShowLayout cr =<< pangoLayoutFreeze pl
+
+	cairoImageSurfaceGetCairoImage s >>= \case
+		CairoImageArgb32 a -> writePng "try-pango-fonts-source-han-sans.png" $ cairoArgb32ToJuicyRGBA8 a
+		_ -> error "never occur"
