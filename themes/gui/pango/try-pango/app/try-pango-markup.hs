8973a3b073929a10801b49caca0ae46d7cac94c2
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2021-04-19 17:09:11 +0900

    try-pango: app/try-pango-markup.hs: font, face, size, style, weight

--- themes/gui/pango/try-pango/app/try-pango-markup.hs
+++ themes/gui/pango/try-pango/app/try-pango-markup.hs
@@ -44,6 +44,42 @@ main = do
 		"<span foreground=\"blue\" size=\"x-large\">Blue text</span> is <i> cool</i>!"
 	pangoCairoShowLayout cr pl
 
+	cairoMoveTo cr 0 160
+	pangoLayoutSetMarkup pl "&#169;Yoshikuni Jujo"
+	pangoCairoShowLayout cr pl
+
+	cairoMoveTo cr 0 180
+	pangoLayoutSetMarkup pl "hello <span font=\"Sans Italic 12\">hello</span>"
+	pangoCairoShowLayout cr pl
+
+	cairoMoveTo cr 0 200
+	pangoLayoutSetMarkup pl "<span face=\"mikachan_o\">こんにちは、</span>世界!"
+	pangoCairoShowLayout cr pl
+
+	cairoMoveTo cr 0 230
+	pangoLayoutSetMarkup pl $
+		"<span size=\"12800\">hello</span> <span size=\"15360\">hello</span> " <>
+		"<span size=\"17920\">hello</span>"
+	pangoCairoShowLayout cr pl
+
+	cairoMoveTo cr 0 250
+	pangoLayoutSetMarkup pl $
+		"<span style=\"normal\">hello</span> <span style=\"oblique\">hello</span> " <>
+		"<span style=\"italic\">hello</span>"
+	pangoCairoShowLayout cr pl
+
+	cairoMoveTo cr 0 270
+	pangoLayoutSetMarkup pl $
+		"<span face=\"Source Han Sans VF\">" <>
+		"<span weight=\"ultralight\">hello</span> " <>
+		"<span weight=\"light\">hello</span> " <>
+		"<span weight=\"normal\">hello</span> " <>
+		"<span weight=\"bold\">hello</span> " <>
+		"<span weight=\"ultrabold\">hello</span> " <>
+		"<span weight=\"heavy\">hello</span>" <>
+		"</span>"
+	pangoCairoShowLayout cr pl
+
 	cairoImageSurfaceGetCairoImage s >>= \case
 		CairoImageArgb32 a -> writePng "try-pango-markup.png" $ cairoArgb32ToJuicyRGBA8 a
 		_ -> error "never occur"
