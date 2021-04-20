c36031ec884aca0ad816f0943f434bb9a0367efb
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2021-04-20 10:35:14 +0900

    try-pango: app/try-pango-markup.hs: more markups

--- themes/gui/pango/try-pango/app/try-pango-markup.hs
+++ themes/gui/pango/try-pango/app/try-pango-markup.hs
@@ -16,7 +16,7 @@ import Graphics.Pango.Rendering.Cairo
 
 main :: IO ()
 main = do
-	s <- cairoImageSurfaceCreate cairoFormatArgb32 500 400
+	s <- cairoImageSurfaceCreate cairoFormatArgb32 500 900
 	cr <- cairoCreate s
 
 	pl <- pangoCairoCreateLayout cr
@@ -99,6 +99,144 @@ main = do
 		"<span stretch=\"expanded\">hello</span> " <>
 		"<span stretch=\"extraexpanded\">hello</span> " <>
 		"<span stretch=\"ultraexpanded\">hello</span>" <>
+		"</span>"
+	pangoCairoShowLayout cr pl
+
+	cairoMoveTo cr 0 330
+	pangoLayoutSetMarkup pl $
+		"<span face=\"Source Han Sans\">" <>
+		"<span font_features=\"normal\">0.05</span> " <>
+		"<span font_features=\"zero\">0.05</span>" <>
+		"</span>"
+	pangoCairoShowLayout cr pl
+
+	cairoMoveTo cr 0 350
+	pangoLayoutSetMarkup pl $
+		"<span color=\"red\">RED</span> " <>
+		"<span color=\"green\">GREEN</span> " <>
+		"<span color=\"blue\">BLUE</span>"
+	pangoCairoShowLayout cr pl
+
+	cairoMoveTo cr 0 370
+	pangoLayoutSetMarkup pl $
+		"<span bgcolor=\"red\">RED</span> " <>
+		"<span bgcolor=\"green\">GREEN</span> " <>
+		"<span bgcolor=\"blue\">BLUE</span>"
+	pangoCairoShowLayout cr pl
+
+	cairoMoveTo cr 0 390
+	pangoLayoutSetMarkup pl $
+		"<span alpha=\"10%\">10%</span> " <>
+		"<span alpha=\"50%\">50%</span> " <>
+		"<span alpha=\"100%\">100%</span>"
+	pangoCairoShowLayout cr pl
+
+	cairoMoveTo cr 0 410
+	pangoLayoutSetMarkup pl $
+		"<span bgcolor=\"green\" bgalpha=\"1\">" <>
+		"<span bgalpha=\"10%\">10%</span> " <>
+		"<span bgalpha=\"50%\">50%</span> " <>
+		"<span bgalpha=\"100%\">100%</span>" <>
+		"</span>"
+	pangoCairoShowLayout cr pl
+
+	cairoMoveTo cr 0 430
+	pangoLayoutSetMarkup pl $
+		"<span underline=\"none\">none</span> " <>
+		"<span underline=\"single\">single</span> " <>
+		"<span underline=\"double\">double</span> " <>
+		"<span underline=\"low\">low ggg</span> " <>
+		"<span underline=\"error\">error</span>"
+	pangoCairoShowLayout cr pl
+
+	cairoMoveTo cr 0 450
+	pangoLayoutSetMarkup pl $
+		"<span underline=\"double\">" <>
+		"<span underline_color=\"red\">red</span> " <>
+		"<span underline_color=\"green\">green</span> " <>
+		"<span underline_color=\"blue\">blue</span>" <>
+		"</span>"
+	pangoCairoShowLayout cr pl
+
+	cairoMoveTo cr 0 480
+	pangoLayoutSetMarkup pl $
+		"<span rise=\"10240\">10</span> " <>
+		"<span rise=\"5012\">5</span> " <>
+		"<span rise=\"0\">0</span> " <>
+		"<span rise=\"-5012\">-5</span> " <>
+		"<span rise=\"-10240\">-10</span> "
+	pangoCairoShowLayout cr pl
+
+	cairoMoveTo cr 0 510
+	pangoLayoutSetMarkup pl $
+		"<span strikethrough=\"true\">" <>
+		"<span strikethrough_color=\"red\">Good-bye!</span>" <>
+		"</span>"
+	pangoCairoShowLayout cr pl
+
+	cairoMoveTo cr 0 530
+	pangoLayoutSetMarkup pl $
+		"<span face=\"虚無\">" <>
+		"<span fallback=\"false\">こんにちは</span> " <>
+		"<span fallback=\"true\">こんにちは</span>" <>
+		"</span>"
+	pangoCairoShowLayout cr pl
+
+	cairoMoveTo cr 0 550
+	pangoLayoutSetMarkup pl $
+		"<span face=\"Source Han Sans\">" <>
+		"<span lang=\"jp\">源ノ角ゴシック 思源黑體 思源黑体</span>\n" <>
+		"<span lang=\"zh-tw\">源ノ角ゴシック 思源黑體 思源黑体</span>\n" <>
+		"<span lang=\"zh\">源ノ角ゴシック 思源黑體 思源黑体</span>" <>
+		"</span>"
+	pangoCairoShowLayout cr pl
+
+	cairoMoveTo cr 0 610
+	pangoLayoutSetMarkup pl $
+		"<span letter_spacing=\"0\">Hello こんにちは\n</span>" <>
+		"<span letter_spacing=\"5120\">Hello こんにちは\n</span>" <>
+		"<span letter_spacing=\"10240\">Hello こんにちは</span>"
+	pangoCairoShowLayout cr pl
+
+	cairoMoveTo cr 0 670
+	pangoLayoutSetMarkup pl $
+		"<span gravity=\"south\">south</span> " <>
+		"<span gravity=\"east\">east</span> " <>
+		"<span gravity=\"north\">north</span> " <>
+		"<span gravity=\"west\">west</span>" -- <>
+--		"<span gravity=\"auto\">auto</span>"
+	pangoCairoShowLayout cr pl
+
+	cairoMoveTo cr 0 710
+	pangoLayoutSetMarkup pl $
+		"<span gravity_hint=\"natural\">" <>
+		"<span gravity=\"south\">south</span> " <>
+		"<span gravity=\"east\">east</span> " <>
+		"<span gravity=\"north\">north</span> " <>
+		"<span gravity=\"west\">west</span> " <>
+--		"<span gravity=\"auto\">auto</span>" <>
+		"</span>"
+	pangoCairoShowLayout cr pl
+
+	cairoMoveTo cr 0 750
+	pangoLayoutSetMarkup pl $
+		"<span gravity_hint=\"strong\">" <>
+		"<span gravity=\"south\">south</span> " <>
+		"<span gravity=\"east\">east</span> " <>
+		"<span gravity=\"north\">north</span> " <>
+		"<span gravity=\"west\">west</span> " <>
+--		"<span gravity=\"auto\">auto</span>" <>
+		"</span>"
+	pangoCairoShowLayout cr pl
+
+	cairoMoveTo cr 0 790
+	pangoLayoutSetMarkup pl $
+		"<span gravity_hint=\"line\">" <>
+		"<span gravity=\"south\">south</span> " <>
+		"<span gravity=\"east\">east</span> " <>
+		"<span gravity=\"north\">north</span> " <>
+		"<span gravity=\"west\">west</span> " <>
+--		"<span gravity=\"auto\">auto</span>" <>
 		"</span>"
 	pangoCairoShowLayout cr pl
 
