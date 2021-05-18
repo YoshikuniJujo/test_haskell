23c3875c6619ea538292fdc6a099a848be5ad010
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2021-05-18 14:13:43 +0900

    try-tribi8als: Complete

--- tribial/try-tribials/src/Complete.hs
+++ tribial/try-tribials/src/Complete.hs
@@ -1,1 +1,15 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
 module Complete where
+
+import Language.Haskell.TH
+
+data Foo = Foo_ Int deriving Show
+
+pattern Foo :: Int -> Foo
+pattern Foo n <- Foo_ n
+
+foo :: DecsQ
+foo = [d| {-# COMPLETE Foo #-} |]
