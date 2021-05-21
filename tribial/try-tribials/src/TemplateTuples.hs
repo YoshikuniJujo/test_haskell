324bee487eb576b01237a22a99b67969a6dba83f
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-05-21 11:04:38 +0900

    try-tribial: GHC.Tuple.Unit

---
+++ tribial/try-tribials/src/TemplateTuples.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module TemplateTuples where
+
+import Language.Haskell.TH
+
+unit :: $(tupleT 0)
+unit = $(tupE [])
+
+singleton :: $(tupleT 1 `appT` conT ''Int)
+singleton = $(tupE [litE $ integerL 123])
+
+two :: $(tupleT 2 `appT` conT ''Int `appT` conT ''Int)
+two = $(tupE [litE $ integerL 123, litE $ integerL 456])
