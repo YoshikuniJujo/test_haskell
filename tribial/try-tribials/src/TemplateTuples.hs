8a8bc0ff5a64542a6a15a98994281c407a7c25a4
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-05-21 11:21:53 +0900

    try-tribial: tupP [_]

--- tribial/try-tribials/src/TemplateTuples.hs
+++ tribial/try-tribials/src/TemplateTuples.hs
@@ -13,3 +13,6 @@ singleton = $(tupE [litE $ integerL 123])
 
 two :: $(tupleT 2 `appT` conT ''Int `appT` conT ''Int)
 two = $(tupE [litE $ integerL 123, litE $ integerL 456])
+
+nakami :: Int
+$(tupP [varP $ mkName "nakami"]) = singleton
