69e07725fdace91e3b3d8b252f581fc21eed9850
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-07-09 10:04:50 +0900

    try-gdk: app/try-decorations.hs: draw red line

--- themes/gui/gdk/try-gdk/app/try-decorations.hs
+++ themes/gui/gdk/try-gdk/app/try-decorations.hs
@@ -11,10 +11,17 @@ import Data.Char
 import Text.Read
 import System.Environment
 
+import Data.Color
+import Graphics.Cairo.Drawing.CairoT
+import Graphics.Cairo.Drawing.Regions
+import Graphics.Cairo.Drawing.Paths
+import Graphics.Cairo.Utilities.Types
+
 import Graphics.Gdk.General
 import Graphics.Gdk.Windows
 import Graphics.Gdk.Events
 import Graphics.Gdk.Values
+import Graphics.Gdk.GdkDrawingContext
 
 import Try.Tools
 
@@ -33,8 +40,18 @@ main = do
 	doWhile_ do
 		threadDelay 100000
 		doWhile $ gdkEventGet >>= \case
+			Just (GdkEventGdkMap m) -> do
+				r <- cairoRegionCreateRectangle $ CairoRectangleIntT 0 0 100 100
+				gdkWindowWithDrawFrame w r \cxt -> do
+					cr <- gdkDrawingContextGetCairoContext cxt
+					cairoSetSourceRgb cr . fromJust $ rgbDouble 0.8 0.2 0.2
+					cairoSetLineWidth cr 5
+					cairoMoveTo cr 10 10
+					cairoLineTo cr 90 90
+					cairoStroke cr
+				pure Nothing
 			Just (GdkEventGdkKeyPress k) -> do
 				kv <- gdkEventKeyKeyval k
 				pure . Just $ kv /= fromIntegral (ord 'q')
-			Just _ -> pure $ Just True
-			Nothing -> pure Nothing
+			Just _ -> pure Nothing
+			Nothing -> pure $ Just True
