69c54c17d5ad73e94fda863d804197484ec80c29
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-07-11 08:43:54 +0900

    try-gdk: tools/mkKeysyms.hs

---
+++ themes/gui/gdk/try-gdk/tools/mkKeysyms.hs
@@ -0,0 +1,17 @@
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Main where
+
+import Data.List
+import System.Process
+
+dstFile :: FilePath
+dstFile = "./data/keysyms.txt"
+
+main :: IO ()
+main = do
+	fp <- (++ "/gdk/gdkkeysyms.h") . drop 2 . head
+		. filter ("gtk-3.0" `isSuffixOf`)
+		. words <$> readProcess "pkg-config" ["--cflags", "gdk-3.0"] ""
+	writeFile dstFile . unlines . map ((!! 1) . words)
+		. filter ("#define " `isPrefixOf`) . lines =<< readFile fp
