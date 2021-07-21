0aae46bad93ca61c88facdc63a3059d9e369a752
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2021-07-22 08:10:51 +0900

    app/simple.hs

---
+++ themes/gui/pango/try-pango/app/simple.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE LambdaCase, OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Main where
+
+import Data.CairoImage
+import Data.JuicyCairo
+import Codec.Picture
+import Graphics.Cairo.Drawing.CairoT
+import Graphics.Cairo.Surfaces.ImageSurfaces
+import Graphics.Cairo.Values
+import Graphics.Pango.Basic.Fonts.PangoFontDescription
+import Graphics.Pango.Basic.LayoutObjects.PangoLayout
+import Graphics.Pango.Basic.VerticalText
+import Graphics.Pango.Rendering.Cairo
+
+import qualified Data.Text as T
+
+main :: IO ()
+main = do
+	s <- cairoImageSurfaceCreate cairoFormatArgb32 300 400
+	cr <- cairoCreate s
+	pl <- pangoCairoCreateLayout cr
+
+	fd <- pangoFontDescriptionNew
+	fd' <- pangoFontDescriptionFreeze fd
+
+	pangoLayoutSet pl . pangoFontDescriptionToNullable $ Just fd'
+	pangoLayoutSet @T.Text pl "Hello, world!\nこんにちは、世界!\n\x1f9a5"
+
+	pangoCairoShowLayout cr =<< pangoLayoutFreeze pl
+
+	cairoImageSurfaceGetCairoImage s >>= \case
+		CairoImageArgb32 a -> writePng "simple.png" $ cairoArgb32ToJuicyRGBA8 a
+		_ -> error "never occur"
