36f317856e70d1e1472ba8e8d11976be54377b11
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2021-08-11 11:06:25 +0900

    try-gdk: app/try-axes.hs: gdkDeviceGetAxes

--- themes/gui/gdk/try-gdk/app/try-axes.hs
+++ themes/gui/gdk/try-gdk/app/try-axes.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DataKinds #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Main where
 
+import Control.Monad
+import System.Environment
+import System.Console.GetOpt
 import Graphics.Gdk.GdkDisplay
 import Graphics.Gdk.GdkSeat
 import Graphics.Gdk.GdkDevice
@@ -10,25 +14,31 @@ import Graphics.Gdk.GdkDevice.GdkAxes
 
 main :: IO ()
 main = do
+	(ss, _as, es) <- getOpt Permute [
+		optPointer, optKeyboard ] <$> getArgs
+	putStrLn `mapM_` es
+
 	dpy <- gdkDisplayOpen ""
 	st <- gdkDisplayGetDefaultSeat dpy
 
-	pnt <- gdkSeatGetPointer st
-	kbd <- gdkSeatGetKeyboard st
+	when (OptPointer `elem` ss) do
+		pnt <- gdkSeatGetPointer st
+		pnts <- gdkDeviceListSlaveDevices pnt
+		printAxis pnt
+		printAxis `mapM_` pnts
 	
-	pnts <- gdkDeviceListSlaveDevices pnt
-	kbds <- gdkDeviceListSlaveDevices kbd
-
-	printAxis pnt
-	printAxis `mapM_` pnts
-
-	printKeys kbd
-	printKeys `mapM_` kbds
+	when (OptKeyboard `elem` ss) do
+		kbd <- gdkSeatGetKeyboard st
+		kbds <- gdkDeviceListSlaveDevices kbd
+		printKeys kbd
+		printKeys `mapM_` kbds
 
 printAxis :: IsGdkDevice d => d 'Pointer -> IO ()
 printAxis d = do
 	putStrLn =<< gdkDeviceGetName d
 	putStrLn . ("\tgdkDeviceGetNAxes: " ++) . show =<< gdkDeviceGetNAxes d
+	putStrLn . ("\tgdkDeviceGetAxes: " ++) . show . gdkAxisFlagList
+		=<< gdkDeviceGetAxes d
 	putStrLn ""
 
 printKeys :: IsGdkDevice d => d 'Keyboard -> IO ()
@@ -36,3 +46,12 @@ printKeys d = do
 	putStrLn =<< gdkDeviceGetName d
 	putStrLn . ("\tgdkDeviceGetNKeys: " ++) . show =<< gdkDeviceGetNKeys d
 	putStrLn ""
+
+data OptSetting
+	= OptPointer
+	| OptKeyboard
+	deriving (Show, Eq)
+
+optPointer, optKeyboard :: OptDescr OptSetting
+optPointer = Option ['p'] ["pointer"] (NoArg OptPointer) "Show pointers"
+optKeyboard = Option ['k'] ["keyboard"] (NoArg OptKeyboard) "Show keyboards"
