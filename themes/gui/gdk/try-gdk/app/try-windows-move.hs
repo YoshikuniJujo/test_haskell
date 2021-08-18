5cba5bb7399d617639209373e67fee3c72287163
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2021-08-18 14:50:50 +0900

    try-gdk: app/try-windows-move.hs: add --delta

--- themes/gui/gdk/try-gdk/app/try-windows-move.hs
+++ themes/gui/gdk/try-gdk/app/try-windows-move.hs
@@ -4,6 +4,10 @@
 
 module Main where
 
+import Foreign.C.Types
+import System.Environment
+import System.Console.GetOpt
+
 import Graphics.Gdk.GdkDisplay
 import Graphics.Gdk.Windows
 import Graphics.Gdk.Windows.GdkWindowAttr
@@ -15,6 +19,9 @@ import Try.Tools
 
 main :: IO ()
 main = do
+	let	opts = [optDelta]
+	(ss, _as, es) <- getOpt Permute opts <$> getArgs
+	let	delta = getDelta ss
 	_dpy <- gdkDisplayOpen ""
 	w <- gdkWindowNew Nothing $ minimalGdkWindowAttr
 		(gdkEventMaskMultiBits [
@@ -29,11 +36,21 @@ main = do
 		GdkEventGdkKeyPress
 			(gdkEventKeyKeyval . gdkEventKey -> c) -> True <$ do
 				let	(dx, dy) = case c of
-						GdkKey_h -> (- 1, 0)
-						GdkKey_j -> (0, 1)
-						GdkKey_k -> (0, - 1)
-						GdkKey_l -> (1, 0)
+						GdkKey_h -> (- delta, 0)
+						GdkKey_j -> (0, delta)
+						GdkKey_k -> (0, - delta)
+						GdkKey_l -> (delta, 0)
 						_ -> (0, 0)
 				(x, y) <- gdkWindowGetRootOrigin w
 				gdkWindowMove w (x + dx) (y + dy)
 		GdkEventGdkAny (gdkEventAny -> e) -> True <$ print e
+
+data Setting = Delta CInt deriving Show
+
+optDelta :: OptDescr Setting
+optDelta = Option ['d'] ["delta"] (ReqArg (Delta . read) "Delta") "Set delta"
+
+getDelta :: [Setting] -> CInt
+getDelta [] = 1
+getDelta (Delta d : _) = d
+getDelta (_ : ss) = getDelta ss
