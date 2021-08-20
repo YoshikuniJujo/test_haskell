6df0f8dcb793561f7c6de96bc4193435d5ef870a
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-08-20 13:37:29 +0900

    try-gdk: app/try-window-attr.hs: set visual

--- themes/gui/gdk/try-gdk/app/try-window-attr.hs
+++ themes/gui/gdk/try-gdk/app/try-window-attr.hs
@@ -27,14 +27,16 @@ import Try.Tools
 
 main :: IO ()
 main = do
-	let	opts = [optTitle, optEvents, optPosition, optSize, optWclass]
+	let	opts = [
+			optTitle, optEvents, optPosition, optSize, optWclass,
+			optVisual ]
 	(ss, _as, es) <- getOpt Permute opts <$> getArgs
 	dpy <- gdkDisplayOpen ""
 	let	scr = gdkDisplayGetDefaultScreen dpy
 	sysv <- gdkScreenGetSystemVisual scr
 	rgbav <- gdkScreenGetRgbaVisual scr
 	putStrLn `mapM_` es
-	let	attr = optsToAttr ss
+	let	attr = optsToAttr sysv (fromMaybe sysv rgbav) ss
 	print attr
 	withGdkWindowAttr attr \x y -> print x >> print y
 	w <- gdkWindowNew Nothing attr
@@ -60,15 +62,18 @@ main = do
 				pure False
 		GdkEventGdkAny (gdkEventAny -> e) -> True <$ print e
 
+data Visual = System | Rgba deriving (Show, Read, Eq)
+
 data OptSetting
 	= OptTitle String
 	| OptEvents [GdkEventMaskSingleBit]
 	| OptPosition (CInt, CInt)
 	| OptSize (CInt, CInt)
 	| OptWclass GdkWindowWindowClass
+	| OptVisual Visual
 	deriving Show
 
-optTitle, optEvents, optPosition, optSize, optWclass :: OptDescr OptSetting
+optTitle, optEvents, optPosition, optSize, optWclass, optVisual :: OptDescr OptSetting
 optTitle = Option ['t'] ["title"] (ReqArg OptTitle "Title") "Set title"
 
 optEvents = Option ['e'] ["events"] (ReqArg (OptEvents . read) "Event masks")
@@ -83,15 +88,23 @@ optSize = Option ['s'] ["size"] (ReqArg (OptSize . read) "(W,H)")
 optWclass = Option ['w'] ["wclass"] (ReqArg (OptWclass . read) "Wclass")
 	"Set wclass"
 
-optsToAttr :: [OptSetting] -> GdkWindowAttr
-optsToAttr [] = minimalGdkWindowAttr
+optVisual = Option ['v'] ["visual"] (ReqArg (OptVisual . read) "Visual")
+	"Set visual"
+
+optsToAttr :: GdkVisual -> GdkVisual -> [OptSetting] -> GdkWindowAttr
+optsToAttr _sysv _rgbav [] = minimalGdkWindowAttr
 	(gdkEventMaskMultiBits [GdkKeyPressMask, GdkFocusChangeMask])
 	100 100 GdkInputOutput GdkWindowToplevel
-optsToAttr (OptTitle t : ss) = (optsToAttr ss) { gdkWindowAttrTitle = Just t }
-optsToAttr (OptEvents es : ss) =
-	(optsToAttr ss) { gdkWindowAttrEventMask = gdkEventMaskMultiBits es }
-optsToAttr (OptPosition (x, y) : ss) =
-	(optsToAttr ss) { gdkWindowAttrX = Just x, gdkWindowAttrY = Just y }
-optsToAttr (OptSize (w, h) : ss) =
-	(optsToAttr ss) { gdkWindowAttrWidth = w, gdkWindowAttrHeight = h }
-optsToAttr (OptWclass w : ss) = (optsToAttr ss) { gdkWindowAttrWclass = w }
+optsToAttr sysv rgbav (OptTitle t : ss) = (optsToAttr sysv rgbav ss) { gdkWindowAttrTitle = Just t }
+optsToAttr sysv rgbav (OptEvents es : ss) =
+	(optsToAttr sysv rgbav ss) { gdkWindowAttrEventMask = gdkEventMaskMultiBits es }
+optsToAttr sysv rgbav (OptPosition (x, y) : ss) =
+	(optsToAttr sysv rgbav ss) { gdkWindowAttrX = Just x, gdkWindowAttrY = Just y }
+optsToAttr sysv rgbav (OptSize (w, h) : ss) =
+	(optsToAttr sysv rgbav ss) { gdkWindowAttrWidth = w, gdkWindowAttrHeight = h }
+optsToAttr sysv rgbav (OptWclass w : ss) =
+	(optsToAttr sysv rgbav ss) { gdkWindowAttrWclass = w }
+optsToAttr sysv rgbav (OptVisual System : ss) =
+	(optsToAttr sysv rgbav ss) { gdkWindowAttrVisual = Just sysv }
+optsToAttr sysv rgbav (OptVisual Rgba : ss) =
+	(optsToAttr sysv rgbav ss) { gdkWindowAttrVisual = Just rgbav }
