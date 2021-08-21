2b80e6af98c78b31289a8e43029e2b878554ef7c
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2021-08-21 21:51:14 +0900

    try-gdk: app/try-event-mask.hs: option --events

--- themes/gui/gdk/try-gdk/app/try-event-mask.hs
+++ themes/gui/gdk/try-gdk/app/try-event-mask.hs
@@ -4,6 +4,9 @@
 
 module Main where
 
+import System.Environment
+import System.Console.GetOpt
+
 import Graphics.Gdk.GdkDisplay
 import Graphics.Gdk.Windows
 import Graphics.Gdk.Windows.GdkWindowAttr
@@ -14,6 +17,10 @@ import Try.Tools
 
 main :: IO ()
 main = do
+	let	opts = [optEvents]
+	(ss, _as, es) <- getOpt Permute opts <$> getArgs
+	putStrLn `mapM_` es
+	print ss
 	_ <- gdkDisplayOpen ""
 	win <- gdkToplevelNew Nothing
 		$ minimalGdkWindowAttr (gdkEventMaskMultiBits []) 400 300
@@ -21,3 +28,11 @@ main = do
 	mainLoop \case
 		GdkEventGdkDelete _d -> pure False
 		GdkEventGdkAny (gdkEventAny -> e) -> True <$ print e
+
+data OptSetting
+	= OptEvents [GdkEventMaskSingleBit]
+	deriving Show
+
+optEvents :: OptDescr OptSetting
+optEvents = Option ['e'] ["events"] (ReqArg (OptEvents . read) "EventMask")
+	"Set event mask"
