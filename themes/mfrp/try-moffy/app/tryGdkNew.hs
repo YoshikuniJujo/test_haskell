b5ea9d6d3168f434188cd9c3a8aff20e6fc10e50
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-09-03 17:48:39 +0900

    try-moffy: app/tryGdkNew.hs: try sameClick

--- themes/mfrp/try-moffy/app/tryGdkNew.hs
+++ themes/mfrp/try-moffy/app/tryGdkNew.hs
@@ -8,14 +8,18 @@ module Main where
 import Control.Concurrent.STM (atomically, newTVar)
 import Control.Moffy
 import Control.Moffy.Run
-import Control.Moffy.Handle
 import Control.Moffy.Event.Window
-import Control.Moffy.Event.Mouse
+import Control.Moffy.Event.DefaultWindow
+import Control.Moffy.Event.Mouse.DefaultWindow
+import Control.Moffy.Handle
+import Control.Moffy.Handle.DefaultWindow
 import Data.Type.Set
 import Data.Map (empty)
 import Trial.TryGdk
 
 import Graphics.Gdk.GdkDisplay
+
+import Trial.Paper
 
 main :: IO ()
 main = do
@@ -23,7 +27,8 @@ main = do
 	i2w <- atomically $ newTVar empty
 	w2i <- atomically $ newTVar empty
 	_ <- gdkDisplayOpen ""
-	print =<< interpretReact @_ @(WindowNew :- MouseDown :- 'Nil)
-		(retry $ handleGdk wid i2w w2i) do
+	(print =<<) . ($ (Nothing :: Maybe WindowId)) $ interpretReactSt @_ @(WindowNew :- DefaultWindowEv :+: MouseDown :- 'Nil)
+		(retrySt $ liftHandle' (handleGdk wid i2w w2i) `mergeSt` handleDefaultWindow) do
 		w <- adjust windowNew
-		adjust $ mouseDown w
+		adjust $ storeDefaultWindow w
+		adjust sameClick
