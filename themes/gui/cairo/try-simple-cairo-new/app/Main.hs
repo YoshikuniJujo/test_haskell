394f9134dedf34747aa383a7ea17ae702b59d408
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2021-09-14 11:36:59 +0900

    try-simple-cairo-new: MakePng

--- themes/gui/cairo/try-simple-cairo-new/app/Main.hs
+++ themes/gui/cairo/try-simple-cairo-new/app/Main.hs
@@ -1,24 +1,14 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE BlockArguments #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Main where
 
 import Data.Maybe
 import Data.Color
-import Data.CairoImage
-import Data.JuicyCairo
-import Codec.Picture
 import Graphics.Cairo.Drawing.CairoT
-import Graphics.Cairo.Surfaces.ImageSurfaces
-import Graphics.Cairo.Values
+import MakePng
 
 main :: IO ()
-main = do
-	sr <- cairoImageSurfaceCreate cairoFormatArgb32 128 128
-	cr <- cairoCreate sr
+main = pngWith "pngs/simple.png" 128 128 \cr -> do
 	cairoSetSourceRgb cr . fromJust $ rgbDouble 0.2 0.6 0.1
 	cairoPaint cr
-	cairoImageSurfaceGetCairoImage sr >>= \case
-		CairoImageArgb32 ci ->
-			writePng "pngs/simple.png" $ cairoArgb32ToJuicyRGBA8 ci
-		_ -> error "never occur"
---
+++ themes/gui/cairo/try-simple-cairo-new/src/MakePng.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE LambdaCase #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module MakePng where
+
+import Data.Int
+import Data.CairoContext
+import Data.CairoImage
+import Data.JuicyCairo
+import Codec.Picture
+import Graphics.Cairo.Drawing.CairoT
+import Graphics.Cairo.Surfaces.ImageSurfaces
+import Graphics.Cairo.Values
+
+pngWith :: FilePath -> Int32 -> Int32 -> (CairoTIO s -> IO ()) -> IO ()
+pngWith fp w h act = do
+	sr <- cairoImageSurfaceCreate cairoFormatArgb32 w h
+	cr <- cairoCreate sr
+	act cr
+	cairoImageSurfaceGetCairoImage sr >>= \case
+		CairoImageArgb32 ci ->
+			writePng fp $ cairoArgb32ToJuicyRGBA8 ci
+		_ -> error "never occur"
