ed1226c24666cdc421c9954c47945c2b2c4c9a1e
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2021-09-15 14:18:07 +0900

    try-simple-cairo-new: app/try-porter-duff.hs

---
+++ themes/gui/cairo/try-simple-cairo-new/app/try-porter-duff.hs
@@ -0,0 +1,68 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE ScopedTypeVariables, TypeApplications #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Main where
+
+import Foreign.C.Types
+import Data.Foldable
+import Data.Maybe
+import Data.Color
+import Data.CairoContext
+import Graphics.Cairo.Drawing.CairoT
+import Graphics.Cairo.Drawing.CairoT.Setting
+import Graphics.Cairo.Drawing.CairoT.Clip
+import Graphics.Cairo.Drawing.CairoT.CairoOperatorT
+import Graphics.Cairo.Drawing.Paths
+import MakePng
+
+main :: IO ()
+main = pngWith "pngs/try-porter-duff-0.png" 768 896 \cr ->
+	for_ (zip [0 ..] operators) \(i :: Int, op) ->
+		tryPorterDuff cr
+			(256 * fromIntegral (i `div` 4))
+			(224 * fromIntegral (i `mod` 4))
+			op
+
+operators :: [Operator]
+operators = [
+	OperatorClear, OperatorSource, OperatorOver, OperatorIn, OperatorOut,
+	OperatorAtop, OperatorDest, OperatorDestOver, OperatorDestIn,
+	OperatorDestOut, OperatorDestAtop, OperatorXor ]
+
+tryPorterDuff :: CairoTIO r -> CDouble -> CDouble -> Operator -> IO ()
+tryPorterDuff cr x0 y0 op = do
+	op0 <- cairoGet @Operator cr
+	cairoRectangle cr l t 192 152
+	cairoClip cr
+	ichimatsu cr l t 192 152
+
+	cairoPushGroup cr
+	cairoRectangle cr l' t' 120 90
+	cairoSetSourceRgba cr . fromJust $ rgbaDouble 0.7 0 0 0.8
+	cairoFill cr
+	cairoSet cr op
+	cairoRectangle cr (l' + 40) (t' + 30) 120 90
+	cairoSetSourceRgba cr . fromJust $ rgbaDouble 0 0 0.9 0.4
+	cairoFill cr
+	cairoPopGroupToSource cr
+
+	cairoPaint cr
+	cairoResetClip cr
+	cairoSet cr op0
+	where l = x0 + 16; t = y0 + 16; l' = l + 16; t' = t + 16
+
+ichimatsu :: CairoTIO s -> CDouble -> CDouble -> CDouble -> CDouble -> IO ()
+ichimatsu cr l t w h =
+	for_ (zip (cycle [0, 1]) [t, t + 32 .. t + h]) \(d, y) -> do
+		for_ (zip (drop d $ cycle [grey, white]) [l, l + 32 .. l + w]) \(c, x) -> do
+			cairoRectangle cr x y 32 32
+			cairoSetSourceRgb cr c
+			cairoFill cr
+
+
+
+grey, white :: Rgb
+grey = fromJust $ rgbDouble 0.7 0.7 0.7
+white = fromJust $ rgbDouble 1 1 1
+
