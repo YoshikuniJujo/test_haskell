bcc9db23f6d50444e0fb4f110a96c43a4f457a32
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-10-04 15:31:46 +0900

    try C Glut

---
+++ other_language/c/opengl/glut/prog1.c
@@ -0,0 +1,43 @@
+#include <GL/glut.h>
+
+void
+display (void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_POLYGON);
+	glColor3d(1.0, 0.0, 0.0);
+	glVertex2d(-0.9, -0.9);
+	glColor3d(0.0, 1.0, 0.0);
+	glVertex2d(0.9, -0.9);
+	glColor3d(0.0, 0.0, 1.0);
+	glVertex2d(0.9, 0.9);
+	glColor3d(1.0, 1.0, 0.0);
+	glVertex2d(-0.9, 0.9);
+	glEnd();
+	glFlush();
+}
+
+void resize(int w, int h)
+{
+	glViewport(0, 0, w, h);
+	glLoadIdentity();
+	glOrtho(-w / 200.0, w / 200.0, -h / 200.0, h / 200.0, -1.0, 1.0);
+}
+
+void init(void)
+{
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+}
+
+int
+main(int argc, char *argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA);
+	glutCreateWindow(argv[0]);
+	glutDisplayFunc(display);
+	glutReshapeFunc(resize);
+	init();
+	glutMainLoop();
+	return 0;
+}
