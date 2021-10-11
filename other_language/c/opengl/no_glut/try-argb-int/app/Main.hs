acf08a18a7dd6256c93ab1fa5c82acdf30de73da
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-10-11 17:11:35 +0900

    try-argb-int: GetTextureImage: getTextureImage

--- other_language/c/opengl/no_glut/try-argb-int/app/Main.hs
+++ other_language/c/opengl/no_glut/try-argb-int/app/Main.hs
@@ -7,19 +7,16 @@ module Main where
 import Prelude hiding (init)
 
 import Foreign.Ptr
-import Foreign.Storable
 import Foreign.Marshal
 import Data.Foldable
 import Data.Word
 import Numeric
 import Graphics.Rendering.OpenGL
-import Graphics.GL
 
 import Lib
 import TryFbo
 import Juicy
-
-#include <GL/glx.h>
+import GetTextureImage
 
 main :: IO ()
 main = do
@@ -33,10 +30,11 @@ main = do
 		print v
 		ctx <- glXCreateContext dpy v Nothing True
 		print ctx
-		glXMakeCurrent dpy Nothing (Just ctx)
-		(fb, cb@(TextureObject cbi)) <- init
+		True <- glXMakeCurrent dpy Nothing (Just ctx)
+		(fb, cb) <- init
 		viewport $= (Position 0 0, Size fboWidth fboHeight)
 		bindFramebuffer Framebuffer $= fb
+
 		clearColor $= Color4 0.8 0.4 0.05 1.0
 		clear [ColorBuffer]
 		color (Color3 1 1 1 :: Color3 GLdouble)
@@ -45,12 +43,13 @@ main = do
 			Vertex2 (- 0.9) 0.9,
 			Vertex2 0.9 0.9 ]
 		flush
+
 		bindFramebuffer Framebuffer $= defaultFramebufferObject
 		allocaBytes (fromIntegral $ fboWidth * fboHeight * 4) \p -> do
-			glGetTextureImage cbi 0 #{const GL_BGRA} #{const GL_UNSIGNED_INT_8_8_8_8_REV}
+			getTextureImage cb 0 glBgra glUnsignedInt_8_8_8_8_rev
 				(fboWidth * fboHeight * 4) p
 			bsss <- groups (fromIntegral fboWidth) . groups 4
-				<$> peekArray (fromIntegral $ fboWidth * fboHeight * 4) p
+				<$> peekArray (fromIntegral $ fboWidth * fboHeight * 4) (castPtr p)
 			print @[[[Word8]]] bsss
 			iss <- groups (fromIntegral fboWidth)
 				<$> peekArray (fromIntegral $ fboWidth * fboHeight) (castPtr p)
@@ -58,7 +57,7 @@ main = do
 			writeArgbPng "foo.png" iss
 			for_ bsss \bss -> do
 				for_ bss \cs -> do
-					let	[b, g, r, a] = fromIntegral <$> cs
+					let	[b, g, r, _a] = fromIntegral <$> cs
 						x :: Int = b + g + r
 					case () of
 						_	| x <= 0xbf  -> putStr " "
@@ -68,12 +67,9 @@ main = do
 							| otherwise -> putStr "#"
 				putStrLn ""
 		
-		glXMakeCurrent dpy Nothing Nothing
+		True <- glXMakeCurrent dpy Nothing Nothing
 		glXDestroyContext dpy ctx
 	xCloseDisplay dpy
-
-makeContext :: Display -> GlXContext -> IO ()
-makeContext dpy ctx = pure ()
 
 groups :: Int -> [a] -> [[a]]
 groups _ [] = []
---
+++ other_language/c/opengl/no_glut/try-argb-int/src/GetTextureImage.hsc
@@ -0,0 +1,22 @@
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module GetTextureImage where
+
+import Foreign.Ptr
+import Graphics.Rendering.OpenGL
+import Graphics.GL
+
+#include <GL/gl.h>
+
+type Format = GLenum
+type Type = GLenum
+type BufSize = GLsizei
+
+getTextureImage ::
+	TextureObject -> Level -> Format -> Type -> BufSize -> Ptr () -> IO ()
+getTextureImage (TextureObject cbi) lvl fmt tp sz p =
+	glGetTextureImage cbi lvl fmt tp sz p
+
+glBgra, glUnsignedInt_8_8_8_8_rev :: GLenum
+glBgra = #{const GL_BGRA}
+glUnsignedInt_8_8_8_8_rev = #{const GL_UNSIGNED_INT_8_8_8_8_REV}
