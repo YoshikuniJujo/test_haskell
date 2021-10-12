fca5a6617ed1e61c472f15e18ebdf168a0d5bd8a
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-10-12 17:49:01 +0900

    try-argb-int: app/try-tenuki.hs: width and height

--- other_language/c/opengl/no_glut/try-argb-int/app/try-tenuki.hs
+++ other_language/c/opengl/no_glut/try-argb-int/app/try-tenuki.hs
@@ -4,21 +4,30 @@
 
 module Main where
 
+import System.Environment
 import Graphics.Rendering.OpenGL
 
 import GetTextureImage
 import PngArgb32
 
 main :: IO ()
-main = writePngArgb32 "tenuki.png" =<< render (Size 512 512) do
-	clearColor $= Color4 1.1 1.0 1.0 1.0
-	clear [ColorBuffer]
-	renderPrimitive Polygon do
-		color $ Color3 @GLdouble 1.0 0.0 0.0
-		vertex @(Vertex2 GLdouble) $ Vertex2 (- 0.9) (- 0.9)
-		color $ Color3 @GLdouble 0.0 1.0 0.0
-		vertex @(Vertex2 GLdouble) $ Vertex2    0.9  (- 0.9)
-		color $ Color3 @GLdouble 0.0 0.0 1.0
-		vertex @(Vertex2 GLdouble) $ Vertex2    0.9     0.9
-		color $ Color3 @GLdouble 1.0 1.0 0.0
-		vertex @(Vertex2 GLdouble) $ Vertex2 (- 0.9)    0.9
+main = do
+	w_ : h_ : _ <- getArgs
+	let	w = read w_
+		h = read h_
+		w' = fromIntegral w
+		h' = fromIntegral h
+	writePngArgb32 "tenuki.png" =<< render (Size w h) do
+		loadIdentity
+		ortho (- w' / 200) (w' / 200) (- h' / 200) (h' / 200) (- 1.0) (1.0)
+		clearColor $= Color4 1.1 1.0 1.0 1.0
+		clear [ColorBuffer]
+		renderPrimitive Polygon do
+			color $ Color3 @GLdouble 1.0 0.0 0.0
+			vertex @(Vertex2 GLdouble) $ Vertex2 (- 0.9) (- 0.9)
+			color $ Color3 @GLdouble 0.0 1.0 0.0
+			vertex @(Vertex2 GLdouble) $ Vertex2    0.9  (- 0.9)
+			color $ Color3 @GLdouble 0.0 0.0 1.0
+			vertex @(Vertex2 GLdouble) $ Vertex2    0.9     0.9
+			color $ Color3 @GLdouble 1.0 1.0 0.0
+			vertex @(Vertex2 GLdouble) $ Vertex2 (- 0.9)    0.9
