63db3f20cf552ab36a2281bae10969e7e7bedd24
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2021-10-14 15:31:01 +0900

    c/vulkan/triangle: Tutorial: Base code: General structure

---
+++ other_language/c/vulkan/triangle/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stdexcept>
+
+class HelloTriangleApplication {
+public:
+	void run() {
+		initVulkan();
+		mainLoop();
+		cleanup();
+	}
+private:
+	void initVulkan() {
+	}
+
+	void mainLoop() {
+	}
+
+	void cleanup() {
+	}
+};
+
+int main()
+{
+	HelloTriangleApplication app;
+
+	try {
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
