a586cfee798c9d5b0079343e942bb15b02127c65
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2021-10-15 12:41:10 +0900

    c/vulkan/triangle: main.cpp: get physical device

--- other_language/c/vulkan/triangle/main.cpp
+++ other_language/c/vulkan/triangle/main.cpp
@@ -71,6 +71,38 @@ private:
 	void initVulkan() {
 		createInstance();
 		setupDebugMessenger();
+		pickPhysicalDevice();
+	}
+
+	void pickPhysicalDevice() {
+		VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+		uint32_t deviceCount = 0;
+		vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+		if (deviceCount == 0) {
+			throw std::runtime_error(
+				"failed to find GPUs with Vulkan support!" );
+		}
+
+		std::vector<VkPhysicalDevice> devices(deviceCount);
+		vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+		for (const auto& device : devices) {
+			if (isDeviceSuitable(device)) {
+				physicalDevice = device;
+				break;
+			}
+		}
+
+		if (physicalDevice == VK_NULL_HANDLE) {
+			throw std::runtime_error("failed to find a suitable GPU!");
+		}
+
+		VkPhysicalDeviceProperties dvProps;
+		vkGetPhysicalDeviceProperties(physicalDevice, &dvProps);
+		std::cout << dvProps.deviceName << std::endl;
+	}
+
+	bool isDeviceSuitable(VkPhysicalDevice device) {
+		return true;
 	}
 
 	void populateDebugMessengerCreateInfo(
