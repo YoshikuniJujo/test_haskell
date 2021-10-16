91874cecf50f95900c0f859c4eb8228bfbcd02d5
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2021-10-16 18:27:33 +0900

    c/vulkan/triangle: main.cpp: make create info for swap chain and set some

--- other_language/c/vulkan/triangle/main.cpp
+++ other_language/c/vulkan/triangle/main.cpp
@@ -132,6 +132,25 @@ private:
 
 		std::cout << extent.width << std::endl;
 		std::cout << extent.height << std::endl;
+
+		uint32_t imageCount =
+			swapChainSupport.capabilities.minImageCount + 1;
+
+		if (	swapChainSupport.capabilities.maxImageCount > 0 &&
+			imageCount > swapChainSupport.capabilities.maxImageCount
+			) {
+			imageCount = swapChainSupport.capabilities.maxImageCount;
+		}
+
+		VkSwapchainCreateInfoKHR createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+		createInfo.surface = surface;
+		createInfo.minImageCount = imageCount;
+		createInfo.imageFormat = surfaceFormat.format;
+		createInfo.imageColorSpace = surfaceFormat.colorSpace;
+		createInfo.imageExtent = extent;
+		createInfo.imageArrayLayers = 1;
+		createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
 	}
 
 	SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device) {
