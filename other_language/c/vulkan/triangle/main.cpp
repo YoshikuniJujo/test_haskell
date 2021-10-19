286b879fc991670a547673eaca60a350ec7e3a4c
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2021-10-19 09:19:33 +0900

    c/vulkan/triangle: main.cpp: Fixed functions: Color blending

--- other_language/c/vulkan/triangle/main.cpp
+++ other_language/c/vulkan/triangle/main.cpp
@@ -196,8 +196,17 @@ private:
 		colorBlendAttachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO;
 		colorBlendAttachment.alphaBlendOp = VK_BLEND_OP_ADD;
 
-		// HERE
-		// VkPipelineColorBlendStateCreateInfo colorBlending{};
+		VkPipelineColorBlendStateCreateInfo colorBlending{};
+		colorBlending.sType =
+			VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+		colorBlending.logicOpEnable = VK_FALSE;
+		colorBlending.logicOp = VK_LOGIC_OP_COPY;
+		colorBlending.attachmentCount = 1;
+		colorBlending.pAttachments = &colorBlendAttachment;
+		colorBlending.blendConstants[0] = 0.0f;
+		colorBlending.blendConstants[1] = 0.0f;
+		colorBlending.blendConstants[2] = 0.0f;
+		colorBlending.blendConstants[3] = 0.0f;
 
 		vkDestroyShaderModule(device, fragShaderModule, nullptr);
 		vkDestroyShaderModule(device, vertShaderModule, nullptr);
