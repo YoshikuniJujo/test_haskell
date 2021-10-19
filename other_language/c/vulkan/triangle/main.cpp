e9c7162a1a9cdd6f7d7eadab1c33793de8c2e561
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2021-10-19 12:47:39 +0900

    c/vulkan/triangle: main.cpp: starting command buffer recording

--- other_language/c/vulkan/triangle/main.cpp
+++ other_language/c/vulkan/triangle/main.cpp
@@ -124,6 +124,20 @@ private:
 			throw std::runtime_error(
 				"failed to allocate command buffers!" );
 		}
+
+		for (size_t i = 0; i < commandBuffers.size(); i++) {
+			VkCommandBufferBeginInfo beginInfo{};
+			beginInfo.sType =
+				VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+			beginInfo.flags = 0;
+			beginInfo.pInheritanceInfo = nullptr;
+
+			if (vkBeginCommandBuffer(commandBuffers[i], &beginInfo)
+				!= VK_SUCCESS) {
+				throw std::runtime_error(
+					"failed to begin recording command buffer!");
+			}
+		}
 	}
 
 	void createCommandPool() {
