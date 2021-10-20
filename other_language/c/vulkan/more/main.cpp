3691da892aeee60cbe40fb90c72bafb8d20a9ecc
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2021-10-20 16:01:41 +0900

    c/vulkan/more: main.cpp: Vertex buffer creation

--- other_language/c/vulkan/more/main.cpp
+++ other_language/c/vulkan/more/main.cpp
@@ -93,7 +93,7 @@ struct Vertex {
 };
 
 const std::vector<Vertex> vertices = {
-	{{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+	{{0.0f, -0.5f}, {1.0f, 1.0f, 1.0f}},
 	{{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
 	{{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
 };
@@ -131,6 +131,8 @@ private:
 	std::vector<VkFence> inFlightFences;
 	std::vector<VkFence> imagesInFlight;
 	size_t currentFrame = 0;
+	VkBuffer vertexBuffer;
+	VkDeviceMemory vertexBufferMemory;
 
 	bool framebufferResized = false;
 
@@ -164,8 +166,73 @@ private:
 		createGraphicsPipeline();
 		createFrameBuffers();
 		createCommandPool();
+		createVertexBuffer();
 		createCommandBuffers();
 		createSyncObjects();
+	}
+
+	void createVertexBuffer() {
+		VkBufferCreateInfo bufferInfo{};
+		bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+		bufferInfo.size = sizeof(vertices[0]) * vertices.size();
+		bufferInfo.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
+		bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+		if (vkCreateBuffer(device, &bufferInfo, nullptr, &vertexBuffer)
+			!= VK_SUCCESS) {
+			throw std::runtime_error(
+				"failed to create vertex buffer!");
+		}
+
+		VkMemoryRequirements memRequirements;
+		vkGetBufferMemoryRequirements(
+			device, vertexBuffer, &memRequirements );
+
+		std::cout << std::endl << "VK MEMORY REQUIREMENTS" << std::endl;
+		std::cout << "size            : " << memRequirements.size << std::endl;
+		std::cout << "alignment       : " << memRequirements.alignment << std::endl;
+		std::cout << "memory type bits: " << memRequirements.memoryTypeBits << std::endl;
+		std::cout << std::endl;
+
+		VkMemoryAllocateInfo allocInfo{};
+		allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+		allocInfo.allocationSize = memRequirements.size;
+		allocInfo.memoryTypeIndex = findMemoryType(
+			memRequirements.memoryTypeBits,
+			VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
+			VK_MEMORY_PROPERTY_HOST_COHERENT_BIT );
+
+		if (vkAllocateMemory(
+			device, &allocInfo, nullptr, &vertexBufferMemory ) !=
+			VK_SUCCESS) {
+			throw std::runtime_error(
+				"failed to allocate vertex buffer memory!" );
+		}
+
+		vkBindBufferMemory(device, vertexBuffer, vertexBufferMemory, 0);
+
+		void* data;
+		vkMapMemory(device,
+			vertexBufferMemory, 0, bufferInfo.size, 0, &data);
+		memcpy(data, vertices.data(), (size_t) bufferInfo.size);
+		vkUnmapMemory(device, vertexBufferMemory);
+	}
+
+	uint32_t findMemoryType(
+		uint32_t typeFilter, VkMemoryPropertyFlags properties ) {
+		VkPhysicalDeviceMemoryProperties memProperties;
+		vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memProperties);
+
+		for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++) {
+			if (	(typeFilter & (1 << i)) &&
+				(memProperties.memoryTypes[i].propertyFlags &
+					properties) == properties ) {
+				return i;
+			}
+		}
+
+		throw std::runtime_error(
+			"failed to find suitable memory type!" );
 	}
 
 	void recreateSwapChain() {
@@ -262,6 +329,12 @@ private:
 
 			vkCmdBindPipeline(commandBuffers[i],
 				VK_PIPELINE_BIND_POINT_GRAPHICS, graphicsPipeline);
+
+			VkBuffer vertexBuffers[] = {vertexBuffer};
+			VkDeviceSize offsets[] = {0};
+			vkCmdBindVertexBuffers(commandBuffers[i],
+				0, 1, vertexBuffers, offsets);
+
 			vkCmdDraw(commandBuffers[i], 3, 1, 0, 0);
 
 			vkCmdEndRenderPass(commandBuffers[i]);
@@ -1230,6 +1303,9 @@ private:
 		std::cout << "BEGIN CLEANUP" << std::endl;
 
 		cleanupSwapChain();
+
+		vkDestroyBuffer(device, vertexBuffer, nullptr);
+		vkFreeMemory(device, vertexBufferMemory, nullptr);
 
 		for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {
 			vkDestroySemaphore(device, renderFinishedSemaphores[i],
