b3f7be173aaf0e41f06001afa253e118ba0ab1a9
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2021-10-21 12:25:32 +0900

    c/vulkan/more: main.cpp: copyBuffer

--- other_language/c/vulkan/more/main.cpp
+++ other_language/c/vulkan/more/main.cpp
@@ -188,7 +188,6 @@ private:
 		vkUnmapMemory(device, vertexBufferMemory);
 	}
 
-
 	void createBuffer(
 		VkDeviceSize size, VkBufferUsageFlags usage,
 		VkMemoryPropertyFlags properties, VkBuffer& buffer,
@@ -228,6 +227,42 @@ private:
 		}
 
 		vkBindBufferMemory(device, vertexBuffer, vertexBufferMemory, 0);
+	}
+
+	void copyBuffer(
+		VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size ) {
+		VkCommandBufferAllocateInfo allocInfo{};
+		allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+		allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+		allocInfo.commandPool = commandPool;
+		allocInfo.commandBufferCount = 1;
+
+		VkCommandBuffer commandBuffer;
+		vkAllocateCommandBuffers(device, &allocInfo, &commandBuffer);
+
+		VkCommandBufferBeginInfo beginInfo{};
+		beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+		beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+
+		vkBeginCommandBuffer(commandBuffer, &beginInfo);
+
+		VkBufferCopy copyRegion{};
+		copyRegion.srcOffset = 0;
+		copyRegion.dstOffset = 0;
+		copyRegion.size = size;
+		vkCmdCopyBuffer(
+			commandBuffer, srcBuffer, dstBuffer, 1, &copyRegion );
+		vkEndCommandBuffer(commandBuffer);
+
+		VkSubmitInfo submitInfo{};
+		submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+		submitInfo.commandBufferCount = 1;
+		submitInfo.pCommandBuffers = &commandBuffer;
+
+		vkQueueSubmit(graphicsQueue, 1, &submitInfo, VK_NULL_HANDLE);
+		vkQueueWaitIdle(graphicsQueue);
+
+		vkFreeCommandBuffers(device, commandPool, 1, &commandBuffer);
 	}
 
 	uint32_t findMemoryType(
