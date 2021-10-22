6a689dee2d7cd150f949b2a44e4bab9105ae5113
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2021-10-22 10:35:31 +0900

    c/vulkan/more: main.cpp: Texture Image

--- other_language/c/vulkan/more/main.cpp
+++ other_language/c/vulkan/more/main.cpp
@@ -151,6 +151,8 @@ private:
 	VkDeviceMemory vertexBufferMemory;
 	VkBuffer indexBuffer;
 	VkDeviceMemory indexBufferMemory;
+	VkImage textureImage;
+	VkDeviceMemory textureImageMemory;
 
 	std::vector<VkBuffer> uniformBuffers;
 	std::vector<VkDeviceMemory> uniformBuffersMemory;
@@ -191,6 +193,7 @@ private:
 		createGraphicsPipeline();
 		createFrameBuffers();
 		createCommandPool();
+		createTextureImage();
 		createVertexBuffer();
 		createIndexBuffer();
 		createUniformBuffers();
@@ -198,6 +201,81 @@ private:
 		createDescriptorSets();
 		createCommandBuffers();
 		createSyncObjects();
+	}
+
+	void createTextureImage() {
+		int textWidth, textHeight, textChannels;
+		stbi_uc* pixels = stbi_load(
+			"textures/texture.jpg", &textWidth, &textHeight,
+			&textChannels, STBI_rgb_alpha);
+		VkDeviceSize imageSize = textWidth * textHeight * 4;
+
+		if (!pixels) {
+			throw std::runtime_error(
+				"failed to load texture image!" );
+		}
+
+		VkBuffer stagingBuffer;
+		VkDeviceMemory stagingBufferMemory;
+
+		createBuffer(
+			imageSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+			VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
+			VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+			stagingBuffer, stagingBufferMemory );
+
+		void* data;
+		vkMapMemory(
+			device, stagingBufferMemory, 0, imageSize, 0, &data );
+		memcpy(data, pixels, static_cast<size_t>(imageSize));
+		vkUnmapMemory(device, stagingBufferMemory);
+
+		stbi_image_free(pixels);
+
+		VkImageCreateInfo imageInfo{};
+		imageInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+		imageInfo.imageType = VK_IMAGE_TYPE_2D;
+		imageInfo.extent.width = static_cast<uint32_t>(textWidth);
+		imageInfo.extent.height = static_cast<uint32_t>(textHeight);
+		imageInfo.extent.depth = 1;
+		imageInfo.mipLevels = 1;
+		imageInfo.arrayLayers = 1;
+		imageInfo.format = VK_FORMAT_R8G8B8A8_SRGB;
+		imageInfo.tiling = VK_IMAGE_TILING_OPTIMAL;
+		imageInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+		imageInfo.usage
+			= VK_IMAGE_USAGE_TRANSFER_DST_BIT
+			| VK_IMAGE_USAGE_SAMPLED_BIT;
+		imageInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+		imageInfo.samples = VK_SAMPLE_COUNT_1_BIT;
+		imageInfo.flags = 0;
+
+		if (vkCreateImage(
+			device, &imageInfo, nullptr, &textureImage )
+			!= VK_SUCCESS) {
+			throw std::runtime_error(
+				"failed to create image!" );
+		}
+
+		VkMemoryRequirements memRequirements;
+		vkGetImageMemoryRequirements(
+			device, textureImage, &memRequirements );
+
+		VkMemoryAllocateInfo allocInfo{};
+		allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+		allocInfo.allocationSize = memRequirements.size;
+		allocInfo.memoryTypeIndex = findMemoryType(
+			memRequirements.memoryTypeBits,
+			VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT );
+
+		if (vkAllocateMemory(device,
+			&allocInfo, nullptr, &textureImageMemory)
+			!= VK_SUCCESS) {
+			throw std::runtime_error(
+				"failed to allocate image memory!" );
+		}
+
+		vkBindImageMemory(device, textureImage, textureImageMemory, 0);
 	}
 
 	void createDescriptorSets() {
