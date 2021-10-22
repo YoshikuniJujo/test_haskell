c036d8194f666116af0ba5ee4636146aaf9e2b91
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2021-10-22 10:51:44 +0900

    c/vulkan/more: main.cpp: abstract image creation

--- other_language/c/vulkan/more/main.cpp
+++ other_language/c/vulkan/more/main.cpp
@@ -232,50 +232,57 @@ private:
 
 		stbi_image_free(pixels);
 
+		createImage(
+			textWidth, textHeight,
+			VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_TILING_OPTIMAL,
+			VK_IMAGE_USAGE_TRANSFER_DST_BIT |
+			VK_IMAGE_USAGE_SAMPLED_BIT,
+			VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+			textureImage, textureImageMemory );
+	}
+
+	void createImage(
+		uint32_t width, uint32_t height, VkFormat format,
+		VkImageTiling tiling, VkImageUsageFlags usage,
+		VkMemoryPropertyFlags properties,
+		VkImage& image, VkDeviceMemory& imageMemory ) {
+
 		VkImageCreateInfo imageInfo{};
 		imageInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
 		imageInfo.imageType = VK_IMAGE_TYPE_2D;
-		imageInfo.extent.width = static_cast<uint32_t>(textWidth);
-		imageInfo.extent.height = static_cast<uint32_t>(textHeight);
+		imageInfo.extent.width = static_cast<uint32_t>(width);
+		imageInfo.extent.height = static_cast<uint32_t>(height);
 		imageInfo.extent.depth = 1;
 		imageInfo.mipLevels = 1;
 		imageInfo.arrayLayers = 1;
-		imageInfo.format = VK_FORMAT_R8G8B8A8_SRGB;
-		imageInfo.tiling = VK_IMAGE_TILING_OPTIMAL;
+		imageInfo.format = format;
+		imageInfo.tiling = tiling;
 		imageInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-		imageInfo.usage
-			= VK_IMAGE_USAGE_TRANSFER_DST_BIT
-			| VK_IMAGE_USAGE_SAMPLED_BIT;
+		imageInfo.usage = usage;
 		imageInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
 		imageInfo.samples = VK_SAMPLE_COUNT_1_BIT;
 		imageInfo.flags = 0;
 
-		if (vkCreateImage(
-			device, &imageInfo, nullptr, &textureImage )
-			!= VK_SUCCESS) {
-			throw std::runtime_error(
-				"failed to create image!" );
+		if (vkCreateImage(device, &imageInfo, nullptr, &image) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create image!");
 		}
 
 		VkMemoryRequirements memRequirements;
-		vkGetImageMemoryRequirements(
-			device, textureImage, &memRequirements );
+		vkGetImageMemoryRequirements(device, image, &memRequirements );
 
 		VkMemoryAllocateInfo allocInfo{};
 		allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
 		allocInfo.allocationSize = memRequirements.size;
 		allocInfo.memoryTypeIndex = findMemoryType(
-			memRequirements.memoryTypeBits,
-			VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT );
+			memRequirements.memoryTypeBits, properties );
 
-		if (vkAllocateMemory(device,
-			&allocInfo, nullptr, &textureImageMemory)
+		if (vkAllocateMemory(device, &allocInfo, nullptr, &imageMemory)
 			!= VK_SUCCESS) {
 			throw std::runtime_error(
 				"failed to allocate image memory!" );
 		}
 
-		vkBindImageMemory(device, textureImage, textureImageMemory, 0);
+		vkBindImageMemory(device, image, imageMemory, 0);
 	}
 
 	void createDescriptorSets() {
