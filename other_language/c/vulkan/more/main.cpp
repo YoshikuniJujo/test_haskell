1aa404dfed1246025c5e8077c1d1942bc65509f5
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2021-10-23 17:16:41 +0900

    c/vulkan/more: main.cpp: Preparing the texture image

--- other_language/c/vulkan/more/main.cpp
+++ other_language/c/vulkan/more/main.cpp
@@ -239,6 +239,21 @@ private:
 			VK_IMAGE_USAGE_SAMPLED_BIT,
 			VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
 			textureImage, textureImageMemory );
+
+		transitionImageLayout(
+			textureImage, VK_FORMAT_R8G8B8A8_SRGB,
+			VK_IMAGE_LAYOUT_UNDEFINED,
+			VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL );
+
+		copyBufferToImage(
+			stagingBuffer, textureImage,
+			static_cast<uint32_t>(textWidth),
+			static_cast<uint32_t>(textHeight) );
+
+		transitionImageLayout(
+			textureImage, VK_FORMAT_R8G8B8A8_SRGB,
+			VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+			VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL );
 	}
 
 	void createImage(
