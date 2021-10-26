d2349e4418dbb86e9dc526999ceddd7d90f0dd93
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2021-10-26 13:24:49 +0900

    c/vulkan/more: tryTinyObjLoader.cpp

--- other_language/c/vulkan/more/tryTinyObjLoader.cpp
+++ other_language/c/vulkan/more/tryTinyObjLoader.cpp
@@ -1,8 +1,39 @@
+#include <iostream>
+
 #define TINYOBJLOADER_IMPLEMENTATION
 #include <tiny_obj_loader.h>
 
 int
 main(int argc, char *argv[])
 {
+	tinyobj::attrib_t attrib;
+	std::vector<tinyobj::shape_t> shapes;
+	std::vector<tinyobj::material_t> materials;
+	std::string warn, err;
+
+	const std::string MODEL_PATH = "viking_room/viking_room.obj";
+
+	if (!tinyobj::LoadObj(
+		&attrib, &shapes, &materials, &warn, &err, MODEL_PATH.c_str())) {
+		throw std::runtime_error(warn + err);
+	}
+
+	int i = 0;
+	for (const auto& shape : shapes) {
+		for (const auto& index : shape.mesh.indices) {
+			if (i < 50) {
+				auto& vs = attrib.vertices;
+				std::cout
+					<< vs[3 * index.vertex_index + 0] << " "
+					<< vs[3 * index.vertex_index + 1] << " "
+					<< vs[3 * index.vertex_index + 2]
+					<< std::endl;
+			}
+			i++;
+		}
+	}
+
+	std::cout << i << std::endl;
+
 	return 0;
 }
