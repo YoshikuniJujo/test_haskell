151174bf4c20a675ad3fbda43e9eb150b7e0b356
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2021-10-27 11:35:33 +0900

    tribial-trials: Shadow

---
+++ tribial/tribial-trials/src/Shadow.hs
@@ -0,0 +1,21 @@
+{-# OPTIONS_GHC -Wname-shadowing #-}
+
+module Shadow where
+
+func1 x y = do x + y
+
+main :: IO ()
+main = do
+	let a = 1
+	print ("a=" ++ (show a))
+
+	let data1 = func1 a 5
+	let a = data1
+	print ("a=" ++ (show a))
+
+	let data1 = func1 a (- 3)
+	let a = data1
+	print ("a=" ++ (show a))
+
+foo :: Int
+foo = let foo = 8 in foo
