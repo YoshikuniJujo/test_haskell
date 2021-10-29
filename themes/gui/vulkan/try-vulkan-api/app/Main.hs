4ee40e6f9f7330d1e4eff46db97e760013736495
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2021-10-29 13:41:21 +0900

    try-vulkan-api: app/Main.hs: createInstance: create VkApplicationInfo

--- themes/gui/vulkan/try-vulkan-api/app/Main.hs
+++ themes/gui/vulkan/try-vulkan-api/app/Main.hs
@@ -1,9 +1,15 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE ScopedTypeVariables, TypeApplications #-}
+{-# LANGUAGE DataKinds #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Main where
 
+import Foreign.C.String
 import Data.Bool
 
+import qualified Graphics.Vulkan as Vk
+import qualified Graphics.Vulkan.Core_1_0 as Vk
 import qualified Graphics.UI.GLFW as Glfw
 
 main :: IO ()
@@ -22,7 +28,20 @@ initWindow = do
 	pure w
 
 initVulkan :: IO ()
-initVulkan = pure ()
+initVulkan = do
+	createInstance
+
+createInstance :: IO ()
+createInstance = withCString "Hello Triangle" \cstr_ht ->
+	withCString "No Engine" \cstr_ne -> do
+		appInfo :: Vk.VkApplicationInfo <- Vk.newVkData \p -> do
+			Vk.writeField @"sType" p Vk.VK_STRUCTURE_TYPE_APPLICATION_INFO
+			Vk.writeField @"pApplicationName" p cstr_ht
+			Vk.writeField @"applicationVersion" p $ Vk._VK_MAKE_VERSION 1 0 0
+			Vk.writeField @"pEngineName" p cstr_ne
+			Vk.writeField @"engineVersion" p $ Vk._VK_MAKE_VERSION 1 0 0
+			Vk.writeField @"apiVersion" p Vk.VK_API_VERSION_1_0
+		pure ()
 
 mainLoop :: Glfw.Window -> IO ()
 mainLoop w = do
