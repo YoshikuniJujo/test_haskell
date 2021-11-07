d27039c76f30599692b54511b6996869a0abeac9
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2021-11-07 15:27:21 +0900

    try-vulkan-api: ReadFile

---
+++ themes/gui/vulkan/try-vulkan-api/src/ReadFile.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE TupleSections #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module ReadFile where
+
+import Foreign.Marshal.Alloc
+import Foreign.ForeignPtr (ForeignPtr)
+import Foreign.Concurrent (newForeignPtr)
+
+import qualified System.IO as S
+
+readFile :: FilePath -> IO (ForeignPtr a, Int)
+readFile fp = do
+	h <- S.openFile fp S.ReadMode
+	n <- fromIntegral <$> S.hFileSize h
+	b <- mallocBytes n
+	n' <- S.hGetBuf h b n
+	(, n') <$> newForeignPtr b (free b)
