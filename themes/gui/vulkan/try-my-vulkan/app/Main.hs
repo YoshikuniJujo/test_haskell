ab63a2b4f2a9b7844c66fee624e5de82cf69bdc2
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2021-11-17 09:41:44 +0900

    try-my-vulkan: app/Main.hs: create GLFW window

--- themes/gui/vulkan/try-my-vulkan/app/Main.hs
+++ themes/gui/vulkan/try-my-vulkan/app/Main.hs
@@ -1,10 +1,26 @@
+{-# LANGUAGE BlockArguments #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Main where
+
+import Control.Monad.Fix
+import Data.Bool
 
 import qualified Graphics.UI.GLFW as Glfw
 
 main :: IO ()
 main = do
 	True <- Glfw.init
+	Glfw.windowHint $ Glfw.WindowHint'ClientAPI Glfw.ClientAPI'NoAPI
+	Just window <- Glfw.createWindow 800 600 "Vulkan window" Nothing Nothing
+
+	while_ do
+		Glfw.pollEvents
+		sc <- Glfw.windowShouldClose window
+		pure $ not sc
+
+	Glfw.destroyWindow window
 	Glfw.terminate
+
+while_ :: Monad m => m Bool -> m ()
+while_ act = fix \f -> bool (pure ()) f =<< act
