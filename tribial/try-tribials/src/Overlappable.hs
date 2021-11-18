129c20fac92fc50771ee03ccddb7b75afe788140
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-11-18 14:07:47 +0900

    try-tribial: Overlappable

---
+++ tribial/try-tribials/src/Overlappable.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Overlappable where
+
+class IsChar c where isChar :: c -> String
+
+instance IsChar Char where isChar _ = "It's Char!"
+
+instance {-# OVERLAPPABLE #-} IsChar a where isChar _ = "It isn't Char."
+
+class SayIsChar c where sayIsChar :: c -> IO ()
+
+instance IsChar c => SayIsChar c where sayIsChar c = putStrLn $ isChar c
