d2d68d663ebf1a19cb69fd91b6ea02127969f175
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2021-11-18 15:34:18 +0900

    try-my-vulkan: show window

--- themes/gui/vulkan/try-my-vulkan/app/try-draw-triangle.hs
+++ themes/gui/vulkan/try-my-vulkan/app/try-draw-triangle.hs
@@ -1,23 +1,46 @@
+{-# LANGUAGE BlockArguments #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Main where
 
+import Control.Monad.Fix
+import Data.Bool
+import Data.Word
+
+import qualified Graphics.UI.GLFW as Glfw
 import qualified Vulkan as Vk
+
+width, height :: Int
+width = 800; height = 600
 
 main :: IO ()
 main = run
 
 run :: IO ()
 run = do
+	w <- initWindow
 	initVulkan
-	mainLoop
-	cleanup
+	mainLoop w
+	cleanup w
+
+initWindow :: IO Glfw.Window
+initWindow = do
+	True <- Glfw.init
+	Glfw.windowHint $ Glfw.WindowHint'ClientAPI Glfw.ClientAPI'NoAPI
+	Glfw.windowHint $ Glfw.WindowHint'Resizable False
+	Just w <- Glfw.createWindow width height "Vulkan" Nothing Nothing
+	pure w
 
 initVulkan :: IO ()
 initVulkan = pure ()
 
-mainLoop :: IO ()
-mainLoop = pure ()
+mainLoop :: Glfw.Window -> IO ()
+mainLoop w = do
+	fix \loop -> bool (pure ()) loop =<< do
+		Glfw.pollEvents
+		not <$> Glfw.windowShouldClose w
 
-cleanup :: IO ()
-cleanup = pure ()
+cleanup :: Glfw.Window  ->IO ()
+cleanup w = do
+	Glfw.destroyWindow w
+	Glfw.terminate
