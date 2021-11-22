251a98606093fbfc7da1e042e264a0f5b8c7afcc
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-11-22 14:36:57 +0900

    c/archive

--- other_language/c/archive/foo.h
+++ other_language/c/archive/foo.h
@@ -1,6 +1,8 @@
 #ifndef _FOO_H
 #define _FOO_H
 
+#define FOO 123
+
 int add(int x, int y);
 
 #endif
--- other_language/c/archive/main.c
+++ other_language/c/archive/main.c
@@ -5,5 +5,6 @@
 int
 main(int argc, char *argv[])
 {
+	printf("%d\n", FOO);
 	printf("%d\n", add(3, 8));
 }
