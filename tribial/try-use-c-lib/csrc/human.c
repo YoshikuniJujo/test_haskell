3aa7f50c1795e179e87e253c7e170fea794b47cc
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-11-24 15:36:50 +0900

    try-use-c-lib: hm_draw_human: HmDrawHumanResult

--- tribial/try-use-c-lib/csrc/human.c
+++ tribial/try-use-c-lib/csrc/human.c
@@ -28,19 +28,38 @@ hm_init_field(void) {
 
 void
 hm_draw_field(void) {
-	for (int i = 0; i < 22; i++) {
+	for (int i = 0; i < FIELD_HEIGHT; i++) {
 		printf("%s\n", hm_field[i]);
 	}
 }
 
 HmDrawHumanResult
+hm_check_inside(int x, int y) {
+	if (	0 <= hm_left(x, y) && hm_right(x, y) < FIELD_WIDTH &&
+		0 <= hm_top(x, y) && hm_bottom(x, y) < FIELD_HEIGHT ) {
+		return HM_DRAW_HUMAN_SUCCESS; }
+	else if (	(0 <= hm_right(x, y) && hm_left(x, y) < FIELD_WIDTH) &&
+			(0 <= hm_bottom(x, y) && hm_top(x, y) < FIELD_HEIGHT) ) {
+		return HM_DRAW_HUMAN_PARTIAL; }
+	else {	return HM_DRAW_HUMAN_OFFSCREEN; }
+}
+
+void
+hm_set_char(int x, int y, char c) {
+	if (0 <= x && x < FIELD_WIDTH && 0 <= y && y < FIELD_HEIGHT) {
+		hm_field[y][x] = c;
+	}
+}
+
+HmDrawHumanResult
 hm_draw_human(int x, int y) {
-	hm_field[y][x] = '\\';
-	hm_field[y][x + 1] = 'o';
-	hm_field[y + 1][x + 1] = 'A';
-	hm_field[y + 1][x + 2] = '\\';
-	hm_field[y + 2][x] = '/';
-	hm_field[y + 2][x + 2] = '\\';
+	hm_set_char(x, y, '\\');
+	hm_set_char(x + 1, y, 'o');
+	hm_set_char(x + 1, y + 1, 'A');
+	hm_set_char(x + 2, y + 1, '\\');
+	hm_set_char(x, y + 2, '/');
+	hm_set_char(x + 2, y + 2, '\\');
 	hm_draw_field();
-	return 0;
+
+	return hm_check_inside(x, y);
 }
