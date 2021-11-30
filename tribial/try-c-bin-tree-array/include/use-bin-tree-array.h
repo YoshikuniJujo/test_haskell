6249b6a7674986cab50619566134f6c79f3983ab
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-11-30 16:22:40 +0900

    try-c-bin-tree-array: allocate and free memory

--- tribial/try-c-bin-tree-array/include/use-bin-tree-array.h
+++ tribial/try-c-bin-tree-array/include/use-bin-tree-array.h
@@ -1,8 +1,18 @@
 #ifndef _USE_BIN_TREE_ARRAY_H
 #define _USE_BIN_TREE_ARRAY_H
 
+#include <stdbool.h>
+#include <stdint.h>
 #include <bin-tree-array.h>
 
 extern int tree[];
+
+typedef struct {
+	bool allocated;
+	int used; } AllocInfo;
+
+extern AllocInfo alloc_info[];
+
+extern uint64_t memory[];
 
 #endif
