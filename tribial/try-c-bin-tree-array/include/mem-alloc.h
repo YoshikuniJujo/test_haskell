043d0d0b23ab3b085cd7e5eb7352cfb8f79dd4e4
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-12-01 11:38:23 +0900

    try-c-bin-tree-array: repair memory allocation

--- tribial/try-c-bin-tree-array/include/mem-alloc.h
+++ tribial/try-c-bin-tree-array/include/mem-alloc.h
@@ -5,6 +5,12 @@
 #include <stdint.h>
 #include <bin-tree-array.h>
 
-extern int tree[];
+typedef struct {
+	bool allocated;
+	int used; } AllocInfo;
+
+extern AllocInfo alloc_info[];
+
+extern uint64_t memory[];
 
 #endif
