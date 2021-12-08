982290faffd8e2c4a0bca45f2232c59c4551284d
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-12-08 13:31:28 +0900

    try-use-c-lib: remove unused source files

--- tribial/try-use-c-lib/include/linked_list.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LINKED_LIST_H
-#define _LINKED_LIST_H
-
-typedef struct Node { void *value; struct Node *next; } Node;
-typedef Node *LinkedList;
-
-LinkedList *ll_list_new(void);
-
-#endif
--- tribial/try-use-c-lib/include/petit_mem_alloc.h
+++
@@ -1,4 +0,0 @@
-#ifndef _PETIT_MEM_ALLOC
-#define _PETIT_MEM_ALLOC
-
-#endif
