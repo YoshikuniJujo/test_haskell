529b269fd8e8661b8152bfd67172b5b3a019d538
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-12-15 15:33:15 +0900

    try-use-c-lib: TryHuman

--- tribial/try-use-c-lib/src/TryHuman.hs
+++ tribial/try-use-c-lib/src/TryHuman.hs
@@ -1,19 +1,9 @@
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables, AllowAmbiguousTypes #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module TryHuman where
 
 import Control.Exception
-import Human.Exception
 
-catchPartial :: IO () -> IO ()
-catchPartial act = act `catch` \(e :: PutHumanPartialError) ->
-	putStrLn "CATCHED" >> print e
-
-catchOffscreen :: IO () -> IO ()
-catchOffscreen act = act `catch` \(e :: PutHumanOffscreenError) ->
-	putStrLn "CATCHED" >> print e
-
-catchOutOfField :: IO () -> IO ()
-catchOutOfField act = act `catch` \(e :: PutHumanOutOfFieldError) -> do
-	putStrLn "CATCHED" >> print e
+catchAndShow :: forall e . Exception e => IO () -> IO ()
+catchAndShow act = act `catch` \(e :: e) -> putStrLn "CATCHED" >> print e
