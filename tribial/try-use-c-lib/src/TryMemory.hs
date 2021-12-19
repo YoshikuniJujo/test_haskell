acd0f1f38edeac1d540679bc8f5cb600093d3090
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2021-12-19 10:05:49 +0900

    try-use-c-lib: TryMemory

---
+++ tribial/try-use-c-lib/src/TryMemory.hs
@@ -0,0 +1,9 @@
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module TryMemory where
+
+import Foreign.Ptr
+import Foreign.C.Types
+
+foreign import ccall "allocate_memory" allocateMemory :: CInt -> IO (Ptr a)
+foreign import ccall "draw_memory" drawMemory :: IO ()
