cca0aeea5a5a389fc13ae4acb93896c4fd839fa2
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-12-20 08:23:27 +0900

    try-use-c-libs: csrc/mem_alloc_draw.c

--- tribial/try-use-c-lib/csrc/mem_alloc_draw.c
+++ tribial/try-use-c-lib/csrc/mem_alloc_draw.c
@@ -4,9 +4,8 @@
 void
 put_used(int i, int sz, char *buf)
 {
-	if (sz == 0) return;
-
 	if (alloc_info[i].allocated) for (int j; j < sz; j++) buf[j] = '*';
+	else if (alloc_info[i].used == 0) /* DO NOTHING */;
 	else {	int hf = sz >> 1;
 		put_used(LEFT_CHILD(i), hf, buf);
 		put_used(RIGHT_CHILD(i), hf, buf + hf); }
@@ -16,10 +15,10 @@ void
 draw_memory(void)
 {
 	char buf[(1 << 7) + 1];
-
 	int i;
 	for (i = 0; i < (1 << 7); i++) buf[i] = '.';
 	buf[i] = '\0';
-	put_used(0, 128, buf);
-	printf("%s\n", buf);
+	put_used(0, 1 << 7, buf);
+	printf("%.64s\n", buf);
+	printf("%s\n", buf + 64);
 }
