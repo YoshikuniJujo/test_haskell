3150727fd0cf44c9d00d1408c69d7b697d77a7c7
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2022-01-13 13:41:51 +0900

    try-swizzle: crr2Fun

--- themes/swizzle/try-swizzle/src/Curry.hs
+++ themes/swizzle/try-swizzle/src/Curry.hs
@@ -1,3 +1,12 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Curry where
+
+import Language.Haskell.TH
+
+crr2Fun :: Q Dec
+crr2Fun = newName "f" >>= \f -> newName "x" >>= \x -> newName "y" >>= \y ->
+	funD (mkName "crr2") [
+		clause [varP f, varP x, varP y]
+			(normalB $ varE f `appE` tupE [	varE x, varE y])
+			[] ]
