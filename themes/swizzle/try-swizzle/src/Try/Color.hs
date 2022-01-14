6bd11a4a84d28ce35618991dd44a6bce43a5187b
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2022-01-14 11:21:05 +0900

    try-swizzle: Try.Color and Try.Flip

---
+++ themes/swizzle/try-swizzle/src/Try/Color.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Try.Color where
+
+import GHC.Generics
+
+import qualified SwizzleClass as S
+import SwizzleFun
+import Curry
+
+swizzle "yzwx"
+
+newtype Red = Red Double deriving Show
+newtype Green = Green Double deriving Show
+newtype Blue = Blue Double deriving Show
+newtype Alpha = Alpha Double deriving Show
+
+data Argb = Argb {
+	argbAlpha :: Alpha,
+	argbRed :: Red,
+	argbGreen :: Green,
+	argbBlue :: Blue }
+	deriving (Show, Generic)
+
+instance S.Swizzle1 Argb where type X Argb = Alpha
+instance S.Swizzle2 Argb where type Y Argb = Red
+instance S.Swizzle3 Argb where type Z Argb = Green
+instance S.Swizzle4 Argb where type W Argb = Blue
+
+sampleArgb :: Argb
+sampleArgb = Argb (Alpha 0.5) (Red 0.9) (Green 0.3) (Blue 0.2)
+
+printRgba :: Red -> Green -> Blue -> Alpha -> IO ()
+printRgba (Red r) (Green g) (Blue b) (Alpha a) = do
+	putStrLn $ "red  : " ++ show r
+	putStrLn $ "green: " ++ show g
+	putStrLn $ "blue : " ++ show b
+	putStrLn $ "alpha: " ++ show a
+
+printArgb :: (
+	S.Swizzle4 argb,
+	S.X argb ~ Alpha, S.Y argb ~ Red, S.Z argb ~ Green, S.W argb ~ Blue ) =>
+	argb -> IO ()
+printArgb = unc4 printRgba . yzwx
---
+++ themes/swizzle/try-swizzle/src/Try/Flip.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Try.Flip where
+
+import Data.List
+
+import Curry
+import SwizzleFun
+
+concat <$> swizzle `mapM` permutations "xyz"
+
+flip13 :: (a -> b -> c -> r) -> c -> b -> a -> r
+flip13 f = crr3 $ unc3 f . zyx
+
+flip' :: (b -> c -> a -> r) -> a -> b -> c -> r
+flip' f = crr3 $ unc3 f . yzx
+
+flip'' :: (c -> a -> b -> r) -> a -> b -> c -> r
+flip'' f = crr3 $ unc3 f . zxy
