dce3a225cf3ae3c4f51fdfbaadf2dffc22229634
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2022-01-21 09:49:03 +0900

    tribial-trials: LambdaCube: MapTypeVal

--- tribial/tribial-trials/src/LambdaCube.hs
+++ tribial/tribial-trials/src/LambdaCube.hs
@@ -1,8 +1,12 @@
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE ScopedTypeVariables, TypeApplications, RankNTypes #-}
+{-# LANGUAGE KindSignatures, DataKinds, ConstraintKinds, TypeOperators #-}
+{-# LANGUAGE MultiParamTypeClasses, AllowAmbiguousTypes, InstanceSigs #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module LambdaCube where
+
+import Data.Kind
 
 class TypeName a where typeName :: String
 
@@ -12,3 +16,28 @@ instance TypeName Bool where typeName = "Bool"
 
 typeNameDouble :: String
 typeNameDouble = typeName @Double
+
+class MapTypeVal c (as :: [Type]) where mapTypeVal :: (forall a . c a => b) -> [b]
+instance MapTypeVal c '[] where mapTypeVal _ = []
+
+{-
+instance (c a, MapTypeVal c as) => MapTypeVal c (a ': as) where
+	mapTypeVal :: (forall a . c a => b) -> [b]
+	mapTypeVal x = x @a : mapTypeVal @c @as x
+	-}
+
+--
+
+class TypeName2 a where typeName2 :: a -> String
+instance TypeName2 Int where typeName2 _ = "Int"
+instance TypeName2 Double where typeName2 _ = "Double"
+instance TypeName2 Bool where typeName2 _ = "Bool"
+
+class MapTypeVal2 c (as :: [Type]) where mapTypeVal2 :: (forall a . c a => a -> b) -> [b]
+instance MapTypeVal2 c '[] where mapTypeVal2 _ = []
+
+instance (c a, MapTypeVal2 c as) => MapTypeVal2 c (a ': as) where
+	mapTypeVal2 x = x (undefined :: a) : mapTypeVal2 @c @as x
+
+typeName' :: forall a . TypeName a => a -> String
+typeName' _ = typeName @a
