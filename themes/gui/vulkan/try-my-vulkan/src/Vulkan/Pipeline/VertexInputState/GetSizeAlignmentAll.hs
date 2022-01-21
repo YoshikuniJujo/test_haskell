195e7cd03bcd12ee9c54e0b0f9c996c6fabb8829
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2022-01-21 16:29:37 +0900

    try-my-vulkan: use size-alignment-list

--- themes/gui/vulkan/try-my-vulkan/src/Vulkan/Pipeline/VertexInputState/GetSizeAlignmentAll.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE AllowAmbiguousTypes #-}
-{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
-
-module Vulkan.Pipeline.VertexInputState.GetSizeAlignmentAll where
-
-import Vulkan.Pipeline.VertexInputState.GetSizeAlignment
-
-class FindSizeAlignmentAll a where
-	findSizeAlignmentAll :: [(Size, Alignment)]
