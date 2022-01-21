2a2c502099ad750ded6479ccda73679f4adc6ca0
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2022-01-21 15:01:46 +0900

    try-size-alignment: move to size-alignment-list

--- themes/memory/try-size-alignment/app/Main.hs
+++ themes/memory/try-size-alignment/app/Main.hs
@@ -1,4 +1,6 @@
 module Main where
 
+import Foreign.Storable.SizeAlignment
+
 main :: IO ()
 main = putStrLn "Slozsoft"
---
+++ themes/memory/try-size-alignment/src/Try/SizeAlignment.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleContexts, FlexibleInstances, UndecidableInstances #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Try.SizeAlignment where
+
+import GHC.Generics
+import Foreign.Storable.SizeAlignment
+
+data Foo = Foo Bool Int Double deriving (Show, Generic)
+instance SizeAlignmentList Foo
+instance MapStorableUntil t '[Bool, Int, Double] => SizeAlignmentListUntil t Foo
