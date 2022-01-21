776c6e034a980a98cb9b8f054a5df3e42911969f
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2022-01-21 10:10:42 +0900

    LambdaCube: sampleTypeSizes

--- tribial/tribial-trials/src/LambdaCube.hs
+++ tribial/tribial-trials/src/LambdaCube.hs
@@ -6,6 +6,7 @@
 
 module LambdaCube where
 
+import Foreign.Storable
 import Data.Kind
 
 class TypeName a where typeName :: String
@@ -41,3 +42,14 @@ instance (c a, MapTypeVal2 c as) => MapTypeVal2 c (a ': as) where
 
 typeName' :: forall a . TypeName a => a -> String
 typeName' _ = typeName @a
+
+{-
+convert :: forall c a b . c a => (forall a . c a => b) -> a -> b
+convert tn _ = tn @a
+-}
+
+sampleTypeNames :: [String]
+sampleTypeNames = mapTypeVal2 @TypeName @[Int, Double, Bool] typeName'
+
+sampleTypeSizes :: [Int]
+sampleTypeSizes = mapTypeVal2 @Storable @[Int, Double, Bool, ()] sizeOf
