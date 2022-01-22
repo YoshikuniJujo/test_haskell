a4b1dbccdd34eb94f4b4f7d7a9410255b304dc04
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2022-01-22 09:41:16 +0900

    try-my-vulkan: Vulkan.Pipeline.VertexInputState.BindingStrideList

--- themes/gui/vulkan/try-my-vulkan/src/Vulkan/Pipeline/VertexInputState/BindingStrideList.hs
+++ themes/gui/vulkan/try-my-vulkan/src/Vulkan/Pipeline/VertexInputState/BindingStrideList.hs
@@ -1,21 +1,34 @@
 {-# LANGUAGE ScopedTypeVariables, TypeApplications #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE KindSignatures, TypeOperators #-}
+{-# LANGUAGE DefaultSignatures #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE FlexibleContexts, FlexibleInstances, UndecidableInstances #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Vulkan.Pipeline.VertexInputState.BindingStrideList where
 
+import GHC.Generics
 import Foreign.Storable.SizeAlignment
 import Data.Kind
 
 import Vulkan.Pipeline.VertexInputState.Flatten
 
-class BindingStrideListList (ts :: [Type]) where
-	bindStrideListList :: [SizeAlignment]
+class BindingStrideList a where
+	bindingStrideList :: [SizeAlignment]
 
-instance BindingStrideListList '[] where bindStrideListList = []
+	default bindingStrideList ::
+		BindingStrideListList (Flatten (Rep a)) => [SizeAlignment]
+	bindingStrideList = bindingStrideListList @(Flatten (Rep a))
+
+instance (BindingStrideListList (Flatten (Rep a))) => BindingStrideList a
+
+class BindingStrideListList (ts :: [Type]) where
+	bindingStrideListList :: [SizeAlignment]
+
+instance BindingStrideListList '[] where bindingStrideListList = []
 
 instance (SizeAlignmentList t, BindingStrideListList ts) =>
 	BindingStrideListList (t ': ts) where
-	bindStrideListList = wholeSizeAlignment @t : bindStrideListList @ts
+	bindingStrideListList =
+		wholeSizeAlignment @t : bindingStrideListList @ts
