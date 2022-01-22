4b8fe76dae1b9f4b2dff4f298707c493f5f4d789
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2022-01-22 14:27:03 +0900

    tribial-trials: LambdaCube: try and error

--- tribial/tribial-trials/src/LambdaCube.hs
+++ tribial/tribial-trials/src/LambdaCube.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE ScopedTypeVariables, TypeApplications, RankNTypes #-}
 {-# LANGUAGE KindSignatures, DataKinds, ConstraintKinds, TypeOperators #-}
 {-# LANGUAGE MultiParamTypeClasses, AllowAmbiguousTypes, InstanceSigs #-}
-{-# LANGUAGE FlexibleContexts, FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts, FlexibleInstances, UndecidableInstances #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module LambdaCube where
@@ -18,14 +18,47 @@ instance TypeName Bool where typeName = "Bool"
 typeNameDouble :: String
 typeNameDouble = typeName @Double
 
-class MapTypeVal c (as :: [Type]) where mapTypeVal :: (forall a . c a => b) -> [b]
-instance MapTypeVal c '[] where mapTypeVal _ = []
+class MapTypeVal (c :: Type -> Constraint) (as :: [Type]) where
+	mapTypeVal :: (forall a . c a => b) -> [b]
 
 {-
-instance (c a, MapTypeVal c as) => MapTypeVal c (a ': as) where
-	mapTypeVal :: (forall a . c a => b) -> [b]
-	mapTypeVal x = x @a : mapTypeVal @c @as x
+instance (MapTypeVal2 c as) => MapTypeVal c as where
+	mapTypeVal x = mapTypeVal2 @c @as y
+		where
+		y _ = x
+		-}
+
+-- class MapTypeVal c (as :: [Type]) where mapTypeVal :: (forall a . b) -> [b]
+instance MapTypeVal c '[] where mapTypeVal _ = []
+
+sampleTypeNames2 :: [String]
+sampleTypeNames2 = mapTypeVal2 @TypeName @'[Int, Double] tn
+	where
+	tn :: forall a . TypeName a => a -> String
+	tn _ = typeName @a
+
+typeNameList :: forall as . MapTypeVal2 TypeName as => [String]
+typeNameList = mapTypeVal2 @TypeName @as tn
+	where
+	tn :: forall a . TypeName a => a -> String
+	tn _ = typeName @a
+
+{-
+sampleTypeNames3 :: [String]
+sampleTypeNames3 = mapTypeVal @TypeName @'[Int, Double] (tn undefined)
+	where
+	tn :: forall a . TypeName a => a -> String
+	tn _ = typeName @a
 	-}
+
+instance (c a, MapTypeVal2 c as) => MapTypeVal c (a ': as) where
+-- instance (MapTypeVal c as) => MapTypeVal c (a ': as) where
+--	mapTypeVal :: forall a' b . (c a' => b) -> [b]
+--	mapTypeVal x = y @a undefined : mapTypeVal @c @as (y undefined)
+	mapTypeVal x = x @a : mapTypeVal2 @c @as y
+		where
+--		y :: forall a b . c a => a -> b
+		y _ = x @a
 
 --
 
@@ -38,7 +71,8 @@ class MapTypeVal2 c (as :: [Type]) where mapTypeVal2 :: (forall a . c a => a -> 
 instance MapTypeVal2 c '[] where mapTypeVal2 _ = []
 
 instance (c a, MapTypeVal2 c as) => MapTypeVal2 c (a ': as) where
-	mapTypeVal2 x = x (undefined :: a) : mapTypeVal2 @c @as x
+--	mapTypeVal2 x = x (undefined :: a) : mapTypeVal2 @c @as x
+	mapTypeVal2 x = x @a undefined : mapTypeVal2 @c @as x
 
 typeName' :: forall a . TypeName a => a -> String
 typeName' _ = typeName @a
