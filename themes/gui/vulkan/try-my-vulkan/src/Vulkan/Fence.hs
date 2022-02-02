b1449ff9dd40c6c9c70ad1ffdbfedcabaed6fa42
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2022-02-02 14:38:48 +0900

    try-my-vulkan: Vulkan.Fence: create and destroy Fence

---
+++ themes/gui/vulkan/try-my-vulkan/src/Vulkan/Fence.hs
@@ -0,0 +1,71 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Vulkan.Fence where
+
+import Foreign.Ptr
+import Foreign.ForeignPtr
+import Foreign.Marshal
+import Foreign.Storable
+import Control.Monad.Cont
+
+import Vulkan.Base
+import Vulkan.Exception
+import Vulkan.AllocationCallbacks
+import Vulkan.Device
+import Vulkan.FenceCreateFlagBits
+
+import qualified Vulkan.AllocationCallbacks.Internal as I
+import qualified Vulkan.Fence.Internal as I
+
+data CreateInfo n = CreateInfo {
+	createInfoNext :: Maybe n,
+	createInfoFlags :: FenceCreateFlags }
+	deriving Show
+
+createInfoToC :: Pointable n => CreateInfo n -> ContT r IO I.CreateInfo
+createInfoToC CreateInfo {
+	createInfoNext = mnxt,
+	createInfoFlags = flgs } = do
+	(castPtr -> pnxt) <- ContT $ withMaybePointer mnxt
+	pure I.CreateInfo {
+		I.createInfoSType = (),
+		I.createInfoPNext = pnxt,
+		I.createInfoFlags = flgs }
+
+data FenceTag
+newtype Fence = Fence (Ptr FenceTag) deriving (Show, Storable)
+
+fenceNullHandle :: Fence
+fenceNullHandle = Fence NullHandle
+
+create :: (Storable n, Storable n') =>
+	Device -> CreateInfo n -> Maybe (AllocationCallbacks n') -> IO Fence
+create dvc ci mac = ($ pure) $ runContT do
+	I.CreateInfo_ fci <- createInfoToC ci
+	pci <- ContT $ withForeignPtr fci
+	pac <- case mac of
+		Nothing -> pure NullPtr
+		Just ac -> ContT $ withAllocationCallbacksPtr ac
+	pf <- ContT alloca
+	lift do	r <- c_vkCreateFence dvc pci pac pf
+		throwUnlessSuccess r
+		peek pf
+
+foreign import ccall "vkCreateFence" c_vkCreateFence ::
+	Device -> Ptr I.CreateInfo -> Ptr I.AllocationCallbacks ->
+	Ptr Fence -> IO Result
+
+destroy :: Storable n =>
+	Device -> Fence -> Maybe (AllocationCallbacks n) -> IO ()
+destroy dvc fnc mac = ($ pure) $ runContT do
+	pac <- case mac of
+		Nothing -> pure NullPtr
+		Just ac -> ContT $ withAllocationCallbacksPtr ac
+	lift $ c_vkDestroyFence dvc fnc pac
+
+foreign import ccall "vkDestroyFence" c_vkDestroyFence ::
+	Device -> Fence -> Ptr I.AllocationCallbacks -> IO ()
