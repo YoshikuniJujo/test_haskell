758eb96acdd9ab3e89b7d2fd8ea043bbbd9c1163
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2022-02-22 16:34:37 +0900

    try-my-vulkan-snd: Vulkan.Ext.DebugUtils: Label

---
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Data/Color.hs
@@ -0,0 +1,5 @@
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Data.Color where
+
+data Rgba = RgbaFloat Float Float Float Float deriving Show
--- themes/gui/vulkan/try-my-vulkan-snd/src/Vulkan/Ext/DebugUtils.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Vulkan/Ext/DebugUtils.hs
@@ -1,15 +1,45 @@
 {-# LANGUAGE CApiFFI #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE ViewPatterns #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Vulkan.Ext.DebugUtils where
 
+import Foreign.Ptr
 import Foreign.C.String
+import Control.Monad.Cont
 import Data.String
 import System.IO.Unsafe
+
+import qualified Data.Text as T
+
+import Data.Color
+import Vulkan.Base
+
+import qualified Vulkan.Ext.DebugUtils.Core as C
 
 foreign import capi "vulkan/vulkan.h value VK_EXT_DEBUG_UTILS_EXTENSION_NAME"
 	c_extensionName :: CString
 
 extensionName :: IsString s => s
 extensionName = unsafePerformIO $ fromString <$> peekCString c_extensionName
+
+data Label n = Label {
+	labelNext :: Maybe n,
+	labelLabelName :: T.Text,
+	labelColor :: Rgba }
+	deriving Show
+
+labelToCore :: Pointable n => Label n -> ContT r IO C.Label
+labelToCore Label {
+	labelNext = mnxt,
+	labelLabelName = ln,
+	labelColor = RgbaFloat r g b a } = do
+	(castPtr -> pnxt) <- maybeToPointer mnxt
+	cln <- textToCString ln
+	pure C.Label {
+		C.labelSType = (),
+		C.labelPNext = pnxt,
+		C.labelPLabelName = cln,
+		C.labelColor = [r, g, b, a] }
