e87a74060ab642d970fb740048ba82a2a0a51f03
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2022-02-26 19:58:18 +0900

    try-my-vulkan-snd: tools/VulkanPhysicalDeviceStruct.hs: halfway

--- themes/gui/vulkan/try-my-vulkan-snd/tools/VulkanPhysicalDeviceStruct.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/tools/VulkanPhysicalDeviceStruct.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE LambdaCase #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
@@ -5,20 +6,52 @@ module VulkanPhysicalDeviceStruct where
 
 import Data.List
 import Data.Char
+import Text.Nowdoc
 
 vulkanCoreH :: FilePath
 vulkanCoreH = "/usr/include/vulkan/vulkan_core.h"
 
+moduleName :: String
+moduleName = "Vulkan.PhysicalDevice.Struct"
+
+directory :: String -> FilePath
+directory mn = "../src/" ++ intercalate "/" (init $ sep '.' mn)
+
+sourceFile :: String -> FilePath
+sourceFile mn = directory mn ++ "/Struct.hsc"
+
+hsName :: String
+hsName = "Limits"
+
+cName :: String -> String -> String
+cName mn hsn = "Vk" ++ concat (tail . init $ sep '.' mn) ++ hsn
+
 data Name = Atom String | List String Int deriving Show
+
+header :: String -> String
+header mn = [nowdoc|
+{-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module |] ++ mn ++ [nowdoc| where
+
+import Foreign.Storable
+import Foreicn.C.Struct
+
+|]
 
 make :: IO ()
 make = do
 	vch <- readFile vulkanCoreH
 	let	ds = takeDefinition "VkPhysicalDeviceLimits" $ lines vch
+	putStrLn $ directory moduleName
+	putStrLn $ sourceFile moduleName
+	putStr $ header moduleName
 	putStr $
+		"struct \"" ++ hsName ++ "\" #{size " ++ cName moduleName hsName ++
+		"}\n\t\t#{alignment " ++ cName moduleName hsName ++ "} [\n" ++
 		intercalate ",\n" (uncurry (field1 "VkPhysicalDeviceLimits") <$> ds) ++
-		" ]\n" ++
-		"\t[''Show, ''Storable]\n"
+		" ]\n\t[''Show, ''Storable]\n"
 
 field1 :: String -> String -> Name -> String
 field1 csn t (Atom n) = "\t(\"" ++ n ++ "\", ''#{type " ++ t ++ "},\n\t\t[| #{peek " ++
@@ -35,7 +68,7 @@ takeDefinition nm = map ((\[t, n] -> (t, makeName $ init n)) . words)
 
 makeName :: String -> Name
 makeName str = case span (/= '[') str of
-	(t, "") -> Atom (t ++ "foo")
+	(t, "") -> Atom t
 	(t, '[' : n_) -> List t . read $ init n_
 	_ -> error "bad list"
 
