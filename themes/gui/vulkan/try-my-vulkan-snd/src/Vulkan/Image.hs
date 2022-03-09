8aa81fd94285b8547b6bfb7ca1868d09a6e024da
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2022-03-09 17:59:32 +0900

    try-my-vulkan-snd: Vulkan.Image: SubresourceRange

---
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Vulkan/Image.hs
@@ -0,0 +1,30 @@
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Vulkan.Image where
+
+import Data.Word
+
+import Vulkan.Image.Enum
+
+import qualified Vulkan.Image.Core as C
+
+data SubresourceRange = SubresourceRange {
+	subresourceRangeAspectMask :: AspectFlags,
+	subresourceRangeBaseMipLevel :: Word32,
+	subresourceRangeLevelCount :: Word32,
+	subresourceRangeBaseArrayLayer :: Word32,
+	subresourceRangeLayerCount :: Word32 }
+	deriving Show
+
+subresourceRangeToCore :: SubresourceRange -> C.SubresourceRange
+subresourceRangeToCore SubresourceRange {
+	subresourceRangeAspectMask = AspectFlagBits am,
+	subresourceRangeBaseMipLevel = bmlv,
+	subresourceRangeLevelCount = lvc,
+	subresourceRangeBaseArrayLayer = baly,
+	subresourceRangeLayerCount = lyc } = C.SubresourceRange {
+		C.subresourceRangeAspectMask = am,
+		C.subresourceRangeBaseMipLevel = bmlv,
+		C.subresourceRangeLevelCount = lvc,
+		C.subresourceRangeBaseArrayLayer = baly,
+		C.subresourceRangeLayerCount = lyc }
--- themes/gui/vulkan/try-my-vulkan-snd/src/Vulkan/ImageView.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Vulkan/ImageView.hs
@@ -1,11 +1,19 @@
+{-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE ViewPatterns #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Vulkan.ImageView where
 
+import Foreign.Ptr
+import Foreign.ForeignPtr
+import Control.Monad.Cont
+
 import Vulkan
 import Vulkan.Enum
-import Vulkan.ImageView.Enum
+import Vulkan.Base
+import Vulkan.Image
 import Vulkan.Component
+import Vulkan.ImageView.Enum
 
 import qualified Vulkan.ImageView.Core as C
 
@@ -15,6 +23,28 @@ data CreateInfo n = CreateInfo {
 	createInfoImage :: Image,
 	createInfoViewType :: Type,
 	createInfoFormat :: Format,
-	createInfoComponents :: Mapping
-	}
+	createInfoComponents :: Mapping,
+	createInfoSubresourceRange :: SubresourceRange }
 	deriving Show
+
+createInfoToC :: Pointable n => CreateInfo n -> ContT r IO (Ptr C.CreateInfo)
+createInfoToC CreateInfo {
+	createInfoNext = mnxt,
+	createInfoFlags = CreateFlagBits flgs,
+	createInfoImage = Image img,
+	createInfoViewType = Type tp,
+	createInfoFormat = Format fmt,
+	createInfoComponents = cpns,
+	createInfoSubresourceRange = srr
+	} = do
+	(castPtr -> pnxt) <- maybeToPointer mnxt
+	let C.CreateInfo_ fCreateInfo = C.CreateInfo {
+		C.createInfoSType = (),
+		C.createInfoPNext = pnxt,
+		C.createInfoFlags = flgs,
+		C.createInfoImage = img,
+		C.createInfoViewType = tp,
+		C.createInfoFormat = fmt,
+		C.createInfoComponents = mappingToCore cpns,
+		C.createInfoSubresourceRange = subresourceRangeToCore srr }
+	ContT $ withForeignPtr fCreateInfo
