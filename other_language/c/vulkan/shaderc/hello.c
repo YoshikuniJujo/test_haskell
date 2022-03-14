a25301c7a738ec8eabe5d2ca281aafa5d903b7b8
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2022-03-14 09:34:01 +0900

    c/vulkan/shaderc: try compile

--- other_language/c/vulkan/shaderc/hello.c
+++ other_language/c/vulkan/shaderc/hello.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <shaderc/shaderc.h>
 
 int
@@ -12,10 +13,27 @@ main(int argc, char *argv[])
 		compiler, "#version 450\nvoid main() {}", 27,
 		shaderc_glsl_vertex_shader, "main.vert", "main", NULL);
 
-	printf("length            : %ld\n", shaderc_result_get_length(result));
-	printf("num warnings      : %ld\n", shaderc_result_get_num_warnings(result));
-	printf("num errors        : %ld\n", shaderc_result_get_num_errors(result));
-	printf("compilation status: %d\n",  shaderc_result_get_compilation_status(result));
+	printf("length            : %ld\n",
+		shaderc_result_get_length(result));
+	printf("num warnings      : %ld\n",
+		shaderc_result_get_num_warnings(result));
+	printf("num errors        : %ld\n",
+		shaderc_result_get_num_errors(result));
+	printf("compilation status: %d\n",
+		shaderc_result_get_compilation_status(result));
+	printf("error message     : \"%s\"\n",
+		shaderc_result_get_error_message(result));
+
+	size_t ln = shaderc_result_get_length(result);
+	const char *bt = shaderc_result_get_bytes(result);
+
+	FILE *op = fopen("tmp.spv", "w");
+
+	if (op == NULL) printf("file open error!!\n");
+
+	fwrite(bt, 1, ln, op);
+
+	fclose(op);
 
 	shaderc_result_release(result);
 	shaderc_compiler_release(compiler);
