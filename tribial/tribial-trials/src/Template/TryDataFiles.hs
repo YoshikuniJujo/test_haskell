c5ebaaf6b2d22f7b419fe39b1612011d1d54b273
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2022-03-16 11:41:23 +0900

    tribial-trials: Template.TryDataFiles: comment out

--- tribial/tribial-trials/src/Template/TryDataFiles.hs
+++ tribial/tribial-trials/src/Template/TryDataFiles.hs
@@ -6,10 +6,10 @@ module Template.TryDataFiles where
 
 import Language.Haskell.TH
 
-import Paths_tribial_trials
+-- import Paths_tribial_trials
 
 runIO do
 	putStrLn "\n*** TEMPLATE TRY DATA FILES ***"
-	print =<< readFile =<< getDataFileName "th/some.txt"
+--	print =<< readFile =<< getDataFileName "th/some.txt"
 	putStrLn ""
 	pure []
