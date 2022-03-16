8f015cc92fe48c326769112421134e6b43195f85
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2022-03-16 12:20:19 +0900

    try-my-vulkan-snd: Shaderc.CompileOptions: setIncludeCallbacks

--- themes/gui/vulkan/try-my-vulkan-snd/src/Shaderc/CompileOptions.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Shaderc/CompileOptions.hs
@@ -1,16 +1,30 @@
+{-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE ViewPatterns #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Shaderc.CompileOptions where
+
+import Foreign.Ptr
+import Foreign.Storable
+import Foreign.Pointable
+import Control.Monad.Cont
 
 import Shaderc.Enum
 import Shaderc.Include
 
 import qualified Shaderc.CompileOptions.Core as C
 
-data C ud = C {
-	cMacroDefinition :: [(String, String)],
-	cSourceLanguage :: SourceLanguage,
-	cGenerateDebugInfo :: Bool,
-	cOptimizationLevel :: OptimizationLevel,
-	cForcedVersionProfile :: (Version, Profile),
-	cIncludeCallbacks :: (ResolveFn ud, Maybe ud) }
+data T ud = T {
+	tMacroDefinition :: [(String, String)],
+	tSourceLanguage :: Maybe SourceLanguage,
+	tGenerateDebugInfo :: Bool,
+	tOptimizationLevel :: Maybe OptimizationLevel,
+	tForcedVersionProfile :: Maybe (Version, Profile),
+	tIncludeCallbacks :: Maybe (ResolveFn ud, Maybe ud) }
+
+setIncludeCallbacks :: (Storable ud, Pointable ud) =>
+	C.T -> ResolveFn ud -> Maybe ud -> ContT r IO ()
+setIncludeCallbacks opts rfun mud = do
+	let	(crfn, crrfn) = resolveFnToCore rfun
+	(castPtr -> pud) <- maybeToPointer mud
+	lift $ C.setIncludeCallbacks opts crfn crrfn pud
