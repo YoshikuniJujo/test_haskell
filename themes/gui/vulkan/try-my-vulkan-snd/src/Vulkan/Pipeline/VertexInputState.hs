5341bc91d8531afa76c7a37a97a82b3abd124af8
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2022-03-24 10:28:11 +0900

    try-my-vulkan-snd: Vulkan.Pipeline.VertexInputState: createInfoToBindingDescriptionRaw

--- themes/gui/vulkan/try-my-vulkan-snd/src/Vulkan/Pipeline/VertexInputState.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Vulkan/Pipeline/VertexInputState.hs
@@ -1,17 +1,28 @@
+{-# LANGUAGE ScopedTypeVariables, TypeApplications #-}
+{-# LANGUAGE MonoLocalBinds #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Vulkan.Pipeline.VertexInputState where
 
+import Foreign.Storable.SizeAlignment
 import Data.Kind
 
+import Vulkan.Pipeline.VertexInputState.BindingStrideList
+
 import qualified Vulkan.Pipeline.VertexInputState.Middle as M
+import qualified Vulkan.VertexInput as VertexInput
 
 data CreateInfo n vs (ts :: [Type]) = CreateInfo {
 	createInfoNext :: Maybe n,
 	createInfoFlags :: M.CreateFlags }
 	deriving Show
 
--- createInfoToBindingDescriptionRaw :: forall n vs ts .
---	BindingStrideList vs 
+createInfoToBindingDescriptionRaw :: forall n vs ts .
+	BindingStrideList vs
+		VertexInput.Rate VertexInput.Rate =>
+	CreateInfo n vs ts -> [(SizeAlignment, VertexInput.Rate)]
+createInfoToBindingDescriptionRaw _ =
+	bindingStrideList @vs @VertexInput.Rate @VertexInput.Rate
