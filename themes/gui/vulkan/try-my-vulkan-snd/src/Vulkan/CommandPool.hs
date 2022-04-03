b56d32aa17b52eb69054c9570218678f96f8425a
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2022-04-03 18:17:57 +0900

    try-my-vulkan-snd: Vulkan.CommandPool: createInfoToCore

--- themes/gui/vulkan/try-my-vulkan-snd/src/Vulkan/CommandPool.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Vulkan/CommandPool.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE ViewPatterns #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Vulkan.CommandPool where
 
+import Foreign.Ptr
 import Foreign.Pointable
 import Control.Monad.Cont
 import Data.Word
@@ -18,7 +21,14 @@ data CreateInfo n = CreateInfo {
 
 createInfoToCore :: Pointable n => CreateInfo n -> ContT r IO C.CreateInfo
 createInfoToCore CreateInfo {
+	createInfoNext = mnxt,
+	createInfoFlags = CreateFlagBits flgs,
+	createInfoQueueFamilyIndex = qfi
 	} = do
+	(castPtr -> pnxt) <- maybeToPointer mnxt
 	pure C.CreateInfo {
-		C.createInfoSType = ()
+		C.createInfoSType = (),
+		C.createInfoPNext = pnxt,
+		C.createInfoFlags = flgs,
+		C.createInfoQueueFamilyIndex = qfi
 		}
