ad078f2f01cb15ef0460af3aebc8ae0ec8260ea7
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2022-04-08 16:09:39 +0900

    try-my-vulkan-snd: try-next-triangle.hs: Setup: Base code

---
+++ themes/gui/vulkan/try-my-vulkan-snd/app/try-next-triangle.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE BlockArguments #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Main where
+
+import Control.Monad.Fix
+import Control.Monad.Reader
+import Data.Bool
+import Data.Maybe
+import Data.IORef
+
+import qualified Graphics.UI.GLFW as GlfwB
+
+main :: IO ()
+main = runReaderT run =<< newGlobal
+
+width, height :: Int
+width = 800; height = 600
+
+data Global = Global {
+	globalWindow :: IORef (Maybe GlfwB.Window)
+	}
+
+readGlobal :: (Global -> IORef a) -> ReaderT Global IO a
+readGlobal ref = lift . readIORef =<< asks ref
+
+writeGlobal :: (Global -> IORef a) -> a -> ReaderT Global IO ()
+writeGlobal ref x = lift . (`writeIORef` x) =<< asks ref
+
+newGlobal :: IO Global
+newGlobal = do
+	w <- newIORef Nothing
+	pure Global {
+		globalWindow = w
+		}
+
+run :: ReaderT Global IO ()
+run = do
+	initWindow
+	initVulkan
+	mainLoop
+	cleanup
+
+initWindow :: ReaderT Global IO ()
+initWindow = do
+	Just w <- lift do
+		True <- GlfwB.init
+		GlfwB.windowHint
+			$ GlfwB.WindowHint'ClientAPI GlfwB.ClientAPI'NoAPI
+		GlfwB.windowHint $ GlfwB.WindowHint'Resizable False
+		GlfwB.createWindow width height "Vulkan" Nothing Nothing
+	writeGlobal globalWindow $ Just w
+
+initVulkan :: ReaderT Global IO ()
+initVulkan = pure ()
+
+mainLoop :: ReaderT Global IO ()
+mainLoop = do
+	w <- fromJust <$> readGlobal globalWindow
+	lift $ fix \loop -> bool (pure ()) loop =<< do
+		GlfwB.pollEvents
+		not <$> GlfwB.windowShouldClose w
+
+cleanup :: ReaderT Global IO ()
+cleanup = do
+	lift . GlfwB.destroyWindow . fromJust =<< readGlobal globalWindow
+	lift $ GlfwB.terminate
