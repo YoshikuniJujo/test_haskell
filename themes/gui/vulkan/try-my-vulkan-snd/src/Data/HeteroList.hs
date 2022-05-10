37333f447a7a5bf5a4abd2a58d316ae3728b49d7
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2022-05-10 15:32:52 +0900

    try-my-vulkan-snd: Data.HeteroList: HeteroList

--- themes/gui/vulkan/try-my-vulkan-snd/src/Data/HeteroList.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Data/HeteroList.hs
@@ -1,15 +1,17 @@
 {-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE TypeFamilies, DataKinds #-}
-{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, UndecidableInstances #-}
+{-# LANGUAGE GADTs, TypeFamilies, DataKinds #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleContexts, FlexibleInstances, UndecidableInstances #-}
 {-# LANGUAGE PatternSynonyms #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Data.HeteroList (
-	Tip(..), (:.:)(..), length, StorableList(..) ) where
+	Tip(..), (:.:)(..), length, StorableList(..), HeteroList(..) ) where
 
 import Prelude hiding (length)
 
 import Foreign.Storable
+import Data.Kind
 
 infixr 5 :.:
 
@@ -32,3 +34,14 @@ instance StorableList () where sizeAlignments _ = []
 instance StorableList Tip where sizeAlignments _ = []
 instance (Storable t, StorableList ts) => StorableList (t :.: ts) where
 	sizeAlignments (x :.: xs) = (sizeOf x, alignment x) : sizeAlignments xs
+
+infixr 5 :..:
+
+data HeteroList (as :: [Type]) where
+	HNil :: HeteroList '[]
+	(:..:) :: a -> HeteroList as -> HeteroList (a ': as)
+
+instance Show (HeteroList '[]) where show HNil = "HNil"
+
+instance (Show a, Show (HeteroList as)) => Show (HeteroList (a ': as)) where
+	show (x :..: xs) = show x ++ " :..: " ++ show xs
