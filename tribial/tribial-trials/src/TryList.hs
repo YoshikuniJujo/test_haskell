2e4dad1e5afe6aa29869fd2d7e2d6fe9c03ec319
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2022-05-13 16:12:06 +0900

    tribial-trials: TryList: modifyElem

--- tribial/tribial-trials/src/TryList.hs
+++ tribial/tribial-trials/src/TryList.hs
@@ -5,6 +5,8 @@ module TryList where
 import Prelude hiding (unzip)
 
 import Control.DeepSeq
+import Data.Bool
+import Data.Maybe
 import Data.List hiding (unzip)
 
 infixIndex :: Eq a => [a] -> [a] -> Maybe Int
@@ -31,3 +33,18 @@ unzip'' :: (NFData a, NFData b) => [(a, b)] -> ([a], [b])
 unzip'' [] = ([], [])
 unzip'' ((x, y) : xys) =
 	(x : fst (force $ unzip'' xys), y : snd (force $ unzip'' xys))
+
+modifyElem :: Int -> (a -> a) -> [a] -> [a]
+modifyElem ii f xs = (\(i, x) -> bool x (f x) $ i == ii) <$> zip [0 ..] xs
+
+modifyElem' :: Traversable t => Int -> (a -> a) -> t a -> t a
+modifyElem' ii f xs = (\(i, x) -> bool x (f x) $ i == ii) <$> fromJust (tzip [0 ..] xs)
+
+tzipWith :: Traversable t => (a -> b -> c) -> [a] -> t b -> Maybe (t c)
+tzipWith f xs = sequenceA . snd . mapAccumL pair xs
+	where
+	pair [] _ = ([], Nothing)
+	pair (x : xs') y = (xs', Just (f x y))
+
+tzip :: Traversable t => [a] -> t b -> Maybe (t (a, b))
+tzip = tzipWith (,)
