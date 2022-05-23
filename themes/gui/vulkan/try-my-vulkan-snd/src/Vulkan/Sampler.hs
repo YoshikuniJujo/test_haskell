c8102470644fb2bc1a3b2e5ad55561b5900b7216
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2022-05-23 12:57:57 +0900

    try-my-vulkan-snd: Vulkan.Sampler: CreateInfo: halfway

--- themes/gui/vulkan/try-my-vulkan-snd/src/Vulkan/Sampler.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Vulkan/Sampler.hs
@@ -2,6 +2,18 @@
 
 module Vulkan.Sampler where
 
+import Vulkan.Enum
+import Vulkan.Sampler.Enum
+
 import qualified Vulkan.Sampler.Core as C
 
 newtype S = S C.S deriving Show
+
+data CreateInfo n = CreateInfo {
+	createInfoNext :: Maybe n,
+	createInfoFlags :: CreateFlags,
+	createInfoMagFilter :: Filter,
+	createInfoMinFilter :: Filter,
+	createInfoMipmapMode :: MipmapMode
+	}
+	deriving Show
