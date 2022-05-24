a27c27257ba7ec6f846ce766751cd545a2da55c3
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2022-05-24 11:05:07 +0900

    try-my-vulkan-snd: try-texture: shader: texture

--- themes/gui/vulkan/try-my-vulkan-snd/app/try-texture.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/app/try-texture.hs
@@ -1719,12 +1719,16 @@ main()
 layout(location = 0) in vec3 fragColor;
 layout(location = 1) in vec2 fragTexCoord;
 
+layout(binding = 1) uniform sampler2D texSampler;
+
 layout(location = 0) out vec4 outColor;
 
 void
 main()
 {
-	outColor = vec4(fragTexCoord, 0.0, 1.0);
+//	outColor = vec4(fragColor, 0.0, 1.0);
+//	outColor = texture(texSampler, fragTexCoord * 2.0);
+	outColor = vec4(fragColor * texture(texSampler, fragTexCoord).rgb, 1.0);
 }
 
 |]
