aacc17290923c9989e372c6720ce04f700fbb9a8
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2022-05-25 11:51:56 +0900

    try-wavefront-obj: tribial

--- tribial/try-wavefront-obj/src/Lib.hs
+++ tribial/try-wavefront-obj/src/Lib.hs
@@ -3,5 +3,10 @@ module Lib where
 import Codec.Wavefront
 import Codec.Wavefront.IO
 
+import qualified Data.ByteString as BS
+
 getSample :: IO WavefrontOBJ
 getSample = either error pure =<< fromFile "../../files/models/viking_room.obj"
+
+getTiny :: IO WavefrontOBJ
+getTiny = either error pure =<< fromFile "tiny.obj"
