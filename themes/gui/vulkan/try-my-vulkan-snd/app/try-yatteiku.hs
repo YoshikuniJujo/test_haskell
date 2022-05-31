33de1863e5b30b684452af686f44cd86394d46bc
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2022-05-31 16:49:54 +0900

    try-my-vulkan-snd: Vulkan.Device: create and getQueue

--- themes/gui/vulkan/try-my-vulkan-snd/app/try-yatteiku.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/app/try-yatteiku.hs
@@ -13,7 +13,7 @@ import Vulkan.Base
 import qualified Vulkan.Enum as Vk
 import qualified Vulkan.Instance as Vk.Instance
 import qualified Vulkan.PhysicalDevice as Vk.PhysicalDevice
-import qualified Vulkan.Device.Middle as Vk.Device
+import qualified Vulkan.Device as Vk.Device
 import qualified Vulkan.QueueFamily as Vk.QueueFamily
 import qualified Vulkan.Device.Queue as Vk.Device.Queue
 import qualified Vulkan.Device.Queue.Enum as Vk.Device.Queue
@@ -52,8 +52,11 @@ main = do
 					[Vk.Khr.validationLayerName],
 				Vk.Device.createInfoEnabledExtensionNames = [],
 				Vk.Device.createInfoEnabledFeatures = Nothing }
-		device <- Vk.Device.create physicalDevice devCreateInfo nil
-		pure ()
+		Vk.Device.create
+			physicalDevice devCreateInfo nil nil \device -> do
+			print =<< Vk.Device.getQueue
+				device graphicsQueueFamilyIndex 0
+			pure ()
 
 pickGraphicsQueueFamilyIndex :: [Vk.QueueFamily.Properties] -> Maybe Word32
 pickGraphicsQueueFamilyIndex ps = fromIntegral <$> findIndex
---
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Vulkan/Device.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Vulkan.Device (
+	D, create, M.CreateInfo(..), M.CreateFlags, pattern M.CreateFlagsZero,
+	getQueue
+	) where
+
+import Foreign.Pointable
+import Control.Exception
+import Data.Word
+
+import Vulkan
+
+import qualified Vulkan.AllocationCallbacks as AllocationCallbacks
+import qualified Vulkan.PhysicalDevice as PhysicalDevice
+import qualified Vulkan.Device.Middle as M
+
+newtype D s = D M.D deriving Show
+
+create :: (Pointable n, Pointable n2, Pointable n3, Pointable n4) =>
+	PhysicalDevice.P -> M.CreateInfo n n2 ->
+	Maybe (AllocationCallbacks.A n3) -> Maybe (AllocationCallbacks.A n4) ->
+	(forall s . D s -> IO a) -> IO a
+create phdvc ci macc macd f =
+	bracket (M.create phdvc ci macc) (`M.destroy` macd) (f . D)
+
+getQueue :: D s -> Word32 -> Word32 -> IO Queue
+getQueue (D dvc) qfi qi = M.getQueue dvc qfi qi
