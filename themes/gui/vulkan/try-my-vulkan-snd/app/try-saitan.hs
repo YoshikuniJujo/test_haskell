8c03911ba09affd977572a6ba2c71d7532763c4b
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2022-06-08 14:57:20 +0900

    try-my-vulkan-snd: try-saitan: findQueueFamily

--- themes/gui/vulkan/try-my-vulkan-snd/app/try-saitan.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/app/try-saitan.hs
@@ -1,14 +1,43 @@
 {-# LANGUAGE QuasiQuotes #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE BlockArguments, OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE DataKinds #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Main where
 
+import Data.Default
+import Data.Bits
+
 import Shaderc.TH
 
+import Vulkan.Base
+
+import qualified Vulkan.Instance as Vk.Instance
+import qualified Vulkan.PhysicalDevice as Vk.PhysicalDevice
+import qualified Vulkan.Queue.Enum as Vk.Queue
+import qualified Vulkan.QueueFamily as Vk.QueueFamily
+import qualified Vulkan.QueueFamily.EnumManual as Vk.QueueFamily
+
+findQueueFamily ::
+	Vk.PhysicalDevice.P -> Vk.Queue.FlagBits -> IO Vk.QueueFamily.Index
+findQueueFamily phdvc qb = do
+	queueFamilyProperties <-
+		Vk.PhysicalDevice.getQueueFamilyProperties phdvc
+	pure . fst . head $ filter ((/= zeroBits)
+		. (.&. qb) . Vk.QueueFamily.propertiesQueueFlags
+		. snd) queueFamilyProperties
+
 main :: IO ()
-main = putStrLn "Slozsoft"
+main = do
+	let	instanceInfo = def
+	Vk.Instance.create @() @() instanceInfo nil nil \inst -> do
+		print inst
+		physicalDevice <- head <$> Vk.PhysicalDevice.enumerate inst
+		print physicalDevice
+		queueFamily <-
+			findQueueFamily physicalDevice Vk.Queue.ComputeBit
+		print queueFamily
 
 [glslComputeShader|
 
