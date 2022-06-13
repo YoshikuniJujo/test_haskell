ac8d34875335ad7e7de717d88be033f5cd3bffdb
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2022-06-13 17:18:26 +0900

    Revert "try-my-vulan-snd: Main: repair"
    This reverts commit c6d18f6231d32cb1d7aec02e2f3422a7382b7f85.

--- themes/gui/vulkan/try-my-vulkan-snd/app/Main.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/app/Main.hs
@@ -162,7 +162,7 @@ newGlobal w = do
 	scivs <- newIORef []
 	lyt <- newIORef $ Vk.Ppl.Lyt.L NullPtr
 	rp <- newIORef $ Vk.RndrPss.R NullPtr
-	gpl <- newIORef Vk.Ppl.PNull
+	gpl <- newIORef Vk.Ppl.GNull
 	scfbs <- newIORef []
 	cp <- newIORef $ Vk.CP.C NullPtr
 	cbs <- newIORef []
@@ -737,7 +737,7 @@ createGraphicsPipeline g@Global {
 			Vk.Ppl.createInfoLayout = pipelineLayout,
 			Vk.Ppl.createInfoRenderPass = rp,
 			Vk.Ppl.createInfoSubpass = 0,
-			Vk.Ppl.createInfoBasePipelineHandle = Vk.Ppl.PNull,
+			Vk.Ppl.createInfoBasePipelineHandle = Vk.Ppl.GNull,
 			Vk.Ppl.createInfoBasePipelineIndex = - 1 }
 	gpl `Vk.Ppl.PCons` Vk.Ppl.PNil <-
 		Vk.Ppl.create @_ @_ @_ @_ @_ @_ @_ @_ @_ @_ @_ @_ @_ @_ @_ @_ @_ @()
