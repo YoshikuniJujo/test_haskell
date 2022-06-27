1c9fe643c078fa5a010a01877ff912b0eb58bb2a
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2022-06-27 10:57:59 +0900

    try-my-vulkan-snd: Gpu.Vulkan.Device.Memory.Buffer: map and unmap

--- themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Device/Memory/Buffer.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Device/Memory/Buffer.hs
@@ -1,19 +1,26 @@
+{-# LANGUAGE ScopedTypeVariables, TypeApplications #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE FlexibleContexts, UndecidableInstances #-}
+{-# LANGUAGE FlexibleContexts, UndecidableInstances, AllowAmbiguousTypes #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Gpu.Vulkan.Device.Memory.Buffer where
 
+import Prelude hiding (map)
+
+import Foreign.Ptr
 import Data.Kind.Object
 import Data.HeteroList
 
 import Gpu.Vulkan.Device.Memory.Buffer.Types
+import Gpu.Vulkan.Device.Memory.Buffer.TypeLevel
 import Gpu.Vulkan.Memory
 
+import qualified Gpu.Vulkan.Device.Type as Device
 import qualified Gpu.Vulkan.Device.Middle as Device.M
 import qualified Gpu.Vulkan.Device.Core as Device.C
+import qualified Gpu.Vulkan.Memory.Middle as Memory.M
 
 data M s (objss :: [[Object]]) =
 	M (HeteroVarList Form objss) Device.C.Memory
@@ -26,3 +33,13 @@ data AllocateInfo n = AllocateInfo {
 	allocateInfoNext :: Maybe n,
 	allocateInfoMemoryTypeIndex :: TypeIndex }
 	deriving Show
+
+map :: forall obj objss sd sm . OffsetSize obj objss =>
+	Device.D sd -> M sm objss -> Memory.M.MapFlags ->
+	IO (Ptr (ObjectType obj))
+map (Device.D dvc) (M fs mem) flgs = do
+	let	(ost, sz) = offsetSize @obj 0 fs
+	Memory.M.map dvc (Device.M.Memory mem) ost sz flgs
+
+unmap :: Device.D sd -> M sm objss -> IO ()
+unmap (Device.D dvc) (M _ mem) = Memory.M.unmap dvc (Device.M.Memory mem)
