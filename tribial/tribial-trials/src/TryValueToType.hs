9fa9e4e19ba75d227bd5b2179680934c7bb0e054
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2022-08-16 14:32:16 +0900

    tribial-trials: TryValueToType

---
+++ tribial/tribial-trials/src/TryValueToType.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE ScopedTypeVariables, RankNTypes, TypeApplications #-}
+{-# LANGUAGE DataKinds, PolyKinds #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module TryValueToType where
+
+data Proxy (t :: k) = Proxy
+
+class TypeBool (b :: Bool) where bool :: Bool
+instance TypeBool 'False where bool = False
+instance TypeBool 'True where bool = True
+
+typeBool :: Bool -> (forall b . TypeBool b => Proxy (b :: Bool) -> a) -> a
+typeBool False f = f $ Proxy @_ @('False)
+typeBool True f = f $ Proxy @_ @('True)
+
+boolToBool :: Bool -> Bool
+boolToBool b = typeBool b \(_ :: Proxy tb) -> bool @tb
