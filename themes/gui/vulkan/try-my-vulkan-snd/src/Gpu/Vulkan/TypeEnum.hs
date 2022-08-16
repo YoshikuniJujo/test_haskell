b0f262c73f4b4cbb28c31123388fb37add1e4ae6
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2022-08-16 16:21:28 +0900

    try-my-vulkan-snd: Gpu.Vulkan.TypeEnum

---
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/TypeEnum.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Gpu.Vulkan.TypeEnum where
+
+import Language.Haskell.TH
+
+import Gpu.Vulkan.TypeEnum.Th
+
+import qualified Gpu.Vulkan.Enum as E
+
+do	is <- lines <$> runIO (readFile "th/vkShaderStageFlagBits.txt")
+	(: []) <$> dataD (pure []) (mkName "ShaderStageFlagBits") [] Nothing
+		((`normalC` []) . mkName <$> is)
+		[]
+
+class ShaderStageFlagBitsToValue (t :: ShaderStageFlagBits) where
+	shaderStageFlagBitsToValue :: E.ShaderStageFlagBits
+
+do	is <- lines <$> runIO (readFile "th/vkShaderStageFlagBits.txt")
+	sequence $ mkInstance <$> is
