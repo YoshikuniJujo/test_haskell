528efbb47a47f8b1a598760c84979181b21b52e7
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2022-08-25 10:26:10 +0900

    tribial-trials: TryTypeLevelNat

--- tribial/tribial-trials/src/TryTypeLevelNat.hs
+++ tribial/tribial-trials/src/TryTypeLevelNat.hs
@@ -9,12 +9,17 @@ module TryTypeLevelNat where
 
 import GHC.TypeNats
 import GHC.Natural
+import Data.Proxy
 
-data Proxy (n :: Nat) = Proxy
+intToTypeNat_ :: Int -> (forall (n :: Nat) . Proxy n -> a) -> a
+intToTypeNat_ 0 f = f (Proxy @0)
+intToTypeNat_ n f = intToTypeNat_ (n - 1) \(_ :: Proxy m) -> f (Proxy @(m + 1))
 
-intToTypeNat :: Int -> (forall n . Proxy n -> a) -> a
-intToTypeNat 0 f = f (Proxy @0)
-intToTypeNat n f = intToTypeNat (n - 1) \(_ :: Proxy m) -> f (Proxy @(m + 1))
+natToTypeNat :: Natural -> (forall n . KnownNat n => Proxy n -> a) -> a
+natToTypeNat nat f = (\(SomeNat n) -> f n) $ someNatVal nat
 
--- sample :: Natural
--- sample = intToTypeNat 15 natVal
+sample1 :: Natural
+sample1 = natToTypeNat 15 natVal
+
+sample2 :: (forall n . KnownNat n => Proxy n -> a) -> a
+sample2 = natToTypeNat $ natVal (Proxy :: Proxy 15)
