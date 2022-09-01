76e20db10a94dee79b5404213d6109c73a2e6185
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2022-09-01 18:10:42 +0900

    try-my-vulkan-snd: Gpu.Vulkan.Device.Memory.ImageBuffer: getMemoryRequirements: halfway

--- themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Device/Memory/ImageBuffer.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Device/Memory/ImageBuffer.hs
@@ -5,10 +5,18 @@
 
 module Gpu.Vulkan.Device.Memory.ImageBuffer where
 
+import qualified Gpu.Vulkan.Device.Type as Device
 import qualified Gpu.Vulkan.Image as Image
 import qualified Gpu.Vulkan.Buffer as Buffer
+import qualified Gpu.Vulkan.Buffer.Middle as Buffer.M
 import qualified Gpu.Vulkan.Device.Memory.ImageBuffer.Kind as K
+import qualified Gpu.Vulkan.Memory.Middle as Memory.M
 
-data ImageBuffer (ib :: K.ImageBuffer) where
-	Image :: Image.INew si fmt -> ImageBuffer ('K.Image fmt)
-	Buffer :: Buffer.B sb objs -> ImageBuffer ('K.Buffer objs)
+data ImageBuffer sib (ib :: K.ImageBuffer) where
+	Image :: Image.INew si fmt -> ImageBuffer si ('K.Image fmt)
+	Buffer :: Buffer.B sb objs -> ImageBuffer sb ('K.Buffer objs)
+
+getMemoryRequirements :: Device.D sd -> ImageBuffer sib fos -> IO Memory.M.Requirements
+getMemoryRequirements (Device.D dvc) (Buffer (Buffer.B _ b)) =
+	Buffer.M.getMemoryRequirements dvc (Buffer.M.B b)
+-- getMemoryRequirements (Device.D dvc) (Image ...
