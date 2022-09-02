dc4d6a04c54525b61ebe2ad118da66f4ad92c654
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2022-09-02 11:37:24 +0900

    try-my-vulkan-snd: Gpu.GpuVulkan.Device.Memory.ImageBuffer: allocateInfoToMiddle

--- themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Device/Memory/ImageBuffer.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Device/Memory/ImageBuffer.hs
@@ -8,11 +8,13 @@ module Gpu.Vulkan.Device.Memory.ImageBuffer where
 import Data.HeteroList
 
 import qualified Gpu.Vulkan.Device.Type as Device
+import qualified Gpu.Vulkan.Device.Middle as Device.M
 import qualified Gpu.Vulkan.Image.Type as Image
 import qualified Gpu.Vulkan.Image.Middle as Image.M
 import qualified Gpu.Vulkan.Buffer as Buffer
 import qualified Gpu.Vulkan.Buffer.Middle as Buffer.M
 import qualified Gpu.Vulkan.Device.Memory.ImageBuffer.Kind as K
+import qualified Gpu.Vulkan.Device.Memory.Buffer as Device.Memory.Buffer
 import qualified Gpu.Vulkan.Memory.Middle as Memory.M
 
 data ImageBuffer sib (ib :: K.ImageBuffer) where
@@ -34,3 +36,26 @@ getMemoryRequirementsList :: Device.D sd ->
 	HeteroVarList (V2 ImageBuffer) sibfoss -> IO [Memory.M.Requirements]
 getMemoryRequirementsList dvc bis =
 	heteroVarListToListM (getMemoryRequirements' dvc) bis
+
+allocateInfoToMiddle ::
+	Device.D sd -> HeteroVarList (V2 ImageBuffer) sibfoss ->
+	Device.Memory.Buffer.AllocateInfo n -> IO (Memory.M.AllocateInfo n)
+allocateInfoToMiddle dvc ibs Device.Memory.Buffer.AllocateInfo {
+	Device.Memory.Buffer.allocateInfoNext = mnxt,
+	Device.Memory.Buffer.allocateInfoMemoryTypeIndex = mti } = do
+	reqss <- getMemoryRequirementsList dvc ibs
+	pure Memory.M.AllocateInfo {
+		Memory.M.allocateInfoNext = mnxt,
+		Memory.M.allocateInfoAllocationSize =
+			memoryRequirementsListToSize 0 reqss,
+		Memory.M.allocateInfoMemoryTypeIndex = mti }
+
+memoryRequirementsListToSize ::
+	Device.M.Size -> [Memory.M.Requirements] -> Device.M.Size
+memoryRequirementsListToSize sz0 [] = sz0
+memoryRequirementsListToSize sz0 (reqs : reqss) =
+	memoryRequirementsListToSize
+		(((sz0 - 1) `div` algn + 1) * algn + sz) reqss
+	where
+	sz = Memory.M.requirementsSize reqs
+	algn = Memory.M.requirementsAlignment reqs
