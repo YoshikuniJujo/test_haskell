4fe78c979a0387828bb4546eb386a77ea9359721
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2022-11-02 14:58:28 +0900

    try-my-vulkan-snd: Gpu.Vulkan.Pipeline.Compute.Middle

--- themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Command/Middle.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Command/Middle.hs
@@ -36,7 +36,7 @@ import qualified Gpu.Vulkan.Command.Core as C
 import qualified Gpu.Vulkan.RenderPass.Middle as RenderPass
 import qualified Gpu.Vulkan.Subpass.Enum as Subpass
 import qualified Gpu.Vulkan.Pipeline.Graphics.Middle as Pipeline
-import qualified Gpu.Vulkan.Pipeline.Compute.Middle as Pipeline.Compute
+import qualified Gpu.Vulkan.Pipeline.Compute.Middle.Internal as Pipeline.Compute
 import qualified Gpu.Vulkan.Pipeline.Enum as Pipeline
 import qualified Gpu.Vulkan.Pipeline.Layout.Middle as Pipeline.Layout
 
