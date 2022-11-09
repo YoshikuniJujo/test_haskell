97131a9f18e4a9e02558aa23c5363848e01822fd
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2022-11-09 12:05:30 +0900

    mastodon: quicksort: quicksort-exe

--- tribial/mastodon/quicksort/app/Main.hs
+++ tribial/mastodon/quicksort/app/Main.hs
@@ -10,16 +10,21 @@ main :: IO ()
 main = do
 	xs <- mkSample' (0, 10 ^ 7) (10 ^ 6)
 	print $ last xs
-	showTime "Data.List.sort (10^6)" (print . last $ sort xs)
-	showTime "quicksort (10^6)     " (print . last $ quicksort xs)
+	showTime "Data.List.sort (10^6)" (print' . last $ sort xs)
+	showTime "quicksort (10^6)     " (print' . last $ quicksort xs)
+	{-
 	xs' <- mkSample' (0, 10 ^ 8) (10 ^ 7)
 	print $ last xs'
-	showTime "Data.List.sort (10^7)" (print . last $ sort xs')
-	showTime "quicksort (10^7)     " (print . last $ quicksort xs')
+	showTime "Data.List.sort (10^7)" (print' . last $ sort xs')
+	showTime "quicksort (10^7)     " (print' . last $ quicksort xs')
+	-}
+
+print' :: Show a => a -> IO ()
+print' x = putStr $ show x ++ "\t"
 
 showTime :: String -> IO a -> IO ()
 showTime nm act = do
-	putStrLn $ nm ++ ": "
+	putStr $ nm ++ ":\t"
 	print =<< time act
 
 time :: IO a -> IO NominalDiffTime
