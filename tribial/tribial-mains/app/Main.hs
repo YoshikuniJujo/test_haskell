9998c564dd14f3662e23802a4c74673444dd3dba
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2022-11-09 18:16:18 +0900

    tribial: tribial-mains

---
+++ tribial/tribial-mains/app/Main.hs
@@ -0,0 +1,9 @@
+module Main (main) where
+
+import Control.Concurrent
+
+main :: IO ()
+main = do
+	putStrLn "foo"
+	threadDelay 3000000
+	putStrLn "bar"
---
+++ tribial/tribial-mains/foobar.py
@@ -0,0 +1,5 @@
+import time
+
+print("foo")
+time.sleep(20)
+print("bar")
