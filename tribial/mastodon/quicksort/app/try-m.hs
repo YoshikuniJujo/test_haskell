a8ed5daac001dbc421163d0644a4a1e6f2bdba22
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2022-11-10 17:32:29 +0900

    mastodon: quicksort: benchmark for M

--- tribial/mastodon/quicksort/app/try-m.hs
+++ tribial/mastodon/quicksort/app/try-m.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE ViewPatterns #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Main (main) where
 
+import Data.Foldable
 import System.Environment
+import System.IO
 import System.Random
 
 import QuickSort.Taocp
@@ -14,40 +17,47 @@ main = do
 	args <- getArgs
 	let	gr = case args of
 			[] -> False
-			["graph"] -> True
+			["graph", _] -> True
+		i = args !! 1
+	h <- openFile ("graph/trial" ++ i ++ ".txt") WriteMode
+	mainTrial h gr
+	hClose h
+
+mainTrial :: Handle -> Bool -> IO ()
+mainTrial h gr = do
 	if gr then pure () else putStrLn "size: 2 ^ 21"
-	sz <- randomRIO (2 ^ i20 - 2 ^ i18, 2 ^ i20)
-	xs <- mkSample' (0, 10 ^ i10) sz
+--	sz <- randomRIO (2 ^ i17 - 2 ^ i8, 2 ^ i14)
+	xs <- mkSample' (0, 10 ^ i10) (2 ^ i17) -- sz
 	readLast xs
-	showTime' gr 1 "quicksort (m = 1)     " (2 ^ i21) (readLast $ quicksortM 1 xs)
-	showTime' gr 2 "quicksort (m = 2)     " (2 ^ i21) (readLast $ quicksortM 2 xs)
-	showTime' gr 3 "quicksort (m = 3)     " (2 ^ i21) (readLast $ quicksortM 3 xs)
-	showTime' gr 4 "quicksort (m = 4)     " (2 ^ i21) (readLast $ quicksortM 4 xs)
-	showTime' gr 5 "quicksort (m = 5)     " (2 ^ i21) (readLast $ quicksortM 5 xs)
-	showTime' gr 6 "quicksort (m = 6)     " (2 ^ i21) (readLast $ quicksortM 6 xs)
-	showTime' gr 7 "quicksort (m = 7)     " (2 ^ i21) (readLast $ quicksortM 7 xs)
-	showTime' gr 8 "quicksort (m = 8)     " (2 ^ i21) (readLast $ quicksortM 8 xs)
-	showTime' gr 9 "quicksort (m = 9)     " (2 ^ i21) (readLast $ quicksortM 9 xs)
-	showTime' gr 10 "quicksort (m = 10)    " (2 ^ i21) (readLast $ quicksortM 10 xs)
-	showTime' gr 11 "quicksort (m = 11)    " (2 ^ i21) (readLast $ quicksortM 11 xs)
-	showTime' gr 12 "quicksort (m = 12)    " (2 ^ i21) (readLast $ quicksortM 12 xs)
-	showTime' gr 13 "quicksort (m = 13)    " (2 ^ i21) (readLast $ quicksortM 13 xs)
-	showTime' gr 14 "quicksort (m = 14)    " (2 ^ i21) (readLast $ quicksortM 14 xs)
-	showTime' gr 15 "quicksort (m = 15)    " (2 ^ i21) (readLast $ quicksortM 15 xs)
-	showTime' gr 16 "quicksort (m = 16)    " (2 ^ i21) (readLast $ quicksortM 16 xs)
-	showTime' gr 17 "quicksort (m = 17)    " (2 ^ i21) (readLast $ quicksortM 17 xs)
-	showTime' gr 18 "quicksort (m = 18)    " (2 ^ i21) (readLast $ quicksortM 18 xs)
-	showTime' gr 27 "quicksort (m = 27)    " (2 ^ i21) (readLast $ quicksortM 27 xs)
-	showTime' gr 36 "quicksort (m = 36)    " (2 ^ i21) (readLast $ quicksortM 36 xs)
-	showTime' gr 128 "quicksort (m = 128)   " (2 ^ i21) (readLast $ quicksortM 128 xs)
-	showTime' gr 256 "quicksort (m = 256)   " (2 ^ i21) (readLast $ quicksortM 256 xs)
---	showTime' gr 512 "quicksort (m = 512)   " (2 ^ i21) (readLast $ quicksortM 512 xs)
---	showTime' gr 1024 "quicksort (m = 1024)  " (2 ^ i21) (readLast $ quicksortM 1024 xs)
---	showTime' gr 2048 "quicksort (m = 2048)  " (2 ^ i21) (readLast $ quicksortM 2048 xs)
+	showTime' h gr 1 "quicksort (m = 1)     " (2 ^ i21) (readLast $ quicksortM 1 xs)
+	showTime' h gr 2 "quicksort (m = 2)     " (2 ^ i21) (readLast $ quicksortM 2 xs)
+	showTime' h gr 3 "quicksort (m = 3)     " (2 ^ i21) (readLast $ quicksortM 3 xs)
+	showTime' h gr 4 "quicksort (m = 4)     " (2 ^ i21) (readLast $ quicksortM 4 xs)
+	showTime' h gr 5 "quicksort (m = 5)     " (2 ^ i21) (readLast $ quicksortM 5 xs)
+	showTime' h gr 6 "quicksort (m = 6)     " (2 ^ i21) (readLast $ quicksortM 6 xs)
+	showTime' h gr 7 "quicksort (m = 7)     " (2 ^ i21) (readLast $ quicksortM 7 xs)
+	showTime' h gr 8 "quicksort (m = 8)     " (2 ^ i21) (readLast $ quicksortM 8 xs)
+	showTime' h gr 9 "quicksort (m = 9)     " (2 ^ i21) (readLast $ quicksortM 9 xs)
+	showTime' h gr 10 "quicksort (m = 10)    " (2 ^ i21) (readLast $ quicksortM 10 xs)
+	showTime' h gr 11 "quicksort (m = 11)    " (2 ^ i21) (readLast $ quicksortM 11 xs)
+	showTime' h gr 12 "quicksort (m = 12)    " (2 ^ i21) (readLast $ quicksortM 12 xs)
+	showTime' h gr 13 "quicksort (m = 13)    " (2 ^ i21) (readLast $ quicksortM 13 xs)
+	showTime' h gr 14 "quicksort (m = 14)    " (2 ^ i21) (readLast $ quicksortM 14 xs)
+	showTime' h gr 15 "quicksort (m = 15)    " (2 ^ i21) (readLast $ quicksortM 15 xs)
+	showTime' h gr 16 "quicksort (m = 16)    " (2 ^ i21) (readLast $ quicksortM 16 xs)
+	showTime' h gr 17 "quicksort (m = 17)    " (2 ^ i21) (readLast $ quicksortM 17 xs)
+	showTime' h gr 18 "quicksort (m = 18)    " (2 ^ i21) (readLast $ quicksortM 18 xs)
+	showTime' h gr 27 "quicksort (m = 27)    " (2 ^ i21) (readLast $ quicksortM 27 xs)
+	showTime' h gr 36 "quicksort (m = 36)    " (2 ^ i21) (readLast $ quicksortM 36 xs)
+	showTime' h gr 128 "quicksort (m = 128)   " (2 ^ i21) (readLast $ quicksortM 128 xs)
+	showTime' h gr 256 "quicksort (m = 256)   " (2 ^ i21) (readLast $ quicksortM 256 xs)
+--	showTime' h gr 512 "quicksort (m = 512)   " (2 ^ i21) (readLast $ quicksortM 512 xs)
+--	showTime' h gr 1024 "quicksort (m = 1024)  " (2 ^ i21) (readLast $ quicksortM 1024 xs)
+--	showTime' h gr 2048 "quicksort (m = 2048)  " (2 ^ i21) (readLast $ quicksortM 2048 xs)
 
 readLast :: [Int] -> IO ()
 readLast ns = putStr $ replicate (last ns - last ns) 'c'
 
-showTime' :: Bool -> Int -> String -> Int -> IO a -> IO ()
-showTime' False _ lbl n act = showTime lbl n act
-showTime' True m _ _ act = showTimeMGraph m act
+showTime' :: Handle -> Bool -> Int -> String -> Int -> IO a -> IO ()
+showTime' _ False _ lbl n act = showTime lbl n act
+showTime' h True m _ _ act = showTimeMGraph h m act
--- tribial/mastodon/quicksort/src/Tools.hs
+++ tribial/mastodon/quicksort/src/Tools.hs
@@ -5,13 +5,14 @@
 module Tools where
 
 import Control.Monad
+import System.IO
 import System.Random
 
 import Data.Time
 
-i5, i6, i7, i8, i9, i10, i16, i17, i18, i19, i20, i21, i22, i23 :: Int
+i5, i6, i7, i8, i9, i10, i14, i16, i17, i18, i19, i20, i21, i22, i23 :: Int
 i5 = 5; i6 = 6; i7 = 7; i8 = 8; i9 = 9; i10 = 10
-i16 = 16; i17 = 17; i18 = 18; i19 = 19; i20 = 20; i21 = 21; i22 = 22; i23 = 23
+i14 = 14; i16 = 16; i17 = 17; i18 = 18; i19 = 19; i20 = 20; i21 = 21; i22 = 22; i23 = 23
 
 showTime :: String -> Int -> IO a -> IO ()
 showTime nm n act = do
@@ -20,11 +21,11 @@ showTime nm n act = do
 	print' t
 	print $ realToFrac t / nLogN n * 10 ^ i7
 
-showTimeMGraph :: Int -> IO a -> IO ()
-showTimeMGraph m act = do
-	putStr $ show m ++ "\t"
+showTimeMGraph :: Handle -> Int -> IO a -> IO ()
+showTimeMGraph h m act = do
+	hPutStr h $ show m ++ "\t"
 	t <- time act
-	print t
+	hPutStrLn h $ show t
 
 print' :: Show a => a -> IO ()
 print' x = putStr $ show x ++ "\t"
