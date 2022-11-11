608da15fa97c802adfa3da8bbe8755d641351b34
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2022-11-11 10:56:36 +0900

    tribial: try-graph: Text

--- tribial/graph/try-graph/app/try-pango-simple.hs
+++ tribial/graph/try-graph/app/try-pango-simple.hs
@@ -4,22 +4,14 @@
 
 module Main (main) where
 
-import Foreign.C.Types
-import Control.Monad.ST
 import Data.CairoImage.Internal
 import Data.JuicyCairo
 import System.Environment
 import Codec.Picture
 import Graphics.Cairo.Drawing.CairoT
-import Graphics.Cairo.Drawing.Transformations
 import Graphics.Cairo.Surfaces.ImageSurfaces
-import Graphics.Pango.Basic.Fonts.PangoFontDescription
-import Graphics.Pango.Basic.LayoutObjects.PangoLayout
-import Graphics.Pango.Rendering.Cairo
 
-import Data.CairoContext
-
-import qualified Data.Text as T
+import Text
 
 main :: IO ()
 main = getArgs >>= \case
@@ -33,16 +25,3 @@ main = getArgs >>= \case
 			CairoImageArgb32 a -> writePng "try-pango-simple.png" $ cairoArgb32ToJuicyRGBA8 a
 			_ -> error "never occur"
 	_ -> error "no family"
-
-putText :: CairoT s RealWorld -> Size -> CDouble -> CDouble -> T.Text -> IO ()
-putText cr sz x y txt = do
-	pl <- pangoCairoCreateLayout cr
-	fd <- pangoFontDescriptionNew
-	pangoFontDescriptionSet fd $ Family "sans"
-	pangoFontDescriptionSet fd sz
-	fd' <- pangoFontDescriptionFreeze fd
-	pangoLayoutSet pl . pangoFontDescriptionToNullable $ Just fd'
-	pangoLayoutSet pl txt
-	cairoIdentityMatrix cr
-	cairoTranslate cr x y
-	pangoCairoShowLayout cr =<< pangoLayoutFreeze pl
---
+++ tribial/graph/try-graph/src/Text.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Text (putText, Size(..)) where
+
+import Foreign.C.Types
+import Control.Monad.ST
+import Data.Text qualified as T
+import Data.CairoContext
+import Graphics.Cairo.Drawing.Transformations
+import Graphics.Pango.Basic.Fonts.PangoFontDescription
+import Graphics.Pango.Basic.LayoutObjects.PangoLayout
+import Graphics.Pango.Rendering.Cairo
+
+putText :: CairoT s RealWorld -> Size -> CDouble -> CDouble -> T.Text -> IO ()
+putText cr sz x y txt = do
+	pl <- pangoCairoCreateLayout cr
+	fd <- pangoFontDescriptionNew
+	pangoFontDescriptionSet fd $ Family "sans"
+	pangoFontDescriptionSet fd sz
+	fd' <- pangoFontDescriptionFreeze fd
+	pangoLayoutSet pl . pangoFontDescriptionToNullable $ Just fd'
+	pangoLayoutSet pl txt
+	cairoIdentityMatrix cr
+	cairoTranslate cr x y
+	pangoCairoShowLayout cr =<< pangoLayoutFreeze pl
