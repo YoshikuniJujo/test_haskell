e483cef92c17ef5ebec2312e1526fd0ef48449f7
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2022-11-14 10:14:38 +0900

    mastodon: quicksort: MergeSort: naturalSort: bad

--- tribial/mastodon/quicksort/src/MergeSort.hs
+++ tribial/mastodon/quicksort/src/MergeSort.hs
@@ -1,11 +1,13 @@
-{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE BlockArguments, LambdaCase #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module MergeSort where
 
 import Control.Monad
 import Control.Monad.ST
+import Data.Foldable
 import Data.Array.ST
+import Data.Bool
 
 naturalSort :: Ord a => [a] -> [a]
 naturalSort ks = take n $ runST
@@ -19,7 +21,37 @@ prepareArray xs = do
 	a <$ uncurry (writeArray a) `mapM_` zip [1 ..] xs
 
 nsort :: Ord a => STArray s Int a -> ST s ()
-nsort ks = pure ()
+nsort ks = do
+	(_, n) <- getBounds ks
+	run ks (n `div` 2) False
+
+run :: Ord a => STArray s Int a -> Int -> Bool -> ST s ()
+run ks n s = do
+	end <- inner ks i j k l 1 True
+	if end	then for_ [1 .. n] \m ->
+			unless s $ writeArray ks i =<< readArray ks (n + m)
+		else run ks n (not s)
+	where (i, j, k, l) = bool (1, n, n + 1, 2 * n) (n + 1, 2 * n, 1, n) s
+
+inner :: Ord a => STArray s Int a ->
+	Int -> Int -> Int -> Int -> Int -> Bool -> ST s Bool
+inner ks i j k l d f
+	| i == j = do
+		writeArray ks k =<< readArray ks i
+		pure f
+	| otherwise = do
+		ki <- readArray ks i
+		kj <- readArray ks j
+		pure True
+		if ki > kj
+			then merge1Right ks i j k d >>= \case
+				Nothing -> inner ks i (j - 1) (k + d) l d f
+				Just (i', k') ->
+					inner ks i' (j - 1) l k' (- d) False
+			else merge1Left ks i j k d >>= \case
+				Nothing -> inner ks (i + 1) j (k + d) l d f
+				Just (j', k') ->
+					inner ks (i + 1) j' l k' (- d) False
 
 testMerge1Left :: (Ord a, Num a) => [a] -> [a]
 testMerge1Left ks = runST do
