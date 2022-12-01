add88c79626a2a4916bfa9da073580e2381692b3
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2022-12-01 16:04:29 +0900

    mastodon: quicksort: try-m: output foo.hason

--- tribial/mastodon/quicksort/app/try-m.hs
+++ tribial/mastodon/quicksort/app/try-m.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE BlockArguments, ImportQualifiedPost #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE BlockArguments, OverloadedStrings #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE ViewPatterns #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
@@ -6,11 +7,14 @@
 module Main (main) where
 
 import Data.Traversable
+import Data.Text qualified as T
 import Data.Time
+import Data.Hason
 import System.Environment
 import System.IO
 import System.Directory
 import System.Random hiding (next)
+import Text.PrettyPrint
 
 import QuickSort.Taocp
 import Tools
@@ -25,7 +29,7 @@ main = do
 			["graph"] -> True
 	i <- next
 	h <- openFile ("graph/trial" ++ show i ++ ".txt") WriteMode
-	mainTrial h gr
+	mainTrial i h gr
 	hClose h
 
 next :: IO Int
@@ -33,8 +37,9 @@ next = (+ 1) . foldl max 0
 	. (read @Int . takeWhile (/= '.') . drop 5 <$>)
 	. filter ("trial" `L.isPrefixOf`) <$> getDirectoryContents "graph"
 
-mainTrial :: Handle -> Bool -> IO ()
-mainTrial h gr = do
+mainTrial :: Int -> Handle -> Bool -> IO ()
+mainTrial i h gr = do
+	mid <- readFile "/etc/machine-id"
 	if gr then pure () else putStrLn "size: 2 ^ 21"
 	sz <- randomRIO (13 * 10 ^ i4, 2 ^ i17)
 	xs <- mkSample' (0, 10 ^ i10) sz
@@ -46,8 +51,13 @@ mainTrial h gr = do
 		[1 .. 16] ++ [19, 22 .. 31] ++
 		[36, 43 .. 57] ++ [66, 91, 128, 181, 256] ) \m -> do
 		t <- showTime' h gr m (readLast $ quicksortM m xs)
-		pure (m, t)
-	print rslt
+		pure $ Dct [
+			("M", I $ fromIntegral m),
+			("time", DT t) ]
+	writeFile ("graph/try-m" ++ show i ++ ".hason") . (++ "\n") . render . ppr $ Dct [
+		("machine-id", T . T.pack $ tail mid),
+		("result", L rslt)
+		]
 
 readLast :: [Int] -> IO ()
 readLast ns = putStr $ replicate (last ns - last ns) 'c'
