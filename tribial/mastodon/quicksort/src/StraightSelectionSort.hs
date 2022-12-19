3d3de579c22dab04b2a2bae4cd47d1abe35b214e
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2022-12-19 14:59:45 +0900

    mastodon: quicksort: StraightSelectionSort: refactoring

--- tribial/mastodon/quicksort/src/StraightSelectionSort.hs
+++ tribial/mastodon/quicksort/src/StraightSelectionSort.hs
@@ -6,19 +6,19 @@ module StraightSelectionSort where
 import Control.Monad.ST
 import Data.Foldable
 import Data.Array.ST
+import Data.Bool
 
 selectionSort :: Ord a => [a] -> [a]
 selectionSort xs =
-	runST $ (>>) <$> ssort' n <*> getElems =<< newListArray (1, n) xs
+	runST $ (>>) <$> ssort n <*> getElems =<< newListArray (1, n) xs
 	where n = length xs
 
-ssort' :: Ord a => Int -> STArray s Int a -> ST s ()
-ssort' n xs = for_ [n, n - 1 .. 2] \j -> do
-	i <- findMax' xs j (j - 1)
-	xj <- readArray xs j; xi <- readArray xs i
+ssort :: Ord a => Int -> STArray s Int a -> ST s ()
+ssort n xs = for_ [n, n - 1 .. 2] \j -> findMax xs j (j - 1) >>= \i -> do
+	xi <- readArray xs i; xj <- readArray xs j
 	writeArray xs i xj; writeArray xs j xi
 
-findMax' :: Ord a => STArray s Int a -> Int -> Int -> ST s Int
-findMax' xs i k = readArray xs k >>= \xk -> readArray xs i >>= \ra -> do
-	let	i' = if ra >= xk then i else k
-	if (k > 1) then findMax' xs i' (k - 1) else pure i'
+findMax :: Ord a => STArray s Int a -> Int -> Int -> ST s Int
+findMax xs i k = readArray xs i >>= \xi -> readArray xs k >>= \xk -> do
+	let	i' = bool k i (xi >= xk)
+	bool (pure i') (findMax xs i' (k - 1)) (k > 1)
