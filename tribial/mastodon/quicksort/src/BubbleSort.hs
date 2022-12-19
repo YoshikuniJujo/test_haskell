0c12f5e9a181cba3d3b423c416c870248e887f4c
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2022-12-19 14:12:58 +0900

    mastodon: quicksort: BubbleSort: refactoring

--- tribial/mastodon/quicksort/src/BubbleSort.hs
+++ tribial/mastodon/quicksort/src/BubbleSort.hs
@@ -5,6 +5,7 @@ module BubbleSort where
 
 import Control.Monad.ST
 import Data.Array.ST
+import Data.Bool
 
 bubbleSort :: Ord a => [a] -> [a]
 bubbleSort xs = runST $ (>>) <$> bsort n <*> getElems =<< newListArray (1, n) xs
@@ -12,16 +13,13 @@ bubbleSort xs = runST $ (>>) <$> bsort n <*> getElems =<< newListArray (1, n) xs
 
 bsort :: Ord a => Int -> STArray s Int a -> ST s ()
 bsort bound xs = swap xs 0 [1 .. bound - 1] >>= \t ->
-	if t == 0 then pure () else bsort t xs
+	bool (bsort t xs) (pure ()) (t == 0)
 
 swap :: Ord a => STArray s Int a -> Int -> [Int] -> ST s Int
 swap xs t = \case
 	[] -> pure t
-	(j : js) -> do
-		xj <- readArray xs j
-		xj1 <- readArray xs $ j + 1
+	(j : js) -> readArray xs j >>= \xj -> readArray xs (j + 1) >>= \xj1 ->
 		if xj > xj1
-			then do	writeArray xs j xj1
-				writeArray xs (j + 1) xj
+			then do	writeArray xs j xj1; writeArray xs (j + 1) xj
 				swap xs j js
 			else swap xs t js
