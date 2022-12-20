74095a66f18f92a5472792f6d3025a4a48525f00
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2022-12-20 09:10:46 +0900

    mastodon: quicksort: QuickSort.Taocp: tribial

--- tribial/mastodon/quicksort/src/QuickSort/Taocp.hs
+++ tribial/mastodon/quicksort/src/QuickSort/Taocp.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE BlockArguments, LambdaCase #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module QuickSort.Taocp where
+module QuickSort.Taocp (quicksort, quicksortM) where
 
 import Control.Monad
 import Control.Monad.ST
@@ -14,42 +14,39 @@ quicksort :: (Ord a, Bounded a) => [a] -> [a]
 quicksort = quicksortM 32
 
 quicksortM :: (Ord a, Bounded a) => Int -> [a] -> [a]
-quicksortM m ks =
-	init . tail $ runST $ (>>) <$> qsort m n <*> getElems =<< array n ks
-	where n = length ks
+quicksortM m xs =
+	init . tail $ runST $ (>>) <$> qsort m n <*> getElems =<< array n xs
+	where n = length xs
 
 array :: Bounded a => Int -> [a] -> ST s (STArray s Int a)
 array n xs = newArray_ (0, n + 1) >>= \a -> do
 	writeArray a 0 minBound; writeArray a (n + 1) maxBound
 	a <$ uncurry (writeArray a) `mapM_` zip [1 ..] xs
 
-mixM :: Int
-mixM = 9
-
 qsort :: Ord a => Int -> Int -> STArray s Int a -> ST s ()
-qsort m n ks = do
-	when (n > m) $ stage m ks 1 n
-	when (m > 1) $ isort n ks
+qsort m n xs = do
+	when (n > m) $ stage m xs 1 n
+	when (m > 1) $ isort n xs
 
 stage :: Ord a => Int -> STArray s Int a -> Int -> Int -> ST s ()
-stage m ks l r = do
-	k <- readArray ks l
-	(j, kj) <- exchange ks k (l + 1) r
-	writeArray ks l kj; writeArray ks j k
+stage m xs l r = do
+	k <- readArray xs l
+	(j, kj) <- exchange xs k (l + 1) r
+	writeArray xs l kj; writeArray xs j k
 	case (r - j >= j - l, r - j > m, j - l > m) of
-		(True, _, True) -> stage m ks l (j - 1) >> stage m ks (j + 1) r
-		(False, True, _) -> stage m ks (j + 1) r >> stage m ks l (j - 1)
-		(_, True, False) -> stage m ks (j + 1) r
-		(_, False, True) -> stage m ks l (j - 1)
+		(True, _, True) -> stage m xs l (j - 1) >> stage m xs (j + 1) r
+		(False, True, _) -> stage m xs (j + 1) r >> stage m xs l (j - 1)
+		(_, True, False) -> stage m xs (j + 1) r
+		(_, False, True) -> stage m xs l (j - 1)
 		_ -> pure ()
 
 exchange :: Ord a => STArray s Int a -> a -> Int -> Int -> ST s (Int, a)
-exchange ks k i j = do 
-	(i', ki) <- whileL i (< k) ks
-	(j', kj) <- whileR j (k <) ks
+exchange xs k i j = do 
+	(i', ki) <- whileL i (< k) xs
+	(j', kj) <- whileR j (k <) xs
 	if j' <= i' then pure (j', kj) else do
-		writeArray ks i' kj; writeArray ks j' ki
-		exchange ks k (i' + 1) (j' - 1)
+		writeArray xs i' kj; writeArray xs j' ki
+		exchange xs k (i' + 1) (j' - 1)
 
 whileL :: Int -> (a -> Bool) -> STArray s Int a -> ST s (Int, a)
 whileL i p a =
