e64e5074a6cb2aef3c141833345434612eff6ce3
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2022-12-20 09:24:06 +0900

    mastodon: quicksoret: BubbleSort: xs -> ks

--- tribial/mastodon/quicksort/src/BubbleSort.hs
+++ tribial/mastodon/quicksort/src/BubbleSort.hs
@@ -8,18 +8,18 @@ import Data.Array.ST
 import Data.Bool
 
 bubbleSort :: Ord a => [a] -> [a]
-bubbleSort xs = runST $ (>>) <$> bsort n <*> getElems =<< newListArray (1, n) xs
-	where n = length xs
+bubbleSort ks = runST $ (>>) <$> bsort n <*> getElems =<< newListArray (1, n) ks
+	where n = length ks
 
 bsort :: Ord a => Int -> STArray s Int a -> ST s ()
-bsort bound xs = swap xs 0 [1 .. bound - 1] >>= \t ->
-	bool (bsort t xs) (pure ()) (t == 0)
+bsort bound ks = swap ks 0 [1 .. bound - 1] >>= \t ->
+	bool (bsort t ks) (pure ()) (t == 0)
 
 swap :: Ord a => STArray s Int a -> Int -> [Int] -> ST s Int
-swap xs t = \case
+swap ks t = \case
 	[] -> pure t
-	(j : js) -> readArray xs j >>= \xj -> readArray xs (j + 1) >>= \xj1 ->
-		if xj > xj1
-			then do	writeArray xs j xj1; writeArray xs (j + 1) xj
-				swap xs j js
-			else swap xs t js
+	(j : js) -> readArray ks j >>= \kj -> readArray ks (j + 1) >>= \kj1 ->
+		if kj > kj1
+			then do	writeArray ks j kj1; writeArray ks (j + 1) kj
+				swap ks j js
+			else swap ks t js
