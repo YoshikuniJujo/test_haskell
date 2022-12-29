9169dfc9bd8fbfc7f3760f779b8504eca20a9ff8
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2022-12-29 14:00:45 +0900

    try-my-vulkan-snd: Gpu.Vulkan.PUshConstant.Middle.Internal

--- themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/PushConstant/Middle.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/PushConstant/Middle.hs
@@ -1,23 +1,6 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Gpu.Vulkan.PushConstant.Middle where
+module Gpu.Vulkan.PushConstant.Middle (
+	Range(..) ) where
 
-import Data.Word
-
-import Gpu.Vulkan.Enum
-
-import qualified Gpu.Vulkan.PushConstant.Core as C
-
-data Range = Range {
-	rangeStageFlags :: ShaderStageFlags,
-	rangeOffset :: Word32,
-	rangeSize :: Word32 }
-	deriving Show
-
-rangeToCore :: Range -> C.Range
-rangeToCore Range {
-	rangeStageFlags = ShaderStageFlagBits sf,
-	rangeOffset = ost, rangeSize = sz
-	} = C.Range {
-		C.rangeStageFlags = sf,
-		C.rangeOffset = ost, C.rangeSize = sz }
+import Gpu.Vulkan.PushConstant.Middle.Internal
