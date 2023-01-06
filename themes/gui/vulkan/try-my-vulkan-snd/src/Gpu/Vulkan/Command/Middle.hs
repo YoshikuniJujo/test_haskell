ff7684be1e9769b5b89627dd460fe3fa8d974a29
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2023-01-06 11:58:19 +0900

    try-my-vulkan-snd: Gpu.Vulkan.Command.Middle: rename functions

--- themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Command/Middle.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Command/Middle.hs
@@ -7,13 +7,13 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Gpu.Vulkan.Command.Middle (
-	beginRenderPassMiddle, endRenderPassMiddle,
-	bindPipelineMiddle, bindPipelineComputeMiddle,
-	bindVertexBuffersMiddle, bindIndexBufferMiddle,
-	pushConstantsMiddle, bindDescriptorSetsMiddle,
-	drawMiddle, drawIndexedMiddle, dispatchMiddle,
-	copyBufferMiddle, copyBufferToImageMiddle, copyImageToBufferMiddle,
-	pipelineBarrierMiddle, blitImageMiddle ) where
+	beginRenderPass, endRenderPass,
+	bindPipeline, bindPipelineCompute,
+	bindVertexBuffers, bindIndexBuffer,
+	pushConstants, bindDescriptorSets,
+	draw, drawIndexed, dispatch,
+	copyBuffer, copyBufferToImage, copyImageToBuffer,
+	pipelineBarrier, blitImage ) where
 
 import Foreign.Marshal.Alloc
 import Foreign.Marshal.Array
@@ -48,36 +48,36 @@ import qualified Gpu.Vulkan.Image.Middle.Internal as Image
 import qualified Gpu.Vulkan.Buffer.Middle.Internal as Buffer.M
 import qualified Gpu.Vulkan.Memory.Middle.Internal as Memory.M
 
-beginRenderPassMiddle :: (Pointable n, ClearValuesToCore ct) =>
+beginRenderPass :: (Pointable n, ClearValuesToCore ct) =>
 	CommandBuffer.M.C -> RenderPass.BeginInfo n ct -> Subpass.Contents -> IO ()
-beginRenderPassMiddle (CommandBuffer.M.C _ cb)
+beginRenderPass (CommandBuffer.M.C _ cb)
 	rpbi (Subpass.Contents spcnt) = ($ pure) $ runContT do
 	prpbi <- RenderPass.beginInfoToCore rpbi
 	lift $ C.beginRenderPass cb prpbi spcnt
 
-endRenderPassMiddle :: CommandBuffer.M.C -> IO ()
-endRenderPassMiddle (CommandBuffer.M.C _ cb) = C.endRenderPass cb
+endRenderPass :: CommandBuffer.M.C -> IO ()
+endRenderPass (CommandBuffer.M.C _ cb) = C.endRenderPass cb
 
-bindPipelineMiddle ::
+bindPipeline ::
 	CommandBuffer.M.C -> Pipeline.BindPoint -> Pipeline.G vs ts -> IO ()
-bindPipelineMiddle (CommandBuffer.M.C rppl cb) (Pipeline.BindPoint pbp) ppl = do
+bindPipeline (CommandBuffer.M.C rppl cb) (Pipeline.BindPoint pbp) ppl = do
 	ppl0 <- readIORef rppl
 	ppl' <- Pipeline.gToCore ppl
 	when (ppl' /= ppl0) do
 		writeIORef rppl ppl'
 		C.bindPipeline cb pbp ppl'
 
-bindPipelineComputeMiddle ::
+bindPipelineCompute ::
 	CommandBuffer.M.C -> Pipeline.BindPoint -> Pipeline.Compute.C -> IO ()
-bindPipelineComputeMiddle (CommandBuffer.M.C rppl cb) (Pipeline.BindPoint pbp) (Pipeline.Compute.C ppl) = do
+bindPipelineCompute (CommandBuffer.M.C rppl cb) (Pipeline.BindPoint pbp) (Pipeline.Compute.C ppl) = do
 	ppl0 <- readIORef rppl
 	when (ppl /= ppl0) do
 		writeIORef rppl ppl
 		C.bindPipeline cb pbp ppl
 
-bindVertexBuffersMiddle ::
+bindVertexBuffers ::
 	CommandBuffer.M.C -> Word32 -> [(Buffer.B, Device.Size)] -> IO ()
-bindVertexBuffersMiddle (CommandBuffer.M.C _ c)
+bindVertexBuffers (CommandBuffer.M.C _ c)
 	fb ((length &&& unzip) -> (bc, (bs, os))) = ($ pure) $ runContT do
 	pb <- ContT $ allocaArray bc
 	lift . pokeArray pb $ (\(Buffer.B b) -> b) <$> bs
@@ -85,17 +85,17 @@ bindVertexBuffersMiddle (CommandBuffer.M.C _ c)
 	lift . pokeArray po $ (\(Device.Size sz) -> sz) <$> os
 	lift $ C.bindVertexBuffers c fb (fromIntegral bc) pb po
 
-bindIndexBufferMiddle ::
+bindIndexBuffer ::
 	CommandBuffer.M.C -> Buffer.B -> Device.Size -> IndexType -> IO ()
-bindIndexBufferMiddle
+bindIndexBuffer
 	(CommandBuffer.M.C _ cb) (Buffer.B ib) (Device.Size sz) (IndexType it) =
 	C.bindIndexBuffer cb ib sz it
 
-pushConstantsMiddle :: forall ts .
+pushConstants :: forall ts .
 	StoreHetero ts =>
 	CommandBuffer.M.C -> Pipeline.Layout.L ->
 	ShaderStageFlags -> Word32 -> HeteroList ts -> IO ()
-pushConstantsMiddle (CommandBuffer.M.C _ cb) (Pipeline.Layout.L lyt)
+pushConstants (CommandBuffer.M.C _ cb) (Pipeline.Layout.L lyt)
 	(ShaderStageFlagBits ss) ost xs = ($ pure) $ runContT do
 	let	sz :: Integral n => n
 		sz = fromIntegral $ storeHeteroSize @ts 0
@@ -103,10 +103,10 @@ pushConstantsMiddle (CommandBuffer.M.C _ cb) (Pipeline.Layout.L lyt)
 	lift do	storeHetero p xs
 		C.pushConstants cb lyt ss ost sz p
 
-bindDescriptorSetsMiddle ::
+bindDescriptorSets ::
 	CommandBuffer.M.C -> Pipeline.BindPoint -> Pipeline.Layout.L ->
 	Word32 -> [Descriptor.Set.D] -> [Word32] -> IO ()
-bindDescriptorSetsMiddle
+bindDescriptorSets
 	(CommandBuffer.M.C _ cb) (Pipeline.BindPoint bp) (Pipeline.Layout.L lyt)
 	fs (length &&& id -> (dsc, dss))
 	(length &&& id -> (doc, dos)) = ($ pure) $ runContT do
@@ -118,29 +118,29 @@ bindDescriptorSetsMiddle
 	lift $ C.bindDescriptorSets
 		cb bp lyt fs (fromIntegral dsc) pdss (fromIntegral doc) pdos
 
-drawMiddle :: CommandBuffer.M.C -> Word32 -> Word32 -> Word32 -> Word32 -> IO ()
-drawMiddle (CommandBuffer.M.C _ cb) vc ic fv fi = C.draw cb vc ic fv fi
+draw :: CommandBuffer.M.C -> Word32 -> Word32 -> Word32 -> Word32 -> IO ()
+draw (CommandBuffer.M.C _ cb) vc ic fv fi = C.draw cb vc ic fv fi
 
-drawIndexedMiddle :: CommandBuffer.M.C ->
+drawIndexed :: CommandBuffer.M.C ->
 	Word32 -> Word32 -> Word32 -> Int32 -> Word32 -> IO ()
-drawIndexedMiddle (CommandBuffer.M.C _ cb) idxc istc fidx vo fist =
+drawIndexed (CommandBuffer.M.C _ cb) idxc istc fidx vo fist =
 	C.drawIndexed cb idxc istc fidx vo fist
 
-dispatchMiddle :: CommandBuffer.M.C -> Word32 -> Word32 -> Word32 -> IO ()
-dispatchMiddle (CommandBuffer.M.C _ cb) = C.dispatch cb
+dispatch :: CommandBuffer.M.C -> Word32 -> Word32 -> Word32 -> IO ()
+dispatch (CommandBuffer.M.C _ cb) = C.dispatch cb
 
-copyBufferMiddle ::
+copyBuffer ::
 	CommandBuffer.M.C -> Buffer.B -> Buffer.B -> [Buffer.C.Copy] -> IO ()
-copyBufferMiddle (CommandBuffer.M.C _ c) (Buffer.B s) (Buffer.B d)
+copyBuffer (CommandBuffer.M.C _ c) (Buffer.B s) (Buffer.B d)
 	(length &&& id -> (rc, rs)) = ($ pure) $ runContT do
 	prs <- ContT $ allocaArray rc
 	lift do	pokeArray prs rs
 		C.copyBuffer c s d (fromIntegral rc) prs
 
-copyBufferToImageMiddle ::
+copyBufferToImage ::
 	CommandBuffer.M.C -> Buffer.M.B -> Image.I -> Image.Layout ->
 	[Buffer.M.ImageCopy] -> IO ()
-copyBufferToImageMiddle (CommandBuffer.M.C _ cb)
+copyBufferToImage (CommandBuffer.M.C _ cb)
 	(Buffer.M.B sb) (Image.I rdi) (Image.Layout dil)
 	(length &&& id -> (rc, rs)) = ($ pure) $ runContT do
 	prs <- ContT $ allocaArray rc
@@ -148,10 +148,10 @@ copyBufferToImageMiddle (CommandBuffer.M.C _ cb)
 	lift do	(_, di) <- readIORef rdi
 		C.copyBufferToImage cb sb di dil (fromIntegral rc) prs
 
-copyImageToBufferMiddle ::
+copyImageToBuffer ::
 	CommandBuffer.M.C -> Image.I -> Image.Layout -> Buffer.M.B ->
 	[Buffer.M.ImageCopy] -> IO ()
-copyImageToBufferMiddle (CommandBuffer.M.C _ cb)
+copyImageToBuffer (CommandBuffer.M.C _ cb)
 	(Image.I rsi) (Image.Layout sil) (Buffer.M.B db)
 	(length &&& id -> (rc, rs)) = ($ pure) $ runContT do
 	prs <- ContT $ allocaArray rc
@@ -159,7 +159,7 @@ copyImageToBufferMiddle (CommandBuffer.M.C _ cb)
 	lift do	(_, si) <- readIORef rsi
 		C.copyImageToBuffer cb si sil db (fromIntegral rc) prs
 
-pipelineBarrierMiddle :: (
+pipelineBarrier :: (
 	StorableHeteroMap ns, StorableHeteroMap ns', StorableHeteroMap ns''
 	) =>
 	CommandBuffer.M.C -> Pipeline.StageFlags -> Pipeline.StageFlags ->
@@ -167,7 +167,7 @@ pipelineBarrierMiddle :: (
 	HeteroVarList Memory.M.Barrier ns ->
 	HeteroVarList Buffer.M.MemoryBarrier ns' ->
 	HeteroVarList Image.MemoryBarrier ns'' -> IO ()
-pipelineBarrierMiddle (CommandBuffer.M.C _ cb)
+pipelineBarrier (CommandBuffer.M.C _ cb)
 	(Pipeline.StageFlagBits ssm) (Pipeline.StageFlagBits dsm)
 	(DependencyFlagBits dfs)
 	mbs bbs ibs = ($ pure) $ runContT do
@@ -186,10 +186,10 @@ pipelineBarrierMiddle (CommandBuffer.M.C _ cb)
 	lift $ C.pipelineBarrier cb ssm dsm dfs (fromIntegral mbc) pmbs
 		(fromIntegral bbc) pbbs (fromIntegral ibc) pibs
 
-blitImageMiddle :: CommandBuffer.M.C ->
+blitImage :: CommandBuffer.M.C ->
 	Image.I -> Image.Layout -> Image.I -> Image.Layout ->
 	[Image.Blit] -> Filter -> IO ()
-blitImageMiddle (CommandBuffer.M.C _ cb)
+blitImage (CommandBuffer.M.C _ cb)
 	(Image.I rsrc) (Image.Layout srcLyt) (Image.I rdst) (Image.Layout dstLyt)
 	(length &&& id -> (bltc, blts)) (Filter ft) = ($ pure) $ runContT do
 	pblts <- ContT $ allocaArray bltc
--- themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Command/Tmp.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Command/Tmp.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE ScopedTypeVariables, RankNTypes, TypeApplications #-}
 {-# LANGUAGE GADTs #-}
@@ -43,55 +44,55 @@ import qualified Gpu.Vulkan.Image.Middle as Image
 import qualified Gpu.Vulkan.Buffer.Middle as Buffer.M
 import qualified Gpu.Vulkan.Memory.Middle as Memory.M
 
-import Gpu.Vulkan.Command.Middle
+import Gpu.Vulkan.Command.Middle qualified as M
 
 beginRenderPass :: (Pointable n, ClearValuesToCore ct) =>
 	CommandBuffer.CC vs -> RenderPass.BeginInfo n ct -> Subpass.Contents -> IO ()
-beginRenderPass (CommandBuffer.CC mcb) = beginRenderPassMiddle mcb
+beginRenderPass (CommandBuffer.CC mcb) = M.beginRenderPass mcb
 
 endRenderPass :: CommandBuffer.CC vs -> IO ()
-endRenderPass (CommandBuffer.CC mcb) = endRenderPassMiddle mcb
+endRenderPass (CommandBuffer.CC mcb) = M.endRenderPass mcb
 
 bindPipeline ::
 	CommandBuffer.CC vs -> Pipeline.BindPoint -> Pipeline.G vs ts -> IO ()
-bindPipeline (CommandBuffer.CC mcb) = bindPipelineMiddle mcb
+bindPipeline (CommandBuffer.CC mcb) = M.bindPipeline mcb
 
 bindPipelineCompute ::
 	CommandBuffer.CC vs -> Pipeline.BindPoint -> Pipeline.Compute.C -> IO ()
-bindPipelineCompute (CommandBuffer.CC mcb) = bindPipelineComputeMiddle mcb
+bindPipelineCompute (CommandBuffer.CC mcb) = M.bindPipelineCompute mcb
 
 draw :: CommandBuffer.CC vs -> Word32 -> Word32 -> Word32 -> Word32 -> IO ()
-draw (CommandBuffer.CC mcb) = drawMiddle mcb
+draw (CommandBuffer.CC mcb) = M.draw mcb
 
 drawIndexed :: CommandBuffer.CC vs ->
 	Word32 -> Word32 -> Word32 -> Int32 -> Word32 -> IO ()
-drawIndexed (CommandBuffer.CC mcb) = drawIndexedMiddle mcb
+drawIndexed (CommandBuffer.CC mcb) = M.drawIndexed mcb
 
 bindVertexBuffers ::
 	CommandBuffer.CC vs -> Word32 -> [(Buffer.B, Device.Size)] -> IO ()
-bindVertexBuffers (CommandBuffer.CC mcb) = bindVertexBuffersMiddle mcb
+bindVertexBuffers (CommandBuffer.CC mcb) = M.bindVertexBuffers mcb
 
 copyBuffer ::
 	CommandBuffer.CC vs -> Buffer.B -> Buffer.B -> [Buffer.C.Copy] -> IO ()
-copyBuffer (CommandBuffer.CC mcb) = copyBufferMiddle mcb
+copyBuffer (CommandBuffer.CC mcb) = M.copyBuffer mcb
 
 bindIndexBuffer ::
 	CommandBuffer.CC vs -> Buffer.B -> Device.Size -> IndexType -> IO ()
-bindIndexBuffer (CommandBuffer.CC mcb) = bindIndexBufferMiddle mcb
+bindIndexBuffer (CommandBuffer.CC mcb) = M.bindIndexBuffer mcb
 
 dispatch :: CommandBuffer.CC vs -> Word32 -> Word32 -> Word32 -> IO ()
-dispatch (CommandBuffer.CC mcb) = dispatchMiddle mcb
+dispatch (CommandBuffer.CC mcb) = M.dispatch mcb
 
 bindDescriptorSets ::
 	CommandBuffer.CC vs -> Pipeline.BindPoint -> Pipeline.Layout.L ->
 	Word32 -> [Descriptor.Set.D] -> [Word32] -> IO ()
-bindDescriptorSets (CommandBuffer.CC mcb) = bindDescriptorSetsMiddle mcb
+bindDescriptorSets (CommandBuffer.CC mcb) = M.bindDescriptorSets mcb
 
 pushConstants :: forall vs ts .
 	StoreHetero ts =>
 	CommandBuffer.CC vs -> Pipeline.Layout.L ->
 	ShaderStageFlags -> Word32 -> HeteroList ts -> IO ()
-pushConstants (CommandBuffer.CC mcb) = pushConstantsMiddle mcb
+pushConstants (CommandBuffer.CC mcb) = M.pushConstants mcb
 
 pipelineBarrier :: (
 	StorableHeteroMap ns, StorableHeteroMap ns', StorableHeteroMap ns''
@@ -101,19 +102,19 @@ pipelineBarrier :: (
 	HeteroVarList Memory.M.Barrier ns ->
 	HeteroVarList Buffer.M.MemoryBarrier ns' ->
 	HeteroVarList Image.MemoryBarrier ns'' -> IO ()
-pipelineBarrier (CommandBuffer.CC mcb) = pipelineBarrierMiddle mcb
+pipelineBarrier (CommandBuffer.CC mcb) = M.pipelineBarrier mcb
 
 copyBufferToImage ::
 	CommandBuffer.CC vs -> Buffer.M.B -> Image.I -> Image.Layout ->
 	[Buffer.M.ImageCopy] -> IO ()
-copyBufferToImage (CommandBuffer.CC mcb) = copyBufferToImageMiddle mcb
+copyBufferToImage (CommandBuffer.CC mcb) = M.copyBufferToImage mcb
 
 copyImageToBuffer ::
 	CommandBuffer.CC vs -> Image.I -> Image.Layout -> Buffer.M.B ->
 	[Buffer.M.ImageCopy] -> IO ()
-copyImageToBuffer (CommandBuffer.CC mcb) = copyImageToBufferMiddle mcb
+copyImageToBuffer (CommandBuffer.CC mcb) = M.copyImageToBuffer mcb
 
 blitImage :: CommandBuffer.CC v ->
 	Image.I -> Image.Layout -> Image.I -> Image.Layout ->
 	[Image.Blit] -> Filter -> IO ()
-blitImage (CommandBuffer.CC mcb) = blitImageMiddle mcb
+blitImage (CommandBuffer.CC mcb) = M.blitImage mcb
