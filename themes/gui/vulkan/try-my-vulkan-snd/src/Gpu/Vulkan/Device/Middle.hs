796bbd7247a2538cf1ff14b07b777647e399f728
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2023-01-09 14:54:17 +0900

    try-my-vulkan-snd: Gpu.Vulkan.Device.Middle

--- themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Device/Middle.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Device/Middle.hs
@@ -1,9 +1,24 @@
+{-# LANGUAGE PatternSynonyms #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Gpu.Vulkan.Device.Middle (
-	D, CreateInfo(..), QueueCreateInfo(..), CreateFlags, CreateFlagBits, create, destroy,
+
+	-- * Type
+
+	D,
+
+	-- * Create and Destroy
+
+	create, destroy,
+	CreateInfo(..), CreateFlags, CreateFlagBits,
+	QueueCreateInfo(..),
+	QueueCreateFlags, QueueCreateFlagBits,
+	pattern QueueCreateProtectedBit, pattern QueueCreateFlagBitsMaxEnum,
+
+	-- * Others
 
 	getQueue, waitIdle, Size
 	) where
 
 import Gpu.Vulkan.Device.Middle.Internal
+import Gpu.Vulkan.Device.Enum
