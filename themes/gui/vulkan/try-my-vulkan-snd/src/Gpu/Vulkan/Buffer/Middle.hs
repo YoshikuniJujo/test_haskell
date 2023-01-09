5d5dcdc2207370d4e26cbb39b4ba415dbc018dc8
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2023-01-09 22:39:33 +0900

    try-my-vulkan-snd: Gpu.Vulkan.Buffer.Middle

--- themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Buffer/Middle.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Buffer/Middle.hs
@@ -1,8 +1,21 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Gpu.Vulkan.Buffer.Middle (
-	B, CreateInfo(..), create, destroy, bindMemory, getMemoryRequirements,
-	ImageCopy(..), MemoryBarrier(..)
-	) where
+
+	-- * Type
+	
+	B,
+
+	-- * Create and Destroy
+
+	create, destroy, CreateInfo(..),
+
+	-- * Bind and Get Memory
+
+	bindMemory, getMemoryRequirements,
+
+	-- * ImageCopy and MemoryBarrier
+
+	ImageCopy(..), MemoryBarrier(..) ) where
 
 import Gpu.Vulkan.Buffer.Middle.Internal
