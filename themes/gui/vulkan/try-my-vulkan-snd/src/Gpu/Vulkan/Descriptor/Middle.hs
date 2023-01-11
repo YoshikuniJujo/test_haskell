392392259edf5d38e4e40dc6fd071aa6b9de687f
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2023-01-11 13:11:06 +0900

    try-my-vulkan-snd: Gpu.Vulkan.Descriptor.Middle

--- themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Descriptor/Middle.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Descriptor/Middle.hs
@@ -1,27 +1,9 @@
-{-# LANGUAGE PatternSynonyms #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Gpu.Vulkan.Descriptor.Middle (
 
 	-- * ImageInfo and BufferInfo
 
-	ImageInfo(..), BufferInfo(..),
-
-	-- * Type
-
-	Type,
-	pattern TypeSampler, pattern TypeCombinedImageSampler,
-	pattern TypeSampledImage, pattern TypeStorageImage,
-	pattern TypeUniformTexelBuffer, pattern TypeStorageTexelBuffer,
-	pattern TypeUniformBuffer, pattern TypeStorageBuffer,
-	pattern TypeUniformBufferDynamic, pattern TypeStorageBufferDynamic,
-	pattern TypeInputAttachment, pattern TypeInlineUniformBlock,
-	pattern TypeAccelerationStructureKhr,
-	pattern TypeAccelerationStructureNv, pattern TypeMutableValve,
-	pattern TypeSampleWeightImageQcom, pattern TypeBlockMatchImageQcom,
-	pattern TypeInlineUniformBlockExt, pattern TypeMaxEnum
-
-	) where
+	ImageInfo(..), BufferInfo(.. ) ) where
 
 import Gpu.Vulkan.Descriptor.Middle.Internal
-import Gpu.Vulkan.Descriptor.Enum
