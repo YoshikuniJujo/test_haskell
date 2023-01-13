b5c9ea85dde4b31480e98b571c495f5892ba000a
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2023-01-13 14:56:06 +0900

    try-my-vulkan-snd: Gpu.Vulkan.Khr.Surface.Middle

--- themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Khr/Surface/Middle.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Khr/Surface/Middle.hs
@@ -1,7 +1,16 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Gpu.Vulkan.Khr.Surface.Middle (
-	S, destroy,
+
+	-- * Type
+
+	S,
+
+	-- * Destroy
+
+	destroy,
+
+	-- * Data Structures
 
 	Capabilities(..), Format(..) ) where
 
