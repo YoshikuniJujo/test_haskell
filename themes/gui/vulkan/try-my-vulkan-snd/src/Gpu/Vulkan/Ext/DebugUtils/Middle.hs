6607ecc6ac4a9d2bad72cda72410ca54ff70f7af
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2023-01-13 12:06:07 +0900

    try-my-vulkan-snd: Gpu.Vuolkan.Ext.DebugUtils.Middle

--- themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Ext/DebugUtils/Middle.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Ext/DebugUtils/Middle.hs
@@ -1,7 +1,13 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Gpu.Vulkan.Ext.DebugUtils.Middle (
-	extensionName, Label(..), ObjectNameInfo(..)
-	) where
+
+	-- * Name
+
+	extensionName,
+
+	-- * Data Structures
+
+	Label(..), ObjectNameInfo(..) ) where
 
 import Gpu.Vulkan.Ext.DebugUtils.Middle.Internal
