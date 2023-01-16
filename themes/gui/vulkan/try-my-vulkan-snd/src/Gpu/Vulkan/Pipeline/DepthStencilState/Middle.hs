a12db939f689009e85100ead7a70a70133b490fc
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2023-01-16 17:55:56 +0900

    try-my-vulkan-snd: Gpu.Vulkan.Pipeline.DepthStencilState.Middle

--- themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Pipeline/DepthStencilState/Middle.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Pipeline/DepthStencilState/Middle.hs
@@ -1,7 +1,9 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Gpu.Vulkan.Pipeline.DepthStencilState.Middle (
-	CreateInfo(..), CreateFlags(..)
-	) where
+
+	-- * CreateInfo
+
+	CreateInfo(..), CreateFlags(..) ) where
 
 import Gpu.Vulkan.Pipeline.DepthStencilState.Middle.Internal
