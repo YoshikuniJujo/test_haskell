424991885b0a425022c22c4108041ff625a1adca
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2023-01-17 10:54:15 +0900

    try-my-vulkan-snd: Gpu.Vulkan.Pipeline.TessellationState.Middle

--- themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Pipeline/TessellationState/Middle.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Pipeline/TessellationState/Middle.hs
@@ -1,5 +1,9 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Gpu.Vulkan.Pipeline.TessellationState.Middle (CreateInfo(..)) where
+module Gpu.Vulkan.Pipeline.TessellationState.Middle (
+
+	-- * CreateInfo
+
+	CreateInfo(..) ) where
 
 import Gpu.Vulkan.Pipeline.TessellationState.Middle.Internal
