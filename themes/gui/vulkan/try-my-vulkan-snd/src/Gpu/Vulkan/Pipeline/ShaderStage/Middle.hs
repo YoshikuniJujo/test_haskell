a990b84936b7a7f62eaba32767a3106bc702265f
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2023-01-17 10:41:47 +0900

    try-my-vulkan-snd: Gpu.Vulkan.Pipeline.ShaderStage.Middle

--- themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Pipeline/ShaderStage/Middle.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Pipeline/ShaderStage/Middle.hs
@@ -1,6 +1,9 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Gpu.Vulkan.Pipeline.ShaderStage.Middle (
+
+	-- * CreateInfo
+
 	CreateInfo(..), CreateInfoListToCore ) where
 
 import Gpu.Vulkan.Pipeline.ShaderStage.Middle.Internal
