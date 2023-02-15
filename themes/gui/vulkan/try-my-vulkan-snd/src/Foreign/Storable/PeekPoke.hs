7bd487d82b694a9a0615640547e37264d22f998a
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2023-02-15 10:06:22 +0900

    try-my-vulkan-snd: Foreign.Storable.PeekPoke: haddock

--- themes/gui/vulkan/try-my-vulkan-snd/src/Foreign/Storable/PeekPoke.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Foreign/Storable/PeekPoke.hs
@@ -6,7 +6,21 @@
 {-# LANGUAGE PatternSynonyms, ViewPatterns #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Foreign.Storable.PeekPoke where
+module Foreign.Storable.PeekPoke (
+
+	-- * Sizable, Peek and Poke
+
+	Sizable(..), Peek(..), peekMaybe, Poke(..),
+
+	-- * Peekable, WithPoked and Pokable
+
+	Peekable, peekArray',
+	WithPoked(..), PtrS, ptrS, withPtrS, withPokedMaybe',
+	Pokable, withPoked, withPokedMaybe,
+
+	-- * Storable' and NullPtr
+
+	Storable', pattern NullPtr ) where
 
 import Foreign.Ptr
 import Foreign.Marshal.Alloc
