568d123770e528833ee9985912f1163fb4b1b6f2
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2023-03-01 10:21:06 +0900

    try-my-vulkan-snd: Gpu.Vulkan.Khr.Surface.PhysicalDevice.Middle: not use ContT

--- themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Khr/Surface/PhysicalDevice/Middle.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Khr/Surface/PhysicalDevice/Middle.hs
@@ -8,7 +8,6 @@ module Gpu.Vulkan.Khr.Surface.PhysicalDevice.Middle (
 import Foreign.Marshal
 import Foreign.Storable
 import Foreign.Storable.PeekPoke
-import Control.Monad.Cont
 
 import Gpu.Vulkan.Exception.Middle
 import Gpu.Vulkan.Exception.Enum
@@ -22,43 +21,37 @@ import qualified Gpu.Vulkan.Khr.Surface.Middle.Internal as M
 
 getSupport :: PhysicalDevice.P -> QueueFamily.Index -> M.S -> IO Bool
 getSupport (PhysicalDevice.P phdvc) (QueueFamily.Index qfi) (M.S sfc) =
-	($ pure) . runContT $ bool32ToBool <$> do
-		pSupported <- ContT alloca
-		lift do	r <- C.getSupport phdvc qfi sfc pSupported
-			throwUnlessSuccess $ Result r
-			peek pSupported
+	bool32ToBool <$> alloca \pSupported -> do
+		r <- C.getSupport phdvc qfi sfc pSupported
+		throwUnlessSuccess $ Result r
+		peek pSupported
 
 getCapabilities :: PhysicalDevice.P -> M.S -> IO M.Capabilities
 getCapabilities (PhysicalDevice.P pdvc) (M.S sfc) =
-	($ pure) . runContT $ M.capabilitiesFromCore <$> do
-		pCapabilities <- ContT alloca
-		lift do	r <- C.getCapabilities pdvc sfc pCapabilities
-			throwUnlessSuccess $ Result r
-			peek pCapabilities
+	M.capabilitiesFromCore <$> alloca \pCapabilities -> do
+		r <- C.getCapabilities pdvc sfc pCapabilities
+		throwUnlessSuccess $ Result r
+		peek pCapabilities
 
 getFormats :: PhysicalDevice.P -> M.S -> IO [M.Format]
 getFormats (PhysicalDevice.P pdvc) (M.S sfc) =
-	($ pure) . runContT $ (M.formatFromCore <$>) <$> do
-		pFormatCount <- ContT alloca
-		(fromIntegral -> formatCount) <- lift do
-			r <- C.getFormats pdvc sfc pFormatCount NullPtr
-			throwUnlessSuccess $ Result r
-			peek pFormatCount
-		pFormats <- ContT $ allocaArray formatCount
-		lift do	r <- C.getFormats pdvc sfc pFormatCount pFormats
-			throwUnlessSuccess $ Result r
-			peekArray formatCount pFormats
+	(M.formatFromCore <$>) <$> alloca \pFormatCount ->
+	C.getFormats pdvc sfc pFormatCount NullPtr >>= \r ->
+	throwUnlessSuccess (Result r) >>
+	peek pFormatCount >>= \(fromIntegral -> formatCount) ->
+	allocaArray formatCount \pFormats -> do
+		r' <- C.getFormats pdvc sfc pFormatCount pFormats
+		throwUnlessSuccess $ Result r'
+		peekArray formatCount pFormats
 
 getPresentModes :: PhysicalDevice.P -> M.S -> IO [PresentMode]
 getPresentModes (PhysicalDevice.P pdvc) (M.S sfc) =
-	($ pure) . runContT $ (PresentMode <$>) <$> do
-		pPresentModeCount <- ContT alloca
-		(fromIntegral -> presentModeCount) <- lift do
-			r <- C.getPresentModes pdvc sfc pPresentModeCount NullPtr
-			throwUnlessSuccess $ Result r
-			peek pPresentModeCount
-		pPresentModes <- ContT $ allocaArray presentModeCount
-		lift do	r <- C.getPresentModes
-				pdvc sfc pPresentModeCount pPresentModes
-			throwUnlessSuccess $ Result r
-			peekArray presentModeCount pPresentModes
+	 (PresentMode <$>) <$> alloca \pPresentModeCount ->
+	C.getPresentModes pdvc sfc pPresentModeCount NullPtr >>= \r ->
+	throwUnlessSuccess (Result r) >>
+	peek pPresentModeCount >>= \(fromIntegral -> presentModeCount) ->
+	allocaArray presentModeCount \pPresentModes -> do
+		r' <- C.getPresentModes
+			pdvc sfc pPresentModeCount pPresentModes
+		throwUnlessSuccess $ Result r'
+		peekArray presentModeCount pPresentModes
