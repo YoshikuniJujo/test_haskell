60cbddd6fb11c0249cff1cfbc166b744c8f854e7
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2023-03-16 17:54:23 +0900

    try-my-vulkan-snd: Gpu.Vulkan.BufferView: halfway

--- themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/BufferView.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/BufferView.hs
@@ -1,3 +1,26 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE MultiParamTypeClasses, AllowAmbiguousTypes #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Gpu.Vulkan.BufferView where
+
+import Data.TypeLevel.Uncurry
+import Gpu.Vulkan.Object qualified as VObj
+
+import Gpu.Vulkan.Device.Middle qualified as Device.M
+import Gpu.Vulkan.TypeEnum qualified as TEnum
+import Gpu.Vulkan.Buffer qualified as Buffer
+import Gpu.Vulkan.BufferView.Middle qualified as M
+
+data CreateInfo n t snmobjs = CreateInfo {
+	createInfoNext :: Maybe n,
+	createInfoFlags :: M.CreateFlags,
+	createInfoBuffer :: U3 Buffer.B snmobjs }
+
+class OffsetRange t (objs :: [VObj.Object]) where
+	offsetRange :: (Device.M.Size, Device.M.Size)
+
+type family FormatOf t :: TEnum.Format
