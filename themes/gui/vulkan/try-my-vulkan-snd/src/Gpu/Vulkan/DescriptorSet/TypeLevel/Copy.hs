1caa515038096c3e2af9fccf91f5f39d33344c68
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2023-04-18 11:09:28 +0900

    try-my-vulkan-snd: Gpu.Vulkan.DescriptorSet.TypeLevel.Copy: class BindingAndArrayElement

--- themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/DescriptorSet/TypeLevel/Copy.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/DescriptorSet/TypeLevel/Copy.hs
@@ -1,6 +1,17 @@
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE MultiParamTypeClasses, AllowAmbiguousTypes #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Gpu.Vulkan.DescriptorSet.TypeLevel.Copy where
 
+import GHC.TypeLits
+
+import Gpu.Vulkan.DescriptorSetLayout.Type qualified as Layout
 import Gpu.Vulkan.DescriptorSet.TypeLevel.Common qualified as Common
+
+class BindingAndArrayElement
+	(bts :: [Layout.BindingType])
+	(bt :: Layout.BindingType) (i :: Nat) where
+	bindingAndArrayElement :: Integral n => (n, n)
