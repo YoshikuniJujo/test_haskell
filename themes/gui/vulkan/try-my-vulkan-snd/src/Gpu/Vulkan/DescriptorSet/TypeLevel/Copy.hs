700ca1aa96815ed7b4ddc7bfa4d97b78006d52f8
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2023-04-18 18:04:05 +0900

    try-my-vulkan-snd: Gpu.Vulkan.DescriptorSet.TypeLevel.Copy: Image

--- themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/DescriptorSet/TypeLevel/Copy.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/DescriptorSet/TypeLevel/Copy.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE ScopedTypeVariables, TypeApplications #-}
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE KindSignatures, TypeOperators #-}
 {-# LANGUAGE MultiParamTypeClasses, AllowAmbiguousTypes #-}
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts, FlexibleInstances, UndecidableInstances #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Gpu.Vulkan.DescriptorSet.TypeLevel.Copy where
@@ -12,6 +12,7 @@ import Data.TypeLevel.Length qualified as TypeLevel.Length
 
 import GHC.TypeLits
 
+import Gpu.Vulkan.TypeEnum qualified as T
 import Gpu.Vulkan.DescriptorSetLayout.Type qualified as Layout
 import Gpu.Vulkan.DescriptorSet.TypeLevel.Common qualified as Common
 
@@ -27,6 +28,53 @@ instance Common.BindingAndArrayElem bts vobjs i =>
 class BindingLength (bt :: Layout.BindingType) where
 	bindingLength :: Integral n => n
 
+instance
+	BindingAndArrayElemImage bts imgs i =>
+	BindingAndArrayElement bts (Layout.Image imgs) i where
+	bindingAndArrayElement = bindingAndArrayElemImage @bts @imgs @i 0 0
+	
+
 instance TypeLevel.Length.Length vobjs =>
 	BindingLength (Layout.Buffer vobjs) where
 	bindingLength = TypeLevel.Length.length @_ @vobjs
+
+class IsPrefixImage
+	(sfs1 :: [(Symbol, T.Format)]) (sfs2 :: [(Symbol, T.Format)])
+
+instance IsPrefixImage '[] sfs2
+instance IsPrefixImage sfs1 sfs2 => IsPrefixImage (bti ': sfs1) (bti ': sfs2)
+
+class BindingAndArrayElemImage
+	(bts :: [Layout.BindingType])
+	(sfs :: [(Symbol, T.Format)]) (i :: Nat) where
+	bindingAndArrayElemImage :: Integral n => n -> n -> (n, n)
+
+instance {-# OVERLAPPABLE #-}
+	IsPrefixImage sfs1 sfs2 => BindingAndArrayElemImage
+		('Layout.Image (sf ': sfs2) ': bts) (sf ': sfs1) 0 where
+	bindingAndArrayElemImage b ae = (b, ae)
+
+instance {-# OVERLAPPABLE #-}
+	BindingAndArrayElemImage
+		('Layout.Image sfs2 ': bts) (sf ': sfs1) (i - 1) =>
+	BindingAndArrayElemImage
+		('Layout.Image (sf ': sfs2) ': bts) (sf ': sfs1) i where
+	bindingAndArrayElemImage b ae = bindingAndArrayElemImage
+		@('Layout.Image sfs2 ': bts) @(sf ': sfs1) @(i - 1) b (ae + 1)
+
+instance BindingAndArrayElemImage bts sfs i =>
+	BindingAndArrayElemImage ('Layout.Image '[] ': bts) sfs i where
+	bindingAndArrayElemImage b _ =
+		bindingAndArrayElemImage @bts @sfs @i (b + 1) 0
+
+instance {-# OVERLAPPABLE #-}
+	BindingAndArrayElemImage ('Layout.Image nmfmts ': bts) sfs i =>
+	BindingAndArrayElemImage ('Layout.Image (nmfmt ': nmfmts) ': bts) sfs i where
+	bindingAndArrayElemImage b ae = bindingAndArrayElemImage
+		@('Layout.Image nmfmts ': bts) @sfs @i b (ae + 1)
+
+instance {-# OVERLAPPABLE #-}
+	BindingAndArrayElemImage bts sfs i =>
+	BindingAndArrayElemImage (bt ': bts) sfs i where
+	bindingAndArrayElemImage b _ =
+		bindingAndArrayElemImage @bts @sfs @i (b + 1) 0
