822502cd36c45c4b40a200db1532f198fc019b15
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2023-04-28 09:23:19 +0900

    try-vulkan-middle: Gpu.Vulkan.AllocationCallbacks.Middle: internal notifications: to Maybe

--- themes/gui/vulkan/try-vulkan-middle/src/Gpu/Vulkan/AllocationCallbacks/Middle/Internal.hs
+++ themes/gui/vulkan/try-vulkan-middle/src/Gpu/Vulkan/AllocationCallbacks/Middle/Internal.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE BlockArguments, LambdaCase #-}
 {-# LANGUAGE MonoLocalBinds #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
@@ -8,17 +9,18 @@ module Gpu.Vulkan.AllocationCallbacks.Middle.Internal (
 import Foreign.Ptr
 import Foreign.ForeignPtr
 import Foreign.Storable.PeekPoke
+import Control.Monad
 
-import qualified Gpu.Vulkan.AllocationCallbacks.Core as C
+import Gpu.Vulkan.AllocationCallbacks.Core qualified as C
 
 data A a = A {
 	allocationCallbacksUserData :: a,
 	allocationCallbacksFnAllocation :: C.FnAllocationFunction a,
 	allocationCallbacksFnReallocation :: C.FnReallocationFunction a,
 	allocationCallbacksFnFree :: C.FnFreeFunction a,
-	allocationCallbacksFnInternalAllocation ::
+	allocationCallbacksFnInternalAllocationFree :: Maybe (
 		C.FnInternalAllocationNotification a,
-	allocationCallbacksFnInternalFree :: C.FnInternalFreeNotification a }
+		C.FnInternalFreeNotification a ) }
 
 maybeToCore :: WithPoked n => Maybe (A n) -> (Ptr C.A -> IO b) -> IO ()
 maybeToCore = \case Nothing -> (() <$) . ($ NullPtr); Just ac -> toCore ac
@@ -32,18 +34,21 @@ withA ac f = withPoked' ud \pud -> do
 	pal <- C.wrapAllocationFunction al
 	pral <- C.wrapReallocationFunction ral
 	pfr <- C.wrapFreeFunction fr
-	pial <- C.wrapInternalAllocationNotification ial
-	pifr <- C.wrapInternalFreeNotification ifr
+	(pial, pifr) <- do
+		case allocationCallbacksFnInternalAllocationFree ac of
+			Nothing -> pure (nullFunPtr, nullFunPtr)
+			Just (ial, ifr) -> do
+				wal <- C.wrapInternalAllocationNotification ial
+				wfr <- C.wrapInternalFreeNotification ifr
+				pure (wal, wfr)
 	withPtrS pud \pud' -> f (C.A (castPtr pud') pal pral pfr pial pifr)
 	freeHaskellFunPtr pal
 	freeHaskellFunPtr pral
 	freeHaskellFunPtr pfr
-	freeHaskellFunPtr pial
-	freeHaskellFunPtr pifr
+	when (pial /= nullFunPtr) $ freeHaskellFunPtr pial
+	when (pifr /= nullFunPtr) $ freeHaskellFunPtr pifr
 	where
 	ud = allocationCallbacksUserData ac
 	al = allocationCallbacksFnAllocation ac
 	ral = allocationCallbacksFnReallocation ac
 	fr = allocationCallbacksFnFree ac
-	ial = allocationCallbacksFnInternalAllocation ac
-	ifr = allocationCallbacksFnInternalFree ac
