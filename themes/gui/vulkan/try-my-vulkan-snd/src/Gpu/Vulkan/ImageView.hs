5a651b73a4a0c68bb4252c9631ed147b1655d1ca
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2023-05-09 14:53:13 +0900

    try-my-vulkan-snd: Gpu.Vulkan.ImageView: remove create and recreate

--- themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/ImageView.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/ImageView.hs
@@ -7,14 +7,15 @@
 {-# LANGUAGE ViewPatterns #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Gpu.Vulkan.ImageView where
+module Gpu.Vulkan.ImageView (
+	INew(..), createNew, recreateNew, CreateInfoNew(..)
+	) where
 
 import GHC.TypeLits
 import Foreign.Storable.PeekPoke
 import Control.Exception
 import Data.TypeLevel.Maybe qualified as TMaybe
 
-import Gpu.Vulkan.Enum
 import Gpu.Vulkan.ImageView.Enum
 
 import qualified Gpu.Vulkan.TypeEnum as T
@@ -28,14 +29,6 @@ import qualified Gpu.Vulkan.ImageView.Middle as M
 
 newtype INew (fmt :: T.Format) (nm :: Symbol) si = INew M.I deriving Show
 
-newtype I s = I M.I deriving Show
-
-iToOld :: INew fmt nm si -> I s
-iToOld (INew i) = I i
-
-iToNew :: I s -> INew fmt nm si
-iToNew (I i) = INew i
-
 data CreateInfoNew n si sm nm ifmt (ivfmt :: T.Format) = CreateInfoNew {
 	createInfoNextNew :: TMaybe.M n,
 	createInfoFlagsNew :: CreateFlags,
@@ -43,15 +36,6 @@ data CreateInfoNew n si sm nm ifmt (ivfmt :: T.Format) = CreateInfoNew {
 	createInfoViewTypeNew :: Type,
 	createInfoComponentsNew :: Component.Mapping,
 	createInfoSubresourceRangeNew :: Image.M.SubresourceRange }
-
-data CreateInfo si sm n = CreateInfo {
-	createInfoNext :: TMaybe.M n,
-	createInfoFlags :: CreateFlags,
-	createInfoImage :: Image.Binded si sm,
-	createInfoViewType :: Type,
-	createInfoFormat :: Format,
-	createInfoComponents :: Component.Mapping,
-	createInfoSubresourceRange :: Image.M.SubresourceRange }
 
 createInfoToMiddleNew :: forall n si sm nm ifmt ivfmt . T.FormatToValue ivfmt =>
 	CreateInfoNew n si sm nm ifmt ivfmt -> M.CreateInfo n
@@ -71,23 +55,6 @@ createInfoToMiddleNew CreateInfoNew {
 	M.createInfoSubresourceRange = srr }
 	where fmt = T.formatToValue @ivfmt
 
-createInfoToMiddle :: CreateInfo si sm n -> M.CreateInfo n
-createInfoToMiddle CreateInfo {
-	createInfoNext = mnxt,
-	createInfoFlags = flgs,
-	createInfoImage = Image.Binded img,
-	createInfoViewType = tp,
-	createInfoFormat = fmt,
-	createInfoComponents = cps,
-	createInfoSubresourceRange = srr } = M.CreateInfo {
-	M.createInfoNext = mnxt,
-	M.createInfoFlags = flgs,
-	M.createInfoImage = img,
-	M.createInfoViewType = tp,
-	M.createInfoFormat = fmt,
-	M.createInfoComponents = cps,
-	M.createInfoSubresourceRange = srr }
-
 createNew :: (
 	T.FormatToValue ivfmt,
 	WithPoked (TMaybe.M n), Pokable c, Pokable d ) =>
@@ -100,16 +67,6 @@ createNew (Device.D dvc) ci
 	(M.create dvc (createInfoToMiddleNew ci) macc)
 	(\i -> M.destroy dvc i macd) (f . INew)
 
-create :: (WithPoked (TMaybe.M n), Pokable c, Pokable d) =>
-	Device.D sd -> CreateInfo si sm n ->
-	Maybe (AllocationCallbacks.A sc c) -> Maybe (AllocationCallbacks.A sd d) ->
-	(forall s . I s -> IO a) -> IO a
-create (Device.D dvc) ci
-	((AllocationCallbacks.toMiddle <$>) -> macc)
-	((AllocationCallbacks.toMiddle <$>) -> macd) f = bracket
-	(M.create dvc (createInfoToMiddle ci) macc)
-	(\i -> M.destroy dvc i macd) (f . I)
-
 recreateNew :: (
 	T.FormatToValue ivfmt,
 	WithPoked (TMaybe.M n), Pokable c, Pokable d ) =>
@@ -120,12 +77,3 @@ recreateNew (Device.D dvc) ci
 	((AllocationCallbacks.toMiddle <$>) -> macc)
 	((AllocationCallbacks.toMiddle <$>) -> macd) (INew i) =
 	M.recreate dvc (createInfoToMiddleNew ci) macc macd i
-
-recreate :: (WithPoked (TMaybe.M n), Pokable c, Pokable d) =>
-	Device.D sd -> CreateInfo si sm n ->
-	Maybe (AllocationCallbacks.A sc c) -> Maybe (AllocationCallbacks.A sd d) ->
-	I s -> IO ()
-recreate (Device.D dvc) ci
-	((AllocationCallbacks.toMiddle <$>) -> macc)
-	((AllocationCallbacks.toMiddle <$>) -> macd) (I i) =
-	M.recreate dvc (createInfoToMiddle ci) macc macd i
