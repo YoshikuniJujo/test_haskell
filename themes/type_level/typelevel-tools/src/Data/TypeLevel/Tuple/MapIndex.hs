b1b73b3cd4c4155ab3ef951b7b2f506deaecd5bb
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-06-29 16:09:32 +0900

    typelevel-tools: Data.TypeLevel.Tuple.MapIndex: M1'3_5, M1'3_7 and so on

--- themes/type_level/typelevel-tools/src/Data/TypeLevel/Tuple/MapIndex.hs
+++ themes/type_level/typelevel-tools/src/Data/TypeLevel/Tuple/MapIndex.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE DataKinds, PolyKinds #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs -fno-warn-missing-export-lists #-}
@@ -8,3 +9,5 @@ module Data.TypeLevel.Tuple.MapIndex where
 import Data.TypeLevel.Tuple.MapIndex.TH
 
 uncurry mkM `mapM` [ (i, n) | n <- [2 .. 10], i <- [0 .. n - 1] ]
+
+uncurry mkMTup `mapM` (concatMap (\n -> (, n) <$> mTupIndices n) [3 .. 8])
--- themes/type_level/typelevel-tools/src/Data/TypeLevel/Tuple/MapIndex/TH.hs
+++ themes/type_level/typelevel-tools/src/Data/TypeLevel/Tuple/MapIndex/TH.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Data.TypeLevel.Tuple.MapIndex.TH (mkM) where
+module Data.TypeLevel.Tuple.MapIndex.TH (mkM, mkMTup, mTupIndices) where
 
 import Language.Haskell.TH
+import Data.List qualified as L
 
 vars :: [String]
 vars = mkVar `concatMap` [1 ..]
@@ -39,11 +41,49 @@ bar ts = promotedT '(:) `appT` promotedTuple ts `appT` (varT $ mkName "rst")
 tuple :: [Type] -> Type
 tuple ts = foldl AppT (TupleT $ length ts) ts
 
+tuple' :: [Kind] -> Kind
+tuple' ts = foldl appK (tupleK $ length ts) ts
+
 promotedTuple :: [TypeQ] -> TypeQ
 promotedTuple ts = foldl appT (promotedTupleT $ length ts) ts
 
-{-
-type family M0_5 (tpl :: [(i, j, k, l, m)]) :: [i] where
-	M0_5 '[] = '[]
-	M0_5 ('(x, y, z, w, v) ': xyzwvs) = x ': M0_5 xyzwvs
-	-}
+mkMTup :: [Int] -> Int -> DecQ
+mkMTup is n = do
+	xyzs <- newName . concat $ take n varNames
+	vs <- newName `mapM` take n varNames
+	ks <- newName `mapM` take n kindNames
+	rst <- newName . concat $ take n varNames
+	bazRaw nm xyzs ks vs rst is
+	where
+	nm = mkName $ "M" ++ L.intercalate "'" (show <$> is) ++ "_" ++ show n
+
+bazRaw :: Name -> Name -> [Name] -> [Name] -> Name -> [Int] -> DecQ
+bazRaw tn xyzs ks vs rst is = closedTypeFamilyD tn
+	[kindedTV xyzs $ listK `appK` (tuple' $ varK <$> ks)]
+	noSig Nothing
+	[	tySynEqn Nothing
+			(conT tn `appT` promotedNilT)
+			promotedNilT,
+		tySynEqn Nothing
+			(conT tn `appT` (
+				promotedTuple (varT <$> vs) `promotedConsType`
+					varT rst ))
+			(promotedTuple (varT . (vs !!) <$> is) `promotedConsType`
+				(conT tn `appT` varT rst))
+		]
+
+promotedConsType :: Quote m => m Type -> m Type -> m Type
+promotedConsType a as = promotedConsT `appT` a `appT` as
+
+varNames :: [String]
+varNames = ((: "") <$> ['a' .. 'z']) ++ [ cs ++ [c] | cs <- varNames, c <- ['a' .. 'z'] ]
+
+kindNames :: [String]
+kindNames = ('k' :) . show <$> [0 :: Int ..]
+
+combinations :: [a] -> [[a]]
+combinations [] = [[]]
+combinations (x : xs) = combinations xs ++ ((x :) <$> combinations xs)
+
+mTupIndices :: Int -> [[Int]]
+mTupIndices n = filter ((`notElem` [0, 1, n]) . length) $ combinations [0 .. n - 1]
