cc706a71cd4897ac8accc362d43b9b931d735e07
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-07-08 22:04:03 +0900

    try-my-vulkan-snd: Gpu.Vulkan.DescriptorSet.BindingAndArrayElem: refactoring: rename bts to lbts

--- themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/DescriptorSet/BindingAndArrayElem.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/DescriptorSet/BindingAndArrayElem.hs
@@ -26,35 +26,35 @@ import Gpu.Vulkan.DescriptorSetLayout.Type qualified as Lyt
 -- * IMAGE
 
 class BindingAndArrayElemImage
-	(bts :: [Lyt.BindingType])
+	(lbts :: [Lyt.BindingType])
 	(iargs :: [(Symbol, T.Format)]) (i :: Nat) where
 	bindingAndArrayElemImage :: Integral n => n -> n -> (n, n)
 
 instance TList.IsPrefixOf iargs liargs =>
 	BindingAndArrayElemImage
-		('Lyt.Image (iarg ': liargs) : bts) (iarg ': iargs) 0 where
+		('Lyt.Image (iarg ': liargs) : lbts) (iarg ': iargs) 0 where
 	bindingAndArrayElemImage b ae = (b, ae)
 
 instance {-# OVERLAPPABLE #-}
 	BindingAndArrayElemImage
-		('Lyt.Image liargs ': bts) (iarg ': iargs) (i - 1) =>
+		('Lyt.Image liargs ': lbts) (iarg ': iargs) (i - 1) =>
 	BindingAndArrayElemImage
-		('Lyt.Image (iarg ': liargs) ': bts) (iarg ': iargs) i where
+		('Lyt.Image (iarg ': liargs) ': lbts) (iarg ': iargs) i where
 	bindingAndArrayElemImage b ae = bindingAndArrayElemImage
-		@('Lyt.Image liargs ': bts) @(iarg ': iargs) @(i - 1) b (ae + 1)
+		@('Lyt.Image liargs ': lbts) @(iarg ': iargs) @(i - 1) b (ae + 1)
 
 instance {-# OVERLAPPABLE #-}
-	BindingAndArrayElemImage ('Lyt.Image liargs : bts) iargs i =>
+	BindingAndArrayElemImage ('Lyt.Image liargs : lbts) iargs i =>
 	BindingAndArrayElemImage
-		('Lyt.Image (liarg ': liargs) ': bts) iargs i where
+		('Lyt.Image (liarg ': liargs) ': lbts) iargs i where
 	bindingAndArrayElemImage b a = bindingAndArrayElemImage
-		@('Lyt.Image liargs : bts) @iargs @i b (a + 1)
+		@('Lyt.Image liargs : lbts) @iargs @i b (a + 1)
 
 instance {-# OVERLAPPABLE #-}
-	BindingAndArrayElemImage bts iargs i =>
-	BindingAndArrayElemImage (bt ': bts) iargs i where
+	BindingAndArrayElemImage lbts iargs i =>
+	BindingAndArrayElemImage (bt ': lbts) iargs i where
 	bindingAndArrayElemImage b _ =
-		bindingAndArrayElemImage @bts @iargs @i (b + 1) 0
+		bindingAndArrayElemImage @lbts @iargs @i (b + 1) 0
 
 -- * BUFFER VIEW
 
@@ -63,29 +63,29 @@ class BindingAndArrayElemBufferView
 	bindingAndArrayElemBufferView :: Integral n => n -> n -> (n, n)
 
 instance TList.IsPrefixOf bvargs lbvargs => BindingAndArrayElemBufferView
-		('Lyt.BufferView (bvarg ': lbvargs) ': bts)
+		('Lyt.BufferView (bvarg ': lbvargs) ': lbts)
 		(bvarg ': bvargs) 0 where
 	bindingAndArrayElemBufferView b a = (b, a)
 
 instance {-# OVERLAPPABLE #-}
 	BindingAndArrayElemBufferView
-		('Lyt.BufferView lbvargs ': bts) (bvarg ': bvargs) (i - 1) =>
+		('Lyt.BufferView lbvargs ': lbts) (bvarg ': bvargs) (i - 1) =>
 	BindingAndArrayElemBufferView
-		('Lyt.BufferView (bvarg ': lbvargs) ': bts)
+		('Lyt.BufferView (bvarg ': lbvargs) ': lbts)
 		(bvarg ': bvargs) i where
 	bindingAndArrayElemBufferView b a = bindingAndArrayElemBufferView
-		@('Lyt.BufferView lbvargs ': bts) @(bvarg ': bvargs) @(i - 1)
+		@('Lyt.BufferView lbvargs ': lbts) @(bvarg ': bvargs) @(i - 1)
 		b a
 
 instance {-# OVERLAPPABLE #-} BindingAndArrayElemBufferView
-	('Lyt.BufferView lbvargs ': bts) bvargs i =>
+	('Lyt.BufferView lbvargs ': lbts) bvargs i =>
 	BindingAndArrayElemBufferView
-		('Lyt.BufferView (bvarg ': lbvargs) ': bts) bvargs i where
+		('Lyt.BufferView (bvarg ': lbvargs) ': lbts) bvargs i where
 	bindingAndArrayElemBufferView b a = bindingAndArrayElemBufferView
-		@('Lyt.BufferView lbvargs ': bts) @bvargs @i b (a + 1)
+		@('Lyt.BufferView lbvargs ': lbts) @bvargs @i b (a + 1)
 
-instance {-# OVERLAPPABLE #-} BindingAndArrayElemBufferView bts bvargs i =>
+instance {-# OVERLAPPABLE #-} BindingAndArrayElemBufferView lbts bvargs i =>
 	BindingAndArrayElemBufferView
-		(bt ': bts) bvargs i where
+		(bt ': lbts) bvargs i where
 	bindingAndArrayElemBufferView b _a =
-		bindingAndArrayElemBufferView @bts @bvargs @i (b + 1) 0
+		bindingAndArrayElemBufferView @lbts @bvargs @i (b + 1) 0
