5fbd56928a5bdc16cbb1fdfd2b6f8b45054b45de
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-07-11 13:27:48 +0900

    try-my-vulkan-snd: Gpu.Vulkan.Framebuffer.Enum

---
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Framebuffer/Enum.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE PackageImports #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Gpu.Vulkan.Framebuffer.Enum (module Gpu.Vulkan.Framebuffer.Enum) where
+
+import "try-vulkan-middle" Gpu.Vulkan.Framebuffer.Enum
