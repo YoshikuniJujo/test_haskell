1e8cf1a81bb863f2c7a2dcfc94cd18ca12b777f3
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-07-11 10:39:58 +0900

    try-vulkan-middle: Gpu.Vulkan.Base.Middle

---
+++ themes/gui/vulkan/try-vulkan-middle/src/Gpu/Vulkan/Base/Middle.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE PatternSynonyms #-}
+{-# OPTIOnS_GHC -Wall -fno-warn-tabs #-}
+
+module Gpu.Vulkan.Base.Middle (pattern NullHandle, ObjectHandle) where
+
+import Gpu.Vulkan.Base.Middle.Internal
