80ece1e80b252799e754edbbe1872f39f37f3fd4
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-07-18 15:02:38 +0900

    try-my-vulkan-snd: Gpu.Vulkan.Memory

--- themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Memory.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Memory.hs
@@ -66,6 +66,8 @@ import Gpu.Vulkan.Memory.OffsetSize
 import Gpu.Vulkan.Memory.Type
 import Gpu.Vulkan.Memory.ImageBuffer
 
+-- ALLOCATE AND BIND
+
 allocateBind :: (
 	WithPoked (TMaybe.M mn), Bindable ibargs,
 	AllocationCallbacks.ToMiddle mac ) =>
@@ -76,16 +78,6 @@ allocateBind :: (
 allocateBind dvc bs ai macc f = allocate dvc bs ai macc \m -> do
 	bnds <- bindAll dvc bs m 0
 	f bnds m
-
-reallocateBind :: (
-	WithPoked (TMaybe.M mn), Rebindable ibargs,
-	AllocationCallbacks.ToMiddle mac ) =>
-	Device.D sd -> HeteroParList.PL (U2 (ImageBufferBinded sm)) ibargs ->
-	AllocateInfo mn ->
-	TPMaybe.M (U2 AllocationCallbacks.A) mac -> M sm ibargs -> IO ()
-reallocateBind dvc bs ai macc mem = do
-	reallocate dvc bs ai macc mem
-	rebindAll dvc bs mem 0
 
 allocate :: (
 	WithPoked (TMaybe.M n), Alignments ibargs,
@@ -101,6 +93,16 @@ allocate dvc@(Device.D mdvc) bs ai
 		Memory.M.allocate mdvc mai mac
 	(\mem -> Memory.M.free mdvc mem mac)
 	\mem -> f =<< newM2' bs mem
+
+reallocateBind :: (
+	WithPoked (TMaybe.M mn), Rebindable ibargs,
+	AllocationCallbacks.ToMiddle mac ) =>
+	Device.D sd -> HeteroParList.PL (U2 (ImageBufferBinded sm)) ibargs ->
+	AllocateInfo mn ->
+	TPMaybe.M (U2 AllocationCallbacks.A) mac -> M sm ibargs -> IO ()
+reallocateBind dvc bs ai macc mem = do
+	reallocate dvc bs ai macc mem
+	rebindAll dvc bs mem 0
 
 reallocate :: (
 	WithPoked (TMaybe.M n), Alignments ibargs,
@@ -121,9 +123,6 @@ data AllocateInfo mn = AllocateInfo {
 
 deriving instance Show (TMaybe.M mn) => Show (AllocateInfo mn)
 deriving instance Eq (TMaybe.M mn) => Eq (AllocateInfo mn)
-
--- deriving instance Show (HeteroParList.PL (U2 ImageBuffer) ibargs) =>
---	Show (M s ibargs)
 
 allocateInfoToMiddle :: forall sd ibargs n . Alignments ibargs =>
 	Device.D sd -> HeteroParList.PL (U2 ImageBuffer) ibargs ->
@@ -162,9 +161,18 @@ memoryRequirementsListToSize sz0 (malgn : malgns) (reqs : reqss) =
 		(((sz0 - 1) `div` algn + 1) * algn + sz) malgns reqss
 	where
 	sz = Memory.M.requirementsSize reqs
---	algn = Memory.M.requirementsAlignment reqs
 	algn = fromIntegral (fromMaybe 1 malgn) `lcm`
 		Memory.M.requirementsAlignment reqs
+
+-- READ AND WRITE
+
+read :: forall nm obj v sd sm ibargs .
+	(VObj.StoreObject v obj, OffsetSize nm obj ibargs) =>
+	Device.D sd -> M sm ibargs -> Memory.M.MapFlags -> IO v
+read dvc mem flgs = bracket
+	(map @nm @obj dvc mem flgs) (const $ unmap dvc mem)
+	(\(ptr :: Ptr (VObj.TypeOfObject obj)) ->
+		VObj.loadObject @_ @obj ptr =<< objectLength @nm @obj mem)
 
 write :: forall nm obj sd sm ibargs v .
 	(VObj.StoreObject v obj, OffsetSize nm obj ibargs) =>
@@ -175,22 +183,11 @@ write dvc mem flgs v = bracket
 		ln <- objectLength @nm @obj mem
 		VObj.storeObject @_ @obj ptr ln v)
 
-read :: forall nm obj v sd sm ibargs .
-	(VObj.StoreObject v obj, OffsetSize nm obj ibargs) =>
-	Device.D sd -> M sm ibargs -> Memory.M.MapFlags -> IO v
-read dvc mem flgs = bracket
-	(map @nm @obj dvc mem flgs) (const $ unmap dvc mem)
-	(\(ptr :: Ptr (VObj.TypeOfObject obj)) ->
-		VObj.loadObject @_ @obj ptr =<< objectLength @nm @obj mem)
-
 map :: forall nm obj sd sm ibargs . OffsetSize nm obj ibargs =>
 	Device.D sd -> M sm ibargs -> Memory.M.MapFlags -> IO (Ptr (VObj.TypeOfObject obj))
 map dvc@(Device.D mdvc) m flgs = do
 	(_, mm) <- readM m
 	(ost, sz) <- offsetSize @nm @obj dvc m 0
---	putStrLn "Vk.Memory.map:"
---	putStr "ost: "; print ost
---	putStr "sz : "; print sz
 	Memory.M.map mdvc mm ost sz flgs
 
 unmap :: Device.D sd -> M sm ibargs -> IO ()
