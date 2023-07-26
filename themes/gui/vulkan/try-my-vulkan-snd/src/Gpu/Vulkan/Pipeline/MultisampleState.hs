6841c63b0a9e01443a4963b28721dc4be838c9be
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-07-26 15:07:42 +0900

    try-my-vulkan-snd: Gpu.Vulkan.Pipeline.Foo x 3: haddock

--- themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Pipeline/MultisampleState.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Pipeline/MultisampleState.hs
@@ -1,5 +1,11 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Gpu.Vulkan.Pipeline.MultisampleState (CreateInfo(..)) where
+module Gpu.Vulkan.Pipeline.MultisampleState (
+
+	-- * CREATE INFO
+
+	CreateInfo(..)
+
+	) where
 
 import Gpu.Vulkan.Pipeline.MultisampleState.Middle
--- themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Pipeline/RasterizationState.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Pipeline/RasterizationState.hs
@@ -1,5 +1,11 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Gpu.Vulkan.Pipeline.RasterizationState (CreateInfo(..)) where
+module Gpu.Vulkan.Pipeline.RasterizationState (
+
+	-- * CREATE INFO
+
+	CreateInfo(..)
+
+	) where
 
 import Gpu.Vulkan.Pipeline.RasterizationState.Middle
