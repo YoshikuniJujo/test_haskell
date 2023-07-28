209181c575aa4fbab0beb79992b364a578ab539c
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-07-28 17:55:13 +0900

    try-my-vulkan-snd: Gpu.Vulkan.QueueFamily: tribial

--- themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/QueueFamily.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/QueueFamily.hs
@@ -2,7 +2,15 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Gpu.Vulkan.QueueFamily (
-	Properties(..), Index, pattern Ignored
+
+	-- * PROPERTIES
+
+	Properties(..),
+
+	-- * INDEX
+
+	Index, pattern Ignored
+
 	) where
 
 import Gpu.Vulkan.QueueFamily.Middle
