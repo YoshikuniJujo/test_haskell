a68dd3a0ca301007599f5e387b9d5e879ce58a31
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2023-07-31 11:57:14 +0900

    try-my-vulkan-snd: Gpu.Vulkan.Subpass: tribial

--- themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Subpass.hs
+++ themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Subpass.hs
@@ -2,7 +2,19 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Gpu.Vulkan.Subpass (
+
+	-- * SUBPASS
+
 	S(..), pattern SExternal,
-	Description(..), Dependency(..) ) where
+
+	-- * SUBPASS DESCRIPTION
+
+	Description(..),
+
+	-- * SUBPASS DEPENDENCY
+
+	Dependency(..)
+
+	) where
 
 import Gpu.Vulkan.Subpass.Middle
