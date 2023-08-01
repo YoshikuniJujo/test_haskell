ad752b7d4a4c37d91cf263179168465045f14e2d
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-08-01 14:45:47 +0900

    try-gpu-vulkan-ext-debug-utils: Gpu.Vulkan.Ext.DebugUtils.Enum

---
+++ themes/gui/vulkan/try-gpu-vulkan-ext-debug-utils/src/Gpu/Vulkan/Ext/DebugUtils/Enum.hs
@@ -0,0 +1,7 @@
+{-# LANGUAGE PackageImports #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Gpu.Vulkan.Ext.DebugUtils.Enum (
+	module Gpu.Vulkan.Ext.DebugUtils.Enum ) where
+
+import "try-vulkan-middle-ext-debug-utils" Gpu.Vulkan.Ext.DebugUtils.Enum
--- themes/gui/vulkan/try-my-vulkan-snd/src/Gpu/Vulkan/Ext/DebugUtils/Messenger/Type.hs
+++
@@ -1,5 +0,0 @@
-module Gpu.Vulkan.Ext.DebugUtils.Messenger.Type where
-
-import qualified Gpu.Vulkan.Ext.DebugUtils.Messenger.Middle as M
-
-newtype M sm = M M.M deriving Show
