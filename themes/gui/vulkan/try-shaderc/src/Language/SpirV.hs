dfeb8ae394231f3389dfa27e11f65bdca0a97587
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-08-01 11:50:01 +0900

    try-shaderc: Language.SpirV: writeFile

--- themes/gui/vulkan/try-shaderc/src/Language/SpirV.hs
+++ themes/gui/vulkan/try-shaderc/src/Language/SpirV.hs
@@ -1,5 +1,7 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Language.SpirV (S) where
+module Language.SpirV (S, writeFile) where
+
+import Prelude hiding (writeFile)
 
 import Language.SpirV.Internal
--- themes/gui/vulkan/try-shaderc/src/Language/SpirV/Internal.hs
+++ themes/gui/vulkan/try-shaderc/src/Language/SpirV/Internal.hs
@@ -11,3 +11,6 @@ import Data.ByteString qualified as BS
 import Language.SpirV.ShaderKind
 
 newtype S (sknd :: ShaderKind) = S BS.ByteString deriving (Show, IsString)
+
+writeFile :: FilePath -> S sknd -> IO ()
+writeFile fp (S bs) = BS.writeFile fp bs
