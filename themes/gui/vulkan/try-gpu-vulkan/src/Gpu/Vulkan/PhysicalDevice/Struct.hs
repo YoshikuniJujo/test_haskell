cfb029b0b8a2f07e5b88e3914ed460b8baf07a87
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-08-07 17:42:43 +0900

    try-gpu-vulkan: Gpu.Vulkan.PhysicalDevice.Struct

---
+++ themes/gui/vulkan/try-gpu-vulkan/src/Gpu/Vulkan/PhysicalDevice/Struct.hs
@@ -0,0 +1,7 @@
+{-# LANGUAGE PackageImports #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Gpu.Vulkan.PhysicalDevice.Struct (
+	module Gpu.Vulkan.PhysicalDevice.Struct ) where
+
+import "try-vulkan-middle" Gpu.Vulkan.PhysicalDevice.Struct
