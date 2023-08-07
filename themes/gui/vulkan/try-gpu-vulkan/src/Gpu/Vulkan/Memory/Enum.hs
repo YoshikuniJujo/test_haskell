7cabb9fdf9b5c1ba4d1f0f5ae013a1ef42a36001
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-08-07 16:36:56 +0900

    try-gpu-vulkan: Gpu.Vulkan.{Memory|Queue}.Enum

---
+++ themes/gui/vulkan/try-gpu-vulkan/src/Gpu/Vulkan/Memory/Enum.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE PackageImports #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Gpu.Vulkan.Memory.Enum (module Gpu.Vulkan.Memory.Enum) where
+
+import "try-vulkan-middle" Gpu.Vulkan.Memory.Enum
---
+++ themes/gui/vulkan/try-gpu-vulkan/src/Gpu/Vulkan/Queue/Enum.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE PackageImports #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Gpu.Vulkan.Queue.Enum (module Gpu.Vulkan.Queue.Enum) where
+
+import "try-vulkan-middle" Gpu.Vulkan.Queue.Enum
