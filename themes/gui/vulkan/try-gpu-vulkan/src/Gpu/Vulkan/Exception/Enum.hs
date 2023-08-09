20b66fa6243986bd314cb3baee622d4ed4e1af47
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-08-09 09:25:22 +0900

    try-gpu-vulkan: Gpu.Vulkan.Exception.Enum and Gpu.Vulkan.ImageView.Enum

---
+++ themes/gui/vulkan/try-gpu-vulkan/src/Gpu/Vulkan/Exception/Enum.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE PackageImports #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Gpu.Vulkan.Exception.Enum (module Gpu.Vulkan.Exception.Enum) where
+
+import "try-vulkan-middle" Gpu.Vulkan.Exception.Enum
---
+++ themes/gui/vulkan/try-gpu-vulkan/src/Gpu/Vulkan/ImageView/Enum.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE PackageImports #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Gpu.Vulkan.ImageView.Enum (module Gpu.Vulkan.ImageView.Enum) where
+
+import "try-vulkan-middle" Gpu.Vulkan.ImageView.Enum
