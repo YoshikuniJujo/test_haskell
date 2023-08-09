da7ee649ed56d79f09d366a0e10fffb5cf5b4c89
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-08-09 11:01:54 +0900

    try-gpu-vulkan-: Gpu.Vulkan.Pipeline.ShaderStage.Enum

---
+++ themes/gui/vulkan/try-gpu-vulkan/src/Gpu/Vulkan/Pipeline/ShaderStage/Enum.hs
@@ -0,0 +1,7 @@
+{-# LANGUAGE PackageImports #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Gpu.Vulkan.Pipeline.ShaderStage.Enum (
+	module Gpu.Vulkan.Pipeline.ShaderStage.Enum ) where
+
+import "try-vulkan-middle" Gpu.Vulkan.Pipeline.ShaderStage.Enum
