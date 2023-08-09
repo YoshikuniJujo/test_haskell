c9d678d1ce35af512a3cb49d735cd61713deb763
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-08-09 10:16:13 +0900

    try-gpu-vulkan-khr: Gpu.Vulkan.Khr.Surface.Enum

---
+++ themes/gui/vulkan/try-gpu-vulkan-khr/src/Gpu/Vulkan/Khr/Surface/Enum.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE PackageImports #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Gpu.Vulkan.Khr.Surface.Enum (module Gpu.Vulkan.Khr.Surface.Enum) where
+
+import "try-vulkan-middle-khr" Gpu.Vulkan.Khr.Surface.Enum
