5240eb217457a73321da514fd6709ee172615c59
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-08-09 09:59:25 +0900

    try-gpu-vulkan: Gpu.Vulkan.ColorComponent.Enum and Gpu.Vulkan.Sample.Enum

---
+++ themes/gui/vulkan/try-gpu-vulkan/src/Gpu/Vulkan/ColorComponent/Enum.hs
@@ -0,0 +1,7 @@
+{-# LANGUAGE PackageImports #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Gpu.Vulkan.ColorComponent.Enum (
+	module Gpu.Vulkan.ColorComponent.Enum ) where
+
+import "try-vulkan-middle" Gpu.Vulkan.ColorComponent.Enum
---
+++ themes/gui/vulkan/try-gpu-vulkan/src/Gpu/Vulkan/Sample/Enum.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE PackageImports #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Gpu.Vulkan.Sample.Enum (module Gpu.Vulkan.Sample.Enum) where
+
+import "try-vulkan-middle" Gpu.Vulkan.Sample.Enum
