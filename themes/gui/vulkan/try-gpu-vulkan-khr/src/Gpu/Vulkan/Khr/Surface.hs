aadba46cd52e37d2bef1cbe7280d78d776653211
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-08-09 10:22:01 +0900

    try-gpu-vulkan-khr: Gpu.Vulkan.Khr.Surface: add Capabilities and Format

--- themes/gui/vulkan/try-gpu-vulkan-khr/src/Gpu/Vulkan/Khr/Surface.hs
+++ themes/gui/vulkan/try-gpu-vulkan-khr/src/Gpu/Vulkan/Khr/Surface.hs
@@ -1,5 +1,6 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Gpu.Vulkan.Khr.Surface (S) where
+module Gpu.Vulkan.Khr.Surface (S, M.Capabilities(..), M.Format(..)) where
 
 import Gpu.Vulkan.Khr.Surface.Type
+import qualified Gpu.Vulkan.Khr.Surface.Middle as M
