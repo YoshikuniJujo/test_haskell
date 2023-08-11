a179901e68ebbf4fe1dc21fa382c76e5c8437b86
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-08-11 10:13:49 +0900

    try-vulkan-compute: csrc/bitonicSortPairs.c

--- themes/gui/vulkan/try-vulkan-compute/src/TryBitonicSortPairs.hs
+++ themes/gui/vulkan/try-vulkan-compute/src/TryBitonicSortPairs.hs
@@ -1,6 +1,7 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module TryBitonicSortPairs (trySeparateNum) where
+module TryBitonicSortPairs (
+	trySeparateNum, bitSeparate ) where
 
 import Foreign.C.Types
 
@@ -8,3 +9,14 @@ foreign import ccall "try_separate_num" c_try_separate_num :: CInt -> IO ()
 
 trySeparateNum :: Int -> IO ()
 trySeparateNum = c_try_separate_num . fromIntegral
+
+foreign import ccall "bit_separate" c_bit_separate :: CInt -> CInt -> CInt
+
+bitSeparate :: Int -> Int -> Int
+bitSeparate bs i =
+	fromIntegral $ c_bit_separate (fromIntegral bs) (fromIntegral i)
+
+foreign import ccall "check_bit" c_check_bit :: CInt -> CInt -> CInt
+
+checkBit :: Int -> Int -> Bool
+checkBit bs i = c_check_bit (fromIntegral bs) (fromIntegral i) /= 0
