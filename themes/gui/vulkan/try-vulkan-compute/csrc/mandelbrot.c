048635bb3c22cd220f45574c49d5d628f5ef0e1b
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-08-15 13:12:25 +0900

    try-vulkan-compute: csrc/mandelbrot.c: pixel_to_point

--- themes/gui/vulkan/try-vulkan-compute/csrc/mandelbrot.c
+++ themes/gui/vulkan/try-vulkan-compute/csrc/mandelbrot.c
@@ -1,12 +1,13 @@
 #include <complex.h>
 
-int escape_time(float complex c, int lm)
+int
+escape_time(float complex c, int lm)
 {
 	float complex z = 0 + 0i;
 
 	for (int i = 0; i <= lm; i++) {
 		z = z * z + c;
-		if (cabs(z) > 2) {
+		if (cabsf(z) > 2) {
 			return i;
 		}
 	}
@@ -14,12 +15,45 @@ int escape_time(float complex c, int lm)
 	return -1;
 }
 
-float complex make_complex(float re, float im)
+float complex
+make_complex(float re, float im)
 {
 	return re + im * I;
 }
 
-int escape_time_hs(float re, float im, int lm)
+int
+escape_time_hs(float re, float im, int lm)
 {
 	return escape_time(make_complex(re, im), lm);
+}
+
+float complex
+pixel_to_point(
+	int w, int h, int x, int y,
+	float complex upper_left, float complex lower_right )
+{
+
+	float width = crealf(lower_right) - crealf(upper_left);
+	float height = cimagf(upper_left) - cimagf(lower_right);
+
+	return crealf(upper_left) + (float)x * width / (float)w +
+		(cimagf(upper_left) - (float)y * height / (float)h) * I;
+
+}
+
+void
+get_complex(float complex c, float *re, float *im)
+{
+	*re = crealf(c); *im = cimagf(c);
+}
+
+void
+pixel_to_point_hs(
+	int w, int h, int x, int y,
+	float lft, float upr, float rgt, float lwr, float *re, float *im )
+{
+	float complex upper_left = make_complex(lft, upr);
+	float complex lower_right = make_complex(rgt, lwr);
+	float complex c = pixel_to_point(w, h, x, y, upper_left, lower_right);
+	get_complex(c, re, im);
 }
--- themes/gui/vulkan/try-vulkan-compute/src/TryMandelbrot.hs
+++ themes/gui/vulkan/try-vulkan-compute/src/TryMandelbrot.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE BlockArguments #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module TryMandelbrot where
 
+import Foreign.Ptr
+import Foreign.Marshal.Alloc
+import Foreign.Storable
 import Foreign.C.Types
 import Data.Complex
+import System.IO.Unsafe
 
 foreign import ccall "escape_time_hs" c_escape_time_hs ::
 	CFloat -> CFloat -> CInt -> CInt
@@ -12,3 +17,14 @@ escapeTime :: Complex CFloat -> CInt -> Maybe CInt
 escapeTime (re :+ im) lm = case c_escape_time_hs re im lm of
 	t | t < 0 -> Nothing
 	t -> Just t
+
+foreign import ccall "pixel_to_point_hs" c_pixel_to_point_hs ::
+	CInt -> CInt -> CInt -> CInt -> CFloat -> CFloat -> CFloat -> CFloat ->
+	Ptr CFloat -> Ptr CFloat -> IO ()
+
+pixelToPoint :: (CInt, CInt) -> (CInt, CInt) ->
+	Complex CFloat -> Complex CFloat -> Complex CFloat
+pixelToPoint (w, h) (x, y) (lft :+ upr) (rgt :+ lwr) = unsafePerformIO
+	$ alloca \pre -> alloca \pim -> do
+		c_pixel_to_point_hs w h x y lft upr rgt lwr pre pim
+		(:+) <$> peek pre <*> peek pim
