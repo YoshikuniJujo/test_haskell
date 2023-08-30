89ffc5ed2a3e427fe8b6104e42b02c6d00ed5d27
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-08-30 14:10:26 +0900

    try-gpu-vulkan: Gpu.Vulkan.DescriptorSetLayout.BindingFlags

---
+++ themes/gui/vulkan/try-gpu-vulkan/src/Gpu/Vulkan/DescriptorSetLayout/BindingFlags.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Gpu.Vulkan.DescriptorSetLayout.BindingFlags (M.CreateInfo(..)) where
+
+import Gpu.Vulkan.DescriptorSetLayout.BindingFlags.Middle qualified as M
