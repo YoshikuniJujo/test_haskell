2fba747111cec588883e0d4c9af19b5838a6e59f
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-09-08 10:00:24 +0900

    tribial-trials: Generics.TypeList

--- tribial/tribial-trials/src/Generics/TypeList.hs
+++ tribial/tribial-trials/src/Generics/TypeList.hs
@@ -16,22 +16,24 @@ data Foo = Foo Int Double Char deriving (Show, Generic)
 
 class Index (ts :: [Type]) (t :: Type) where index :: (Int, Int)
 
-class Index' (t0 :: Type -> Type) (ts :: [Type]) (t :: Type) where
-	index' :: (Int, Int)
-
 instance (Generic t0, Index' (Rep t0) ts t) => Index (t0 ': ts) t where
 	index = index' @(Rep t0) @ts @t
 
-instance Index' (M1 S _m (K1 R t)) ts t where index' = (0, 0)
+class Index' (t0 :: Type -> Type) (ts :: [Type]) (t :: Type) where
+	index' :: (Int, Int)
 
-instance {-# OVERLAPPABLE #-} Index ts t =>
-	Index' (M1 S _m (K1 R t0)) ts t where
+instance Index' body ts t => Index' (M1 _i _c body) ts t where
+	index' = index' @body @ts @t
+
+instance Index' (K1 _i t) ts t where index' = (0, 0)
+
+instance {-# OVERLAPPABLE #-} Index ts t => Index' (K1 R t0) ts t where
 	index' = (+ 1) `first` index @ts @t
 
-instance Index' (M1 S _m (K1 R t) :*: _ms) ts t where index' = (0, 0)
+instance Index' (body :*: bs) ts t => Index' (M1 _i _c body :*: bs) ts t where
+	index' = index' @(body :*: bs) @ts @t
+
+instance Index' (K1 R t :*: _ms) ts t where index' = (0, 0)
 
 instance {-# OVERLAPPABLE #-} Index' ms ts t => Index' (_m :*: ms) ts t where
 	index' = (+ 1) `second` index' @ms @ts @t
-
-instance Index' body ts t => Index' (D1 _d (C1 _c body)) ts t where
-	index' = index' @body @ts @t
