556a30dcacc0116bac0d887eb31a8382a900298d
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-09-15 10:50:51 +0900

    tribial-trials: Number.Floating: decs

--- tribial/tribial-trials/src/Number/Floating.hs
+++ tribial/tribial-trials/src/Number/Floating.hs
@@ -18,3 +18,15 @@ incs = iterate next $ encodeFloat (r ^ (ds - 1)) (- (ds - a))
 	r = floatRadix @n undefined
 	(a, _) = floatRange @n undefined
 	ds = floatDigits @n undefined
+
+prev :: RealFloat n => n -> n
+prev n	| x > 0 = encodeFloat (x - 1) y
+	| otherwise = encodeFloat mx (y - 1)
+	where
+	r = floatRadix n
+	ds = floatDigits n
+	mx = r ^ ds - 1
+	(x, y) = decodeFloat n
+
+decs :: forall n . RealFloat n => [n]
+decs = iterate prev 1
