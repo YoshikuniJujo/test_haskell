1aadef0b18413853dc20ee5a659bd95abb428731
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-09-27 10:37:02 +0900

    some

--- themes/gui/vulkan/try_using/try-vulkan-graphics/app/try-multiple-windows.hs
+++ themes/gui/vulkan/try_using/try-vulkan-graphics/app/try-multiple-windows.hs
@@ -1013,8 +1013,10 @@ data SyncObjects (ssos :: (Type, Type, Type)) where
 createSyncObjects ::
 	Vk.Dvc.D sd -> (forall sias srfs siff . SyncObjects '(sias, srfs, siff) -> IO a ) -> IO a
 createSyncObjects dvc f =
-	Vk.Semaphore.create @'Nothing dvc def nil' \ias ->
-	Vk.Semaphore.create @'Nothing dvc def nil' \rfs ->
+	Vk.Semaphore.group dvc nil' \iasgrp ->
+	Vk.Semaphore.group dvc nil' \rfsgrp ->
+	Vk.Semaphore.create' @_ @Nothing iasgrp () def >>= \(Right ias) ->
+	Vk.Semaphore.create' @_ @Nothing rfsgrp () def >>= \(Right rfs) ->
 	Vk.Fence.create @'Nothing dvc fncInfo nil' \iff ->
 	f $ SyncObjects ias rfs iff
 	where
