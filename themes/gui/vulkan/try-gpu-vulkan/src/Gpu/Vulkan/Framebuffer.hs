c8d9744a9f42dac6d738ed98ac86a32feb38d1a7
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-09-30 14:44:31 +0900

    try-gpu-vulkan: Gpu.Vulkan.Framebuffer: Group

--- themes/gui/vulkan/try-gpu-vulkan/src/Gpu/Vulkan/Framebuffer.hs
+++ themes/gui/vulkan/try-gpu-vulkan/src/Gpu/Vulkan/Framebuffer.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE DataKinds, PolyKinds #-}
@@ -12,16 +13,24 @@ module Gpu.Vulkan.Framebuffer (
 
 	-- * CREATE
 
-	create, recreate, F, CreateInfo(..)
+	create, recreate, F, CreateInfo(..),
+
+	-- ** Group
+
+	group, Group, create', destroy, lookup
 
 	) where
 
+import Prelude hiding (lookup)
 import Foreign.Storable.PeekPoke
+import Control.Concurrent.STM
+import Control.Concurrent.STM.TSem
 import Control.Exception
 import Data.TypeLevel.Tuple.Uncurry
 import Data.TypeLevel.Maybe qualified as TMaybe
 import Data.TypeLevel.ParMaybe qualified as TPMaybe
-import qualified Data.HeteroParList as HeteroParList
+import Data.HeteroParList qualified as HeteroParList
+import Data.Map qualified as Map
 import Data.Word
 
 import Gpu.Vulkan.Framebuffer.Enum
@@ -81,3 +90,56 @@ recreate :: (WithPoked (TMaybe.M mn), AllocationCallbacks.ToMiddle mac) =>
 recreate (Device.D dvc) ci
 	(AllocationCallbacks.toMiddle -> macc) (F fb) =
 	M.recreate dvc (createInfoToMiddle ci) macc macc fb
+
+data Group sd ma sf k = Group (Device.D sd)
+	(TPMaybe.M (U2 AllocationCallbacks.A) ma) TSem (TVar (Map.Map k (F sf)))
+
+group :: AllocationCallbacks.ToMiddle ma =>
+	Device.D sd -> TPMaybe.M (U2 AllocationCallbacks.A) ma ->
+	(forall sf . Group sd ma sf k -> IO a) -> IO a
+group dvc@(Device.D mdvc) mac@(AllocationCallbacks.toMiddle -> mmac) f = do
+	(sem, m) <- atomically $ (,) <$> newTSem 1 <*> newTVar Map.empty
+	rtn <- f $ Group dvc mac sem m
+	((\(F ff) -> M.destroy mdvc ff mmac) `mapM_`) =<< atomically (readTVar m)
+	pure rtn
+
+create' :: (
+	Ord k, WithPoked (TMaybe.M mn), AllocationCallbacks.ToMiddle ma) =>
+	Group sd ma sf k -> k -> CreateInfo mn sr aargs -> IO (Either String (F sf))
+create' (Group (Device.D mdvc)
+	(AllocationCallbacks.toMiddle -> mmac) sem sf) k
+	(createInfoToMiddle -> ci) = do
+	ok <- atomically do
+		mx <- Map.lookup k <$> readTVar sf
+		case mx of
+			Nothing -> waitTSem sem >> pure True
+			Just _ -> pure False
+	if ok
+	then do	f <- M.create mdvc ci mmac
+		let	f' = F f
+		atomically $ modifyTVar sf (Map.insert k f') >> signalTSem sem
+		pure $ Right f'
+	else pure . Left $
+		"Gpu.Vulkan.Framebuffer.create': The key already exist"
+
+destroy :: (Ord k, AllocationCallbacks.ToMiddle ma) =>
+	Group sd ma sf k -> k -> IO (Either String ())
+destroy (Group (Device.D mdvc)
+	(AllocationCallbacks.toMiddle -> ma) sem fs) k = do
+	mf <- atomically do
+		mx <- Map.lookup k <$> readTVar fs
+		case mx of
+			Nothing -> pure Nothing
+			Just _ -> waitTSem sem >> pure mx
+	case mf of
+		Nothing -> pure $ Left
+			"Gpu.Vulkan.Framebuffer.destroy: No such key"
+		Just (F f) -> do
+			M.destroy mdvc f ma
+			atomically do
+				modifyTVar fs $ Map.delete k
+				signalTSem sem
+				pure $ Right ()
+
+lookup :: Ord k => Group sd ma sf k -> k -> IO (Maybe (F sf))
+lookup (Group _ _ _sem fs) k = atomically $ Map.lookup k <$> readTVar fs
