bba4d2e4fec400504c072d7d4df15f3f8a9bcbf3
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-10-18 11:37:13 +0900

    try-gpu-vulkan-khr: Gpu.Vulkah.Khr.Surface: export destroy and lookup

--- themes/gui/vulkan/try-gpu-vulkan-khr/src/Gpu/Vulkan/Khr/Surface.hs
+++ themes/gui/vulkan/try-gpu-vulkan-khr/src/Gpu/Vulkan/Khr/Surface.hs
@@ -2,8 +2,9 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Gpu.Vulkan.Khr.Surface (
-	S, group, Group,
+	S, group, Group, destroy, lookup,
 
 	Capabilities(..), Format(..)) where
 
+import Prelude hiding (lookup)
 import Gpu.Vulkan.Khr.Surface.Internal
