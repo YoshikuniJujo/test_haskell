9b0167e29314f472b36fa68e290345d5b334fcb5
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-10-19 17:14:39 +0900

    try-glfw-group: Graphics.UI.GlfwG.Key: repair

--- themes/gui/vulkan/try-glfw-group/src/Graphics/UI/GlfwG/Key.hs
+++ themes/gui/vulkan/try-glfw-group/src/Graphics/UI/GlfwG/Key.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Graphics.UI.GlfwG.Key (B.Key, B.KeyState, B.ModifierKeys) where
+module Graphics.UI.GlfwG.Key (
+	B.Key(..), B.KeyState(..), B.ModifierKeys(..) ) where
 
 import Graphics.UI.GLFW qualified as B
