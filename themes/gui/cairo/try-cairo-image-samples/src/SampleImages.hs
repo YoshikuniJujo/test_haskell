efec783ec7690a958657856c4ac9bf7d2d28bec5
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-10-26 16:29:50 +0900

    try-cairo-image-samples

---
+++ themes/gui/cairo/try-cairo-image-samples/src/SampleImages.hs
@@ -0,0 +1,36 @@
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module SampleImages where
+
+import Control.Monad.Primitive
+import Control.Monad.ST
+import Data.Maybe
+import Data.Color
+import Data.CairoImage.Internal
+import Graphics.Cairo.Surfaces.ImageSurfaces
+import Graphics.Cairo.Drawing.CairoT
+import Graphics.Cairo.Drawing.Paths
+
+twoRectangles :: Argb32
+twoRectangles = case runST twoRectanglesPrim of
+	CairoImageArgb32 i -> i
+	_ -> error "never occur"
+
+twoRectanglesPrim :: PrimMonad m => m CairoImage
+twoRectanglesPrim = do
+	sfc0 <- cairoImageSurfaceCreate CairoFormatArgb32 256 256
+	cr <- cairoCreate sfc0
+
+	cairoSetSourceRgb cr . fromJust $ rgbDouble 0.7 0.7 0.7
+	cairoRectangle cr 0 0 256 256
+	cairoFill cr
+
+	cairoSetSourceRgb cr . fromJust $ rgbDouble 0.8 0.2 0.3
+	cairoRectangle cr 50 50 110 110
+	cairoFill cr
+
+	cairoSetSourceRgb cr . fromJust $ rgbDouble 0.7 0.7 0.3
+	cairoRectangle cr 100 130 100 70
+	cairoFill cr
+
+	cairoImageSurfaceGetCairoImage sfc0
