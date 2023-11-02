3fb506bde16cbd9cf6fc895aec9ccb51be3424a3
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2023-11-02 11:48:57 +0900

    try-vulkan-rectangle: Gpu.Vulkan.CairoImage: drawView

--- themes/gui/vulkan/try_using/try-vulkan-rectangles/src/Gpu/Vulkan/CairoImage.hs
+++ themes/gui/vulkan/try_using/try-vulkan-rectangles/src/Gpu/Vulkan/CairoImage.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE ImportQualifiedPost #-}
-{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE BlockArguments, LambdaCase #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE DataKinds #-}
@@ -12,14 +12,21 @@ module Gpu.Vulkan.CairoImage (
 import Foreign.Ptr
 import Foreign.Marshal.Array
 import Foreign.Storable
+import Control.Monad.Primitive
 import Data.Maybe
 import Data.Array
 import Data.Word
+import Data.Color
+import Data.CairoImage
+import Graphics.Cairo.Drawing.CairoT
+import Graphics.Cairo.Drawing.Paths
+import Graphics.Cairo.Surfaces.ImageSurfaces
 import Gpu.Vulkan.TypeEnum qualified as Vk.T
 import Gpu.Vulkan.Object.Base qualified as BObj
 
 import Convert
 import SampleImages
+import Trial.Followbox.ViewType as VT
 
 newtype CairoArgb32 = CairoArgb32 Argb32 deriving Show
 
@@ -83,3 +90,41 @@ instance BObj.IsImage CairoArgb32 where
 		CairoArgb32 $ generateImage w h \x y ->
 			pixelRgbaToPixelArgb32 $ (pss' ! y) ! x
 		where pss' = listArray (0, h - 1) (listArray (0, w - 1) <$> pss)
+
+drawView :: PrimMonad m =>
+	CairoSurfaceImageT s (PrimState m) -> CairoT r (PrimState m) -> View ->
+	m Argb32
+drawView sfc0 cr v = do
+	cairoSetSourceRgb cr . fromJust $ rgbDouble 0.7 0.7 0.7
+	cairoRectangle cr 0 0 256 256
+	cairoFill cr
+
+	cairoSetSourceRgb cr . fromJust $ rgbDouble 0.8 0.2 0.3
+	cairoRectangle cr 50 50 110 110
+	cairoFill cr
+
+	cairoSetSourceRgb cr . fromJust $ rgbDouble 0.7 0.7 0.3
+	cairoRectangle cr 100 130 100 70
+	cairoFill cr
+
+	cairoImageSurfaceGetCairoImage sfc0 >>= \case
+		CairoImageArgb32 i -> pure i
+		_ -> error "never occur"
+
+drawLine :: PrimMonad m =>
+	CairoSurfaceImageT s (PrimState m) -> CairoT r (PrimState m) -> Line ->
+	m ()
+drawLine sfc cr (Line' _ _ _ _) = do
+	pure ()
+
+drawText :: PrimMonad m =>
+	CairoSurfaceImageT s (PrimState m) -> CairoT r (PrimState m) -> VText ->
+	m ()
+drawText sfc cr (Text' _ _ _ _ _) = do
+	pure ()
+
+drawImage :: PrimMonad m =>
+	CairoSurfaceImageT s (PrimState m) -> CairoT r (PrimState m) -> VT.Image ->
+	m ()
+drawImage sfc cr (Image' _ _) = do
+	pure ()
