51b789385dd431db4a121ce30f0286b754668f37
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-11-08 10:57:04 +0900

    gtk4: hello.c

---
+++ other_language/c/gtk4/hello/hello.c
@@ -0,0 +1,24 @@
+#include <gtk/gtk.h>
+
+static void
+app_activate(GApplication *app, gpointer *user_data)
+{
+	g_print("GtkApplication is activated.\n");
+}
+
+int
+main(int argc, char *argv[])
+{
+
+	GtkApplication *app;
+	int stat;
+
+	app = gtk_application_new(
+		"com.github.ToshioCP.pr1", G_APPLICATION_DEFAULT_FLAGS );
+	g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);
+	stat = g_application_run(G_APPLICATION(app), argc, argv);
+	g_object_unref(app);
+
+	return stat;
+
+}
