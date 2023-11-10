53c31c76aa72ebf4e6b7cf2ccf769db0c8c347b6
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-11-10 11:09:55 +0900

    gtk4/tfe: tfe.c: GtkBox

--- other_language/c/gtk4/tfe/tfe.c
+++ other_language/c/gtk4/tfe/tfe.c
@@ -1,17 +1,47 @@
 #include <gtk/gtk.h>
 
 static void
-app_activate(GApplication *app, gpointer *user_data)
+click1_cb(GtkButton *btn, GtkWindow *win)
+{
+	const char *s;
+
+	s = gtk_button_get_label(btn);
+	if (g_strcmp0(s, "Hello.") == 0)
+		gtk_button_set_label(btn, "Good-bye.");
+	else
+		gtk_button_set_label(btn, "Hello.");
+}
+
+static void
+click2_cb(GtkButton *btn, GtkWindow *win)
+{
+	gtk_window_destroy(win);
+}
+
+static void
+app_activate(GApplication *app)
 {
 	GtkWidget *win;
-	GtkWidget *lab;
+	GtkWidget *box;
+	GtkWidget *btn1;
+	GtkWidget *btn2;
 
 	win = gtk_application_window_new(GTK_APPLICATION(app));
-	gtk_window_set_title(GTK_WINDOW(win), "WINDOW");
+	gtk_window_set_title(GTK_WINDOW(win), "LABEL");
 	gtk_window_set_default_size(GTK_WINDOW(win), 400, 300);
 
-	lab = gtk_label_new("Hello.");
-	gtk_window_set_child(GTK_WINDOW(win), lab);
+	box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+	gtk_box_set_homogeneous(GTK_BOX(box), TRUE);
+	gtk_window_set_child(GTK_WINDOW(win), box);
+
+	btn1 = gtk_button_new_with_label("Hello.");
+	g_signal_connect(btn1, "clicked", G_CALLBACK(click1_cb), NULL);
+
+	btn2 = gtk_button_new_with_label("Close");
+	g_signal_connect(btn2, "clicked", G_CALLBACK(click2_cb), win);
+
+	gtk_box_append(GTK_BOX(box), btn1);
+	gtk_box_append(GTK_BOX(box), btn2);
 
 	gtk_window_present(GTK_WINDOW(win));
 }
