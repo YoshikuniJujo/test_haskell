7ce33f14ce6a984e1ddcefd9d263a4c8a26f7972
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-11-11 18:54:34 +0900

    gtk4/tfe: tfe.c: show file contents

--- other_language/c/gtk4/tfe/tfe.c
+++ other_language/c/gtk4/tfe/tfe.c
@@ -1,12 +1,24 @@
 #include <gtk/gtk.h>
 
 static void
-app_activate(GApplication *app, gpointer *user_data)
+app_activate(GApplication *app)
+{
+	g_printerr("You need a filename argument.\n");
+}
+
+static void
+app_open(GApplication *app, GFile *files[], int n_files, char *hint)
 {
 	GtkWidget *win;
 	GtkWidget *scr;
 	GtkWidget *tv;
 	GtkTextBuffer *tb;
+
+	char *contents;
+	gsize length;
+	char *filename;
+	GError *err = NULL;
+
 	gchar *text;
 
 	text = "Once upon a time, "
@@ -24,20 +36,32 @@ app_activate(GApplication *app, gpointer *user_data)
 		;
 
 	win = gtk_application_window_new(GTK_APPLICATION(app));
-	gtk_window_set_title(GTK_WINDOW(win), "WINDOW");
 	gtk_window_set_default_size(GTK_WINDOW(win), 400, 300);
-
-	tv = gtk_text_view_new();
-	tb = gtk_text_view_get_buffer(GTK_TEXT_VIEW(tv));
-	gtk_text_buffer_set_text(tb, text, -1);
-	gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(tv), GTK_WRAP_WORD_CHAR);
 
 	scr = gtk_scrolled_window_new();
 	gtk_window_set_child(GTK_WINDOW(win), scr);
 
+	tv = gtk_text_view_new();
+	tb = gtk_text_view_get_buffer(GTK_TEXT_VIEW(tv));
+	gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(tv), GTK_WRAP_WORD_CHAR);
+	gtk_text_view_set_editable(GTK_TEXT_VIEW(tv), FALSE);
 	gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(scr), tv);
 
-	gtk_window_present(GTK_WINDOW(win));
+	if (g_file_load_contents(files[0], NULL, &contents, &length, NULL, &err)) {
+		gtk_text_buffer_set_text(tb, contents, length);
+		g_free(contents);
+
+		if ((filename = g_file_get_basename(files[0])) != NULL) {
+			gtk_window_set_title(GTK_WINDOW(win), filename);
+			g_free(filename);
+		}
+		gtk_window_present(GTK_WINDOW(win));
+	} else {
+		g_printerr("%s.\n", err->message);
+		g_error_free(err);
+		gtk_window_destroy(GTK_WINDOW(win));
+	}
+
 }
 
 int
@@ -50,6 +74,7 @@ main(int argc, char *argv[])
 	app = gtk_application_new(
 		"com.github.ToshioCP.tfvv2", G_APPLICATION_HANDLES_OPEN );
 	g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);
+	g_signal_connect(app, "open", G_CALLBACK(app_open), NULL);
 	stat = g_application_run(G_APPLICATION(app), argc, argv);
 	g_object_unref(app);
 
