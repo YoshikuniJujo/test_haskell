c529daa43ca721f43ce0ee5bb2413f9242b48bde
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-11-12 14:35:48 +0900

    gtk4/tfe: tfe.c: GtkNotebook

--- other_language/c/gtk4/tfe/tfe.c
+++ other_language/c/gtk4/tfe/tfe.c
@@ -29,36 +29,32 @@ app_open(GApplication *app, GFile *files[], int n_files, char *hint)
 	gtk_window_set_title(GTK_WINDOW(win), "file viewr");
 	gtk_window_set_default_size(GTK_WINDOW(win), 600, 400);
 	nb = gtk_notebook_new();
-	lab = gtk_label_new("TEST");
-
-	scr = gtk_scrolled_window_new();
 	gtk_window_set_child(GTK_WINDOW(win), nb);
-	gtk_notebook_append_page(GTK_NOTEBOOK(nb), scr, lab);
 
 	for (i = 0; i < n_files; i++) {
-		g_print("%d\n", i);
-	}
+		if (g_file_load_contents(files[i], NULL, &contents, &length, NULL, &err)) {
+			scr = gtk_scrolled_window_new();
+			tv = gtk_text_view_new();
+			tb = gtk_text_view_get_buffer(GTK_TEXT_VIEW(tv));
+			gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(tv), GTK_WRAP_WORD_CHAR);
+			gtk_text_view_set_editable(GTK_TEXT_VIEW(tv), FALSE);
+			gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(scr), tv);
 
-	tv = gtk_text_view_new();
-	tb = gtk_text_view_get_buffer(GTK_TEXT_VIEW(tv));
-
-	if (g_file_load_contents(files[0], NULL, &contents, &length, NULL, &err)) {
-		gtk_text_buffer_set_text(tb, contents, length);
-		g_free(contents);
-
-		if ((filename = g_file_get_basename(files[0])) != NULL) {
-			g_free(filename);
+			gtk_text_buffer_set_text(tb, contents, length);
+			g_free(contents);
+			if ((filename = g_file_get_basename(files[i])) != NULL) {
+				lab = gtk_label_new(filename);
+				g_free(filename);
+			} else	lab = gtk_label_new("");
+			gtk_notebook_append_page(GTK_NOTEBOOK(nb), scr, lab);
+		} else {
+			g_printerr("%s.\n", err->message);
+			g_error_free(err);
 		}
-		gtk_window_present(GTK_WINDOW(win));
-	} else {
-		g_printerr("%s.\n", err->message);
-		g_error_free(err);
-		gtk_window_destroy(GTK_WINDOW(win));
 	}
-
-	gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(tv), GTK_WRAP_WORD_CHAR);
-	gtk_text_view_set_editable(GTK_TEXT_VIEW(tv), FALSE);
-	gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(scr), tv);
+	if (gtk_notebook_get_n_pages(GTK_NOTEBOOK(nb)) > 0)
+		gtk_window_present(GTK_WINDOW(win));
+	else	gtk_window_destroy(GTK_WINDOW(win));
 
 }
 
