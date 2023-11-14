d162644f9b5dfa5f08c90a71e26f77049753ab8a
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-11-14 15:50:59 +0900

    gtk4/toshio_cp/src/misc: da.c

---
+++ other_language/c/gtk4/toshio_cp/src/misc/da.c
@@ -0,0 +1,48 @@
+#include <gtk/gtk.h>
+
+static void
+draw_function(
+	GtkDrawingArea *area, cairo_t *cr,
+	int width, int height, gpointer user_data )
+{
+	int square_size = 40.0;
+
+	cairo_set_source_rgb(cr, 1.0, 1.0, 1.0);
+	cairo_paint(cr);
+	cairo_set_line_width(cr, 2.0);
+	cairo_set_source_rgb(cr, 0.0, 0.0, 0.0);
+	cairo_rectangle(cr,
+		width / 2.0 - square_size / 2, height / 2.0 - square_size / 2,
+		square_size, square_size);
+	cairo_stroke(cr);
+}
+
+static void
+app_activate(GApplication *app, gpointer user_data)
+{
+	GtkWidget *win = gtk_application_window_new(GTK_APPLICATION(app));
+	GtkWidget *area = gtk_drawing_area_new();
+
+	gtk_window_set_title(GTK_WINDOW(win), "da");
+	gtk_drawing_area_set_draw_func(
+		GTK_DRAWING_AREA(area), draw_function, NULL, NULL );
+	gtk_window_set_child(GTK_WINDOW(win), area);
+
+	gtk_window_present(GTK_WINDOW(win));
+}
+
+#define APPLICATION_ID "com.github.ToshioCP.da"
+
+int
+main(int argc, char **argv)
+{
+	GtkApplication *app;
+	int stat;
+
+	app = gtk_application_new(APPLICATION_ID, G_APPLICATION_DEFAULT_FLAGS);
+	g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);
+	stat = g_application_run(G_APPLICATION(app), argc, argv);
+	g_object_unref(app);
+
+	return stat;
+}
