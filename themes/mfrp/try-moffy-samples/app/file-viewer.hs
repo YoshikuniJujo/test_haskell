e844dd71cf139f639fbb9040814bcd85dc8af546
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-11-21 09:28:42 +0900

    try-moffy-samples: Stopgap.Graphics.UI.Gtk.Notebook: appendPage

--- themes/mfrp/try-moffy-samples/app/file-viewer.hs
+++ themes/mfrp/try-moffy-samples/app/file-viewer.hs
@@ -1,12 +1,14 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE BlockArguments, LambdaCase, OverloadedStrings #-}
 {-# LANGUAGE ImplicitParams #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Main (main) where
 
 import Control.Monad
 import Control.Exception
+import Data.Foldable
 import System.IO
 import System.Environment
 import System.Exit
@@ -16,9 +18,11 @@ import Stopgap.Graphics.UI.Gtk.Application qualified as Gtk.Application
 import Stopgap.Graphics.UI.Gtk.Window qualified as Gtk.Window
 import Stopgap.Graphics.UI.Gtk.ApplicationWindow
 	qualified as Gtk.ApplicationWindow
+import Stopgap.Graphics.UI.Gtk.Notebook qualified as Gtk.Notebook
 import Stopgap.Graphics.UI.Gtk.ScrolledWindow qualified as Gtk.ScrolledWindow
 import Stopgap.Graphics.UI.Gtk.TextView qualified as Gtk.TextView
 import Stopgap.Graphics.UI.Gtk.TextBuffer qualified as Gtk.TextBuffer
+import Stopgap.Graphics.UI.Gtk.Label qualified as Gtk.Label
 import Stopgap.System.GLib.Error qualified as G.Error
 import Stopgap.System.GLib.Error.Io qualified as G.Error.Io
 import Stopgap.System.GLib.Application qualified as G.Application
@@ -33,21 +37,28 @@ appOpen :: (?makeEFuns :: [G.Error.MakeEFun]) =>
 	Gtk.Application.A s -> [G.File.F] -> String -> Null -> IO ()
 appOpen app files _hint Null = do
 	win <- Gtk.ApplicationWindow.new app
+	Gtk.Window.setTitle win "file viewer"
 	Gtk.Window.setDefaultSize win 400 300
-	scr <- Gtk.ScrolledWindow.new
-	Gtk.Window.setChild win scr
+	nb <- Gtk.Notebook.new
+	Gtk.Window.setChild win nb
 
-	tv <- Gtk.TextView.new
-	tb <- Gtk.TextView.getBuffer tv
-	Gtk.TextView.setWrapMode tv Gtk.WrapWordChar
-	Gtk.TextView.setEditable tv False
-	Gtk.ScrolledWindow.setChild scr tv
+	for_ files \f -> handle (\(e :: G.Error.Io.I) -> print e) do
+		(text, _) <- G.File.loadContents f Nothing
+		scr <- Gtk.ScrolledWindow.new
 
-	(text, _) <- G.File.loadContents (files !! 0) Nothing
-		`onException` Gtk.Window.destroy win
-	Gtk.TextBuffer.setText tb text
-	Gtk.Window.setTitle win =<< G.File.getBasename (files !! 0)
-	Gtk.Window.present win
+		tv <- Gtk.TextView.new
+		tb <- Gtk.TextView.getBuffer tv
+		Gtk.TextView.setWrapMode tv Gtk.WrapWordChar
+		Gtk.TextView.setEditable tv False
+		Gtk.ScrolledWindow.setChild scr tv
+
+		Gtk.TextBuffer.setText tb text
+
+		lab <- Gtk.Label.new =<< G.File.getBasename f
+		void $ Gtk.Notebook.appendPage nb scr lab
+
+	nps <- Gtk.Notebook.getNPages nb
+	if nps > 0 then Gtk.Window.present win else Gtk.Window.destroy win
 
 main :: IO ()
 main = let	?makeEFuns = [G.Error.Io.mkEFun] in
--- themes/mfrp/try-moffy-samples/src/Stopgap/Graphics/UI/Gtk/Notebook.hsc
+++ themes/mfrp/try-moffy-samples/src/Stopgap/Graphics/UI/Gtk/Notebook.hsc
@@ -1,10 +1,12 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE ViewPatterns #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Stopgap.Graphics.UI.Gtk.Notebook where
 
 import Foreign.Ptr
+import Data.Int
 import Stopgap.Data.Ptr
 
 import Stopgap.Graphics.UI.Gtk.Widget qualified as Gtk.Widget
@@ -20,3 +22,18 @@ new :: IO N
 new = N <$> c_gtk_notebook_new
 
 foreign import ccall "gtk_notebook_new" c_gtk_notebook_new :: IO (Ptr NTag)
+
+appendPage :: (Gtk.Widget.IsW c, Gtk.Widget.IsW l) =>
+	N -> c -> l -> IO #{type int}
+appendPage (N n)
+	(Gtk.Widget.toW -> Gtk.Widget.W c) (Gtk.Widget.toW -> Gtk.Widget.W l) =
+	c_gtk_notebook_append_page n c l
+
+foreign import ccall "gtk_notebook_append_page" c_gtk_notebook_append_page ::
+	Ptr NTag -> Ptr Gtk.Widget.WTag -> Ptr Gtk.Widget.WTag -> IO #{type int}
+
+getNPages :: N -> IO #{type int}
+getNPages (N n) = c_gtk_notebook_get_n_pages n
+
+foreign import ccall "gtk_notebook_get_n_pages" c_gtk_notebook_get_n_pages ::
+	Ptr NTag -> IO #{type int}
