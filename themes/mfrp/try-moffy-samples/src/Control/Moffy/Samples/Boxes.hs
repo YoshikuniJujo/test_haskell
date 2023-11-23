23267113f8c85f135e9fd13515c73137c9168698
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-11-23 22:26:05 +0900

    try-moffy-samples: Control.Moffy.Samples.Event.Mouse

---
+++ themes/mfrp/try-moffy-samples/src/Control/Moffy/Samples/Boxes.hs
@@ -0,0 +1,1 @@
+module Control.Moffy.Samples.Boxes where
---
+++ themes/mfrp/try-moffy-samples/src/Control/Moffy/Samples/Event/Mouse.hs
@@ -0,0 +1,7 @@
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Control.Moffy.Samples.Event.Mouse where
+
+data Button = ButtonPrimary | ButtonMiddle | ButtonSecondary deriving Show
+
+-- data Down
