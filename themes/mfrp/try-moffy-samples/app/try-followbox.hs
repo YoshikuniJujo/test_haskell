35c77864625aa9b33260925cb06ea7b869ac1772
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-12-04 16:17:16 +0900

    try-moffy-samples: Control.Moffy.Samples.Followbox.RunGtkField: view1ToView1

--- themes/mfrp/try-moffy-samples/app/try-followbox.hs
+++ themes/mfrp/try-moffy-samples/app/try-followbox.hs
@@ -3,31 +3,10 @@
 {-# LANGUAGE TypeOperators #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Main where
+module Main (main) where
 
 import Control.Moffy.Samples.Followbox
 import Control.Moffy.Samples.Followbox.RunGtkField
-import Control.Moffy.Samples.Viewable.Text
-import Control.Moffy.Samples.Viewable.Image
-import Data.Type.Flip
-import Data.Type.Set
-import Data.OneOfThem
-import Data.Color
-
-import Control.Moffy.Samples.Followbox.ViewType qualified as T
-import Control.Moffy.Samples.View
-import Control.Moffy.Samples.Viewable.Shape
 
 main :: IO ()
-main = runFollowbox "firefox" Nothing $ viewToView <$%> followbox
-
-viewToView :: T.View -> View
-viewToView (T.View vs) = View $ (view1ToView1 `apply`) <$> vs
-
-view1ToView1 :: OneOfThemFun (VText :- Line :- Image :- 'Nil) View1
-view1ToView1 =
-	(\(Line' (T.Color r g b) lw p0 p1) ->
-		VLine (RgbWord8 r g b) lw p0 p1) >--
-	(\(T.Text' (T.Color r g b) fn fs p txt) ->
-		VText (RgbWord8 r g b) fn fs p txt) >-- SingletonFun
-	(\(T.Image' p (T.Png w h dt)) -> VImage p w h dt)
+main = runFollowbox "firefox" followbox
--- themes/mfrp/try-moffy-samples/src/Control/Moffy/Samples/Followbox/RunGtkField.hs
+++ themes/mfrp/try-moffy-samples/src/Control/Moffy/Samples/Followbox/RunGtkField.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE PatternSynonyms #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Control.Moffy.Samples.Followbox.RunGtkField (runFollowbox) where
@@ -11,19 +13,30 @@ import Control.Moffy.Samples.Event.CalcTextExtents
 import Control.Moffy.Samples.Handle.TChan
 import Control.Moffy.Samples.Run.TChan
 import Control.Concurrent.STM
-import Data.Type.Set ((:-), (:+:))
+import Data.Type.Set ((:-), (:+:), pattern Nil)
+import Data.Type.Flip
+import Data.Color
 import System.Random
 
 import Control.Moffy.Samples.Followbox.Event
 import Control.Moffy.Samples.Followbox.Handle
 import Control.Moffy.Samples.Followbox.TypeSynonym
 
-import Control.Moffy.Samples.Run.Gtk4 hiding (GuiEv)
+import Control.Moffy.Samples.Run.Gtk4
 
 import Control.Moffy.Samples.View
 
-runFollowbox :: String -> Maybe GithubNameToken -> Sig s FollowboxEv View () -> IO ()
-runFollowbox brws tkn sig = do
+import Control.Moffy.Samples.Followbox.ViewType qualified as T
+import Control.Moffy.Samples.Viewable.Text
+import Control.Moffy.Samples.Viewable.Image
+import Control.Moffy.Samples.Viewable.Shape
+import Data.OneOfThem
+
+runFollowbox :: String -> Sig s FollowboxEv T.View () -> IO ()
+runFollowbox brws sig = runFollowbox_ brws Nothing $ viewToView <$%> sig
+
+runFollowbox_ :: String -> Maybe GithubNameToken -> Sig s FollowboxEv View () -> IO ()
+runFollowbox_ brws tkn sig = do
 	(cer, ceo, cv) <- atomically $
 		(,,) <$> newTChan <*> newTChan <*> newTChan
 	_ <- forkIO $ runFollowboxGen cer ceo brws tkn cv (sig >> emit Stopped)
@@ -40,3 +53,14 @@ handleFollowbox ::
 	(TChan (EvReqs (CalcTextExtents :- GuiEv)), TChan (EvOccs (CalcTextExtents :- GuiEv))) -> Browser ->
 	Maybe GithubNameToken -> HandleF IO (CalcTextExtents :- GuiEv :+: FollowboxEv)
 handleFollowbox = handleFollowboxWith (uncurry . handle)
+
+viewToView :: T.View -> View
+viewToView (T.View vs) = View $ (view1ToView1 `apply`) <$> vs
+
+view1ToView1 :: OneOfThemFun (VText :- Line :- Image :- 'Nil) View1
+view1ToView1 =
+	(\(Line' (T.Color r g b) lw p0 p1) ->
+		VLine (RgbWord8 r g b) lw p0 p1) >--
+	(\(T.Text' (T.Color r g b) fn fs p txt) ->
+		VText (RgbWord8 r g b) fn fs p txt) >-- SingletonFun
+	(\(T.Image' p (T.Png w h dt)) -> VImage p w h dt)
