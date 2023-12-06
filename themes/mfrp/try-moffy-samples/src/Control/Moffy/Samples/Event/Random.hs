ed4cfe4b34f045a024731eca14fe862ea0623f4f
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-12-06 16:31:25 +0900

    try-moffy-samples: use try-moffy-samples-events

--- themes/mfrp/try-moffy-samples/src/Control/Moffy/Samples/Event/Random.hs
+++
@@ -1,9 +0,0 @@
-{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
-
-module Control.Moffy.Samples.Event.Random (
-	-- * Type
-	RandomEv,
-	-- * Get Random
-	getRandom, getRandomR ) where
-
-import Control.Moffy.Samples.Event.Random.Internal (RandomEv, getRandom, getRandomR)
