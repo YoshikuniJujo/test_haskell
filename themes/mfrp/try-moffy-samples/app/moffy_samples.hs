2a962588d767ffbf6ec0f5ceaff7264ced3fe2b1
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-12-09 14:17:51 +0900

    try-moffy-samples: Control.Moffy.Samples.FollowboxOrigin: halfway

--- themes/mfrp/try-moffy-samples/app/moffy_samples.hs
+++ themes/mfrp/try-moffy-samples/app/moffy_samples.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE LambdaCase #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
@@ -7,12 +8,14 @@ import System.Environment
 import Control.Moffy.Samples.Boxes
 import Control.Moffy.Samples.Boxes.Run.Gtk4
 import Control.Moffy.Samples.Followbox
+import Control.Moffy.Samples.FollowboxOrigin qualified as O
 import Control.Moffy.Samples.Followbox.Run.Gtk4
 
 main :: IO ()
 main = getArgs >>= \case
 	["boxes"] -> runBoxes boxes
 	["followbox"] -> runFollowbox "firefox" followbox
+	["followbox-origin"] -> runFollowbox "firefox" O.followbox
 	_ -> putStrLn $
 		"Usage: moffy_samples [SUBCOMMAND]\n\n" ++
 		"Commands are\n\tboxes\n\tfollowbox"
