03517eca8bd8175d14c55988d84396c9e40bb2be
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-12-13 16:22:52 +0900

    zenn-use-moffy: Boxes: doubler

--- tribial/zenn/zenn-use-moffy/src/Boxes.hs
+++ tribial/zenn/zenn-use-moffy/src/Boxes.hs
@@ -1,11 +1,17 @@
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE BlockArguments, LambdaCase #-}
+{-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Boxes where
 
 import Control.Moffy
+import Control.Moffy.Event.Time
 import Control.Moffy.Samples.Event.Mouse qualified as Mouse
 import Data.Type.Set
+import Data.Or
 import Data.Bool
 
 clickOn :: Mouse.Button -> React s (Singleton Mouse.Down) ()
@@ -15,3 +21,13 @@ leftClick, middleClick, rightClick :: React s (Singleton Mouse.Down) ()
 leftClick = clickOn Mouse.ButtonPrimary
 middleClick = clickOn Mouse.ButtonMiddle
 rightClick = clickOn Mouse.ButtonSecondary
+
+before :: Firstable es es' a b =>
+	React s es a -> React s es' b -> React s (es :+: es') Bool
+a `before` b = (<$> a `first` b) \case L _ -> True; _ -> False
+
+doubler :: React s (Mouse.Down :- Singleton TryWait) ()
+doubler = do
+	adjust rightClick
+	r <- rightClick `before` sleep 0.2
+	if r then pure () else doubler
