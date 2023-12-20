fa61458c010128c95a5c9230839705d1857c0178
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-12-20 11:29:38 +0900

    c/gtk/draw: drawLine.c

---
+++ other_language/c/gtk/draw/drawLine.c
@@ -0,0 +1,28 @@
+#include <gtk/gtk.h>
+
+void
+draw_callback(GtkWidget *da, cairo_t *cr)
+{
+	cairo_move_to(cr, 150, 100);
+	cairo_line_to(cr, 300, 200);
+	cairo_stroke(cr);
+}
+
+int
+main(int argc, char *argv[])
+{
+
+	gtk_init(&argc, &argv);
+
+	GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+	GtkWidget *da = gtk_drawing_area_new();
+	gtk_container_add(GTK_CONTAINER(window), da);
+	g_signal_connect(da, "draw", G_CALLBACK(draw_callback), NULL);
+
+	gtk_widget_show_all(window);
+	gtk_main();
+
+	return 0;
+}
