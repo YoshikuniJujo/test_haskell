1f9d0118fe2140c5852166a287720aef08b182a2
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-12-21 14:12:01 +0900

    2nd/zenn-use-moffy: Boxes

--- tribial/zenn/moffies/2nd/zenn-use-moffy/src/Boxes.hs
+++ tribial/zenn/moffies/2nd/zenn-use-moffy/src/Boxes.hs
@@ -54,3 +54,50 @@ wiggleRect :: Rect -> Sig s (Singleton DeltaTime) Rect ()
 wiggleRect (Rect lu rd) = rectAtTime <$%> elapsed where
 	rectAtTime t = let dx = sin (realToFrac t * 5) * 15 in
 		Rect ((+ dx) `A.first` lu) ((+ dx) `A.first` rd)
+
+firstPoint :: React s (Mouse.Move :- Singleton Mouse.Down) Point
+firstPoint = let err = const "never occur" in
+	either error id . atResult err err <$> Mouse.position `at` leftClick
+
+completeRect :: Point -> Sig s (Mouse.Move :- Singleton Mouse.Up) Rect Rect
+completeRect p1 = (const $ error "never occur") `either` fst
+	<$> curRect p1 `until` leftUp
+
+defineRect :: Sig s (Mouse.Move :- Mouse.Down :- Singleton Mouse.Up) Rect Rect
+defineRect = adjustSig . completeRect =<< waitFor (adjust firstPoint)
+
+colorList :: Infinite BColor
+colorList = cycle $ fromList [Red .. Magenta]
+
+cycleColor :: Sig s (Singleton Mouse.Down) BColor ()
+cycleColor = go colorList where
+	go (h :~ t) = do
+		emit h
+		bool (pure ()) (go t)
+			=<< waitFor (middleClick `before` rightClick)
+
+chooseBoxColor :: Rect -> Sig s (Mouse.Down :- Singleton DeltaTime) Box ()
+chooseBoxColor r = Box <$%> adjustSig (wiggleRect r) <*%> adjustSig cycleColor
+
+posInside :: Rect -> Sig s evs Point y -> React s evs ()
+posInside rct = void . find (`inside` rct)
+	where inside (x, y) (Rect (l, u) (r, d)) =
+		(l <= x && x <= r || r <= x && x <= l) &&
+		(u <= y && y <= d || d <= y && y <= u)
+
+drClickOn :: Rect -> React s (Mouse.Move :- Mouse.Down :- Singleton TryWait) ()
+drClickOn rct = posInside rct
+	$ fst <$%> Mouse.position `indexBy` repeat doubler
+
+box :: Sig s
+	(Mouse.Move :- Mouse.Down :- Mouse.Up :- DeltaTime :- Singleton TryWait)
+	Box ()
+box = do
+	b <- (`Box` Red) <$%> adjustSig defineRect
+	adjustSig $ chooseBoxColor b
+	waitFor . adjust $ drClickOn b
+
+boxes :: Sig s
+	(Mouse.Move :- Mouse.Down :- Mouse.Up :- DeltaTime :- Singleton TryWait)
+	[Box] ()
+boxes = void $ parList (spawn box)
