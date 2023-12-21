460744276e97451ed820602f42404ba3346a196a
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2023-12-21 13:08:11 +0900

    2nd/zenn-use-moffy: Boxes: halfway

---
+++ tribial/zenn/moffies/2nd/zenn-use-moffy/src/Boxes.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE BlockArguments, LambdaCase #-}
+{-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Boxes where
+
+import Prelude hiding (until, cycle, repeat)
+import Control.Arrow qualified as A
+import Control.Monad
+import Control.Moffy
+import Control.Moffy.Event.Time
+import Control.Moffy.Samples.Event.Mouse qualified as Mouse
+import Control.Moffy.Samples.Boxes.Viewable
+import Data.Type.Set
+import Data.Type.Flip
+import Data.Or
+import Data.List.NonEmpty (fromList)
+import Data.List.Infinite (Infinite(..), cycle)
+import Data.Bool
+
+clickOn :: Mouse.Button -> React s (Singleton Mouse.Down) ()
+clickOn b = bool (clickOn b) (pure ()) . (== b) =<< Mouse.down
+
+leftClick, middleClick, rightClick :: React s (Singleton Mouse.Down) ()
+leftClick = clickOn Mouse.ButtonPrimary
+middleClick = clickOn Mouse.ButtonMiddle
+rightClick = clickOn Mouse.ButtonSecondary
+
+releaseOn :: Mouse.Button -> React s (Singleton Mouse.Up) ()
+releaseOn b = bool (releaseOn b) (pure ()) . (== b) =<< Mouse.up
+
+leftUp, middleUp, rightUp :: React s (Singleton Mouse.Up) ()
+leftUp = releaseOn Mouse.ButtonPrimary
+middleUp = releaseOn Mouse.ButtonMiddle
+rightUp = releaseOn Mouse.ButtonSecondary
+
+before :: Firstable es es' a b =>
+	React s es a -> React s es' b -> React s (es :+: es') Bool
+a `before` b = (<$> a `first` b) \case L _ -> True; _ -> False
+
+doubler :: React s (Mouse.Down :- Singleton TryWait) ()
+doubler = do
+	adjust rightClick
+	r <- rightClick `before` sleep 0.2
+	if r then pure () else doubler
+
+curRect :: Point -> Sig s (Singleton Mouse.Move) Rect ()
+curRect p1 = Rect p1 <$%> Mouse.position
+
+wiggleRect :: Rect -> Sig s (Singleton DeltaTime) Rect ()
+wiggleRect (Rect lu rd) = rectAtTime <$%> elapsed where
+	rectAtTime t = let dx = sin (realToFrac t * 5) * 15 in
+		Rect ((+ dx) `A.first` lu) ((+ dx) `A.first` rd)
