96d950f42061a59fd028372475ec5d0ab55da6e8
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-12-25 07:48:11 +0900

    try-gpu-vulkan: Gpu.Vulkan.Misc: remove not exported

--- themes/gui/vulkan/try-gpu-vulkan/src/Gpu/Vulkan/Misc.hs
+++ themes/gui/vulkan/try-gpu-vulkan/src/Gpu/Vulkan/Misc.hs
@@ -6,32 +6,8 @@
 
 module Gpu.Vulkan.Misc (nil') where
 
-import Foreign.Ptr
-import Foreign.Storable
-import Foreign.C.String
 import Data.Kind
-import Data.Default
-import Data.Text qualified as Txt
-import Data.Text.Foreign qualified as Txt
-import Data.IORef
-
 import Data.TypeLevel.ParMaybe qualified as TPMaybe
 
 nil' :: TPMaybe.M (t :: k -> Type) 'Nothing
 nil' = TPMaybe.N
-
-cstrToText :: CString -> IO Txt.Text
-cstrToText cs = Txt.peekCStringLen =<< cstringToCStringLen cs
-
-cstringLength :: CString -> IO Int
-cstringLength pc = do
-	c <- peek pc
-	case c of
-		0 -> pure 0
-		_ -> (+ 1) <$> cstringLength (pc `plusPtr` 1)
-
-cstringToCStringLen :: CString -> IO CStringLen
-cstringToCStringLen cs = (cs ,) <$> cstringLength cs
-
-newDefaultIORef :: Default a => IO (IORef a)
-newDefaultIORef = newIORef def
