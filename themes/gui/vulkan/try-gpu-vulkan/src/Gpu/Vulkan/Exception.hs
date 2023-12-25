7a21f95bcef5fe2c58a3848c08cc741d1273444e
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-12-25 17:32:20 +0900

    try-gpu-vulkan: reexport Gpu.Vulkan.Exception.Enum from Gpu.Vulkan.Exception

--- themes/gui/vulkan/try-gpu-vulkan/src/Gpu/Vulkan/Exception.hs
+++ themes/gui/vulkan/try-gpu-vulkan/src/Gpu/Vulkan/Exception.hs
@@ -1,7 +1,12 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Gpu.Vulkan.Exception (
-	MultiResult(..)
+
+	MultiResult(..),
+
+	module Gpu.Vulkan.Exception.Enum
+
 	) where
 
 import Gpu.Vulkan.Exception.Middle
+import Gpu.Vulkan.Exception.Enum
