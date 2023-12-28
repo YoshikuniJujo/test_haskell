920c57924434b2fea45cdc49db2da284bfed647b
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2023-12-28 10:29:22 +0900

    try-tools-yj: elemNotAll, errorIf and errorIfNot

--- packages/try-tools-yj/src/Data/Bool/ToolsYj.hs
+++ packages/try-tools-yj/src/Data/Bool/ToolsYj.hs
@@ -1,8 +1,14 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Data.Bool.ToolsYj (onlyIf) where
+module Data.Bool.ToolsYj (onlyIf, errorIf, errorIfNot) where
 
 import Data.Bool
 
 onlyIf :: (a -> Bool) -> a -> Maybe a
 onlyIf p x = bool Nothing (Just x) (p x)
+
+errorIf :: String -> Bool -> IO ()
+errorIf msg = bool (pure ()) (error msg)
+
+errorIfNot :: String -> Bool -> IO ()
+errorIfNot msg = bool (error msg) (pure ())
