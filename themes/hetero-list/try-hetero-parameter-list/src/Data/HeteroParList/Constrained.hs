1819c55c44e02d9d685cb2cb61219052c95b77a7
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2024-01-04 09:25:31 +0900

    try-hetero-parameter-list: Data.HeteroParList.Constrained: rename from (:***) to (:^*)

--- themes/hetero-list/try-hetero-parameter-list/src/Data/HeteroParList/Constrained.hs
+++ themes/hetero-list/try-hetero-parameter-list/src/Data/HeteroParList/Constrained.hs
@@ -10,10 +10,10 @@ import Data.Kind
 
 data PL c (t :: k -> Type) (ss :: [k]) where
 	Nil :: PL c t '[]
-	(:***) :: c s => t s -> PL c t ss -> PL c t (s ': ss)
+	(:^*) :: c s => t s -> PL c t ss -> PL c t (s ': ss)
 
 instance Show (PL c t '[]) where show Nil = "Nil"
 
 instance (Show (t s), Show (PL c t ss)) =>
 	Show (PL c t (s ': ss)) where
-	show (x :*** xs) = show x ++ " :*** " ++ show xs
+	show (x :^* xs) = show x ++ " :^* " ++ show xs
