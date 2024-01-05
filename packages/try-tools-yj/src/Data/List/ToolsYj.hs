960d1cacff5b0c817bd5cd7e935c1381967762a7
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2024-01-05 13:20:07 +0900

    try-tools-yj: Data.List.ToolsYj.findDefault

--- packages/try-tools-yj/src/Data/List/ToolsYj.hs
+++ packages/try-tools-yj/src/Data/List/ToolsYj.hs
@@ -1,11 +1,15 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Data.List.ToolsYj (elemAll, elemNotAll) where
+module Data.List.ToolsYj (elemAll, elemNotAll, findDefault) where
 
-import Data.List ((\\))
+import Data.Maybe
+import Data.List ((\\), find)
 
 elemAll :: Eq a => [a] -> [a] -> Bool
 elemAll es = null . (es \\)
 
 elemNotAll :: Eq a => [a] -> [a] -> Bool
 elemNotAll es = not . elemAll es
+
+findDefault :: a -> (a -> Bool) -> [a] -> a
+findDefault d p = fromMaybe d . find p
