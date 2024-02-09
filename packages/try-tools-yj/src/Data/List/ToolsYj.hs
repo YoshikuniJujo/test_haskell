41d1ba30a97bd1c3700a888f3e19edab1c45da34
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2024-02-09 10:54:28 +0900

    try-tools-yj: Data.List.ToolsYj: listToTuple4

--- packages/try-tools-yj/src/Data/List/ToolsYj.hs
+++ packages/try-tools-yj/src/Data/List/ToolsYj.hs
@@ -1,6 +1,6 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Data.List.ToolsYj (elemAll, elemNotAll, findDefault) where
+module Data.List.ToolsYj (elemAll, elemNotAll, findDefault, listToTuple4) where
 
 import Data.Maybe
 import Data.List ((\\), find)
@@ -13,3 +13,7 @@ elemNotAll es = not . elemAll es
 
 findDefault :: a -> (a -> Bool) -> [a] -> a
 findDefault d p = fromMaybe d . find p
+
+listToTuple4 :: [a] -> (a, a, a, a)
+listToTuple4 [r, g, b, a] = (r, g, b, a)
+listToTuple4 _ = error "The length of the list is not 4"
