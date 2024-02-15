9a228daac0a7107501c608f913b9549367482fa2
Yoshikuni Jujo <YoshikuniJujo@gmail.com>
2024-02-15 12:23:23 +0900

    try-cairo: app/tryST.hs

---
+++ themes/gui/cairo/try-cairo/app/tryST.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE BlockArguments, LambdaCase #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+import Control.Monad.Primitive
+import Control.Monad.ST
+import Data.CairoImage.Internal
+import Graphics.Cairo.Drawing.CairoT
+import Graphics.Cairo.Drawing.CairoPatternT
+import Graphics.Cairo.Drawing.Transformations
+import Graphics.Cairo.Surfaces.ImageSurfaces
+
+import Paths_try_cairo
+import Parts (checkPattern, readArgb32, writeArgb32)
+
+main :: IO ()
+main = do
+	logo <- CairoImageArgb32 <$>
+		(readArgb32 =<< getDataFileName "HaskellLogo.png")
+
+	putStrLn "*** TEST ARGB 32 BEGIN ***"
+
+	writeArgb32 "HaskellLogoRotated.png" $ runST $ draw logo
+
+	putStrLn "*** TEST ARGB 32 END ***"
+
+draw :: PrimMonad m => CairoImage -> m Argb32
+draw logo = do
+	sfc0 <- cairoImageSurfaceCreate CairoFormatArgb32 256 256
+	cr <- cairoCreate sfc0
+	checkPattern cr 256 256
+	sfc <- cairoImageSurfaceCreateForCairoImage logo
+	ptn <- cairoPatternCreateForSurface sfc
+	cairoTranslate cr 128 ((256 - sqrt 2 * 128) / 2)
+	cairoRotate cr (pi / 4)
+	cairoSetSource cr ptn
+	cairoPaint cr
+	(<$> cairoImageSurfaceGetCairoImage sfc0) \case
+		CairoImageArgb32 i -> i -- writeArgb32 "HaskellLogoRotated.png" i
+		_ -> error "image format error"
