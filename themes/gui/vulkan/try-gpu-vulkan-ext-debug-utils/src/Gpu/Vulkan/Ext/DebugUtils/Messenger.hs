46ec5c2d762a2a375afe2893f563ea52e914d9b6
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2024-04-22 10:41:52 +0900

    try-gpu-vulkan-ext-debug-utils: use new package

--- themes/gui/vulkan/try-gpu-vulkan-ext-debug-utils/src/Gpu/Vulkan/Ext/DebugUtils/Messenger.hs
+++ themes/gui/vulkan/try-gpu-vulkan-ext-debug-utils/src/Gpu/Vulkan/Ext/DebugUtils/Messenger.hs
@@ -19,7 +19,7 @@ import Data.TypeLevel.Maybe qualified as TMaybe
 import Data.TypeLevel.ParMaybe qualified as TPMaybe
 import Data.TypeLevel.Tuple.Uncurry
 
-import Gpu.Vulkan.PNextOld.Middle qualified as MI
+import Gpu.Vulkan.PNext.Middle qualified as MI
 
 import qualified Gpu.Vulkan.AllocationCallbacks.Internal as AllocationCallbacks
 import qualified Gpu.Vulkan.Instance.Internal as Instance
@@ -27,7 +27,7 @@ import qualified Gpu.Vulkan.Ext.DebugUtils.Messenger.Middle as M
 
 create :: (
 	WithPoked (TMaybe.M mn),
-	MI.FindPNextChainAll cb, Storable' ud,
+	MI.FindChainAll cb, Storable' ud,
 	AllocationCallbacks.ToMiddle mac ) =>
 	Instance.I si -> M.CreateInfo mn cb ud ->
 	TPMaybe.M (U2 AllocationCallbacks.A) mac -> IO a -> IO a
