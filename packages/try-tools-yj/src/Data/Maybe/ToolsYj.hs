a67210d61f784de79c8b88d339f31d97efd48b65
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2024-04-30 10:50:11 +0900

    try-tools-yj: Data.Maybe.ToolsYj: findMaybe

--- packages/try-tools-yj/src/Data/Maybe/ToolsYj.hs
+++ packages/try-tools-yj/src/Data/Maybe/ToolsYj.hs
@@ -1,12 +1,18 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE BlockArguments, LambdaCase #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Data.Maybe.ToolsYj (orErrorIO, findMaybeM) where
+module Data.Maybe.ToolsYj (orErrorIO, findMaybe, findMaybeM) where
 
 orErrorIO :: String -> Maybe a -> IO a
 orErrorIO msg = maybe (error msg) pure
 
-findMaybeM :: Monad  m => (a -> m (Maybe b)) -> [a] -> m (Maybe (a, b))
+findMaybe :: (a -> Maybe b) -> [a] -> Maybe (a, b)
+findMaybe prd = \case
+	[] -> Nothing
+	p : ps -> ($ prd p) \case
+		Nothing -> findMaybe prd ps; Just x -> Just (p, x)
+
+findMaybeM :: Monad m => (a -> m (Maybe b)) -> [a] -> m (Maybe (a, b))
 findMaybeM prd = \case
 	[] -> pure Nothing
 	p : ps -> prd p >>= \case
