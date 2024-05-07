36fb469c95f9e84d9491ada82e3c3cc117443eef
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2024-05-07 12:43:00 +0900

    remove try-gpu-vulkan-khr

--- themes/gui/vulkan/try-gpu-vulkan-khr/src/Gpu/Vulkan/Khr/Surface/Enum.hs
+++
@@ -1,6 +0,0 @@
-{-# LANGUAGE PackageImports #-}
-{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
-
-module Gpu.Vulkan.Khr.Surface.Enum (module Gpu.Vulkan.Khr.Surface.Enum) where
-
-import "try-gpu-vulkan-middle-khr-surface" Gpu.Vulkan.Khr.Surface.Enum
--- themes/gui/vulkan/try-gpu-vulkan-khr/src/Gpu/Vulkan/Khr/Surface/Type.hs
+++
@@ -1,7 +0,0 @@
-{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
-
-module Gpu.Vulkan.Khr.Surface.Type where
-
-import Gpu.Vulkan.Khr.Surface.Middle as M
-
-newtype S ss = S M.S deriving Show
