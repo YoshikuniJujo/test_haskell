73a7952dde03c4ed2c76d31d403b1bc560d9f948
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2024-05-07 11:17:59 +0900

    try-gpu-vulkan-middle-khr-surface

---
+++ themes/gui/vulkan/try-gpu-vulkan-middle-khr-surface/src/Gpu/Vulkan/Khr/Surface/Middle.hs
@@ -0,0 +1,13 @@
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Gpu.Vulkan.Khr.Surface.Middle (
+
+	-- * DESTROY
+
+	destroy, S,
+
+	-- * CAPABILITIES AND FORMAT
+
+	Capabilities(..), Format(..) ) where
+
+import Gpu.Vulkan.Khr.Surface.Middle.Internal
