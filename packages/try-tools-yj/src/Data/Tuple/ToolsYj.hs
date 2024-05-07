872c944500a9f77b47635ed71bc23c0ee820ba16
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2024-05-07 13:28:00 +0900

    try-tools-yj: Data.Tuple.ToolsYj: mapTup3

--- packages/try-tools-yj/src/Data/Tuple/ToolsYj.hs
+++ packages/try-tools-yj/src/Data/Tuple/ToolsYj.hs
@@ -1,6 +1,15 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Data.Tuple.ToolsYj (uncurryDup) where
+module Data.Tuple.ToolsYj (
+	uncurryDup,
+	appTup3, mapTup3
+	) where
 
 uncurryDup :: (a -> b -> c -> d) -> ((a, b), c) -> d
 uncurryDup = uncurry . uncurry
+
+appTup3 :: (a -> b) -> (c -> d) -> (e -> f) -> (a, c, e) -> (b, d, f)
+appTup3 f g h (x, y, z) = (f x, g y, h z)
+
+mapTup3 :: (a -> b) -> (a, a, a) -> (b, b, b)
+mapTup3 f = appTup3 f f f
