74acb67b52ffc4c14ad8acdf6a83ec53eec31e42
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2024-06-05 18:13:19 +0900

    try-a-parser-combinator: halfway

--- other_language/purescript/try-a-parser-combinator/src/Parser.purs
+++ other_language/purescript/try-a-parser-combinator/src/Parser.purs
@@ -1,5 +1,7 @@
 module Parser where
 
+import Control.Monad
+import Control.Apply
 import Data.Functor
 import Data.Function
 import Data.Tuple
@@ -12,6 +14,12 @@ runParser :: forall a b . Parser a b -> List a -> Maybe (Tuple b (List a))
 runParser (Parser g) = g
 
 instance Functor (Parser a) where
-        map g f = Parser \s -> case runParser f s of
-                Just (Tuple v s') -> Just $ Tuple (g v) s'
-                Nothing -> Nothing
+        map g f = Parser \s -> first g <$> f `runParser` s
+
+instance Apply (Parser a) where
+        apply fg f = Parser \s -> do
+                Tuple g s' <- fg `runParser` s
+                (g `first` _) <$> f `runParser` s'
+
+first :: forall a b c . (a -> b) -> Tuple a c  -> Tuple b c
+first f (Tuple x y) = Tuple (f x) y
