391d43525e70ba8674923c27dece4b079f0401dd
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2024-06-11 10:05:43 +0900

    try-ffi-by-example: Test: has field constraint

--- other_language/purescript/try-ffi-by-example/src/Test.purs
+++ other_language/purescript/try-ffi-by-example/src/Test.purs
@@ -8,3 +8,9 @@ gcd' n 0 = n
 gcd' n m
         | n > m = gcd' (n - m) m
         | otherwise = gcd' (m - n) n
+
+foo :: { bar :: String } -> String
+foo { bar : b } = b
+
+hoge :: forall r . { piyo :: String | r } -> String
+hoge { piyo : p } = p
