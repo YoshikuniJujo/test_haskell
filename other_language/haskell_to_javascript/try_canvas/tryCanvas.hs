80f2fc959a372a6071b860c050e1828ef41dcd0e
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2024-06-18 10:12:35 +0900

    haskel_to_javascript: tryCanvas.hs

--- other_language/haskell_to_javascript/try_canvas/tryCanvas.hs
+++ other_language/haskell_to_javascript/try_canvas/tryCanvas.hs
@@ -1,30 +1,95 @@
+{-# LANGUAGE BlockArguments #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Main where
 
 import GHC.JS.Prim
-
-foreign import javascript "((id) => { return document.getElementById(id); })"
-	js_getElementById :: JSVal -> IO (JSVal)
-
-foreign import javascript "((e, t) => { e.textContent = t; })"
-	js_setTextContent :: JSVal -> JSVal -> IO ()
-
-foreign import javascript "((c) => { return c.getContext('2d'); })"
-	js_getContext2d :: JSVal -> IO (JSVal)
-
-foreign import javascript "((c) => { c.fillStyle = 'rgb(200, 0, 0)'; c.fillRect(10, 10, 50, 50); })"
-	js_tryRectangle :: JSVal -> IO ()
-
-foreign import javascript "((c) => { c.fillStyle = 'rgba(0, 0, 200, 0.5)'; c.fillRect(30, 30, 50, 50); })"
-	js_tryRectangle2 :: JSVal -> IO ()
+import Data.Word
 
 main :: IO ()
 main = do
 	foo <- js_getElementById (toJSString "foo")
 	js_setTextContent foo (toJSString "bar")
-	canvas <- js_getElementById (toJSString "canvas")
-	ctx <- js_getContext2d canvas
-	js_tryRectangle ctx
-	js_tryRectangle2 ctx
+	Just canvas <- getCanvasById "canvas"
+	ctx <- getContext2D canvas
+	setFillStyle ctx $ Rgb 200 0 0
+	fillRect ctx
+		$ Rectangle { left = 10, top = 10, width = 50, height = 50 }
+	setFillStyle ctx $ Rgba 0 0 200 0.5
+	fillRect ctx
+		$ Rectangle { left = 20, top = 20, width = 50, height = 50 }
+	setFillStyle ctx $ Rgb 0 0 0
+	fillRect ctx
+		$ Rectangle { left = 125, top = 25, width = 100, height = 100 }
+	clearRect ctx
+		$ Rectangle { left = 145, top = 45, width = 60, height = 60 }
+	strokeRect ctx
+		$ Rectangle { left = 150, top = 50, width = 50, height = 50 }
 	putStrLn "tryCanvas"
+
+getCanvasById :: String -> IO (Maybe Canvas)
+getCanvasById i = do
+	e <- js_getElementById $ toJSString i
+	pure if js_isCanvas e then Just $ Canvas e else Nothing
+
+data Canvas = Canvas JSVal
+
+foreign import javascript "((id) => { return document.getElementById(id); })"
+	js_getElementById :: JSVal -> IO (JSVal)
+
+foreign import javascript "((e) => { return e.getContext; })"
+	js_isCanvas :: JSVal -> Bool
+
+getContext2D :: Canvas -> IO Context2D
+getContext2D (Canvas c) = Context2D <$> js_getContext2d c
+
+data Context2D = Context2D JSVal
+
+foreign import javascript "((c) => { return c.getContext('2d'); })"
+	js_getContext2d :: JSVal -> IO (JSVal)
+
+setFillStyle :: Context2D -> Color -> IO ()
+setFillStyle (Context2D ctx) = js_setFillStyle ctx . colorToJSVal
+
+foreign import javascript "((ctx, clr) => { ctx.fillStyle = clr; })"
+	js_setFillStyle :: JSVal -> JSVal -> IO ()
+
+fillRect, strokeRect, clearRect :: Context2D -> Rectangle -> IO ()
+fillRect = rectFromJs js_fillRect
+strokeRect = rectFromJs js_strokeRect
+clearRect = rectFromJs js_clearRect
+
+rectFromJs ::
+	(JSVal -> Int -> Int -> Int -> Int -> IO ()) ->
+	Context2D -> Rectangle -> IO ()
+rectFromJs a (Context2D ctx)
+	Rectangle { left = l, top = t, width = w, height = h } =
+	a ctx l t w h
+
+foreign import javascript "((ctx, l, t, w, h) => { ctx.fillRect(l, t, w, h); })"
+	js_fillRect :: JSVal -> Int -> Int -> Int -> Int -> IO ()
+
+foreign import javascript "((ctx, l, t, w, h) => { ctx.strokeRect(l, t, w, h); })"
+	js_strokeRect :: JSVal -> Int -> Int -> Int -> Int -> IO ()
+
+foreign import javascript "((ctx, l, t, w, h) => { ctx.clearRect(l, t, w, h); })"
+	js_clearRect :: JSVal -> Int -> Int -> Int -> Int -> IO ()
+
+foreign import javascript "((e, t) => { e.textContent = t; })"
+	js_setTextContent :: JSVal -> JSVal -> IO ()
+
+data Color = Rgb Word8 Word8 Word8 | Rgba Word8 Word8 Word8 Double deriving Show
+
+colorToJSVal :: Color -> JSVal
+colorToJSVal = toJSString . colorToString
+
+colorToString :: Color -> String
+colorToString (Rgb r g b) =
+	"rgb(" ++ show r ++ ", " ++ show g ++ ", "  ++ show b ++ ")"
+colorToString (Rgba r g b a) =
+	"rgba(" ++ show r ++ ", " ++ show g ++ ", " ++ show b ++ ", " ++
+	show a ++ ")"
+
+data Rectangle =
+	Rectangle { left :: Int, top :: Int, width :: Int, height :: Int }
+	deriving Show
