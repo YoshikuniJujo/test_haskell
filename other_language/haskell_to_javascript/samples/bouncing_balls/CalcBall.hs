451a04d40d20d3393bd6dc4fb632d4e22c4dbf33
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2024-07-30 11:33:50 +0900

    haskell_to_javascript: bouncing_balls: CalcBall: refactoring

--- other_language/haskell_to_javascript/samples/bouncing_balls/CalcBall.hs
+++ other_language/haskell_to_javascript/samples/bouncing_balls/CalcBall.hs
@@ -1,30 +1,32 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module CalcBall where
+module CalcBall (ballPos) where
+
+import Control.Arrow
 
 timeToHeight :: Double
 timeToHeight = - 5 * 10 ** (- 4)
-
-heightToTime :: Double -> Double
-heightToTime h = sqrt $ h / (- timeToHeight)
 
 bounce :: Double
 bounce = 0.81
 
 ballPos :: Double -> Double -> (Double, Double) -> (Double, Maybe Double)
-ballPos t t0 (x, y) = (x, ballHeight t t0 y)
+ballPos t = second . getHeight . (t -)
 
-ballHeight :: Double -> Double -> Double -> Maybe Double
-ballHeight t t0 h = getHeight (t - t0) h
+getHeight :: Double -> Double -> Maybe Double
+getHeight t h
+	| t > 1000 && h < 1 = Nothing | h < 1 = Just 0
+	| t < heightToTime2 h = Just $ height t h
+	| otherwise = getHeight (t - heightToTime2 h) (bounce * h)
 
 height :: Double -> Double -> Double
 height t h
 	| t < heightToTime h = timeToHeight * t ^ (2 :: Int) + h
-	| otherwise = timeToHeight * (t - (1 + sqrt bounce) * heightToTime h) ^ (2 :: Int) + bounce * h
+	| otherwise =
+		timeToHeight * (t - heightToTime2 h) ^ (2 :: Int) + bounce * h
 
-getHeight :: Double -> Double -> Maybe Double
-getHeight t h
-	| t > 1000 && h < 1 = Nothing
-	| h < 1 = Just 0
-	| t < (1 + sqrt bounce) * heightToTime h = Just $ height t h
-	| otherwise = getHeight (t - (1 + sqrt bounce) * heightToTime h) (bounce * h)
+heightToTime :: Double -> Double
+heightToTime = sqrt . (/ (- timeToHeight))
+
+heightToTime2 :: Double -> Double
+heightToTime2 = ((1 + sqrt bounce) *) . heightToTime
