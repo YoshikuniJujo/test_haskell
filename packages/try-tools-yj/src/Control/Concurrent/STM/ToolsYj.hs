6ac60e48cf6056662ec7bf05ce49d98e38b72266
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2024-08-28 13:35:59 +0900

    try-tools-yj: Control.Concurrent.STM.ToolsYj: readModifyTVar

--- packages/try-tools-yj/src/Control/Concurrent/STM/ToolsYj.hs
+++ packages/try-tools-yj/src/Control/Concurrent/STM/ToolsYj.hs
@@ -1,6 +1,6 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Control.Concurrent.STM.ToolsYj (newDefaultTVar, checkFlag) where
+module Control.Concurrent.STM.ToolsYj (newDefaultTVar, readModifyTVar, checkFlag) where
 
 import Control.Concurrent.STM
 import Data.Default
@@ -8,6 +8,9 @@ import Data.Bool
 
 newDefaultTVar :: Default a => STM (TVar a)
 newDefaultTVar = newTVar def
+
+readModifyTVar :: TVar a -> (a -> a) -> STM a
+readModifyTVar v f = readTVar v <* modifyTVar v f
 
 checkFlag :: TVar Bool -> STM Bool
 checkFlag flg = readTVar flg >>= bool (pure False) (True <$ writeTVar flg False)
