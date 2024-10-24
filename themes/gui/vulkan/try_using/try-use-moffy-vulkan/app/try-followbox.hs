79fbb7c453f7526225c834d0ad529f4c975b9811
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2024-10-24 11:55:04 +0900

    try-use-moffy-vulkan: app/try-followbox.hs: refactoring: halfway

--- themes/gui/vulkan/try_using/try-use-moffy-vulkan/app/try-followbox.hs
+++ themes/gui/vulkan/try_using/try-use-moffy-vulkan/app/try-followbox.hs
@@ -15,18 +15,15 @@ import Control.Monad
 import Control.Moffy
 import Control.Moffy.Event.CalcTextExtents qualified as CTE
 
-import UseCairo
+import UseCairo qualified as Vk
 import KeyToXKey
 
 import Control.Monad.Fix
-import Control.Monad.Trans
 import Control.Concurrent
 import Control.Concurrent.STM hiding (retry)
 import Data.Default
 import Data.List.Length
 import Data.Map qualified as M
-import Data.Bool
-import Options.Declarative
 import Gpu.Vulkan.Cglm qualified as Cglm
 import Gpu.Vulkan qualified as Vk
 import Graphics.UI.GlfwG.Mouse qualified as GlfwG.Ms
@@ -52,252 +49,190 @@ import Data.Type.Flip
 
 import Control.Moffy.Event.CalcTextExtents (CalcTextExtents(..))
 
-main :: IO ()
-main = run_ action
+----------------------------------------------------------------------
+--
+-- * MAIN
+-- * SEND REQUEST/EVENT TO VULKAN/MOFFY
+-- * RUN FOLLOWBOX
+-- * RECTANGLES
+--
+----------------------------------------------------------------------
 
-action :: Flag "f" '["flat"] "BOOL" "flat or not" Bool ->
-	Cmd "Draw Rectangles" ()
-action f = liftIO do
-	(cinp, cout) <- atomically $ (,) <$> newTChan <*> newTChan
-	vext <- atomically $ newTVar M.empty
-	let	inp = writeTChan cinp
-		oute = isEmptyTChan cout
-		outp = readTChan cout
-		ext = readTVarOr (Vk.Extent2d 0 0) vext
-	cow <- atomically newTChan
-	cocc <- atomically newTChan
+-- MAIN
+
+main :: IO ()
+main = do
+	(ccmd@(writeTChan -> cmd), cev@(readTChan -> ev), ex) <- atomically
+		$ (,,) <$> newTChan <*> newTChan <*> newTVar M.empty
+	let	ext = lookupOr (Vk.Extent2d 0 0) ex
+	ff $ threadDelay 20000 >> atomically (cmd Vk.GetEvent)
+	(crqs@(readTChan -> rqs), cocc@(writeTChan -> occ))
+		<- atomically $ (,) <$> newTChan <*> newTChan
+	ffa $ mffReqsToVk cmd cocc =<< rqs
+
 	c' <- atomically newTChan
 	e <- atomically newTChan
 	v <- atomically . newTVar $ View []
-	_ <- forkIO $ untilEnd e cow cocc c' ((inp, (oute, outp)), ext) v
-	_ <- forkIO $ runFollowboxGen cow cocc "firefox" Nothing c' do
+
+	_ <- forkIO $ untilEnd e crqs cocc c' ((cmd, ev), ext) v
+	_ <- forkIO $ runFollowboxGen crqs cocc "firefox" Nothing c' do
 		i <- waitFor $ adjust windowNew
 		_ <- waitFor . adjust $ setCursorFromName i Default
 		waitFor . adjust $ storeDefaultWindow i
 		M.singleton i <$%> adjustSig (followbox i)
 		waitFor . adjust $ windowDestroy i
-	rectangles cinp cout vext
-	atomically $ readTChan e
+	Vk.rectangles ccmd cev ex
+	where
+	f = void . forkIO . void
+	ff = void . forkIO . forever; ffa = ff . atomically
 
-readTVarOr :: Ord k => a -> TVar (M.Map k (TVar a)) -> k -> STM a
-readTVarOr d mp k = do
-	mv <- (M.lookup k) <$> readTVar mp
-	case mv of
-		Nothing -> pure d
-		Just v -> readTVar v
+lookupOr :: Ord k => a -> TVar (M.Map k (TVar a)) -> k -> STM a
+lookupOr d mp k = maybe (pure d) readTVar . M.lookup k =<< readTVar mp
 
-processEvReqs :: (Command Int -> STM ()) -> TChan (EvOccs GuiEv) -> EvReqs GuiEv -> IO ()
-processEvReqs inp cocc rqs = do
+-- SEND REQUEST/EVENT TO VULKAN/MOFFY
+
+mffReqsToVk :: (Vk.Command Int -> STM ()) -> TChan (EvOccs GuiEv) -> EvReqs GuiEv -> STM ()
+mffReqsToVk inp cocc rqs = do
 	case project rqs of
 		Nothing -> pure ()
-		Just WindowNewReq -> atomically do
-			inp OpenWindow
+		Just WindowNewReq -> do
+			inp Vk.OpenWindow
 	case project rqs of
 		Nothing -> pure ()
 		Just (WindowDestroyReq i@(WindowId k)) -> do
-			putStrLn $ "window destroy: " ++ show i
-			atomically do
-				inp . DestroyWindow $ fromIntegral k
+			do
+				inp . Vk.DestroyWindow $ fromIntegral k
 				writeTChan cocc . App.expand . App.Singleton $ OccWindowDestroy i
 	case project rqs of
 		Nothing -> pure ()
-		Just (CalcTextExtentsReq wid fnm fsz txt) -> do
-			putStr "CalcTextExtentsReq: "
-			print (wid, fnm, fsz, txt)
+		Just (CalcTextExtentsReq wid fnm fsz txt) -> pure ()
 	case project rqs of
 		Nothing -> pure ()
 		Just (SetCursorFromNameReq wid nc) -> do
-			putStrLn $ "SetCursorFromNameReq: " ++
-				show wid ++ " " ++ show nc
-			atomically do
+			do
 				writeTChan cocc . App.expand . App.Singleton
 					$ OccSetCursorFromName wid nc Success
 	case project rqs of
 		Nothing -> pure ()
-		Just (SetCursorFromPngReq _ _) -> do
-			putStrLn "SetCursorFromPngReq"
-	case project rqs of
-		Nothing -> pure ()
-		Just r@(CTE.CalcTextExtentsReq _ _ _ _) -> do
-			putStrLn "TextLayoutExtentReq"
-			atomically . inp $ CalcTextLayoutExtent r
-	case project rqs of
-		Nothing -> pure ()
-		Just WindowConfigureReq -> putStrLn "WindowConfigureReq"
-	case project rqs of
-		Nothing -> pure ()
-		Just (r :: NewLockId) -> putStrLn $ "NewLockIdReq: " ++ show r
+		Just r@(CTE.CalcTextExtentsReq _ _ _ _) ->
+			inp $ Vk.CalcTextLayoutExtent r
 
 untilEnd :: TChan () -> TChan (EvReqs GuiEv) -> TChan (EvOccs GuiEv) ->
 	TChan (M.Map WindowId View) ->
-	((Command Int -> STM (), (STM Bool, STM (Event Int))), Int -> STM Vk.Extent2d) ->
+	((Vk.Command Int -> STM (), STM (Vk.Event Int)), Int -> STM Vk.Extent2d) ->
 	TVar View -> IO ()
-untilEnd e cow cocc c' ((inp, (oute, outp)), ext) tvw = do
-	_ <- forkIO $ forever do
-		threadDelay 20000
-		atomically $ inp GetEvent
+untilEnd e cow cocc c' ((inp, outp), ext) tvw = do
 
 	_ <- forkIO . forever $ atomically (readTChan c') >>= \vs -> do
 		putStrLn $ "VIEW: " ++ show vs
 		atomically . writeTVar tvw $ vs M.! WindowId 0
 		e0 <- atomically $ ext 0
-		atomically . inp $ SetPicture (vs M.! WindowId 0)
-		atomically . inp $ Draw (M.fromList [(0, (def, instances' 1024 1024 e0))])
+		atomically . inp $ Vk.SetPicture (vs M.! WindowId 0)
+		atomically . inp $ Vk.Draw (M.fromList [(0, (def, rects 1024 1024 e0))])
 
-	_ <- forkIO $ forever do
+	fix \loop -> do
 		threadDelay 500
-		ow <- atomically $ tryReadTChan cow
-		maybe (pure ()) (processEvReqs inp cocc) ow
-
-	($ instances) $ fix \loop rs -> do
-		threadDelay 500
-		o <- atomically do
-			bool (Just <$> outp) (pure Nothing) =<< oute
+		o <- atomically do Just <$> outp
+--			bool (Just <$> outp) (pure Nothing) =<< oute
 		case o of
-			Nothing -> loop rs
-			Just EventEnd -> putStrLn "THE WORLD ENDS" >> atomically (writeTChan e ())
-			Just (EventKeyDown w ky) -> do
+			Nothing -> loop
+			Just Vk.EventEnd -> putStrLn "THE WORLD ENDS" >> atomically (writeTChan e ())
+			Just (Vk.EventKeyDown w ky) -> do
 				putStrLn $ "KEY DOWN: " ++ show w ++ " " ++ show ky
 				atomically . writeTChan cocc
 					. App.expand . App.Singleton
 					. OccKeyDown (WindowId $ fromIntegral w) $ keyToXKey ky
-				loop rs
-			Just (EventKeyUp w ky) -> do
+				loop
+			Just (Vk.EventKeyUp w ky) -> do
 				putStrLn $ "KEY UP  : " ++ show w ++ " " ++ show ky
-				loop rs
-			Just (EventMouseButtonDown w GlfwG.Ms.MouseButton'1) -> do
+				loop
+			Just (Vk.EventMouseButtonDown w GlfwG.Ms.MouseButton'1) -> do
 				putStrLn "BUTTON LEFT DOWN"
 				atomically . writeTChan cocc
 					. App.expand . App.Singleton
 					$ OccMouseDown (WindowId $ fromIntegral w) ButtonLeft
-				loop instances
-			Just (EventMouseButtonDown w GlfwG.Ms.MouseButton'2) -> do
+				loop
+			Just (Vk.EventMouseButtonDown w GlfwG.Ms.MouseButton'2) -> do
 				putStrLn "BUTTON RIGHT DOWN"
 				atomically . writeTChan cocc
 					. App.expand . App.Singleton
 					$ OccMouseDown (WindowId $ fromIntegral w) ButtonRight
-				loop instances2
-			Just (EventMouseButtonDown w GlfwG.Ms.MouseButton'3) -> do
+				loop
+			Just (Vk.EventMouseButtonDown w GlfwG.Ms.MouseButton'3) -> do
 				atomically . writeTChan cocc
 					. App.expand . App.Singleton
 					$ OccMouseDown (WindowId $ fromIntegral w) ButtonMiddle
-				loop rs
-			Just (EventMouseButtonUp w GlfwG.Ms.MouseButton'1) -> do
+				loop
+			Just (Vk.EventMouseButtonUp w GlfwG.Ms.MouseButton'1) -> do
 				atomically . writeTChan cocc
 					. App.expand . App.Singleton
 					$ OccMouseUp (WindowId $ fromIntegral w) ButtonLeft
-				loop rs
-			Just (EventMouseButtonUp w GlfwG.Ms.MouseButton'2) -> do
+				loop
+			Just (Vk.EventMouseButtonUp w GlfwG.Ms.MouseButton'2) -> do
 				atomically . writeTChan cocc
 					. App.expand . App.Singleton
 					$ OccMouseUp (WindowId $ fromIntegral w) ButtonRight
-				loop rs
-			Just (EventMouseButtonUp w GlfwG.Ms.MouseButton'3) -> do
+				loop
+			Just (Vk.EventMouseButtonUp w GlfwG.Ms.MouseButton'3) -> do
 				atomically . writeTChan cocc
 					. App.expand . App.Singleton
 					$ OccMouseUp (WindowId $ fromIntegral w) ButtonMiddle
-				loop rs
-			Just (EventMouseButtonDown _ _) -> loop rs
-			Just (EventMouseButtonUp _ _) -> loop rs
-			Just (EventCursorPosition k x y) -> do
+				loop
+			Just (Vk.EventMouseButtonDown _ _) -> loop
+			Just (Vk.EventMouseButtonUp _ _) -> loop
+			Just (Vk.EventCursorPosition k x y) -> do
 				atomically . writeTChan cocc
 					. App.expand . App.Singleton
 					$ OccMouseMove (WindowId $ fromIntegral k) (x, y)
-				loop rs
-			Just (EventOpenWindow k) -> do
+				loop
+			Just (Vk.EventOpenWindow k) -> do
 				putStrLn $ "open window: " ++ show k
 				atomically . writeTChan cocc
 					. App.expand . App.Singleton
 					. OccWindowNew . WindowId $ fromIntegral k
-				loop rs
-			Just (EventDeleteWindow k) -> do
+				loop
+			Just (Vk.EventDeleteWindow k) -> do
 				putStrLn $ "delete window: " ++ show k
 				atomically . writeTChan cocc
 					. App.expand . App.Singleton
 					. OccDeleteEvent . WindowId $ fromIntegral k
-				loop rs
-			Just (EventTextLayoutExtentResult ex) -> do
+				loop
+			Just (Vk.EventTextLayoutExtentResult ex) -> do
 				putStrLn $ "EventTextLayoutExtentResult: " ++ show ex
 				atomically . writeTChan cocc
 					. App.expand $ App.Singleton ex
-				loop rs
-			Just EventNeedRedraw -> do
+				loop
+			Just Vk.EventNeedRedraw -> do
 				putStrLn "EVENT NEED REDRAW"
 				e0 <- atomically $ ext 0
-				atomically . inp $ Draw (M.fromList [(0, (def, instances' 1024 1024 e0))])
-				loop rs
+				atomically . inp $ Vk.Draw (M.fromList [(0, (def, rects 1024 1024 e0))])
+				loop
 
-instances :: Float -> [Rectangle]
-instances tm = let m = calcModel tm in
+-- RECTANGLES
+
+rects :: Float -> Float -> Vk.Extent2d -> [Vk.Rectangle]
+rects w h ex = let m = model w h ex in
 	[
-		Rectangle (RectPos . Cglm.Vec2 $ (- 1) :. (- 1) :. NilL)
-			(RectSize . Cglm.Vec2 $ 0.3 :. 0.3 :. NilL)
-			(RectColor . Cglm.Vec4 $ 1.0 :. 0.0 :. 0.0 :. 1.0 :. NilL)
+		Vk.Rectangle (Vk.RectPos . Cglm.Vec2 $ (- 2) :. (- 2) :. NilL)
+			(Vk.RectSize . Cglm.Vec2 $ 4 :. 4 :. NilL)
+			(Vk.RectColor . Cglm.Vec4 $ 1.0 :. 0.0 :. 0.0 :. 1.0 :. NilL)
 			m,
-		Rectangle (RectPos . Cglm.Vec2 $ 1 :. 1 :. NilL)
-			(RectSize . Cglm.Vec2 $ 0.2 :. 0.2 :. NilL)
-			(RectColor . Cglm.Vec4 $ 0.0 :. 1.0 :. 0.0 :. 1.0 :. NilL)
+		Vk.Rectangle (Vk.RectPos . Cglm.Vec2 $ 1 :. 1 :. NilL)
+			(Vk.RectSize . Cglm.Vec2 $ 0.2 :. 0.2 :. NilL)
+			(Vk.RectColor . Cglm.Vec4 $ 0.0 :. 1.0 :. 0.0 :. 1.0 :. NilL)
 			m,
-		Rectangle (RectPos . Cglm.Vec2 $ 1.5 :. (- 1.5) :. NilL)
-			(RectSize . Cglm.Vec2 $ 0.3 :. 0.6 :. NilL)
-			(RectColor . Cglm.Vec4 $ 0.0 :. 0.0 :. 1.0 :. 1.0 :. NilL)
+		Vk.Rectangle (Vk.RectPos . Cglm.Vec2 $ 1.5 :. (- 1.5) :. NilL)
+			(Vk.RectSize . Cglm.Vec2 $ 0.3 :. 0.6 :. NilL)
+			(Vk.RectColor . Cglm.Vec4 $ 0.0 :. 0.0 :. 1.0 :. 1.0 :. NilL)
 			m,
-		Rectangle (RectPos . Cglm.Vec2 $ (- 1.5) :. 1.5 :. NilL)
-			(RectSize . Cglm.Vec2 $ 0.6 :. 0.3 :. NilL)
-			(RectColor . Cglm.Vec4 $ 1.0 :. 1.0 :. 1.0 :. 1.0 :. NilL)
-			m
-		]
-
-instances2 :: Float -> [Rectangle]
-instances2 tm = let m = calcModel tm in
-	[
-		Rectangle (RectPos . Cglm.Vec2 $ (- 1) :. (- 1) :. NilL)
-			(RectSize . Cglm.Vec2 $ 0.3 :. 0.3 :. NilL)
-			(RectColor . Cglm.Vec4 $ 0.0 :. 1.0 :. 0.0 :. 1.0 :. NilL)
-			m,
-		Rectangle (RectPos . Cglm.Vec2 $ 1 :. 1 :. NilL)
-			(RectSize . Cglm.Vec2 $ 0.6 :. 0.6 :. NilL)
-			(RectColor . Cglm.Vec4 $ 0.0 :. 0.0 :. 1.0 :. 1.0 :. NilL)
-			m,
-		Rectangle (RectPos . Cglm.Vec2 $ 1.5 :. (- 1.5) :. NilL)
-			(RectSize . Cglm.Vec2 $ 0.6 :. 0.3 :. NilL)
-			(RectColor . Cglm.Vec4 $ 1.0 :. 1.0 :. 1.0 :. 1.0 :. NilL)
-			m,
-		Rectangle (RectPos . Cglm.Vec2 $ (- 1.5) :. 1.5 :. NilL)
-			(RectSize . Cglm.Vec2 $ 0.6 :. 0.3 :. NilL)
-			(RectColor . Cglm.Vec4 $ 1.0 :. 0.0 :. 0.0 :. 1.0 :. NilL)
-			m
-		]
-
-calcModel :: Float -> RectModel
-calcModel tm = RectModel $ Cglm.rotate Cglm.mat4Identity
-		(tm * Cglm.rad 90) (Cglm.Vec3 $ 0 :. 0 :. 1 :. NilL)
-
-instances' :: Float -> Float -> Vk.Extent2d -> [Rectangle]
-instances' w h ex = let m = calcModel2 w h ex in
-	[
---		Rectangle (RectPos . Cglm.Vec2 $ (- 1) :. (- 1) :. NilL)
-		Rectangle (RectPos . Cglm.Vec2 $ (- 2) :. (- 2) :. NilL)
---			(RectSize . Cglm.Vec2 $ 0.3 :. 0.3 :. NilL)
-			(RectSize . Cglm.Vec2 $ 4 :. 4 :. NilL)
-			(RectColor . Cglm.Vec4 $ 1.0 :. 0.0 :. 0.0 :. 1.0 :. NilL)
-			m,
-		Rectangle (RectPos . Cglm.Vec2 $ 1 :. 1 :. NilL)
-			(RectSize . Cglm.Vec2 $ 0.2 :. 0.2 :. NilL)
-			(RectColor . Cglm.Vec4 $ 0.0 :. 1.0 :. 0.0 :. 1.0 :. NilL)
-			m,
-		Rectangle (RectPos . Cglm.Vec2 $ 1.5 :. (- 1.5) :. NilL)
-			(RectSize . Cglm.Vec2 $ 0.3 :. 0.6 :. NilL)
-			(RectColor . Cglm.Vec4 $ 0.0 :. 0.0 :. 1.0 :. 1.0 :. NilL)
-			m,
-		Rectangle (RectPos . Cglm.Vec2 $ (- 1.5) :. 1.5 :. NilL)
-			(RectSize . Cglm.Vec2 $ 0.6 :. 0.3 :. NilL)
-			(RectColor . Cglm.Vec4 $ 1.0 :. 1.0 :. 1.0 :. 1.0 :. NilL)
+		Vk.Rectangle (Vk.RectPos . Cglm.Vec2 $ (- 1.5) :. 1.5 :. NilL)
+			(Vk.RectSize . Cglm.Vec2 $ 0.6 :. 0.3 :. NilL)
+			(Vk.RectColor . Cglm.Vec4 $ 1.0 :. 1.0 :. 1.0 :. 1.0 :. NilL)
 			m
 		]
 
 
-calcModel2 :: Float -> Float -> Vk.Extent2d -> RectModel
-calcModel2 w0 h0 Vk.Extent2d { Vk.extent2dWidth = w, Vk.extent2dHeight = h } =
-	RectModel $ Cglm.scale Cglm.mat4Identity
+model :: Float -> Float -> Vk.Extent2d -> Vk.RectModel
+model w0 h0 Vk.Extent2d { Vk.extent2dWidth = w, Vk.extent2dHeight = h } =
+	Vk.RectModel $ Cglm.scale Cglm.mat4Identity
 		(Cglm.Vec3 $ (w0 / fromIntegral w) :. (h0 / fromIntegral h) :. 1 :. NilL)
