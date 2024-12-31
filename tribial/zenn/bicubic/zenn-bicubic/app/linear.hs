27916ac60ff7c3282aef6ff089b8424082e25e95
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2024-12-31 21:08:07 +0900

    zenn-bicubic: app/linear.hs

--- tribial/zenn/bicubic/zenn-bicubic/app/linear.hs
+++ tribial/zenn/bicubic/zenn-bicubic/app/linear.hs
@@ -41,6 +41,8 @@ radius = 5
 radius' :: CDouble
 radius' = 3
 
+ceiling' = (+ 1) . floor
+
 main :: IO ()
 main = do
 	s <- cairoImageSurfaceCreate CairoFormatArgb32
@@ -51,16 +53,22 @@ main = do
 
 	for_ [1, 1 + 1 / 3 .. 3] \y ->
 		for_ [1, 1 + 1 / 3 .. 3] \x -> do
-			let c = colorsA ! (round y) ! (round x)
+			let	c00 = colorsA ! floor y ! floor x
+				c10 = colorsA ! floor y ! ceiling' x
+				c01 = colorsA ! ceiling' y ! floor x
+				c11 = colorsA ! ceiling' y ! ceiling' x
+				c0 = (c00 `mul` (fromIntegral (ceiling' x) - x)) `add` (c10 `mul` (x - fromIntegral (floor x)))
+				c1 = (c01 `mul` (fromIntegral (ceiling' x) - x)) `add` (c11 `mul` (x - fromIntegral (floor x)))
+				c = (c0 `mul` (fromIntegral (ceiling' y) - y)) `add` (c1 `mul` (y - fromIntegral (floor y)))
 			cairoSetSourceRgb cr c
 			cairoRectangle cr
 				(leftMargin + 3 * unit * (x - 1 / 6))
-				(leftMargin + 3 * unit * (y - 1 / 6))
+				(topMargin + 3 * unit * (y - 1 / 6))
 				(3 * unit * (1 / 3))
 				(3 * unit * (1 / 3))
 			cairoFill cr
 
-	cairoSet cr $ LineWidth 2
+	cairoSet cr $ LineWidth 1
 	cairoSetSourceRgb cr . fromJust $ rgbDouble 0.6 0.6 0.6
 
 	for_ [0 .. number] \y -> do
@@ -75,7 +83,13 @@ main = do
 
 	for_ [1, 1 + 1 / 3 .. 3] \y ->
 		for_ [1, 1 + 1 / 3 .. 3] \x -> do
-			let c = colorsA ! (round y) ! (round x)
+			let	c00 = colorsA ! floor y ! floor x
+				c10 = colorsA ! floor y ! ceiling' x
+				c01 = colorsA ! ceiling' y ! floor x
+				c11 = colorsA ! ceiling' y ! ceiling' x
+				c0 = (c00 `mul` (fromIntegral (ceiling' x) - x)) `add` (c10 `mul` (x - fromIntegral (floor x)))
+				c1 = (c01 `mul` (fromIntegral (ceiling' x) - x)) `add` (c11 `mul` (x - fromIntegral (floor x)))
+				c = (c0 `mul` (fromIntegral (ceiling' y) - y)) `add` (c1 `mul` (y - fromIntegral (floor y)))
 			cairoMoveTo cr
 				(leftMargin + 3 * unit * x + radius')
 				(topMargin + 3 * unit * y)
@@ -116,7 +130,7 @@ main = do
 -}
 
 	cairoImageSurfaceGetCairoImage s >>= \case
-		CairoImageArgb32 a -> writePng "nearest.png" $ cairoArgb32ToJuicyRGBA8 a
+		CairoImageArgb32 a -> writePng "linear.png" $ cairoArgb32ToJuicyRGBA8 a
 		_ -> error "never occur"
 
 colors :: [[Rgb CDouble]]
@@ -159,3 +173,14 @@ mkPoint16s :: [([a], [a])] -> [[Point16 a]]
 mkPoint16s (
 	(xs, xs'), (ys, ys'), (zs, zs'), (ws, ws') = []
 	-}
+
+mul :: Rgb CDouble -> CDouble -> Rgb CDouble
+mul (RgbDouble r g b) n = fromJust $ rgbDouble (r * n) (g * n) (b * n)
+
+add :: Rgb CDouble -> Rgb CDouble -> Rgb CDouble
+add (RgbDouble r1 g1 b1)  (RgbDouble r2 g2 b2) = fromJust $ rgbDouble (r1 +! r2) (g1 +! g2) (b1 +! b2)
+
+(+!) :: CDouble -> CDouble -> CDouble
+a +! b	| a + b < 0 = 0
+	| a + b < 1 = a + b
+	| otherwise = a + b
