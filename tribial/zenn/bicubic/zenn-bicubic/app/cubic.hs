8695a6664f5087c064d9f9e4cf428d22ba3d81ca
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-01-02 08:49:22 +0900

    zenn-bicubic: app/cubic.hs

--- tribial/zenn/bicubic/zenn-bicubic/app/cubic.hs
+++ tribial/zenn/bicubic/zenn-bicubic/app/cubic.hs
@@ -4,6 +4,7 @@
 
 module Main where
 
+import GHC.Stack
 import Foreign.C.Types
 import Data.Foldable
 import Data.Maybe
@@ -43,7 +44,7 @@ radius' = 3
 
 ceiling' = (+ 1) . floor
 
-main :: IO ()
+main :: HasCallStack => IO ()
 main = do
 	s <- cairoImageSurfaceCreate CairoFormatArgb32
 		(floor $ leftMargin + unit * number + rightMargin)
@@ -54,17 +55,13 @@ main = do
 		for_ [1, 1 + 1 / 3 .. 3] \x -> do
 			let	(xi, xd) = properFraction x
 				(yi, yd) = properFraction y
-				cs = (<$> [yi - 1 .. yi + 2]) \a ->
+				css = (<$> [yi - 1 .. yi + 2]) \a ->
 					(<$> [xi - 1 .. xi + 2]) \b -> colorsA ! a ! b
-				-- c00 = colorsA !  yi ! xi
-				c00 = cs !! 1 !! 1
-				c10 = cs !! 1 !! 2
-				c01 = cs !! 2 !! 1
-				c11 = cs !! 2 !! 2
-				c0 = (c00 `mul` (fromIntegral (ceiling' x) - x)) `add` (c10 `mul` (x - fromIntegral (floor x)))
-				c1 = (c01 `mul` (fromIntegral (ceiling' x) - x)) `add` (c11 `mul` (x - fromIntegral (floor x)))
-				c = (c0 `mul` (fromIntegral (ceiling' y) - y)) `add` (c1 `mul` (y - fromIntegral (floor y)))
-			cairoSetSourceRgb cr c
+				css' = (<$> zip [-1 .. 2] css) \(yi, cs) ->
+					(<$> zip [-1 .. 2] cs) \(xi, c) ->
+						c `mul` (formula (abs $ yd - yi) * formula (abs $ xd - xi))
+				c = foldl add (0, 0, 0) $ concat css'
+			cairoSetSourceRgb cr $ uncurry3 rgbDouble' c
 			cairoRectangle cr
 				(leftMargin + 3 * unit * (x - 1 / 6))
 				(topMargin + 3 * unit * (y - 1 / 6))
@@ -87,13 +84,23 @@ main = do
 
 	for_ [1, 1 + 1 / 3 .. 3] \y ->
 		for_ [1, 1 + 1 / 3 .. 3] \x -> do
+			let	(xi, xd) = properFraction x
+				(yi, yd) = properFraction y
+				css = (<$> [yi - 1 .. yi + 2]) \a ->
+					(<$> [xi - 1 .. xi + 2]) \b -> colorsA ! a ! b
+				css' = (<$> zip [-1 .. 2] css) \(yi, cs) ->
+					(<$> zip [-1 .. 2] cs) \(xi, c) ->
+						c `mul` (formula (abs $ yd - yi) * formula (abs $ xd - xi))
+				c = foldl add (0, 0, 0) $ concat css'
+				{-
 			let	c00 = colorsA ! floor y ! floor x
 				c10 = colorsA ! floor y ! ceiling' x
 				c01 = colorsA ! ceiling' y ! floor x
 				c11 = colorsA ! ceiling' y ! ceiling' x
 				c0 = (c00 `mul` (fromIntegral (ceiling' x) - x)) `add` (c10 `mul` (x - fromIntegral (floor x)))
 				c1 = (c01 `mul` (fromIntegral (ceiling' x) - x)) `add` (c11 `mul` (x - fromIntegral (floor x)))
-				c = (c0 `mul` (fromIntegral (ceiling' y) - y)) `add` (c1 `mul` (y - fromIntegral (floor y)))
+				c = uncurry3 rgbDouble' $ (c0 `mul'` (fromIntegral (ceiling' y) - y)) `add` (c1 `mul'` (y - fromIntegral (floor y)))
+				-}
 			cairoMoveTo cr
 				(leftMargin + 3 * unit * x + radius')
 				(topMargin + 3 * unit * y)
@@ -104,7 +111,7 @@ main = do
 			cairoSet cr $ LineWidth 2
 			cairoSetSourceRgb cr . fromJust $ rgbDouble 0.5 0.5 0.5
 			cairoStrokePreserve cr
-			cairoSetSourceRgb cr c
+			cairoSetSourceRgb cr $ uncurry3 rgbDouble' c
 			cairoFill cr
 
 	for_ (zip [0 .. 4] colors) \(y, cs) ->
@@ -136,6 +143,14 @@ main = do
 	cairoImageSurfaceGetCairoImage s >>= \case
 		CairoImageArgb32 a -> writePng "cubic.png" $ cairoArgb32ToJuicyRGBA8 a
 		_ -> error "never occur"
+
+formula :: CDouble -> CDouble
+formula v_
+--	| v < 1 = 1 - v
+--	| otherwise = 0
+	| v < 1 = (3 * v ^ 3 - 5 * v ^ 2 + 2) / 2
+	| otherwise = (- v ^ 3 + 5 * v ^ 2 - 8 * v + 4) / 2
+	where v = abs v_
 
 colors :: [[Rgb CDouble]]
 colors = sep 5 randomColors
@@ -178,13 +193,26 @@ mkPoint16s (
 	(xs, xs'), (ys, ys'), (zs, zs'), (ws, ws') = []
 	-}
 
-mul :: Rgb CDouble -> CDouble -> Rgb CDouble
-mul (RgbDouble r g b) n = fromJust $ rgbDouble (r * n) (g * n) (b * n)
+mul :: HasCallStack => Rgb CDouble -> CDouble -> (CDouble, CDouble, CDouble)
+mul (RgbDouble r g b) n = (r * n, g * n, b * n)
 
-add :: Rgb CDouble -> Rgb CDouble -> Rgb CDouble
-add (RgbDouble r1 g1 b1)  (RgbDouble r2 g2 b2) = fromJust $ rgbDouble (r1 +! r2) (g1 +! g2) (b1 +! b2)
+mul' :: HasCallStack => RgbRaw -> CDouble -> (CDouble, CDouble, CDouble)
+mul' (r, g, b) n = (r * n, g * n, b * n)
+
+type RgbRaw = (CDouble, CDouble, CDouble)
+
+add :: RgbRaw -> RgbRaw -> RgbRaw
+add (r1, g1, b1)  (r2, g2, b2) = (r1 +! r2, g1 +! g2, b1 +! b2)
 
 (+!) :: CDouble -> CDouble -> CDouble
 a +! b	| a + b < 0 = 0
 	| a + b < 1 = a + b
 	| otherwise = a + b
+
+rgbDouble' :: (HasCallStack, Ord d, Num d) => d -> d -> d -> Rgb d
+rgbDouble' r g b = fromJust $ rgbDouble r' g' b'
+	where
+	to01 x	| x < 0 = 0
+		| x > 1 = 1
+		| otherwise = x
+	r' = to01 r; g' = to01 g; b' = to01 b
