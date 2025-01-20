27371e9d5024c262e7c8b1ffdca876bfc3621d01
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2025-01-20 09:26:04 +0900

    try-swizzle-lens

--- themes/swizzle/try-swizzle-lens/src/Data/SwizzleLens.hs
+++ themes/swizzle/try-swizzle-lens/src/Data/SwizzleLens.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeOperators #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Data.SwizzleLens where
 
 import Data.SwizzleLens.TH
 
-concat <$> swizzleLens `mapM` tail [
-	concat [x', y', z', w', u'] |
+concat <$> (swizzleLens . (: [])) `mapM` ("xyz" ++ reverse ['a' .. 'w'])
+
+concat <$> swizzleLens `mapM` filter ((> 1) . length) [
+	concat [x', y', z', w', v'] |
 	x' <- ["", "x"], y' <- ["", "y"],
-	z' <- ["", "z"], w' <- ["", "w"], u' <- ["", "u"] ]
+	z' <- ["", "z"], w' <- ["", "w"], v' <- ["", "v"] ]
--- themes/swizzle/try-swizzle-lens/src/Try.hs
+++
@@ -1,9 +0,0 @@
-module Try where
-
-newtype Const c a = Const { runConst :: c } deriving Show
-
-instance Functor (Const c) where _ `fmap` Const c = Const c
-
-newtype Identity a = Identity { runIdentity :: a } deriving Show
-
-instance Functor Identity where f `fmap` Identity x = Identity $ f x
