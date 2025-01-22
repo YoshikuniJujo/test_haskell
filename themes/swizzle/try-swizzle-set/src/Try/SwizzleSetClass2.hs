774218a639c8224944db6b1bf8452a27f65232f5
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-01-22 11:06:04 +0900

    try-swizzle-set: Try.SwizzleSetClass2: xyzttd

--- themes/swizzle/try-swizzle-set/src/Try/SwizzleSetClass2.hs
+++ themes/swizzle/try-swizzle-set/src/Try/SwizzleSetClass2.hs
@@ -32,3 +32,21 @@ foo :: (Show a, Show b, Show c) => [a] -> [b] -> c -> String
 foo [] _ c = show c
 foo _ [] c = show c
 foo (x : xs) (y : ys) c = show x ++ " (" ++ foo xs ys c ++ ") " ++ show y
+
+bar :: [String] -> [String] -> [String]
+bar xs us =
+	scanr (\(x, u) -> (++ (") " ++ u)) . ((x ++ " (") ++)) "s" $ zip xs us
+
+xyzt0 :: DecsQ
+xyzt0 = [d|
+	xyz :: (SwizzleSet1 (Y (Z s w) v) u, SwizzleSet2 (Z s w) v, SwizzleSet3 s w) =>
+		s -> (u, v, w) -> X (Y (Z s w) v) u
+	xyz s (u, v, w) = x (y (z s w) v) u
+	|]
+
+xyzttd :: DecQ
+xyzttd = newName "s" >>= \s -> newName `mapM` ["u", "v", "w"] >>= \[u, v, w] ->
+	sigD (mkName "xyz")
+		(varT s `arrT` tupT [u, v, w] `arrT`
+			foldr (\(xu, ul) -> (`appT` ul) . (xu `appT`)) (varT s)
+				(zip (conT . mkName <$> ["X", "Y", "Z"]) (varT <$> [u, v, w])))
