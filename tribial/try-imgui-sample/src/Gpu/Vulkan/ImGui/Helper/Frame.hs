0a2bc535072af31fc0c554f2d95bba3a6fd84791
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2025-03-09 21:10:29 +0900

    try-imgui-sample: Gpu.Vulkan.ImGui.Helper.Frame

--- tribial/try-imgui-sample/src/Gpu/Vulkan/ImGui/Helper/Frame.hs
+++ tribial/try-imgui-sample/src/Gpu/Vulkan/ImGui/Helper/Frame.hs
@@ -1,3 +1,10 @@
+{-# LANGUAGE ImportQualifiedPost #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Gpu.Vulkan.ImGui.Helper.Frame where
+
+import Gpu.Vulkan.CommandPool.Type qualified as Vk.CmdPl
+
+data FC scp = FC {
+	fCCommandPool :: Vk.CmdPl.C scp
+	}
