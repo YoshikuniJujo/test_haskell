0283557adce6c6bcf11b83012afbc945fbfeba59
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2025-03-10 12:57:05 +0900

    try-imgui-sample: Gpu.Vulkan.ImGui.Helper.FrameSemaphores: fCToMiddle

--- tribial/try-imgui-sample/src/Gpu/Vulkan/ImGui/Helper/FrameSemaphores.hs
+++ tribial/try-imgui-sample/src/Gpu/Vulkan/ImGui/Helper/FrameSemaphores.hs
@@ -5,7 +5,16 @@ module Gpu.Vulkan.ImGui.Helper.FrameSemaphores where
 
 import Gpu.Vulkan.Semaphore.Internal qualified as Vk.Smph
 
+import Gpu.Vulkan.ImGui.Helper.FrameSemaphores.Middle qualified as M
+
 data FC sias srcs = FC {
 	fCImageAcquiredSemaphore :: Vk.Smph.S sias,
 	fCRenderCompleteSemaphore :: Vk.Smph.S srcs }
 	deriving Show
+
+fCToMiddle :: FC sias srcs -> M.FC
+fCToMiddle FC {
+	fCImageAcquiredSemaphore = Vk.Smph.S ias,
+	fCRenderCompleteSemaphore = Vk.Smph.S rcs } = M.FC {
+	M.fCImageAcquiredSemaphore = ias,
+	M.fCRenderCompleteSemaphore = rcs }
