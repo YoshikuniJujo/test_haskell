f46a8364b35690f73a5c084fa03d4e5d333bbaa5
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2025-03-13 00:13:45 +0900

    try-imgui-sample: Gpu.Vulkn.ImGui.Core: InitInfo: halfway

--- tribial/try-imgui-sample/include/imgui_c.h
+++ tribial/try-imgui-sample/include/imgui_c.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <stdbool.h>
+#include <vulkan/vulkan.h>
+
 typedef int ImGuiConfigFlags;
 
 enum ImGuiConfigFlags_
@@ -19,4 +22,38 @@ enum ImGuiConfigFlags_
     ImGuiConfigFlags_NavEnableSetMousePos   = 1 << 2,   // [moved/renamed in 1.91.4] -> use bool io.ConfigNavMoveSetMousePos
     ImGuiConfigFlags_NavNoCaptureKeyboard   = 1 << 3,   // [moved/renamed in 1.91.4] -> use bool io.ConfigNavCaptureKeyboard
 #endif
+};
+
+struct ImGui_ImplVulkan_InitInfo
+{
+    uint32_t                        ApiVersion;                 // Fill with API version of Instance, e.g. VK_API_VERSION_1_3 or your value of VkApplicationInfo::apiVersion. May be lower than header version (VK_HEADER_VERSION_COMPLETE)
+    VkInstance                      Instance;
+    VkPhysicalDevice                PhysicalDevice;
+    VkDevice                        Device;
+    uint32_t                        QueueFamily;
+    VkQueue                         Queue;
+    VkDescriptorPool                DescriptorPool;             // See requirements in note above; ignored if using DescriptorPoolSize > 0
+    VkRenderPass                    RenderPass;                 // Ignored if using dynamic rendering
+    uint32_t                        MinImageCount;              // >= 2
+    uint32_t                        ImageCount;                 // >= MinImageCount
+    VkSampleCountFlagBits           MSAASamples;                // 0 defaults to VK_SAMPLE_COUNT_1_BIT
+
+    // (Optional)
+    VkPipelineCache                 PipelineCache;
+    uint32_t                        Subpass;
+
+    // (Optional) Set to create internal descriptor pool instead of using DescriptorPool
+    uint32_t                        DescriptorPoolSize;
+
+    // (Optional) Dynamic Rendering
+    // Need to explicitly enable VK_KHR_dynamic_rendering extension to use this, even for Vulkan 1.3.
+    bool                            UseDynamicRendering;
+#ifdef IMGUI_IMPL_VULKAN_HAS_DYNAMIC_RENDERING
+    VkPipelineRenderingCreateInfoKHR PipelineRenderingCreateInfo;
+#endif
+
+    // (Optional) Allocation, Debugging
+    const VkAllocationCallbacks*    Allocator;
+    void                            (*CheckVkResultFn)(VkResult err);
+    VkDeviceSize                    MinAllocationSize;          // Minimum allocation size. Set to 1024*1024 to satisfy zealous best practices validation layer and waste a little memory.
 };
--- tribial/try-imgui-sample/src/Gpu/Vulkan/ImGui/Core.hsc
+++ tribial/try-imgui-sample/src/Gpu/Vulkan/ImGui/Core.hsc
@@ -1,3 +1,7 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE PatternSynonyms, ViewPatterns #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Gpu.Vulkan.ImGui.Core (
@@ -6,6 +10,13 @@ module Gpu.Vulkan.ImGui.Core (
 	) where
 
 import Foreign.Ptr
+import Foreign.Storable
+import Foreign.C.Struct
+import Data.Word
+
+import Gpu.Vulkan.Instance.Core qualified as Vk.Ist
+
+#include "imgui_c.h"
 
 checkVersion :: IO ()
 checkVersion = cxx_imgui_check_version
@@ -18,3 +29,14 @@ data ContextTag
 
 foreign import ccall "imgui_check_version" cxx_imgui_check_version :: IO ()
 foreign import ccall "create_context_no_arg" cxx_create_context_no_arg :: IO Context
+
+struct "InitInfo" #{size struct ImGui_ImplVulkan_InitInfo}
+	#{alignment struct ImGui_ImplVulkan_InitInfo} [
+	("ApiVersion", ''#{type uint32_t},
+		[| #{peek struct ImGui_ImplVulkan_InitInfo, ApiVersion} |],
+		[| #{poke struct ImGui_ImplVulkan_InitInfo, ApiVersion} |]),
+	("Instance", ''Vk.Ist.I,
+		[| #{peek struct ImGui_ImplVulkan_InitInfo, Instance} |],
+		[| #{poke struct ImGui_ImplVulkan_InitInfo, Instance} |])
+	]
+	[''Show, ''Storable]
