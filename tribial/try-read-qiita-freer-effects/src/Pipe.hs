a4fd0a640dea517d0a41e4ddef144cd8500d51e6
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2025-04-02 22:56:59 +0900

    try-read-qiita-freer-effects: pipe example

--- tribial/try-read-qiita-freer-effects/src/Pipe.hs
+++ tribial/try-read-qiita-freer-effects/src/Pipe.hs
@@ -8,6 +8,9 @@
 
 module Pipe where
 
+import Control.Monad
+import Data.Char
+import System.IO
 import Eff
 
 data Pipe i o r where
@@ -49,3 +52,36 @@ foo = yield @() 123
 
 bar :: Member IO effs => Eff (Pipe Integer () ': effs) ()
 bar = print' =<< await @Integer @()
+
+lift :: Member m effs => m a -> Eff effs a
+lift = (`Bind` Pure) . inj
+
+hRead :: Member IO (Pipe () String ': effs) => Handle -> Eff (Pipe () String ': effs) ()
+hRead h = do
+	eof <- lift $ hIsEOF h
+	if eof then return () else do
+		l <- (`Bind` Pure) . inj $ hGetLine h
+		yield @() l
+		hRead h
+
+writeString :: Member IO (Pipe String () ': effs) => Eff (Pipe String () ': effs) ()
+writeString = do
+	ms <- await @_ @()
+	case ms of
+		Just s -> lift (putStrLn s) >> writeString
+		_ -> pure ()
+
+takeP :: forall a effs . Int -> Eff (Pipe a a ': effs) ()
+takeP 0 = pure ()
+takeP n = do
+	mx <- await @a @a
+	case mx of
+		Nothing -> pure ()
+		Just x -> yield @a @a x >> takeP (n - 1)
+
+convert :: forall a b effs . (a -> b) -> Eff (Pipe a b ': effs) ()
+convert f = await @a @b >>= maybe (pure ()) ((>> convert f) . yield @a @b . f)
+
+baz :: IO ()
+baz = void . runM . runPipe
+	$ hRead stdin =$= takeP 3 =$= convert (map toUpper) =$= writeString
