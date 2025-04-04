2f75e5306de0c12395262439bad332b6315f91de
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-04-04 09:43:11 +0900

    Revert "try-control-open-union: more generally"
    This reverts commit a13891eb27ee4068f49cb89895e53cd4e823b0b2.

--- themes/freer-effects/yafe/try-control-open-union/src/Control/OpenUnion.hs
+++ themes/freer-effects/yafe/try-control-open-union/src/Control/OpenUnion.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE ScopedTypeVariables, TypeApplications #-}
-{-# LANGUAGE ExistentialQuantification, TypeFamilies, GADTs #-}
-{-# LANGUAGE DataKinds, PolyKinds #-}
+{-# LANGUAGE ExistentialQuantification, GADTs #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE KindSignatures, TypeOperators #-}
 {-# LANGUAGE MultiParamTypeClasses, AllowAmbiguousTypes #-}
 {-# LANGUAGE FlexibleContexts, FlexibleInstances, UndecidableInstances #-}
@@ -9,11 +9,7 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Control.OpenUnion (
-	U, Member, T, inj, prj, decomp, extract, weaken, NonDet(..),
-
-	U', inj', prj', decomp', extract', weaken',
-
-	Foo, Bar
+	U, Member, T, inj, prj, decomp, extract, weaken, NonDet(..)
 	) where
 
 import Control.NonDetable qualified as NonDetable
@@ -24,11 +20,9 @@ type T = Type -> Type
 
 data U (ts :: [T]) a = forall t . U Word (t a)
 
-data U' (ts :: [k]) (as :: [k']) = forall t . U' Word (t as)
+newtype P (t :: T) (ts :: [T]) = P { unP :: Word }
 
-newtype P (t :: k) (ts :: [k]) = P { unP :: Word }
-
-class Member (t :: k) (ts :: [k]) where elemNo :: P t ts
+class Member (t :: T) (ts :: [T]) where elemNo :: P t ts
 
 instance Member t (t ': ts) where elemNo = P 0
 
@@ -38,16 +32,8 @@ instance {-# OVERLAPPABLE #-} Member t ts => Member t (_t' ': ts) where
 inj :: forall t ts a . Member t ts => t a -> U ts a
 inj = U $ unP (elemNo :: P t ts)
 
-inj' :: forall t ts as . Member t ts => t as -> U' ts as
-inj' = U' $ unP (elemNo :: P t ts)
-
 prj :: forall t ts a . Member t ts => U ts a -> Maybe (t a)
 prj (U i x)
-	| i == unP (elemNo :: P t ts) = Just $ unsafeCoerce x
-	| otherwise = Nothing
-
-prj' :: forall t ts a . Member t ts => U' ts a -> Maybe (t a)
-prj' (U' i x)
 	| i == unP (elemNo :: P t ts) = Just $ unsafeCoerce x
 	| otherwise = Nothing
 
@@ -55,21 +41,11 @@ decomp :: U (t ': ts) a -> Either (U ts a) (t a)
 decomp (U 0 tx) = Right $ unsafeCoerce tx
 decomp (U i tx) = Left $ U (i - 1) tx
 
-decomp' :: U' (t ': ts) a -> Either (U' ts a) (t a)
-decomp' (U' 0 tx) = Right $ unsafeCoerce tx
-decomp' (U' i tx) = Left $ U' (i - 1) tx
-
 extract :: U '[t] a -> t a
 extract (U _ tx) = unsafeCoerce tx
 
-extract' :: U' '[t] a -> t a
-extract' (U' _ tx) = unsafeCoerce tx
-
 weaken :: U r a -> U (any ': r) a
 weaken (U n a) = U (n + 1) a
-
-weaken' :: U' r a -> U' (any ': r) a
-weaken' (U' n a) = U' (n + 1) a
 
 -- instance (NonDetable nd, Member nd effs) => NonDetable (U effs) where
 instance Member NonDet effs => NonDetable.N (U effs) where
@@ -78,7 +54,3 @@ instance Member NonDet effs => NonDetable.N (U effs) where
 data NonDet a where MZero :: NonDet a; MPlus :: NonDet Bool
 
 instance NonDetable.N NonDet where mz = MZero; mp = MPlus
-
-type Foo (ts :: [k]) = U' ts ('[] :: [Type])
-
-type Bar ts a = U' ts '[a]
