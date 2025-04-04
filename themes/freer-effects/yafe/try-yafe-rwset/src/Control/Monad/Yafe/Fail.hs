c77cfbb02aaab0b697ce815ab017c79d0654097f
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2025-04-04 21:02:24 +0900

    try-yafe-rwset: Control.Monad.Yafe.Fail: runFailExc

--- themes/freer-effects/yafe/try-yafe-rwset/src/Control/Monad/Yafe/Fail.hs
+++ themes/freer-effects/yafe/try-yafe-rwset/src/Control/Monad/Yafe/Fail.hs
@@ -7,6 +7,7 @@
 module Control.Monad.Yafe.Fail where
 
 import Control.Monad.Yafe.Eff qualified as Eff
+import Control.Monad.Yafe.Except qualified as Except
 import Control.OpenUnion qualified as Union
 
 runFail :: Eff.E (Union.Fail ': effs) a -> Eff.E effs (Either String a)
@@ -16,6 +17,9 @@ runFail =
 runFailIO :: Union.Member IO effs => Eff.E (Union.Fail ': effs) a -> Eff.E effs a
 runFailIO =
 	Eff.handleRelay pure \(Union.Fail msg) -> const . Eff.eff $ fail @IO msg
+
+runFailExc :: Union.Member (Except.Exc String) effs => Eff.E (Union.Fail ': effs) a -> Eff.E effs a
+runFailExc = Eff.handleRelay pure \(Union.Fail msg) -> const $ Except.throwError msg
 
 tryFail :: (
 	Union.Member Union.Fail effs,
