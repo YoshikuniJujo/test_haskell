ea91defb6fdd8460c0c22080e0ead11773336857
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2025-04-05 09:09:07 +0900

    try-yafee-rwseft: rename

--- themes/freer-effects/yafee/try-yafee-rwseft/src/Control/Monad/Yafee/Reader.hs
+++ themes/freer-effects/yafee/try-yafee-rwseft/src/Control/Monad/Yafee/Reader.hs
@@ -5,23 +5,23 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Control.Monad.Yafee.Reader where
+module Control.Monad.Yafee.Reader (R(..), ask, local, run) where
 
 import Control.Monad.Yafee.Eff qualified as Eff
 import Control.OpenUnion qualified as Union
 
-data Reader e a where Ask :: Reader e e
+data R e a where Ask :: R e e
 
-ask :: Union.Member (Reader e) effs => Eff.E effs e
+ask :: Union.Member (R e) effs => Eff.E effs e
 ask = Eff.eff Ask
 
 local :: forall e effs a .
-	Union.Member (Reader e) effs => (e -> e) -> Eff.E effs a -> Eff.E effs a
+	Union.Member (R e) effs => (e -> e) -> Eff.E effs a -> Eff.E effs a
 local f m = do
 	e <- f <$> ask
-	let	h :: Reader e v -> (v -> Eff.E effs a) -> Eff.E effs a
+	let	h :: R e v -> (v -> Eff.E effs a) -> Eff.E effs a
 		h Ask k = k e
 	Eff.interpose pure h m
 
-runReader :: Eff.E (Reader e ': effs) a -> e -> Eff.E effs a
-m `runReader` e = Eff.handleRelay pure (\Ask k -> k e) m
+run :: Eff.E (R e ': effs) a -> e -> Eff.E effs a
+m `run` e = Eff.handleRelay pure (\Ask k -> k e) m
--- themes/freer-effects/yafee/try-yafee-rwseft/src/Control/Monad/Yafee/State.hs
+++ themes/freer-effects/yafee/try-yafee-rwseft/src/Control/Monad/Yafee/State.hs
@@ -7,7 +7,8 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Control.Monad.Yafee.State where
+module Control.Monad.Yafee.State (
+	S(..), get, gets, put, modify, run, transaction ) where
 
 import Control.Monad.Fix
 import Control.Monad.Yafee.Eff qualified as Eff
@@ -15,35 +16,35 @@ import Control.Monad.Freer qualified as Freer
 import Control.OpenUnion qualified as Union
 import Data.FTCQueue qualified as FTCQueue
 
-data State s a where Get :: State s s; Put :: !s -> State s ()
+data S s a where Get :: S s s; Put :: !s -> S s ()
 
-get :: Union.Member (State s) effs => Eff.E effs s
+get :: Union.Member (S s) effs => Eff.E effs s
 get = Eff.eff Get
 
-gets :: Union.Member (State s) effs => (s -> t) -> Eff.E effs t
+gets :: Union.Member (S s) effs => (s -> t) -> Eff.E effs t
 gets f = f <$> get
 
-put :: Union.Member (State s) effs => s -> Eff.E effs ()
+put :: Union.Member (S s) effs => s -> Eff.E effs ()
 put = Eff.eff . Put
 
-modify :: Union.Member (State s) effs => (s -> s) -> Eff.E effs ()
+modify :: Union.Member (S s) effs => (s -> s) -> Eff.E effs ()
 modify f = put . f =<< get
 
-runState :: Eff.E (State s ': effs) a -> s -> Eff.E effs (a, s)
-Freer.Pure x `runState` s = pure (x, s)
-(u Freer.:>>= q) `runState` s = case Union.decomp u of
+run :: Eff.E (S s ': effs) a -> s -> Eff.E effs (a, s)
+Freer.Pure x `run` s = pure (x, s)
+(u Freer.:>>= q) `run` s = case Union.decomp u of
 	Left u' -> u' Freer.:>>=
-		FTCQueue.singleton ((`runState` s) `Freer.comp` q)
-	Right Get -> q `Freer.app` s `runState` s
-	Right (Put s') -> q `Freer.app` () `runState` s'
+		FTCQueue.singleton ((`run` s) `Freer.comp` q)
+	Right Get -> q `Freer.app` s `run` s
+	Right (Put s') -> q `Freer.app` () `run` s'
 
-transactionState :: forall s effs a .
-	Union.Member (State s) effs => Eff.E effs a -> Eff.E effs a
-transactionState m = do
+transaction :: forall s effs a .
+	Union.Member (S s) effs => Eff.E effs a -> Eff.E effs a
+transaction m = do
 	(s0 :: s) <- get
 	($ m) . ($ s0) $ fix \go s -> \case
 		Freer.Pure x -> put s >> pure x
-		u Freer.:>>= q -> case Union.prj @(State s) u of
+		u Freer.:>>= q -> case Union.prj @(S s) u of
 			Nothing -> u Freer.:>>=
 				FTCQueue.singleton (go s `Freer.comp` q)
 			Just Get -> go s $ q `Freer.app` s
