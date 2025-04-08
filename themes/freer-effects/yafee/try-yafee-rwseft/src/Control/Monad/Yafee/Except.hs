b7d2e6316d4b75f54c9ad2e8bfe37b595abc0862
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-04-08 09:43:09 +0900

    try-yafee-rwseft: Control.Monad.Yafee.Except: runFoo

--- themes/freer-effects/yafee/try-yafee-rwseft/src/Control/Monad/Yafee/Except.hs
+++ themes/freer-effects/yafee/try-yafee-rwseft/src/Control/Monad/Yafee/Except.hs
@@ -4,10 +4,15 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Control.Monad.Yafee.Except (E(..), throw, run, catch) where
+module Control.Monad.Yafee.Except (
+
+	E(..), throw, run, runExc, runFail, runIO, catch
+
+	) where
 
 import Control.Monad.Yafee.Eff qualified as Eff
 import Control.OpenUnion qualified as Union
+import Control.Exception qualified as IO
 
 data E e a = Throw e deriving Show
 
@@ -16,6 +21,23 @@ throw = Eff.eff . Throw
 
 run :: Eff.E (E e ': effs) a -> Eff.E effs (Either e a)
 run = Eff.handleRelay (pure . Right) \(Throw e) -> const . pure $ Left e
+
+runIO :: (
+	IO.Exception e,
+	Union.Member IO effs ) => Eff.E (E e ': effs) a -> Eff.E effs a
+runIO = Eff.handleRelay pure \(Throw e) f -> Eff.eff (IO.throwIO e) >>= f
+
+runExc :: (
+	Union.Member (E e') effs
+	) =>
+	(e -> e') -> Eff.E (E e ': effs) a -> Eff.E effs a
+runExc c = Eff.handleRelay pure \(Throw e) f -> throw (c e) >>= f
+
+runFail :: (
+	Union.Member Union.Fail effs
+	) =>
+	Eff.E (E String ': effs) a -> Eff.E effs a
+runFail = Eff.handleRelay pure \(Throw e) f -> fail e >>= f
 
 catch :: Union.Member (E e) effs =>
 	Eff.E effs a -> (e -> Eff.E effs a) -> Eff.E effs a
