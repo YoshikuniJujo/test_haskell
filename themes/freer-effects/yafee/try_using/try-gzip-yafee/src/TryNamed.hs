74db369ccaa595031f59ea8f65c1eec11c05bf36
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-04-08 09:27:35 +0900

    try-gzip-yafee: TryNamed

---
+++ themes/freer-effects/yafee/try_using/try-gzip-yafee/src/TryNamed.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module TryNamed where
+
+import Control.Monad.Yafee.Eff qualified as Eff
+import Control.Monad.Yafee.State qualified as State
+import Control.OpenUnion qualified as Union
+
+countUpDown :: (
+	Union.Member (State.Named "count-up" Int) effs,
+	Union.Member (State.Named "count-down" Int) effs
+	) =>
+	Eff.E effs ()
+countUpDown = do
+	State.modifyN @"count-up" @Int (+ 1)
+	State.modifyN @"count-down" @Int (subtract 1)
+
+foo :: (
+	Union.Member (State.Named "count-up" Int) effs,
+	Union.Member (State.Named "count-down" Int) effs
+	) =>
+	Eff.E effs ()
+foo = do
+	countUpDown
+	countUpDown
+	countUpDown
+	countUpDown
+
+fooRunned :: (((), Int), Int)
+fooRunned = Eff.run
+	. (flip (State.runNamed @"count-up") 0)
+	$ (flip (State.runNamed @"count-down") 10) foo
