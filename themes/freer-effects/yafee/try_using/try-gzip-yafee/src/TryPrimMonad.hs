fbfe85d8a25638109ae38b5b6e28073edd79bd71
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-04-08 10:24:40 +0900

    try-gzip-yafee: TryPrimMonad

--- themes/freer-effects/yafee/try_using/try-gzip-yafee/src/TryPrimMonad.hs
+++ themes/freer-effects/yafee/try_using/try-gzip-yafee/src/TryPrimMonad.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE ScopedTypeVariables, TypeApplications #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE FlexibleContexts #-}
@@ -8,9 +9,11 @@
 module TryPrimMonad where
 
 import Control.Monad.Primitive
+import Control.Monad.ST
 import Control.Monad.Yafee.Eff qualified as Eff
 import Control.Monad.Yafee.Pipe qualified as Pipe
 import Control.OpenUnion qualified as Union
+import Data.Maybe
 import Data.Vector.Mutable qualified as Vector
 
 foo :: PrimMonad m => m Int
@@ -24,19 +27,29 @@ send xs = Pipe.yield @() `mapM_` xs
 
 put :: forall m effs . (
 	PrimMonad m,
-	Union.Member m effs,
+	Union.Base m effs,
 	Union.Member (Pipe.P Int ()) effs ) =>
 	Vector.MVector (PrimState m) (Maybe Int) -> Eff.E effs ()
 put v = do
 	x <- Pipe.await @Int @()
-	Eff.eff $ Vector.write @m v 3 x
+	Eff.effBase $ Vector.write @m v 3 x
 
-{-
 bar :: forall m effs . (
 	PrimMonad m,
-	Union.Member m effs ) =>
-	Eff.E (Pipe.P Int () ': effs) ()
+	Union.Base m (Pipe.P Int () ': effs),
+	Union.Base m (Pipe.P () () ': effs) ) =>
+	Eff.E (Pipe.P () () ': effs) (Vector.MVector (PrimState m) (Maybe Int))
 bar = do
-	v <- Eff.eff $ Vector.new @m 10
-	send [3, 5, 8] Pipe.=$= put @m v
-	-}
+	v <- Eff.effBase $ Vector.new @m 10
+	send @(Pipe.P () Int ': effs)  [123, 5, 8] Pipe.=$=
+		put @m @(Pipe.P Int () ': effs) v
+	pure v
+
+barRunned' :: forall m . PrimMonad m => m (Maybe (Vector.MVector (PrimState m) (Maybe Int)))
+barRunned' = Eff.runM $ Pipe.run (bar @m :: Eff.E '[(Pipe.P () ()), m] (Vector.MVector (PrimState m) (Maybe Int)))
+
+tryIO :: IO (Maybe Int)
+tryIO = barRunned' >>= (`Vector.read` 3) . fromJust
+
+tryST :: Maybe Int
+tryST = runST $ barRunned' >>= (`Vector.read` 3) . fromJust
