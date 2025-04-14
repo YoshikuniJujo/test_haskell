32a8a5310fc8258c7ecfa20ab9c1e768a34f4da1
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2025-04-14 14:05:19 +0900

    try-gzip-yafee: refactoring: halfway

--- themes/freer-effects/yafee/try_using/try-gzip-yafee/app/try-gzip-yafee-new.hs
+++ themes/freer-effects/yafee/try_using/try-gzip-yafee/app/try-gzip-yafee-new.hs
@@ -19,12 +19,11 @@ import Control.Monad.Yafee.Pipe qualified as Pipe
 import Control.Monad.Yafee.State qualified as State
 import Control.Monad.Yafee.Except qualified as Except
 import Control.Monad.Yafee.Fail qualified as Fail
+import Control.Monad.Yafee.IO qualified as YafeeIO
 import Control.OpenUnion qualified as Union
 import Data.Bits
 import Data.Word
 import Data.ByteString qualified as BS
-import Data.Time.Clock()
-import Data.Time.Clock.POSIX
 import System.IO
 import System.Environment
 
@@ -35,7 +34,6 @@ import Pipe.DataCheck
 
 import Gzip
 import ByteStringNum
-import Numeric
 
 import BitArray(BitArray(..))
 
@@ -51,58 +49,22 @@ main = do
 	h <- openFile fp ReadMode
 	(putStrLn . Prelude.take 1000 . show =<<) . runMyEff $
 		fromHandle h Pipe.=$= onDemand @MyEff Pipe.=$= do
-		checkRight Pipe.=$= crcPipe Pipe.=$= do
-			State.put $ RequestBytes 2
-			Just ids <- Pipe.await
-			when (ids /= "\31\139")
-				$ Except.throw @String "Bad magic"
-			State.put $ RequestBytes 1
-			Just cm <- Pipe.await
-			print' $ CompressionMethod $ BS.head cm
-			Just flgs <- (readFlags . BS.head =<<) <$> Pipe.await
-			print' flgs
-			State.put $ RequestBytes 4
-			Just mtm <- Pipe.await @BS.ByteString
-			print' . posixSecondsToUTCTime
-				. realToFrac . word32ToCTime $ bsToNum mtm
-			State.put $ RequestBytes 1
-			print' =<< Pipe.await @BS.ByteString
-			print' . (OS . BS.head <$>)
-				=<< Pipe.await @BS.ByteString
-			when (flagsRawExtra flgs) do
-				State.put $ RequestBytes 2
-				Just xlen <-
-					(bsToWord16 <$>) <$> Pipe.await @BS.ByteString
-				State.put . RequestBytes $ fromIntegral xlen
-				print' . (decodeExtraFields <$>) =<< Pipe.await
-			State.put RequestString
-			when (flagsRawName flgs)
-				$ print' =<< Pipe.await @BS.ByteString
-			when (flagsRawComment flgs)
-				$ print' =<< Pipe.await @BS.ByteString
-			when (flagsRawHcrc flgs) do
-				compCrc
-				putStrLn' . (`showHex` "") . (.&. 0xffff)
-					. (\(Crc c) -> c) =<< State.get @Crc
-				State.put $ RequestBytes 2
-				maybe (pure ()) putStrLn'
-					. (((`showHex` "") . bsToNum @Word16) <$>)
-					=<< Pipe.await @BS.ByteString
+		YafeeIO.print =<< checkRight Pipe.=$= readHeader
 		blocks Pipe.=$= format 100 Pipe.=$=
 			crcPipe Pipe.=$= do
 				fix \go -> Pipe.await >>= \case
 					Nothing -> pure ()
-					Just x -> print' x >> go
+					Just x -> YafeeIO.print x >> go
 				compCrc
 
-		print' . crcToByteString =<< State.get
+		YafeeIO.print . crcToByteString =<< State.get
 
 		State.put $ RequestBytes 4
 		Just efoo <- Pipe.await
-		print' =<< getRight =<< getJust =<< case efoo of
+		YafeeIO.print =<< getRight =<< getJust =<< case efoo of
 			Left _ -> Pipe.await
 			Right _ -> pure $ Just efoo
-		print' @Word32 . bsToNum =<< getRight =<< getJust =<< Pipe.await
+		YafeeIO.print @Word32 . bsToNum =<< getRight =<< getJust =<< Pipe.await
 
 type MyEff = '[
 	State.S Request,
@@ -116,8 +78,7 @@ type MyEff = '[
 	Except.E String, Fail.F, IO ]
 
 type family TupleL t ts where
-	TupleL t '[] = t
-	TupleL t (t' ': ts) = TupleL (t, t') ts
+	TupleL t '[] = t; TupleL t (t' ': ts) = TupleL (t, t') ts
 
 runMyEff :: Eff.E (Pipe.P () () ': MyEff) a -> IO
 	(Either String (Either String (TupleL a '[
@@ -131,8 +92,57 @@ runMyEff = Eff.runM . Fail.run . Except.run
 	. (`State.run` byteStringToBitArray "")
 	. (`State.run` RequestBytes 0) . Pipe.run
 
-print' :: (Show a, Union.Member IO effs) => a -> Eff.E effs ()
-print' = Eff.eff . print
-
-putStrLn' :: Union.Member IO effs => String -> Eff.E effs ()
-putStrLn' = Eff.eff . putStrLn
+readHeader :: (
+	Union.Member (State.S Request) effs,
+	Union.Member (State.S Crc) effs,
+	Union.Member (Except.E String) effs,
+	Union.Member Fail.F effs ) =>
+	Eff.E (Pipe.P BS.ByteString o ': effs) GzipHeader
+readHeader = crcPipe Pipe.=$= do
+	State.put $ RequestBytes 2
+	Just ids <- Pipe.await
+	when (ids /= "\31\139")
+		$ Except.throw @String "Bad magic"
+	State.put $ RequestBytes 1
+	Just cm <- (CompressionMethod . BS.head <$>) <$> Pipe.await
+	Just flgs <- (readFlags . BS.head =<<) <$> Pipe.await
+	State.put $ RequestBytes 4
+	Just mtm <- (word32ToCTime . bsToNum <$>) <$> Pipe.await @BS.ByteString
+	State.put $ RequestBytes 1
+	Just ef <- (BS.head <$>) <$> Pipe.await @BS.ByteString
+	Just os <- (OS . BS.head <$>) <$> Pipe.await @BS.ByteString
+	mexflds <- if (flagsRawExtra flgs)
+	then do
+		State.put $ RequestBytes 2
+		Just xlen <-
+			(bsToWord16 <$>) <$> Pipe.await @BS.ByteString
+		State.put . RequestBytes $ fromIntegral xlen
+		Just exflgs <- (decodeExtraFields <$>) <$> Pipe.await
+		pure exflgs
+	else pure []
+	State.put RequestString
+	mnm <- if (flagsRawName flgs)
+	then do	nm <- Pipe.await @BS.ByteString
+		pure nm
+	else pure Nothing
+	mcmmt <- if (flagsRawComment flgs)
+	then do
+		cmmt <- Pipe.await @BS.ByteString
+		pure cmmt
+	else pure Nothing
+	when (flagsRawHcrc flgs) do
+		compCrc
+		crc <- (\(Crc c) -> fromIntegral $ (.&. 0xffff) c) <$> State.get @Crc
+		State.put $ RequestBytes 2
+		Just crc0 <- (bsToNum @Word16 <$>) <$>
+			Pipe.await @BS.ByteString
+		when (crc /= crc0) $
+			Except.throw @String "Header CRC check failed"
+	pure GzipHeader {
+		gzipHeaderCompressionMethod = cm,
+		gzipHeaderFlags = Flags {
+			flagsText = flagsRawText flgs,
+			flagsHcrc = flagsRawHcrc flgs },
+		gzipHeaderModificationTime = mtm, gzipHeaderExtraFlags = ef,
+		gzipHeaderOperatingSystem = os, gzipHeaderExtraField = mexflds,
+		gzipHeaderFileName = mnm, gzipHeaderComment = mcmmt }
