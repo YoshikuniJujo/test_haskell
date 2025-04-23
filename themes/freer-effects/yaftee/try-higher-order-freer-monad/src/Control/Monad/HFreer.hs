a3cc41f0d791f212c2e402d6e8962afe54dba065
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-04-23 13:16:06 +0900

    try-higher-order-freer-monad

---
+++ themes/freer-effects/yaftee/try-higher-order-freer-monad/src/Control/Monad/HFreer.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Control.Monad.HFreer (H(..), app, comp) where
+
+import Control.Applicative
+import Control.Monad
+import Control.Monad.Freer.NonDetable qualified as NonDetable
+import Control.Monad.Freer.Failable qualified as Failable
+import Data.FTCQueue qualified as Q
+import Data.Bool
+
+data H h a = Pure a | forall x . h (H h) x :>>= Q.Q (H h) x a
+
+instance Functor (H h) where
+	fmap f = \case
+		Pure x -> Pure $ f x; hx :>>= q -> hx :>>= (q Q.|> (Pure . f))
+
+instance Applicative (H h) where
+	pure = Pure
+	Pure f <*> m = f <$> m; hx :>>= q <*> m = hx :>>= (q Q.|> (<$> m))
+
+instance Monad (H h) where
+	Pure x >>= f = f x; hx :>>= q >>= f = hx :>>= (q Q.|> f)
+
+instance NonDetable.N (h (H h)) => Alternative (H h) where
+	empty = NonDetable.mz :>>= Q.singleton Pure
+	m1 <|> m2 = NonDetable.mp :>>= Q.singleton (bool m1 m2)
+
+instance NonDetable.N (h (H h)) => MonadPlus (H h) where
+	mzero = empty; mplus = (<|>)
+
+instance Failable.F (h (H h)) => MonadFail (H h) where
+	fail msg = Failable.fail msg :>>= Q.singleton Pure
+
+app :: Q.Q (H h) a b -> a -> H h b
+q `app` x = case Q.viewl q of
+	Q.One f -> f x
+	f Q.:| r -> case f x of
+		Pure y -> r `app` y
+		hx :>>= q' -> hx :>>= (q' Q.>< r)
+
+comp :: (H h b -> H h' c) -> Q.Q (H h) a b -> a -> H h' c
+comp = (. app) . (.)
