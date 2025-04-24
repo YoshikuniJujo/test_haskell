1833ca839c2c31e2ffcbbf76765a8dfd69d57c32
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2025-04-25 05:39:00 +0900

    try-yaftee-basic-mnonads: writer

---
+++ themes/freer-effects/yaftee/try-yaftee-basic-monads/src/Control/Monad/Yaftee/Writer.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE BlockArguments, TupleSections #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Control.Monad.Yaftee.Writer where
+
+import Control.Arrow
+import Control.Monad.Yaftee.Eff qualified as Eff
+import Control.HigherOpenUnion qualified as Union
+import Data.HFunctor qualified as HFunctor
+
+type W w = Union.FromFirst (W_ w)
+
+data W_ w a where Tell :: w -> W_ w ()
+
+tell :: Union.Member (W w) effs => w -> Eff.E effs ()
+tell = Eff.eff . Tell
+
+run :: (HFunctor.H (Union.U effs), Monoid w) =>
+	Eff.E (W w ': effs) a -> Eff.E effs (a, w)
+run = (uncurry (flip (,))
+	<$>) . Eff.handleRelay
+		(mempty ,) snd \(Tell w) -> (((w <>) `first`) <$>) . ($ ())
