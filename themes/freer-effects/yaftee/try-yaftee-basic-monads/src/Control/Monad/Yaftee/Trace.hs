8d3d6b1bfcc79e5370f1bb9f3ddd69df5caec5fc
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-04-25 09:25:37 +0900

    try-yaftee-basic-monads: trace

---
+++ themes/freer-effects/yaftee/try-yaftee-basic-monads/src/Control/Monad/Yaftee/Trace.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE BlockArguments, LambdaCase #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Control.Monad.Yaftee.Trace where
+
+import Control.Monad.Yaftee.Eff qualified as Eff
+import Control.Monad.HFreer qualified as HFreer
+import Control.HigherOpenUnion qualified as Union
+
+type T = Union.FromFirst T_
+
+data T_ a where T_ :: String -> T_ ()
+
+trace :: Union.Member T effs => String -> Eff.E effs ()
+trace = Eff.eff . T_
+
+run :: Eff.E '[T] a -> IO a
+run = \case
+	HFreer.Pure x -> pure x
+	u HFreer.:>>= q -> case Union.extracth u of
+		Union.FromFirst (T_ s) k ->
+			putStrLn s >> run (q `HFreer.app` k ())
