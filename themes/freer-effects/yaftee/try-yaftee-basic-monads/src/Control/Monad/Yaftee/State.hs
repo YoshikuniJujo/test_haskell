c3394daf2f01dda63d3627a9a452803df619ff87
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-04-25 14:04:59 +0900

    try-yaftee-basic-monads: State

--- themes/freer-effects/yaftee/try-yaftee-basic-monads/src/Control/Monad/Yaftee/State.hs
+++ themes/freer-effects/yaftee/try-yaftee-basic-monads/src/Control/Monad/Yaftee/State.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE ImportQualifiedPost #-}
-{-# LANGUAGE BlockArguments, TupleSections #-}
+{-# LANGUAGE BlockArguments, LambdaCase, TupleSections #-}
 {-# LANGUAGE ScopedTypeVariables, TypeApplications #-}
 {-# LANGUAGE RequiredTypeArguments #-}
 {-# LANGUAGE GADTs #-}
@@ -13,18 +13,21 @@ module Control.Monad.Yaftee.State (
 
 	-- * NORMAL
 
-	S, get, put, run,
+	S, get, gets, put, modify, run, transaction,
 
 	-- * NAMED
 
-	Named, getN, putN, runN
+	Named, getN, getsN, putN, modifyN, runN, transactionN
 
 	) where
 
 import GHC.TypeLits
+import Control.Monad.Fix
 import Control.Monad.Yaftee.Eff qualified as Eff
+import Control.Monad.HFreer qualified as HFreer
 import Control.HigherOpenUnion qualified as Union
 import Data.HFunctor qualified as HFunctor
+import Data.FTCQueue qualified as Q
 
 -- NORMAL
 
@@ -33,12 +36,22 @@ type S s = Named "" s
 get :: Union.Member (S s) effs => Eff.E effs s
 get = getN ""
 
+gets :: Union.Member (S s) effs => (s -> a) -> Eff.E effs a
+gets = getsN ""
+
 put :: Union.Member (S s) effs => s -> Eff.E effs ()
 put = putN ""
+
+modify :: Union.Member (S s) effs => (s -> s) -> Eff.E effs ()
+modify = modifyN ""
 
 run :: HFunctor.H (Union.U effs) =>
 	Eff.E (S s ': effs) a -> s -> Eff.E effs (a, s)
 run = runN
+
+transaction :: forall effs a . forall s -> Union.Member (S s) effs =>
+	Eff.E effs a -> Eff.E effs a
+transaction = transactionN ""
 
 -- NAMED
 
@@ -51,21 +64,28 @@ getN :: forall s effs .
 	forall nm -> Union.Member (Named nm s) effs => Eff.E effs s
 getN nm = Eff.eff (Get @nm)
 
+getsN :: forall s effs a . forall nm ->
+	(Union.Member (Named nm s) effs) => (s -> a) -> Eff.E effs a
+getsN nm f = f <$> getN nm
+
 putN :: forall s effs .
 	forall nm -> Union.Member (Named nm s) effs => s -> Eff.E effs ()
 putN nm = Eff.eff . (Put @nm)
+
+modifyN :: forall s effs . forall nm -> Union.Member (Named nm s) effs =>
+	(s -> s) -> Eff.E effs ()
+modifyN nm f = putN nm . f =<< getN nm
 
 runN :: HFunctor.H (Union.U effs) =>
 	Eff.E (Named nm s ': effs) a -> s -> Eff.E effs (a, s)
 runN = Eff.handleRelayS (,) fst snd \st k s ->
 	case st of Get -> k s s; Put s' -> k () s'
 
-	{-
-m `runN` s = case m of
-	HFreer.Pure x -> HFreer.Pure (x, s)
-	u HFreer.:>>= q -> case Union.decomp u of
-		Left u' -> HFunctor.map (`runN` s) (, s) u' HFreer.:>>=
-			Q.singleton \(x, s') -> q `HFreer.app` x `runN` s'
-		Right (Union.FromFirst Get k) -> q `HFreer.app` (k s) `runN` s
-		Right (Union.FromFirst (Put s') k) -> q `HFreer.app` (k ()) `runN` s'
-		-}
+transactionN :: forall effs a . forall nm s ->
+	Union.Member (Named nm s) effs => Eff.E effs a -> Eff.E effs a
+transactionN nm s m = getN @s nm >>= \s0 -> ($ m) . ($ s0) $ fix \go s' -> \case
+	HFreer.Pure x -> putN nm s' >> pure x
+	u HFreer.:>>= q -> case Union.prj @(Named nm s) u of
+		Nothing -> u HFreer.:>>= Q.singleton (go s' `HFreer.comp` q)
+		Just (Union.FromFirst Get k) -> go s' $ q `HFreer.app` k s'
+		Just (Union.FromFirst (Put t) k) -> go t $ q `HFreer.app` k ()
