e6e77ec68d3fb3bc6992cfc09436d53688eef808
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-04-25 12:15:30 +0900

    try-yaftee-basic-monads: Fail

--- themes/freer-effects/yaftee/try-yaftee-basic-monads/src/Control/Monad/Yaftee/Fail.hs
+++ themes/freer-effects/yaftee/try-yaftee-basic-monads/src/Control/Monad/Yaftee/Fail.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ExplicitForAll #-}
+{-# LANGUAGE RequiredTypeArguments #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Control.Monad.Yaftee.Fail where
+module Control.Monad.Yaftee.Fail (F, run, runExc, runExcN) where
 
 import Control.Monad.Yaftee.Eff qualified as Eff
 import Control.Monad.Yaftee.Except qualified as Except
@@ -24,11 +26,19 @@ run = \case
 			(either (HFreer.Pure . Left) (run `HFreer.comp` q))
 		Right (Union.FromFirst (Union.Fail m) _) -> HFreer.Pure $ Left m
 
-runExc :: (HFunctor.H (Union.U effs), Union.Member (Except.E e) effs) =>
+runExc :: forall e effs a .
+	(HFunctor.H (Union.U effs), Union.Member (Except.E e) effs) =>
 	(String -> e) -> Eff.E (F ': effs) a -> Eff.E effs a
-runExc err = \case
+runExc = runExcN ""
+
+runExcN :: forall e effs a . forall nm ->
+	(HFunctor.H (Union.U effs), Union.Member (Except.Named nm e) effs) =>
+	(String -> e) -> Eff.E (F ': effs) a -> Eff.E effs a
+runExcN nm err = \case
 	HFreer.Pure x -> HFreer.Pure x
 	u HFreer.:>>= q -> case Union.decomp u of
 		Left u' -> HFunctor.map run Right u' HFreer.:>>= Q.singleton
-			(either (Except.throw . err) (runExc err `HFreer.comp` q))
-		Right (Union.FromFirst (Union.Fail m) _) -> Except.throw $ err m
+			(either	(Except.throwN nm . err)
+				(runExcN nm err `HFreer.comp` q))
+		Right (Union.FromFirst (Union.Fail m) _) ->
+			Except.throwN nm $ err m
