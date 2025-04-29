a800a5111065f118d92d2293d165810f2807d5f0
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2025-04-29 12:12:09 +0900

    tribial-trials: Yaftee.UseFTCQ.Pipe.List.to

--- tribial/tribial-trials/src/Yaftee/UseFTCQ/Pipe/List.hs
+++ tribial/tribial-trials/src/Yaftee/UseFTCQ/Pipe/List.hs
@@ -1,8 +1,26 @@
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Yaftee.UseFTCQ.Pipe.List where
 
+import Control.Monad.Fix
+import Data.Bool
+import Yaftee.UseFTCQ.Eff qualified as Eff
 import Yaftee.UseFTCQ.Pipe qualified as Pipe
+import Yaftee.UseFTCQ.Fail qualified as Fail
+import Yaftee.UseFTCQ.HFreer qualified as HFreer
+import Yaftee.OpenUnion qualified as Union
 
 from xs = Pipe.yield `traverse` xs
+
+to :: forall effs i o o' r .
+	(Union.HFunctor (Union.U effs), Union.Member Fail.F effs) =>
+	Eff.E (Pipe.P ': effs) i o r -> Eff.E effs i o' (Maybe [o])
+to p = Pipe.run do
+	(_, HFreer.Pure r) <- p Pipe.=$= fix \go -> do
+		Pipe.isMore >>= bool (pure []) ((:) <$> Pipe.await <*> go)
+	pure r
