c3f4ad024f5d8495f7fbbb76067ce3ffa3456d5d
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-05-07 15:22:32 +0900

    try-yaftee-conduit-bytestring: Control.Monad.Yaftee.Pipe.ByteString.OnDemand

--- themes/freer-effects/yaftee/try-yaftee-conduit-bytestring/src/Control/Monad/Yaftee/Pipe/ByteString.hs
+++ themes/freer-effects/yaftee/try-yaftee-conduit-bytestring/src/Control/Monad/Yaftee/Pipe/ByteString.hs
@@ -3,8 +3,11 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Control.Monad.Yaftee.Pipe.ByteString where
+module Control.Monad.Yaftee.Pipe.ByteString (
+	hGet, hGet', putStr
+	) where
 
+import Prelude hiding (putStr)
 import Control.Monad.Fix
 import Control.Monad.Yaftee.Eff qualified as Eff
 import Control.Monad.Yaftee.Pipe qualified as Pipe
@@ -12,11 +15,15 @@ import Control.Monad.Yaftee.IO qualified as IO
 import Control.HigherOpenUnion qualified as U
 import Data.Bool
 import Data.ByteString qualified as BS
-import System.IO
+import System.IO hiding (putStr)
 
+hGet :: (U.Member Pipe.P es, U.Base (U.FromFirst IO) es) =>
+	Int -> Handle -> Eff.E es i BS.ByteString ()
 hGet bfsz h = fix \go -> Eff.effBase (not <$> hIsEOF h) >>=
 	bool (pure ()) (Eff.effBase (BS.hGetSome h bfsz) >>= Pipe.yield >> go)
 
+hGet' :: (U.Member Pipe.P es, U.Base (U.FromFirst IO) es) =>
+	Int -> Handle -> Eff.E es i (Maybe BS.ByteString) ()
 hGet' bfsz h = fix \go -> Eff.effBase (not <$> hIsEOF h) >>= bool
 	(Pipe.yield Nothing)
 	(Eff.effBase (BS.hGetSome h bfsz) >>= Pipe.yield . Just >> go)
--- themes/freer-effects/yaftee/try-yaftee-conduit-bytestring/src/Control/Monad/Yaftee/Pipe/ByteString/OnDemand.hs
+++ themes/freer-effects/yaftee/try-yaftee-conduit-bytestring/src/Control/Monad/Yaftee/Pipe/ByteString/OnDemand.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE ImportQualifiedPost #-}
-{-# LANGUAGE BlockArguments, LambdaCase #-}
+{-# LANGUAGE BlockArguments, LambdaCase, OverloadedStrings #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ViewPatterns #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Control.Monad.Yaftee.Pipe.ByteString.OnDemand where
+module Control.Monad.Yaftee.Pipe.ByteString.OnDemand (
+	onDemand, Request(..)
+	) where
 
 import Control.Monad.Fix
 import Control.Monad.Yaftee.Eff qualified as Eff
@@ -20,7 +23,7 @@ data Request
 	| RequestBytes Int
 	| RequestBuffer Int
 	| RequestString
-	| RequestPushBadk BitArray.B
+	| RequestPushBack BitArray.B
 	deriving Show
 
 onDemand :: (
@@ -35,6 +38,12 @@ onDemand = fix \go -> State.get >>= \case
 		maybe (Except.throw errne) ((>> go) . Pipe.yield)
 	RequestBytes ln -> takeBytes ln >>=
 		maybe (Except.throw errne) ((>> go) . Pipe.yield)
+	RequestBuffer ln -> takeBuffer ln >>=
+		maybe (Except.throw errne) ((>> go) . Pipe.yield)
+	RequestString -> takeString >>=
+		maybe (Except.throw errne) ((>> go) . Pipe.yield)
+	RequestPushBack ba -> State.modify (ba `BitArray.append`) >>
+		Pipe.yield (Right "") >> go
 	where
 	errne :: String
 	errne = "Not enough ByteString"
@@ -62,7 +71,47 @@ takeBytes ln = State.get >>= \ba -> case BitArray.byteBoundary ba of
 			Just (t, d) ->
 				Just (Right t) <$ State.put (BitArray.fromByteString d)
 
--- takeBuffer ::
+takeBuffer :: (
+	Union.Member Pipe.P effs,
+	Union.Member (State.S BitArray.B) effs ) =>
+	Int -> Eff.E effs (Maybe BS.ByteString) o
+		(Maybe (Either BitArray.B BS.ByteString))
+takeBuffer ln = State.get >>= \ba -> case BitArray.byteBoundary ba of
+	Left (t, d) -> Just (Left t) <$ State.put d
+	Right b -> case BitArray.toByteString b of
+		Left _ -> error "bad"
+		Right bs -> case splitAt' ln bs of
+			Nothing -> readMore >>= bool
+				(bool	(Just (Right bs) <$ State.put BitArray.empty)
+					(pure Nothing) (BS.null bs))
+				(takeBuffer ln)
+			Just (t, d) -> Just (Right t)
+				<$ State.put (BitArray.fromByteString d)
+
+takeString ::
+	(Union.Member Pipe.P effs, Union.Member (State.S BitArray.B) effs) =>
+	Eff.E effs (Maybe BS.ByteString) o
+		(Maybe (Either BitArray.B BS.ByteString))
+takeString = State.get >>= \ba -> case BitArray.byteBoundary ba of
+	Left (t, d) -> Just (Left t) <$ State.put d
+	Right b -> case BitArray.toByteString b of
+		Left _ -> error "bad"
+		Right bs -> case splitString bs of
+			Nothing -> readMore >>= bool (pure Nothing) takeString
+			Just (t, d) -> Just (Right t)
+				<$ State.put (BitArray.fromByteString d)
+
+	{- case splitString . fromRight $ BitArray.toByteString bs of
+		Nothing -> readMore >>= bool (pure Nothing) takeString
+		Just (t, d) -> Just (Right t)
+			<$ State.put (BitArray.fromByteString d)
+			-}
+
+splitString :: BS.ByteString -> Maybe (BS.ByteString, BS.ByteString)
+splitString bs = case BS.span (/= 0) bs of
+	(_, "") -> Nothing
+	(t, BS.uncons -> Just (z, d)) -> Just (BS.snoc t z, d)
+	_ -> error "Never occur"
 
 splitAt' :: Int -> BS.ByteString -> Maybe (BS.ByteString, BS.ByteString)
 splitAt' ln bs = if BS.length bs < ln then Nothing else Just $ BS.splitAt ln bs
