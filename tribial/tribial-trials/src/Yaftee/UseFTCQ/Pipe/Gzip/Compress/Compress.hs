4e23fd713ef696cde0317b53d8bc580dcc734ec8
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-05-09 12:54:02 +0900

    tribial-trials: Yaftee.UseFTCQ.Pipe.Gzip.Compress.Compress: remove not used lines

--- tribial/tribial-trials/src/Yaftee/UseFTCQ/Pipe/Gzip/Compress/Compress.hs
+++ tribial/tribial-trials/src/Yaftee/UseFTCQ/Pipe/Gzip/Compress/Compress.hs
@@ -7,28 +7,23 @@
 {-# LANGUAGE PatternSynonyms #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs -fno-warn-x-partial #-}
 
-module Yaftee.UseFTCQ.Pipe.Gzip.Compress.Compress where
+module Yaftee.UseFTCQ.Pipe.Gzip.Compress.Compress (
+	compressFile, compressRL
+	) where
 
-import Foreign.Marshal.Alloc
 import Control.Arrow
-import Control.Monad
 import Control.Monad.Fix
 import Yaftee.UseFTCQ.Eff qualified as Eff
 import Yaftee.UseFTCQ.IO qualified as YIO
 import Yaftee.UseFTCQ.Pipe qualified as Pipe
--- import Yaftee.UseFTCQ.Pipe.Tools qualified as PipeTools
 import Yaftee.UseFTCQ.Pipe.List qualified as PipeL
-import Yaftee.UseFTCQ.Pipe.IO qualified as PipeIO
 import Yaftee.UseFTCQ.Pipe.ByteString qualified as PipeBS
 import Yaftee.UseFTCQ.State qualified as State
-import Yaftee.UseFTCQ.Except qualified as Except
 import Yaftee.UseFTCQ.Fail qualified as Fail
 import Yaftee.UseFTCQ.HFreer qualified as HFreer
 import Yaftee.OpenUnion qualified as Union
-import Data.Bits
 import Data.Maybe
 import Data.List qualified as L
-import Data.Sequence qualified as Seq
 import Data.Map qualified as Map
 import Data.Swizzle qualified as Swizzle
 import Data.Bool
@@ -40,17 +35,26 @@ import Yaftee.UseFTCQ.Pipe.Gzip.RunLength
 import Yaftee.UseFTCQ.Pipe.Gzip.Compress.Triple
 
 import Data.Calc
-import Data.HuffmanTree (BinTree, mkTr, pairToCodes)
+import Data.HuffmanTree (pairToCodes)
 
 import Yaftee.UseFTCQ.Pipe.Bits (pattern O, pattern I)
 import Yaftee.UseFTCQ.Pipe.Bits qualified as PipeBits
 
 import Data.PackageMerge qualified as PackageMerge
-import Yaftee.UseFTCQ.Pipe.Gzip.Block
-import Yaftee.UseFTCQ.Pipe.Gzip.Huffman
 import Yaftee.UseFTCQ.Pipe.Crc
 import Tools.ByteStringNum
 import Yaftee.UseFTCQ.Pipe.Gzip.Compress.Gzip
+
+compressFile :: RunLengthType BS.ByteString RunLength r -> FilePath -> FilePath -> IO ()
+compressFile crl fp ofp = do
+	((rl, fln), cr) <- getRunLengths crl fp
+	let	crbs = crcToByteString cr
+		flnbs = fileLengthToByteString fln
+		bts = bazbaz rl
+		bd = bitsToByteStringRaw $ bts ++ [O, O, O, O, O, O, O]
+		hdr = encodeGzipHeader $ gzipHeaderToRaw sampleGzipHeader { gzipHeaderFileName = Just "OnDemand.hs" }
+	BS.writeFile ofp $
+		hdr `BS.append` bd `BS.append` crbs `BS.append` flnbs
 
 compressRL :: (
 	Union.Member Pipe.P effs,
@@ -119,62 +123,6 @@ runCandidate (_l, (l', ys)) = do
 	State.modify \st' -> foldl updateTriple st' $ BS.unpack bs
 	Pipe.yield `mapM_` ys
 
-{-
-tryCompress :: IO (((((), [()]), Triple), BS.ByteString), AheadPos)
-tryCompress = withFile "samples/abcdef4.txt" ReadMode \h ->
-	Eff.runM . (`State.run` AheadPos 0) . (`State.run` ("" :: BS.ByteString))
-		. (`State.run` triple0) . ((\(HFreer.Pure x) -> x) . snd . fromJust <$>) . Pipe.run
-		$ PipeBS.hGet 100 h Pipe.=$= compressRL Pipe.=$= fix \go ->
-			Pipe.await >>= (\bs -> YIO.print bs >> go)
-			-}
-
-{-
-tryCompress' :: FilePath -> IO ((((), Triple), BS.ByteString), AheadPos)
-tryCompress' fp = withFile fp WriteMode \hw -> alloca \p ->
-	withFile "samples/abcdef4.txt" ReadMode \h ->
-		Eff.runM . (`State.run` AheadPos 0) . (`State.run` ("" :: BS.ByteString))
-			. (`State.run` triple0) . ((\(HFreer.Pure x) -> x) . snd . fromJust <$>) . Pipe.run
-			$ PipeBS.hGet 100 h Pipe.=$= compressRL Pipe.=$=
-				(fix \go -> Pipe.await >>= (\bs -> Pipe.yield (runLengthToWord32 bs) >> go)) Pipe.=$=
-				listToAtom Pipe.=$=
-				PipeIO.hPutStorable hw p
-				-}
-
-foo, bar :: BS.ByteString
-foo = "\x1f\x8b\x08\x08\xd2\x25\xea\x67\x00\x03\x61\x62\x63\x64\x65\x66\x34\x2e\x74\x78\x74\x00"
-bar = "\x00\x6e\x24\x3a\x63\x19\x00\x00\x00"
-
-{-
-tryCompress'' :: IO ((((BS.ByteString, Triple), BS.ByteString), AheadPos), ([PipeBits.B], [PipeBits.B]))
-tryCompress'' = withFile "samples/abcdef4.txt" ReadMode \h ->
-	Eff.runM
-		. (`State.run` ([] :: [PipeBits.B], [] :: [PipeBits.B]))
-		. (`State.run` AheadPos 0)
-		. (`State.run` ("" :: BS.ByteString))
-		. (`State.run` triple0) . ((\(HFreer.Pure x) -> x) . snd . fromJust <$>) . Pipe.run
-
-		$	(Pipe.yield [I, I, O] >> PipeBS.hGet 100 h Pipe.=$=
-				compressRL Pipe.=$=
-				PipeTools.convert (runLengthToBits
-					(listToMap fixedTableList)
-					(listToMap fixedDstTableList))
-				) Pipe.=$=
-
-			PipeBits.toByteString Pipe.=$= fix \go -> do
---			PipeTools.convert (((`showHex` "") =<<) . BS.unpack) Pipe.=$= fix \go -> do
-			Pipe.isMore >>= bool (pure "") do
-				mbs <- Pipe.await
-				case mbs of
-					bs -> (bs `BS.append`) <$> go
-			
---			PipeIO.putStrLn
--}
-
-{-
-getFoo :: IO BS.ByteString
-getFoo = (foo `BS.append`) . (`BS.append` bar) . fst . fst . fst . fst <$> tryCompress''
--}
-
 tryCompress''' ::
 	Eff.E '[
 		Pipe.P,
@@ -219,68 +167,6 @@ getSorted = (((: []) `first`) <$>) . L.sortOn snd . runLengthsToLitLenFreqs
 
 getDistSorted :: [RunLength] -> [([Int], Int)]
 getDistSorted = (((: []) `first`) <$>) . L.sortOn snd . runLengthsToDistFreqs
-
-rawTableToByteString :: Int -> Map.Map Int Int -> BS.ByteString
-rawTableToByteString n m = BS.pack $ maybe 0 fromIntegral . (m Map.!?) <$> [0 .. n - 1]
-
-makeLitLenTable :: [RunLength] -> BS.ByteString
-makeLitLenTable = rawTableToByteString 286 . PackageMerge.run 14 . getSorted
-
-makeDistTable :: [RunLength] -> BS.ByteString
-makeDistTable = rawTableToByteString 30 . PackageMerge.run 14 . getDistSorted
-
-makeCompressed :: [RunLength] -> [PipeBits.B]
-makeCompressed rl = runLengthToBits dll dd =<< rl
-	where
-	dll = tableToDict . PackageMerge.run 14 $ getSorted rl
-	dd = tableToDict . PackageMerge.run 14 $ getDistSorted rl
-
-listToAtom :: Union.Member Pipe.P effs => Eff.E effs [a] a ()
-listToAtom = fix \go -> Pipe.await >>= \case
-	xs -> Pipe.yield `mapM_` xs >> go
-
-compressIntoFormatX :: FilePath -> FilePath -> IO ()
-compressIntoFormatX ifl ofl = do
-	((rl, _fln), _crc) <- (((++ [RunLengthEndOfInput]) `first`) `first`) <$> getRunLengths compressRL ifl
-	let	tll = makeLitLenTable rl
-		td = makeDistTable rl
-		cd = bitsToByteStringRaw $ makeCompressed rl
-	BS.writeFile ofl $ tll `BS.append` td `BS.append` cd
-
-readFromFormatX :: FilePath -> IO (BinTree Int, BinTree Int, BS.ByteString)
-readFromFormatX fp = do
-	h <- openFile fp ReadMode
-	tll <- mkTr [0 ..] . BS.unpack <$> BS.hGet h 286
-	td <- mkTr [0 ..] . BS.unpack <$> BS.hGet h 30
-	cnt <- BS.hGetContents h
-	pure (tll, td, cnt)
-
-huffmanDecompress :: (
-	Union.Member Pipe.P effs,
-	Union.Member (State.S (BinTree Int, BinTree Int)) effs,
-	Union.Member (State.S ExtraBits) effs,
-	Union.Member Fail.F effs ) =>
-	BinTree Int -> BinTree Int -> Eff.E effs PipeBits.B RunLength ()
-huffmanDecompress tll td = void $ huffmanPipe Pipe.=$= do
-	State.put $ (id &&& id) tll
-	putDecoded tll td 0
-
-byteStringToBits :: BS.ByteString -> [PipeBits.B]
-byteStringToBits bs = (\w -> bool O I . (w `testBit`) <$> [0 .. 7]) =<< BS.unpack bs
-
-{-
-decFormatX fp = do
-	(tll, td, cnt) <- readFromFormatX fp
-	Eff.runM . Fail.run
-		. (\m -> State.runN @_ @"format" m ("" :: BS.ByteString))
-		. (`State.run` (Seq.empty :: Seq.Seq Word8))
-		. (`State.run` ExtraBits 0) . (`State.run` (fixedTable, fixedTable)) . Pipe.run
-		$ Pipe.yield `mapM` (byteStringToBits cnt) Pipe.=$=
-			huffmanDecompress tll td Pipe.=$=
-			runLength Pipe.=$=
-			format 100 Pipe.=$=
-			PipeBS.putStr
-			-}
 
 get :: (
 	Union.Member Pipe.P effs,
@@ -328,21 +214,6 @@ newtype AheadPos = AheadPos Int deriving Show
 nextAheadPos :: AheadPos -> AheadPos
 nextAheadPos (AheadPos p) = AheadPos $ p + 1
 
-tryDecompress :: FilePath -> IO (Either String ())
-tryDecompress fp = withFile fp ReadMode \h -> alloca \p ->
-	Eff.runM . Except.run . void . Pipe.run $
-		PipeIO.hGetStorable h p Pipe.=$=
-		word32ToRunLength Pipe.=$=
-		PipeIO.print
-
-tryDecompress' :: FilePath -> IO (Either String ((), Seq.Seq Word8))
-tryDecompress' fp = withFile fp ReadMode \h -> alloca \p ->
-	Eff.runM . Except.run . (`State.run` Seq.empty) . void . Pipe.run $
-		PipeIO.hGetStorable h p Pipe.=$=
-		word32ToRunLength Pipe.=$=
-		runLength Pipe.=$=
-		PipeIO.print
-
 runLengthToBits :: Map.Map Int [PipeBits.B] -> Map.Map Int [PipeBits.B] -> RunLength -> [PipeBits.B]
 runLengthToBits ml _ (RunLengthLiteral b) = ml Map.! fromIntegral b
 runLengthToBits ml _ (RunLengthLiteralBS bs) = (ml Map.!) . fromIntegral =<< BS.unpack bs
@@ -375,32 +246,9 @@ huffmanLenToCodes l n = (l, []) : go (n - 1)
 		| m < 7 = [(16, PipeBits.listFromNum 2 (m - 3))]
 		| otherwise = (16, [I, I]) : go (m - 6)
 
-runLengthToLitLenDstList :: [RunLength] -> (Int, Int, [Int])
-runLengthToLitLenDstList rl = let
-	(lnll, tll) = fromJust $ huffMapToList 257 . PackageMerge.run 14 $ getSorted rl
-	(lnd, td) = fromJust $ huffMapToList 1 . PackageMerge.run 14 $ getDistSorted rl in
-	(lnll, lnd, tll ++ td)
-
-runLengthToCodes :: [RunLength] -> [(Int, [PipeBits.B])]
-runLengthToCodes rl = uncurry huffmanLenToCodes
-	=<< (head &&& length) <$> L.group (Swizzle.z $ runLengthToLitLenDstList rl)
-
-runLengthToTableTable :: Num a => [RunLength] -> Map.Map Int a
-runLengthToTableTable rl = PackageMerge.run 6
-	. (((: []) `first`) <$>)
-	. L.sortOn snd
-	. ((head &&& length) <$>)
-	. L.group . L.sort $ fst <$> runLengthToCodes rl
-
 mkLitLenDistTableBits :: Map.Map Int [PipeBits.B] -> [(Int, [PipeBits.B])] -> [PipeBits.B]
 mkLitLenDistTableBits _ [] = []
 mkLitLenDistTableBits m ((alp, ebs) : aes) = m Map.! alp ++ ebs ++ mkLitLenDistTableBits m aes
-
-mkLitLenDistTableFromRunLength :: [RunLength] -> [PipeBits.B]
-mkLitLenDistTableFromRunLength rl = mkLitLenDistTableBits m aes
-	where
-	m = tableToDict $ runLengthToTableTable rl
-	aes = runLengthToCodes rl
 
 tableTableLenOrder :: [Int]
 tableTableLenOrder = [16, 17, 18, 0, 8, 7, 9, 6, 10, 5, 11, 4, 12, 3, 13, 2, 14, 1, 15]
@@ -488,17 +336,6 @@ bazbaz rl_ =
 type RunLengthType = Eff.E [
 	Pipe.P, State.S FileLength, State.S Crc, State.S Triple,
 	State.S BS.ByteString, State.S AheadPos, Fail.F, YIO.I]
-
-compressFile :: RunLengthType BS.ByteString RunLength r -> FilePath -> FilePath -> IO ()
-compressFile crl fp ofp = do
-	((rl, fln), cr) <- getRunLengths crl fp
-	let	crbs = crcToByteString cr
-		flnbs = fileLengthToByteString fln
-		bts = bazbaz rl
-		bd = bitsToByteStringRaw $ bts ++ [O, O, O, O, O, O, O]
-		hdr = encodeGzipHeader $ gzipHeaderToRaw sampleGzipHeader { gzipHeaderFileName = Just "OnDemand.hs" }
-	BS.writeFile ofp $
-		hdr `BS.append` bd `BS.append` crbs `BS.append` flnbs
 
 bitsToByteStringRaw :: [PipeBits.B] -> BS.ByteString
 bitsToByteStringRaw bits = getPure
