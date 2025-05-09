b1841ec016ccc9b346479d26213269b10b9ad929
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-05-09 16:16:34 +0900

    try-yaftee-conduit: Control.Monad.Yaftee.Pipe.List: bundle and bundle'

--- themes/freer-effects/yaftee/try-yaftee-conduit/src/Control/Monad/Yaftee/Pipe/List.hs
+++ themes/freer-effects/yaftee/try-yaftee-conduit/src/Control/Monad/Yaftee/Pipe/List.hs
@@ -5,8 +5,11 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Control.Monad.Yaftee.Pipe.List (from, to) where
+module Control.Monad.Yaftee.Pipe.List (
+	from, to, bundle, bundle' ) where
 
+import Control.Arrow
+import Control.Monad
 import Control.Monad.Fix
 import Control.Monad.Yaftee.Eff qualified as Eff
 import Control.Monad.Yaftee.Pipe qualified as Pipe
@@ -28,3 +31,19 @@ to p = (fromJust <$>) . Pipe.run $ fromPure . snd <$> p Pipe.=$= fix \go ->
 
 fromPure :: F.H h i o a -> a
 fromPure = \case F.Pure x -> x; _ -> error "not Pure"
+
+bundle :: U.Member Pipe.P es => Int -> Eff.E es a [a] r
+bundle n = fix \go -> (Pipe.yield =<< replicateM n Pipe.await) >> go
+
+bundle' :: U.Member Pipe.P es => Int -> Eff.E es a [a] ()
+bundle' n = fix \go -> do
+	(f, xs) <- replicateAwait n
+	Pipe.yield xs
+	bool go (pure ()) f
+
+replicateAwait :: U.Member Pipe.P es => Int -> Eff.E es a o (Bool, [a])
+replicateAwait = \case
+	0 -> pure (False, [])
+	n -> Pipe.isMore >>= bool
+		(pure (True, []))
+		(Pipe.await >>= \x -> ((x :) `second`) <$> replicateAwait (n - 1))
