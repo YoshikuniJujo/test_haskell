d3c2b1b71ad1f2167db7e21ffbc72bffc9c7bc2e
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-05-13 10:50:41 +0900

    tribial-trials: Yaftee.UseFTCQ.Pipe.Gzip.Compress.Triple: refactoring

--- tribial/tribial-trials/src/Yaftee/UseFTCQ/Pipe/Gzip/Compress/Triple.hs
+++ tribial/tribial-trials/src/Yaftee/UseFTCQ/Pipe/Gzip/Compress/Triple.hs
@@ -1,10 +1,20 @@
 {-# LANGUAGE ImportQualifiedPost #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE LambdaCase, TupleSections #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs -fno-warn-x-partial #-}
 
 module Yaftee.UseFTCQ.Pipe.Gzip.Compress.Triple (
 
-	T, empty, update, indexLength, distance
+	-- * TYPE
+
+	T, empty,
+
+	-- * UPDATE
+
+	update,
+
+	-- * QUERY
+
+	indexLength, distance
 
 	) where
 
@@ -17,90 +27,68 @@ import Data.Bool
 import Data.Word
 import Data.ByteString qualified as BS
 
--- * API
+-- TYPE
 
 type T = (Int, Seq.Seq Word8, Map.Map BS.ByteString [Int])
 
 empty :: T
 empty = (0, Seq.empty, Map.empty)
 
+-- UPDATE
+
 update :: T -> Word8 -> T
-update = pushWithMax maxOffset
-
-indexLength :: Monad m =>
-	T -> Word8 -> Word8 -> Word8 -> m (Maybe Word8) -> m (Maybe (Int, Int))
-indexLength t b0 b1 b2 = maxLengthFromTriple t $ BS.pack [b0, b1, b2]
-
-distance :: Int -> T -> Int
-distance i' (i, s, _) = i + Seq.length s - i' + 3
-
--- * INTERNAL
+update = push maxOffset
 
 maxOffset :: Int
 maxOffset = 32768
 
-pushWithMax :: Int -> T -> Word8 -> T
-pushWithMax mx (i, s, d) b = (i', s' Seq.|> b, d')
+push :: Int -> T -> Word8 -> T
+push mx (i, s, d) b = (i', s' Seq.|> b, d')
 	where
+	(i', s') = bool (i, s) (i + 1, tailS s) (ln >= mx)
+	d' = Map.alter (add $ i + ln + 1) tr d
+	add j = maybe (Just [j]) (Just . (j :) . takeWhile (>= i'))
 	ln = Seq.length s
-	i' = bool i (i + 1) (ln >= mx)
-	s' = bool s (tailS s) (ln >= mx)
-	j = i + ln
 	tr = BS.pack $ toList (takeR 2 s) ++ [b]
-	d' = Map.alter (modifyElem i' j) tr d
 
-modifyElem :: Int -> Int -> Maybe [Int] -> Maybe [Int]
-modifyElem i j = \case
-	Nothing -> Just [j + 1]
-	Just is -> Just $ (j + 1) : takeWhile (>= i) is
+tailS :: Seq.Seq a -> Seq.Seq a
+tailS s = case Seq.viewl s of (_ Seq.:< s') -> s'; _ -> error "bad"
 
 takeR :: Int -> Seq.Seq a -> Seq.Seq a
 takeR n s = Seq.drop (Seq.length s - n) s
 
-tailS :: Seq.Seq a -> Seq.Seq a
-tailS s = case Seq.viewl s of
-	(_ Seq.:< s') -> s'
-	_ -> error "bad"
+-- QUERY
 
-maxLengthFromTriple :: Monad m =>
-	T -> BS.ByteString -> m (Maybe Word8) -> m (Maybe (Int, Int))
-maxLengthFromTriple st tr gb = maxLength (getRotables st tr) gb
-
-getIndices :: T -> BS.ByteString -> [Int]
-getIndices (_, _, d) tr = fromMaybe [] $ Map.lookup tr d
-
-indexToRotate :: T -> BS.ByteString -> Int -> (Int, Rotable Word8)
-indexToRotate (i, s, _) tr i' = (i', (s', s'))
-	where s' = Seq.drop (i' - i) s Seq.>< Seq.fromList (BS.unpack tr)
-
-getRotables :: T -> BS.ByteString -> [(Int, Rotable Word8)]
-getRotables st tr = indexToRotate st tr <$> is
-	where
-	is = getIndices st tr
+indexLength :: Monad m =>
+	T -> Word8 -> Word8 -> Word8 -> m (Maybe Word8) -> m (Maybe (Int, Int))
+indexLength t b0 b1 b2 = maxLength . getRts t $ BS.pack [b0, b1, b2]
 
 maxLength :: (Monad m, Eq a) =>
 	[(Int, Rotable a)] -> m (Maybe a) -> m (Maybe (Int, Int))
 maxLength [] _ = pure Nothing
 maxLength rs gb = gb >>= \case
 	Nothing -> pure $ Just (fst $ head rs, 0)
-	Just b -> do
-		case checkStrings rs b of
-			[] -> pure $ Just (fst $ head rs, 0)
-			rs' -> (((+ 1) `second`) <$>) <$> maxLength rs' gb
+	Just b -> case (`chk1` b) `mapMaybe` rs of
+		[] -> pure $ Just (fst $ head rs, 0)
+		rs' -> (((+ 1) `second`) <$>) <$> maxLength rs' gb
 
-checkStrings :: Eq a => [(Int, Rotable a)] -> a -> [(Int, Rotable a)]
-checkStrings rs b = (`check1` b) `mapMaybe` rs
+chk1 :: Eq a => (Int, Rotable a) -> a -> Maybe (Int, Rotable a)
+chk1 (i, r) b = flip (bool Nothing) `uncurry` ((b ==) *** Just . (i ,) $ uncR r)
 
-check1 :: Eq a => (Int, Rotable a) -> a -> Maybe (Int, Rotable a)
-check1 r b
-	| b == b' = Just (fst r, r')
-	| otherwise = Nothing
-	where
-	(b', r') = unconsR $ snd r
+getRts :: T -> BS.ByteString -> [(Int, Rotable Word8)]
+getRts (i, s, d) tr = idxToRt i s tr <$> fromMaybe [] (Map.lookup tr d)
+
+idxToRt :: Int -> Seq.Seq Word8 -> BS.ByteString -> Int -> (Int, Rotable Word8)
+idxToRt i s tr i' = (i', (s', s'))
+	where s' = Seq.drop (i' - i) s Seq.>< Seq.fromList (BS.unpack tr)
+
+distance :: Int -> T -> Int
+distance i' (i, s, _) = i + Seq.length s - i' + 3
+
+-- ROTABLE
 
 type Rotable a = (Seq.Seq a, Seq.Seq a)
 
-unconsR :: Rotable a -> (a, Rotable a)
-unconsR (s, p) = case Seq.viewl p of
-	Seq.EmptyL -> unconsR (s, s)
-	x Seq.:< p' -> (x, (s, p'))
+uncR :: Rotable a -> (a, Rotable a)
+uncR (s, p) = case Seq.viewl p of
+	Seq.EmptyL -> uncR (s, s); x Seq.:< p' -> (x, (s, p'))
