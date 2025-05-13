fd70457b17e498da709b9975da23ca5f8efe81dc
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-05-13 14:31:07 +0900

    try-yaftee-conduit: Control.Monad.Yaftee.Pipe.Tools: convert''

--- themes/freer-effects/yaftee/try-yaftee-conduit/src/Control/Monad/Yaftee/Pipe/Tools.hs
+++ themes/freer-effects/yaftee/try-yaftee-conduit/src/Control/Monad/Yaftee/Pipe/Tools.hs
@@ -3,7 +3,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Control.Monad.Yaftee.Pipe.Tools (convert, convert', checkRight) where
+module Control.Monad.Yaftee.Pipe.Tools (convert, convert', convert'', checkRight) where
 
 import Control.Monad.Fix
 import Control.Monad.Yaftee.Eff qualified as Eff
@@ -18,6 +18,11 @@ convert f = fix \go -> Pipe.await >>= ((>> go) . Pipe.yield . f)
 convert' :: U.Member Pipe.P effs => (a -> b) -> Eff.E effs a b ()
 convert' f = fix \go -> Pipe.isMore
 	>>= bool (pure ()) (Pipe.await >>= ((>> go) . Pipe.yield . f))
+
+convert'' :: U.Member Pipe.P es => (Bool -> a -> b) -> a -> Eff.E es a b ()
+convert'' f = fix \go p -> Pipe.isMore >>= bool
+	(Pipe.yield (f True p))
+	(Pipe.await >>= \x -> ((>> go x) . Pipe.yield $ f False p))
 
 checkRight :: (U.Member Pipe.P es, U.Member (Except.E String) es) =>
 	Eff.E es (Either a b) b r
