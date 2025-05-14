054b27362bd0ba977cccb7b4820dd8ef1fe5cbdf
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-05-14 16:27:06 +0900

    try-yaftee-conduit: Control.Monad.Yaftee.Pipe.Tools: skipLeft1

--- themes/freer-effects/yaftee/try-yaftee-conduit/src/Control/Monad/Yaftee/Pipe/Tools.hs
+++ themes/freer-effects/yaftee/try-yaftee-conduit/src/Control/Monad/Yaftee/Pipe/Tools.hs
@@ -1,9 +1,16 @@
 {-# LANGUAGE ImportQualifiedPost #-}
-{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE BlockArguments, LambdaCase #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Control.Monad.Yaftee.Pipe.Tools (convert, convert', convert'', checkRight) where
+module Control.Monad.Yaftee.Pipe.Tools (
+
+	convert, convert', convert'', checkRight,
+
+	skipLeft1
+
+	) where
 
 import Control.Monad.Fix
 import Control.Monad.Yaftee.Eff qualified as Eff
@@ -28,3 +35,11 @@ checkRight :: (U.Member Pipe.P es, U.Member (Except.E String) es) =>
 	Eff.E es (Either a b) b r
 checkRight = fix \go -> Pipe.await >>= (>> go)
 	. either (const $ Except.throw "(Left _) exist") (Pipe.yield)
+
+skipLeft1 :: (U.Member Pipe.P es, U.Member (Except.E String) es) =>
+	Eff.E es (Either a b) o b
+skipLeft1 = Pipe.await >>= \case
+	Left _ -> Pipe.await >>= \case
+		Left _ -> Except.throw @String "Not Right"
+		Right x -> pure x
+	Right x -> pure x
