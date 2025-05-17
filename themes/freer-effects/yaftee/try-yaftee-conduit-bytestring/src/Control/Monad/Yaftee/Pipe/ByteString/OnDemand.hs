aa52d7c12bb5e06ea4ac6aa42ba161753d8f7985
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2025-05-17 13:03:53 +0900

    try-yaftee-conduit-bytestring: Control.Monad.Yaftee.Pipe.ByteString.OnDemand: name

--- themes/freer-effects/yaftee/try-yaftee-conduit-bytestring/src/Control/Monad/Yaftee/Pipe/ByteString/OnDemand.hs
+++ themes/freer-effects/yaftee/try-yaftee-conduit-bytestring/src/Control/Monad/Yaftee/Pipe/ByteString/OnDemand.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE BlockArguments, LambdaCase, OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables, TypeApplications #-}
+{-# LANGUAGE RequiredTypeArguments #-}
 {-# LANGUAGE DataKinds, ConstraintKinds #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE FlexibleContexts #-}
@@ -31,35 +33,36 @@ import Data.Bool
 import Data.ByteString qualified as BS
 import Data.ByteString.BitArray qualified as BitArray
 
-run_ :: HFunctor.Loose (Union.U es) =>
-	Eff.E (States `Append` es) i o a -> Eff.E es i o ()
-run_ = void . (`State.run` RequestBuffer 100) . (`State.run` BitArray BitArray.empty)
+run_ :: forall nm es i o a . HFunctor.Loose (Union.U es) =>
+	Eff.E (States nm `Append` es) i o a -> Eff.E es i o ()
+run_ = void . (flip (State.runN @nm) $ RequestBuffer 100)
+	. (flip (State.runN @nm) $ BitArray BitArray.empty)
 
-type States = '[State.S BitArray, State.S Request]
+type States nm = '[State.Named nm BitArray, State.Named nm Request]
 
-onDemand :: (
+onDemand :: forall es r . forall nm -> (
 	Union.Member Pipe.P es,
-	Members es, Union.Member (Except.E String) es ) =>
+	Members nm es, Union.Member (Except.E String) es ) =>
 	Eff.E es BS.ByteString (Either BitArray.B BS.ByteString) r
-onDemand = fix \go -> State.get >>= \case
-	RequestBits ln -> takeBits ln >>=
+onDemand nm = fix \go -> State.getN nm >>= \case
+	RequestBits ln -> takeBits nm ln >>=
 		maybe (Except.throw errne) ((>> go) . Pipe.yield)
-	RequestBytes ln -> takeBytes ln >>=
+	RequestBytes ln -> takeBytes nm ln >>=
 		maybe (Except.throw errne) ((>> go) . Pipe.yield)
-	RequestBuffer ln -> takeBuffer ln >>=
+	RequestBuffer ln -> takeBuffer nm ln >>=
 		maybe (Except.throw errne) ((>> go) . Pipe.yield)
-	RequestString -> takeString >>=
+	RequestString -> takeString nm >>=
 		maybe (Except.throw errne) ((>> go) . Pipe.yield)
 	RequestPushBack ba ->
-		State.modify (BitArray . (ba `BitArray.append`) . unBitArray) >>
+		State.modifyN nm (BitArray . (ba `BitArray.append`) . unBitArray) >>
 		Pipe.yield (Right "") >> go
 	where
 	errne :: String
 	errne = "Not enough ByteString"
 
-type Members es = (
-	Union.Member (State.S Request) es,
-	Union.Member (State.S BitArray) es )
+type Members nm es = (
+	Union.Member (State.Named nm Request) es,
+	Union.Member (State.Named nm BitArray) es )
 
 data Request
 	= RequestBits Int
@@ -71,61 +74,62 @@ data Request
 
 newtype BitArray = BitArray { unBitArray :: BitArray.B } deriving Show
 
-takeBits :: (
+takeBits :: forall es o . forall nm -> (
 	Union.Member Pipe.P es,
-	Union.Member (State.S BitArray) es ) =>
+	Union.Member (State.Named nm BitArray) es ) =>
 	Int -> Eff.E es BS.ByteString o
 		(Maybe (Either BitArray.B BS.ByteString))
-takeBits ln = State.gets unBitArray >>= \ba -> case BitArray.splitAt ln ba of
-	Nothing -> readMore >>= bool (pure Nothing) (takeBits ln)
-	Just (t, d) -> Just (BitArray.toByteString t) <$ State.put (BitArray d)
+takeBits nm ln = State.getsN nm unBitArray >>= \ba -> case BitArray.splitAt ln ba of
+	Nothing -> readMore nm >>= bool (pure Nothing) (takeBits nm ln)
+	Just (t, d) -> Just (BitArray.toByteString t) <$ State.putN nm (BitArray d)
 
-takeBytes :: (
+takeBytes :: forall nm -> (
 	Union.Member Pipe.P es,
-	Union.Member (State.S BitArray) es ) =>
+	Union.Member (State.Named nm BitArray) es ) =>
 	Int -> Eff.E es BS.ByteString o
 		(Maybe (Either BitArray.B BS.ByteString))
-takeBytes ln = State.gets unBitArray >>= \ba -> case BitArray.byteBoundary ba of
-	Left (t, d) -> Just (Left t) <$ State.put (BitArray d)
+takeBytes nm ln = State.getsN nm unBitArray >>= \ba -> case BitArray.byteBoundary ba of
+	Left (t, d) -> Just (Left t) <$ State.putN nm (BitArray d)
 	Right b -> case BitArray.toByteString b of
 		Left _ -> error "bad"
 		Right bs -> case splitAt' ln bs of
-			Nothing -> readMore >>= bool (pure Nothing) (takeBytes ln)
+			Nothing -> readMore nm >>= bool (pure Nothing) (takeBytes nm ln)
 			Just (t, d) -> Just (Right t)
-				<$ State.put (BitArray $ BitArray.fromByteString d)
+				<$ State.putN nm (BitArray $ BitArray.fromByteString d)
 
-takeBuffer :: (
+takeBuffer :: forall nm -> (
 	Union.Member Pipe.P effs,
-	Union.Member (State.S BitArray) effs ) =>
+	Union.Member (State.Named nm BitArray) effs ) =>
 	Int -> Eff.E effs BS.ByteString o
 		(Maybe (Either BitArray.B BS.ByteString))
-takeBuffer ln = State.gets unBitArray >>= \ba -> case BitArray.byteBoundary ba of
-	Left (t, d) -> Just (Left t) <$ State.put (BitArray d)
+takeBuffer nm ln = State.getsN nm unBitArray >>= \ba -> case BitArray.byteBoundary ba of
+	Left (t, d) -> Just (Left t) <$ State.putN nm (BitArray d)
 	Right b -> case BitArray.toByteString b of
 		Left _ -> error "bad"
 		Right bs -> case splitAt' ln bs of
-			Nothing -> readMore >>= bool
-				(bool	(Just (Right bs) <$ State.put (BitArray BitArray.empty))
+			Nothing -> readMore nm >>= bool
+				(bool	(Just (Right bs) <$ State.putN nm (BitArray BitArray.empty))
 					(pure Nothing) (BS.null bs))
-				(takeBuffer ln)
+				(takeBuffer nm ln)
 			Just (t, d) -> Just (Right t)
-				<$ State.put (BitArray $ BitArray.fromByteString d)
+				<$ State.putN nm (BitArray $ BitArray.fromByteString d)
 
 takeString ::
-	(Union.Member Pipe.P effs, Union.Member (State.S BitArray) effs) =>
+	forall nm ->
+	(Union.Member Pipe.P effs, Union.Member (State.Named nm BitArray) effs) =>
 	Eff.E effs BS.ByteString o
 		(Maybe (Either BitArray.B BS.ByteString))
-takeString = State.gets unBitArray >>= \ba -> case BitArray.byteBoundary ba of
-	Left (t, d) -> Just (Left t) <$ State.put (BitArray d)
+takeString nm = State.getsN nm unBitArray >>= \ba -> case BitArray.byteBoundary ba of
+	Left (t, d) -> Just (Left t) <$ State.putN nm (BitArray d)
 	Right b -> case BitArray.toByteString b of
 		Left _ -> error "bad"
 		Right bs -> case splitString bs of
-			Nothing -> readMore >>= bool (pure Nothing) takeString
+			Nothing -> readMore nm >>= bool (pure Nothing) (takeString nm)
 			Just (t, d) -> Just (Right t)
-				<$ State.put (BitArray $ BitArray.fromByteString d)
+				<$ State.putN nm (BitArray $ BitArray.fromByteString d)
 
 	{- case splitString . fromRight $ BitArray.toByteString bs of
-		Nothing -> readMore >>= bool (pure Nothing) takeString
+		Nothing -> readMore nm >>= bool (pure Nothing) takeString
 		Just (t, d) -> Just (Right t)
 			<$ State.put (BitArray.fromByteString d)
 			-}
@@ -139,12 +143,12 @@ splitString bs = case BS.span (/= 0) bs of
 splitAt' :: Int -> BS.ByteString -> Maybe (BS.ByteString, BS.ByteString)
 splitAt' ln bs = if BS.length bs < ln then Nothing else Just $ BS.splitAt ln bs
 
-readMore :: (
+readMore :: forall nm -> (
 	Union.Member Pipe.P es,
-	Union.Member (State.S BitArray) es
+	Union.Member (State.Named nm BitArray) es
 	) =>
 	Eff.E es BS.ByteString o Bool
-readMore = Pipe.awaitMaybe >>= \case
+readMore nm = Pipe.awaitMaybe >>= \case
 	Nothing -> pure False
-	Just bs -> True <$ State.modify
+	Just bs -> True <$ State.modifyN nm
 		(BitArray . (`BitArray.appendByteString` bs) . unBitArray)
