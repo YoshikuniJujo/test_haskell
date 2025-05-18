8d0bd74c6dd7d45962efe9d412a34743ee95f357
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2025-05-18 18:59:23 +0900

    try-gzip-yaftee: compress: tribial

--- themes/freer-effects/yaftee/try_using/try-gzip-yaftee/app/try-gzip-yaftee-compress.hs
+++ themes/freer-effects/yaftee/try_using/try-gzip-yaftee/app/try-gzip-yaftee-compress.hs
@@ -7,19 +7,19 @@
 
 module Main where
 
+import Control.Monad
 import Control.Monad.Yaftee.Eff qualified as Eff
 import Control.Monad.Yaftee.Pipe qualified as Pipe
 import Control.Monad.Yaftee.Pipe.Tools qualified as PipeT
 import Control.Monad.Yaftee.Pipe.List qualified as PipeL
-import Control.Monad.Yaftee.Pipe.IO qualified as PipeIO
 import Control.Monad.Yaftee.Pipe.Bits qualified as PipeB
 import Control.Monad.Yaftee.Pipe.ByteString qualified as PipeBS
 import Control.Monad.Yaftee.Pipe.ByteString.Crc qualified as PipeCrc
 import Control.Monad.Yaftee.State qualified as State
 import Control.HigherOpenUnion qualified as U
-import Data.Bits
 import Data.Bool
 import Data.ByteString qualified as BS
+import Data.ByteString.ToolsYj qualified as BS
 import Data.ByteString.Bit qualified as Bit
 import System.IO
 import System.Environment
@@ -31,28 +31,23 @@ import Data.Gzip.Block
 main :: IO ()
 main = do
 	ifp : ofp : _ <- getArgs
-	withFile ifp ReadMode \hr -> withFile ofp WriteMode \ho ->
-		Eff.runM
-			. (`State.run` Bit.empty)
-			. (`State.run` ("" :: BS.ByteString))
-			. (`State.run` FileLength 0)
-			. PipeCrc.runCrc32 @"foobar" . RunLength.run . PipeL.to
-			$ PipeBS.hGet 64 hr Pipe.=$=
-				PipeCrc.crc32' "foobar" Pipe.=$=
-				lengthPipe Pipe.=$=
-				RunLength.compressRL Pipe.=$=
---				PipeIO.print
---				{-
-				PipeL.bundle' 500 Pipe.=$=
-				PipeT.convert'' runLengthsToBits [] Pipe.=$= do
-					Pipe.yield $ encodeGzipHeader sampleGzipHeader
-					PipeB.toByteString'
-					PipeCrc.compCrc32 "foobar"
-					Pipe.yield . PipeCrc.crc32ToByteString =<< State.getN "foobar"
-					Pipe.yield . numToBs' 4 . unFileLength =<< State.get
-				Pipe.=$= PipeBS.hPutStr' ho
---				-}
-	pure ()
+	void $ withFile ifp ReadMode \r -> withFile ofp WriteMode \o -> Eff.runM
+		. (`State.run` Bit.empty)
+		. (`State.run` ("" :: BS.ByteString))
+		. (`State.run` FileLength 0)
+		. PipeCrc.runCrc32 @"foobar" . RunLength.run . PipeL.to
+		$ PipeBS.hGet 64 r Pipe.=$=
+			PipeCrc.crc32' "foobar" Pipe.=$=
+			lengthPipe Pipe.=$=
+			RunLength.compressRL Pipe.=$=
+			PipeL.bundle' 500 Pipe.=$=
+			PipeT.convert'' runLengthsToBits [] Pipe.=$= do
+				Pipe.yield $ encodeGzipHeader sampleGzipHeader
+				PipeB.toByteString'
+				PipeCrc.compCrc32 "foobar"
+				Pipe.yield . PipeCrc.crc32ToByteString =<< State.getN "foobar"
+				Pipe.yield . BS.fromBits . unFileLength =<< State.get
+			Pipe.=$= PipeBS.hPutStr' o
 
 lengthPipe :: (U.Member Pipe.P es, U.Member (State.S FileLength) es) =>
 	Eff.E es BS.ByteString BS.ByteString ()
@@ -61,6 +56,3 @@ lengthPipe = (Pipe.isMore >>=) . bool (pure ()) $ Pipe.await >>= \bs -> do
 	Pipe.yield bs >> lengthPipe
 
 newtype FileLength = FileLength { unFileLength :: Int } deriving Show
-
-numToBs' ln _ | ln < 1 = ""
-numToBs' ln n = fromIntegral (n .&. 0xff) `BS.cons` numToBs' (ln - 1) (n `shiftR` 8)
