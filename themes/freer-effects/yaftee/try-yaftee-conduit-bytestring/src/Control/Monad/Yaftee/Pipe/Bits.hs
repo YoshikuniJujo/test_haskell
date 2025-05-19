d591f59cb5fd0ab5a2e3ef785cbd2285fe6c5192
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2025-05-19 11:38:22 +0900

    try-yaftee-conduit-bytestring: Control.Monad.Yaftee.Pipe.Bits: Queue

--- themes/freer-effects/yaftee/try-yaftee-conduit-bytestring/src/Control/Monad/Yaftee/Pipe/Bits.hs
+++ themes/freer-effects/yaftee/try-yaftee-conduit-bytestring/src/Control/Monad/Yaftee/Pipe/Bits.hs
@@ -4,7 +4,11 @@
 {-# LANGUAGE PatternSynonyms #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Control.Monad.Yaftee.Pipe.Bits where
+module Control.Monad.Yaftee.Pipe.Bits (
+
+	toByteString, toByteString', Queue
+
+	) where
 
 import Control.Arrow
 import Control.Monad.Fix
@@ -17,26 +21,31 @@ import Data.ByteString qualified as BS
 import Data.ByteString.Bit (pattern O)
 import Data.ByteString.Bit qualified as Bit
 
-toByteString :: (U.Member Pipe.P es, U.Member (State.S Bit.Queue) es) =>
+toByteString :: (U.Member Pipe.P es, U.Member (State.S Queue) es) =>
 	Eff.E es [Bit.B] BS.ByteString r
 toByteString = fix \go -> (>> go) do
-	State.modify . flip Bit.append =<< Pipe.await
+	State.modify . flip append =<< Pipe.await
 	Pipe.yield =<< uncurry (<$)
-		. (BS.pack *** State.put) . unfoldr' Bit.popByte =<< State.get
+		. (BS.pack *** State.put . Queue) . unfoldr' Bit.popByte =<< State.gets unQueue
 
-toByteString' :: (U.Member Pipe.P es, U.Member (State.S Bit.Queue) es) =>
+toByteString' :: (U.Member Pipe.P es, U.Member (State.S Queue) es) =>
 	Eff.E es [Bit.B] BS.ByteString ()
 toByteString' = fix \go -> Pipe.isMore >>= bool
-	do	State.modify (`Bit.append` [O, O, O, O, O, O, O])
+	do	State.modify $ Queue . (`Bit.append` [O, O, O, O, O, O, O]) . unQueue
 		Pipe.yield =<< uncurry (<$)
-			. (BS.pack *** State.put)
-			. unfoldr' Bit.popByte =<< State.get
-	do	State.modify . flip Bit.append =<< Pipe.await
+			. (BS.pack *** State.put . Queue)
+			. unfoldr' Bit.popByte =<< State.gets unQueue
+	do	State.modify . flip append =<< Pipe.await
 		Pipe.yield =<< uncurry (<$)
-			. (BS.pack *** State.put)
-			. unfoldr' Bit.popByte =<< State.get
+			. (BS.pack *** State.put . Queue)
+			. unfoldr' Bit.popByte =<< State.gets unQueue
 		go
 	
 
 unfoldr' :: (b -> Maybe (a, b)) -> b -> ([a], b)
 unfoldr' f = fix \go s -> maybe ([], s) (\(x, s') -> (x :) `first` go s') $ f s
+
+newtype Queue = Queue { unQueue :: Bit.Queue } deriving Show
+
+append :: Queue -> [Bit.B] -> Queue
+append (Queue q) bs = Queue $ q `Bit.append` bs
