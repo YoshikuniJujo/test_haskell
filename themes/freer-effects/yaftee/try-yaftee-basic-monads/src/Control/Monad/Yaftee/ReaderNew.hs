ebc124e33adf7d8861148cdc1057f1dc4b2aaa79
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-05-20 16:40:00 +0900

    try-yaftee-basic-monads: Control.Monad.Yaftee.ReaderNew

---
+++ themes/freer-effects/yaftee/try-yaftee-basic-monads/src/Control/Monad/Yaftee/ReaderNew.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE ScopedTypeVariables, TypeApplications #-}
+{-# LANGUAGE RequiredTypeArguments #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures, TypeOperators #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Control.Monad.Yaftee.ReaderNew (
+
+	-- * NORMAL
+
+	R, ask, local, run,
+
+	-- * NAMED
+
+	Named, askN, localN, runN
+
+	) where
+
+import GHC.TypeLits
+import Control.Monad.Yaftee.Eff qualified as Eff
+import Control.Monad.HigherFreer qualified as F
+import Control.HigherOpenUnion qualified as Union
+import Data.Kind
+import Data.Functor.Identity
+import Data.HigherFunctor qualified as HFunctor
+import Data.FTCQueue qualified as Q
+
+-- * NORMAL
+
+type R e = Named "" e
+
+ask :: Union.Member (R e) effs => Eff.E effs i o e
+ask = askN ""
+
+local :: forall e effs i o a . Union.Member (R e) effs =>
+	(e -> e) -> Eff.E effs i o a -> Eff.E effs i o a
+local = localN ""
+
+run :: forall e effs i o a . HFunctor.Loose (Union.U effs) =>
+	Eff.E (R e ': effs) i o a -> e -> Eff.E effs  i o a
+run = runN
+
+-- * NAMED
+
+data Named (nm :: Symbol) e (f :: Type -> Type -> Type -> Type) i o a where
+	Ask :: forall nm e f i o . Named nm e f i o e
+	Local :: forall nm e f i o a . (e -> e) -> f i o a -> Named nm e f i o a
+
+askN :: forall nm -> Union.Member (Named nm e) effs => Eff.E effs i o e
+askN nm = Eff.effh (Ask @nm)
+
+localN :: forall nm -> Union.Member (Named nm e) effs =>
+	(e -> e) -> Eff.E effs i o a -> Eff.E effs i o a
+localN nm = (Eff.effh .) . Local @nm
+
+runN :: forall nm e effs i o a . HFunctor.Loose (Union.U effs) =>
+	Eff.E (Named nm e ': effs) i o a -> e -> Eff.E effs i o a
+m `runN` e = case m of
+	F.Pure x -> F.Pure x
+	u F.:>>= q -> case Union.decomp u of
+		Left u' -> HFunctor.map ((Identity <$>) . (`runN` e)) Identity u' F.:>>=
+			Q.singleton (((`runN` e) F.. q) . runIdentity)
+		Right Ask -> (q F.$ e) `runN` e
+		Right (Local f a) ->  (`runN` e) F.. q =<< a `runN` f e
--- themes/freer-effects/yaftee/try-yaftee-basic-monads/src/Trials/Reader.hs
+++ themes/freer-effects/yaftee/try-yaftee-basic-monads/src/Trials/Reader.hs
@@ -1,1 +1,28 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
 module Trials.Reader where
+
+import Control.Monad.Yaftee.Eff qualified as Eff
+import Control.Monad.Yaftee.ReaderNew qualified as Reader
+import Control.Monad.Yaftee.IO qualified as IO
+import Control.HigherOpenUnion qualified as U
+
+sampleReaderNew :: (
+	U.Member (Reader.R Int) es,
+	U.Base IO.I es
+	) =>
+	Eff.E es i o ()
+sampleReaderNew = do
+	IO.print =<< Reader.ask @Int
+	Reader.local (* (2 :: Int)) do
+		IO.print =<< Reader.ask @Int
+	IO.print =<< Reader.ask @Int
+
+runSampleReaderNew ::
+	Eff.E '[Reader.R Int, IO.I] i o () -> IO ()
+runSampleReaderNew = Eff.runM . (`Reader.run` 123)
