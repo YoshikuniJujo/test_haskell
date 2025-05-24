fbc8973a86cf192422d6b27883263673f3697dda
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2025-05-25 08:18:32 +0900

    try-yaftee-png-show: get width and height

--- themes/freer-effects/yaftee/try_using/try-yaftee-png-show/app/Main.hs
+++ themes/freer-effects/yaftee/try_using/try-yaftee-png-show/app/Main.hs
@@ -1,22 +1,28 @@
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE ViewPatterns #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Main (main) where
 
 import Control.Monad
+import Control.Monad.Yaftee.Eff qualified as Eff
 import Control.Monad.Yaftee.Pipe qualified as Pipe
 import Control.Monad.Yaftee.Pipe.Tools qualified as PipeT
-import Control.Monad.Yaftee.Pipe.Png.Decode
+import Control.Monad.Yaftee.Pipe.Png.DecodeNew
 import Control.Monad.Yaftee.Pipe.ByteString qualified as PipeBS
+import Control.Monad.Yaftee.State qualified as State
 import Control.Monad.Yaftee.Except qualified as Except
 import Control.Monad.Yaftee.Fail qualified as Fail
 import Control.Monad.Yaftee.IO qualified as IO
 import Data.TypeLevel.List
+import Data.Word
 import Data.ByteString qualified as BS
 import Data.CairoImage.Internal
+import Data.Png.Header
 import System.IO
 import System.Environment
 import Graphics.Pipe.Write
@@ -25,12 +31,23 @@ import Graphics.Pipe.Draw
 main :: IO ()
 main = do
 	fpi : fpo : _ <- getArgs
+	hh <- openFile fpi ReadMode
+
+	(_, (fromIntegral -> wdt, fromIntegral -> hgt)) <- Eff.runM . (`State.run` ((0, 0) :: (Word32, Word32)))
+		. Except.run @String . Fail.runExc id . pngRun @"chunk" @"deflate" . Pipe.run
+		$ PipeBS.hGet 64 hh Pipe.=$= pngHeader "chunk" "deflate" \hdr -> do
+			IO.print hdr
+			State.put (headerWidth hdr, headerHeight hdr)
+
+	hClose hh
+
 	h <- openFile fpi ReadMode
-	img <- newImageMut @Argb32Mut 16 16
+
+	img <- newImageMut @Argb32Mut wdt hgt
 	writeDrawPipe Effs fpo img (Except.run . Fail.runExc id . pngRun) $
 		PipeBS.hGet 64 h Pipe.=$=
 		(void (png "chunk" "deflate" IO.print) `Except.catch` IO.print @String) Pipe.=$=
 		PipeT.convert BS.tail Pipe.=$=
-		drawCairoImageRgba32 IO img 16 16
+		drawCairoImageRgba32 IO img wdt hgt
 
 type Effs = PngStates "chunk" "deflate" `Append` '[Fail.F, (Except.E String)]
