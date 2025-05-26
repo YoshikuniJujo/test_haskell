0338df9f25cc20fe95c202ada7ca34b2eeab2dc5
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2025-05-26 22:11:47 +0900

    try-yaftee-cairo-image: Graphcis.Pipe.Draw: drawCairoImageRgba32Adam7

--- themes/freer-effects/yaftee/try_using/try-yaftee-cairo-image/src/Graphics/Pipe/Draw.hs
+++ themes/freer-effects/yaftee/try_using/try-yaftee-cairo-image/src/Graphics/Pipe/Draw.hs
@@ -6,7 +6,8 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Graphics.Pipe.Draw (
-	drawCairoImageRgba32, drawCairoImageRgb24 ) where
+	drawCairoImageRgba32, drawCairoImageRgba32Adam7,
+	drawCairoImageRgb24 ) where
 
 import Foreign.C.Types
 import Control.Monad.Fix
@@ -37,6 +38,46 @@ drawCairoImageRgba32 m img w h act = ($ 0) $ fix \go p ->
 	draw :: CInt -> Word8 -> Word8 -> Word8 -> Word8 -> m ()
 	draw p a r g b = putPixel (img :: Argb32Mut (PrimState m)) (p `mod` w) (p `div` w)
 		(PixelArgb32Straight a r g b)
+
+drawCairoImageRgba32Adam7 :: forall m ->
+	(PrimMonad m, U.Member Pipe.P es, U.Base (U.FromFirst m) es) =>
+	Argb32Mut (PrimState m) -> CInt -> CInt -> Eff.E es BS.ByteString o () -> Eff.E es BS.ByteString o ()
+drawCairoImageRgba32Adam7 m img w h act = ($ 0) $ fix \go p ->
+	if p < w * h
+	then do
+		cs <- byteStringToTuple4s <$> Pipe.await
+		let	ln = L.genericLength cs
+		Eff.effBase $ for_ ([p .. p + ln - 1] `zip` cs) \(p', c) ->
+			uncurry4 (draw p') $ rgbaToArgb c
+		act
+		go $ p + ln
+	else pure ()
+	where
+	draw :: CInt -> Word8 -> Word8 -> Word8 -> Word8 -> m ()
+	draw p a r g b = putPixel (img :: Argb32Mut (PrimState m)) x y
+		(PixelArgb32Straight a r g b)
+		where (x, y) = calcPos w h p
+
+calcPos :: Integral n => n -> n -> n -> (n, n)
+calcPos w h p
+	| p < wh64 = (p `mod` w8 * 8, p `div` w8 * 8)
+	| p < wh32 = let p' = p - wh64 in (p' `mod` w8 * 8 + 4, p `div` w8 * 8)
+	| p < wh16 = let p' = p - wh32 in (p' `mod` w4 * 4, p' `div` w4 * 8 + 4)
+	| p < wh8 = let p' = p - wh16 in (p' `mod` w4 * 4 + 2, p' `div` w4 * 4)
+	| p < wh4 = let p' = p - wh8 in (p' `mod` w2 * 2, p' `div` w2 * 4 + 2)
+	| p < wh2 = let p' = p - wh4 in (p' `mod` w2 * 2 + 1, p' `div` w2 * 2)
+	| p < wh = let p' = p - wh2 in (p' `mod` w, p' `div` w * 2 + 1)
+	where
+	wh64 = w * h `div` 64
+	wh32 = w * h `div` 32
+	wh16 = w * h `div` 16
+	wh8 = w * h `div` 8
+	wh4 = w * h `div` 4
+	wh2 = w * h `div` 2
+	wh = w * h
+	w8 = w `div` 8
+	w4 = w `div` 4
+	w2 = w `div` 2
 
 drawCairoImageRgb24 :: forall m ->
 	(PrimMonad m, U.Member Pipe.P es, U.Base (U.FromFirst m) es) =>
