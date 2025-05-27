169c448ad779f06bcf3e17995ca13c62f410adb0
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-05-27 11:16:26 +0900

    try-yaftee-cairo-image: fix about Adam7

--- themes/freer-effects/yaftee/try_using/try-yaftee-cairo-image/src/Graphics/Pipe/Draw.hs
+++ themes/freer-effects/yaftee/try_using/try-yaftee-cairo-image/src/Graphics/Pipe/Draw.hs
@@ -7,7 +7,7 @@
 
 module Graphics.Pipe.Draw (
 	drawCairoImageRgba32, drawCairoImageRgba32Adam7,
-	drawCairoImageRgb24 ) where
+	drawCairoImageRgb24, drawCairoImageRgb24Adam7 ) where
 
 import Foreign.C.Types
 import Control.Monad.Fix
@@ -60,24 +60,26 @@ drawCairoImageRgba32Adam7 m img w h act = ($ 0) $ fix \go p ->
 
 calcPos :: Integral n => n -> n -> n -> (n, n)
 calcPos w h p
-	| p < wh64 = (p `mod` w8 * 8, p `div` w8 * 8)
-	| p < wh32 = let p' = p - wh64 in (p' `mod` w8 * 8 + 4, p' `div` w8 * 8)
-	| p < wh16 = let p' = p - wh32 in (p' `mod` w4 * 4, p' `div` w4 * 8 + 4)
-	| p < wh8 = let p' = p - wh16 in (p' `mod` w4 * 4 + 2, p' `div` w4 * 4)
-	| p < wh4 = let p' = p - wh8 in (p' `mod` w2 * 2, p' `div` w2 * 4 + 2)
-	| p < wh2 = let p' = p - wh4 in (p' `mod` w2 * 2 + 1, p' `div` w2 * 2)
-	| p < wh = let p' = p - wh2 in (p' `mod` w, p' `div` w * 2 + 1)
+	| p < wh1 = (p `mod` w8 * 8, p `div` w8 * 8)
+	| p < wh2 = let p' = p - wh1 in (p' `mod` w8 * 8 + 4, p' `div` w8 * 8)
+	| p < wh3 = let p' = p - wh2 in (p' `mod` w4 * 4, p' `div` w4 * 8 + 4)
+	| p < wh4 = let p' = p - wh3 in (p' `mod` w4 * 4 + 2, p' `div` w4 * 4)
+	| p < wh5 = let p' = p - wh4 in (p' `mod` w2 * 2, p' `div` w2 * 4 + 2)
+	| p < wh6 = let p' = p - wh5 in (p' `mod` w2 * 2 + 1, p' `div` w2 * 2)
+	| p < wh7 = let p' = p - wh6 in (p' `mod` w, p' `div` w * 2 + 1)
 	where
-	wh64 = w * h `div` 64
-	wh32 = w * h `div` 32
-	wh16 = w * h `div` 16
-	wh8 = w * h `div` 8
-	wh4 = w * h `div` 4
-	wh2 = w * h `div` 2
-	wh = w * h
+	wh1 = (w `div'` 8) * (h `div'` 8)
+	wh2 = (w `div'` 4) * (h `div'` 8)
+	wh3 = (w `div'` 4) * (h `div'` 4)
+	wh4 = (w `div'` 2) * (h `div'` 4)
+	wh5 = (w `div'` 2) * (h `div'` 2)
+	wh6 = w * (h `div'` 2)
+	wh7 = w * h
 	w8 = w `div` 8
 	w4 = w `div` 4
 	w2 = w `div` 2
+
+a `div'` b = (a - 1) `div` b + 1
 
 drawCairoImageRgb24 :: forall m ->
 	(PrimMonad m, U.Member Pipe.P es, U.Base (U.FromFirst m) es) =>
@@ -97,6 +99,26 @@ drawCairoImageRgb24 m img w h act = ($ 0) $ fix \go p ->
 	draw :: CInt -> Word8 -> Word8 -> Word8 -> Word8 -> m ()
 	draw p a r g b = putPixel (img :: Argb32Mut (PrimState m)) (p `mod` w) (p `div` w)
 		(PixelArgb32Straight a r g b)
+
+drawCairoImageRgb24Adam7 :: forall m ->
+	(PrimMonad m, U.Member Pipe.P es, U.Base (U.FromFirst m) es) =>
+	Argb32Mut (PrimState m) -> CInt -> CInt -> Eff.E es BS.ByteString o () ->
+	Eff.E es BS.ByteString o ()
+drawCairoImageRgb24Adam7 m img w h act = ($ 0) $ fix \go p ->
+	if p < w * h
+	then do
+		cs <- byteStringToTuple3s <$> Pipe.await
+		let	ln = L.genericLength cs
+		Eff.effBase $ for_ ([p .. p + ln - 1] `zip` cs) \(p', c) ->
+			uncurry4 (draw p') $ rgbToArgb c
+		act
+		go $ p + ln
+	else pure ()
+	where
+	draw :: CInt -> Word8 -> Word8 -> Word8 -> Word8 -> m ()
+	draw p a r g b = putPixel (img :: Argb32Mut (PrimState m)) x y
+		(PixelArgb32Straight a r g b)
+		where (x, y) = calcPos w h p
 
 uncurry4 :: (a -> b -> c -> d -> e) -> (a, b, c, d) -> e
 uncurry4 f (x, y, z, w) = f x y z w
