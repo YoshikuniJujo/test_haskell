fbdb680b7a13952705c0e951db9981f1bff2b41d
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-05-29 15:29:38 +0900

    try-gzip-yaftee: app/try-zlib-decompress.hs

---
+++ themes/freer-effects/yaftee/try_using/try-gzip-yaftee/app/try-zlib-decompress.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE DataKinds #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Main where
+
+import Control.Monad
+import Control.Monad.Yaftee.Eff qualified as Eff
+import Control.Monad.Yaftee.Pipe qualified as Pipe
+import Control.Monad.Yaftee.Pipe.ByteString qualified as PipeBS
+import Control.Monad.Yaftee.Pipe.ByteString.OnDemand qualified as OnDemand
+import Control.Monad.Yaftee.Pipe.Zlib.Decompress qualified as Zlib
+import Control.Monad.Yaftee.State qualified as State
+import Control.Monad.Yaftee.Except qualified as Except
+import Control.Monad.Yaftee.Fail qualified as Fail
+import System.IO
+import System.Environment
+
+import Pipe.Huffman qualified as Huffman
+
+main :: IO ()
+main = do
+	fp : _ <- getArgs
+	h <- openFile fp ReadMode
+	void . Eff.runM . Except.run @String . Fail.run . Zlib.runNew_ @"foobar"
+		. (`State.run` Huffman.IsLiteral (const False :: Int -> Bool))
+		. (`State.run` Huffman.PhaseOthers)
+		. Pipe.run $ PipeBS.hGet 64 h Pipe.=$=
+			OnDemand.onDemand "foobar" Pipe.=$=
+			Zlib.decompressNew "foobar" 100 Pipe.=$= PipeBS.putStr
