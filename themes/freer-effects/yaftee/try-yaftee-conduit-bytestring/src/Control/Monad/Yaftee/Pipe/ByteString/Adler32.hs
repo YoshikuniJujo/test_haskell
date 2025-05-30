05b71f097c791cc6b02aa2e419dd375c2bf44589
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2025-05-30 20:49:29 +0900

    try-yaftee-conduit-bytestring: Adler32: bang

--- themes/freer-effects/yaftee/try-yaftee-conduit-bytestring/src/Control/Monad/Yaftee/Pipe/ByteString/Adler32.hs
+++ themes/freer-effects/yaftee/try-yaftee-conduit-bytestring/src/Control/Monad/Yaftee/Pipe/ByteString/Adler32.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE RequiredTypeArguments #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE BangPatterns #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Control.Monad.Yaftee.Pipe.ByteString.Adler32 (
@@ -43,7 +44,7 @@ adler32 :: forall nm -> (
 adler32 nm = ($ 5551) $ fix \go n -> do
 	bs <- Pipe.await
 	a <- State.getN nm
-	let	(n', a') = adler32Step (n, a) bs
+	let	!(n', a') = adler32Step (n, a) bs
 	State.putN nm a'
 	Pipe.yield bs
 	go n'
