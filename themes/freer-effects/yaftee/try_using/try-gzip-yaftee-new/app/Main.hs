30667cdd67b60df8e302d6a291277ea7524d7a76
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-06-03 16:27:20 +0900

    try-gzip-yaftee-new: Gzip decompress: fixed huffman

--- themes/freer-effects/yaftee/try_using/try-gzip-yaftee-new/app/Main.hs
+++ themes/freer-effects/yaftee/try_using/try-gzip-yaftee-new/app/Main.hs
@@ -1,13 +1,15 @@
 {-# LANGUAGE ImportQualifiedPost #-}
-{-# LANGUAGE BlockArguments, OverloadedStrings #-}
+{-# LANGUAGE BlockArguments, LambdaCase, OverloadedStrings #-}
 {-# LANGUAGE ExplicitForAll, TypeApplications #-}
 {-# LANGUAGE RequiredTypeArguments #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Main (main) where
 
+import GHC.TypeLits
 import Control.Arrow
 import Control.Monad
 import Control.Monad.ToolsYj
@@ -32,35 +34,91 @@ import System.IO
 import System.Environment
 
 import Control.Monad.Yaftee.Pipe.Gzip.Decompress
+import Pipe.Huffman qualified as Huffman
+import Pipe.Runlength qualified as RunLength
+
+import Data.Gzip
+import Data.Gzip.Calc
 
 main :: IO ()
 main = do
 	fp : _ <- getArgs
 	h <- openFile fp ReadMode
-	let	f = IO.print
 	void . Eff.runM . Except.run @String . Fail.runExc id
+
+			. RunLength.run_ @_ @"foobar"
+
+			. (`State.run` Huffman.PhaseOthers)
+			. (`State.run` Huffman.IsLiteral @Int (const False))
+
+			. (flip (State.runN @"foobar") $ Huffman.BitArray (BitArray.fromByteString ""))
+			. Huffman.run @"foobar" @Int
+
 			. OnDemand.run_ @"foobar"
 			. Crc.runCrc32 @"foobar"
+			. PipeLBS.lengthRun @"foobar"
 			. Pipe.run $
 		(`Except.catch` IO.putStrLn) . void $ PipeLBS.hGet 64 h Pipe.=$=
-			OnDemand.onDemand "foobar" Pipe.=$= do
-				_ <- PipeT.checkRight Pipe.=$= readHeader "foobar" f
+			gzipDecompress Pipe.=$= PipeIO.print
 
-				Crc.resetCrc32 "foobar"
-				doWhile_ block Pipe.=$= Crc.crc32 "foobar" Pipe.=$= PipeIO.print
-				Crc.compCrc32 "foobar"
+gzipDecompress :: (
+	U.Member Pipe.P es,
 
-				IO.print . Crc.crc32ToByteString =<< State.getN "foobar"
+	RunLength.Members "foobar" es,
 
-				State.putN "foobar" $ OnDemand.RequestBuffer 100
-				PipeIO.print
+	U.Member (State.S Huffman.Phase) es,
+	U.Member (State.S (Huffman.IsLiteral Int)) es,
+	U.Member (State.Named "foobar" (Huffman.BinTreePair Int)) es,
+	U.Member (State.Named "foobar" Huffman.BitArray) es,
+	U.Member (State.Named "foobar" Huffman.ExtraBits) es,
+
+	U.Member (State.Named "foobar" OnDemand.Request) es,
+	U.Member (State.Named "foobar" OnDemand.BitArray) es,
+	U.Member (State.Named "foobar" Crc.Crc32) es,
+	U.Member (State.Named "foobar" PipeLBS.Length) es,
+	U.Member (Except.E String) es,
+	U.Member Fail.F es,
+	U.Base IO.I es
+	) =>
+	Eff.E es LBS.ByteString LBS.ByteString ()
+gzipDecompress = void $ OnDemand.onDemand "foobar" Pipe.=$= do
+	_ <- PipeT.checkRight Pipe.=$= readHeader "foobar" (const $ pure ())
+
+	Crc.resetCrc32 "foobar"
+	_ <- doWhile_ block Pipe.=$=
+		RunLength.runlength "foobar" Pipe.=$=
+		PipeT.convert (either (LBS.pack . (: [])) id) Pipe.=$=
+--		PipeT.convert RunLength.toByteString Pipe.=$=
+		Crc.crc32 "foobar" Pipe.=$= PipeLBS.length "foobar"
+	Crc.compCrc32 "foobar"
+
+	crc <- State.getN "foobar"
+	ln <- State.getN "foobar"
+
+	IO.print crc
+	IO.print ln
+
+	State.putN "foobar" $ OnDemand.RequestBytes 4
+	Just crc0 <- Crc.crc32FromByteString <$> (Except.getRight @String "bad" =<< Pipe.await)
+	Just ln0 <- PipeLBS.lengthFromByteString 4 <$> (Except.getRight @String "bad" =<< Pipe.await)
+
+	when (crc /= crc0) $ Except.throw @String "CRC32 error"
+	when (ln /= ln0) $ Except.throw @String "length error"
+
+	Pipe.yield ""
 
 block :: (
 	U.Member Pipe.P es,
+	U.Member (State.S Huffman.Phase) es,
+	U.Member (State.S (Huffman.IsLiteral Int)) es,
+	U.Member (State.Named "foobar" (Huffman.BinTreePair Int)) es,
+	U.Member (State.Named "foobar" Huffman.BitArray) es,
+	U.Member (State.Named "foobar" Huffman.ExtraBits) es,
 	U.Member (State.Named "foobar" OnDemand.Request) es,
 	U.Member (Except.E String) es,
-	U.Member Fail.F es ) =>
-	Eff.E es (Either BitArray.B LBS.ByteString) LBS.ByteString Bool
+	U.Member Fail.F es
+	) =>
+	Eff.E es (Either BitArray.B LBS.ByteString) RunLength.R Bool
 block = do
 	State.putN "foobar" $ OnDemand.RequestBits 1
 	Just bf <- either
@@ -75,10 +133,87 @@ block = do
 			State.putN "foobar" $ OnDemand.RequestBytes 4
 			ln <- pairToLength =<< PipeT.skipLeft1
 			State.putN "foobar" $ OnDemand.RequestBytes ln
-			Pipe.yield =<< Except.getRight "bad" =<< Pipe.await
-		_ -> error "yet"
+			Pipe.yield . RunLength.LiteralBS =<< Except.getRight @String "bad" =<< Pipe.await
+		_	| bt == 1 -> do
+			State.putN "foobar" $ OnDemand.RequestBuffer 100
+			huffmanBits "foobar"
 
-pairToLength bs0 = fromIntegral @Word16 ln <$ do
-	when (LBS.length bs0 /= 4) $ Except.throw @String "not 4 bytes"
-	when (ln /= complement cln) $ Except.throw @String "bad pair"
-	where (ln, cln) = (LBS.toBits *** LBS.toBits) $ LBS.splitAt 2 bs0
+			State.putN "foobar" . OnDemand.RequestPushBack =<< State.getsN "foobar" Huffman.unBitArray
+			State.putN "foobar" $ Huffman.BitArray BitArray.empty
+			Right "" <- Pipe.await; pure ()
+
+		_ -> error "yet"
+	where
+	pairToLength bs0 = fromIntegral @Word16 ln <$ do
+		when (LBS.length bs0 /= 4) $ Except.throw @String "not 4 bytes"
+		when (ln /= complement cln) $ Except.throw @String "bad pair"
+		where (ln, cln) = (LBS.toBits *** LBS.toBits) $ LBS.splitAt 2 bs0
+
+huffmanBits :: forall (nm :: Symbol) -> (
+	U.Member Pipe.P es,
+	U.Member (State.S Huffman.Phase) es,
+	U.Member (State.S (Huffman.IsLiteral Int)) es,
+	U.Member (State.Named nm (Huffman.BinTreePair Int)) es,
+	U.Member (State.Named nm Huffman.BitArray) es,
+	U.Member (State.Named nm Huffman.ExtraBits) es,
+	U.Member Fail.F es
+	) =>
+	Eff.E es (Either BitArray.B LBS.ByteString) RunLength.R ()
+huffmanBits nm = void do
+	Huffman.huffman' @Int @Word16 nm Pipe.=$= do
+		let	(ht, hdt) = (
+				Huffman.makeTree [0 :: Int ..] fixedHuffmanList,
+				Huffman.makeTree [0 :: Int ..] fixedHuffmanDstList )
+
+		State.put Huffman.PhaseLitLen
+		State.put $ Huffman.IsLiteral \i -> (0 :: Int) <= i && i <= 255
+
+		Huffman.putTree nm ht
+		litLen' nm ht hdt 0
+
+litLen' :: forall (nm :: Symbol) -> (
+	U.Member Pipe.P es,
+	U.Member (State.S Huffman.Phase) es,
+	U.Member (State.Named nm (Huffman.BinTreePair Int)) es,
+	U.Member (State.Named nm Huffman.ExtraBits) es
+	) =>
+	Huffman.BinTree Int -> Huffman.BinTree Int -> Int ->
+	Eff.E es (Either Int Word16) RunLength.R ()
+litLen' nm t dt pri = Pipe.await >>= \case
+	Left 256 -> pure ()
+	Left i	| 0 <= i && i <= 255 -> do
+			Pipe.yield (RunLength.Literal $ fromIntegral i)
+			litLen' nm t dt 0
+		| 257 <= i && i <= 264 -> Huffman.putTree nm dt >> dist' nm t dt (calcLength i 0) 0
+		| 265 <= i && i <= 284 -> do
+			Huffman.putExtraBits nm $ (fromIntegral i - 261) `div` 4
+			litLen' nm t dt i
+		| i == 285 -> Huffman.putTree nm dt >> dist' nm t dt (calcLength i 0) 0
+	Right eb -> do
+		Huffman.putTree nm dt
+		dist' nm t dt (calcLength pri eb) 0
+	r -> error $ "litLen': " ++ show r
+
+dist' :: forall (nm :: Symbol) -> (
+	U.Member Pipe.P es,
+	U.Member (State.S Huffman.Phase) es,
+	U.Member (State.Named nm (Huffman.BinTreePair Int)) es,
+	U.Member (State.Named nm Huffman.ExtraBits) es
+	) =>
+	Huffman.BinTree Int -> Huffman.BinTree Int -> RunLength.Length -> Int ->
+	Eff.E es (Either Int Word16) RunLength.R ()
+dist' nm t dt ln pri = Pipe.await >>= \case
+	Left i	| 0 <= i && i <= 3 -> do
+			Pipe.yield $ RunLength.LenDist ln (calcDist i 0)
+			State.put Huffman.PhaseLitLen
+			Huffman.putTree nm t
+			litLen' nm t dt 0
+		| 4 <= i && i <= 29 -> do
+			Huffman.putExtraBits nm $ (fromIntegral i - 2) `div` 2
+			dist' nm t dt ln i
+	Right eb -> do
+		Pipe.yield (RunLength.LenDist ln (calcDist pri eb))
+		State.put Huffman.PhaseLitLen
+		Huffman.putTree nm t
+		litLen' nm t dt 0
+	r -> error $ "dist': " ++ show r
---
+++ themes/freer-effects/yaftee/try_using/try-gzip-yaftee-new/src/Pipe/Huffman.hs
@@ -0,0 +1,241 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ScopedTypeVariables, TypeApplications #-}
+{-# LANGUAGE RequiredTypeArguments #-}
+{-# LANGUAGE DataKinds, ConstraintKinds #-}
+{-# LANGUAGE KindSignatures, TypeOperators #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE PatternSynonyms, ViewPatterns #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Pipe.Huffman (
+	run, States,
+	huffman, Members, putTree, putExtraBits, makeTree,
+
+	huffman', BitArray(..),
+
+	BinTreePair, BinTree, ExtraBits(..), Pkg,
+
+	Phase(..), IsLiteral(..)
+
+	) where
+
+import GHC.TypeLits
+import Control.Arrow
+import Control.Monad
+import Control.Monad.Yaftee.Eff qualified as Eff
+import Control.Monad.Yaftee.Pipe qualified as Pipe
+import Control.Monad.Yaftee.State qualified as State
+import Control.Monad.Yaftee.Fail qualified as Fail
+import Control.HigherOpenUnion qualified as U
+import Data.TypeLevel.List
+import Data.HigherFunctor qualified as HFunctor
+import Data.Bits
+import Data.Bool
+import Data.Int
+import Data.Huffman
+import Data.ByteString.Lazy qualified as LBS
+import Data.ByteString.Bit (pattern O, pattern I)
+import Data.ByteString.Bit qualified as Bit
+import Data.ByteString.Lazy.BitArray qualified as BitArray
+
+type Pkg = "try-gzip-yaftee"
+
+data BinTreePair a = BinTreePair (BinTree a) (BinTree a)
+
+binTreePair :: (BinTree a, BinTree a) -> BinTreePair a
+binTreePair = uncurry BinTreePair
+
+unBinTreePair :: BinTreePair a -> (BinTree a, BinTree a)
+unBinTreePair (BinTreePair x y) = (x, y)
+
+step :: forall a es i o . forall nm -> U.Member (State.Named nm (BinTreePair a)) es =>
+	Bit.B -> Eff.E es i o (Maybe a)
+step nm b = State.getsN nm unBinTreePair >>= \(t0, t) -> let
+	(mr, nt) = decode1 t0 t b in
+	mr <$ State.modifyN @(BinTreePair a) nm (binTreePair . (second $ const nt) . unBinTreePair)
+
+newtype ExtraBits = ExtraBits Int64 deriving Show
+
+huffman :: forall a eb es r . forall nm -> Bits eb => (
+	U.Member Pipe.P es,
+	U.Member (State.Named nm (BinTreePair a)) es,
+	U.Member (State.Named nm ExtraBits) es,
+	U.Member Fail.F es ) =>
+	Eff.E es Bit.B (Either a eb) r
+huffman nm = State.getN nm >>= \case
+	ExtraBits 0 ->
+		(\b -> (maybe (pure ()) (Pipe.yield . Left) =<< step nm b) >> huffman nm)
+			=<< Pipe.await
+	ExtraBits n -> do
+		Pipe.yield . Right =<< takeBits16' @eb @(Either a eb) n
+		State.putN nm $ ExtraBits 0
+		huffman nm
+
+data Phase = PhaseLitLen | PhaseOthers deriving (Show, Eq)
+
+newtype IsLiteral a = IsLiteral (a -> Bool)
+
+huffman' :: forall a eb es r . forall nm -> Bits eb => (
+	U.Member Pipe.P es,
+	U.Member (State.S Phase) es,
+	U.Member (State.S (IsLiteral a)) es,
+	U.Member (State.Named nm (BinTreePair a)) es,
+	U.Member (State.Named nm ExtraBits) es,
+	U.Member (State.Named nm BitArray) es,
+	U.Member Fail.F es ) =>
+	Eff.E es (Either BitArray.B LBS.ByteString) (Either a eb) ()
+huffman' nm = State.getN nm >>= \case
+	ExtraBits 0 -> do
+		ph <- State.get
+		il <- State.get @(IsLiteral a)
+		if ph == PhaseLitLen
+			then stepNew nm a il >>= mapM_ (Pipe.yield . Left) >> huffman' nm
+			else stepNew nm a (IsLiteral $ const False) >>= mapM_ (Pipe.yield . Left) >> huffman' nm
+		{-
+			then getBuffer nm a >>= step' @a nm il >>= mapM_ (Pipe.yield . Left) >> huffman' nm
+			else getBuffer nm a >>= step' @a nm (IsLiteral $ const False) >>= mapM_ (Pipe.yield . Left) >> huffman' nm
+			-}
+	ExtraBits n -> takeBits16New nm n >>= \case
+		Nothing -> pure ()
+		Just eb -> do
+			Pipe.yield $ Right eb
+			State.putN nm $ ExtraBits 0
+			huffman' nm
+
+stepNew :: forall (nm :: Symbol) a -> (
+	U.Member Pipe.P es,
+	U.Member (State.Named nm (BinTreePair a)) es,
+	U.Member (State.Named nm BitArray) es,
+	U.Member (State.S Phase) es
+	) =>
+	IsLiteral a ->
+	Eff.E es (Either BitArray.B LBS.ByteString) o [a]
+stepNew nm a (IsLiteral p) = State.getsN nm unBinTreePair >>= \(t0, t) -> do
+	mr <- State.getsModifyN nm $ go t0 t
+	case mr of
+		Nothing -> fillBuffer nm >> stepNew nm a (IsLiteral p)
+		Just (rs, nt, b) -> rs <$ do
+			State.modifyN @(BinTreePair a) nm (binTreePair . (second $ const nt) . unBinTreePair)
+			when b $ State.put $ PhaseOthers
+{-
+	BitArray ba <- State.getN nm
+	if BitArray.null ba
+		then fillBuffer nm >> stepNew nm a (IsLiteral p)
+		else do
+			let	((rs, nt), (ba'', b)) = decodeBitArray p t0 t ba
+			rs <$ do
+				State.modifyN @(BinTreePair a) nm (binTreePair . (second $ const nt) . unBinTreePair)
+				State.putN nm (BitArray ba'')
+				when b $ State.put PhaseOthers
+				-}
+	where
+	go t0 t ba = if BitArray.null $ unBitArray ba
+		then Nothing
+		else let	((rs, nt), (ba'', b)) = decodeBitArray p t0 t $ unBitArray ba in
+				Just ((rs, nt, b), BitArray ba'')
+
+fillBuffer :: forall (nm :: Symbol) -> (
+	U.Member Pipe.P es,
+	U.Member (State.Named nm BitArray) es ) =>
+	Eff.E es (Either BitArray.B LBS.ByteString) o ()
+fillBuffer nm = State.putN nm . BitArray . either id BitArray.fromByteString =<< Pipe.await
+
+getBuffer :: forall (nm :: Symbol) a -> (
+	U.Member Pipe.P es,
+	U.Member (State.Named nm (BinTreePair a)) es,
+	U.Member (State.Named nm BitArray) es
+	) =>
+	Eff.E es (Either BitArray.B LBS.ByteString) o BitArray.B
+getBuffer nm a = do
+	BitArray ba <- State.getN nm
+	if BitArray.null ba
+		then either id BitArray.fromByteString <$> Pipe.await
+		else pure ba
+
+step' :: forall a es i o . forall nm -> (
+	U.Member (State.S Phase) es,
+	U.Member (State.Named nm BitArray) es,
+	U.Member (State.Named nm (BinTreePair a)) es
+	) => IsLiteral a ->
+	BitArray.B -> Eff.E es i o [a]
+step' nm (IsLiteral p) ba = State.getsN nm unBinTreePair >>= \(t0, t) -> let
+	((rs, nt), (ba', b)) = decodeBitArray p t0 t ba in
+	rs <$ do
+		State.modifyN @(BinTreePair a) nm (binTreePair . (second $ const nt) . unBinTreePair)
+		State.putN nm (BitArray ba')
+		when b $ State.put PhaseOthers
+
+await' :: forall nm -> (
+	U.Member Pipe.P es,
+	U.Member (State.Named nm BitArray) es ) =>
+	Eff.E es (Either BitArray.B LBS.ByteString) o (Maybe Bit.B)
+await' nm = State.getsN nm (BitArray.pop . unBitArray) >>= \case
+	Nothing -> readMore nm >>= bool (pure Nothing) (await' nm)
+	Just (b, ba) -> Just b <$ State.putN nm (BitArray ba)
+
+readMore :: forall (nm :: Symbol) -> (
+	U.Member Pipe.P es,
+	U.Member (State.Named nm BitArray) es
+	) =>
+	Eff.E es (Either BitArray.B LBS.ByteString) o Bool
+readMore nm = Pipe.awaitMaybe >>= \case
+	Nothing -> pure False
+	Just (either id BitArray.fromByteString -> ba)
+		| BitArray.null ba -> pure False
+		| otherwise -> True <$ State.modifyN nm (`appendBitArray` ba)
+
+newtype BitArray = BitArray { unBitArray :: BitArray.B } deriving Show
+
+appendBitArray :: BitArray -> BitArray.B -> BitArray
+appendBitArray (BitArray ba) ba' = BitArray $ ba `BitArray.append` ba'
+
+
+type Members nm a es = (
+	U.Member (State.Named nm (BinTreePair a)) es,
+	U.Member (State.Named nm ExtraBits) es )
+
+run :: forall nm a es i o r . (HFunctor.Loose (U.U es), Ord a) =>
+	Eff.E (States nm a `Append` es) i o r ->
+	Eff.E es i o ((r, ExtraBits), (BinTreePair a))
+run = flip (State.runN @nm) (BinTreePair bt bt) . flip (State.runN @nm) (ExtraBits 0)
+	where bt = makeTree [] ([] :: [Int])
+
+type States nm a = '[State.Named nm ExtraBits, State.Named nm (BinTreePair a)]
+
+putTree :: forall a es i o . forall nm -> U.Member (State.Named nm (BinTreePair a)) es =>
+	BinTree a -> Eff.E es i o ()
+putTree nm tr = State.putN nm (BinTreePair tr tr)
+
+putExtraBits ::
+	forall nm -> U.Member (State.Named nm ExtraBits) es => Int64 -> Eff.E es i o ()
+putExtraBits nm = State.putN nm . ExtraBits
+
+takeBits16' :: forall eb o es . Bits eb => U.Member Pipe.P es =>
+	Int64 -> Eff.E es Bit.B o eb
+takeBits16' n = bitsToWord16' <$> replicateM (fromIntegral n) Pipe.await
+
+bitsToWord16' :: Bits eb => [Bit.B] -> eb
+bitsToWord16' = foldr (\b w -> w `shiftL` 1 .|. case b of O -> zeroBits; I -> bit 0) zeroBits
+
+takeBits16New :: forall eb o es . forall (nm :: Symbol) -> (
+	U.Member Pipe.P es,
+	U.Member (State.Named nm BitArray) es,
+	Bits eb
+	) =>
+	Int64 -> Eff.E es (Either BitArray.B LBS.ByteString) o (Maybe eb)
+takeBits16New nm n = State.getsModifyN nm go >>= \case
+	Nothing -> readMore nm >>= bool (pure Nothing) (takeBits16New nm n)
+	Just eb -> pure $ Just eb
+	where
+	go ba = case BitArray.popBits n $ unBitArray ba of
+		Nothing -> Nothing
+		Just (eb, ba) -> Just (eb, BitArray ba)
+	{-
+takeBits16New nm n = State.getsN nm (BitArray.popBits n . unBitArray) >>= \case
+	Nothing -> readMore nm >>= bool (pure Nothing) (takeBits16New nm n)
+	Just (eb, ba) -> Just eb <$ State.putN nm (BitArray ba)
+	-}
+
+bitArrayToWord16 :: Bits eb => BitArray -> eb
+bitArrayToWord16 = BitArray.toBits . unBitArray
