f5bc86222e1cdbc52cad90a64f001946d368e480
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2025-06-09 09:38:19 +0900

    try-yaftee-conduit: Control.Monad.Yaftee.Pipe.Tools: length

--- themes/freer-effects/yaftee/try-yaftee-conduit/src/Control/Monad/Yaftee/Pipe/Tools.hs
+++ themes/freer-effects/yaftee/try-yaftee-conduit/src/Control/Monad/Yaftee/Pipe/Tools.hs
@@ -1,22 +1,38 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE BlockArguments, LambdaCase #-}
-{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ExplicitForAll, TypeApplications #-}
+{-# LANGUAGE RequiredTypeArguments #-}
+{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Control.Monad.Yaftee.Pipe.Tools (
 
-	convert, convert', convert'', checkRight,
+	-- * CONVERT
 
-	skipLeft1
+	convert, convert', convert'',
+
+	-- * EITHER
+
+	checkRight, skipLeft1,
+
+	-- * LENGTH
+
+	lengthRun, length, Length
 
 	) where
 
+import Prelude hiding (length)
+import Prelude qualified as P
+import Control.Monad
 import Control.Monad.Fix
 import Control.Monad.Yaftee.Eff qualified as Eff
 import Control.Monad.Yaftee.Pipe qualified as Pipe
+import Control.Monad.Yaftee.State qualified as State
 import Control.Monad.Yaftee.Except qualified as Except
 import Control.HigherOpenUnion qualified as U
+import Data.HigherFunctor qualified as HFunctor
 import Data.Bool
 
 convert :: U.Member Pipe.P effs => (a -> b) -> Eff.E effs a b r
@@ -43,3 +59,17 @@ skipLeft1 = Pipe.await >>= \case
 		Left _ -> Except.throw @String "Not Right"
 		Right x -> pure x
 	Right x -> pure x
+
+lengthRun :: forall nm es i o a . HFunctor.Loose (U.U es) =>
+	Eff.E (State.Named nm Length ': es) i o a -> Eff.E es i o (a, Length)
+lengthRun = (`State.runN` (0 :: Length))
+
+length :: forall nm -> (
+	Foldable t,
+	U.Member Pipe.P es, U.Member (State.Named nm Length) es ) =>
+	Eff.E es (t a) (t a) r
+length nm = forever $ Pipe.await >>= \s ->
+	State.modifyN nm (+ Length (P.length s)) >> Pipe.yield s
+
+newtype Length = Length { unLength :: Int }
+	deriving (Show, Eq, Ord, Enum, Num, Real, Integral)
