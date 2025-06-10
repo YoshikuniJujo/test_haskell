407c8ad3139c6865d05694a0bcbc3d70db017b7c
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-06-10 17:52:23 +0900

    try-yaftee-cairo-image-seq: Graphics.Pipe.Draw

--- themes/freer-effects/yaftee/try_using/try-yaftee-cairo-image-seq/app/Main.hs
+++ themes/freer-effects/yaftee/try_using/try-yaftee-cairo-image-seq/app/Main.hs
@@ -9,81 +9,36 @@
 
 module Main (main) where
 
-import Foreign.C.Types
 import Control.Monad
 import Control.Monad.ST
 import Control.Monad.Yaftee.Eff qualified as Eff
 import Control.Monad.Yaftee.Pipe qualified as Pipe
-import Control.Monad.Yaftee.Pipe.Tools qualified as PipeT
-import Control.Monad.Yaftee.IO qualified as IO
 import Control.HigherOpenUnion qualified as U
-import Data.Foldable
 import Data.CairoImage.Internal
-import Data.JuicyCairo
-import Codec.Picture
-import Graphics.Cairo.Drawing.CairoT
-import Graphics.Cairo.Drawing.CairoPatternT
-import Graphics.Cairo.Surfaces.ImageSurfaces
 
-import Data.Time
 import Data.Color
+
+import Graphics.Pipe.Draw
+import System.Environment
 
 main :: IO ()
 main = do
+	args <- getArgs
 	img <- newImageMut @Argb32Mut 16 16
-	writeDrawPipe "foobar.png" 16 16 examplePipe img
+	writeDrawPipe "foobar.png" img 16 16 case args of
+		["blue"] -> examplePipe2
+		_ -> examplePipe
 
-writeDrawPipe :: FilePath -> Int -> Int -> (Argb32Mut RealWorld -> IO ()) -> Argb32Mut RealWorld -> IO ()
-writeDrawPipe fp wdt hgt act img = do
-	let	img' = CairoImageMutArgb32 img
-
-	act img
-
-	sfc0 <- cairoImageSurfaceCreate CairoFormatArgb32 (fromIntegral wdt) (fromIntegral hgt)
-	cr <- cairoCreate sfc0
-
-	sfc <- CairoSurfaceTImage <$> cairoImageSurfaceCreateForCairoImageMut img'
-	ptn <- cairoPatternCreateForSurface sfc
-	cairoSetSource cr ptn
-	cairoPaint cr
-
-	cairoImageSurfaceGetCairoImageMut sfc0 >>= \case
-		CairoImageMutArgb32 i -> writeArgb32Mut fp i
-		_ -> error "bad"
-
-putPixels img [] c = pure ()
-putPixels img ((x, y) : xys) c = putPixel img x y c >> putPixels img xys c
-
-writeArgb32Mut :: FilePath -> Argb32Mut RealWorld -> IO ()
-writeArgb32Mut fp = (writePng fp =<<) . cairoArgb32MutToJuicyRGBA8
-
-example img = do
-	i <- (`mod` 5) . floor . utctDayTime <$> getCurrentTime
-	print . utctDayTime =<< getCurrentTime
-	j <- floor . (* 50) . utctDayTime <$> getCurrentTime
-	print i
-	print j
-	putPixels img
-		[ (x, y) | x <- [0 .. 15], y <- [0 .. 15] ]
-		(PixelArgb32Straight 255 0 0 0)
-	putPixels img
-		[ (x, y) | x <- [1 .. 14], y <- [1 .. 14] ]
-		(PixelArgb32Straight 255 128 128 128)
-	putPixels img
-		[ (x, y) | x <- [2 + i .. 13 - i], y <- [2 + i .. 13 - i] ]
-		(PixelArgb32Straight 255 j (255 - j) 0)
-
+examplePipe, examplePipe2 :: Argb32Mut RealWorld -> IO ()
 examplePipe img =
 	void $ Eff.runM . Pipe.run
-		$ samplePipe Pipe.=$= drawColor img
+		$ samplePipe @Double Pipe.=$= drawColor img
 
-drawColor :: (RealFrac d, U.Member Pipe.P es, U.Base IO.I es) =>
-	Argb32Mut RealWorld -> Eff.E es [Rgba d] o ()
-drawColor img = void $
-	PipeT.convert ((\(RgbaWord8 r g b a) -> PixelArgb32Straight a r g b) <$>) Pipe.=$=
-	draw img 0
+examplePipe2 img =
+	void $ Eff.runM . Pipe.run
+		$ samplePipe2 @Double Pipe.=$= drawColor img
 
-samplePipe :: (
+samplePipe, samplePipe2 :: (
 	RealFrac d,
 	U.Member Pipe.P es
 	) =>
@@ -91,13 +46,9 @@ samplePipe :: (
 samplePipe = (`mapM_` [0 .. 15]) \y -> Pipe.yield ((\x ->
 	RgbaWord8 (x * 15 + 30) (y * 15 + 30) ((225 - x * 15 `div` 2 - y * 15 `div` 2) * 2 `div` 3) 255) <$> [0 .. 15])
 
-drawLine img y ps = for_ (zip [0 ..] ps) \(x, p) -> putPixel img x y p
-
-draw :: (
-	U.Member Pipe.P es,
-	U.Base IO.I es
-	) =>
-	Argb32Mut RealWorld -> CInt -> Eff.E es [PixelArgb32] o r
-draw img y = do
-	Eff.effBase @IO . drawLine img y =<< Pipe.await
-	draw img (y + 1)
+samplePipe2 = (`mapM_` [0 .. 15]) \y -> Pipe.yield ((\x ->
+	RgbaWord8
+		((225 - x * 15 `div` 2 - y * 15 `div` 2) * 2 `div` 3)
+		(y * 15 + 30)
+		(x * 15 + 30)
+		255) <$> [0 .. 15])
---
+++ themes/freer-effects/yaftee/try_using/try-yaftee-cairo-image-seq/src/Graphics/Pipe/Draw.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE BlockArguments, LambdaCase #-}
+{-# LANGUAGE RankNTypes, TypeApplications #-}
+{-# LANGUAGE RequiredTypeArguments #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Graphics.Pipe.Draw (writeDrawPipe, drawColor) where
+
+import Foreign.C.Types
+import Control.Monad
+import Control.Monad.ST
+import Control.Monad.Yaftee.Eff qualified as Eff
+import Control.Monad.Yaftee.Pipe qualified as Pipe
+import Control.Monad.Yaftee.Pipe.Tools qualified as PipeT
+import Control.Monad.Yaftee.IO qualified as IO
+import Control.HigherOpenUnion qualified as U
+import Data.Foldable
+import Data.CairoImage.Internal
+import Data.JuicyCairo
+import Codec.Picture
+import Graphics.Cairo.Drawing.CairoT
+import Graphics.Cairo.Drawing.CairoPatternT
+import Graphics.Cairo.Surfaces.ImageSurfaces
+
+import Data.Color
+
+writeDrawPipe :: FilePath ->
+	Argb32Mut RealWorld ->
+	Int -> Int -> (Argb32Mut RealWorld -> IO ()) -> IO ()
+writeDrawPipe fp img wdt hgt act = do
+	let	img' = CairoImageMutArgb32 img
+
+	act img
+
+	sfc0 <- cairoImageSurfaceCreate CairoFormatArgb32 (fromIntegral wdt) (fromIntegral hgt)
+	cr <- cairoCreate sfc0
+
+	sfc <- CairoSurfaceTImage <$> cairoImageSurfaceCreateForCairoImageMut img'
+	ptn <- cairoPatternCreateForSurface sfc
+	cairoSetSource cr ptn
+	cairoPaint cr
+
+	cairoImageSurfaceGetCairoImageMut sfc0 >>= \case
+		CairoImageMutArgb32 i -> writeArgb32Mut fp i
+		_ -> error "bad"
+
+writeArgb32Mut :: FilePath -> Argb32Mut RealWorld -> IO ()
+writeArgb32Mut fp = (writePng fp =<<) . cairoArgb32MutToJuicyRGBA8
+
+drawColor :: (RealFrac d, U.Member Pipe.P es, U.Base IO.I es) =>
+	Argb32Mut RealWorld -> Eff.E es [Rgba d] o ()
+drawColor img = void $
+	PipeT.convert ((\(RgbaWord8 r g b a) -> PixelArgb32Straight a r g b) <$>) Pipe.=$=
+	draw img 0
+
+drawLine :: ImageMut im => im RealWorld -> CInt -> [PixelMut im] -> IO ()
+drawLine img y ps = for_ (zip [0 ..] ps) \(x, p) -> putPixel img x y p
+
+draw :: (
+	U.Member Pipe.P es,
+	U.Base IO.I es
+	) =>
+	Argb32Mut RealWorld -> CInt -> Eff.E es [PixelArgb32] o r
+draw img y = do
+	Eff.effBase @IO . drawLine img y =<< Pipe.await
+	draw img (y + 1)
