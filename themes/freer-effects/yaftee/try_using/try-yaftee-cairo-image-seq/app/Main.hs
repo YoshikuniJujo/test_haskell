dd5bfb6af0402b199a55062fa69bf49098a29805
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-06-10 17:15:00 +0900

    try-yaftee-cairo-image-seq: app/Main.hs: draw from Pipe

--- themes/freer-effects/yaftee/try_using/try-yaftee-cairo-image-seq/app/Main.hs
+++ themes/freer-effects/yaftee/try_using/try-yaftee-cairo-image-seq/app/Main.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE ImportQualifiedPost #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE BlockArguments, LambdaCase #-}
 {-# LANGUAGE RankNTypes, TypeApplications #-}
 {-# LANGUAGE RequiredTypeArguments #-}
 {-# LANGUAGE DataKinds #-}
@@ -9,7 +9,15 @@
 
 module Main (main) where
 
+import Foreign.C.Types
+import Control.Monad
 import Control.Monad.ST
+import Control.Monad.Yaftee.Eff qualified as Eff
+import Control.Monad.Yaftee.Pipe qualified as Pipe
+import Control.Monad.Yaftee.Pipe.Tools qualified as PipeT
+import Control.Monad.Yaftee.IO qualified as IO
+import Control.HigherOpenUnion qualified as U
+import Data.Foldable
 import Data.CairoImage.Internal
 import Data.JuicyCairo
 import Codec.Picture
@@ -18,11 +26,12 @@ import Graphics.Cairo.Drawing.CairoPatternT
 import Graphics.Cairo.Surfaces.ImageSurfaces
 
 import Data.Time
+import Data.Color
 
 main :: IO ()
 main = do
 	img <- newImageMut @Argb32Mut 16 16
-	writeDrawPipe "foobar.png" 16 16 example img
+	writeDrawPipe "foobar.png" 16 16 examplePipe img
 
 writeDrawPipe :: FilePath -> Int -> Int -> (Argb32Mut RealWorld -> IO ()) -> Argb32Mut RealWorld -> IO ()
 writeDrawPipe fp wdt hgt act img = do
@@ -64,4 +73,31 @@ example img = do
 		[ (x, y) | x <- [2 + i .. 13 - i], y <- [2 + i .. 13 - i] ]
 		(PixelArgb32Straight 255 j (255 - j) 0)
 
+examplePipe img =
+	void $ Eff.runM . Pipe.run
+		$ samplePipe Pipe.=$= drawColor img
 
+drawColor :: (RealFrac d, U.Member Pipe.P es, U.Base IO.I es) =>
+	Argb32Mut RealWorld -> Eff.E es [Rgba d] o ()
+drawColor img = void $
+	PipeT.convert ((\(RgbaWord8 r g b a) -> PixelArgb32Straight a r g b) <$>) Pipe.=$=
+	draw img 0
+
+samplePipe :: (
+	RealFrac d,
+	U.Member Pipe.P es
+	) =>
+	Eff.E es i [Rgba d] ()
+samplePipe = (`mapM_` [0 .. 15]) \y -> Pipe.yield ((\x ->
+	RgbaWord8 (x * 15 + 30) (y * 15 + 30) ((225 - x * 15 `div` 2 - y * 15 `div` 2) * 2 `div` 3) 255) <$> [0 .. 15])
+
+drawLine img y ps = for_ (zip [0 ..] ps) \(x, p) -> putPixel img x y p
+
+draw :: (
+	U.Member Pipe.P es,
+	U.Base IO.I es
+	) =>
+	Argb32Mut RealWorld -> CInt -> Eff.E es [PixelArgb32] o r
+draw img y = do
+	Eff.effBase @IO . drawLine img y =<< Pipe.await
+	draw img (y + 1)
