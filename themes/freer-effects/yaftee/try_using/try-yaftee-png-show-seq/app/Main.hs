6a60a5a8d21a2cefe5bb7b3de6db436395774dfd
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-06-11 10:30:17 +0900

    try-yaftee-png-show-seq: tribial

--- themes/freer-effects/yaftee/try_using/try-yaftee-png-show-seq/app/Main.hs
+++ themes/freer-effects/yaftee/try_using/try-yaftee-png-show-seq/app/Main.hs
@@ -9,10 +9,8 @@ import Control.Monad
 import Control.Monad.Yaftee.Eff qualified as Eff
 import Control.Monad.Yaftee.Pipe qualified as Pipe
 import Control.Monad.Yaftee.Pipe.Tools qualified as PipeT
-import Control.Monad.Yaftee.Pipe.IO qualified as PipeIO
 import Control.Monad.Yaftee.Pipe.ByteString qualified as PipeBS
 import Control.Monad.Yaftee.Pipe.Png.Decode qualified as Png
-import Control.Monad.Yaftee.State qualified as State
 import Control.Monad.Yaftee.Except qualified as Except
 import Control.Monad.Yaftee.Fail qualified as Fail
 import Control.Monad.Yaftee.IO qualified as IO
@@ -38,22 +36,24 @@ main = do
 	print hdr
 	let	wdt = fromIntegral $ headerWidth hdr
 		hgt = fromIntegral $ headerHeight hdr
-	print $ headerInterlaceMethod hdr
+		ilm = headerInterlaceMethod hdr
+		xss = case ilm of
+			InterlaceMethodNon -> repeat [0 ..]
 	img <- newImageArgb32Mut wdt hgt
 
-	h <- openFile fp ReadMode
+	h' <- openFile fp ReadMode
 	writeDrawPipe "foobar.png" img wdt hgt $ \img ->
 		void . Eff.runM . Except.run @String
 			. Fail.runExc id . Png.run_ @"foobar" . Pipe.run
 			. (`Except.catch` IO.print @String)
-			. void $ PipeBS.hGet 32 h Pipe.=$=
+			. void $ PipeBS.hGet 32 h' Pipe.=$=
 				PipeT.convert bsToSeq Pipe.=$=
 				Png.decode @Double "foobar"
 					(\_ -> pure ()) (\_ -> pure ()) Pipe.=$=
 				PipeT.convert (either
 					((`toRgba` AlphaWord8 255) <$>)
 					id) Pipe.=$=
-				drawColor img (repeat [0 ..])
+				drawColor img xss
 
 bsToSeq :: BS.ByteString -> Seq.Seq Word8
 bsToSeq = Seq.fromList . BS.unpack
