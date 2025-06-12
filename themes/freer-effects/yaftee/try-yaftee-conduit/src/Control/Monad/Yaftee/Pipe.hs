588268a565b38b3a26b8cf41722486e9511770f0
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-06-12 15:34:56 +0900

    try-yaftee-conduit: bang bang bang bang

--- themes/freer-effects/yaftee/try-yaftee-conduit/src/Control/Monad/Yaftee/Pipe.hs
+++ themes/freer-effects/yaftee/try-yaftee-conduit/src/Control/Monad/Yaftee/Pipe.hs
@@ -39,9 +39,9 @@ data P f i o a where
 	Await :: P f i o i
 	Yield :: forall f i o . !o -> P f i o ()
 	(:=$=) :: forall f i x o r r' .
-		f i x r -> f x o r' -> P f i o (f i x r, f x o r')
+		!(f i x r) -> !(f x o r') -> P f i o (f i x r, f x o r')
 	(:=@=) :: forall f i x o r r' .
-		f i x r -> f x o r' -> P f i o (f i x r, f x o r')
+		!(f i x r) -> !(f x o r') -> P f i o (f i x r, f x o r')
 
 isEmpty, isMore :: U.Member P es => Eff.E es i o Bool
 isEmpty = not <$> Eff.effh IsMore
