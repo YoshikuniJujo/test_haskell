e15f6a10862199a38832469366bcb2cbad6a0d5f
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-06-20 17:25:02 +0900

    try-zlib-library: Control.Monad.Yaftee.PIpe.Zlib

--- themes/freer-effects/yaftee/try_using/try-zlib-library/app/Main.hs
+++ themes/freer-effects/yaftee/try_using/try-zlib-library/app/Main.hs
@@ -10,30 +10,23 @@
 
 module Main (main) where
 
-import Foreign.Ptr
 import Foreign.C.ByteArray qualified as CByteArray
 import Control.Monad
-import Control.Monad.ToolsYj
-import Control.Monad.Primitive
 import Control.Monad.Yaftee.Eff qualified as Eff
 import Control.Monad.Yaftee.Pipe qualified as Pipe
 import Control.Monad.Yaftee.Pipe.Tools qualified as PipeT
 import Control.Monad.Yaftee.Pipe.ByteString qualified as PipeBS
-import Control.Monad.Yaftee.State qualified as State
 import Control.Monad.Yaftee.Except qualified as Except
 import Control.Monad.Yaftee.IO qualified as IO
-import Control.HigherOpenUnion qualified as U
-import Data.HigherFunctor qualified as HFunctor
 import System.IO
 import System.Environment
 
-import Codec.Compression.Zlib.Structure.Core
 import Codec.Compression.Zlib.Constant.Core
-import Codec.Compression.Zlib.Basic.Core
 import Codec.Compression.Zlib.Advanced.Core
 
 import Data.ByteString.FingerTree qualified as BSF
-import Data.ByteString.FingerTree.CString qualified as BSF
+
+import Control.Monad.Yaftee.Pipe.Zlib qualified as Zlib
 
 inputBufSize, outputBufSize :: Int
 inputBufSize = 64 * 4
@@ -50,63 +43,13 @@ main = do
 		<$> CByteArray.malloc inputBufSize
 		<*> CByteArray.malloc outputBufSize
 
-	void . Eff.runM . Except.run @ReturnCode . inflateRun @"foobar" . Pipe.run
+	void . Eff.runM . Except.run @ReturnCode . Zlib.inflateRun @"foobar" . Pipe.run
 		. (`Except.catch` IO.print @ReturnCode) . void
 		$ PipeBS.hGet readBufSize hi Pipe.=$=
 			PipeT.convert BSF.fromStrict Pipe.=$=
-			inflatePipe "foobar" IO (WindowBitsZlibAndGzip 15) i o Pipe.=$=
+			Zlib.inflate "foobar" IO (WindowBitsZlibAndGzip 15) i o Pipe.=$=
 			PipeT.convert BSF.toStrict Pipe.=$=
 			PipeBS.hPutStr ho
 
 	CByteArray.free i; CByteArray.free o
 	hClose ho; hClose hi
-
-inflateRun :: forall nm es i o r . HFunctor.Loose (U.U es) =>
-	Eff.E (State.Named nm (Maybe BSF.ByteString) ': es) i o r ->
-	Eff.E es i o (r, Maybe BSF.ByteString)
-inflateRun = (`State.runN` (Nothing :: Maybe BSF.ByteString))
-
-inflatePipe :: forall nm m -> (
-	PrimBase m,
-	U.Member Pipe.P es, U.Member (State.Named nm (Maybe BSF.ByteString)) es,
-	U.Member (Except.E ReturnCode) es, U.Base (U.FromFirst m) es ) =>
-	WindowBits -> CByteArray.B -> CByteArray.B ->
-	Eff.E es BSF.ByteString BSF.ByteString ()
-inflatePipe nm m wbs (i, ni) (o, no) = do
-	bs0 <- Pipe.await
-	((castPtr -> i0, n0), ebs0) <-
-		Eff.effBase . unsafeIOToPrim @m $ BSF.poke (castPtr i, ni) bs0
-	either (const $ pure ()) (putInput nm) ebs0
-	strm <- Eff.effBase $ streamThaw @m streamInitial {
-		streamNextIn = i0, streamAvailIn = fromIntegral n0,
-		streamNextOut = o, streamAvailOut = fromIntegral no }
-	rc0 <- Eff.effBase (inflateInit2 @m strm wbs)
-	when (rc0 /= Ok) $ Except.throw rc0
-
-	doWhile_ do
-		rc <- Eff.effBase $ inflate @m strm NoFlush
-		ai <- Eff.effBase @m $ availIn strm
-		(fromIntegral -> ao) <- Eff.effBase @m $ availOut strm
-		when (rc `notElem` [Ok, StreamEnd]) $ Except.throw rc
-		Pipe.yield =<< Eff.effBase
-			(unsafeIOToPrim @m $ BSF.peek (castPtr o, no - ao))
-		Eff.effBase . nextOut @m strm o $ fromIntegral no
-		when (rc /= StreamEnd && ai == 0) do
-			((i', n), ebs) <- Eff.effBase . unsafeIOToPrim @m
-				. BSF.poke (castPtr i, ni) =<< getInput nm
-			either (const $ pure ()) (putInput nm) ebs
-			Eff.effBase $ nextIn @m strm (castPtr i') (fromIntegral n)
-		pure $ rc /= StreamEnd
-
-	rc <- Eff.effBase (inflateEnd @m strm)
-	when (rc /= Ok) $ Except.throw rc
-
-getInput :: forall es i o . forall nm ->
-	(U.Member Pipe.P es, U.Member (State.Named nm (Maybe i)) es) =>
-	Eff.E es i o i
-getInput nm =
-	State.getN nm >>= maybe Pipe.await (<$ State.putN @(Maybe i) nm Nothing)
-
-putInput :: forall nm ->
-	(U.Member (State.Named nm (Maybe i)) es) => i -> Eff.E es i o ()
-putInput nm = State.putN nm . Just
---
+++ themes/freer-effects/yaftee/try_using/try-zlib-library/src/Control/Monad/Yaftee/Pipe/Zlib.hs
@@ -0,0 +1,85 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE BlockArguments, LambdaCase #-}
+{-# LANGUAGE ScopedTypeVariables, TypeApplications #-}
+{-# LANGUAGE RequiredTypeArguments #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Control.Monad.Yaftee.Pipe.Zlib (inflateRun, inflate) where
+
+import Foreign.Ptr
+import Foreign.C.ByteArray qualified as CByteArray
+import Control.Monad
+import Control.Monad.ToolsYj
+import Control.Monad.Primitive
+import Control.Monad.Yaftee.Eff qualified as Eff
+import Control.Monad.Yaftee.Pipe qualified as Pipe
+import Control.Monad.Yaftee.State qualified as State
+import Control.Monad.Yaftee.Except qualified as Except
+import Control.HigherOpenUnion qualified as U
+import Data.HigherFunctor qualified as HFunctor
+
+import Codec.Compression.Zlib.Structure.Core qualified as Zlib
+import Codec.Compression.Zlib.Constant.Core qualified as Zlib
+import Codec.Compression.Zlib.Basic.Core qualified as Zlib
+import Codec.Compression.Zlib.Advanced.Core qualified as Zlib
+
+import Data.ByteString.FingerTree qualified as BSF
+import Data.ByteString.FingerTree.CString qualified as BSF
+
+inflateRun :: forall nm es i o r . HFunctor.Loose (U.U es) =>
+	Eff.E (State.Named nm (Maybe ByteString) ': es) i o r ->
+	Eff.E es i o (r, Maybe ByteString)
+inflateRun = (`State.runN` (Nothing :: Maybe ByteString))
+
+newtype ByteString = ByteString { unByteString :: BSF.ByteString } deriving Show
+
+inflate :: forall nm m -> (
+	PrimBase m,
+	U.Member Pipe.P es, U.Member (State.Named nm (Maybe ByteString)) es,
+	U.Member (Except.E Zlib.ReturnCode) es, U.Base (U.FromFirst m) es ) =>
+	Zlib.WindowBits -> CByteArray.B -> CByteArray.B ->
+	Eff.E es BSF.ByteString BSF.ByteString ()
+inflate nm m wbs
+	(castPtr -> i, ni) (o@(castPtr -> o'), no@(fromIntegral -> no')) = do
+	bs0 <- Pipe.await
+	((castPtr -> i0, fromIntegral -> n0), ebs0) <-
+		Eff.effBase . unsafeIOToPrim @m $ BSF.poke (i, ni) bs0
+	either (const $ pure ()) (putInput nm ByteString) ebs0
+	strm <- Eff.effBase $ Zlib.streamThaw @m Zlib.streamInitial {
+		Zlib.streamNextIn = i0, Zlib.streamAvailIn = n0,
+		Zlib.streamNextOut = o, Zlib.streamAvailOut = no' }
+	rc0 <- Eff.effBase $ Zlib.inflateInit2 @m strm wbs
+	when (rc0 /= Zlib.Ok) $ Except.throw rc0
+
+	doWhile_ do
+		rc <- Eff.effBase $ Zlib.inflate @m strm Zlib.NoFlush
+		ai <- Eff.effBase @m $ Zlib.availIn strm
+		(fromIntegral -> ao) <- Eff.effBase @m $ Zlib.availOut strm
+		when (rc `notElem` [Zlib.Ok, Zlib.StreamEnd]) $ Except.throw rc
+		Pipe.yield =<< Eff.effBase
+			(unsafeIOToPrim @m $ BSF.peek (o', no - ao))
+		Eff.effBase $ Zlib.nextOut @m strm o no'
+		when (rc /= Zlib.StreamEnd && ai == 0) do
+			((castPtr -> i', fromIntegral -> n), ebs) <- Eff.effBase
+				. unsafeIOToPrim @m
+				. BSF.poke (i, ni) =<< getInput nm unByteString
+			either (const $ pure ()) (putInput nm ByteString) ebs
+			Eff.effBase $ Zlib.nextIn @m strm i' n
+		pure $ rc /= Zlib.StreamEnd
+
+	rc <- Eff.effBase $ Zlib.inflateEnd @m strm
+	when (rc /= Zlib.Ok) $ Except.throw rc
+
+getInput :: forall es i' i o . forall nm ->
+	(U.Member Pipe.P es, U.Member (State.Named nm (Maybe i')) es) =>
+	(i' -> i) -> Eff.E es i o i
+getInput nm un = State.getN nm
+	>>= maybe Pipe.await ((<$ State.putN @(Maybe i') nm Nothing) . un)
+
+putInput :: forall nm -> (U.Member (State.Named nm (Maybe i')) es) =>
+	(i -> i') -> i -> Eff.E es i o ()
+putInput nm to = State.putN nm . Just . to
