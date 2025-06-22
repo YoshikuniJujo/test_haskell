1ddfc03168dc1c9605ee9a80cb1098aeacdabe90
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2025-06-22 21:15:21 +0900

    try-yaftee-conduit-bytestring-ft: Control.Monad.Yaftee.Pipe.ByteString.FingerTree.OnDemand: halfway

--- themes/freer-effects/yaftee/try-yaftee-conduit-bytestring-ft/src/Control/Monad/Yaftee/Pipe/ByteString/FingerTree/OnDemand.hs
+++ themes/freer-effects/yaftee/try-yaftee-conduit-bytestring-ft/src/Control/Monad/Yaftee/Pipe/ByteString/FingerTree/OnDemand.hs
@@ -1,18 +1,37 @@
 {-# LANGUAGE ImportQualifiedPost #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE BlockArguments, LambdaCase, OverloadedStrings #-}
 {-# LANGUAGE ExplicitForAll #-}
 {-# LANGUAGE RequiredTypeArguments #-}
+{-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ViewPatterns #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Control.Monad.Yaftee.Pipe.ByteString.FingerTree.OnDemand where
 
+import Control.Monad.Fix
 import Control.Monad.Yaftee.Eff qualified as Eff
 import Control.Monad.Yaftee.Pipe qualified as Pipe
 import Control.Monad.Yaftee.State qualified as State
+import Control.Monad.Yaftee.Except qualified as Except
 import Control.HigherOpenUnion qualified as U
 import Data.Bool
 import Data.ByteString.FingerTree qualified as BSF
+
+onDemand :: forall nm -> (
+	U.Member Pipe.P es,
+	Members nm es, U.Member (Except.E String) es ) =>
+	Eff.E es BSF.ByteString BSF.ByteString r
+onDemand nm = fix \go -> State.getN nm >>= \case
+	RequestBytes ln -> takeBytes nm ln >>=
+		maybe (Except.throw errne) ((>> go) . Pipe.yield)
+	where
+	errne :: String
+	errne = "Not enough ByteString"
+
+type Members nm es = (
+	U.Member (State.Named nm Request) es,
+	U.Member (State.Named nm ByteString) es )
 
 data Request
 	= RequestBytes Int
@@ -43,6 +62,21 @@ takeBuffer nm ln = State.getsN nm unByteString >>= \bs ->
 				(pure Nothing) (BSF.null bs))
 			(takeBuffer nm ln)
 		Just (t, d) -> Just t <$ State.putN nm (ByteString d)
+
+takeString ::
+	forall nm ->
+	(U.Member Pipe.P es, U.Member (State.Named nm ByteString) es) =>
+	Eff.E es BSF.ByteString o (Maybe BSF.ByteString)
+takeString nm = State.getsN nm unByteString >>= \bs ->
+	case splitString bs of
+		Nothing -> readMore nm >>= bool (pure Nothing) (takeString nm)
+		Just (t, d) -> Just t <$ State.putN nm (ByteString d)
+
+splitString :: BSF.ByteString -> Maybe (BSF.ByteString, BSF.ByteString)
+splitString bs = case BSF.span (/= 0) bs of
+	(_, "") -> Nothing
+	(t, BSF.uncons -> Just (z, d)) -> Just (BSF.snoc t z, d)
+	_ -> error "Never occur"
 
 readMore :: forall nm -> (
 	U.Member Pipe.P es,
