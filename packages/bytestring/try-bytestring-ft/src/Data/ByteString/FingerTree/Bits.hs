73257999d9dde145dae3a633c8616cfe1af1ebba
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-06-23 17:54:11 +0900

    try-bytestring-ft: prepare Data.ByteString.FingerTree.Bits

---
+++ packages/bytestring/try-bytestring-ft/src/Data/ByteString/FingerTree/Bits.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Data.ByteString.FingerTree.Bits where
+
+-- see ~/projects/tools-yj/src/Data/ByteString/ToolsYj.hs
