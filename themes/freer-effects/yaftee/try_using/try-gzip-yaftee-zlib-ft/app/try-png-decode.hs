6627b4364f7a5930a9547b5a0a85db5c213b0cf2
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-06-24 17:59:17 +0900

    try-gzip-yaftee-zlib-ft: app/try-png-decode.hs: inflate

--- themes/freer-effects/yaftee/try_using/try-gzip-yaftee-zlib-ft/app/try-png-decode.hs
+++ themes/freer-effects/yaftee/try_using/try-gzip-yaftee-zlib-ft/app/try-png-decode.hs
@@ -5,6 +5,7 @@
 
 module Main where
 
+import Foreign.C.ByteArray qualified as CByteArray
 import Control.Monad
 import Control.Monad.Yaftee.Eff qualified as Eff
 import Control.Monad.Yaftee.Pipe qualified as Pipe
@@ -13,6 +14,7 @@ import Control.Monad.Yaftee.Pipe.IO qualified as PipeIO
 import Control.Monad.Yaftee.Pipe.ByteString qualified as PipeBS
 import Control.Monad.Yaftee.Pipe.ByteString.FingerTree.OnDemand qualified as OnDemand
 import Control.Monad.Yaftee.Pipe.Png.Decode.Header qualified as Header
+import Control.Monad.Yaftee.Pipe.Png.Decode.Chunk qualified as Chunk
 import Control.Monad.Yaftee.State qualified as State
 import Control.Monad.Yaftee.Except qualified as Except
 import Control.Monad.Yaftee.IO qualified as IO
@@ -22,17 +24,31 @@ import Data.Png.Header qualified as Header
 import System.IO
 import System.Environment
 
+import Control.Monad.Yaftee.Pipe.Zlib qualified as PipeZ
+
+import Codec.Compression.Zlib.Advanced.Core qualified as Zlib
+import Codec.Compression.Zlib.Constant.Core qualified as Zlib
+
 main :: IO ()
 main = do
 	fp : _ <- getArgs
 	h <- openFile fp ReadMode
-	void . Eff.runM . Except.run @String
+	ib <- CByteArray.malloc 64
+	ob <- CByteArray.malloc 64
+	void . Eff.runM . Except.run @String . Except.run @Zlib.ReturnCode
+		. PipeZ.inflateRun @"foobar"
+		. Chunk.chunkRun_ @"foobar"
 		. OnDemand.run @"foobar"
 		. flip (State.runN @"foobar") Header.header0
 		. Pipe.run
 		$ PipeBS.hGet 64 h Pipe.=$=
 			PipeT.convert BSF.fromStrict Pipe.=$= do
---				OnDemand.onDemand "foobar" Pipe.=$= Header.read "foobar" IO.print
---				State.putN "foobar" $ OnDemand.RequestBuffer 32
-				forever $ Pipe.yield =<< Pipe.await
+				fhdr <- Chunk.readBytes "foobar" 8
+				IO.print fhdr
+				Chunk.chunk "foobar" 500
+			Pipe.=$= do
+				OnDemand.onDemand "foobar" Pipe.=$= Header.read "foobar" IO.print
+				PipeZ.inflate "foobar" IO (Zlib.WindowBitsZlib 15) ib ob
 			Pipe.=$= PipeIO.print
+	CByteArray.free ib
+	CByteArray.free ob
---
+++ themes/freer-effects/yaftee/try_using/try-gzip-yaftee-zlib-ft/src/Control/Monad/Yaftee/Pipe/Png/Decode/Chunk.hs
@@ -0,0 +1,137 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE BlockArguments, LambdaCase, OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RequiredTypeArguments #-}
+{-# LANGUAGE DataKinds, ConstraintKinds #-}
+{-# LANGUAGE KindSignatures, TypeOperators #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Control.Monad.Yaftee.Pipe.Png.Decode.Chunk (
+
+	chunkRun_, ChunkStates,
+	chunk, ChunkMembers,
+
+	Chunk(..),
+
+	readBytes, Sequence, Crc32
+
+	) where
+
+import GHC.TypeLits
+import Control.Monad
+import Control.Monad.ToolsYj
+import Control.Monad.Fix
+import Control.Monad.Yaftee.Eff qualified as Eff
+import Control.Monad.Yaftee.Pipe qualified as Pipe
+import Control.Monad.Yaftee.Pipe.MonoTraversable.Crc32 qualified as PipeCrc32
+import Control.Monad.Yaftee.State qualified as State
+import Control.Monad.Yaftee.Except qualified as Except
+import Control.HigherOpenUnion qualified as U
+import Data.TypeLevel.List
+import Data.HigherFunctor qualified as HFunctor
+import Data.Foldable
+import Data.Bool
+import Data.Char
+import Data.Word.Crc32 qualified as Crc32
+import Data.ByteString.FingerTree qualified as BSF
+import Data.ByteString.FingerTree qualified as Seq
+import Data.Word.Word8 qualified as Seq
+
+chunkRun_ :: forall nm es i o r . HFunctor.Loose (U.U es) =>
+	Eff.E (ChunkStates nm `Append` es) i o r ->
+	Eff.E es i o ()
+chunkRun_ = void
+	. (`State.runN` Crc32 Crc32.initial)
+	. (`State.runN` Sequence BSF.empty)
+	. (`State.runN` Chunk (seqFromString "IHDR"))
+
+type ChunkStates nm =
+	'[State.Named nm Chunk, State.Named nm Sequence, State.Named nm Crc32]
+
+type ChunkMembers nm es = (
+	U.Member (State.Named nm Chunk) es,
+	U.Member (State.Named nm Sequence) es,
+	U.Member (State.Named nm Crc32) es )
+
+chunk :: forall (nm :: Symbol) -> (
+	U.Member Pipe.P es,
+	U.Member (State.Named nm Chunk) es,
+	U.Member (State.Named nm Crc32) es,
+	U.Member (State.Named nm Sequence) es,
+	U.Member (Except.E String) es
+	) =>
+	Int -> Eff.E es BSF.ByteString BSF.ByteString ()
+chunk nm = doWhile_ . chunk1 nm
+
+chunk1 :: forall (nm :: Symbol) -> (
+	U.Member Pipe.P es,
+	ChunkMembers nm es,
+	U.Member (Except.E String) es
+	) =>
+	Int -> Eff.E es BSF.ByteString BSF.ByteString Bool
+chunk1 nm m = do
+	n <- Seq.toBitsBE <$> readBytes nm 4
+	resetCrc32 nm
+	cn <- readBytes nm 4
+	State.putN nm $ Chunk cn
+--	Pipe.yield . Left $ ChunkBegin cn
+--	for_ (split m n) \n' -> Pipe.yield =<< Right <$> readBytes nm n'
+	for_ (split m n) \n' -> Pipe.yield =<< readBytes nm n'
+	compCrc32 nm
+	Crc32 crc1 <- State.getN nm
+	crc0 <- Crc32.fromWord . Seq.toBitsBE <$> readBytes nm 4
+	when (crc1 /= crc0) $ Except.throw @String "chunk1: CRC32 error"
+--	Pipe.yield . Left $ ChunkEnd cn
+	pure $ cn /= seqFromString "IEND"
+	where
+	split n = fix \go -> \case
+		0 -> []
+		m'	| n < m' -> n : go (m' - n)
+			| otherwise -> [m']
+
+seqFromString :: String -> BSF.ByteString
+seqFromString = BSF.pack . (fromIntegral . ord <$>)
+
+readBytes :: forall (nm :: Symbol) -> (
+	U.Member Pipe.P es,
+	U.Member (State.Named nm Sequence) es,
+	U.Member (State.Named nm Crc32) es,
+	U.Member (Except.E String) es ) =>
+	Int -> Eff.E es BSF.ByteString o BSF.ByteString
+readBytes nm n = State.getsN nm (Seq.splitAt' n . unSequence) >>= \case
+	Nothing -> readMore nm
+		>>= bool (Except.throw @String "no more ByteString") (readBytes nm n)
+	Just (t, d) -> t <$ do
+		State.modifyN nm $ Crc32 . (`PipeCrc32.step` t) . unCrc32
+		State.putN nm (Sequence d)
+
+resetCrc32 :: forall (nm :: Symbol) ->
+	(U.Member (State.Named nm Crc32) es) => Eff.E es i o ()
+resetCrc32 nm = State.putN nm $ Crc32 Crc32.initial
+
+compCrc32 :: forall (nm :: Symbol) ->
+	(U.Member (State.Named nm Crc32) es) => Eff.E es i o ()
+compCrc32 nm = State.modifyN nm $ Crc32 . Crc32.complement . unCrc32
+
+readMore :: forall (nm :: Symbol) -> (
+	U.Member Pipe.P es,
+	U.Member (State.Named nm Sequence) es ) =>
+	Eff.E es BSF.ByteString o Bool
+readMore nm = Pipe.awaitMaybe >>= \case
+	Nothing -> pure False
+	Just bs -> True <$ State.modifyN nm (`appendSequence` bs)
+
+newtype Sequence = Sequence { unSequence :: BSF.ByteString } deriving Show
+newtype Crc32 = Crc32 { unCrc32 :: Crc32.C } deriving Show
+
+appendSequence :: Sequence -> BSF.ByteString -> Sequence
+appendSequence (Sequence bs1) bs2 = Sequence $ bs1 `BSF.append` bs2
+
+newtype Chunk = Chunk BSF.ByteString
+
+instance Show Chunk where
+	show (Chunk s) = "(Chunk " ++ (chr . fromIntegral <$> BSF.unpack s) ++ ")"
