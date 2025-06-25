ee1e29aa24e7d0e457bf61a0c28c69ccdaf0826b
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-06-25 18:08:04 +0900

    try-bytestring-ft: test/Spec.hs

--- packages/bytestring/try-bytestring-ft/test/Spec.hs
+++ packages/bytestring/try-bytestring-ft/test/Spec.hs
@@ -1,2 +1,16 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+import Control.Monad
+import Data.Char
+import Data.ByteString qualified as BS
+import Data.ByteString.FingerTree qualified as BSF
+import System.Exit
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+	when (BSF.toStrict (foldr (BSF.:<) "" ((fromIntegral . ord) <$> (take 1000000 (cycle "abcde") :: String))) /= BS.concat (replicate 200000 "abcde")) $ exitWith (ExitFailure 1)
+	when (BSF.toStrict (foldr (BSF.:<|) "" (replicate 10000 "abcde")) /= BS.concat (replicate 10000 "abcde")) $ exitWith (ExitFailure 1)
+	putStrLn "Test suite not yet implemented"
+
+
