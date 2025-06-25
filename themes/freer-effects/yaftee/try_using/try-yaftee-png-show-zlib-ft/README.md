59b0f7994e47543e8268664030b33633953477f2
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-06-25 12:19:42 +0900

    try-yaftee-png-show-zlib-ft

---
+++ themes/freer-effects/yaftee/try_using/try-yaftee-png-show-zlib-ft/README.md
@@ -0,0 +1,1 @@
+# try-yaftee-png-show-zlib-ft
---
+++ themes/freer-effects/yaftee/try_using/try-yaftee-png-show-zlib-ft/app/Main.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE DataKinds #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Main (main) where
+
+import Control.Monad.Yaftee.Eff qualified as Eff
+import Control.Monad.Yaftee.Pipe qualified as Pipe
+import Control.Monad.Yaftee.Pipe.Tools qualified as PipeT
+import Control.Monad.Yaftee.Pipe.ByteString qualified as PipeBS
+import Control.Monad.Yaftee.Pipe.Png.Decode qualified as Png
+import Control.Monad.Yaftee.Except qualified as Except
+import Data.ByteString.FingerTree qualified as BSF
+import System.IO
+import System.Environment
+
+main :: IO ()
+main = do
+	fp : _ <- getArgs
+	h <- openFile fp ReadMode
+	Right hdr <- Eff.runM
+		. Except.run @String . Png.runHeader @"foobar" . Pipe.run
+		$ PipeBS.hGet 32 h Pipe.=$=
+			PipeT.convert BSF.fromStrict Pipe.=$=
+			Png.decodeHeader "foobar"
+	print hdr
