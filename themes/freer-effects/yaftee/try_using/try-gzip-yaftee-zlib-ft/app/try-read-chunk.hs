b232b9f7841af043100b0ae6feafb3253d259e95
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2025-07-01 22:15:32 +0900

    try-gzip-yaftee-zlib-ft: encode Chunks

--- themes/freer-effects/yaftee/try_using/try-gzip-yaftee-zlib-ft/app/try-read-chunk.hs
+++ themes/freer-effects/yaftee/try_using/try-gzip-yaftee-zlib-ft/app/try-read-chunk.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE ImportQualifiedPost #-}
-{-# LANGUAGE BlockArguments, OverloadedStrings #-}
+{-# LANGUAGE BlockArguments, LambdaCase, OverloadedStrings #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE DataKinds #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
@@ -7,6 +7,7 @@
 module Main where
 
 import Control.Monad
+import Control.Monad.Fix
 import Control.Monad.Yaftee.Eff qualified as Eff
 import Control.Monad.Yaftee.Pipe qualified as Pipe
 import Control.Monad.Yaftee.Pipe.Tools qualified as PipeT
@@ -18,29 +19,42 @@ import Control.Monad.Yaftee.Except qualified as Except
 import Control.Monad.Yaftee.IO qualified as IO
 import Data.Word
 import Data.Word.Word8 qualified as Word8
+import Data.Word.Crc32 qualified as Crc32
 import Data.ByteString.FingerTree qualified as BSF
-import Data.ByteString.FingerTree.Bits qualified as BSFB
+import Data.ByteString.FingerTree.Bits qualified as BSF
+import Data.Png qualified as Png
 import System.IO
 import System.Environment
 
 main :: IO ()
 main = do
-	fp : _ <- getArgs
+	fp : fpo : _ <- getArgs
 	h <- openFile fp ReadMode
+	ho <- openFile fpo WriteMode
 	void . Eff.runM . Except.run @String
 		. Steps.chunkRun_ @"foobar" . Pipe.run
 		. (`Except.catch` IO.putStrLn)
-		. void $ PipeBS.hGet 32 h Pipe.=$=
-			PipeT.convert BSF.fromStrict Pipe.=$=
-			Steps.chunk "foobar" Pipe.=$=
-				forever do
-					bd <- Pipe.await
+		. void $ PipeBS.hGet 32 h
+			Pipe.=$= PipeIO.debugPrint
+			Pipe.=$= PipeT.convert BSF.fromStrict Pipe.=$=
+			Steps.chunk "foobar" Pipe.=$= (fix \go -> Pipe.awaitMaybe >>= \case
+				Nothing -> Pipe.yield $ Chunk {
+					chunkName = "IEND",
+					chunkBody = "" }
+				Just bd -> do
 					Steps.Chunk nm <-
 						State.getN @Steps.Chunk "foobar"
 					Pipe.yield $ Chunk {
 						chunkName = nm,
 						chunkBody = bd }
-			Pipe.=$= PipeIO.print
+					void go)
+			Pipe.=$= do
+				Pipe.yield Png.fileHeader
+				PipeT.convert chunkToByteString
+			Pipe.=$= PipeT.convert BSF.toStrict
+			Pipe.=$= PipeIO.debugPrint
+			Pipe.=$= PipeBS.hPutStr ho
+	hClose h; hClose ho
 
 data Chunk = Chunk {
 	chunkName :: BSF.ByteString,
@@ -49,6 +63,8 @@ data Chunk = Chunk {
 
 chunkToByteString :: Chunk -> BSF.ByteString
 chunkToByteString Chunk { chunkName = nm, chunkBody = bd } =
-	BSFB.fromBits' ln
+	BSF.fromBitsBE' ln <> nmbd <> BSF.fromBitsBE' (Crc32.toWord crc)
 	where
 	ln = fromIntegral @_ @Word32 $ BSF.length bd
+	nmbd = nm <> bd
+	crc = Crc32.complement $ BSF.foldl' Crc32.step Crc32.initial nmbd
