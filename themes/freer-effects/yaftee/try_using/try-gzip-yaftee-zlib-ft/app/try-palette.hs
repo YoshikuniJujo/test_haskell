2a6e079d6db7b1f1ef3cc83eed198532e731b2f1
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-07-11 17:53:16 +0900

    try-gzip-yaftee-zlib-ft: app/try-palette.hs: prepare

---
+++ themes/freer-effects/yaftee/try_using/try-gzip-yaftee-zlib-ft/app/try-palette.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE DataKinds #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Main where
+
+import Control.Monad
+import Control.Monad.Yaftee.Eff qualified as Eff
+import Control.Monad.Yaftee.Pipe qualified as Pipe
+import Control.Monad.Yaftee.Pipe.Tools qualified as PipeT
+import Control.Monad.Yaftee.Pipe.IO qualified as PipeIO
+import Control.Monad.Yaftee.Pipe.ByteString qualified as PipeBS
+import Control.Monad.Yaftee.Pipe.Png.Decode.Steps qualified as Steps
+import Control.Monad.Yaftee.State qualified as State
+import Control.Monad.Yaftee.Except qualified as Except
+import Control.Monad.Yaftee.IO qualified as IO
+import Data.ByteString.FingerTree qualified as BSF
+import System.IO
+import System.Environment
+
+main :: IO ()
+main = do
+	fp : _ <- getArgs
+	h <- openFile fp ReadMode
+	void . Eff.runM . Except.run @String
+		. Steps.chunkRun_ @"foobar"
+		. Pipe.run
+		. void $ PipeBS.hGet 32 h
+			Pipe.=$= PipeT.convert BSF.fromStrict
+			Pipe.=$= Steps.chunk "foobar"
+			Pipe.=$= forever do
+				IO.print =<< Pipe.await
+				IO.print =<< State.getN @Steps.Chunk "foobar"
