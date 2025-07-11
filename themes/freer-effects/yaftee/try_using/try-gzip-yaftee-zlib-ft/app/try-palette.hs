2d0d341d78ca5b4861bd61294c1192e47dc21ec9
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2025-07-11 22:54:24 +0900

    try-gzip-yaftee-zlib-ft: app/try-palette.hs: readPalette

--- themes/freer-effects/yaftee/try_using/try-gzip-yaftee-zlib-ft/app/try-palette.hs
+++ themes/freer-effects/yaftee/try_using/try-gzip-yaftee-zlib-ft/app/try-palette.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE ImportQualifiedPost #-}
-{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE BlockArguments, OverloadedStrings #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ViewPatterns #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Main where
 
+import Control.Arrow
 import Control.Monad
 import Control.Monad.Yaftee.Eff qualified as Eff
 import Control.Monad.Yaftee.Pipe qualified as Pipe
@@ -16,6 +18,9 @@ import Control.Monad.Yaftee.Pipe.Png.Decode.Steps qualified as Steps
 import Control.Monad.Yaftee.State qualified as State
 import Control.Monad.Yaftee.Except qualified as Except
 import Control.Monad.Yaftee.IO qualified as IO
+import Data.MonoTraversable
+import Data.Vector qualified as V
+import Data.Word
 import Data.ByteString.FingerTree qualified as BSF
 import System.IO
 import System.Environment
@@ -31,5 +36,16 @@ main = do
 			Pipe.=$= PipeT.convert BSF.fromStrict
 			Pipe.=$= Steps.chunk "foobar"
 			Pipe.=$= forever do
-				IO.print =<< Pipe.await
-				IO.print =<< State.getN @Steps.Chunk "foobar"
+				bs <- Pipe.await
+				cnk <- State.getN @Steps.Chunk "foobar"
+				IO.print bs
+				IO.print cnk
+				when (cnk == Steps.Chunk "PLTE") .
+					IO.print $ readPalette bs
+
+data Palette = Palette (V.Vector (Word8, Word8, Word8)) deriving Show
+
+readPalette :: BSF.ByteString -> Palette
+readPalette = Palette . V.unfoldr \bs -> case BSF.splitAt' 3 bs of
+	Nothing -> Nothing
+	Just (otoList -> [r, g, b], bs') -> Just ((r, g, b), bs')
