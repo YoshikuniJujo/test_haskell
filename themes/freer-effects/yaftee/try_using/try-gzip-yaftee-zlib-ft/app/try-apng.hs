a4d810edda62c21a406d1fa1fbd95ac97a02182c
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-08-13 15:18:02 +0900

    try-gzip-yaftee-zlib-ft: app/try-apng.hs

---
+++ themes/freer-effects/yaftee/try_using/try-gzip-yaftee-zlib-ft/app/try-apng.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE DataKinds #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Main where
+
+import Control.Monad
+import Control.Monad.Yaftee.Eff qualified as Eff
+import Control.Monad.Yaftee.Pipe qualified as Pipe
+import Control.Monad.Yaftee.Pipe.Tools qualified as PipeT
+import Control.Monad.Yaftee.Pipe.ByteString qualified as PipeBS
+import Control.Monad.Yaftee.Pipe.Png.Decode qualified as Png
+import Control.Monad.Yaftee.Except qualified as Except
+import Data.ByteString.FingerTree qualified as BSF
+
+import System.IO
+import System.Environment
+
+main :: IO ()
+main = do
+	fp : _ <- getArgs
+
+	hh <- openFile fp ReadMode
+	Right hdr <- Eff.runM . Except.run @String
+		. Png.runHeader @"foobar" . Pipe.run
+		. void $ PipeBS.hGet 32 hh
+			Pipe.=$= PipeT.convert BSF.fromStrict
+			Pipe.=$= Png.decodeHeader "foobar"
+	hClose hh
+
+	print hdr
