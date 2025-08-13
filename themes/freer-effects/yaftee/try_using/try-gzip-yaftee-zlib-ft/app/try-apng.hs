0215dbe131e4d05337c2147becc8170d451fb71c
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-08-13 16:22:12 +0900

    try-gzip-yaftee-zlib-ft: app/try-apng.hs: halfway

--- themes/freer-effects/yaftee/try_using/try-gzip-yaftee-zlib-ft/app/try-apng.hs
+++ themes/freer-effects/yaftee/try_using/try-gzip-yaftee-zlib-ft/app/try-apng.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE BlockArguments, OverloadedStrings #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE DataKinds #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
@@ -9,9 +10,13 @@ import Control.Monad
 import Control.Monad.Yaftee.Eff qualified as Eff
 import Control.Monad.Yaftee.Pipe qualified as Pipe
 import Control.Monad.Yaftee.Pipe.Tools qualified as PipeT
+import Control.Monad.Yaftee.Pipe.IO qualified as PipeIO
 import Control.Monad.Yaftee.Pipe.ByteString qualified as PipeBS
 import Control.Monad.Yaftee.Pipe.Png.Decode qualified as Png
+import Control.Monad.Yaftee.Pipe.Png.Decode.Steps qualified as Steps
 import Control.Monad.Yaftee.Except qualified as Except
+import Control.Monad.Yaftee.Fail qualified as Fail
+import Control.Monad.Yaftee.IO qualified as IO
 import Data.ByteString.FingerTree qualified as BSF
 
 import System.IO
@@ -30,3 +35,19 @@ main = do
 	hClose hh
 
 	print hdr
+
+	h <- openFile fp ReadMode
+
+
+	void . Eff.runM . Except.run @String
+		. Steps.chunkRun_ @"foobar"
+		. Fail.run
+		. Pipe.run
+		. (`Except.catch` IO.putStrLn)
+		. void $ PipeBS.hGet 32 h
+			Pipe.=$= PipeT.convert BSF.fromStrict
+			Pipe.=$= Steps.chunk "foobar"
+			Pipe.=$= forever do
+		--		"" <- Pipe.await
+				bs <- Pipe.await
+				IO.print bs
