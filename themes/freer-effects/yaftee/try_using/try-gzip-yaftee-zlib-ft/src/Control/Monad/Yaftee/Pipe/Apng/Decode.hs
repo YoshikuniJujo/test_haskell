2124efbb09547d2e16d8d17cc9b89d8484a3ca9a
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-08-26 16:36:05 +0900

    try-gzip-yaftee-zlib-ft: Control.Monad.Yaftee.Pipe.Apng.Decode: export Fctl(..)

--- themes/freer-effects/yaftee/try_using/try-gzip-yaftee-zlib-ft/src/Control/Monad/Yaftee/Pipe/Apng/Decode.hs
+++ themes/freer-effects/yaftee/try_using/try-gzip-yaftee-zlib-ft/src/Control/Monad/Yaftee/Pipe/Apng/Decode.hs
@@ -8,7 +8,7 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Control.Monad.Yaftee.Pipe.Apng.Decode (
-	apngRun_, ApngStates, apngPipe, ApngMembers, Body(..) ) where
+	apngRun_, ApngStates, apngPipe, ApngMembers, Body(..), Fctl(..) ) where
 
 import Control.Arrow
 import Control.Monad
