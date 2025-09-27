e374a0e90e07cd10139b69509bc8d72a344ced8a
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2025-09-27 11:15:24 +0900

    try-gzip-yaftee-zlib-ft: app/try-apng-codec.hs: copy from app/try-apng

---
+++ themes/freer-effects/yaftee/try_using/try-gzip-yaftee-zlib-ft/app/try-apng-codec.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE BlockArguments, OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables, TypeApplications #-}
+{-# LANGUAGE RequiredTypeArguments #-}
+{-# LANGUAGE DataKinds, ConstraintKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Main (main) where
+
+import Control.Monad
+import Control.Monad.Yaftee.Eff qualified as Eff
+import Control.Monad.Yaftee.Pipe qualified as Pipe
+import Control.Monad.Yaftee.Pipe.Tools qualified as PipeT
+import Control.Monad.Yaftee.Pipe.IO qualified as PipeIO
+import Control.Monad.Yaftee.Pipe.ByteString qualified as PipeBS
+import Control.Monad.Yaftee.Pipe.Png.Decode qualified as Png
+import Control.Monad.Yaftee.Pipe.Zlib qualified as PipeZ
+import Control.Monad.Yaftee.Except qualified as Except
+import Control.Monad.Yaftee.Fail qualified as Fail
+import Control.Monad.Yaftee.IO qualified as IO
+import Data.ByteString.FingerTree qualified as BSF
+
+import System.IO
+import System.Environment
+
+import Codec.Compression.Zlib.Constant.Core qualified as Zlib
+
+import Control.Monad.Yaftee.Pipe.Apng.Decode
+
+main :: IO ()
+main = do
+	fp : _ <- getArgs
+
+	hh <- openFile fp ReadMode
+	Right hdr <- Eff.runM . Except.run @String
+		. Png.runHeader @"foobar" . Pipe.run
+		. void $ PipeBS.hGet 32 hh
+			Pipe.=$= PipeT.convert BSF.fromStrict
+			Pipe.=$= Png.decodeHeader "foobar"
+	hClose hh
+
+	print hdr
+
+	h <- openFile fp ReadMode
+	ibd <- PipeZ.cByteArrayMalloc 64
+	obd <- PipeZ.cByteArrayMalloc 64
+
+	void . Eff.runM . Except.run @String . Except.run @Zlib.ReturnCode
+		. Fail.runExc id id
+		. apngRun_ @"foobar"
+		. Pipe.run
+		. (`Fail.catch` IO.putStrLn)
+		. (`Except.catch` IO.print @Zlib.ReturnCode)
+		. (`Except.catch` IO.putStrLn)
+		. void $ PipeBS.hGet 32 h
+			Pipe.=$= PipeT.convert BSF.fromStrict
+			Pipe.=$= apngPipe "foobar" hdr ibd obd
+			Pipe.=$= PipeIO.print'
