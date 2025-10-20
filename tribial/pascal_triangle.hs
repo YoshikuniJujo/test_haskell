a7cf2a559d04ba9edd7bf4424c7420726a0f0d1a
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2025-10-20 12:08:57 +0900

    tribial

--- tribial/pascal_triangle.hs
+++ tribial/pascal_triangle.hs
@@ -13,10 +13,10 @@ middle :: Int -> String -> String
 middle n s = replicate ((n - length s) `div` 2) ' ' ++ s
 
 printAll :: Int -> [[Integer]] -> IO ()
-printAll n ns = putStrLn `mapM_` showAll 80 (take n ns)
+printAll n ns = putStrLn `mapM_` showAll 5 120 (take n ns)
 
-showAll :: Int -> [[Integer]] -> [String]
-showAll n = (showLine n <$>)
+showAll :: Int -> Int -> [[Integer]] -> [String]
+showAll m n = (showLine m n <$>)
 
-showLine :: Int -> [Integer] -> String
-showLine n = middle n . unwords . (show <$>)
+showLine :: Int -> Int -> [Integer] -> String
+showLine m n = middle n . unwords . (middle m . show <$>)
