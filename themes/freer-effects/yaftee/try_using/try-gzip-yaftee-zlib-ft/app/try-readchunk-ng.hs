5d1fa7701de851b9f28940552bace8b99468b36a
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-10-21 10:49:54 +0900

    try-gzip-yaftee-zlib-ft: Control.MOnad.Yaftee.Pipe.PngNg.Decode.Chunk

--- themes/freer-effects/yaftee/try_using/try-gzip-yaftee-zlib-ft/app/try-readchunk-ng.hs
+++ themes/freer-effects/yaftee/try_using/try-gzip-yaftee-zlib-ft/app/try-readchunk-ng.hs
@@ -9,48 +9,34 @@
 module Main (main) where
 
 import Control.Monad
-import Control.Monad.ToolsYj
-import Control.Monad.Fix
 import Control.Monad.Yaftee.Eff qualified as Eff
 import Control.Monad.Yaftee.Pipe qualified as Pipe
 import Control.Monad.Yaftee.Pipe.Tools qualified as PipeT
 import Control.Monad.Yaftee.Pipe.ByteString qualified as PipeBS
 import Control.Monad.Yaftee.Pipe.BytesCrc32 qualified as Bytes
+import Control.Monad.Yaftee.Pipe.PngNg.Decode.Chunk
+import Control.Monad.Yaftee.Pipe.Png.Encode.Chunk qualified as Chunk
 import Control.Monad.Yaftee.State qualified as State
 import Control.Monad.Yaftee.Except qualified as Except
 import Control.Monad.Yaftee.Fail qualified as Fail
 import Control.Monad.Yaftee.IO qualified as IO
 import Control.HigherOpenUnion qualified as U
-import Data.Foldable
-import Data.Word.Word8 qualified as Word8
-import Data.Word.Crc32 qualified as Crc32
 import Data.ByteString.FingerTree qualified as BSF
-import Data.Png qualified as Png
 import System.IO
 import System.Environment
-
-import Control.Monad.Yaftee.Pipe.Png.Encode.Chunk qualified as Chunk
 
 main :: IO ()
 main = do
 	fp : fpo : _ <- getArgs
-	putStrLn fp
-	h <- openFile fp ReadMode
-	ho <- openFile fpo WriteMode
+	h <- openFile fp ReadMode; ho <- openFile fpo WriteMode
 	void . Eff.runM
-		. Bytes.bytesRun_ @"foobar" . Except.run @String . Fail.run
-		. (flip (State.runN @"foobar") ("" :: BSF.ByteString))
-		. Pipe.run
-		. (`Fail.catch` IO.putStrLn)
-		. (`Except.catch` IO.putStrLn) . void $ PipeBS.hGet 32 h
+		. Bytes.bytesRun_ @"foobar"
+		. flip (State.runN @"foobar") ("" :: BSF.ByteString)
+		. Except.run @String . Fail.run . Pipe.run
+		. (`Fail.catch` IO.putStrLn) . (`Except.catch` IO.putStrLn)
+		. void $ PipeBS.hGet 32 h
 		Pipe.=$= PipeT.convert BSF.fromStrict
-		Pipe.=$= do
-			fhdr <- Bytes.readBytes "foobar" 8
-			when (fhdr /= Png.fileHeader)
-				$ Except.throw @String "file header error"
-
-			doWhile_ $ chunk1 "foobar" 10
-
+		Pipe.=$= chunks "foobar"
 		Pipe.=$= forever do
 			ChunkBegin cnm <- Pipe.await
 			bd <- chunkBody "foobar"
@@ -60,41 +46,10 @@ main = do
 		Pipe.=$= PipeBS.hPutStr ho
 	hClose h; hClose ho
 
-chunk1 :: forall nm -> (
-	U.Member Pipe.P es, Bytes.BytesMembers nm es,
-	U.Member (Except.E String) es ) =>
-	Int -> Eff.E es BSF.ByteString Chunk Bool
-chunk1 nm m = do
-	n <- Word8.toBitsBE <$> Bytes.readBytes nm 4
-	Bytes.resetCrc32 nm
-	cnm <- Bytes.readBytes nm 4
-	Pipe.yield $ ChunkBegin cnm
-	for_ (split m n) \n' -> Pipe.yield . ChunkBody =<< Bytes.readBytes nm n'
-	Bytes.compCrc32 nm
-	Bytes.Crc32 crc1 <- State.getN nm
-	crc0 <- Crc32.fromWord . Word8.toBitsBE <$> Bytes.readBytes nm 4
-	when (crc1 /= crc0) $ Except.throw @String "chunk1: CRC32 error"
-	Pipe.yield ChunkEnd
-	pure $ cnm /= "IEND"
-	where
-	split n = fix \go -> \case
-		0 -> []
-		m'	| n < m' -> n : go (m' - n)
-			| otherwise -> [m']
-
-data Chunk
-	= ChunkBegin BSF.ByteString
-	| ChunkBody BSF.ByteString | ChunkEnd deriving Show
-
 chunkBody :: forall nm -> (
-	U.Member Pipe.P es,
-	U.Member (State.Named nm BSF.ByteString) es,
-	U.Member (Except.E String) es
-	) =>
-	Eff.E es Chunk o BSF.ByteString
+	U.Member Pipe.P es, U.Member (State.Named nm BSF.ByteString) es,
+	U.Member (Except.E String) es ) => Eff.E es Chunk o BSF.ByteString
 chunkBody nm = Pipe.await >>= \case
-	ChunkBody bd -> do
-		State.modifyN nm (<> bd)
-		chunkBody nm
+	ChunkBody bd -> State.modifyN nm (<> bd) >> chunkBody nm
 	ChunkEnd -> State.getN nm <* State.putN @BSF.ByteString nm ""
 	_ -> Except.throw @String "chunkBody: not ChunkBody"
