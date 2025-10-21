9b72864348a0631f2899f8bc578e460ab1139677
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2025-10-21 18:47:12 +0900

    try-gzip-yaftee-zlib-ft: fix

--- themes/freer-effects/yaftee/try_using/try-gzip-yaftee-zlib-ft/src/System/File/Png/Gray1/NoInterlace.hs
+++ themes/freer-effects/yaftee/try_using/try-gzip-yaftee-zlib-ft/src/System/File/Png/Gray1/NoInterlace.hs
@@ -248,8 +248,8 @@ pipeDat nm m iorf hdr w h ibe obe = void $
 		Unfilter.pngFilter hdr bs0 $ Header.calcSizes hdr w h
 	Pipe.=$= PipeT.convert BSF.pack
 	Pipe.=$= PipeZ.deflate nm m sampleOptions ibe obe
-	Pipe.=$= Buffer.format' nm BSF.splitAt' "" 1000
---	Pipe.=$= Buffer.format' nm BSF.splitAt' "" 100000
+--	Pipe.=$= Buffer.format' nm BSF.splitAt' "" 1000
+	Pipe.=$= Buffer.format' nm BSF.splitAt' "" 100000
 	Pipe.=$= PipeT.convert \dt sn' ->
 		(bool ((, sn') . (Chunk "IDAT")) ((, sn' + 1) . (Chunk "fdAT" . (BSF.fromBitsBE' sn' <>))) iorf) dt
 
