d1885e436bcb232490aec0e52315178a60c23d3f
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-10-22 17:58:54 +0900

    try-gzip-yaftee-zlib-ft: app/try-decod-lifegame-gray1.hs

--- themes/freer-effects/yaftee/try_using/try-gzip-yaftee-zlib-ft/app/try-decod-lifegame-gray1.hs
+++ themes/freer-effects/yaftee/try_using/try-gzip-yaftee-zlib-ft/app/try-decod-lifegame-gray1.hs
@@ -1,13 +1,16 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE BlockArguments, LambdaCase, OverloadedStrings #-}
-{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ExplicitForAll, TypeApplications #-}
+{-# LANGUAGE RequiredTypeArguments #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ViewPatterns #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Main (main) where
 
 import Control.Monad
+import Control.Monad.ST
 import Control.Monad.Yaftee.Eff qualified as Eff
 import Control.Monad.Yaftee.Pipe qualified as Pipe
 import Control.Monad.Yaftee.Pipe.Tools qualified as PipeT
@@ -21,6 +24,8 @@ import Control.Monad.Yaftee.Pipe.Zlib qualified as PipeZ
 import Control.Monad.Yaftee.State qualified as State
 import Control.Monad.Yaftee.Except qualified as Except
 import Control.Monad.Yaftee.IO qualified as IO
+import Control.HigherOpenUnion qualified as U
+import Data.ByteString qualified as BS
 import Data.ByteString.FingerTree qualified as BSF
 import Data.Png.Header qualified as Header
 import Data.Png.Header.Data qualified as Header
@@ -35,7 +40,6 @@ import Lifegame.Words qualified as Lifegame
 main :: IO ()
 main = do
 	fp : fpo : _ <- getArgs
-
 	hh <- openFile fp ReadMode
 	Right hdr <- Eff.runM . Except.run @String . Png.runHeader @"foobar"
 		. Pipe.run . (`Except.catch` IO.putStrLn)
@@ -43,43 +47,45 @@ main = do
 		Pipe.=$= PipeT.convert BSF.fromStrict
 		Pipe.=$= Png.decodeHeader "foobar"
 	hClose hh
-
-	let	rs = (+ 1) <$> Header.headerToRows hdr
-
 	h <- openFile fp ReadMode
 	ibd <- PipeZ.cByteArrayMalloc 64
 	obd <- PipeZ.cByteArrayMalloc 64
-
 	void . Eff.runM . Except.run @String . Except.run @Zlib.ReturnCode
 		. Buffer.run @"foobar" @BSF.ByteString . PipeZ.run @"foobar"
 		. Steps.chunkRun_ @"foobar" . Pipe.run
 		. (`Except.catch` IO.putStrLn)
 		. (`Except.catch` IO.print @Zlib.ReturnCode)
 		. void $ PipeBS.hGet 32 h
-
--- DECODE
-
-		Pipe.=$= PipeT.convert BSF.fromStrict
-		Pipe.=$= Steps.chunk "foobar"
-		Pipe.=$= forever do
-			bs <- Pipe.await
-			cnk <- State.getN @Steps.Chunk "foobar"
-			when ("IDAT" `Chunk.isChunkName` cnk) $ Pipe.yield bs
-		Pipe.=$= PipeZ.inflate "foobar" IO (Zlib.WindowBitsZlib 15) ibd obd
-		Pipe.=$= Buffer.format "foobar" BSF.splitAt' "" rs
-		Pipe.=$= Unfilter.pngUnfilter' hdr
-
--- ENCODE
-
-		Pipe.=$= do
-			img <- ImageG1.generateFromBytesM
-				(fromIntegral $ Header.headerWidth hdr)
-				(fromIntegral $ Header.headerHeight hdr)
-				Pipe.await
-			Eff.effBase do
-				let	brd = Lifegame.gray1ToBoard img
-					img' n = Lifegame.boardToGray1' n brd
-				ImageG1.printAsAscii $ img' 3
-				Png.write fpo $ img' 20
-
+		Pipe.=$= pngToImageGray1 "foobar" hdr ibd obd
+		Pipe.=$= (Pipe.await >>=) \img -> Eff.effBase do
+			let	brd = Lifegame.gray1ToBoard img
+				img' n = Lifegame.boardToGray1' n brd
+			ImageG1.printAsAscii $ img' 3
+			Png.write fpo $ img' 15
+	PipeZ.cByteArrayFree ibd
+	PipeZ.cByteArrayFree obd
 	hClose h
+
+pngToImageGray1 :: forall nm -> (
+	U.Member Pipe.P es, Steps.ChunkMembers nm es,
+	U.Member (State.Named nm (Buffer.Monoid BSF.ByteString)) es,
+	U.Member (State.Named nm (Maybe PipeZ.ByteString)) es,
+	U.Member (Except.E String) es, U.Member (Except.E Zlib.ReturnCode) es,
+	U.Base IO.I es ) =>
+	Header.Header ->
+	PipeZ.CByteArray RealWorld -> PipeZ.CByteArray RealWorld ->
+	Eff.E es BS.ByteString ImageG1.G ()
+pngToImageGray1 nm hdr ibd obd = void $ PipeT.convert BSF.fromStrict
+	Pipe.=$= Steps.chunk nm
+	Pipe.=$= forever do
+		bs <- Pipe.await
+		cnk <- State.getN @Steps.Chunk nm
+		when ("IDAT" `Chunk.isChunkName` cnk) $ Pipe.yield bs
+	Pipe.=$= PipeZ.inflate nm IO (Zlib.WindowBitsZlib 15) ibd obd
+	Pipe.=$= Buffer.format nm BSF.splitAt' "" rs
+	Pipe.=$= Unfilter.pngUnfilter' hdr
+	Pipe.=$= (Pipe.yield =<< ImageG1.generateFromBytesM
+		(fromIntegral $ Header.headerWidth hdr)
+		(fromIntegral $ Header.headerHeight hdr)
+		Pipe.await)
+	where rs = (+ 1) <$> Header.headerToRows hdr
