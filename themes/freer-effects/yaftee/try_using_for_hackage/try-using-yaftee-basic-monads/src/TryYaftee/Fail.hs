edd4e6d5fac51a3f86f6852ace42f6842b2620ad
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-11-10 15:39:06 +0900

    try-using-yaftee-basic-monads: TryYaftee.Fail

---
+++ themes/freer-effects/yaftee/try_using_for_hackage/try-using-yaftee-basic-monads/src/TryYaftee/Fail.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# OPTIOnS_GHC -Wall -fno-warn-tabs #-}
+
+module TryYaftee.Fail where
+
+import Control.Monad.Yaftee.Eff qualified as Eff
+import Control.Monad.Yaftee.Except qualified as Except
+import Control.Monad.Yaftee.Fail qualified as Fail
+import Control.Monad.Yaftee.IO qualified as IO
+import Control.HigherOpenUnion qualified as U
+import Control.Exception
+
+run :: Monad m => Eff.E '[Fail.F, U.FromFirst m] i o a -> m (Either String a)
+run = Eff.runM . Fail.run
+
+runIO :: Eff.E '[Fail.F, Except.E ErrorCall, IO.I] i o a -> IO a
+runIO = Eff.runM . Except.runIO . Fail.runExc ErrorCall (\(ErrorCall str) -> str)
+
+sample0 :: MonadFail m => m ()
+sample0 = do
+	fail "foobar"
+
+catch :: (U.Member Fail.F es, U.Base IO.I es) =>
+	Eff.E es i o () -> Eff.E es i o ()
+catch = (`Fail.catch` \msg -> IO.putStrLn $ "FAIL OCCUR: " ++ msg)
