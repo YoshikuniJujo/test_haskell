684be9a5cd22ce4b49a2b585979865da9f3970b7
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-11-10 15:20:51 +0900

    try-using-yaftee-basic-monads: TryYaftee.Except

---
+++ themes/freer-effects/yaftee/try_using_for_hackage/try-using-yaftee-basic-monads/src/TryYaftee/Except.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module TryYaftee.Except where
+
+import Control.Monad.Yaftee.Eff qualified as Eff
+import Control.Monad.Yaftee.Except qualified as Except
+import Control.Monad.Yaftee.IO qualified as IO
+import Control.HigherOpenUnion qualified as U
+import Control.Exception
+
+runIO :: Exception e => Eff.E '[Except.E e, IO.I] i o a -> IO a
+runIO = Eff.runM . Except.runIO
+
+runM :: Monad m => Eff.E '[Except.E e, U.FromFirst m] i o a -> m (Either e a)
+runM = Eff.runM . Except.run
+
+type ExceptErrorCallIO = ExceptIO ErrorCall
+type ExceptIO e = '[Except.E e, IO.I]
+
+sampleIo :: (
+	U.Member (Except.E ErrorCall) es,
+	U.Base IO.I es ) =>
+	Eff.E es i o ()
+sampleIo = do
+	IO.putStrLn "Hello, world!"
+	Except.throw $ ErrorCall "Hello"
+	IO.putStrLn "Good-bye, world!"
+
+sampleCatch :: (U.Member (Except.E ErrorCall) es, U.Base IO.I es) =>
+	Eff.E es i o () -> Eff.E es i o ()
+sampleCatch = (`Except.catch` \(e :: ErrorCall) -> IO.putStrLn $ "ERROR OCCUR: " ++ show e)
