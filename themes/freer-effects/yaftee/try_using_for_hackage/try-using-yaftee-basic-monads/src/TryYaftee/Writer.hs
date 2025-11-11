b49c0aed8b0549f9fbf5021eb935c5f5b71368f0
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-11-11 12:05:05 +0900

    try-using-yaftee-basic-monads: TryYaftee.Writer

---
+++ themes/freer-effects/yaftee/try_using_for_hackage/try-using-yaftee-basic-monads/src/TryYaftee/Writer.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module TryYaftee.Writer where
+
+import Control.Monad
+import Control.Monad.Yaftee.Eff qualified as Eff
+import Control.Monad.Yaftee.Writer qualified as Writer
+import Control.Monad.Yaftee.IO qualified as IO
+import Control.HigherOpenUnion qualified as U
+
+run :: Monoid w => Eff.E '[Writer.W w, IO.I] i o r -> IO (r, w)
+run = Eff.runM . Writer.run
+
+getLines :: (U.Member (Writer.W [String]) es, U.Base IO.I es) => Eff.E es i o ()
+getLines = do
+	ln <- IO.getLine
+	when (not $ null ln) do
+		Writer.tell [ln]
+		getLines
+
