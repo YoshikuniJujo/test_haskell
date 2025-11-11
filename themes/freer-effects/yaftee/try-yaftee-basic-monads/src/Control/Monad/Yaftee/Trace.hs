7f37fe63ad9330ac5fa9224d8456843135d1be63
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-11-11 11:33:08 +0900

    try-yaftee-basic-monads: Control.Monad.Yaftee.Trace: ignore

--- themes/freer-effects/yaftee/try-yaftee-basic-monads/src/Control/Monad/Yaftee/Trace.hs
+++ themes/freer-effects/yaftee/try-yaftee-basic-monads/src/Control/Monad/Yaftee/Trace.hs
@@ -5,11 +5,18 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Control.Monad.Yaftee.Trace (T, trace, run) where
+{-# LANGUAGE TypeOperators #-}
+
+module Control.Monad.Yaftee.Trace (T, trace, run, ignore) where
 
 import Control.Monad.Yaftee.Eff qualified as Eff
 import Control.Monad.HigherFreer qualified as HFreer
 import Control.HigherOpenUnion qualified as Union
+
+import Control.Monad.HigherFreer qualified as F
+import Data.HigherFunctor qualified as HFunctor
+import Data.FTCQueue qualified as Q
+import Data.Functor.Identity
 
 type T = Union.FromFirst T_
 data T_ a where T_ :: String -> T_ ()
@@ -22,3 +29,13 @@ run = \case
 	HFreer.Pure x -> pure x
 	u HFreer.:>>= q -> case Union.extracth u of
 		Union.FromFirst (T_ s) k -> putStrLn s >> run (q HFreer.$ k ())
+
+ignore ::
+	HFunctor.Loose (Union.U es) =>
+	Eff.E (T ': es) i o r -> Eff.E es i o r
+ignore = \case
+	HFreer.Pure x -> HFreer.Pure x
+	u F.:>>= q -> case Union.decomp u of
+		Left u' -> HFunctor.map ((Identity <$>) . ignore) Identity u'
+			F.:>>= Q.singleton ((ignore F.. q) . runIdentity)
+		Right (Union.FromFirst (T_ _) k) -> ignore HFreer.. q $ k ()
