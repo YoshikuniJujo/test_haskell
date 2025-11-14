ee99823908d192d0866260d4020355d0e2524d6a
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-11-14 15:08:23 +0900

    try-using-yaftee-basic-monads: TryYaftee.Except

--- themes/freer-effects/yaftee/try_using_for_hackage/try-using-yaftee-basic-monads/src/TryYaftee/Except.hs
+++ themes/freer-effects/yaftee/try_using_for_hackage/try-using-yaftee-basic-monads/src/TryYaftee/Except.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE BlockArguments #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables, TypeApplications #-}
 {-# LANGUAGE RequiredTypeArguments, ExplicitNamespaces #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
@@ -14,27 +14,41 @@ import Control.Monad.Yaftee.IO qualified as IO
 import Control.HigherOpenUnion qualified as U
 import Control.Exception
 
+runIOSampleIO :: IO ()
+runIOSampleIO = runIO @ErrorCall sampleIO
+
+runMSampleIO :: IO (Either ErrorCall ())
+runMSampleIO = runM @_ @ErrorCall sampleIO
+
+catchSampleIO :: IO ()
+catchSampleIO = runIO @ErrorCall $ sampleCatch sampleIO
+
+runIOSampleFromIO :: IO String
+runIOSampleFromIO = runIO @SomeException sampleFromIO
+
+runMSampleFromIO :: IO (Either SomeException String)
+runMSampleFromIO = runM @_ @SomeException sampleFromIO
+
 runIO :: Exception e => Eff.E '[Except.E e, IO.I] i o a -> IO a
 runIO = Eff.runM . Except.runIO
 
 runM :: Monad m => Eff.E '[Except.E e, U.FromFirst m] i o a -> m (Either e a)
 runM = Eff.runM . Except.run
 
-type ExceptErrorCallIO = ExceptIO ErrorCall
-type ExceptIO e = '[Except.E e, IO.I]
-
-sampleIo :: (
+sampleIO :: (
 	U.Member (Except.E ErrorCall) es,
 	U.Base IO.I es ) =>
 	Eff.E es i o ()
-sampleIo = do
+sampleIO = do
 	IO.putStrLn "Hello, world!"
 	_ <- Except.throw $ ErrorCall "Hello"
 	IO.putStrLn "Good-bye, world!"
 
 sampleCatch :: (U.Member (Except.E ErrorCall) es, U.Base IO.I es) =>
 	Eff.E es i o () -> Eff.E es i o ()
-sampleCatch = (`Except.catch` \(e :: ErrorCall) -> IO.putStrLn $ "ERROR OCCUR: " ++ show e)
+sampleCatch = (`Except.catch` \(e :: ErrorCall) ->
+	IO.putStrLn $ "ERROR OCCUR: " ++ show e)
 
-sampleFromIO :: (U.Member (Except.E SomeException) es, U.Base IO.I es) => Eff.E es i o String
+sampleFromIO :: (U.Member (Except.E SomeException) es, U.Base IO.I es) =>
+	Eff.E es i o String
 sampleFromIO = Except.fromIO (type SomeException) $ readFile "foobar"
