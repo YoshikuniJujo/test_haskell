965de68e0691bad2c08326e32ee2b2e8e4e1a5fe
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-11-18 14:45:23 +0900

    try-zlib-library: app/try-gzip-deflate.hs

--- themes/freer-effects/yaftee/try_using/try-zlib-library/app/try-gzip-deflate.hs
+++ themes/freer-effects/yaftee/try_using/try-zlib-library/app/try-gzip-deflate.hs
@@ -7,44 +7,39 @@ module Main (main) where
 
 import Control.Monad
 import Control.Monad.Yaftee.Eff qualified as Eff
-import Control.Monad.Yaftee.Pipe qualified as Pipe
-import Control.Monad.Yaftee.Pipe.Tools qualified as PipeT
-import Control.Monad.Yaftee.Pipe.IO qualified as PipeIO
-import Control.Monad.Yaftee.Pipe.ByteString qualified as PipeBS
-import Control.Monad.Yaftee.Except qualified as Except
+import Control.Monad.Yaftee.Pipe qualified as Pp
+import Control.Monad.Yaftee.Pipe.Tools qualified as PpT
+import Control.Monad.Yaftee.Pipe.IO qualified as PpIO
+import Control.Monad.Yaftee.Pipe.ByteString qualified as PpBS
+import Control.Monad.Yaftee.Pipe.Zlib qualified as PpZ
+import Control.Monad.Yaftee.Except qualified as Ex
 import Control.Monad.Yaftee.IO qualified as IO
 import Data.ByteString.FingerTree qualified as BSF
--- import Data.Gzip.Header
 import System.IO
 import System.Environment
-
-import Control.Monad.Yaftee.Pipe.Zlib qualified as PipeZ
-import Codec.Compression.Zlib.Constant.Core qualified as Zlib
-import Codec.Compression.Zlib.Advanced.Core qualified as Zlib
+import Codec.Compression.Zlib.Constant.Core qualified as Z
+import Codec.Compression.Zlib.Advanced.Core qualified as Z
 
 main :: IO ()
 main = do
-	fp : _ <- getArgs
-	h <- openFile fp ReadMode
-	ib <- PipeZ.cByteArrayMalloc 64
-	ob <- PipeZ.cByteArrayMalloc 64
-	void . Eff.runM . Except.run @Zlib.ReturnCode
-		. PipeZ.run @"foobar"
-		. Pipe.run
-		. (`Except.catch` IO.print @Zlib.ReturnCode)
-		. void $ PipeBS.hGet 32 h Pipe.=$=
-			PipeT.convert BSF.fromStrict Pipe.=$= do
---				Pipe.yield . BSF.fromStrict
---					$ encodeGzipHeader sampleGzipHeader
-				PipeZ.deflate "foobar" IO sampleOptions ib ob
---				forever $ Pipe.yield =<< Pipe.await
-			Pipe.=$=
-				PipeIO.print
+	fp : as <- getArgs; h <- openFile fp ReadMode
+	let	opt = case as of
+			[] -> options $ Z.WindowBitsRaw 15
+			["zlib"] -> options $ Z.WindowBitsZlib 15
+			["gzip"] -> options $ Z.WindowBitsGzip 15
+			_ -> error "bad arguments"
+	ib <- PpZ.cByteArrayMalloc 64; ob <- PpZ.cByteArrayMalloc 64
+	_ <- Eff.runM . Ex.run @Z.ReturnCode . PpZ.run @"" . Pp.run
+		. (`Ex.catch` IO.print @Z.ReturnCode) . void $ PpBS.hGet 32 h
+			Pp.=$= PpT.convert BSF.fromStrict
+			Pp.=$= PpZ.deflate "" IO opt ib ob
+			Pp.=$= PpIO.print
+	PpZ.cByteArrayFree ib; PpZ.cByteArrayFree ob; hClose h
 
-sampleOptions :: PipeZ.DeflateOptions
-sampleOptions = PipeZ.DeflateOptions {
-	PipeZ.deflateOptionsCompressionLevel = Zlib.DefaultCompression,
-	PipeZ.deflateOptionsCompressionMethod = Zlib.Deflated,
-	PipeZ.deflateOptionsWindowBits = Zlib.WindowBitsRaw 15,
-	PipeZ.deflateOptionsMemLevel = Zlib.MemLevel 8,
-	PipeZ.deflateOptionsCompressionStrategy = Zlib.DefaultStrategy }
+options :: Z.WindowBits -> PpZ.DeflateOptions
+options wb = PpZ.DeflateOptions {
+	PpZ.deflateOptionsCompressionLevel = Z.DefaultCompression,
+	PpZ.deflateOptionsCompressionMethod = Z.Deflated,
+	PpZ.deflateOptionsWindowBits = wb,
+	PpZ.deflateOptionsMemLevel = Z.MemLevel 8,
+	PpZ.deflateOptionsCompressionStrategy = Z.DefaultStrategy }
