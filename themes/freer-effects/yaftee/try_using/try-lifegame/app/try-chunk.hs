abb6523c22e93bfb9ca39b66f59511c26195d2b6
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-11-26 17:34:46 +0900

    try-lifegame: app/try-chunk.hs: encodeRun_

--- themes/freer-effects/yaftee/try_using/try-lifegame/app/try-chunk.hs
+++ themes/freer-effects/yaftee/try_using/try-lifegame/app/try-chunk.hs
@@ -1,8 +1,9 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE BlockArguments, LambdaCase, OverloadedStrings #-}
-{-# LANGUAGE ExplicitForAll, TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables, TypeApplications #-}
 {-# LANGUAGE RequiredTypeArguments #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
@@ -20,12 +21,24 @@ import Control.Monad.Yaftee.Fail qualified as Fail
 import System.IO
 import System.Environment
 
+import Control.Monad.Yaftee.State qualified as St
+import Data.TypeLevel.List
+import Data.Word.Crc32 qualified as Crc32
+import Data.HigherFunctor qualified as F
+import Control.HigherOpenUnion qualified as U
+
 main :: IO ()
 main = do
 	fp : fpo : _ <- getArgs
 	h <- openFile fp ReadMode
 	ho <- openFile fpo WriteMode
-	void . Eff.runM . Except.run @String . Fail.run . OnDemand.run @"foo"
-		. PipeCrc32.run @"foo" . PipeCrc32.run @"bar" . Pipe.run
+	void . Eff.runM . encodeRun_ @"foo" . PipeCrc32.run @"bar"
+		. Except.run @String . Fail.run . Pipe.run
 		$ hDecode "foo" h 32 50 Pipe.=$= hEncode "bar" ho
 	hClose ho; hClose h
+
+encodeRun_ :: forall nm es i o r .
+	F.Loose (U.U es) =>
+	Eff.E (St.Named nm Crc32.C ': OnDemand.States nm `Append` es) i o r ->
+	Eff.E es i o ()
+encodeRun_ = void . OnDemand.run @nm . PipeCrc32.run @nm
