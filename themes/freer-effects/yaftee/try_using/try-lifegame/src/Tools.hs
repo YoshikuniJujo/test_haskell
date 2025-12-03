2cafcfa3265964052e4e4ff33ad747dbb602d389
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-12-03 11:23:48 +0900

    try-lifetame: tribial

--- themes/freer-effects/yaftee/try_using/try-lifegame/src/Tools.hs
+++ themes/freer-effects/yaftee/try_using/try-lifegame/src/Tools.hs
@@ -1,45 +1,30 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE BlockArguments, LambdaCase #-}
-{-# LANGUAGE ScopedTypeVariables, TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE RequiredTypeArguments #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Tools (
-
-	div', boolsToWords, pop
-
-	) where
-
-import GHC.TypeLits
+module Tools (div', boolsToWords, pop) where
 
 import Control.Monad.Yaftee.Eff qualified as Eff
 import Control.Monad.Yaftee.State qualified as State
 import Control.HigherOpenUnion qualified as U
-
 import Data.Bits
 import Data.Bool
 import Data.Word
 
-boolsToWords :: [Bool] -> [Word8]
-boolsToWords = (boolsToWord <$>) . sep 8
-
-boolsToWord :: [Bool] -> Word8
-boolsToWord = go 0 . to8
-	where
-	go r [] = r
-	go r (b : bs) = go (bool id (.|. 1) b (r `shiftL` 1)) bs
-	to8 bs = bs ++ replicate (8 - Prelude.length bs) False
-
-sep :: Int -> [a] -> [[a]]
-sep _ [] = []
-sep n xs = take n xs : sep n (drop n xs)
-
 div' :: Integral n => n -> n -> n
 m `div'`n = (m - 1) `div` n + 1
 
-pop :: forall (nm :: Symbol) ->
-	(U.Member (State.Named nm [a]) es) => Eff.E es i o (Maybe a)
+boolsToWords :: [Bool] -> [Word8]
+boolsToWords = (btw 0 . to8 <$>) . sep 8
+	where
+	btw r = \case
+		[] -> r
+		b : bs -> btw (bool id (.|. 1) b (r `shiftL` 1)) bs
+	to8 bs = bs ++ replicate (8 - length bs) False
+	sep n = \case [] -> []; xs -> take n xs : sep n (drop n xs)
+
+pop :: forall nm -> (U.Member (State.Named nm [a]) es) => Eff.E es i o (Maybe a)
 pop nm = State.getsModifyN nm \case [] -> Nothing; x : xs -> Just (x, xs)
