75a2c5ca7cbeec37a3d3544dd0d30a97cab53ef5
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-12-17 11:37:34 +0900

    try-use-yaftee: Data.Png

---
+++ tribial/try-use-yaftee/src/Data/Png.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE BlockArguments, LambdaCase, OverloadedStrings #-}
+{-# LANGUAGE ExplicitForAll, TypeApplications #-}
+{-# LANGUAGE RequiredTypeArguments #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Data.Png (
+
+	module Data.Png.Header,
+	module Data.Png.Filter,
+
+	Datable(..),
+
+	) where
+
+import Data.ByteString.FingerTree qualified as BSF
+import Data.Png.Header
+import Data.Png.Filter
+
+class Datable a where
+	isDat :: a -> Bool; endDat :: a -> Bool
+	toDat :: Data.Png.Header.Header -> a -> BSF.ByteString
