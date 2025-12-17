a09177017639d043ebc388a439443476aab17072
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2025-12-18 05:47:52 +0900

    tribial

--- themes/freer-effects/yaftee/try_using/try-lifegame-simple/src/Lifegame/Png/Filter.hs
+++ themes/freer-effects/yaftee/try_using/try-lifegame-simple/src/Lifegame/Png/Filter.hs
@@ -20,7 +20,7 @@ import Lifegame.Tools
 
 filter :: (U.Member Pipe.P es, U.Member (Except.E String) es) =>
 	Png.Header -> [Size] -> Eff.E es BSF.ByteString [Word8] ()
-filter hdr ss = (\bs -> filterRaw hdr bs ss) =<< Pipe.await
+filter hdr ss = flip (filterRaw hdr) ss =<< Pipe.await
 
 type Size = (Int, Int)
 
@@ -45,9 +45,10 @@ filterRaw hdr r0 = \case
 
 filterTail :: (U.Member Pipe.P es, U.Member (Except.E String) es) =>
 	Int -> [Word8] -> Int -> Eff.E es BSF.ByteString [Word8] ()
-filterTail _ _ 0 = pure ()
-filterTail bpp pr n = Pipe.awaitMaybe >>= \case
-	Nothing -> pure (); Just BSF.Empty -> pure ()
-	Just bs -> do
-		Pipe.yield $ Png.filter bpp pr bs
-		filterTail bpp (BSF.unpack bs) (n - 1)
+filterTail bpp pr = \case
+	0 -> pure ()
+	n -> Pipe.awaitMaybe >>= \case
+		Nothing -> pure (); Just BSF.Empty -> pure ()
+		Just bs -> do
+			Pipe.yield $ Png.filter bpp pr bs
+			filterTail bpp (BSF.unpack bs) (n - 1)
