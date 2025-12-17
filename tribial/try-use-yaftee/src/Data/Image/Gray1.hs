f35a18a3723c6fc9842ffc10d8ce88e86d01ed9b
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-12-17 10:52:54 +0900

    try-use-yaftee: Data.Image.Gray1

---
+++ tribial/try-use-yaftee/src/Data/Image/Gray1.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Data.Image.Gray1 where
+
+import Data.Vector qualified as V
+import Data.Word
+import Lifegame.Tools
+
+data G = G { width :: Int, height :: Int, body :: V.Vector Word8 } deriving Show
+
+unconsRow :: G -> Maybe (V.Vector Word8, G)
+unconsRow G { height = h } | h < 1 = Nothing
+unconsRow G { width = w, height = h, body = bd } = Just
+	(V.take w' bd, G { width = w, height = h - 1, body = V.drop w' bd })
+	where w' = w `div'` 8
