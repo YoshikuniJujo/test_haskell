75930112958890db0288943d48641ffa67269f62
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-12-17 12:03:11 +0900

    try-lifegame-simple: Lifegame.Png.Filter

--- themes/freer-effects/yaftee/try_using/try-lifegame-simple/src/Lifegame/Png/Filter.hs
+++ themes/freer-effects/yaftee/try_using/try-lifegame-simple/src/Lifegame/Png/Filter.hs
@@ -5,7 +5,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Lifegame.Png.Filter (filter, unfilter, Size) where
+module Lifegame.Png.Filter (filter, Size) where
 
 import Prelude hiding (filter)
 import Control.Monad
@@ -15,26 +15,33 @@ import Control.Monad.Yaftee.Except qualified as Except
 import Control.HigherOpenUnion qualified as U
 import Data.Word
 import Data.ByteString.FingerTree qualified as BSF
-import Data.Png.Header qualified as Header
-import Data.Png.Filter qualified as Filter
+import Data.Png qualified as Png
+import Lifegame.Tools
+
+filter :: (U.Member Pipe.P es, U.Member (Except.E String) es) =>
+	Png.Header -> [Size] -> Eff.E es BSF.ByteString [Word8] ()
+filter hdr ss = (\bs -> filterRaw hdr bs ss) =<< Pipe.await
 
 type Size = (Int, Int)
 
-filter :: (U.Member Pipe.P es, U.Member (Except.E String) es) =>
-	Header.Header -> [Size] -> Eff.E es BSF.ByteString [Word8] ()
-filter hdr ss = (\bs -> filterRaw hdr bs ss) =<< Pipe.await
-
 filterRaw :: (U.Member Pipe.P es, U.Member (Except.E String) es) =>
-	Header.Header ->
+	Png.Header ->
 	BSF.ByteString -> [Size] -> Eff.E es BSF.ByteString [Word8] ()
-filterRaw _ _ [] = pure ()
-filterRaw hdr r0 ((w, h) : ss) = void do
-	Pipe.yield $ Filter.filter bpp (rowBytes hdr w `replicate` 0) r0
-	filterTail bpp (BSF.unpack r0) (h - 1)
-	case ss of
-		[] -> pure ()
-		_ -> (\bs -> filterRaw hdr bs ss) =<< Pipe.await
-	where bpp = headerToBytesPerPixel hdr
+filterRaw hdr r0 = \case
+	[] -> pure ()
+	(w, h) : ss -> void do
+		Pipe.yield $ Png.filter bpp (rowBytes w `replicate` 0) r0
+		filterTail bpp (BSF.unpack r0) (h - 1)
+		case ss of
+			[] -> pure ()
+			_ -> (\bs -> filterRaw hdr bs ss) =<< Pipe.await
+	where
+	bpp = (fromIntegral (Png.bitDepth hdr) *
+			Png.colorTypeSampleNum (Png.colorType hdr)) `div'` 8
+	rowBytes wdt = ((wdt * bd - 1) * sn) `div'` 8
+		where
+		bd = fromIntegral $ Png.bitDepth hdr
+		sn = Png.colorTypeSampleNum $ Png.colorType hdr
 
 filterTail :: (U.Member Pipe.P es, U.Member (Except.E String) es) =>
 	Int -> [Word8] -> Int -> Eff.E es BSF.ByteString [Word8] ()
@@ -42,38 +49,5 @@ filterTail _ _ 0 = pure ()
 filterTail bpp pr n = Pipe.awaitMaybe >>= \case
 	Nothing -> pure (); Just BSF.Empty -> pure ()
 	Just bs -> do
-		Pipe.yield $ Filter.filter bpp pr bs
+		Pipe.yield $ Png.filter bpp pr bs
 		filterTail bpp (BSF.unpack bs) (n - 1)
-
-unfilter :: (U.Member Pipe.P es, U.Member (Except.E String) es) =>
-	Header.Header -> Eff.E es BSF.ByteString [Word8] ()
-unfilter hdr = void $ Pipe.await >>= \bs ->
-	(>>) <$> Pipe.yield <*> unfilterTail bpp =<< either Except.throw pure
-		(Filter.unfilter bpp (replicate rbs 0) bs)
-	where bpp = headerToBytesPerPixel hdr; rbs = headerToRowBytes hdr
-
-unfilterTail :: (U.Member Pipe.P es, U.Member (Except.E String) es) =>
-	Int -> [Word8] -> Eff.E es BSF.ByteString [Word8] ()
-unfilterTail bpp prior = Pipe.awaitMaybe >>= \case
-	Nothing -> pure (); Just BSF.Empty -> pure ()
-	Just bs -> (>>) <$> Pipe.yield <*> unfilterTail bpp
-		=<< either Except.throw pure (Filter.unfilter bpp prior bs)
-
-headerToBytesPerPixel :: Integral n => Header.Header -> n
-headerToBytesPerPixel hdr =
-	(fromIntegral (Header.bitDepth hdr) *
-		Header.colorTypeSampleNum (Header.colorType hdr)) `div'` 8
-
-headerToRowBytes :: Integral n => Header.Header -> n
-headerToRowBytes hdr@Header.Header { Header.width = w, Header.height = h } =
-	(fromIntegral w * fromIntegral h * sampleNum hdr) `div'` 8
-
-rowBytes :: Integral n => Header.Header -> n -> n
-rowBytes hdr wdt = ((wdt * bd - 1) * sampleNum hdr) `div'` 8
-	where bd = fromIntegral $ Header.bitDepth hdr
-
-sampleNum :: Integral n => Header.Header -> n
-sampleNum = Header.colorTypeSampleNum . Header.colorType
-
-div' :: Integral n => n -> n -> n
-a `div'` b = (a - 1) `div` b + 1
