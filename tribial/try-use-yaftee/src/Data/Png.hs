5827e69db017c349567e077e3f43df1197230a8a
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-12-19 15:24:53 +0900

    try-use-yaftee: System.File.Png.Gray1.NoInterlace

--- tribial/try-use-yaftee/src/Data/Png.hs
+++ tribial/try-use-yaftee/src/Data/Png.hs
@@ -11,13 +11,22 @@ module Data.Png (
 
 	module Data.Png.Header,
 
-	Datable(..),
+	Datable(..), PixelsGray1(..)
 
 	) where
 
+import Data.Word
 import Data.ByteString.FingerTree qualified as BSF
 import Data.Png.Header
 
 class Datable a where
 	isDat :: a -> Bool; endDat :: a -> Bool
 	toDat :: Data.Png.Header.Header -> a -> BSF.ByteString
+
+-- FCTL PIXEL GRAY1
+
+data PixelsGray1 = PG1Pixels [Word8] deriving Show
+
+instance Datable PixelsGray1 where
+	isDat (PG1Pixels _) = True; endDat (PG1Pixels _) = False
+	toDat _ (PG1Pixels bs) = BSF.pack bs
--- tribial/try-use-yaftee/src/Lifegame/Tools.hs
+++ tribial/try-use-yaftee/src/Lifegame/Tools.hs
@@ -4,3 +4,6 @@ module Lifegame.Tools where
 
 div' :: Integral n => n -> n -> n
 a `div'` b = (a - 1) `div` b + 1
+
+times_ :: (Integral n, Monad m) => n -> m a -> m ()
+n `times_` act | n < 1 = pure () | otherwise = act >> (n - 1) `times_` act
