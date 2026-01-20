7a61a033fcef5978cb1fb427c8632f045a4f67c6
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-01-20 14:15:15 +0900

    try-hello-nostr: SHA-256

--- tribial/try_nostr/try-hello-nostr/app/Main.hs
+++ tribial/try_nostr/try-hello-nostr/app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables, TypeApplications #-}
 {-# LANGUAGE ViewPatterns #-}
@@ -6,10 +7,18 @@
 module Main (main) where
 
 import Data.Foldable
-import Data.Text
+import Data.List qualified as L
+import Data.Char
+import Data.ByteString.Char8 qualified as BSC
+import Data.ByteString.UTF8 qualified as BSU
+import Data.Text qualified as T
+import Data.Scientific
 import Data.Aeson
+import Data.Aeson.KeyMap qualified as A
+import Numeric
 import Wuss
 import Network.WebSockets
+import Crypto.Hash.SHA256
 
 main :: IO ()
 main = runSecureClient "nos.lol" 443 "/" ws
@@ -18,10 +27,73 @@ ws :: ClientApp ()
 ws cnn = do
 	putStrLn "Connected!"
 
-	sendTextData cnn ("[\"REQ\", \"foobar12345\", { \"kinds\": [1] }]" :: Text)
+	sendTextData cnn ("[\"REQ\", \"foobar12345\", { \"kinds\": [1] }]" :: T.Text)
 
-	(Just (Array (toList -> [_, _, obj])) :: Maybe Value) <- decode <$> receiveData cnn
+	(Just (Array (toList -> [_, _, Object obj])) :: Maybe Value) <- decode <$> receiveData cnn
 
 	print obj
 
-	sendClose cnn ("Bye!" :: Text)
+	putStrLn "\n*** FOR SERIALIZE ***\n"
+
+	let	Just (String pk) = A.lookup "pubkey" obj
+		Just crat = fromScientific <$> A.lookup "created_at" obj
+		Just knd = fromScientific <$> A.lookup "kind" obj
+		Just tgs = A.lookup "tags" obj
+		Just idnt = A.lookup "id" obj
+--		Just cnt = escape <$> A.lookup "content" obj
+		Just cnt = escape <$> A.lookup "content" obj
+
+	print pk
+	print crat
+	print knd
+	print tgs
+	print cnt
+
+	putStrLn ""
+	let	srzd = serialize pk crat knd tgs cnt
+	putStrLn srzd
+	putStrLn . strToHexStr . BSC.unpack . hash $ BSU.fromString srzd
+	print idnt
+
+	sendClose cnn ("Bye!" :: T.Text)
+
+escape, escape' :: Value -> T.Text
+escape (String txt) = esc txt
+	where
+	esc "" = ""
+	esc ('\n' T.:< ts) = "\\n" <> esc ts
+	esc ('"' T.:< ts) = "\\\"" <> esc ts
+	esc ('\\' T.:< ts) = "\\\\" <> esc ts
+	esc ('\r' T.:< ts) = "\\r" <> esc ts
+	esc ('\t' T.:< ts) = "\\t" <> esc ts
+	esc ('\b' T.:< ts) = "\\b" <> esc ts
+	esc ('\f' T.:< ts) = "\\f" <> esc ts
+	esc (c T.:< ts) = c T.:< esc ts
+escape _ = error "bad"
+
+escape' (String txt) = txt
+
+fromScientific :: Value -> Int
+fromScientific (Number s) = round s
+fromScientific _ = error "bad"
+
+serialize :: T.Text -> Int -> Int -> Value -> T.Text -> String
+serialize pk crat knd tgs cnt = "[0," ++
+	show pk ++ "," ++ show crat ++ "," ++ show knd ++ "," ++
+	serializeTags tgs ++ "," ++ "\"" ++ T.unpack cnt ++ "\"]"
+
+serializeTags :: Value -> String
+serializeTags (Array a) = '[' :
+	L.intercalate "," (serializeStrArray <$> toList a) ++
+	"]"
+serializeTags _ = error "bad"
+
+serializeStrArray :: Value -> String
+serializeStrArray (Array a) = '[' :
+	L.intercalate "," ((\(String s) -> show s) <$> toList a) ++ "]"
+serializeStrArray _ = error "bad"
+
+strToHexStr :: String -> String
+strToHexStr = concat . (sh <$>) . map ord
+	where
+	sh n = let s = showHex n "" in replicate (2 - length s) '0' ++ s
