f0944b85c7a6206b065f73cfac25f686a07ad46b
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-02-12 15:40:03 +0900

    try-nostr-event-ng: tribial

---
+++ themes/sns/nostr/try-nostr-event-ng/src/Bech32.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Bech32 where
+
+import Control.Monad
+import Data.Text qualified as T
+import Codec.Binary.Bech32
+import Data.ByteString qualified as BS
+import Data.Text.Encoding
+
+input :: T.Text
+input = "example1f3hhyetdyp5hqum4d5sxgmmvdaezqumfwssxzmt9wsss9un3cx"
+
+dataPart :: T.Text -> Maybe BS.ByteString
+dataPart b = let Right (_, dataPartToBytes -> d) = decode b in d
+
+dataPart' :: T.Text -> T.Text -> Maybe BS.ByteString
+dataPart' tg0 b = let Right (humanReadablePartToText -> tg, dataPartToBytes -> d) = decode b in do
+	guard $ tg == tg0
+	d
--- themes/sns/nostr/try-nostr-event-ng/src/Nostr/Event.hs
+++ themes/sns/nostr/try-nostr-event-ng/src/Nostr/Event.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Nostr.Event where
 
 import Data.Text qualified as T
+import Data.Text.IO qualified as T
 import Data.UnixTime
 import Crypto.Curve.Secp256k1
+
+import Bech32
 
 data E = E {
 	pubkey :: Pub,
@@ -14,3 +18,18 @@ data E = E {
 	tags :: [(T.Text, (T.Text, [T.Text]))],
 	content :: T.Text }
 	deriving Show
+
+sample :: FilePath -> IO E
+sample fp = do
+	Just pub <- dataPart . chomp <$> T.readFile fp
+	Just pk <- pure $ parse_point pub
+	ut <- getUnixTime
+	pure E {
+		pubkey = pk,
+		created_at = ut,
+		kind = 1,
+		tags = [],
+		content = "Hello" }
+
+chomp :: T.Text -> T.Text
+chomp t = if T.last t == '\n' then T.init t else t
