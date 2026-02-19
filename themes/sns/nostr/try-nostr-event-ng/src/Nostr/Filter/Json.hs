b5874fbf584d4687025a9e307e8f0f0394c4fcb9
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-02-19 16:08:34 +0900

    try-nostr-event-ng: Nostr.Filter.Json: encode

--- themes/sns/nostr/try-nostr-event-ng/src/Nostr/Filter/Json.hs
+++ themes/sns/nostr/try-nostr-event-ng/src/Nostr/Filter/Json.hs
@@ -4,16 +4,35 @@
 
 module Nostr.Filter.Json where
 
+import Foreign.C.Types
 import Data.Maybe
 import Data.Vector qualified as V
+import Data.Text qualified as T
+import Data.String
 import Data.Aeson qualified as A
 import Data.Aeson.Types qualified as A
 import Data.Aeson.KeyMap qualified as A
+import Data.UnixTime
+import Crypto.Curve.Secp256k1
 import Nostr.Filter qualified as Filter
 
 import Tools
 
 encode :: Filter.Filter -> A.Value
 encode f = A.Object . A.fromList $ catMaybes [
-	("ids" ,) . A.Array . V.fromList . (A.String . bsToHexText <$>) <$> Filter.ids f
-	]
+	("ids" ,) . A.Array . V.fromList
+		. (A.String . bsToHexText <$>) <$> Filter.ids f,
+	("authors" ,) . A.Array . V.fromList
+		. (A.String . bsToHexText . serialize_point <$>)
+		<$> Filter.authors f,
+	("kinds" ,) . A.Array . V.fromList
+		. (A.Number . fromIntegral <$>) <$> Filter.kinds f ] ++
+	(uncurry tagToValue <$> Filter.tags f) ++ catMaybes [
+	("since" ,) . A.Number . fromIntegral
+		. (\(CTime ct) -> ct) . utSeconds <$> Filter.since f,
+	("until" ,) . A.Number . fromIntegral
+		. (\(CTime ct) -> ct) . utSeconds <$> Filter.until f,
+	("limit" ,) . A.Number . fromIntegral <$> Filter.limit f ]
+
+tagToValue :: Char -> [T.Text] -> (A.Key, A.Value)
+tagToValue k vs = (fromString ['#', k], A.Array . V.fromList $ A.String <$> vs)
