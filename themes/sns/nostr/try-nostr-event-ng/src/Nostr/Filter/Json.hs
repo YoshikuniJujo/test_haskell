1481f12a351170acde7a246c5e508549bcc536e7
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-02-19 16:18:52 +0900

    try-nostr-event-ng: Nostr.Filter.Json: repair

--- themes/sns/nostr/try-nostr-event-ng/src/Nostr/Filter/Json.hs
+++ themes/sns/nostr/try-nostr-event-ng/src/Nostr/Filter/Json.hs
@@ -7,10 +7,10 @@ module Nostr.Filter.Json where
 import Foreign.C.Types
 import Data.Maybe
 import Data.Vector qualified as V
+import Data.ByteString qualified as BS
 import Data.Text qualified as T
 import Data.String
 import Data.Aeson qualified as A
-import Data.Aeson.Types qualified as A
 import Data.Aeson.KeyMap qualified as A
 import Data.UnixTime
 import Crypto.Curve.Secp256k1
@@ -23,7 +23,7 @@ encode f = A.Object . A.fromList $ catMaybes [
 	("ids" ,) . A.Array . V.fromList
 		. (A.String . bsToHexText <$>) <$> Filter.ids f,
 	("authors" ,) . A.Array . V.fromList
-		. (A.String . bsToHexText . serialize_point <$>)
+		. (A.String . bsToHexText . BS.tail . serialize_point <$>)
 		<$> Filter.authors f,
 	("kinds" ,) . A.Array . V.fromList
 		. (A.Number . fromIntegral <$>) <$> Filter.kinds f ] ++
