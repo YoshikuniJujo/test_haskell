05d44fa8b021925b9c256c0f4ac8fc77dab991b6
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-02-21 06:31:39 +0900

    tribial

--- tribial/battery_level_golf.hs
+++ tribial/battery_level_golf.hs
@@ -1,18 +1,33 @@
-import Control.Concurrent
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
+import Control.Concurrent
+import Data.List
+import System.Directory
+import System.FilePath
+
+main :: IO ()
 main = do
 	run1
 	threadDelay 10000000
 	main
 
-fp = "/sys/class/power_supply/BAT0/charge_"
+path :: FilePath
+path = "/sys/class/power_supply"
 
+getPath :: IO FilePath
+getPath = (path </>) . head . filter isBat <$> getDirectoryContents path
+	where
+	isBat = ("BAT" `isPrefixOf`)
+
+run1 :: IO ()
 run1 = do
-	n <- read <$> readFile (fp ++ "now")
-	f <- read <$> readFile (fp ++ "full")
+	fp <- getPath
+	n <- read <$> readFile (fp </> "charge_now")
+	f <- read <$> readFile (fp </> "charge_full")
 	putStrLn $ '[' : graph 74 (n / f) ++ "] " ++
 		show (round $ n / f * 100 :: Int) ++ "%"
 
+graph :: Int -> Double -> String
 graph n d =
 	a `replicate` '*' ++ (n - a) `replicate` ' '
 	where a = round (fromIntegral n * d)
