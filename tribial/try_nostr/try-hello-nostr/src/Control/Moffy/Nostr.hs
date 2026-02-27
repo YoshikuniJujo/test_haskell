e1d058ac4079ab19f999696ea98ca321c0f8a8e1
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-02-27 10:20:57 +0900

    try-hello-nostr: Control.MOnffy.Nostr: sampleFilter'

--- tribial/try_nostr/try-hello-nostr/src/Control/Moffy/Nostr.hs
+++ tribial/try_nostr/try-hello-nostr/src/Control/Moffy/Nostr.hs
@@ -9,6 +9,8 @@
 
 module Control.Moffy.Nostr where
 
+import Prelude hiding (break)
+import Control.Monad
 import Control.Concurrent
 import Control.Concurrent.STM
 import Control.Moffy
@@ -17,8 +19,10 @@ import Data.Type.Set
 import Data.OneOrMore qualified as OOM
 import Data.OneOrMoreApp
 import Data.Vector qualified as V
+import Data.Bool
 import Data.ByteString.Lazy.Char8 qualified as LBSC
 import Data.Text qualified as T
+import Data.Text.IO qualified as T
 import Data.Aeson qualified as A
 import Nostr.Event qualified as Event
 import Nostr.Event.Json qualified as EvJsn
@@ -52,7 +56,7 @@ instance Request Halt where
 type Events = Req :- Event :- Eose :- Halt :- 'Nil
 
 sample :: IO ()
-sample = run "nos.lol" "443" $ do
+sample = run print "nos.lol" "443" $ do
 	_ <- waitFor . adjust $ await (ReqReq "foobar" Filter.Filter {
 		Filter.ids = Nothing, Filter.authors = Nothing,
 		Filter.kinds = Just [1], Filter.tags = [],
@@ -61,17 +65,64 @@ sample = run "nos.lol" "443" $ do
 	emit =<< waitFor (adjust $ await EventReq \(OccEvent _nm ev) -> ev)
 	waitFor . adjust $ await HaltReq id
 
+sampleFilter :: Filter.Filter -> IO ()
+sampleFilter flt = run print "nos.lol" "443" $ do
+	_ <- waitFor . adjust $ await (ReqReq "foobar" flt) id
+	emit =<< waitFor (adjust $ await EventReq \(OccEvent _nm ev) -> ev)
+	waitFor . adjust $ await HaltReq id
+
+sampleFilter' :: Filter.Filter -> IO ()
+sampleFilter' flt = run printEvent "nos.lol" "443" $ untilEose "foobar" flt
+
+printEvent :: Event.E -> IO ()
+printEvent ev = do
+	print ev
+	T.putStrLn $ Event.content ev
+
+untilEose :: T.Text -> Filter.Filter -> Sig s Events Event.E ()
+untilEose nm flt = do
+	waitFor $ request nm flt
+	void $ (forever $ emit =<< (waitFor $ awaitNameEvent nm)) `break` awaitNameEose nm
+	waitFor . adjust $ await HaltReq (const ())
+
+request :: T.Text -> Filter.Filter -> React s Events ()
+request nm flt = adjust $ await (ReqReq nm flt) (const ())
+
+awaitEvent :: React s Events (T.Text, Event.E)
+awaitEvent = adjust $ await EventReq \(OccEvent nm ev) -> (nm, ev)
+
+awaitNameEvent :: T.Text -> React s Events Event.E
+awaitNameEvent nm0 = do
+	(nm, ev) <- awaitEvent
+	if nm == nm0 then pure ev else awaitNameEvent nm0
+
+awaitEose :: React s Events T.Text
+awaitEose = adjust $ await EoseReq \(OccEose nm) -> nm
+
+awaitNameEose :: T.Text -> React s Events ()
+awaitNameEose nm0 = bool (awaitNameEose nm0) (pure ()) . (== nm0) =<< awaitEose
+
+filterAuthor :: T.Text -> Either String Filter.Filter
+filterAuthor pb = do
+	pk <- Event.publicFromBech32 pb
+	pure Filter.Filter {
+		Filter.ids = Nothing,
+		Filter.authors = Just [pk], Filter.kinds = Just [1],
+		Filter.tags = [],
+		Filter.since = Nothing, Filter.until = Nothing,
+		Filter.limit = Just 5 }
+
 handleTChan :: TChan (EvReqs es) -> TChan (EvOccs es) -> Handle IO es
 handleTChan cr co rq = do
 	atomically $ writeTChan cr rq
 	atomically $ readTChan co
 
-run :: Show a => String -> String -> Sig s Events a r -> IO ()
-run raddr rprt s = do
+run :: (a -> IO ()) -> String -> String -> Sig s Events a r -> IO ()
+run pr raddr rprt s = do
 	cr <- atomically newTChan
 	co <- atomically newTChan
 	_ <- forkIO $ do
-		_ <- interpret (handleTChan cr co) print s
+		_ <- interpret (handleTChan cr co) pr s
 		pure ()
 	Ws.runSecureClient raddr (read rprt) "/" $ ws "foo" cr co
 
@@ -98,23 +149,24 @@ ws _pub cr co cnn = do
 				pure True
 		print $ a && b && c
 		pure $ a && b && c
+	Ws.sendClose cnn ("Bye!" :: T.Text)
 
 readPost :: TChan (EvOccs Events) -> Ws.Connection -> IO ()
 readPost co cnn = Ws.receiveData cnn >>= \rdt -> do
-		LBSC.putStrLn rdt
-		case A.decode rdt of
-			Just (A.Array (V.toList -> [
-					A.String "EOSE",
-					A.String nm ])) ->
-				atomically . writeTChan co . expand . Singleton $ OccEose nm
-			Just (A.Array (V.toList -> [
-					A.String "EVENT",
-					A.String nm,
-					A.Object jsn ])) -> do
-				Just ev <- pure $ EvJsn.decode jsn
-				atomically . writeTChan co . expand
-					. Singleton $ OccEvent nm ev
-			Just _ -> pure (); Nothing -> error "bad"
+--	LBSC.putStrLn rdt
+	case A.decode rdt of
+		Just (A.Array (V.toList -> [
+				A.String "EOSE",
+				A.String nm ])) ->
+			atomically . writeTChan co . expand . Singleton $ OccEose nm
+		Just (A.Array (V.toList -> [
+				A.String "EVENT",
+				A.String nm,
+				A.Object jsn ])) -> do
+			Just ev <- pure $ EvJsn.decode jsn
+			atomically . writeTChan co . expand
+				. Singleton $ OccEvent nm ev
+		Just _ -> pure (); Nothing -> error "bad"
 
 req :: T.Text -> Filter.Filter -> LBSC.ByteString
 req nm = A.encode . A.Array . V.fromList
