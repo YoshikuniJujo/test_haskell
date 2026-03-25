dca947578ed07e18dd503e9c2b3f28b326d726a0
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-03-25 10:09:21 +0900

    try-hello-haskell: app/Main.hs

--- tribial/docker/try-hello-haskell/try-hello-haskell/app/Main.hs
+++ tribial/docker/try-hello-haskell/try-hello-haskell/app/Main.hs
@@ -3,6 +3,8 @@ module Main (main) where
 main :: IO ()
 main = do
 	putStrLn "*** BEGIN HELLO ***"
+	putStrLn "world.txt"
 	putStrLn =<< readFile "world.txt"
+	putStrLn "hello.txt"
 	putStrLn =<< readFile "hello.txt"
 	putStrLn "*** END HELLO ***"
