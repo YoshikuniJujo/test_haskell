fb38eb03c43fbc5f6e06f7e0878f854c839f01a5
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-03-25 10:54:26 +0900

    try-yoshj-bot: app/reply.hs

--- tribial/try_nostr/try-yoshj-bot/try-yoshj-bot/src/Tools.hs
+++ tribial/try_nostr/try-yoshj-bot/try-yoshj-bot/src/Tools.hs
@@ -1,11 +1,29 @@
+{-# LANGUAGE ImportQualifiedPost #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Tools (withFileCreationMask, mask) where
+module Tools (
+
+	toHex,
+	withFileCreationMask, mask
+
+	) where
 
 import Control.Exception (bracket)
 import Data.Bits
+import Data.Char
+import Data.ByteString.Char8 qualified as BSC
+import Data.Text qualified as T
 import System.Posix.Types
 import System.Posix.Files
+import Numeric
+
+toHex :: BSC.ByteString -> T.Text
+toHex = T.pack . strToHexStr . BSC.unpack
+
+strToHexStr :: String -> String
+strToHexStr = concat . (sh <$>) . map ord
+	where
+	sh n = let s = showHex n "" in replicate (2 - length s) '0' ++ s
 
 withFileCreationMask :: FileMode -> IO a -> IO a
 withFileCreationMask md =
