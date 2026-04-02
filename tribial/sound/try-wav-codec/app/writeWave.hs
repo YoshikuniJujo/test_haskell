67b0046a70fbc016d69b3499282999fc9bf0e35a
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-04-02 16:13:49 +0900

    try-wav-codec: app/writeWave.hs

--- tribial/sound/try-wav-codec/app/writeWave.hs
+++ tribial/sound/try-wav-codec/app/writeWave.hs
@@ -12,19 +12,25 @@ import WriteMonoral16
 
 main :: IO ()
 main = do
-	nm : (read -> hz) : _ <- getArgs
-	print $ waveFormat (monoral hz)
-	writeWave (nm ++ ".png") (monoral hz) 0 Nothing 0.02 0.01
-	putMonoral16 (nm ++ ".wav") (monoral hz)
+	nm : (read -> hz) : (read -> l) : _ <- getArgs
+	print $ waveFormat (monoral hz l)
+	writeWave (nm ++ ".png") (monoral hz l) 0 Nothing 0.02 0.01
+	putMonoral16 (nm ++ ".wav") (monoral hz l)
 
-monoral :: Double -> Monoral16
-monoral hz = Monoral16 {
+monoral :: Double -> Double -> Monoral16
+monoral hz l = Monoral16 {
 	waveFormat = sampleWaveFormatEx,
-	waveData = round <$> zipWith (*) mask (sinWave hz)
+	waveData = (round <$>) . zipWith (*) (mask l)
+		$ foldr (zipWith (+)) (repeat 0) [
+			sinWave hz 6000,
+			sinWave (hz * 2) 2000,
+			sinWave (hz * 3) 700,
+			sinWave (hz * 4) 200
+			]
 	}
 
-mask :: [Double]
-mask = [0, 12 / 48000 .. 1] ++ [1, 1 - 1 / (48000 - 4800) .. 0]
+mask :: Double -> [Double]
+mask l = [0, 12 / 48000 .. 1] ++ [1, 1 - 1 / (l * 48000) .. 0]
 
-sinWave :: Double -> [Double]
-sinWave hz = (* 24000) . sin <$> [0, (2 * pi) * hz / 48000 ..]
+sinWave :: Double -> Double -> [Double]
+sinWave hz l = (* l) . sin <$> [0, (2 * pi) * hz / 48000 ..]
