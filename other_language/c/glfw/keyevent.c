34274c1bfa281644a5b11e021f447da6ffcee6b5
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-04-03 11:11:07 +0900

    glfw: keyevent.c

---
+++ other_language/c/glfw/keyevent.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <GLFW/glfw3.h>
+
+void
+key_callback(GLFWwindow* winndow, int key, int scancode, int action, int mods)
+{
+	printf("hello\n");
+}
+
+int
+main()
+{
+	printf("Hello, GLFW!\n");
+
+	GLFWwindow* window;
+
+	if (!glfwInit()) {
+		printf("glfwInit() return FALSE\n");
+		return -1;
+	}
+
+	window = glfwCreateWindow(640, 480, "HELLO GLFW", NULL, NULL);
+
+	glfwSetKeyCallback(window, key_callback);
+
+	if (!window) {
+		printf("glfwCrateWindow fail\n");
+		glfwTerminate();
+		return -1;
+	}
+
+	int present = glfwJoystickPresent(GLFW_JOYSTICK_1);
+	int present2 = glfwJoystickPresent(GLFW_JOYSTICK_2);
+
+	printf ("JOYSTICK 1 PRESENT: %d\n", present);
+	printf ("JOYSTICK 2 PRESENT: %d\n", present2);
+
+	while (!glfwWindowShouldClose(window)) {
+//		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+		int a;
+		a = (a + 1) % 50;
+
+		int count;
+		const float* axes = glfwGetJoystickAxes(GLFW_JOYSTICK_1, &count);
+		if (a == 0) {
+			printf("%d\n", count);
+			for (int i = 0; i < count; i++) {
+				printf("%f\n", axes[i]);
+			}
+		}
+	}
+
+	glfwTerminate();
+	return 0;
+}
