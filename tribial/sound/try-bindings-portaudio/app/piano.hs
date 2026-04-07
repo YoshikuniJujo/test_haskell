1fff2fb4be632c764b90f99a418c152be0d0bc02
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-04-07 12:32:12 +0900

    try-bindings-portaudio: app/piano.hs: tribial

--- tribial/sound/try-bindings-portaudio/app/piano.hs
+++ tribial/sound/try-bindings-portaudio/app/piano.hs
@@ -23,26 +23,28 @@ main = join $ execParser (info app mempty)
 
 app :: Parser (IO ())
 app = do
-	buf <- option auto $ long "buffer" <> help "number of samples for the buffer" <> value 1024
-	device <- option auto $ long "device" <> help "device index" <> value (-1)
+	buf <- option auto $ long "buffer" <>
+		help "number of samples for the buffer" <> value 1024
+	device <- option auto $ long "device" <>
+		help "device index" <> value (-1)
 	helper
+	pure $ run device buf
 
-	pure $ do
-		phase <- atomically $ newTMVar 0
-		playSound device buf \o -> do
-			i0 <- atomically do
-				i <- takeTMVar phase
-				putTMVar phase $ i + (MV.length o)
-				pure i
-			($ 0) . ($ i0) $ fix \go' i0' i' ->
-				if i' == MV.length o then pure () else do
-					let	v = Hz.waveform Hz.la V.!
-							((i0' + i') `mod` round (Hz.samplingRate / Hz.la))
-						sz = size (i0' + i')
-						v' = v * sz
-					MV.write o i' (V2 v' v')
-					go' i0' (i' + 1)
-			pure if (i0 > 370000) then Complete else Continue
+run :: Int -> Int -> IO ()
+run device buf = do
+	phase <- atomically $ newTMVar 0
+	playSound device buf \o -> do
+		i0 <- atomically
+			$ (<$) <$> id <*> putTMVar phase . (+ MV.length o)
+				=<< takeTMVar phase
+		($ 0) $ fix \go i -> if i == MV.length o then pure () else do
+			let	v = Hz.waveform Hz.la V.! ((i0 + i) `mod` la)
+				v' = v * size (i0 + i)
+			MV.write o i (V2 v' v')
+			go $ i + 1
+		pure if (i0 > 370000) then Complete else Continue
+	where
+	la = round $ Hz.samplingRate / Hz.la
 
 size :: Int -> Float
 size n	| n < 1000 = fromIntegral n / 2000
