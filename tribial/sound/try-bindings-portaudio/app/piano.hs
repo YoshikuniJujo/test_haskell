00e1d84fd7330c8cbfb55b0cbac21c1eac615235
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-04-07 16:33:30 +0900

    try-bindings-portaudio: app/piano.hs

---
+++ tribial/sound/try-bindings-portaudio/app/piano.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Main (main) where
+
+import Data.Map qualified as Map
+
+main :: IO ()
+main = putStrLn "piano"
+
+type Sound = Map.Map Doremi PhaseLoudness
+
+data Doremi = Do | Re | Mi | Fa | So | La | Ti | HDo deriving Show
+
+data PhaseLoudness = PhaseLoudness {
+	plPhase :: Int,
+	plLoudness :: Loudness }
+	deriving Show
+
+data Loudness
+	= Constant Float
+	| Changing {
+		changingNow :: Float,
+		changingDiff :: Float,
+		changingTo :: Float }
+	deriving Show
