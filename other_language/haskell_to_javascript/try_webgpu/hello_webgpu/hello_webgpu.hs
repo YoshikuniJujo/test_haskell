ef4d29b632b0bf497fbbc0bdf9d93f08c5909906
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-06-16 16:50:20 +0900

    heskell_to_javascript: hello_webgpu: get canvas

--- other_language/haskell_to_javascript/try_webgpu/hello_webgpu/hello_webgpu.hs
+++ other_language/haskell_to_javascript/try_webgpu/hello_webgpu/hello_webgpu.hs
@@ -5,6 +5,7 @@ module Main (main) where
 import GHC.JS.Value.Navigator qualified as JS.Navigator
 import GHC.JS.Value.Navigator.Webgpu qualified as JS.Navigator
 import GHC.JS.Value.Window qualified as JS.Window
+import GHC.JS.Value.Document qualified as JS.Document
 
 import GHC.JS.Value.Gpu qualified as JS.Gpu
 import GHC.JS.Value.GpuAdapter qualified as JS.GpuAdapter
@@ -13,7 +14,7 @@ import GHC.JS.Value.GpuAdapterInfo qualified as JS.GpuAdapterInfo
 main :: IO ()
 main = do
 	putStrLn "Hello"
-	print $ JS.Window.document JS.Window.w
+
 	print $ JS.Navigator.n
 	let	Just g = JS.Navigator.gpu JS.Navigator.n
 	print g
@@ -24,3 +25,18 @@ main = do
 	print i
 	print $ JS.GpuAdapterInfo.architecture i
 	print =<< JS.GpuAdapter.requestDevice a
+
+	let document = JS.Window.document JS.Window.w
+	print document
+
+	canvas <- JS.Document.getElementByTagName document "canvas"
+	canvasList <- JS.Document.getElementsByTagName document "canvas"
+--	canvas <- JS.Document.getElementsByTagName document "p"
+--	canvas <- JS.Document.getElementById document "foobar"
+
+	print canvas
+	print canvasList
+
+	pure ()
+
+--	print canvas
