e5ab26f236efa6be07bcdcbeb2afeedfb2121657
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-06-18 10:03:31 +0900

    yj-javascript-dom: GHC.JS.Value.Array

---
+++ other_language/haskell_to_javascript/pkgs/yj-javascript-dom/src/GHC/JS/Value/Array.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE ViewPatterns #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module GHC.JS.Value.Array where
+
+import GHC.JS.Prim (JSVal)
+import GHC.JS.Value qualified as JS.Value
+import GHC.JS.Value.Object qualified as JS.Object
+
+newtype A = A { unA :: JSVal }
+
+instance Show A where
+	show a = "(" ++ JS.Object.toString (JS.Object.toO a) ++ ")"
+
+instance JS.Value.IsJSVal A where toJSVal (A v) = v
+instance JS.Value.V A where toV = JS.Object.toValue; fromV = JS.Object.fromValue
+
+instance JS.Object.IsO A
+
+new :: IO A
+new = A <$> js_new
+
+foreign import javascript "(() => { return Array() })" js_new :: IO JSVal
+
+fromList :: JS.Value.V a => [a] -> IO A
+fromList xs = do
+	a <- new
+	mapM_ (push a) (JS.Value.toV <$> xs)
+	pure a
+
+push :: A -> JS.Value.Some -> IO ()
+push (A a) (JS.Value.toJSVal -> x) =js_push a x
+
+foreign import javascript "((a, x) => { a.push(x); })" js_push :: JSVal -> JSVal -> IO ()
