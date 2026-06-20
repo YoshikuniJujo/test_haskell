a5d1a87dd5c428ebdd8dd87236c6268fbbadf08f
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-06-21 06:16:15 +0900

    yj-javascript-value: GHC.JS.Value: Bool

--- other_language/haskell_to_javascript/pkgs/yj-javascript-value/src/GHC/JS/Value.hs
+++ other_language/haskell_to_javascript/pkgs/yj-javascript-value/src/GHC/JS/Value.hs
@@ -3,8 +3,10 @@
 
 module GHC.JS.Value (V(..), IsJSVal(..), Some, cast, consoleLog) where
 
-import GHC.JS.Prim (JSVal, toJSString, toJSInt)
 import Data.Typeable qualified as Tp
+import Data.Bool
+
+import GHC.JS.Prim (JSVal, toJSString, toJSInt)
 
 data Some = forall v . V v => Some v
 
@@ -25,6 +27,12 @@ instance V String
 
 instance IsJSVal Int where toJSVal = toJSInt
 instance V Int
+
+instance IsJSVal Bool where toJSVal = bool js_false js_true
+instance V Bool
+
+foreign import javascript "(() => { return false })" js_false :: JSVal
+foreign import javascript "(() => { return true })" js_true :: JSVal
 
 consoleLog :: Some -> IO ()
 consoleLog o = js_consoleLog $ toJSVal o
