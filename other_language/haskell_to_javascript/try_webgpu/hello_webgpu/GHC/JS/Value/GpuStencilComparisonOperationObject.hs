8d7b090c509da283c23b333fd3b17da876bf7059
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-06-26 11:08:22 +0900

    hello_webgpu: GHC.JS.Value.GpuStencilComparisonOperationObject

--- other_language/haskell_to_javascript/try_webgpu/hello_webgpu/GHC/JS/Value/GpuStencilComparisonOperationObject.hs
+++ other_language/haskell_to_javascript/try_webgpu/hello_webgpu/GHC/JS/Value/GpuStencilComparisonOperationObject.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module GHC.JS.Value.GpuStencilComparisonOperationObject where
@@ -7,10 +8,18 @@ import GHC.JS.Value.GpuDepthStencilCompare qualified as
 
 data G = G {
 	compare :: JS.GpuDepthStencilCompare.G,
-	depthFailOp :: StencilOperation
-	}
+	depthFailOp :: StencilOperation,
+	failOp :: StencilOperation,
+	passOp :: StencilOperation }
+	deriving Show
 
 data StencilOperation
 	= Zero | Keep | Replace | Invert
-	| DecrementClamp | DecrementWrap | IncreamentClamp | IncrementWrap
+	| DecrementClamp | DecrementWrap | IncrementClamp | IncrementWrap
 	deriving Show
+
+stencilOperationToString :: StencilOperation -> String
+stencilOperationToString = \case
+	Zero -> "zero"; Keep -> "keep"; Replace -> "replace"; Invert -> "invert"
+	DecrementClamp -> "decrement-clamp"; DecrementWrap -> "decrement-wrap"
+	IncrementClamp -> "increment-clamp"; IncrementWrap -> "increment-wrap"
