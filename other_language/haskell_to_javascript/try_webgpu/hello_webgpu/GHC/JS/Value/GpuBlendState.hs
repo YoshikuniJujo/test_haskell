14d959e4909b0cdb9783577c6e774476ea5245dd
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-07-02 16:03:55 +0900

    hello_webgpu: GHC.JS.Value.GpuBlendState

--- other_language/haskell_to_javascript/try_webgpu/hello_webgpu/GHC/JS/Value/GpuBlendState.hs
+++ other_language/haskell_to_javascript/try_webgpu/hello_webgpu/GHC/JS/Value/GpuBlendState.hs
@@ -2,6 +2,7 @@
 
 module GHC.JS.Value.GpuBlendState where
 
+import GHC.JS.Value qualified as JS.Value
 import GHC.JS.Value.Object qualified as JS.Object
 import GHC.JS.Value.GpuBlendComponent qualified as JS.GpuBlendComponent
 
@@ -14,3 +15,8 @@ toObject :: G -> ST s (JS.Object.ST s)
 toObject g = JS.Object.new >>= \o -> o <$ do
 	JS.Object.set o "color" $ color g
 	JS.Object.set o "alpha" $ alpha g
+
+instance JS.Value.IsJSVal G where
+	toJSVal g = JS.Value.toJSVal $ runST $ JS.Object.freeze =<< toObject g
+
+instance JS.Value.V G where toV = JS.Object.toValue; fromV = JS.Object.fromValue
