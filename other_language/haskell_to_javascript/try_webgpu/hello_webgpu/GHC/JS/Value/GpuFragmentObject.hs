35ec735922537f01ba52af3c34e2cd8c421e4088
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-07-03 14:11:10 +0900

    hello_webgpu: GHC.JS.Value.GpuFragmentObject

--- other_language/haskell_to_javascript/try_webgpu/hello_webgpu/GHC/JS/Value/GpuFragmentObject.hs
+++ other_language/haskell_to_javascript/try_webgpu/hello_webgpu/GHC/JS/Value/GpuFragmentObject.hs
@@ -20,9 +20,23 @@ data G = G {
 	constants :: Maybe JS.GpuOverridableConstant.G,
 	entryPoint :: Maybe String,
 	gModule :: JS.GpuShaderModule.G,
-	targets :: Targets
-	}
+	targets :: Targets }
 	deriving Show
+
+toObject :: G -> ST s (JS.Object.ST s)
+toObject g = do
+	o <- JS.Object.new
+	maybe (pure ()) (JS.Object.set o "constants") $ constants g
+	maybe (pure ()) (JS.Object.set o "entryPoint") $ entryPoint g
+	JS.Object.set o "module" $ gModule g
+	JS.Object.set o "targets" $ targets g
+	pure o
+
+instance JS.Value.IsJSVal G where
+	toJSVal g = runST $ JS.Value.toJSVal <$> toObject g
+
+instance JS.Value.V G where
+	toV = JS.Object.toValue; fromV = JS.Object.fromValue
 
 type Targets = [Target]
 
