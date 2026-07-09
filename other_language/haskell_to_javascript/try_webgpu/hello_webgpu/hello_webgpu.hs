5a056070f66dfd24c8941274db96da156d9cf53b
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-07-09 11:58:23 +0900

    hello_webgpu: hello_webgpu.hs: refactoring

--- other_language/haskell_to_javascript/try_webgpu/hello_webgpu/hello_webgpu.hs
+++ other_language/haskell_to_javascript/try_webgpu/hello_webgpu/hello_webgpu.hs
@@ -33,9 +33,6 @@ import GHC.JS.Value.GpuVertexBufferAttributeLayout qualified as
 import GHC.JS.Value.GpuVertexBufferLayout qualified as JS.GpuVertexBufferLayout
 import GHC.JS.Value.GpuVertexObject qualified as JS.GpuVertexObject
 
-import GHC.JS.Value.GpuOverridableConstant qualified as JS.GpuOverridableConstant
-import GHC.JS.Value.GpuBlendComponent qualified as JS.GpuBlendComponent
-
 import GHC.JS.Value.GpuFragmentObject qualified as JS.GpuFragmentObject
 import GHC.JS.Value.GpuTextureFormat qualified as JS.GpuTextureFormat
 
@@ -45,207 +42,58 @@ import GHC.JS.Value.GpuCommandEncoder qualified as JS.GpuCommandEncoder
 import GHC.JS.Value.GpuRenderPassEncoder qualified as JS.GpuRenderPassEncoder
 import GHC.JS.Value.GpuColorAttachmentObject qualified as JS.GpuColorAttachmentObject
 
+import GHC.JS.Value.GpuShaderModule qualified as JS.GpuShaderModule
+import GHC.JS.Value.GpuTexture qualified as JS.GpuTexture
+
 import Data.Bits
 import Data.Maybe
 import Data.UnionColor
 
 main :: IO ()
 main = do
-	g <- maybeError "WebGPU not supported" $ JS.Navigator.gpu JS.Navigator.n
-	a <- JS.Gpu.requestAdapter g
-	print a
-	device <- JS.GpuAdapter.requestDevice a
+	gpu <- maybeError "WebGPU not supported" $ JS.Navigator.gpu JS.Navigator.n
+	dvc <- JS.GpuAdapter.requestDevice =<< JS.Gpu.requestAdapter gpu
 	JS.EventTarget.addEventListenerSimple
-		(fromJust $ JS.Value.cast device) "uncapturederror" \ev -> do
-		JS.Value.consoleLog "error error error"
-		JS.Value.consoleLog ev
-		putStrLn "error occur"
-	print device
-
-	let document = JS.Window.document JS.Window.w
-	print document
-
-	canvas <- JS.Document.getElementByTagName document "canvas"
-	canvasList <- JS.Document.getElementsByTagName document "canvas"
---	canvas <- JS.Document.getElementsByTagName document "p"
---	canvas <- JS.Document.getElementById document "foobar"
-
-	print canvas
-	print canvasList
-	print =<< JS.HtmlCollection.length canvasList
-	cvs <- JS.HtmlCollection.item canvasList 0
-	print cvs
-
-	let	Just c = JS.Element.fromE cvs
-	print =<< JS.HtmlCanvasElement.getWidth c
-	print =<< JS.HtmlCanvasElement.getHeight c
-	Just ctx <- JS.HtmlCanvasElement.getContext c
-		JS.HtmlCanvasElement.ContextTypeWebGpu
-	print ctx
-	JS.Value.consoleLog ctx
-
-	JS.Value.consoleLog canvas
-	JS.Value.consoleLog a
-
-	JS.Value.consoleLog "Hello"
-
-	JS.Value.consoleLog ctx
-
-	format <- JS.Gpu.getPreferredCanvasFormat g
-	let	conf = (JS.GpuCanvasContext.configuration device
-			$ JS.GpuTextureFormat.preferredCanvasToConfig format) {
-			JS.GpuCanvasContext.alphaMode =
-				Just JS.GpuCanvasContext.AlphaModePremultiplied
-			}
-		
-	maybe (error "bad") (`JS.GpuCanvasContext.configure` conf)
-		$ JS.CanvasContext.fromC ctx
-
-	JS.Value.consoleLog ctx
-
-
-	shdrm <- JS.GpuDevice.createShaderModule device
+		(fromJust $ JS.Value.cast dvc) "uncapturederror" JS.Value.consoleLog
+	cvs <- maybeError "not canvas" . JS.Element.fromE
+		=<< (`JS.HtmlCollection.item` 0)
+		=<< JS.Document.getElementsByTagName
+			(JS.Window.document JS.Window.w) "canvas"
+	ctx <- maybeError "Cannot get WebGPU context"
+		. (JS.CanvasContext.fromC =<<)
+		=<< JS.HtmlCanvasElement.getContext cvs
+			JS.HtmlCanvasElement.ContextTypeWebGpu
+	fmt <- JS.GpuTextureFormat.preferredCanvasToConfig
+		<$> JS.Gpu.getPreferredCanvasFormat gpu
+	JS.GpuCanvasContext.configure ctx
+		$ (JS.GpuCanvasContext.configuration dvc fmt) {
+		JS.GpuCanvasContext.alphaMode =
+			Just JS.GpuCanvasContext.AlphaModePremultiplied }
+	shdrm <- JS.GpuDevice.createShaderModule dvc
 		$ (JS.GpuDevice.shaderModuleDescriptor shaders) {
-			JS.GpuDevice.shaderModuleDescriptorLabel = Just "GOOD SHADERS"
-			}
-
-	JS.Value.consoleLog shdrm
-
-	JS.Value.consoleLog (123 :: Int)
-
-	print =<< JS.Float32Array.new (JS.Value.toV (10 :: Int))
-
-	vertices <- JS.Float32Array.fromFloatList [
-		0, 0.6, 0, 1, 1, 0,
-		0, 1, -0.5, -0.6,  0, 1,
-		0, 1, 0, 1, 0.5, -0.6,
-		0, 1, 0, 0, 1, 1 ]
-	print vertices
-	print $ JS.Float32Array.byteLength vertices
-
-	print JS.GpuBufferUsage.copySrc
-	print JS.GpuBufferUsage.copyDst
-	print JS.GpuBufferUsage.index
-	print JS.GpuBufferUsage.mapRead
-	print JS.GpuBufferUsage.queryResolve
-	print JS.GpuBufferUsage.vertex
-
-	bffr <- JS.GpuDevice.createBuffer device
-		$ (JS.GpuDevice.bufferDescriptor
-			(JS.Float32Array.byteLength vertices)
-			(	JS.GpuBufferUsage.Vertex .|.
-				JS.GpuBufferUsage.CopyDst )) {
-			JS.GpuDevice.bufferDescriptorLabel =
-				Just "VERTEX BUFFER" }
-	print bffr
-	JS.Value.consoleLog bffr
-
-	let	queue = JS.GpuDevice.queue device
-		bffrln = JS.Float32Array.length vertices
-	print bffr
-	print queue
-	print bffrln
-	JS.GpuQueue.writeBuffer queue bffr 0 vertices 0 bffrln
-
-	let	attrs = [
-			JS.GpuVertexBufferAttributeLayout.G {
-				JS.GpuVertexBufferAttributeLayout.shaderLocation = 0,
-				JS.GpuVertexBufferAttributeLayout.offset = 0,
-				JS.GpuVertexBufferAttributeLayout.format =
-					JS.GpuVertexFormat.GFloat32x4
-				},
-			JS.GpuVertexBufferAttributeLayout.G {
-				JS.GpuVertexBufferAttributeLayout.shaderLocation = 1,
-				JS.GpuVertexBufferAttributeLayout.offset = 16,
-				JS.GpuVertexBufferAttributeLayout.format =
-					JS.GpuVertexFormat.GFloat32x4
-				}
-			]
-		vertexBuffers = [
-			JS.GpuVertexBufferLayout.G {
-				JS.GpuVertexBufferLayout.attributes = attrs,
-				JS.GpuVertexBufferLayout.arrayStride = 32,
-				JS.GpuVertexBufferLayout.stepMode =
-					JS.GpuVertexBufferLayout.StepModeVertex
-				}
-			]
-		pipelineDescriptor = JS.GpuRenderPipeline.Descriptor {
-			JS.GpuRenderPipeline.descriptorDepthStencil =
-				Nothing,
-			JS.GpuRenderPipeline.descriptorFragment =
-				Just JS.GpuFragmentObject.G {
-					JS.GpuFragmentObject.constants = Nothing,
-					JS.GpuFragmentObject.entryPoint =
-						Just "fragment_main",
-					JS.GpuFragmentObject.gModule = shdrm,
-					JS.GpuFragmentObject.targets = [
-						JS.GpuFragmentObject.Target {
-							JS.GpuFragmentObject.blend = Nothing,
-							JS.GpuFragmentObject.format = JS.GpuTextureFormat.fromPreferredCanvas format,
-							JS.GpuFragmentObject.writeMask = Nothing
-							} ]
-					},
-			JS.GpuRenderPipeline.descriptorLabel = Just "foobar",
-			JS.GpuRenderPipeline.descriptorLayout = JS.GpuRenderPipeline.Auto,
-			JS.GpuRenderPipeline.descriptorVertex =
-				JS.GpuVertexObject.G {
-					JS.GpuVertexObject.constants = Nothing,
-					JS.GpuVertexObject.entryPoint =
-						Just "vertex_main",
-					JS.GpuVertexObject.gModule = shdrm,
-					JS.GpuVertexObject.buffers = vertexBuffers }
-			}
-	JS.Value.consoleLog attrs
-	JS.Value.consoleLog vertexBuffers
-	JS.Value.js_consoleLog . JS.GpuOverridableConstant.fromValue
-		$ JS.GpuOverridableConstant.I32 (- 5)
-	JS.Value.consoleLog JS.GpuBlendComponent.def
-	print JS.GpuFragmentObject.js_red
-	print JS.GpuFragmentObject.js_green
-	print JS.GpuFragmentObject.js_blue
-	print JS.GpuFragmentObject.js_alpha
-	print JS.GpuFragmentObject.js_all
-	JS.Value.consoleLog $ [
-		JS.GpuFragmentObject.Target {
-			JS.GpuFragmentObject.blend = Nothing,
-			JS.GpuFragmentObject.format =
-				JS.GpuTextureFormat.R8Unorm,
-			JS.GpuFragmentObject.writeMask = Nothing } ]
-	print =<< JS.Gpu.getPreferredCanvasFormat g
-	JS.Value.consoleLog pipelineDescriptor
-	renderPipeline <- JS.GpuRenderPipeline.create device pipelineDescriptor
-	JS.Value.consoleLog renderPipeline
-	commandEncoder <- JS.GpuCommandEncoder.create device
-	JS.Value.consoleLog commandEncoder
-	txtr <- maybe (error "bad") JS.GpuCanvasContext.getCurrentTexture
-		$ JS.CanvasContext.fromC ctx
-	JS.Value.consoleLog txtr
-	let	renderPassDescriptor = JS.GpuRenderPassEncoder.Descriptor {
-			JS.GpuRenderPassEncoder.colorAttachments = [
-				JS.GpuColorAttachmentObject.G {
-					JS.GpuColorAttachmentObject.clearValue =
-						rgbaDoubleRaw 0 0.5 1 1,
-					JS.GpuColorAttachmentObject.loadOp =
---						JS.GpuColorAttachmentObject.Load,
-						JS.GpuColorAttachmentObject.Clear,
-					JS.GpuColorAttachmentObject.storeOp =
-						JS.GpuColorAttachmentObject.Store,
-					JS.GpuColorAttachmentObject.view = txtr
-					}
-				]
-			}
-	JS.Value.consoleLog renderPassDescriptor
-	passEncoder <- JS.GpuCommandEncoder.beginRenderPass
-		commandEncoder renderPassDescriptor
-	JS.Value.consoleLog renderPipeline
-	JS.Value.consoleLog passEncoder
-	JS.GpuRenderPassEncoder.setPipeline passEncoder renderPipeline
-	JS.GpuRenderPassEncoder.setVertexBuffer passEncoder 0 bffr
-	JS.GpuRenderPassEncoder.draw passEncoder 3
-	JS.GpuRenderPassEncoder.end passEncoder
-	cbffr <- JS.GpuCommandEncoder.finish commandEncoder
-	JS.Value.consoleLog cbffr
-	JS.GpuQueue.submit queue [cbffr]
+			JS.GpuDevice.shaderModuleDescriptorLabel =
+				Just "GOOD SHADERS" }
+	vrts <- JS.Float32Array.fromFloatList [
+		0, 0.6, 0, 1, 1, 0, 0, 1, -0.5, -0.6,  0, 1,
+		0, 1, 0, 1, 0.5, -0.6, 0, 1, 0, 0, 1, 1 ]
+	bffr <- JS.GpuDevice.createBuffer dvc $ (JS.GpuDevice.bufferDescriptor
+		(JS.Float32Array.byteLength vrts)
+		(JS.GpuBufferUsage.Vertex .|. JS.GpuBufferUsage.CopyDst)) {
+		JS.GpuDevice.bufferDescriptorLabel = Just "VERTEX BUFFER" }
+	JS.GpuQueue.writeBuffer (JS.GpuDevice.queue dvc)
+		bffr 0 vrts 0 (JS.Float32Array.length vrts)
+	cmdEnc <- JS.GpuCommandEncoder.create dvc
+	pssEnc <- JS.GpuCommandEncoder.beginRenderPass cmdEnc
+		. renderPassDescriptor
+		=<< JS.GpuCanvasContext.getCurrentTexture ctx
+	JS.GpuRenderPassEncoder.setPipeline pssEnc
+		=<< JS.GpuRenderPipeline.create
+			dvc (pipelineDescriptor shdrm fmt)
+	JS.GpuRenderPassEncoder.setVertexBuffer pssEnc 0 bffr
+	JS.GpuRenderPassEncoder.draw pssEnc 3
+	JS.GpuRenderPassEncoder.end pssEnc
+	JS.GpuQueue.submit (JS.GpuDevice.queue dvc) . (: [])
+		=<< JS.GpuCommandEncoder.finish cmdEnc
 
 shaders :: String
 shaders = """
@@ -271,6 +119,69 @@ shaders = """
 		return fragData.color;
 	}
 	"""
+
+renderPassDescriptor :: JS.GpuTexture.G -> JS.GpuRenderPassEncoder.Descriptor
+renderPassDescriptor txtr = JS.GpuRenderPassEncoder.Descriptor {
+			JS.GpuRenderPassEncoder.colorAttachments = [
+				JS.GpuColorAttachmentObject.G {
+					JS.GpuColorAttachmentObject.clearValue =
+						rgbaDoubleRaw 0 0.5 1 1,
+					JS.GpuColorAttachmentObject.loadOp =
+						JS.GpuColorAttachmentObject.Clear,
+					JS.GpuColorAttachmentObject.storeOp =
+						JS.GpuColorAttachmentObject.Store,
+					JS.GpuColorAttachmentObject.view = txtr
+					}
+				]
+			}
+
+pipelineDescriptor :: JS.GpuShaderModule.G ->
+	JS.GpuTextureFormat.CanvasConfig -> JS.GpuRenderPipeline.Descriptor
+pipelineDescriptor shdrm fmt = JS.GpuRenderPipeline.Descriptor {
+			JS.GpuRenderPipeline.descriptorDepthStencil = Nothing,
+			JS.GpuRenderPipeline.descriptorFragment =
+				Just JS.GpuFragmentObject.G {
+					JS.GpuFragmentObject.constants = Nothing,
+					JS.GpuFragmentObject.entryPoint =
+						Just "fragment_main",
+					JS.GpuFragmentObject.gModule = shdrm,
+					JS.GpuFragmentObject.targets = frgTgts },
+			JS.GpuRenderPipeline.descriptorLabel = Just "foobar",
+			JS.GpuRenderPipeline.descriptorLayout =
+				JS.GpuRenderPipeline.Auto,
+			JS.GpuRenderPipeline.descriptorVertex =
+				JS.GpuVertexObject.G {
+					JS.GpuVertexObject.constants = Nothing,
+					JS.GpuVertexObject.entryPoint =
+						Just "vertex_main",
+					JS.GpuVertexObject.gModule = shdrm,
+					JS.GpuVertexObject.buffers = vertexBuffers } }
+	where
+	frgTgts = [
+			JS.GpuFragmentObject.Target {
+			JS.GpuFragmentObject.blend = Nothing,
+			JS.GpuFragmentObject.format =
+				JS.GpuTextureFormat.fromCanvasConfig fmt,
+			JS.GpuFragmentObject.writeMask = Nothing } ]
+
+vertexBuffers :: [JS.GpuVertexBufferLayout.G]
+vertexBuffers = [
+	JS.GpuVertexBufferLayout.G {
+		JS.GpuVertexBufferLayout.attributes = attrs,
+		JS.GpuVertexBufferLayout.arrayStride = 32,
+		JS.GpuVertexBufferLayout.stepMode =
+			JS.GpuVertexBufferLayout.StepModeVertex } ]
+	where attrs = [
+		JS.GpuVertexBufferAttributeLayout.G {
+			JS.GpuVertexBufferAttributeLayout.shaderLocation = 0,
+			JS.GpuVertexBufferAttributeLayout.offset = 0,
+			JS.GpuVertexBufferAttributeLayout.format =
+				JS.GpuVertexFormat.GFloat32x4 },
+		JS.GpuVertexBufferAttributeLayout.G {
+			JS.GpuVertexBufferAttributeLayout.shaderLocation = 1,
+			JS.GpuVertexBufferAttributeLayout.offset = 16,
+			JS.GpuVertexBufferAttributeLayout.format =
+				JS.GpuVertexFormat.GFloat32x4 } ]
 
 maybeError :: MonadFail m => String -> Maybe a -> m a
 maybeError em = \case Nothing -> fail em; Just x -> pure x
