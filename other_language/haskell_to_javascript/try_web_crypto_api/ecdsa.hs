1110dfccf90ea50406a5e291ca29f3095af12f20
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-07-14 20:42:23 +0900

    try_web_crypto_api: ecdsa.hs

--- other_language/haskell_to_javascript/try_web_crypto_api/ecdsa.hs
+++ other_language/haskell_to_javascript/try_web_crypto_api/ecdsa.hs
@@ -17,7 +17,8 @@ demoECDSA = do
 	JS.Value.consoleLog sk
 	JS.Value.consoleLog pk
 	e <- newTextEncoder
-	JS.Value.js_consoleLog =<< encode e "われ泣きぬれて蟹みそを食べる"
+	dt <- encode e "われ泣きぬれて蟹みそを食べる"
+	JS.Value.consoleLog =<< sign sk dt
 
 keyPair :: IO (PrivateKey, PublicKey)
 keyPair = (PrivateKey . js_privateKey &&& PublicKey . js_publicKey) <$> js_keyPair
@@ -47,8 +48,23 @@ newTextEncoder = TextEncoder <$> js_newTextEncoder
 foreign import javascript "(() => { return new TextEncoder() })"
 	js_newTextEncoder :: IO JSVal
 
-encode :: TextEncoder -> String -> IO JSVal
-encode (JS.Value.toJSVal -> te) (toJSString -> s) = js_encode te s
+encode :: TextEncoder -> String -> IO Uint8Array
+encode (JS.Value.toJSVal -> te) (toJSString -> s) = Uint8Array <$> js_encode te s
 
 foreign import javascript "((e, s) => { return e.encode(s) })"
 	js_encode :: JSVal -> JSVal -> IO JSVal
+
+newtype Uint8Array = Uint8Array JSVal
+
+instance JS.Value.IsJSVal Uint8Array where toJSVal (Uint8Array uia) = uia
+instance JS.Value.V Uint8Array
+
+sign :: PrivateKey -> Uint8Array -> IO ArrayBufferUint8
+sign (JS.Value.toJSVal -> sk) (JS.Value.toJSVal -> dt) = ArrayBufferUint8 <$> js_sign sk dt
+
+foreign import javascript interruptible "((sk, dt, cont) => { globalThis.crypto.subtle.sign({ name : 'ECDSA', hash: { name: 'SHA-256' } }, sk, dt).then((s) => cont(s)) })"
+	js_sign :: JSVal -> JSVal -> IO JSVal
+
+newtype ArrayBufferUint8 = ArrayBufferUint8 JSVal
+instance JS.Value.IsJSVal ArrayBufferUint8 where toJSVal (ArrayBufferUint8 a) = a
+instance JS.Value.V ArrayBufferUint8
