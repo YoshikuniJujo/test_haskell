9e8aeb6634689594cd32cc5b7dd999c6da6bc0a9
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-07-14 17:46:26 +0900

    try_web_crypto_api: ecdsa.hs

--- other_language/haskell_to_javascript/try_web_crypto_api/ecdsa.hs
+++ other_language/haskell_to_javascript/try_web_crypto_api/ecdsa.hs
@@ -1,8 +1,9 @@
 {-# LANGUAGE JavaScriptFFI #-}
+{-# LANGUAGE ViewPatterns #-}
 
 module Main where
 
-import GHC.JS.Prim (JSVal)
+import GHC.JS.Prim (JSVal, toJSString)
 import GHC.JS.Value qualified as JS.Value
 
 main :: IO ()
@@ -11,6 +12,25 @@ main = demoECDSA
 demoECDSA :: IO ()
 demoECDSA = do
 	JS.Value.js_consoleLog =<< js_keyPair
+	e <- newTextEncoder
+	JS.Value.js_consoleLog =<< encode e "われ泣きぬれて蟹みそを食べる"
 
 foreign import javascript interruptible "((cont) => { globalThis.crypto.subtle.generateKey( { name: 'ECDSA', namedCurve: 'P-256' }, true, ['sign', 'verify'] ).then((p) => cont(p)) })"
 	js_keyPair :: IO JSVal
+
+newtype TextEncoder = TextEncoder JSVal
+
+instance JS.Value.IsJSVal TextEncoder where toJSVal (TextEncoder te) = te
+instance JS.Value.V TextEncoder
+
+newTextEncoder :: IO TextEncoder
+newTextEncoder = TextEncoder <$> js_newTextEncoder
+
+foreign import javascript "(() => { return new TextEncoder() })"
+	js_newTextEncoder :: IO JSVal
+
+encode :: TextEncoder -> String -> IO JSVal
+encode (JS.Value.toJSVal -> te) (toJSString -> s) = js_encode te s
+
+foreign import javascript "((e, s) => { return e.encode(s) })"
+	js_encode :: JSVal -> JSVal -> IO JSVal
