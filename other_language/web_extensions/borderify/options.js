3056423b782e45f62a5eb0fe2e41d9e624bb7629
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-07-18 06:29:29 +0900

    borderify: checkbox

--- other_language/web_extensions/borderify/options.js
+++ other_language/web_extensions/borderify/options.js
@@ -1,12 +1,16 @@
 async function foo() {
-const checkForDebug = document.getElementById('debug');
-checkForDebug.addEventListener('change', async (event) => {
-	if (event.target.checked) {
-		await browser.storage.local.set({ slothDebug: true });
-	} else {
-		await browser.storage.local.set({ slothDebug: false });
-	}
-});
+	const checkForDebug = document.getElementById('debug');
+
+	const ckd = await browser.storage.local.get("slothDebug");
+	checkForDebug.checked = ckd;
+
+	checkForDebug.addEventListener('change', async (event) => {
+		if (event.target.checked) {
+			await browser.storage.local.set({ slothDebug: true });
+		} else {
+			await browser.storage.local.set({ slothDebug: false });
+		}
+	});
 }
 
 foo();
