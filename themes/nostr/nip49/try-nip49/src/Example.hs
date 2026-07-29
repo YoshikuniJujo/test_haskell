9113c134a4746a805b8c9c73fd9362699f37f16c
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-07-29 16:40:41 +0900

    try-nip49: Example: correct warning

--- themes/nostr/nip49/try-nip49/src/Example.hs
+++ themes/nostr/nip49/try-nip49/src/Example.hs
@@ -1,13 +1,19 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Example where
+module Example (
+	decrypted, hdr, vsn
+	) where
 
+import Data.Maybe
+import Data.Word
 import Data.ByteString qualified as BS
 
 import Lib
 import BchEcc
 
+{-
 password = "nostr"
 
 logN = 16
@@ -20,19 +26,29 @@ example = do
 	Scrypted { salt = slt, pass = sk } <- getSaltSymmetricKey
 	putStrLn $ "SALT = " ++ show slt
 	putStrLn $ "SYMMETRIC_KEY = " ++ show sk
+	-}
 
+encrypted :: String
 encrypted = "ncryptsec1qgg9947rlpvqu76pj5ecreduf9jxhselq2nae2kghhvd5g7dgjtcxfqtd67p9m0w57lspw8gsq6yphnm8623nsl8xn9j4jdzz84zm3frztj3z7s35vpzmqf6ksu8r89qk5z2zxfmu5gv8th8wclt0h4p"
 
+checked :: Maybe Checked
 checked = check =<< sepHrpDt encrypted
 
-Just (hdr, dt) = checkedToHdrDat =<< checked
+hdr :: String
+dt :: [Word8]
+(hdr, dt) = fromJust $ checkedToHdrDat =<< checked
 
+vsn, lgN, slt, nnc, ksb, r0, r1, r2, r3, ct :: [Word8]
 (vsn, r0) = splitAt 1 dt
-([lgN], r1) = splitAt 1 r0
+(lgN, r1) = splitAt 1 r0
 (slt, r2) = splitAt 16 r1
 (nnc, r3) = splitAt 24 r2
 (ksb, ct) = splitAt 1 r3
 
-symmetricKey = scrypt (fromIntegral lgN) (BS.pack slt) "nostr"
+symmetricKey :: BS.ByteString
+symmetricKey = case lgN of
+	[lgn] -> scrypt (fromIntegral lgn) (BS.pack slt) "nostr"
+	_ -> error "never occur"
 
+decrypted :: BS.ByteString
 decrypted = decrypt symmetricKey (BS.pack nnc) (BS.pack ksb) (BS.pack ct)
