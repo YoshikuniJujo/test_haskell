4457d888549bb098df13b88cdc04ecf86b236628
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-08-04 00:03:07 +0900

    try-nip49: tribial

--- themes/nostr/nip49/try-nip49/src/Example.hs
+++ themes/nostr/nip49/try-nip49/src/Example.hs
@@ -29,7 +29,11 @@ import MyWords
 import Ncryptsec qualified
 
 encrypted :: T.Text
-encrypted = "ncryptsec1qgg9947rlpvqu76pj5ecreduf9jxhselq2nae2kghhvd5g7dgjtcxfqtd67p9m0w57lspw8gsq6yphnm8623nsl8xn9j4jdzz84zm3frztj3z7s35vpzmqf6ksu8r89qk5z2zxfmu5gv8th8wclt0h4p"
+encrypted =
+	"ncryptsec1" <>
+	"qgg9947rlpvqu76pj5ecreduf9jxhselq2nae2kghhvd5g7dgjtcxfqtd67p9m0w" <>
+	"57lspw8gsq6yphnm8623nsl8xn9j4jdzz84zm3frztj3z7s35vpzmqf6ksu8r89q" <>
+	"k5z2zxfmu5gv8th8wclt0h4p"
 
 checksum :: T.Text
 checksum = T.drop (T.length encrypted - 6) encrypted
