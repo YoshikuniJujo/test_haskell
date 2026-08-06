a321f89432dab720dec4ee585b273f0ad777347a
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-08-06 14:55:53 +0900

    try-nip49: test_vectors

--- themes/nostr/nip49/try-nip49/app/makeHexSamples.hs
+++ themes/nostr/nip49/try-nip49/app/makeHexSamples.hs
@@ -3,9 +3,11 @@
 
 module Main where
 
+import Control.Monad
 import Data.ByteString qualified as BS
 import Data.Hex qualified as Hex
 import System.Environment
+import System.Entropy
 
 main :: IO ()
 main = do
@@ -13,4 +15,5 @@ main = do
 	print fp
 	let	hs = Hex.H . BS.pack <$> [
 			[0 .. 31], replicate 32 0, replicate 32 0xff ]
-	Hex.writeFileList fp hs
+	hs' <- (Hex.H <$>) <$> replicateM 3 (getEntropy 32)
+	Hex.writeFileList fp (hs ++ hs')
