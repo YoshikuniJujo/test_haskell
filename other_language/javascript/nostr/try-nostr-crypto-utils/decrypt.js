d53422f73507376d81374a89af380134c9674175
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-08-06 18:05:27 +0900

    try-nostr-crypto-utils: decrypt.js

---
+++ other_language/javascript/nostr/try-nostr-crypto-utils/decrypt.js
@@ -0,0 +1,18 @@
+import { bech32 } from 'bech32';
+import { nip49 } from 'nostr-crypto-utils';
+import fs from 'fs';
+
+const password = fs.readFileSync('/home/tatsuya/project/test_haskell/themes/nostr/nip49/try-nip49/test_vectors/test03.password').toString();
+
+
+const ncryptsec = fs.readFileSync('/home/tatsuya/project/test_haskell/themes/nostr/nip49/try-nip49/test_vectors/test03.ncryptsec').toString();
+
+console.log(password);
+console.log(ncryptsec);
+
+const secretKey = nip49.decrypt(ncryptsec, password);
+const words = bech32.toWords(secretKey);
+const encoded = bech32.encode('nsec', words, 1000);
+
+console.log(secretKey);
+console.log(encoded);
