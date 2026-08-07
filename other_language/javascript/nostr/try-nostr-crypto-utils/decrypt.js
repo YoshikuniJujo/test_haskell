8d865ea61b03171461d9c768b273999e9a357093
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-08-07 12:53:04 +0900

    tribial

--- other_language/javascript/nostr/try-nostr-crypto-utils/decrypt.js
+++ other_language/javascript/nostr/try-nostr-crypto-utils/decrypt.js
@@ -2,10 +2,10 @@ import { bech32 } from 'bech32';
 import { nip49 } from 'nostr-crypto-utils';
 import fs from 'fs';
 
-const password = fs.readFileSync('/home/tatsuya/project/test_haskell/themes/nostr/nip49/try-nip49/test_vectors/test03.password').toString();
+const password = fs.readFileSync('/home/tatsuya/project/test_haskell/themes/nostr/nip49/try-nip49/test_vectors/test04.password').toString();
 
 
-const ncryptsec = fs.readFileSync('/home/tatsuya/project/test_haskell/themes/nostr/nip49/try-nip49/test_vectors/test03.ncryptsec').toString();
+const ncryptsec = fs.readFileSync('/home/tatsuya/project/test_haskell/themes/nostr/nip49/try-nip49/test_vectors/test04.ncryptsec').toString();
 
 console.log(password);
 console.log(ncryptsec);
