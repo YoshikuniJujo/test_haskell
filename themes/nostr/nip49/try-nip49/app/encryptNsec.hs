8bd878a65b759f4d0dcc4ca63675ffd133108b9c
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-08-10 16:19:47 +0900

    try-nip49: app/encryptNsec.hs: read password from stdin

--- themes/nostr/nip49/try-nip49/app/encryptNsec.hs
+++ themes/nostr/nip49/try-nip49/app/encryptNsec.hs
@@ -5,6 +5,7 @@ module Main where
 
 import Data.Text.IO qualified as T
 import System.Environment
+import System.IO.Yj
 
 import Ncryptsec qualified
 
@@ -12,5 +13,6 @@ main :: IO ()
 main = do
 	[nsfp, pssfp, cnsfp] <- getArgs
 	ns <- T.readFile nsfp
---	T.writeFile cnsfp =<< Ncryptsec.fromNsec 16 0 (withNoEcho getLine) ns
-	T.writeFile cnsfp =<< Ncryptsec.fromNsec 16 0 (readFile pssfp) ns
+	T.writeFile cnsfp =<< case pssfp of
+		"-" -> Ncryptsec.fromNsec 16 0 (withNoEcho getLine) ns
+		_ -> Ncryptsec.fromNsec 16 0 (readFile pssfp) ns
