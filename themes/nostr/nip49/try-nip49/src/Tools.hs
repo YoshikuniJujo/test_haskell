19f027847fd6ba91a81a2c9f68b3fdebaa46ca7d
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-08-10 12:36:22 +0900

    try-nip49: use try-bech32

--- themes/nostr/nip49/try-nip49/src/Tools.hs
+++ themes/nostr/nip49/try-nip49/src/Tools.hs
@@ -31,14 +31,3 @@ randomR = curry (state . R.randomR)
 
 select :: [a] -> State R.StdGen a
 select xs = state $ ((xs !!) `first`) . R.randomR (0, length xs - 1)
-
-spanR :: (a -> Bool) -> [a] -> Either [a] (NE.NonEmpty a, [a])
-spanR p = \case
-	[] -> Left []
-	x : xs -> case (p x, spanR p xs) of
-		(_, Right td) -> Right $ (x NE.<|) `first` td
-		(False, Left d) -> Right (x NE.:| [], d)
-		(True, Left d) -> Left $ x : d
-
-takeR :: Int -> [a] -> [a]
-takeR n xs = take (length xs - n) xs
