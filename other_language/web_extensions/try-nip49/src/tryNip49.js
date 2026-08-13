908856f4339bba3ca16ebdd7d737291856ccacc8
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-08-13 21:17:51 +0900

    try-nip49: tribial

--- other_language/web_extensions/try-nip49/src/tryNip49.js
+++ other_language/web_extensions/try-nip49/src/tryNip49.js
@@ -4,9 +4,9 @@ const nostr = {
 
 	getPublicKey() {
 		return new window.Promise((resolve) => {
-				resolve(
-					"cc77438831fb0816e9f41443cf2603c90cdd7d558e49edc5f6b72326c22452d0"
-				);
+			resolve(
+				"cc77438831fb0816e9f41443cf2603c90cdd7d558e49edc5f6b72326c22452d0"
+			);
 		});
 	},
 
