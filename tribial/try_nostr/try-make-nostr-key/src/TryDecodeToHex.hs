13f3d9ac10d19e4e973587a0dc7e601254d12757
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-08-13 19:17:42 +0900

    try-make-nostr-key: TryDecodeToHex

--- tribial/try_nostr/try-make-nostr-key/src/TryDecodeToHex.hs
+++ tribial/try_nostr/try-make-nostr-key/src/TryDecodeToHex.hs
@@ -2,10 +2,12 @@
 
 module TryDecodeToHex where
 
+import Data.ByteString qualified as BS
 import Data.Text qualified as T
 import Data.Text.IO qualified as T
 import System.Environment
 import Codec.Binary.Bech32
+import Numeric
 
 home :: IO FilePath
 home = getEnv "HOME"
@@ -14,3 +16,14 @@ npub :: IO T.Text
 npub = do
 	hm <- home
 	T.readFile $ hm ++ "/tmp/npub"
+
+npub' = decode <$> npub
+
+npubBytes = do
+	Right (_, dp) <- npub'
+	pure $ dataPartToBytes dp
+
+word8ToHex w = replicate (2 - length s) '0' ++ s
+	where s = showHex w ""
+
+toHex = concat . (word8ToHex <$>) . BS.unpack
