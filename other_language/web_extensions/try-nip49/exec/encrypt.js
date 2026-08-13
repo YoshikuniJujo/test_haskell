81b17e93e26086925e5e4ecb3ffa84bdf3df6cb7
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-08-13 16:37:46 +0900

    exec/encrypt.js

--- other_language/web_extensions/try-nip49/exec/encrypt.js
+++ other_language/web_extensions/try-nip49/exec/encrypt.js
@@ -1,9 +1,8 @@
 import fs from 'node:fs/promises';
+import { webcrypto } from 'node:crypto';
 import { scrypt } from '@noble/hashes/scrypt.js';
 import { xchacha20poly1305 } from '@noble/ciphers/chacha.js'
 import * as Bech32 from '../src/bech32.js';
-
-console.log("UNSAFE FIXED SALT AND NONCE ENCRYPTER");
 
 const buffer = await fs.readFile('../../../themes/nostr/nip49/try-nip49/test_vectors/test00.nsec');
 const password = await fs.readFile('../../../themes/nostr/nip49/try-nip49/test_vectors/test00.password');
@@ -13,13 +12,11 @@ const seckey = Bech32.decode(text);
 console.log(text);
 console.log(Bech32.decode(text));
 
-const salt = new Uint8Array([
-	55, 255, 75, 198, 238, 145, 63, 237,
-	106, 172, 118, 243, 250, 254, 78, 104 ]);
+const salt = new Uint8Array(16);
+const nonce = new Uint8Array(24);
 
-const nonce = new Uint8Array([
-	129, 12, 236, 49, 110, 48, 213, 101, 27, 172, 252, 96, 193, 29,
-	226, 116, 94, 19, 113, 28, 114, 246, 0, 162 ]);
+webcrypto.getRandomValues(salt);
+webcrypto.getRandomValues(nonce);
 
 const smkey = scrypt(password, salt, { N: 2 ** 16, r: 8, p: 1, dkLen: 32 });
 
@@ -31,4 +28,8 @@ console.log(nonce);
 const chacha = xchacha20poly1305(smkey, nonce, new Uint8Array([0]));
 const encrypted = chacha.encrypt(seckey);
 
-console.log(Bech32.encode('ncryptsec', new Uint8Array([2, 16, ...salt, ...nonce, 0, ...encrypted])));
+const ncryptsec = Bech32.encode('ncryptsec', new Uint8Array([2, 16, ...salt, ...nonce, 0, ...encrypted]));
+console.log(ncryptsec);
+
+fs.writeFile('test_vectors/test00.ncryptsec', ncryptsec);
+fs.writeFile('test_vectors/test00.password', password);
