b8fdd204682ac5f8d087855bcd6f9c6bb2977793
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-08-13 21:12:12 +0900

    try-nip49: getPublicKey stub

--- other_language/web_extensions/try-nip49/src/tryNip49.js
+++ other_language/web_extensions/try-nip49/src/tryNip49.js
@@ -1,1 +1,21 @@
 document.body.style.border = "5px solid red";
+
+const nostr = {
+
+	getPublicKey() {
+		return new window.Promise((resolve) => {
+				resolve(
+					"cc77438831fb0816e9f41443cf2603c90cdd7d558e49edc5f6b72326c22452d0"
+				);
+		});
+	},
+
+	async signEvent(event) {
+		throw new Error("yet");
+	}
+};
+
+window.wrappedJSObject.nostr =
+	cloneInto(nostr, window, {
+		cloneFunctions: true
+	});
