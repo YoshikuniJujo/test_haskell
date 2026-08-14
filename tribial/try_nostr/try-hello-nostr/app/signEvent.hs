8d24c202bbfc9111eace3e4418dc5ef91676d96d
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-08-14 17:17:54 +0900

    try-hello-nostr: app/signEvent.hs

---
+++ tribial/try_nostr/try-hello-nostr/app/signEvent.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Main where
+
+import Data.ByteString.Lazy qualified as BSL
+import Data.Text.IO qualified as T
+import System.Environment
+import Data.Aeson qualified as A
+
+import Nostr.Event qualified as Event
+import Nostr.Event.Json qualified as EvJs
+import AddSignature
+
+main :: IO ()
+main = do
+	skf : pkf : evf : _ <- getArgs
+	Right sk <- Event.secretFromBech32 <$> T.readFile skf
+	Right pk <- Event.publicFromBech32 <$> T.readFile pkf
+	Just ev <-  (EvJs.decodeNoPub =<<) . A.decode <$> BSL.readFile evf
+	print . EvJs.encode' =<< signature sk pk ev
