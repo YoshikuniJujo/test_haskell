33a0c88f235d0d43104e0a5bfe92bdae2df0f2e7
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-08-14 18:27:14 +0900

    try-nip49: exec/signEvent.js

---
+++ other_language/web_extensions/try-nip49/exec/signEvent.js
@@ -0,0 +1,39 @@
+import fs from 'node:fs/promises';
+import { sha256 } from '@noble/hashes/sha2.js';
+import { schnorr, hashes } from '@noble/secp256k1';
+import * as Bech32 from '../src/bech32.js';
+
+const [skf, pkf, evf] = process.argv.slice(2);
+
+const sk = Bech32.decode(await fs.readFile(skf, 'utf8'));
+const pk = Bech32.decode(await fs.readFile(pkf, 'utf8'));
+const ev = JSON.parse(await fs.readFile(evf, 'utf8'));
+
+const pkh = Array.from(pk, b => b.toString(16).padStart(2, '0')).join('');
+
+const evpk = { ...ev, pubkey: pkh }
+
+const serialized = JSON.stringify([
+	0,
+	evpk.pubkey,
+	evpk.created_at,
+	evpk.kind,
+	evpk.tags,
+	evpk.content ]);
+
+const hash = sha256(new TextEncoder().encode(serialized));
+
+const idBytes = sha256(new TextEncoder().encode(serialized));
+hashes.sha256 = sha256;
+const auxRand = new Uint8Array(32);
+const sig = schnorr.sign(idBytes, sk, auxRand);
+// auxRand is fixed for debugging. Remove this in production.
+
+console.log(pk);
+console.log(pkh);
+console.log(ev);
+console.log(evpk);
+console.log(serialized);
+console.log(Buffer.from(hash).toString('hex'));
+
+console.log(Buffer.from(sig).toString('hex'));
