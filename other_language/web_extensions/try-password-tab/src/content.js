b6737b7ac2733e8bdce0e7f466802b8746e8d1ac
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-08-19 12:42:10 +0900

    try-password-tab: content: bug fix

--- other_language/web_extensions/try-password-tab/src/content.js
+++ other_language/web_extensions/try-password-tab/src/content.js
@@ -4,7 +4,7 @@ const tryPasswordTab = {
 
 	queryInput()
 	{
-		return new window.Promise(async (rs, rj) => {
+		return new window.Promise((rs, rj) => {
 			const rid = crypto.randomUUID();
 			pendingRequests.set(rid, { resolve: rs, reject: rj });
 			browser.runtime.sendMessage(
@@ -16,14 +16,14 @@ const tryPasswordTab = {
 browser.runtime.onMessage.addListener((m) => {
 	switch (m.method) {
 		case "pushInput":
-			const rs = pendingRequests.get(m.request).resolve;
-			if (!rs) {
+			const rq = pendingRequests.get(m.request);
+			if (!rq) {
 				console.error(
 					"invalid input request",
 					{ request: m.request } );
 				throw new Error("Invalid input request"); }
 			pendingRequests.delete(m.request);
-			rs(m.value);
+			rq.resolve(m.value);
 			break; } });
 
 window.wrappedJSObject.tryPasswordTab =
