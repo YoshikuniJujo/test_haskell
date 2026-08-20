81c9b8fa57c80a87c319317691b3c4a94c72330b
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-08-20 11:58:28 +0900

    try-password-tab: requests

---
+++ other_language/web_extensions/try-password-tab/src/requests.js
@@ -0,0 +1,51 @@
+import { } from "./mutex.js"
+
+export async function
+create(rid, stid, itid)
+{
+	const { requests: rqs = {} } =
+		await browser.storage.session.get("requests");
+	rqs[rid] = { sourceTab: stid, inputTab: itid, state: "pending" };
+	await browser.storage.session.set({ requests: rqs });
+}
+
+export async function
+returned(rid, tid)
+{
+	const { requests: rqs = {} } =
+		await browser.storage.session.get("requests");
+	const rq = rqs[rid];
+	if (!rq) { throw new Error(`Unknown request: ${rid}`); }
+	chkInputTab(rid, rq.inputTab, tid);
+	delete rqs[rid];
+	await browser.storage.session.set( { requests: rqs });
+	return rq.sourceTab;
+}
+
+function chkInputTab(rid, tid0, tid)
+{
+	if (tid !== tid0) {
+		console.error(
+			"possible attack: returnInput was recieved from " +
+			"a tab different from the input tab",
+			{	requestId: rid,
+				expectedInputTabId: tid0,
+				actualSenderTabId: tid } );
+		throw new Error(
+			"security violation: input was not returned " +
+			"by the input tab associated with this request" ); }
+}
+
+export async function
+removeTab(tid)
+{
+	const { requests: rqs = {} } =
+		await browser.storage.session.get("requests");
+	const tabs = [];
+	for (const[rid, rq] of Object.entries(rqs)) {
+		if (rq.sourceTab !== tid && rq.inputTab !== tid) continue;
+		if (rq.sourceTab === tid) tabs.push(rq.inputTab);
+		delete rqs[rid]; }
+	browser.storage.session.set({ requests: rqs });
+	return tabs;
+}
