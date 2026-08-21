6d74ca8fda830422e44b44d15da8cb992b299615
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-08-22 07:25:42 +0900

    try-password-tab: answer: refactoring

--- other_language/web_extensions/try-password-tab/src/answer.js
+++ other_language/web_extensions/try-password-tab/src/answer.js
@@ -3,62 +3,61 @@ import { Mutex } from "./mutex.js"
 const mutex = new Mutex;
 
 export async function
-create(rid, stid, itid)
+create(a, st, it)
 {
 	await mutex.acquire();
-	try {	const { requests: rqs = {} } =
-			await browser.storage.session.get("requests");
-		rqs[rid] = {
-			sourceTab: stid, inputTab: itid, state: "pending" };
-		await browser.storage.session.set({ requests: rqs }); }
+	try {	const { answers: aws = {} } =
+			await browser.storage.session.get("answers");
+		aws[a] = { sourceTab: st, inputTab: it, state: "pending" };
+		await browser.storage.session.set({ answers: aws }); }
 	finally { mutex.release(); }
 }
 
 export async function
-returned(rid, tid)
+returned(a, it)
 {
 	await mutex.acquire();
-	try {	const { requests: rqs = {} } =
-			await browser.storage.session.get("requests");
-		const rq = rqs[rid];
-		if (!rq) { throw new Error(`Unknown request: ${rid}`); }
-		chkInputTab(rid, rq.inputTab, tid);
-		delete rqs[rid];
-		await browser.storage.session.set( { requests: rqs });
-		return rq.sourceTab; }
+	try {	const { answers: aws = {} } =
+			await browser.storage.session.get("answers");
+		const aw = aws[a];
+		if (!aw) { throw new Error(`Unknown answer: ${a}`); }
+		chkInputTab(a, aw.inputTab, it);
+		delete aws[a];
+		await browser.storage.session.set( { answers: aws });
+		return aw.sourceTab; }
 	finally { mutex.release(); }
 
 }
 
 function
-chkInputTab(rid, tid0, tid)
+chkInputTab(a, it0, it)
 {
-	if (tid !== tid0) {
+	if (it !== it0) {
 		console.error(
-			"possible attack: returnInput was received from " +
+			"possible attack: returnPass was received from " +
 			"a tab different from the input tab",
-			{	requestId: rid,
-				expectedInputTabId: tid0,
-				actualSenderTabId: tid } );
+			{	answer: a,
+				expectedInputTabId: it0,
+				actualSenderTabId: it } );
 		throw new Error(
 			"security violation: input was not returned " +
-			"by the input tab associated with this request" ); }
+			"by the input tab associated with this answer" ); }
 }
 
 export async function
 removeTab(t)
 {
 	await mutex.acquire();
-	try {	const { requests: rqs = {} } =
-			await browser.storage.session.get("requests");
-		const tabs = []; const rmrqs = [];
-		for (const[rid, rq] of Object.entries(rqs)) {
-			if (rq.sourceTab === t) tabs.push(rq.inputTab);
-			else if (rq.inputTab === t)
-				rmrqs.push({ answer: rid, source: rq.sourceTab });
+	try {	const { answers: aws = {} } =
+			await browser.storage.session.get("answers");
+		const tbs = []; const rmaws = [];
+		for (const[a, aw] of Object.entries(aws)) {
+			if (aw.sourceTab === t) tbs.push(aw.inputTab);
+			else if (aw.inputTab === t)
+				rmaws.push({ answer: a, source: aw.sourceTab });
 			else continue;
-			delete rqs[rid]; }
-		await browser.storage.session.set({ requests: rqs });
-		return { toClose: tabs, answers: rmrqs }; }
+			delete aws[a]; }
+		await browser.storage.session.set({ answers: aws });
+		return { toClose: tbs, answers: rmaws }; }
 	finally { mutex.release(); }
 }
