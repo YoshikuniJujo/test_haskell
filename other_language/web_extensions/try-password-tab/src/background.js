e920d24ca53e45df06893dab28f87ed6290b998a
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-08-25 13:24:49 +0900

    try-password-tab: background: answer as argument

--- other_language/web_extensions/try-password-tab/src/background.js
+++ other_language/web_extensions/try-password-tab/src/background.js
@@ -4,40 +4,40 @@ const answer = new Answer(browser.storage.session, "answers");
 
 browser.runtime.onMessage.addListener((m, s) => {
 	switch (m.method) {
-		case "queryPass": return qryPass(m.answer, s.tab.id);
-		case "returnPass": return rtnPass(m.answer, m.val, s.tab.id);
-		case "contentStarted": return pageVanished(s.tab.id); } });
-browser.tabs.onRemoved.addListener(pageVanished);
+		case "queryPass": return qryPass(answer, m.answer, s.tab.id);
+		case "returnPass": return rtnPass(answer, m.answer, m.val, s.tab.id);
+		case "contentStarted": return pageVanished(answer, s.tab.id); } });
+browser.tabs.onRemoved.addListener((tid) => pageVanished(answer, tid));
 
 async function
-qryPass(a, st)
+qryPass(asw, aid, st)
 {
 	const it = await browser.tabs.create({
 		active: false,
 		url: browser.runtime.getURL(
-			`input.html?answer=${encodeURIComponent(a)}` ) });
-	const use = await answer.create(a, st, it.id);
+			`input.html?answer=${encodeURIComponent(aid)}` ) });
+	const use = await asw.create(aid, st, it.id);
 	if (use !== it.id) await browser.tabs.remove(it.id);
 	await browser.tabs.update(use, { active: true });
 }
 
 async function
-rtnPass(a, v, it)
+rtnPass(asw, aid, v, it)
 {
 	if (v === "password") {
-		const sts = await answer.returned(a, it);
+		const sts = await asw.returned(aid, it);
 		for (const s of sts)
 			await browser.tabs.sendMessage(s,
-				{ method: "pushPass", answer: a, value: v });
+				{ method: "pushPass", answer: aid, value: v });
 		await browser.tabs.update(sts[0], { active: true });
 		await browser.tabs.remove(it); }
 	else {	await browser.tabs.sendMessage( it, { method: "wrongPass" }); }
 }
 
 async function
-pageVanished(vt)
+pageVanished(asw, vt)
 {
-	const r = await answer.removeTab(vt);
+	const r = await asw.removeTab(vt);
 	for (const t of r.toClose) await browser.tabs.remove(t);
 	for (const a of r.answers)
 		for (const s of a.sources)
