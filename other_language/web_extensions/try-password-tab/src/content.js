6532b4c12f99da512978660a4c8c28b86794dac2
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-08-29 23:40:58 +0900

    try-password-tab: conetnt: refactoring

--- other_language/web_extensions/try-password-tab/src/content.js
+++ other_language/web_extensions/try-password-tab/src/content.js
@@ -1,9 +1,8 @@
 browser.runtime.sendMessage({ method: "contentStarted" });
 
-const requestsWaitingForAnswer = new Map();
+const newRequestsWaitingForAnswer = new Map();
 
 const pendingPassword = new Map();
-const newPendingRequests = new Map();
 
 const tryPasswordTab = {
 
@@ -12,15 +11,13 @@ const tryPasswordTab = {
 		return new window.Promise(async (rslv, rjct) => {
 			try {
 				const rid = crypto.randomUUID();
-				const rs = requestsWaitingForAnswer.get(answer) ?? [];
-				rs.push(rid);
-				requestsWaitingForAnswer.set(answer, rs);
 				browser.runtime.sendMessage(
 					{ method: "queryPass", answer: answer } );
 				pendingPassword.set(rid, rslv);
 				await new Promise((rs, rj) => {
-					newPendingRequests.set(rid,
-						{ resolve: rs, reject: rj });
+					const nrqs = newRequestsWaitingForAnswer.get(answer) ?? [];
+					nrqs.push({ resolve: rs, reject: rj });
+					newRequestsWaitingForAnswer.set(answer, nrqs);
 				});
 
 				browser.runtime.sendMessage(
@@ -50,38 +47,40 @@ browser.runtime.onMessage.addListener((m) => {
 			break;
 		}
 		case "passwordReady": {
-			const rids = requestsWaitingForAnswer.get(m.answer);
+			const rids = newRequestsWaitingForAnswer.get(m.answer);
 			for (const rid of rids) {
+				/*
 				const nrq = newPendingRequests.get(rid);
 				if (!nrq) {
 					console.error(
 						"invalid input request",
 						{ request: rid, answer: m.answer } );
 					throw new Error("Invalid input request"); }
-				nrq.resolve();
-				newPendingRequests.delete(rid);
+					*/
+				rid.resolve();
 			}
-			requestsWaitingForAnswer.delete(m.answer);
+			newRequestsWaitingForAnswer.delete(m.answer);
 			break; }
 		case "passError": {
 			console.log(typeof m.answer, m.answer);
 			console.log(
-				[...requestsWaitingForAnswer.keys()].map(k => [typeof k, k]) );
-			console.log(requestsWaitingForAnswer);
-			const rids = requestsWaitingForAnswer.get(m.answer);
+				[...newRequestsWaitingForAnswer.keys()].map(k => [typeof k, k]) );
+			console.log(newRequestsWaitingForAnswer);
+			const rids = newRequestsWaitingForAnswer.get(m.answer);
 			console.log(rids);
 			for (const rid of rids) {
+				/*
 				const nrq = newPendingRequests.get(rid);
 				if (!nrq) {
 					console.error(
 						"invalid input request",
 						{ request: rid, answer: m.answer } );
 					throw new Error("Invalid input request"); }
-				newPendingRequests.delete(rid);
-				nrq.reject(
+					*/
+				rid.reject(
 					new window.Error("Input tab was closed for answer: " +
 						m.answer) ); }
-			requestsWaitingForAnswer.delete(m.answer);
+			newRequestsWaitingForAnswer.delete(m.answer);
 			break; } } });
 
 window.wrappedJSObject.tryPasswordTab =
