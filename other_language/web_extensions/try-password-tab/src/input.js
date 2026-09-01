53f9bac238b617134fb0f59d8116a536b858cfb0
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-09-01 16:45:04 +0900

    try-password-tab: input: hide inputed password

--- other_language/web_extensions/try-password-tab/src/input.js
+++ other_language/web_extensions/try-password-tab/src/input.js
@@ -1,11 +1,17 @@
 const answer = new URLSearchParams(location.search).get("publicKey");
 const input = document.querySelector("#input");
 const error = document.querySelector("#error");
+const show = document.querySelector("#show-password");
+
 input.focus();
 
 input.addEventListener("keydown", (event) => {
 	if (event.key === "Enter") {
 		event.preventDefault(); sndPass(answer, input.value); } });
+
+show.addEventListener("change", () => {
+	input.type = show.checked ? "text" : "password";
+});
 
 document.querySelector("#send").addEventListener("click", () => {
 	sndPass(answer, input.value); });
