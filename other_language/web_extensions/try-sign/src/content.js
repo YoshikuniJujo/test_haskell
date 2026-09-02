7d394fc73d67c0371311964e24dfdaa54a27ccfd
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-09-02 12:34:01 +0900

    try-sign: content: refactoring

--- other_language/web_extensions/try-sign/src/content.js
+++ other_language/web_extensions/try-sign/src/content.js
@@ -5,9 +5,6 @@ import * as Schnorr from "./crypto/schnorr.js";
 const nsec = Bech32.decode(KeyPair.nsec);
 const npub = Bech32.decode(KeyPair.npub);
 
-console.log("try-sign");
-console.log(npub);
-
 document.documentElement.style.border = "5px solid green";
 
 const nostr = {
@@ -20,35 +17,9 @@ const nostr = {
 	},
 
 	signEvent(event) {
-		console.log(event);
-		console.log(event.created_at);
-		console.log(event.kind);
-		console.log(event.tags);
-		console.log(event.content);
-
 		return new window.Promise(async (resolve) => {
-
-			const ev = {
-				created_at: event.created_at,
-				kind: event.kind,
-				tags: event.tags,
-				content: event.content };
-
-			const rtn = await Schnorr.signEvent(ev, nsec, npub);
-
-			console.log("rtn obtained");
-			console.log(rtn.id);
-			console.log("id obtained");
-
-			const rslt = {
-				created_at: rtn.created_at,
-				kind: rtn.kind,
-				tags: rtn.tags,
-				content: rtn.content,
-				id: rtn.id,
-				pubkey: rtn.pubkey,
-				sig: rtn.sig }
-			resolve(cloneInto(rslt, window));
+			const rtn = await Schnorr.signEvent(event, nsec, npub);
+			resolve(cloneInto(rtn, window));
 		});
 	}
 }
