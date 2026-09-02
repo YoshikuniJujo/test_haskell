c35a926d93b4c082b062f8243fb0216283f50bce
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-09-02 17:01:51 +0900

    try-sign: codec/word: refactoring

--- other_language/web_extensions/try-sign/src/codec/bech32.js
+++ other_language/web_extensions/try-sign/src/codec/bech32.js
@@ -4,17 +4,17 @@ import * as Word from './word.js';
 export function encode(hrp, dp) {
 
 	const c5 = chunks(5, Array.from(dp));
-	const w40sInit = c5.init.map(Word.word8sToWord40);
-	const w40Last = Word.word8sToWord40(c5.last) << 8n * (5n - BigInt(c5.lastN))
+	const w40sInit = c5.init.map(Word.pack8sTo40);
+	const w40Last = Word.pack8sTo40(c5.last) << 8n * (5n - BigInt(c5.lastN))
 	const w40s = {
 		init: w40sInit,
 		last: w40Last,
 		lastN: c5.lastN * 8 }
-	const w5sInit = w40s.init.map(Word.word40ToWord5s);
-	const w5sLast = Word.word40ToWord5s(w40s.last).slice(0, Math.ceil(w40s.lastN / 5));
+	const w5sInit = w40s.init.map(Word.unpack40To5s);
+	const w5sLast = Word.unpack40To5s(w40s.last).slice(0, Math.ceil(w40s.lastN / 5));
 	const w5s = w5sInit.flat().concat(w5sLast);
 	const w5s2 = [...hrpExpand([...hrp]), ...w5s];
-	const checksum = Word.word30ToWord5List(generate(w5s2));
+	const checksum = Word.unpack30To5s(generate(w5s2));
 	const w5s3 = [...w5s, ...checksum];
 
 	return hrp + '1' + w5s3.map(w => charset[w]).join('');
@@ -33,11 +33,12 @@ export function decode(txt) {
 	}
 	const c8 = chunks(8, dp5.slice(0, -6));
 	const c40 = {
-		init: c8.init.map(Word.word5sToWord40),
-		last: Word.word5sToWord40(c8.last) << 5n * (8n - BigInt(c8.lastN)),
+		init: c8.init.map(Word.pack5sTo40),
+		last: Word.pack5sTo40(c8.last) << 5n * (8n - BigInt(c8.lastN)),
 		lastN: c8.lastN * 5 }
-	const dataPartInit = c40.init.map(Word.word40ToWord8List);
-	const dataPartLast = Word.word40ToWord8ListTail(c40.last, c40.lastN / 8);
+	const dataPartInit = c40.init.map(Word.unpack40To8s);
+	const dataPartLast = Word.unpack40To8s(c40.last).slice(0, c40.lastN / 8);
+		// Word.word40ToWord8ListTail(c40.last, c40.lastN / 8);
 	const dataPart = new Uint8Array(dataPartInit.flat().concat(dataPartLast));
 	return { humanReadable: hrp.join(''), data: dataPart }
 }
