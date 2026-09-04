d7cf0417e220ee66b4b9889cc0c76108817cfb2f
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-09-04 13:54:56 +0900

    my-sign: template for window.mySign.addUser and window.mySign.login

--- other_language/web_extensions/my-sign/src/content.js
+++ other_language/web_extensions/my-sign/src/content.js
@@ -1,1 +1,18 @@
 document.documentElement.style.border = "5px solid green";
+
+window.mySign = {
+
+	addUser(uid, pswd)
+	{
+		console.log("mySign.addUser", uid);
+	},
+
+	login(uid, pswd)
+	{
+		console.log("mySign.login", uid);
+	}
+
+};
+
+window.wrappedJSObject.mySign =
+	cloneInto(mySign, window, { cloneFunctions: true });
