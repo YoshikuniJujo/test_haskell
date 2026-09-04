c2447597fb65e64832ab7d7b169fcd356a5fee76
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-09-04 10:20:04 +0900

    my-sign: login: CLI

--- other_language/web_extensions/my-sign/exec/try-hono.js
+++ other_language/web_extensions/my-sign/exec/try-hono.js
@@ -1,10 +1,24 @@
+import "fake-indexeddb/auto";
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
+import { addUser, login } from "../src/try-indexeddb/login.js"
 
 const app = new Hono();
 
 app.get("/", c => {
 	console.log("get /");
 	return c.text("hello\n"); });
+
+app.post("/add", async c => {
+	const { uid, pswd } = await c.req.json();
+	await addUser(indexedDB, uid, pswd);
+	return c.json({ ok: true });
+});
+
+app.post("/login", async c => {
+	const {uid, pswd } = await c.req.json();
+	const ok = await login(indexedDB, uid, pswd);
+	return c.json({ ok });
+});
 
 serve(app);
