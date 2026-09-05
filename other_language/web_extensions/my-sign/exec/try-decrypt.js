c4574d4f979ed5e5bde6c6c9f2e3b6d69f7957ee
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-09-05 14:58:06 +0900

    my-sign: exec/try-decrypt

--- other_language/web_extensions/my-sign/exec/try-decrypt.js
+++ other_language/web_extensions/my-sign/exec/try-decrypt.js
@@ -1,4 +1,6 @@
 import { readFile } from "node:fs/promises";
+import { scrypt } from "@noble/hashes/scrypt.js";
+import { xchacha20poly1305 } from "@noble/ciphers/chacha.js";
 import * as Bech32 from "../src/codec/bech32.js";
 import { readPassword } from "./readPassword.js";
 
@@ -32,10 +34,20 @@ const encrypted = {
 	version: vsn[0], nonce: nnc,
 	keySecurityByte: aad[0], cipherText: ct, mac: mac };
 
-const pswd = new TextEncoder().encode(await readPassword());
+// const pswd = new TextEncoder().encode(await readPassword());
+const pswd = await readPassword();
 
 console.log(encrypted);
 console.log(pswd);
 
 const symKeyPrms = { logN: lgn[0], salt: slt };
 console.log(symKeyPrms);
+
+const smkey = scrypt(pswd.normalize("NFKC"), symKeyPrms.salt,
+	{ N: 2 ** symKeyPrms.logN, r: 8, p: 1, dkLen: 32 });
+
+console.log(smkey);
+
+const ciphertext = new Uint8Array([...ct, ...mac]);
+const chacha = xchacha20poly1305(smkey, encrypted.nonce, aad);
+const secretKey = chacha.decrypt(ciphertext);
