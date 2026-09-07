cfbe39270af2a59511dfa43c95beab7ea67eaec1
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-09-07 16:43:56 +0900

    my-sign: exec/try-sign.js

--- other_language/web_extensions/my-sign/exec/try-sign.js
+++ other_language/web_extensions/my-sign/exec/try-sign.js
@@ -5,6 +5,10 @@ import { readPassword } from "./readPassword.js";
 import * as Ncryptsec from "../src/crypto/ncryptsec.js"
 import { signEvent } from "../src/sign/schnorr.js"
 
+import { sha256 } from "@noble/hashes/sha2.js";
+
+import { schnorr } from "@noble/secp256k1";
+
 const [filePath, pkf, evf] = process.argv.slice(2);
 
 const text = await readFile(filePath, "utf8");
@@ -18,4 +22,25 @@ const pswd = await readPassword();
 const secretKey = await Ncryptsec.decrypt(encrypted, pswd);
 
 console.log(ev);
-console.log(await signEvent(ev, secretKey, pk));
+
+const signed = await signEvent(ev, secretKey, pk);
+console.log(signed);
+
+const srlzd = JSON.stringify( [
+	0, signed.pubkey, signed.created_at, signed.kind,
+	signed.tags, signed.content] );
+const hash = sha256(new TextEncoder().encode(srlzd));
+
+console.log(Array.from(hash, b => b.toString(16).padStart(2, "0")).join(""));
+console.log(signed.id);
+
+console.log(await schnorr.verifyAsync(hexToBytes(signed.sig), hash, hexToBytes(signed.pubkey)));
+
+function
+hexToBytes(hex)
+{
+	const bs = new Uint8Array(hex.length / 2);
+	for (let i = 0; i < bs.length; i++)
+		bs[i] = parseInt(hex.slice(i * 2, i * 2 + 2), 16);
+	return bs
+}
