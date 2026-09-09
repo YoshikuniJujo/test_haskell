e48916280015915227f3638458c6b98676a6c3ba
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-09-09 15:47:40 +0900

    try-password-tab: mapArray

--- other_language/web_extensions/try-password-tab/src/content.js
+++ other_language/web_extensions/try-password-tab/src/content.js
@@ -34,15 +34,3 @@ browser.runtime.onMessage.addListener((m) => { switch (m.method) {
 
 window.wrappedJSObject.tryPasswordTab =
 	cloneInto(tryPasswordTab, window, { cloneFunctions: true });
-
-function
-addToArrayMap(map, k, v)
-{
-	const vs = map.get(k); if (vs) vs.push(v); else map.set(k, [v]);
-}
-
-function
-forEachValues(map, k, f)
-{
-	const vs = map.get(k); map.delete(k); for (const v of vs) f(v);
-}
---
+++ other_language/web_extensions/try-password-tab/src/mapArray.js
@@ -0,0 +1,11 @@
+function
+addToArrayMap(map, k, v)
+{
+	const vs = map.get(k); if (vs) vs.push(v); else map.set(k, [v]);
+}
+
+function
+forEachValues(map, k, f)
+{
+	const vs = map.get(k); map.delete(k); for (const v of vs) f(v);
+}
