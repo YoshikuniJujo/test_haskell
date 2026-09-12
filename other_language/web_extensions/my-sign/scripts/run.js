00b6e32358bb9a9a79c1051a06f60f32b36ec901
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-09-13 08:25:43 +0900

    my-sign: trivial

--- other_language/web_extensions/my-sign/scripts/run.js
+++ other_language/web_extensions/my-sign/scripts/run.js
@@ -5,7 +5,8 @@ const p = spawn(
 	[
 		"web-ext", "run", "--source-dir", "dist",
 		"--firefox-profile=./web-ext-profile",
-		"--url", "https://yoshikunijujo.github.io/others/try-sign",
+		"--url", "https://yoshikunijujo.github.io/others/try-sign#account=alice",
+		"--url", "https://yoshikunijujo.github.io/others/try-sign#account=bob",
 		"--url", "about:debugging#/runtime/this-firefox",
 		"--url", "about:addons"
 	],
