cd4624c1e41bc9109b109ddb906396f73dd9a3ef
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-09-15 07:30:54 +0900

    my-sign: add mock

--- other_language/web_extensions/my-sign/exec/try-options-state.js
+++ other_language/web_extensions/my-sign/exec/try-options-state.js
@@ -1,3 +1,20 @@
 import { OptionsState } from "../src/optionsState.js";
 
 console.log(new OptionsState());
+
+class StorageSessionMock {
+	constructor() {
+		this.data = new Map();
+	}
+
+	async get(key) {
+		return {
+			[key]: this.data.get(key)
+		};
+	}
+
+	async set(values) {
+		for (const [key, value] of Object.entries(values))
+			this.data.set(key, value);
+	}
+}
