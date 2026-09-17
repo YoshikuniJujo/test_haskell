e39b34801fd65c6524bba841e2d18f2b73d5f322
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-09-17 23:54:10 +0900

    my-sign: tools/checkTags.hs

---
+++ other_language/web_extensions/my-sign/tools/checkTags.hs
@@ -0,0 +1,37 @@
+module Main where
+
+import Control.Arrow
+
+main :: IO ()
+main = putStrLn "foobar"
+
+filterVoid :: [((Int, Int), String)] -> [((Int, Int), String)]
+filterVoid = filter $ (`notElem` ["!doctype", "meta", "link", "input", "hr"]) . snd
+
+getOpens :: (Int, Int) -> String -> [((Int, Int), String)]
+getOpens (l, c) ('<' : str) = getClose (l, c) (l, c + 1) "" str
+getOpens (l, c) ('\n' : str) = getOpens (l + 1, 0) str
+getOpens (l, c) ('\t' : str) = getOpens (l, c + 8) str
+getOpens (l, c) (_ : str) = getOpens (l, c + 1) str
+getOpens _ "" = []
+
+getClose :: (Int, Int) -> (Int, Int) -> String -> String -> [((Int, Int), String)]
+getClose (l0, c0) (l, c) s ('>' : str) = ((l0, c0), head . words $ reverse s) : getOpens (l, c + 1) str
+getClose (l0, c0) (l, c) s ('\n' : str) = getClose (l0, c0) (l + 1, 0) ('\n' : s) str
+getClose (l0, c0) (l, c) s (chr : str) = getClose (l0, c0) (l, c + 1) (chr : s) str
+
+data Tag = OpenTag String | CloseTag String deriving Show
+
+toTag :: String -> Tag
+toTag ('/' : nm) = CloseTag nm
+toTag nm = OpenTag nm
+
+tagMatch :: [((Int, Int), String)] -> [((Int, Int), Tag)] -> Maybe (((Int, Int), String), ((Int, Int), String))
+tagMatch os ((ps, OpenTag nm ) : ts) = tagMatch ((ps, nm) : os) ts
+tagMatch ((ops, onm) : os) ((ps, CloseTag nm) : ts)
+	| onm == nm = tagMatch os ts
+	| otherwise = Just ((ops, onm), (ps, nm))
+tagMatch [] [] = Nothing
+
+check :: String -> Maybe (((Int, Int), String), ((Int, Int), String))
+check = tagMatch [] . ((toTag `second`) <$>) . filterVoid . getOpens (1, 0)
