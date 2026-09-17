3c6306f6007094d50903c121c4ade23823736c3a
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-09-18 00:05:39 +0900

    my-sign: tools/checkTags.hs

--- other_language/web_extensions/my-sign/tools/checkTags.hs
+++ other_language/web_extensions/my-sign/tools/checkTags.hs
@@ -1,24 +1,30 @@
+{-# OPTIONS_GHC -Wall -fno-warn-tabs -fno-warn-x-partial #-}
+
 module Main where
 
 import Control.Arrow
+import System.Environment
 
 main :: IO ()
-main = putStrLn "foobar"
+main = do
+	[fp] <- getArgs
+	print . check =<< readFile fp
 
 filterVoid :: [((Int, Int), String)] -> [((Int, Int), String)]
 filterVoid = filter $ (`notElem` ["!doctype", "meta", "link", "input", "hr"]) . snd
 
 getOpens :: (Int, Int) -> String -> [((Int, Int), String)]
 getOpens (l, c) ('<' : str) = getClose (l, c) (l, c + 1) "" str
-getOpens (l, c) ('\n' : str) = getOpens (l + 1, 0) str
+getOpens (l, _) ('\n' : str) = getOpens (l + 1, 0) str
 getOpens (l, c) ('\t' : str) = getOpens (l, c + 8) str
 getOpens (l, c) (_ : str) = getOpens (l, c + 1) str
 getOpens _ "" = []
 
 getClose :: (Int, Int) -> (Int, Int) -> String -> String -> [((Int, Int), String)]
 getClose (l0, c0) (l, c) s ('>' : str) = ((l0, c0), head . words $ reverse s) : getOpens (l, c + 1) str
-getClose (l0, c0) (l, c) s ('\n' : str) = getClose (l0, c0) (l + 1, 0) ('\n' : s) str
+getClose (l0, c0) (l, _) s ('\n' : str) = getClose (l0, c0) (l + 1, 0) ('\n' : s) str
 getClose (l0, c0) (l, c) s (chr : str) = getClose (l0, c0) (l, c + 1) (chr : s) str
+getClose _ _ _ [] = error "not closed tag"
 
 data Tag = OpenTag String | CloseTag String deriving Show
 
@@ -32,6 +38,7 @@ tagMatch ((ops, onm) : os) ((ps, CloseTag nm) : ts)
 	| onm == nm = tagMatch os ts
 	| otherwise = Just ((ops, onm), (ps, nm))
 tagMatch [] [] = Nothing
+tagMatch os ts = error $ show os ++ " " ++ show ts
 
 check :: String -> Maybe (((Int, Int), String), ((Int, Int), String))
 check = tagMatch [] . ((toTag `second`) <$>) . filterVoid . getOpens (1, 0)
