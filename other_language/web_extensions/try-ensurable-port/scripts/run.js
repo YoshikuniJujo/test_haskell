b14be73a875f568a487174a5a645851e09afc3e2
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-09-23 14:57:06 +0900

    try-ensurable-port

---
+++ other_language/web_extensions/try-ensurable-port/scripts/run.js
@@ -0,0 +1,12 @@
+import { spawn } from "node:child_process";
+
+const p = spawn(
+	"npx",
+	[
+		"web-ext", "run", "--source-dir", "dist",
+		"--url", "https://yoshikunijujo.github.io/"
+	],
+	{ stdio: "inherit" }
+);
+
+p.on("exit", (code) => process.exit(code ?? 1));
