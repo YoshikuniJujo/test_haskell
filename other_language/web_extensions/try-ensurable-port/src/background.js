82784af71c753fd463c6c6609dbbaf754e5ff951
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-09-24 12:25:34 +0900

    try-ensurable-port: refactoring

--- other_language/web_extensions/try-ensurable-port/src/background.js
+++ other_language/web_extensions/try-ensurable-port/src/background.js
@@ -3,37 +3,9 @@ console.log("BACKGROUND BEGIN");
 browser.runtime.onMessage.addListener((m, s) => {
 	console.log("background.js: receive message:", m, s);
 	switch(m.method) {
-		case "sendMessageToMe":
-			return browser.tabs.sendMessage(s.tab.id, {
-				method: "messageFromBackground" } );
+		case "sendMessageToMe": return sendMessageToMe(s);
 		case "connectToMe":
-			return new Promise((rs, rj) => {
-				console.log("background.js: connectToMe");
-				const port = browser.tabs.connect(s.tab.id,
-					{ name: "port" } );
-				port.onDisconnect.addListener(() => {
-					console.log(
-						"background.js: disconnect:",
-						port);
-					console.log(
-						"background.js: disconnect: " +
-						"port.error:", port.error);
-					if (port.error)
-						rj(new Error(port.error));
-					else rs();
-				});
-				port.onMessage.addListener(m => {
-					console.log(
-						"background.js: port receive:",
-						m );
-					port.disconnect();
-					rs();
-				});
-				console.log("background.js: port is ", port);
-				console.log(
-					"background.js: port.error is ",
-					port.error );
-				port.postMessage("Foo Bar"); });
+			return new Promise((rs, rj) => connectToMe(s, rs, rj));
 		case "openOptionsInTab":
 			(async () => {
 				console.log("background: openInTab");
@@ -46,3 +18,29 @@ browser.runtime.onMessage.addListener((m, s) => {
 			break;
 	}
 });
+
+
+async function
+sendMessageToMe(s)
+{
+	return browser.tabs.sendMessage(s.tab.id, {
+		method: "messageFromBackground" });
+}
+
+async function
+connectToMe(s, rs, rj)
+{
+	console.log("background.js: connectToMe");
+	const port = browser.tabs.connect(s.tab.id, { name: "port" } );
+	port.onDisconnect.addListener(() => {
+		console.log("background.js: disconnect:", port);
+		console.log("background.js: disconnect: port.error:",
+			port.error);
+		if (port.error) rj(new Error(port.error)); else rs(); });
+	port.onMessage.addListener(m => {
+		console.log("background.js: port receive:", m);
+		port.disconnect(); rs(); });
+	console.log("background.js: port is ", port);
+	console.log( "background.js: port.error is ", port.error );
+	port.postMessage("Foo Bar");
+}
